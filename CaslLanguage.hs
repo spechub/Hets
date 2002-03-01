5289f22a6ce88bfe2c7b174b39b11705c6021371
Klaus Luettich <luettich@informatik.uni-bremen.de>
2002-03-01 12:26:35 +0000

    Updated parsing of mixfix ids and compound ids. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@71 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CaslLanguage.hs
+++ CaslLanguage.hs
@@ -19,7 +19,7 @@ casl_letter = ['a'..'z'] ++ ['A'..'Z'] ++
 	      [toEnum(248) .. toEnum(253)] ++ -- icelandic thorn
 	      [toEnum(255)] 
 
-casl_no_bracket_sign = "+-*/|\\&=,`<>!?:$@#^~¡¿×÷£©±¶§¹²³¢º¬µ.·"
+casl_no_bracket_sign = "+-*/|\\&=`<>!?:$@#^~¡¿×÷£©±¶§¹²³¢°¬µ.·"
 
 casl_reserved_words = 
     "and arch as assoc axiom axioms closed comm def else end " ++
@@ -94,18 +94,21 @@ dot_words = try (do { dot  <- char '.'
 -- parsers returning Ids according Id.hs
 simple_id = do { sp <- getPosition
 	       ; id <- identifier
-	       ; return (Id([Token(id,convToPos sp)],[]))
+	       ; return (Id [Token(id,convToPos sp)] [])
 	       }
 
 casl_id = comp_id -- <|> token_id
  
 token_id = do { tok <- try CaslLanguage.token
-	      ; return (Id([tok],[]))
+	      ; return (Id [tok] [])
 	      }
 
 token = do { sp  <- getPosition 
-	   ; tok <-  choice [identifier,lexeme dot_words,operator, 
-			     fmap (\x -> show x) (digit <|> charLiteral)]
+	   ; tok <-  choice [identifier,
+			     lexeme dot_words,
+			     operator, 
+			     fmap (\x -> show x) (charLiteral),
+			     fmap (:"") (digit)]
 	   ; return (Token(tok,convToPos sp))
 	   }
 
@@ -115,15 +118,100 @@ place = (do { sp  <- getPosition
 	    }
 	 ) <?> "place"
 
-mixfix_id = do { ids <- many1 (try CaslLanguage.token <|> 
-			      try CaslLanguage.place)
-	       ; return (Id(ids,[]))
-	       }
+place_token = do { pla <- try CaslLanguage.place
+		 ; option ([pla]) (do { tok <- try CaslLanguage.token
+				      ; return [pla,tok]
+				      })
+		 }
 
-comp_id = do { mid <- mixfix_id
-             ; ts <- return (case mid of Id(t,_)-> t)
-             ; option (Id(ts, [])) 
-               (do {cs <- squares (sepBy1 comp_id comma);
-                    return (Id(ts, cs))
+
+mixfix_id accum = do { ts <- (try (fmap (:[]) CaslLanguage.place) 
+			      <|>
+			      try (fmap (:[]) CaslLanguage.token) 
+			      <|>
+			      special_between (symbol "{") (symbol "}") 
+			      (mkTokLst (mixfix_id []))
+			      <|>
+			      try (special_between (symbol "[") (symbol "]") 
+				   (mkTokLst (mixfix_id [])))
+			      <|>
+			      bracket_pair "{" "}"
+			      <|>
+			      bracket_pair "[" "]"
+			      <?> "mixfix id"
+			     )
+		     ; sqs <- option [] (lookAhead (fmap (:[]) (symbol "[")))
+		     ; fts <- option [] (lookAhead 
+					 (fmap (:[]) (CaslLanguage.token)))
+		     ; Id ats cs <- 
+		       let 
+		       accum_ts = (accum ++ ts)
+		       is_last_op = (case (last accum_ts) of 
+				     t@(Token(s,_)) -> not
+				                       ((last s) `elem` "[{}]"
+							||
+							isPlace (t)) )
+		       in
+		       if sqs == [] then
+		       (if is_last_op then
+			(if fts == [] then
+			 option (Id accum_ts []) (mixfix_id accum_ts)
+			 else
+			 unexpected "token follows token" 
+			)
+			else -- not is_last_op
+			option (Id accum_ts []) (mixfix_id accum_ts)
+		       )
+		       else -- sqs \= []
+		       (if isPlace (last accum_ts) then
+			option (Id accum_ts []) (mixfix_id accum_ts)
+			else
+			return (Id accum_ts [])
+		       )
+		     ; return (Id ats [])
+		     }
+    where mkTokLst p = fmap (\(Id ts cs) -> ts) p
+	  bracket_pair open close = do { o_sp <- getPosition
+				       ; o <- try (symbol open)
+				       ; c_sp <- getPosition
+				       ; c <- try (symbol close)
+				       ; return [Token(o,convToPos o_sp),
+						 Token(c,convToPos c_sp)]
+				       }		
+
+special_between start end p = do { st_sp <- getPosition
+				 ; st <- start
+				 ; res <- special_manyTill p end
+				 ; return ([(Token(st,convToPos st_sp))] ++
+					   res) -- res also contains end
+				 }
+
+special_manyTill p end = scan
+    where scan  = do{ sp <- getPosition
+		    ; e <- end
+		    ; return [Token(e,convToPos sp)] }
+		  <|>
+		  do{ x <- p
+		    ; xs <- scan
+		    ; return (x ++ xs) }
+
+
+comp_id = do { Id ts _ <- test_mixfix_id (mixfix_id [])
+             ; Id ts cs <- option (Id ts []) 
+               (do { cs <- squares (sepBy1 comp_id comma)
+                   ; return (Id ts cs)
                    })
+	     ; option (Id ts cs) 
+	       (do { ps <- many CaslLanguage.place
+		   ; return (Id (ts++ps) cs)
+		   })
              }
+    where test_mixfix_id p = do { id@(Id ts _) <- p
+				; if (case ts of [] -> False
+				                 [a] -> not (isPlace a)
+				                 otherwise -> True) 
+				  then
+				  return id
+				  else
+				  fail "only one place is not a legal mixfix id "
+				}
