06efb096bf27e866de501f440301b367b6a07648
Klaus Luettich <luettich@informatik.uni-bremen.de>
2002-03-27 12:10:03 +0000

    Added a new ATerm datastructure based on FiniteMaps for a less memory usage. Added three instances for ATermConversion: Integer, List and String. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@115 cec4b9c1-7d33-0410-9eda-942365e851bb

--- haterm-1.0/src/ATermConversion.hs
+++ haterm-1.0/src/ATermConversion.hs
@@ -2,13 +2,78 @@ module ATermConversion where
 
 import ATermAbstractSyntax
 import ATermReadWrite
+import List (find,mapAccumL)
 
 class ATermConvertible t where
-  toATerm   :: t -> ATerm
-  fromATerm :: ATerm -> t
+  toATerm   :: ATermTable -> t -> (ATermTable,ATerm)
+  toATerm1  :: ATermTable -> t -> ATermTable
+  toATerm1 at t = let (at',_) = toATerm at t in at' 
+  fromATerm :: ATermTable -> t
+  
 
-toATermString t		= writeATerm (toATerm t)
-toSharedATermString t	= writeSharedATerm (toATerm t)
-fromATermString s 	= fromATerm (readATerm s)
+toATermString at t	 = writeATerm       (toATerm1 at t)
+toSharedATermString at t = writeSharedATerm (toATerm1 at t)
+fromATermString s 	 = fromATerm (readATerm s)
 
-fromATermError t u = error ("Cannot convert ATerm to "++t++": "++(show u))
+toATermString1 t         = writeATerm (toATerm1 emptyATermTable t)
+toSharedATermString1 t   = writeSharedATerm (toATerm1 emptyATermTable t)
+
+fromATermError t u = error ("Cannot convert ATerm to "++t++": "++(err u))
+    where err u = case u of 
+		  AAppl s _ -> "!AAppl "++s
+		  AList _   -> "!AList"
+		  otherwise -> "!AInt"
+
+{- for Integer derive : ATermConvertible hand written!-}
+
+instance ATermConvertible Integer where
+    toATerm at x        = addATerm (AInt x) at
+    fromATerm at = case aterm of 
+		   (AInt x)  -> x
+		   otherwise -> fromATermError "Integer" aterm
+	where aterm = getATerm at
+
+
+toATermStr at s      = addATerm (AAppl s' []) at
+    where s'  = concat ["\"",s'',"\""]
+	  s'' = concatMap conv s
+	  conv '\"' = "\\\""
+	  conv '\n' = "\\\n"
+	  conv '\t' = "\\\t"
+	  conv '\\' = "\\\\"
+	  conv x    = [x]
+fromATermStr at = case aterm of
+		  (AAppl s []) -> conv s'
+		      where s' = case s of
+				  ('\"':so) -> case reverse so of
+			                         ('\"':sr) -> reverse sr
+						 _         -> err
+				  _         -> err
+			    conv ('\\':x:xs) = 
+				 if x == '\n' then x:(conv xs)
+				 else err
+			    conv (x:xs)      = x:(conv xs)
+		  otherwise    -> err 
+    where aterm = getATerm at
+	  err   = fromATermError "String" aterm
+
+instance ATermConvertible a => ATermConvertible [a] where
+    toATerm at l       = addATerm (AList l') at'
+	where (at',l') = List.mapAccumL toATerm at l
+    fromATerm at = case aterm of
+		   (AList l) -> map conv l
+		   otherwise -> fromATermError "[a]" aterm
+	where aterm  = getATerm at
+	      conv t = fromATerm (getATermByIndexSp1 t at)
+
+
+--- some helpers needed and used by DrIFT instances ---------------------------
+-- throws an error in case that there is no ATerm in the list
+findATerm :: [Maybe ATerm] -> ATerm 
+findATerm l = case List.find just l of
+				    (Just(Just t)) -> t
+				    otherwise      -> error "No aterm found"
+    where just mt = case mt of
+		    (Just _) -> True
+		    Nothing  -> False
+-------------------------------------------------------------------------------
--- haterm-1.0/src/ATermReadWrite.hs
+++ haterm-1.0/src/ATermReadWrite.hs
@@ -31,84 +31,94 @@ import ATermAbstractSyntax
 
 -- added by KL
 import Char
-import Memo
 
+import Memo
+import FiniteMap
+import List
 --- From String to ATerm ------------------------------------------------------
 
-readATerm ('!':str)	= let (t,_,_,_) = readTAF str [] 0 in t
-readATerm str		= let (t,_)   = readAT str       in t
+readATerm ('!':str)	= 
+    let ((at,_),_,_,_) = readTAF emptyATermTable str emptyTable 0 in at
+readATerm str		= 
+    let ((at,_),_)     = readAT  emptyATermTable str      in at
 
                                                                -- non-shared --
 
-readAT ('[':str)	=  let (kids, str') = readATs (dropSpaces str)
-			   in (AList kids, str')
-readAT str@(c:cs)
+readAT at ('[':str)	=  let ((at',kids), str') = readATs at (dropSpaces str)
+			   in (addATerm (AList kids) at', str')
+readAT at str@(c:cs)
   | isIntHead c		=  let (i,str') = span isDigit cs
-                           in (AInt (read (c:i)),str')
+                           in (addATerm (AInt (read (c:i))) at,str')
   | otherwise		=  let (c,str')      = readAFun str
-		   	       (kids, str'') = readParenATs  (dropSpaces str')
- 			   in (AAppl c kids, str'')
+		   	       ((at',kids), str'') = readParenATs at 
+	                                               (dropSpaces str')
+ 			   in (addATerm (AAppl c kids) at', str'')
 
 readAFun ('"':str)	=  let (c,('"':str')) = spanNotQuote' str 
                            in (quote c,str')
 readAFun str		=  spanAFunChar str
 
-readParenATs ('(':str)	=  readATs (dropSpaces str)
-readParenATs str	=  ([],str)
+readParenATs at ('(':str) =  readATs at (dropSpaces str)
+readParenATs at str	  =  ((at,[]),str)
 
 
-readATs (')':str)	=  ([],str)
-readATs (']':str)	=  ([],str)
-readATs str		=  readATs1 str
+readATs at (')':str)	=  ((at,[]),str)
+readATs at (']':str)	=  ((at,[]),str)
+readATs at str		=  readATs1 at str
 
-readATs1 str		=  let (t,str')   = readAT (dropSpaces str)
-			       (ts,str'') = readATs' (dropSpaces str')
-			   in (t:ts,str'')
+readATs1 at str		=  let ((at',t),str')    = readAT at (dropSpaces str)
+			       ((at'',ts),str'') = readATs' at' 
+						             (dropSpaces str')
+			   in ((at'',t:ts),str'')
 
-readATs' (',':str)	= readATs1 (dropSpaces str)
-readATs' (')':str)	= ([],str)
-readATs' (']':str)	= ([],str)
+readATs' at (',':str)	= readATs1 at (dropSpaces str)
+readATs' at (')':str)	= ((at,[]),str)
+readATs' at (']':str)	= ((at,[]),str)
 
                                                                    -- shared --
 
-readTAF ('#':str) tbl l	=  let (i,str') = spanAbbrevChar str
-			   in (getElement (deAbbrev i) tbl, str',tbl,
-			       l+(length i)+1)    
-readTAF ('[':str) tbl l	=  let (kids, str',tbl',l') = readTAFs 
-                                                      (dropSpaces str) tbl 1
-	                       t     = AList kids	                    
-	                       tbl'' = condAddElement t l' tbl'
-			   in (t, str',tbl'',l+l')
-readTAF str@(x:xs) tbl l 
-  | isIntHead x         =  let (i,str') = span isDigit xs
-                               l'       = length (x:i)
-                               t        = AInt (read (x:i)) 
-	                       tbl'     = condAddElement t l' tbl
-                           in (t,str',tbl', l+l')
+readTAF at ('#':str) tbl l =  let (i,str') = spanAbbrevChar str
+			      in (addATerm (getElement (deAbbrev i) tbl) at, 
+				  str',tbl,
+				  l+(length i)+1)    
+readTAF at ('[':str) tbl l =  let ((at',kids), str',tbl',l') = readTAFs at
+                                                         (dropSpaces str) tbl 1
+	                          t            = AList kids       
+	                          tbl''        = condAddElement t l' tbl'
+	                          at_t         = addATerm t at'
+			   in (at_t, str',tbl'',l+l')
+readTAF at str@(x:xs) tbl l 
+  | isIntHead x         =  let (i,str')   = span isDigit xs
+                               l'         = length (x:i)
+                               t          = AInt (read (x:i)) 
+	                       tbl'       = condAddElement t l' tbl
+	                       at_t       = addATerm t at
+                           in (at_t,str',tbl', l+l')
   | otherwise           =  let (c,str')           = readAFun str
-		   	       (kids, str'',tbl',l') = readParenTAFs 
+		   	       ((at',kids), str'',tbl',l') = readParenTAFs at
                                                         (dropSpaces str') tbl 0
 			       t                  = AAppl c kids
-                               l1' = if null kids then 0 else l'
-	                       l''    = (length c) + l1'
+	                       l''    = (length c) + l'
 	                       tbl''  = condAddElement t l'' tbl'
- 			   in (t, str'',tbl'',l'')
+	                       at_t   = addATerm t at'
+ 			   in (at_t, str'',tbl'',l'')
 
-readParenTAFs ('(':str)	tbl l	=  readTAFs (dropSpaces str) tbl l
-readParenTAFs str tbl l		=  ([],str,tbl,l)
+readParenTAFs at ('(':str)	tbl l	=  readTAFs at (dropSpaces str) tbl l
+readParenTAFs at str tbl l		=  ((at,[]),str,tbl,l)
 
-readTAFs (')':str) tbl l =  ([],str,tbl,l+1)
-readTAFs (']':str) tbl l =  ([],str,tbl,l+1)
-readTAFs str tbl l       =  readTAFs1 str tbl l
+readTAFs at (')':str) tbl l =  ((at,[]),str,tbl,l+1)
+readTAFs at (']':str) tbl l =  ((at,[]),str,tbl,l+1)
+readTAFs at str tbl l       =  readTAFs1 at str tbl l
 
-readTAFs1 str tbl l   =  let (t,str',tbl',l')= readTAF (dropSpaces str) tbl l
-			     (ts,str'',tbl'',l'') = readTAFs' 
-                                                (dropSpaces str') tbl' l'
-			   in (t:ts,str'',tbl'',l'')
+readTAFs1 at str tbl l = let ((at',t),str',tbl',l')= readTAF at 
+						 (dropSpaces str) tbl l
+			     ((at'',ts),str'',tbl'',l'') = readTAFs' at'
+                                                 (dropSpaces str') tbl' l'
+			   in ((at'',t:ts),str'',tbl'',l'')
 
-readTAFs' (',':str) tbl	l = readTAFs1 (dropSpaces str) tbl (l+1)
-readTAFs' (')':str) tbl	l = ([],str,tbl,l+1)
-readTAFs' (']':str) tbl	l = ([],str,tbl,l+1)
+readTAFs' at (',':str) tbl	l = readTAFs1 at (dropSpaces str) tbl (l+1)
+readTAFs' at (')':str) tbl	l = ((at,[]),str,tbl,l+1)
+readTAFs' at (']':str) tbl	l = ((at,[]),str,tbl,l+1)
 
                                                                   -- helpers --
 
@@ -143,36 +153,60 @@ condAddElement t l tbl =
 
 --- From ATerm to String  -----------------------------------------------------
 
-writeATerm t            = writeAT t
-writeSharedATerm t	= let (s,_) = writeTAF t [] in '!':s
+writeATerm at           = writeAT at 
+writeSharedATerm at	= let (s,_) = writeTAF at emptyTable in '!':s
                                                                -- non-shared --
 
-writeAT 		:: ATerm -> String
-writeAT (AAppl c ts)	=  writeATermAux c (map writeAT ts)
-writeAT (AList ts)	=  bracket (commaSep (map writeAT ts))
-writeAT (AInt i)	=  show i
+writeAT        :: ATermTable -> String
+writeAT at     =  
+    case (getATerm at) of
+	     (AAppl c ts) -> writeATermAux c (writeAT' at ts)  
+	     (AList ts)   -> bracket (commaSep (writeAT' at ts)) 
+	     (AInt i)     -> (show i)
+	     (AIndex _)   -> error err_wrong_store
 
-                                                                   -- shared --
+writeAT' at []     = []
+writeAT' at (t:ts) = 
+    case t of 
+    (AIndex i) -> let (at',t')   = getATermByIndex i at
+		      str = case t' of
+			    (AIndex _) -> error err_ref_index
+			    otherwise  -> writeAT at'
+		      strs = writeAT' at ts
+		  in str:strs
+    otherwise -> error err_wrong_store
+  						                   -- shared --
 
-writeTAF		:: ATerm -> Table -> (String,Table)
-writeTAF t tbl          =  case indexOf t tbl of
-				(Just i) -> (abbrev i,tbl)
-				Nothing  -> (str, 
-					     condAddElement t 
-					                    (length str) 
-					                    tbl') 
-					    where (str,tbl') = writeTAF' t tbl
+writeTAF :: ATermTable -> Table	-> (String,Table)
+writeTAF at tbl =  
+    case indexOf t tbl of
+	     (Just i) -> (abbrev i,tbl)
+	     Nothing  -> (str,
+			  condAddElement t
+				         (length str) 
+				         tbl') 
+		  where (str,tbl') = writeTAF' at tbl
+    where t = getATerm at
 
-writeTAF' (AAppl c ts) tbl	= let (kids,tbl') = writeTAFs ts tbl
-           			  in (writeATermAux c kids,tbl')
-writeTAF' (AList ts) tbl	= let (kids,tbl') = writeTAFs ts tbl
-				  in (bracket (commaSep kids),tbl')
-writeTAF' (AInt i) tbl		= (show i,tbl)
+writeTAF' at tbl = 
+    case getATerm at of
+	     (AAppl c ts) -> let (kids,tbl') = writeTAFs at ts tbl
+           		     in (writeATermAux c kids,tbl')
+	     (AList ts)   -> let (kids,tbl') = writeTAFs at ts tbl
+			     in (bracket (commaSep kids),tbl')
+	     (AInt i)     -> (show i,tbl)
+	     (AIndex _)   -> error err_wrong_store
 
-writeTAFs [] tbl		=  ([],tbl)
-writeTAFs (t:ts) tbl	=  let (str,tbl')   = writeTAF t tbl
-			       (strs,tbl'') = writeTAFs ts tbl'
-			   in ((str:strs),tbl'')
+writeTAFs at [] tbl	=  ([],tbl)
+writeTAFs at (t:ts) tbl =  
+    case t of
+    (AIndex i) -> let (at',t')   = getATermByIndex i at
+		      (str,tbl') = case t' of
+				   (AIndex _) -> error err_ref_index
+				   otherwise  -> writeTAF at' tbl
+		      (strs,tbl'') =  writeTAFs at ts tbl'
+		  in ((str:strs),tbl'')
+    otherwise -> error err_wrong_store
 
                                                                   -- helpers --
  
@@ -188,6 +222,18 @@ parenthesise str	= "("++str++")"
 
 --- Tables of ATerms ----------------------------------------------------------
 
+type Table		=  ATermTable
+emptyTable		=  emptyATermTable
+indexOf t tbl           =  case getATermIndex t tbl of
+			   (-1) -> Nothing
+			   i    -> Just i
+
+lengthTable (_,_,i)     =  i
+
+addElement t tbl	=  addATerm1 t tbl
+getElement i tbl	=  let (_,t)    = getATermByIndex i tbl in t
+ 
+{-
 type Table		=  [ATerm]
 emptyTable		=  []
 indexOf t []		=  Nothing
@@ -204,7 +250,7 @@ getElement i tbl	=  tbl!!!i
 (_:xs) !!! n | n>0 =  xs !!! (n-1)
 (_:_)  !!! _       =  error "!!!: negative index"
 []     !!! _       =  error "!!!: index too large"
-
+-}
 --- Base 64 encoding ----------------------------------------------------------
 
 mkAbbrev x
@@ -213,12 +259,12 @@ mkAbbrev x
 
 mkAbbrevAux x
   | x == 0	= []
-  | x > 0	= (toBase64!!!m:mkAbbrevAux d) where (d,m) = divMod x 64
+  | x > 0	= (toBase64!!m:mkAbbrevAux d) where (d,m) = divMod x 64
 
 deAbbrev x		=  deAbbrevAux (reverse x)
 
 deAbbrevAux []		=  0
-deAbbrevAux (c:cs)	=  let (Just i) = indexOf c toBase64
+deAbbrevAux (c:cs)	=  let (Just i) = elemIndex c toBase64
                	               r        = deAbbrevAux cs
 			   in (i + 64*r)
 
@@ -234,6 +280,6 @@ toBase64 =
 abbrev = memo abbrev' 
     where abbrev' i = "#"++mkAbbrev i
 
-next_abbrev tbl = abbrev (toInteger (length tbl)+1)
+next_abbrev tbl = abbrev ((lengthTable tbl)+1)
 
 -------------------------------------------------------------------------------
