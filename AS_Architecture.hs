32d102316101552372f108ec8905b45b8c88e6bf
Klaus Luettich <luettich@informatik.uni-bremen.de>
2002-04-02 10:25:16 +0000

    A first attemt to fix the abstract syntax. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@123 cec4b9c1-7d33-0410-9eda-942365e851bb

--- AS_Architecture.hs
+++ AS_Architecture.hs
@@ -14,48 +14,43 @@ import AS_Structured
 import Grothendieck
 import Id
 
-data ARCH_SPEC_DEFN = Arch_spec_defn(ARCH_SPEC_NAME,ARCH_SPEC)
+data ARCH_SPEC_DEFN = Arch_spec_defn ARCH_SPEC_NAME ARCH_SPEC [Pos]
 		      deriving (Show,Eq)
 
-data ARCH_SPEC = Basic_arch_spec([UNIT_DECL_DEFN],RESULT_UNIT)
-	       | Arch_spec_name ARCH_SPEC_NAME
+data ARCH_SPEC = Basic_arch_spec [UNIT_DECL_DEFN] UNIT_EXPRESSION [Pos]
+	       | Arch_spec_name ARCH_SPEC_NAME [Pos]
 		 deriving (Show,Eq)
 
-data UNIT_DECL_DEFN = Unit_decl(UNIT_NAME,UNIT_SPEC,UNIT_IMPORTED)
-		    | UNIT_DEFN UNIT_DEFN
+data UNIT_DECL_DEFN = Unit_decl UNIT_NAME UNIT_SPEC UNIT_IMPORTED [Pos]
+		    | Unit_defn UNIT_NAME UNIT_EXPRESSION [Pos]
 		      deriving (Show,Eq)
 
-data UNIT_DEFN = Unit_defn(UNIT_NAME,UNIT_EXPRESSION)
-		 deriving (Show,Eq)
-data UNIT_IMPORTED = Unit_imported [UNIT_TERM]
+data UNIT_IMPORTED = Unit_imported [UNIT_TERM] [Pos]
 		     deriving (Show,Eq)
 
-data UNIT_SPEC_DEFN = Unit_spec_defn(SPEC_NAME,UNIT_SPEC)
+data UNIT_SPEC_DEFN = Unit_spec_defn SPEC_NAME UNIT_SPEC [Pos]
 		      deriving (Show,Eq)
 
-data UNIT_SPEC = Unit_type([SPEC],SPEC)
-	       | Spec_name SPEC_NAME
-	       | Arch_unit_spec ARCH_SPEC
-	       | Closed_unit_spec UNIT_SPEC 
+data UNIT_SPEC = Unit_type [SPEC] SPEC [Pos]
+	       | Spec_name SPEC_NAME [Pos]
+	       | Arch_unit_spec ARCH_SPEC [Pos]
+	       | Closed_unit_spec UNIT_SPEC [Pos]
 		 deriving (Show,Eq)
 
-data RESULT_UNIT = Result_unit UNIT_EXPRESSION
-		   deriving (Show,Eq)
-
-data UNIT_EXPRESSION = Unit_expression([UNIT_BINDING],UNIT_TERM)
+data UNIT_EXPRESSION = Unit_expression [UNIT_BINDING] UNIT_TERM [Pos]
 		       deriving (Show,Eq)
 
-data UNIT_BINDING = Unit_binding(UNIT_NAME,UNIT_SPEC)
+data UNIT_BINDING = Unit_binding UNIT_NAME UNIT_SPEC [Pos]
 		    deriving (Show,Eq) 
 
-data UNIT_TERM = Unit_reduction(UNIT_TERM,RESTRICTION)
-	       | Unit_translation(UNIT_TERM,RENAMING)
-	       | Amalgamation [UNIT_TERM]
-	       | Local_unit([UNIT_DEFN],UNIT_TERM)
-	       | Unit_appl(UNIT_NAME,[FIT_ARG_UNIT])
+data UNIT_TERM = Unit_reduction UNIT_TERM RESTRICTION [Pos]
+	       | Unit_translation UNIT_TERM RENAMING [Pos]
+	       | Amalgamation [UNIT_TERM] [Pos]
+	       | Local_unit [UNIT_DECL_DEFN] UNIT_TERM [Pos] 
+	       | Unit_appl UNIT_NAME [FIT_ARG_UNIT] [Pos]
 		 deriving (Show,Eq)
 
-data FIT_ARG_UNIT = Fit_arg_unit(UNIT_TERM,G_symb_map_items_list)
+data FIT_ARG_UNIT = Fit_arg_unit UNIT_TERM G_symb_map_items_list [Pos] 
 		    deriving (Show,Eq)
 
 type ARCH_SPEC_NAME = SIMPLE_ID
