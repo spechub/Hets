af8711f6f0f04d7897f856a1444f6407e173199c
Klaus Luettich <luettich@informatik.uni-bremen.de>
2002-04-02 10:26:00 +0000

    A first attempt to fix the abstrct syntax, but without Annotations. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@124 cec4b9c1-7d33-0410-9eda-942365e851bb

--- AS_Library.hs
+++ AS_Library.hs
@@ -14,36 +14,37 @@ import AS_Structured
 import Grothendieck
 import Id
 
-data LIB_DEFN = Lib_defn(LIB_NAME,[LIB_ITEM])
+data LIB_DEFN = Lib_defn LIB_NAME [LIB_ITEM] [Pos]
 		deriving (Show,Eq)
 
-data LIB_ITEM = Spec_defn(SPEC_NAME,GENERICITY,SPEC)
---	      | SPEC_DEFN SPEC_DEFN
---	      | VIEW_DEFN
-	      | View_defn(VIEW_NAME,GENERICITY,VIEW_TYPE,G_symb_map_items_list)
---	      | ARCH_SPEC_DEFN ARCH_SPEC_DEFN
-	      | Arch_spec_defn(ARCH_SPEC_NAME,ARCH_SPEC)
---	      | UNIT_SPEC_DEFN UNIT_SPEC_DEFN
-	      | Unit_spec_defn(SPEC_NAME,UNIT_SPEC)
---	      | DOWNLOAD_ITEMS DOWNLOAD_ITEMS
-	      | Download_items (LIB_NAME,[ITEM_NAME_OR_MAP])
+data LIB_ITEM = Spec_defn SPEC_NAME GENERICITY SPEC [Pos] 
+
+	      | View_defn VIEW_NAME GENERICITY VIEW_TYPE 
+		          G_symb_map_items_list [Pos] 
+
+	      | Arch_spec_defn ARCH_SPEC_NAME ARCH_SPEC [Pos]
+
+	      | Unit_spec_defn SPEC_NAME UNIT_SPEC [Pos]
+
+	      | Download_items  LIB_NAME [ITEM_NAME_OR_MAP] [Pos] 
 		deriving (Show,Eq)
 
 data ITEM_NAME_OR_MAP = Item_name ITEM_NAME 
-		      | Item_name_map(ITEM_NAME,ITEM_NAME)
+		      | Item_name_map ITEM_NAME ITEM_NAME [Pos]
 			deriving (Show,Eq)
 
 type ITEM_NAME = SIMPLE_ID
 
-data LIB_NAME = Lib_version(LIB_ID,VERSION_NUMBER)
-	      | Lib_id LIB_ID
+data LIB_NAME = Lib_version LIB_ID VERSION_NUMBER [Pos]
+	      | Lib_id LIB_ID [Pos]
 		deriving (Show,Eq)
 
-data LIB_ID = Direct_link URL
-	    | Indirect_link PATH 
+data LIB_ID = Direct_link URL [Pos]
+	    | Indirect_link PATH [Pos]
 	      deriving (Show,Eq)
 
-data VERSION_NUMBER = Version_number [Int] deriving (Show,Eq)
+data VERSION_NUMBER = Version_number [Int] [Pos] 
+		      deriving (Show,Eq) 
 
 type URL = String
 type PATH = String
@@ -52,36 +53,36 @@ type PATH = String
 cast_S_L_Spec_defn :: SPEC_DEFN -> LIB_ITEM 
 cast_L_S_Spec_defn :: LIB_ITEM  -> SPEC_DEFN
 
-cast_S_L_Spec_defn (AS_Structured.Spec_defn (x,y,z)) = 
-    (AS_Library.Spec_defn (x,y,z)) 
+cast_S_L_Spec_defn (AS_Structured.Spec_defn x y z p) = 
+    (AS_Library.Spec_defn x y z p) 
 
-cast_L_S_Spec_defn (AS_Library.Spec_defn (x,y,z)) =
-    (AS_Structured.Spec_defn (x,y,z))
+cast_L_S_Spec_defn (AS_Library.Spec_defn x y z p) =
+    (AS_Structured.Spec_defn x y z p)
 
 cast_S_L_View_defn :: VIEW_DEFN -> LIB_ITEM 
 cast_L_S_View_defn :: LIB_ITEM  -> VIEW_DEFN
 
-cast_S_L_View_defn (AS_Structured.View_defn (w,x,y,z)) = 
-    (AS_Library.View_defn (w,x,y,z)) 
+cast_S_L_View_defn (AS_Structured.View_defn w x y z p) = 
+    (AS_Library.View_defn w x y z p) 
 
-cast_L_S_View_defn (AS_Library.View_defn (w,x,y,z)) =
-    (AS_Structured.View_defn (w,x,y,z))
+cast_L_S_View_defn (AS_Library.View_defn w x y z p) =
+    (AS_Structured.View_defn w x y z p)
 
 cast_A_L_Arch_spec_defn :: ARCH_SPEC_DEFN -> LIB_ITEM
 cast_L_A_Arch_spec_defn :: LIB_ITEM       -> ARCH_SPEC_DEFN
 
-cast_A_L_Arch_spec_defn (AS_Architecture.Arch_spec_defn (x,y)) =
-    (AS_Library.Arch_spec_defn (x,y))
+cast_A_L_Arch_spec_defn (AS_Architecture.Arch_spec_defn x y p) =
+    (AS_Library.Arch_spec_defn x y p)
 
-cast_L_A_Arch_spec_defn (AS_Library.Arch_spec_defn (x,y)) =
-    (AS_Architecture.Arch_spec_defn (x,y))
+cast_L_A_Arch_spec_defn (AS_Library.Arch_spec_defn x y p) =
+    (AS_Architecture.Arch_spec_defn x y p)
 
 cast_A_L_Unit_spec_defn :: UNIT_SPEC_DEFN -> LIB_ITEM
 cast_L_A_Unit_spec_defn :: LIB_ITEM       -> UNIT_SPEC_DEFN
 
-cast_A_L_Unit_spec_defn (AS_Architecture.Unit_spec_defn (x,y)) =
-    (AS_Library.Unit_spec_defn (x,y))
+cast_A_L_Unit_spec_defn (AS_Architecture.Unit_spec_defn x y p) =
+    (AS_Library.Unit_spec_defn x y p)
 
-cast_L_A_Unit_spec_defn (AS_Library.Unit_spec_defn (x,y)) =
-    (AS_Architecture.Unit_spec_defn (x,y))
+cast_L_A_Unit_spec_defn (AS_Library.Unit_spec_defn x y p) =
+    (AS_Architecture.Unit_spec_defn x y p)
 
