be387b5f8cbbd994a5656db4f934d1991baa69a4
Klaus Luettich <luettich@informatik.uni-bremen.de>
2002-04-02 10:23:22 +0000

    An first attempt to fix the abstract syntax tree for CASL basic specs. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@121 cec4b9c1-7d33-0410-9eda-942365e851bb

--- AS_Basic_CASL.hs
+++ AS_Basic_CASL.hs
@@ -1,134 +1,153 @@
 module AS_Basic where
 
+import Id
+import AS_Annotation 
+
 data BASIC_SPEC = Basic_spec [BASIC_ITEMS]
 		  deriving (Show,Eq)
 
 data BASIC_ITEMS = Sig_items SIG_ITEMS
-		 | Free_datatype [DATATYPE_DECL]
-		 | Sort_gen [SIG_ITEMS]
-		 | Var_items [VAR_DECL]
-		 | Local_var_axioms([VAR_DECL],[TERM])
-		 | Axiom_items [TERM]
+		 | Free_datatype [DATATYPE_DECL] [Pos] 
+		 | Sort_gen [SIG_ITEMS] [Pos]
+		 | Var_items [VAR_DECL] [Pos]
+		 | Local_var_axioms [VAR_DECL] [FORMULA] [Pos]
+		 | Axiom_items [AXIOM] [Pos]
 		   deriving (Show,Eq)
 
-data SIG_ITEMS = Sort_items [SORT_ITEM] 
-	       | Op_items [OP_ITEM]
-	       | Pred_items [OP_ITEM]
-	       | Datatype_items [DATATYPE_DECL]
+data SIG_ITEMS = Sort_items [SORT_ITEM] [Pos] [Annotation]
+	       | Op_items [OP_ITEM] [Pos] [Annotation]
+	       | Pred_items [OP_ITEM] [Pos] [Annotation]
+	       | Datatype_items [DATATYPE_DECL] [Annotation]
 		 deriving (Show,Eq)
 
-data SORT_ITEM = Sort_decl [TYPE]
-	       | Sub_sort_decl([TYPE],TYPE)
-	       | Iso_decl [TYPE]
-	       | Subsort_defn(TYPE,VAR,TYPE,TERM)
+data SORT_ITEM = Sort_decl [SORT] [Pos] [Annotation]
+	       | Sub_sort_decl [SORT] SORT [Pos] [Annotation]
+	       | Subsort_defn SORT VAR SORT FORMULA [Pos] [Annotation]
+	       | Iso_decl [SORT] [Pos] [Annotation]
 		 deriving (Show,Eq)
 
-data OP_ITEM = Op_decl([OP_NAME],TYPE,[OP_ATTR])
-	     | Op_defn(OP_NAME,TYPE,TERM)
+data OP_ITEM = Op_decl [OP_NAME] OP_TYPE [OP_ATTR] [Pos] [Annotation]
+	     | Op_defn OP_NAME OP_HEAD TERM [Pos] [Annotation]
 	       deriving (Show,Eq)
 
-data TYPE = Type(TYPE_NAME,[TYPE])
-	    deriving (Show,Eq)
+data OP_TYPE = Total_op_type [SORT] SORT [Pos] 
+	     | Partial_op_type [SORT] SORT [Pos]
+	       deriving (Show,Eq)
+
+data OP_HEAD = Total_op_head [ARG_DECL] SORT [Pos]
+	     | Partial_op_head [ARG_DECL] SORT [Pos]
+	       deriving (Show,Eq)
+
+data ARG_DECL = Arg_decl [VAR] SORT [Pos]
+		deriving (Show,Eq)
 
 data OP_ATTR = Assoc_op_attr | Common_op_attr | Idem_op_attr
 	     | Unit_op_attr TERM
 	       deriving (Show,Eq)
-{- Aufgegangen in OP_ITEM
-data PRED_ITEM = Pred_decl([PRED_NAME],PRED_TYPE)
-	       | Pred_defn(PRED_NAME,PRED_HEAD,FORMULA)
+
+data PRED_ITEM = Pred_decl [PRED_NAME] PRED_TYPE [Pos] [Annotation]
+	       | Pred_defn PRED_NAME PRED_HEAD FORMULA [Pos] [Annotation]
 		 deriving (Show,Eq)
 
-data PRED_TYPE = Pred_type [SORT]
+data PRED_TYPE = Pred_type [SORT] [Pos]
 		 deriving (Show,Eq)
 
-data PRED_HEAD = Pred_head [ARG_DECL]
+data PRED_HEAD = Pred_head [ARG_DECL] [Pos]
 		 deriving (Show,Eq)
--}
-data DATATYPE_DECL = Datatype_decl(TYPE,[ALTERNATIVE])
+
+data DATATYPE_DECL = Datatype_decl SORT [ALTERNATIVE] [Pos] 
 		     deriving (Show,Eq)
 
-data ALTERNATIVE = Construct(TOTALITY,OP_NAME,[COMPONENTS])
-		 | Subsorts [TYPE]
+data ALTERNATIVE = Total_construct OP_NAME [COMPONENTS] [Pos] [Annotation]
+		 | Partial_construct OP_NAME [COMPONENTS] [Pos] [Annotation]
+		 | Subsorts [SORT] [Pos] [Annotation]
 		   deriving (Show,Eq)
 
-data COMPONENTS = Select(TOTALITY,[OP_NAME],TYPE)
-		| No_select TYPE
+data COMPONENTS = Total_select [OP_NAME] SORT [Pos]
+		| Patial_select [OP_NAME] SORT [Pos] 
+		| Sort SORT
 		  deriving (Show,Eq)
 
-data VAR_DECL = Var_decl([VAR],TYPE)
+data VAR_DECL = Var_decl [VAR] SORT [Pos]
 		deriving (Show,Eq)
 
--- The Mixfix-constructor also contains 
+data AXIOM = Axiom FORMULA [Pos] [Annotation] 
+	     deriving (Show,Eq)
+	     -- only used for Annotations and Pos of semicolon
 
-data TERM = Mixfix(BRACKET_KIND,[TERM])
-	  | Qual_name(QUAL_KIND,ID,TYPE)
-	  | Typed_term(CAST_KIND,TERM,TYPE)
-	  | Token_or_place TOKEN_OR_PLACE
-	  | Literal(LITERAL_KIND,String)
-	  | Binding(BINDER,[VAR_DECL],TERM)
-	    deriving (Show,Eq)
 
-data CAST_KIND = Sorted_term | Membership | Cast
+{- Position definition for FORMULA: 
+   Information on parens are also encoded in [Pos].  If there
+   are more Pos than necessary there is a pair of Pos enclosing the
+   other Pos informations which encode the brackets of every kind
+-}
+
+data FORMULA = Quantfication QUANTIFIER [VAR_DECL] FORMULA [Pos]
+	     | Conjunction [FORMULA] [Pos]
+	     | Disjunction [FORMULA] [Pos]
+	     | Implication FORMULA FORMULA [Pos]
+	     | Equivalence FORMULA FORMULA [Pos]
+	     | Negation FORMULA [Pos]
+	     | True_atom [Pos]
+	     | False_atom [Pos]
+	     | Predication PRED_SYMB [TERM] [Pos]
+	     | Definednes TERM [Pos]
+	     | Existl_equation TERM TERM [Pos]
+	     | Strong_equation TERM TERM [Pos]
+	     | Membership TERM SORT [Pos]
+	     -- a formula left original for mixfix analysis
+	     | Unparsed_formula String [Pos]
+	       deriving (Show,Eq)
+
+data QUANTIFIER = Universal | Existential | Unique_existential
+		  deriving (Show,Eq)
+
+data PRED_SYMB = Pred_name PRED_NAME 
+	       | Qual_pred_name PRED_NAME PRED_TYPE
 		 deriving (Show,Eq)
 
-data BRACKET_KIND = Brack | Paren | Brace
-		    deriving (Show,Eq)
+{- Position and kind of brackets is provided by the list of Tokens -}
 
-data QUAL_KIND = Var | Pred | Op
-		 deriving (Show,Eq)
-
-data BINDER = Lambda(NOTATION,TOTALITY)
-	    | Forall | Exists | Exists_uniq
-	      deriving (Show,Eq)
-
-data NOTATION = Arg_decl | Explicit_lambda
-		deriving (Show,Eq)
-
-data TOTALITY = Partial | Total
-		deriving (Show,Eq)
-
-data USER = User | System
+data TERM = Simple_id SIMPLE_ID [Token]-- "Var" might be a better constructor
+	  | Qual_var VAR SORT [Pos] [Token]
+	  | Application OP_SYMB [TERM] [Pos] [Token]
+	  | Sorted_term TERM SORT [Pos] [Token]
+	  | Cast TERM SORT [Pos] [Token]
+	  | Conditional TERM FORMULA TERM [Pos] [Token]
+	  | Unparsed_term String [Pos]
 	    deriving (Show,Eq)
 
-data LITERAL_KIND = String | Fraction | Floating | Digits
-		    deriving (Show,Eq)
+data OP_SYMB = Op_name OP_NAME
+	     | Qual_op_name OP_NAME OP_TYPE
+	       deriving (Show,Eq)
 
-type TYPE_NAME = String
+type OP_NAME = Id
 
-type OP_NAME = ID
+type PRED_NAME = Id
+
+type SORT = Token 
 
 type VAR = SIMPLE_ID
 
-type SIMPLE_ID = String
-    
-data ID = Token_id TOKEN -- extra constructor
-	| Token_places [TOKEN_OR_PLACE]
---	| Comp_token_id(TOKEN,[ID])
-	| Comp_id([TOKEN_OR_PLACE],[ID])
-	  deriving (Show,Eq)
-
-type TOKEN = String
-
-data TOKEN_OR_PLACE = Place
-		    | Token TOKEN
-		      deriving (Show,Eq)
-
 ----- 
-data SYMB_ITEMS = Symb_items(SYMB_KIND,[SYMB])
+data SYMB_ITEMS = Symb_items SYMB_KIND [SYMB] 
 		  deriving (Show,Eq)
 
-data SYMB_MAP_ITEMS = Symb_map_items(SYMB_KIND,[SYMB_OR_MAP])
+data SYMB_MAP_ITEMS = Symb_map_items SYMB_KIND [SYMB_OR_MAP] 
 		      deriving (Show,Eq)
-
-data SYMB_OR_MAP = Symb SYMB
-		 | Symb_map(SYMB,SYMB) 
-		   deriving (Show,Eq)
 
 data SYMB_KIND = Implicit | Sorts_kind 
 	       | Ops_kind | Preds_kind
 		 deriving (Show,Eq)
 
-data SYMB = Symb_id ID
-	  | Qual_id(ID,TYPE)
+data SYMB = Symb_id Id
+	  | Qual_id Id TYPE 
 	    deriving (Show,Eq)
 
+data TYPE = O_type OP_TYPE
+	  | P_type PRED_TYPE
+	    deriving (Show,Eq)
+
+data SYMB_OR_MAP = Symb SYMB
+		 | Symb_map SYMB SYMB
+		   deriving (Show,Eq)
