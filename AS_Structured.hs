9cce266654dda1205036b7c3ada146d57ecc4783
Klaus Luettich <luettich@informatik.uni-bremen.de>
2002-04-02 10:24:19 +0000

    A first attempt to fix this structure, but without Annotations. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@122 cec4b9c1-7d33-0410-9eda-942365e851bb

--- AS_Structured.hs
+++ AS_Structured.hs
@@ -1,8 +1,9 @@
--- needs ghc -fglasgow-exts -package lang
+-- needs ghc -fglasgow-exts
 
 {- HetCATS/AS_Structured.hs
    $Id$
-   Klaus Luettich
+   Author: Klaus Lüttich
+   Year:   2002
 
    These data structures describe the abstract syntax tree for heterogenous 
    structured specifications in HetCASL.
@@ -11,53 +12,57 @@
    Deriving Show and Eq is not so easy with stuff from the Grothendieck logic
 -}
 
+{-! global: ATermConvertible !-}
+
 module AS_Structured where
 import Id
 import Grothendieck
 import Logic
 
 data SPEC = Basic_spec G_basic_spec 
-	  | Translation(SPEC,RENAMING)
-	  | Reduction(SPEC,RESTRICTION)
-	  | Union [SPEC]
-	  | Extension [SPEC]
-	  | Free_spec SPEC
-	  | Local_spec(SPEC,SPEC)
-	  | Closed_spec(SPEC)
-          | Group SPEC
-          | Spec_inst(SPEC_NAME,[FIT_ARG])
+	  | Translation SPEC RENAMING 
+	  | Reduction SPEC RESTRICTION 
+	  | Union [SPEC] [Pos]
+	  | Extension [SPEC] [Pos]
+	  | Free_spec SPEC [Pos]
+	  | Local_spec SPEC SPEC [Pos]
+	  | Closed_spec SPEC [Pos]
+          | Group SPEC [Pos]
+          | Spec_inst SPEC_NAME [FIT_ARG] 
 	    deriving (Show,Eq)
 
 
-data RENAMING = Renaming G_symb_map_items_list
+data RENAMING = Renaming G_symb_map_items_list [Pos]
 		deriving (Show,Eq)
 
-data RESTRICTION = Hidden G_symb_items_list
-		 | Revealed G_symb_map_items_list
+data RESTRICTION = Hidden G_symb_items_list [Pos]
+		 | Revealed G_symb_map_items_list [Pos]
 		   deriving (Show,Eq)
 
-data SPEC_DEFN = Spec_defn(SPEC_NAME,GENERICITY,SPEC)
+data SPEC_DEFN = Spec_defn SPEC_NAME GENERICITY SPEC [Pos]
+               -- 
 		 deriving (Show,Eq)
 
-data GENERICITY = Genericity(PARAMS,IMPORTED)
+data GENERICITY = Genericity PARAMS IMPORTED [Pos]
 		  deriving (Show,Eq)
 
-data PARAMS = Params [SPEC]
+data PARAMS = Params [SPEC] [Pos]
 	      deriving (Show,Eq)
 
-data IMPORTED = Imported [SPEC]
+data IMPORTED = Imported [SPEC] [Pos]
 		deriving (Show,Eq)
 
-data FIT_ARG = Fit_spec(SPEC,G_symb_map_items_list)
-	     | Fit_view(VIEW_NAME,[FIT_ARG])
+data FIT_ARG = Fit_spec SPEC G_symb_map_items_list [Pos] 
+	     | Fit_view VIEW_NAME [FIT_ARG] [Pos]
 	       deriving (Show,Eq)
 
-data VIEW_DEFN = View_defn(VIEW_NAME,GENERICITY,VIEW_TYPE,
-			   G_symb_map_items_list)
+data VIEW_DEFN = View_defn VIEW_NAME GENERICITY VIEW_TYPE 
+			   G_symb_map_items_list [Pos]
 		  deriving (Show,Eq)
 
-data VIEW_TYPE = View_type(SPEC,SPEC)
+data VIEW_TYPE = View_type SPEC SPEC [Pos]
 		 deriving (Show,Eq)
 
 type SPEC_NAME = SIMPLE_ID
 type VIEW_NAME = SIMPLE_ID
+
