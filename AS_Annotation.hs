b3c2c920772f634af45392e8de5920173fcfd43b
Klaus Luettich <luettich@informatik.uni-bremen.de>
2002-04-02 12:50:34 +0000

    The Abstract Syntax for Annotations. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@126 cec4b9c1-7d33-0410-9eda-942365e851bb

--- AS_Annotation.hs
+++ AS_Annotation.hs
@@ -1,22 +1,42 @@
 module AS_Annotation where
 import Id
 
-data Annotation = Comment_line String 
-		| Comment_group [String]
+{-! global: ATermConvertible !-}
+
+data Annotation = Comment_line String [Pos]
+		| Comment_group [String] [Pos]
 		-- constructors for unparsed annotes
-		| Annote_line(String,String)
-		| Annote_group(String,[String])
+		| Annote_line String String [Pos]
+		| Annote_group String [String] [Pos]
 		-- known annotes
-		| Display_anno(Id, [(String,String)])
-		| String_anno(Id,Id)
-		| List_anno(Id,Id,Id)
-		| Number_anno(Id)
-		| Float_anno(Id,Id)
-		| Prec_anno(Bool,[Id],[Id]) -- true = < , false = <>
-		| Lassoc_anno [Id]
-		| Rassoc_anno [Id]
-		| Label [String]
+		| Display_anno Id [(String,String)] [Pos]
+		-- postion of comment start, keywords and comment end
+		| String_anno Id Id [Pos] 
+		-- postion of comment start, commas and comment end
+		| List_anno Id Id Id [Pos] 
+		-- postion of comment start, commas and comment end
+		| Number_anno Id [Pos] 
+		-- postion of comment start, commas and comment end
+		| Float_anno Id Id [Pos] 
+		-- postion of comment start, commas and comment end
+		| Prec_anno Bool [Id] [Id] [Pos] 
+		--          ^              ^ p{:[p,]++[p}]++[p<]++
+		--          |                 [p{]++[p,]++[p}]
+		--          | true = <   false = <>
+		| Lassoc_anno [Id] [Pos] -- position of commas
+		| Rassoc_anno [Id] [Pos] -- position of commas
+		| Label [String] [Pos] 
+		-- postion of comment start and comment end
+
+		-- All annotations below are only as commentline allowed
+		| Logic_anno  String String [Pos] 
+		-- position of comment start, first string, snd string
+		| Hide_anno  String String String [Pos] 
+		-- position of comment start, 1st str, 2nd str, 3rd str
+		| With_anno  String String String [Pos]
+		-- position of comment start, 1st str, 2nd str, 3rd str
 		| Implies | Definitional | Conservative
-		-- position information for annotations 
-		| Pos_anno(Region,Annotation)
+		-- position information for annotations is now provided 
+		-- by every annotation
+		-- | Pos_anno Region Annotation 
 		  deriving (Show,Eq)   
