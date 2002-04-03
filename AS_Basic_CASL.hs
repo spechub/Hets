0e8d01b6db91eb0b0fea001f5b6306fe376341eb
Klaus Luettich <luettich@informatik.uni-bremen.de>
2002-04-03 09:21:26 +0000

    Added support for Annoted-datatype avaoiding an Annotation list on every constructor. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@129 cec4b9c1-7d33-0410-9eda-942365e851bb

--- AS_Basic_CASL.hs
+++ AS_Basic_CASL.hs
@@ -6,31 +6,31 @@ import AS_Annotation
 data BASIC_SPEC = Basic_spec [BASIC_ITEMS]
 		  deriving (Show,Eq)
 
-data BASIC_ITEMS = Sig_items SIG_ITEMS
+data BASIC_ITEMS = Sig_items (Annoted SIG_ITEMS)
 		 | Free_datatype [DATATYPE_DECL] [Pos] 
 		 | Sort_gen [SIG_ITEMS] [Pos]
 		 | Var_items [VAR_DECL] [Pos]
-		 | Local_var_axioms [VAR_DECL] [FORMULA] [Pos]
-		 | Axiom_items [AXIOM] [Pos]
+		 | Local_var_axioms [VAR_DECL] [Annoted FORMULA] [Pos]
+		 | Axiom_items [Annoted FORMULA] [Pos]
 		   deriving (Show,Eq)
 
-data SIG_ITEMS = Sort_items [SORT_ITEM] [Pos] [Annotation]
-	       | Op_items [OP_ITEM] [Pos] [Annotation]
-	       | Pred_items [OP_ITEM] [Pos] [Annotation]
-	       | Datatype_items [DATATYPE_DECL] [Annotation]
+data SIG_ITEMS = Sort_items [Annoted SORT_ITEM] [Pos] 
+	       | Op_items [Annoted OP_ITEM] [Pos]
+	       | Pred_items [Annoted OP_ITEM] [Pos]
+	       | Datatype_items [Annoted DATATYPE_DECL]
 		 deriving (Show,Eq)
 
-data SORT_ITEM = Sort_decl [SORT] [Pos] [Annotation]
-	       | Sub_sort_decl [SORT] SORT [Pos] [Annotation]
-	       | Subsort_defn SORT VAR SORT FORMULA [Pos] [Annotation]
-	       | Iso_decl [SORT] [Pos] [Annotation]
+data SORT_ITEM = Sort_decl [SORT] [Pos]
+	       | Sub_sort_decl [SORT] SORT [Pos]
+	       | Subsort_defn SORT VAR SORT (Annoted FORMULA) [Pos]
+	       | Iso_decl [SORT] [Pos]
 		 deriving (Show,Eq)
 
-data OP_ITEM = Op_decl [OP_NAME] OP_TYPE [OP_ATTR] [Pos] [Annotation]
-	     | Op_defn OP_NAME OP_HEAD TERM [Pos] [Annotation]
+data OP_ITEM = Op_decl [OP_NAME] OP_TYPE [OP_ATTR] [Pos]
+	     | Op_defn OP_NAME OP_HEAD TERM [Pos]
 	       deriving (Show,Eq)
 
-data OP_TYPE = Total_op_type [SORT] SORT [Pos] 
+data OP_TYPE = Total_op_type [SORT] SORT [Pos]
 	     | Partial_op_type [SORT] SORT [Pos]
 	       deriving (Show,Eq)
 
@@ -45,8 +45,8 @@ data OP_ATTR = Assoc_op_attr | Common_op_attr | Idem_op_attr
 	     | Unit_op_attr TERM
 	       deriving (Show,Eq)
 
-data PRED_ITEM = Pred_decl [PRED_NAME] PRED_TYPE [Pos] [Annotation]
-	       | Pred_defn PRED_NAME PRED_HEAD FORMULA [Pos] [Annotation]
+data PRED_ITEM = Pred_decl [PRED_NAME] PRED_TYPE [Pos]
+	       | Pred_defn PRED_NAME PRED_HEAD (Annoted FORMULA) [Pos]
 		 deriving (Show,Eq)
 
 data PRED_TYPE = Pred_type [SORT] [Pos]
@@ -55,12 +55,12 @@ data PRED_TYPE = Pred_type [SORT] [Pos]
 data PRED_HEAD = Pred_head [ARG_DECL] [Pos]
 		 deriving (Show,Eq)
 
-data DATATYPE_DECL = Datatype_decl SORT [ALTERNATIVE] [Pos] 
+data DATATYPE_DECL = Datatype_decl SORT [Annoted ALTERNATIVE] [Pos] 
 		     deriving (Show,Eq)
 
-data ALTERNATIVE = Total_construct OP_NAME [COMPONENTS] [Pos] [Annotation]
-		 | Partial_construct OP_NAME [COMPONENTS] [Pos] [Annotation]
-		 | Subsorts [SORT] [Pos] [Annotation]
+data ALTERNATIVE = Total_construct OP_NAME [COMPONENTS] [Pos]
+		 | Partial_construct OP_NAME [COMPONENTS] [Pos]
+		 | Subsorts [SORT] [Pos]
 		   deriving (Show,Eq)
 
 data COMPONENTS = Total_select [OP_NAME] SORT [Pos]
@@ -70,11 +70,6 @@ data COMPONENTS = Total_select [OP_NAME] SORT [Pos]
 
 data VAR_DECL = Var_decl [VAR] SORT [Pos]
 		deriving (Show,Eq)
-
-data AXIOM = Axiom FORMULA [Pos] [Annotation] 
-	     deriving (Show,Eq)
-	     -- only used for Annotations and Pos of semicolon
-
 
 {- Position definition for FORMULA: 
    Information on parens are also encoded in [Pos].  If there
