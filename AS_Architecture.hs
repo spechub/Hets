924b04a090e70947e2eaef7ca73e1a7fde9db446
Klaus Luettich <luettich@informatik.uni-bremen.de>
2002-04-10 15:06:05 +0000

    Updated documentation concerning positions. Annotations should now possible where neccessary and allowed. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@139 cec4b9c1-7d33-0410-9eda-942365e851bb

--- AS_Architecture.hs
+++ AS_Architecture.hs
@@ -10,47 +10,68 @@
 -}
 
 module AS_Architecture where
+
+import Id
+import AS_Annotation
+
 import AS_Structured
 import Grothendieck
-import Id
+
 
 data ARCH_SPEC_DEFN = Arch_spec_defn ARCH_SPEC_NAME (Annoted ARCH_SPEC) [Pos]
+		      -- pos: "arch","spec","=",opt "end"
 		      deriving (Show,Eq)
 
-data ARCH_SPEC = Basic_arch_spec [UNIT_DECL_DEFN] UNIT_EXPRESSION [Pos]
-	       | Arch_spec_name ARCH_SPEC_NAME [Pos]
+data ARCH_SPEC = Basic_arch_spec [Annoted UNIT_DECL_DEFN]
+		                 (Annoted UNIT_EXPRESSION) [Pos]
+	         -- pos: "unit","result"
+	       | Arch_spec_name ARCH_SPEC_NAME
+	       | Group_arch_spec (Annoted ARCH_SPEC) [Pos]
+		 -- pos: "{","}"
 		 deriving (Show,Eq)
 
-data UNIT_DECL_DEFN = Unit_decl UNIT_NAME UNIT_SPEC UNIT_IMPORTED [Pos]
+data UNIT_DECL_DEFN = Unit_decl UNIT_NAME UNIT_SPEC [Annoted UNIT_TERM] [Pos]
+		      -- pos: ":",opt ("given"; Annoted holds pos of commas)
 		    | Unit_defn UNIT_NAME UNIT_EXPRESSION [Pos]
+		      -- pos: "="
 		      deriving (Show,Eq)
 
-data UNIT_IMPORTED = Unit_imported [UNIT_TERM] [Pos]
-		     deriving (Show,Eq)
-
 data UNIT_SPEC_DEFN = Unit_spec_defn SPEC_NAME UNIT_SPEC [Pos]
+		      -- pos: "unit","spec","=", opt "end"
 		      deriving (Show,Eq)
 
 data UNIT_SPEC = Unit_type [SPEC] SPEC [Pos]
-	       | Spec_name SPEC_NAME [Pos]
+	         -- pos: opt "*"s , "->"
+{-	       | Spec_name SPEC_NAME [Pos]  ??? -}
 	       | Arch_unit_spec ARCH_SPEC [Pos]
+		 -- pos: "arch","spec"
 	       | Closed_unit_spec UNIT_SPEC [Pos]
+		 -- pos: "closed"
 		 deriving (Show,Eq)
 
-data UNIT_EXPRESSION = Unit_expression [UNIT_BINDING] UNIT_TERM [Pos]
+data UNIT_EXPRESSION = Unit_expression [UNIT_BINDING] (Annoted UNIT_TERM) [Pos]
+		       -- pos: opt "lambda",semi colons, "."
 		       deriving (Show,Eq)
 
 data UNIT_BINDING = Unit_binding UNIT_NAME UNIT_SPEC [Pos]
+		    -- pos: ":"
 		    deriving (Show,Eq) 
 
-data UNIT_TERM = Unit_reduction UNIT_TERM RESTRICTION [Pos]
-	       | Unit_translation UNIT_TERM RENAMING [Pos]
-	       | Amalgamation [UNIT_TERM] [Pos]
-	       | Local_unit [UNIT_DECL_DEFN] UNIT_TERM [Pos] 
+data UNIT_TERM = Unit_reduction (Annoted UNIT_TERM) RESTRICTION
+	       | Unit_translation (Annoted UNIT_TERM) RENAMING 
+	       | Amalgamation [Annoted UNIT_TERM] [Pos]
+		 -- pos: "and"s
+	       | Local_unit [Annoted UNIT_DECL_DEFN] (Annoted UNIT_TERM) [Pos] 
+		 -- pos: "local", "within"
 	       | Unit_appl UNIT_NAME [FIT_ARG_UNIT] [Pos]
+		 -- pos: many of "[","]"
+	       | Group_unit_term (Annoted UNIT_TERM) [Pos]
+		 -- pos: "{","}"
 		 deriving (Show,Eq)
 
-data FIT_ARG_UNIT = Fit_arg_unit UNIT_TERM G_symb_map_items_list [Pos] 
+data FIT_ARG_UNIT = Fit_arg_unit (Annoted UNIT_TERM) 
+		                 G_symb_map_items_list [Pos] 
+		    -- pos: opt "fit"
 		    deriving (Show,Eq)
 
 type ARCH_SPEC_NAME = SIMPLE_ID
