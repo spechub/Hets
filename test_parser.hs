8b8adefb7aba51a31d34f66b29048d93742a4e04
Klaus Luettich <luettich@informatik.uni-bremen.de>
2002-05-17 10:32:59 +0000

    Added some undocumented commandline options to choose the test cases. Comparing Chistians and Klaus Id-parsers is now simplified. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@215 cec4b9c1-7d33-0410-9eda-942365e851bb

--- test_parser.hs
+++ test_parser.hs
@@ -1,10 +1,21 @@
 module Main where
+
+import System
 import Parsec
 import ParsecPerm
 import CaslLanguage
 import Anno_Parser
 
+import Id
+
 import AS_Annotation
+import Token
+
+-- import Prepositional
+
+-- # import LogicGraph
+
+data TtT = TtT {f_::String , pY:: Pos} deriving Show {-! derive: update !-}
 
 testP p s =  case (parse p "" s) of
 	     Left err -> do{ putStr "parse error at "
@@ -40,9 +51,50 @@ testFile par name = do { inp <- readFile name
 			       ; testPL p line
 			       }
 
+testFileC name = do { inp <- readFile name
+		    ; sequence (map (testLine) (lines inp))
+		    }
+    where testLine line = do { putStr "** Input was: "
+			     ; putStrLn line
+			     ; putStr "** Result(KL) is: "
+			     ; test_id casl_id line
+			     ; putStr "** Result(CM) is: "
+			     ; test_id parseId line
+			       }
+
 testData p s = parse (do {whiteSpace ; res <- p; eof ; return res}) "" s
 
-main = testPL annotation "%%Aha\n"
+test_id p inp = case (testData p inp) of 
+		Left err -> print err
+		Right id@(Id mix comp _) -> do { if comp == [] then 
+					       putChar 'M' 
+					       else 
+					       putChar 'C'
+					     ; putStr ": " 
+					     ; putStrLn (showId id "") }
 
+main = do { as <- getArgs
+	  ; (p,files) <- return (extract_par as)
+	  ; sequence (map (parseFile' p) files)  
+	  }
+    where extract_par = extract_par' "annotations" [] 
+	  extract_par' p ac as = 
+	      if as == [] then
+		 error "*** No Filename or argument given"
+	      else
+	      case as of 
+		      [s] -> (p,ac ++ as) 
+		      x:ltl@(y:tl) -> if x == "-p" then
+				      extract_par' y ac tl
+				      else 
+				      extract_par' p (ac++x:[]) (ltl)	 
+	  parseFile' s = case s of
+			 "annotations" -> parseFile annotations
+			 "casl_id"     -> testFileC' 
+			 otherwise     -> error ("*** unknown parser " ++ s)
+	  testFileC' s = do { testFileC s
+			    ; return ()
+			    }
+	      
 testId = testPL (sepBy casl_id semi) 
 	 "__++__ ; __+*[y__,a_l'__,4]__ ; {__}[__] ; __a__b[__z]" 
