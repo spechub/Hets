6271e7e857b1756dd75396e982644113b5e17bb3
Klaus Luettich <luettich@informatik.uni-bremen.de>
2002-05-17 12:09:20 +0000

    Changed the prederfined LaTeX print functions. And added an verbatim renderer for Doc s. Also the helpers are removed. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@219 cec4b9c1-7d33-0410-9eda-942365e851bb

--- PrettyPrint.hs
+++ PrettyPrint.hs
@@ -22,37 +22,26 @@ import Pretty
 -- for infomation on the predefined functions see above
 class Show a => PrettyPrint a where
     printLatex, printLatex0, printText, printText0  :: a -> Doc
-    printLatex0 a = ptext "\\verb¦" <> printText0 a <> char '¦'
-    printLatex  a = ptext "\\verb¦" <> printText  a <> char '¦'
+    printLatex0 a = printText0 a
+    printLatex  a = printText  a
     printText  a = printText0 a
 
-{-    
+
 -- two Styles for Formatting (Standard is Style PageMode 100 1.5)
-latexStyle, asciiStyle :: Style
-latexStyle = Style PageMode 80 1.7 
+latexStyle, textStyle :: Style
+latexStyle = style {lineLength=80, ribbonsPerLine= 1.7} 
 -- maximum line length 80 with 47 printable chars (up to 33 indentation chars) 
-asciiStyle = latexStyle
+textStyle = latexStyle
 
 -- functions for producing IO printable Strings
-renderLatex, renderAscii :: Doc -> String
-renderLatex d = renderStyle latexStyle d'
-    where d' = d
+renderLatex,renderText,renderLatexVerb :: Doc -> String
+
+renderLatex d = renderStyle latexStyle d
 -- this lacks some environment starting and closing LaTeX commands
 
-renderAscii d = renderStyle asciiStyle d
--}
+renderLatexVerb d = renderStyle latexStyle d'
+    where d' = text "\\begin{verbatim}" $+$ d $+$ text "\\end{verbatim}"
 
-{-
+renderText d = renderStyle textStyle d
+
 -- helpers ------------------------------------
--- reduce nested \\verb; things
-close_verb :: Doc -> Doc
-close_verb Empty                 = Empty
-close_verb (NilAbove doc)        = NilAbove (close_verb doc)
-close_verb (TextBeside td i doc) = 
-                 if is_verb td then 
-		    TextBeside td i (remove_and_close_verb doc) 
-		 else TextBeside td i (close_verb doc)
-close_verb (Nest i doc)          = Nest i (close_verb doc)
-close_verb d = d
--}
-
