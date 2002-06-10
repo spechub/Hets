6b8c75b6df8d3e31cc5613426fcf37ead39c65ce
Pascal Schmidt <pleasure.and.pain@web.de>
2002-06-10 19:38:19 +0000

    Added CASL_Sublogics datatype git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@269 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CASL/Sublogics.hs
+++ CASL/Sublogics.hs
@@ -29,14 +29,24 @@ min_sublogic_basic_spec  Analysefunktion: für basic spec Bitmaske berechnen
    (Bits fuer Features setzen und rekursiv verodern)
 is_in_basic_spec  Testfunktion: pruefen, ob errechnete Bitmaske <= vorgegebene
 
-     - writing real functions
-
 -}
 
 module Sublogics_CASL where
 
+import AS_Annotation
 import AS_Basic_CASL
 import LocalEnv
+
+data CASL_Sublogics = CASL_Sublogics
+                      { has_sub::Bool,   -- subsorting
+                        has_part::Bool,  -- partiality
+                        has_cons::Bool,  -- sort generation contraints
+                        has_eq::Bool,    -- equality
+                        has_pred::Bool,  -- predicates
+                        is_ghorn::Bool,  -- generalized positive cond. logic
+                        is_horn::Bool,   -- positive conditional logic
+                        is_atom::Bool    -- atomic logic
+                      } deriving (Show,Eq)
 
 strip_anno :: Annoted a -> a
 strip_anno (Annoted i _ _ _) = i
