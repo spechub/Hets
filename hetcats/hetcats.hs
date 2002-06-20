59de6982a4c61d72398cf4600c6241b6980f02aa
Klaus Luettich <luettich@informatik.uni-bremen.de>
2002-06-20 15:08:23 +0000

    This Version accepts two Filenames:       the first schould be an ATerm (either in TAF or TRM format).           this file will be read.       the second file will be written by this version and contains a pretty           printed casl specification. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@307 cec4b9c1-7d33-0410-9eda-942365e851bb

--- hetcats/hetcats.hs
+++ hetcats/hetcats.hs
@@ -13,20 +13,23 @@ module Main where
 
 import Options
 import System
+import IO
 import ATC_sml_cats
+import Print_HetCASL
 
 -- import ReadFn
 -- import WriteFn
 -- import ProcessFn
 
 main = do as <- getArgs
-	  case as of 
-		  [x] -> read_sml_ATerm x >>= print
-		  _   -> error "give a filename to read"
+	  (inp,oup) <- case as of 
+		        [inp,oup] -> return (inp,oup)
+			_   -> error "give a filename to read and a filename to write"
+	  ld <- read_sml_ATerm inp
+	  putStrLn $ "In: " ++ inp ++ "\nOut: " ++ oup ++ "\n" ++ show ld
+	  putStrLn "Starting to write the output..."
+	  hout <- openFile oup WriteMode
+	  hPutStr hout $ printLIB_DEFN_text ld
 
 m = putStrLn "*** hetcats: Not yet implented"
 
-{-
-{-
-
--}-}
