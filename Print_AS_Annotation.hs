a508cd11957a8e1c0dc0dcb89087bd5df761aa4e
Klaus Luettich <luettich@informatik.uni-bremen.de>
2002-06-21 14:46:52 +0000

    Annotations are printed right know, so that they can be parsed again. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@312 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Print_AS_Annotation.hs
+++ Print_AS_Annotation.hs
@@ -23,59 +23,69 @@ import Pretty
 instance PrettyPrint Annotation where
     printText0 ga (Comment_line aa _) =
 	let aa' = printText0 ga aa
-	in aa'
+	in ptext "%%" <> aa' <> ptext "\n"
     printText0 ga (Comment_group aa _) =
 	let aa' = printText0 ga aa
-	in aa'
+	in ptext "%{" <> aa' <> ptext "}%"
     printText0 ga (Annote_line aa ab _) =
 	let aa' = printText0 ga aa
 	    ab' = printText0 ga ab
-	in aa' <+> ab'
+	in ptext "%" <> aa' <+> ab' <> ptext "\n"
     printText0 ga (Annote_group aa ab _) =
 	let aa' = printText0 ga aa
 	    ab' = printText0 ga ab
-	in aa' <+> ab'
+	in printGroup aa' ab'
     printText0 ga (Display_anno aa ab _) =
 	let aa' = printText0 ga aa
-	    ab' = cat $ map printPair ab
-	in aa' <+> ab'
+	    ab' = fcat $ punctuate space $ map printPair $ filter nullSnd ab
+	in printGroup (ptext "display") $ aa' <+> ab'
 	   where printPair (s1,s2) = ptext s1 <+> ptext s2
+		 nullSnd (_,s2) = not $ null s2
     printText0 ga (String_anno aa ab _) =
 	let aa' = printText0 ga aa
 	    ab' = printText0 ga ab
-	in aa' <+> ab'
+	in printLine (ptext "string") $ aa' <> comma <+> ab'
     printText0 ga (List_anno aa ab ac _) =
 	let aa' = printText0 ga aa
 	    ab' = printText0 ga ab
 	    ac' = printText0 ga ac
-	in aa' <+> ab' <+> ac'
+	in printLine (ptext "list") $ aa' <> comma <+> ab' <> comma <+> ac'
     printText0 ga (Number_anno aa _) =
 	let aa' = printText0 ga aa
-	in aa'
+	in printLine (ptext "number") aa'
     printText0 ga (Float_anno aa ab _) =
 	let aa' = printText0 ga aa
 	    ab' = printText0 ga ab
-	in aa' <+> ab'
+	in printLine (ptext "floating") $ aa' <> comma <+> ab'
     printText0 ga (Prec_anno pflag ab ac _) =
 	let aa' = if pflag then ptext "<" else ptext "<>"
-	    ab' = printText0 ga ab
-	    ac' = printText0 ga ac
-	in  ab' <+> aa' <+> ac'
+	    ab' = fcat $ punctuate (comma <> space) $ map (printText0 ga) ab
+	    ac' = fcat $ punctuate (comma <> space) $ map (printText0 ga) ac
+	in  printGroup (ptext "prec") $ braces ab' <+> aa' <+> braces ac'
     printText0 ga (Lassoc_anno aa _) =
-	let aa' = printText0 ga aa
-	in aa'
+	printGroup (ptext "left_assoc") $ fcat $ 
+				punctuate (comma <> space) $ 
+					  map (printText0 ga) aa
     printText0 ga (Rassoc_anno aa _) =
-	let aa' = printText0 ga aa
-	in aa'
+	printGroup (ptext "right_assoc") $ fcat $ 
+				punctuate (comma <> space) $ 
+					  map (printText0 ga) aa
     printText0 ga (Label aa _) =
 	let aa' = printText0 ga aa
-	in aa'
+	in ptext "%(" <> aa' <> ptext ")%"
     printText0 ga (Implies _) =
-	empty
+	printLine (ptext "implies") empty
     printText0 ga (Definitional _) =
-	empty
+	printLine (ptext "def") empty
     printText0 ga (Conservative _) =
-	empty
+	printLine (ptext "cons") empty
+
+
+printGroup :: Doc -> Doc -> Doc
+printGroup key grp = ptext "%" <> key <> ptext "(" <+> grp <> ptext ")%"
+
+printLine :: Doc -> Doc -> Doc
+printLine key line = ptext "%" <> key <+> line <> ptext "\n"
 
 instance (PrettyPrint a) => PrettyPrint (Annoted a) where
     printText0 ga (Annoted i _ las ras) =
