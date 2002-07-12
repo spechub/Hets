8b66bf418e14c581e9a5914a3aa2ea1edfe896f1
Christian Maeder <Christian.Maeder@dfki.de>
2002-07-12 15:47:04 +0000

    removed dependencies from individuell logics (and transitive reexport) added G_morphism from Grothendieck.hs to avoid its dependency from LogicRepr.hs what/where is G_morhism used (for)? git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@358 cec4b9c1-7d33-0410-9eda-942365e851bb

--- LogicGraph.hs
+++ LogicGraph.hs
@@ -17,21 +17,18 @@
    Theoretical Computer Science, July 2003
 
    Todo:
-   Add many many logics.
+   Add many many logics. (but somewhere else!)
 
   Jorina:
    aufbauend auf Logic.hs Logik für CASL mit Unterlogiken
 
-   data CASL = CASL
-   data CASL_sublogics = CASL_sublogics { has_sub::Bool ...
-   instance Logic CASL CASL_sublogics () () () () () () () () where
-     logic_name _ = "CASL"
-     has_parser _ = False
-     has_printer _ = False
-     has_analysis _ = False
-     map_sen = undefined
+
+   removed instance example for Logic
+   (see CASL/Logic_CASL.hs for an example)
    
-    Rest alles undefined, bis auf
+    Rest alles undefined 
+    (no! simply ignore compiler warnings 
+    as long proper implementations are missing)
 
      sublogic_names :: id -> sublogics -> [String] 
                 -- the first name is the principal name
@@ -47,57 +44,41 @@
 
 -}
 
-module LogicGraph (module LogicGraph,module Logic_CASL) where
+module LogicGraph where
 
--- exporting the imported Logic instances makes it more easy to access
--- the Logic_ids defined in those modules
 import Logic
 import Set
-import Dynamic
-import Logic_CASL
-
--- The Logic instances needed for the logic list
--- import Logic_CASL
-
-instance Typeable a => Typeable (Set a) where
-  typeOf t = mkAppTy (mkTyCon "Set") [typeOf t]
-  
+import LogicRepr
 
 -- Existential types for logics and representations
 
 data AnyLogic = forall id sublogics
-        basic_spec sentence symb_items_list symb_map_items_list
+        basic_spec sentence symb_items symb_map_items
         local_env sign morphism symbol raw_symbol .
         Logic id sublogics
-         basic_spec sentence symb_items_list symb_map_items_list
+         basic_spec sentence symb_items symb_map_items
          local_env sign morphism symbol raw_symbol =>
         Logic id
 
 data AnyRepresentation = forall id1 sublogics1
-        basic_spec1 sentence1 symb_items_list1 symb_map_items_list1
+        basic_spec1 sentence1 symb_items1 symb_map_items1
         local_env1 sign1 morphism1 symbol1 raw_symbol1
         id2 sublogics2
-        basic_spec2 sentence2 symb_items_list2 symb_map_items_list2
+        basic_spec2 sentence2 symb_items2 symb_map_items2
         local_env2 sign2 morphism2 symbol2 raw_symbol2 .
       (Logic id1 sublogics1
-        basic_spec1 sentence1 symb_items_list1 symb_map_items_list1
+        basic_spec1 sentence1 symb_items1 symb_map_items1
         local_env1 sign1 morphism1 symbol1 raw_symbol1,
       Logic id2 sublogics2
-        basic_spec2 sentence2 symb_items_list2 symb_map_items_list2 
+        basic_spec2 sentence2 symb_items2 symb_map_items2 
         local_env2 sign2 morphism2 symbol2 raw_symbol2) =>
   Repr(LogicRepr id1 sublogics1 basic_spec1 sentence1 
-                 symb_items_list1 symb_map_items_list1
+                 symb_items1 symb_map_items1
                  local_env1 sign1 morphism1 symbol1 raw_symbol1
                  id2 sublogics2 basic_spec2 sentence2 
-                 symb_items_list2 symb_map_items_list2
+                 symb_items2 symb_map_items2
                  local_env2 sign2 morphism2 symbol2 raw_symbol2)
 
-
---  Composition of representations
-
-coerce :: a -> Maybe b = fromDynamic . toDyn
-the = maybe undefined id
-g `comp` (Just f) = \x -> f x >>= g
 
 {- This does not work due to needed ordering:
 instance Functor Set where
@@ -106,24 +87,6 @@ instance Monad Set where
   return = unitSet
   m >>= k          = unionManySets (setToList (fmap k m))
 -}
-g `compSet` (Just f) = \x -> unionManySets (map g (setToList (f x)))
-
-id_repr i s = LogicRepr{ 
-  repr_name = "id_"++head (sublogic_names i s),
-  source = i, target = i,
-  source_sublogic = s, 
-  target_sublogic = s,
---  map_basic_spec = Just,
-  map_sentence = \_ -> Just,
-  map_sign = \sigma -> Just (sigma,[]),
-  projection = Just (proj_sublogic_sign i s,
-                     proj_sublogic_morphism i s,
-                     proj_sublogic_epsilon i s,
-                     -- proj_sublogic_basic_spec i s,
-                     proj_sublogic_symbol i s),
-  map_morphism = Just,
-  map_symbol = \x -> unitSet x
- }
 
 comp_repr :: AnyRepresentation -> AnyRepresentation -> Maybe AnyRepresentation
 comp_repr (Repr (r1 :: {-Logic id1 sublogics1
@@ -132,7 +95,8 @@ comp_repr (Repr (r1 :: {-Logic id1 sublogics1
         Logic id2 sublogics2
         basic_spec2 sentence2 symb_items2 symb_map_items2 
         local_env2 sign2 morphism2 symbol2 raw_symbol2) => -}
-        LogicRepr id1 sublogics1 basic_spec1 sentence1 symb_items1 symb_map_items1
+        LogicRepr id1 sublogics1 basic_spec1 sentence1 
+                symb_items1 symb_map_items1
                 local_env1 sign1 morphism1 symbol1 raw_symbol1
             id2 sublogics2 basic_spec2 sentence2 symb_items2 symb_map_items2
                 local_env2 sign2 morphism2 symbol2 raw_symbol2))
@@ -142,36 +106,51 @@ comp_repr (Repr (r1 :: {-Logic id1 sublogics1
          Logic id4 sublogics4
          basic_spec4 sentence4 symb_items4 symb_map_items4 
          local_env4 sign4 morphism4 symbol4 raw_symbol4) => -}
-         LogicRepr id3 sublogics3 basic_spec3 sentence3 symb_items3 symb_map_items3
+         LogicRepr id3 sublogics3 basic_spec3 sentence3 
+	        symb_items3 symb_map_items3
                 local_env3 sign3 morphism3 symbol3 raw_symbol3
             id4 sublogics4 basic_spec4 sentence4 symb_items4 symb_map_items4
                 local_env4 sign4 morphism4 symbol4 raw_symbol4)) = 
-  case coerce (source r2)::Maybe id2 of
-  Nothing -> Nothing
-  Just _ -> if target_sublogic r1 <= the (coerce (source_sublogic r2)::Maybe sublogics2) then
-   Just (Repr (LogicRepr{ 
-   repr_name = repr_name r1++";"++repr_name r2,
-   source = source r1, target = target r2,
-   source_sublogic = source_sublogic r1, 
-   target_sublogic = target_sublogic r2,
-   -- map_basic_spec = map_basic_spec r2 `comp` (coerce (map_basic_spec r1)::Maybe(basic_spec1 -> Maybe basic_spec3)),
-   map_sentence = 
-     ( \sigma -> 
-        map_sentence r2 (the (((coerce (map_sign r1 sigma))::Maybe sign3)))
-       `comp` (coerce (map_sentence r1 sigma)::Maybe(sentence1 -> Maybe sentence3))),
-   map_sign = map_sign r2 `comp` (coerce (map_sign r1)::Maybe(sign1 -> Maybe sign3)),
-   projection = undefined {- (proj_sublogic_sign i s,
-                      proj_sublogic_morphism i s,
-                      proj_sublogic_epsilon i s,
-                      proj_sublogic_basic_spec i s,
-                      proj_sublogic_symbol i s) -} ,
-   map_morphism = map_morphism r2 `comp` (coerce (map_morphism r1)::Maybe(morphism1 -> Maybe morphism3)),
-   map_symbol = map_symbol r2 `compSet` (coerce (map_symbol r1)::Maybe(symbol1 -> Set symbol3))
- }))
-                else Nothing
+  case coerce (target r1) (source r2) r2 :: Maybe(
+	  LogicRepr id2 sublogics2 basic_spec2 sentence2 
+                symb_items2 symb_map_items2
+                local_env2 sign2 morphism2 symbol2 raw_symbol2
+            id4 sublogics4 basic_spec4 sentence4 symb_items4 symb_map_items4
+                local_env4 sign4 morphism4 symbol4 raw_symbol4)
+		of 
+		Nothing -> Nothing
+		Just r3 -> case LogicRepr.comp_repr r1 r3 of
+			   Nothing -> Nothing
+			   Just r4 -> Just (Repr r4)
+ 
+data G_morphism = forall id1 sublogics1
+        basic_spec1 sentence1 symb_items1 symb_map_items1
+        local_env1 sign1 morphism1 symbol1 raw_symbol1
+        id2 sublogics2
+        basic_spec2 sentence2 symb_items2 symb_map_items2 
+        local_env2 sign2 morphism2 symbol2 raw_symbol2 .
+      (Logic id1 sublogics1
+        basic_spec1 sentence1 symb_items1 symb_map_items1
+        local_env1 sign1 morphism1 symbol1 raw_symbol1,
+       Logic id2 sublogics2
+        basic_spec2 sentence2 symb_items2 symb_map_items2 
+        local_env2 sign2 morphism2 symbol2 raw_symbol2) =>
+  G_morphism id1 id2 
+   (LogicRepr id1 sublogics1 basic_spec1 sentence1 
+              symb_items1 symb_map_items1
+              local_env1 sign1 morphism1 symbol1 raw_symbol1
+              id2 sublogics2 basic_spec2 sentence2 
+              symb_items2 symb_map_items2
+              local_env2 sign2 morphism2 symbol2 raw_symbol2)
+   morphism2 
+
+{- 
+
+build these somewhere in a more top-level module
 
 the_logic_list :: [AnyLogic] = [Logic CASL]
 		  -- [Logic Prae] -- [Logic CASL, Logic HasCASL, ...]
 the_representation_list :: [AnyRepresentation] = []
 
+-}
 
