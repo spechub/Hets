3b34e1a1f7909341ef9e42503ddbbda7636920dc
Christian Maeder <Christian.Maeder@dfki.de>
2002-07-12 15:42:23 +0000

    adapted coerce and moved G_morphism to LogicGraph.hs git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@357 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Grothendieck.hs
+++ Grothendieck.hs
@@ -30,8 +30,6 @@
 module Grothendieck where
 
 import Logic
-import LogicGraph(coerce)
-import Dynamic
 import PrettyPrint
 import Pretty
 
@@ -50,8 +48,8 @@ instance PrettyPrint G_basic_spec where
     printText0 ga (G_basic_spec _ s) = printText0 ga s
 
 instance Eq G_basic_spec where
-  (G_basic_spec _ s1) == (G_basic_spec _ s2) =
-     coerce s1 == Just s2
+  (G_basic_spec i1 s1) == (G_basic_spec i2 s2) =
+     coerce i1 i2 s1 == Just s2
 
 data G_sentence = forall id sublogics
         basic_spec sentence symb_items symb_map_items
@@ -99,27 +97,6 @@ data G_local_env = forall id sublogics
          local_env sign morphism symbol raw_symbol =>
   G_local_env id local_env 
 
-data G_morphism = forall id1 sublogics1
-        basic_spec1 sentence1 symb_items1 symb_map_items1
-        local_env1 sign1 morphism1 symbol1 raw_symbol1
-        id2 sublogics2
-        basic_spec2 sentence2 symb_items2 symb_map_items2 
-        local_env2 sign2 morphism2 symbol2 raw_symbol2 .
-      (Logic id1 sublogics1
-        basic_spec1 sentence1 symb_items1 symb_map_items1
-        local_env1 sign1 morphism1 symbol1 raw_symbol1,
-       Logic id2 sublogics2
-        basic_spec2 sentence2 symb_items2 symb_map_items2 
-        local_env2 sign2 morphism2 symbol2 raw_symbol2) =>
-  G_morphism id1 id2 
-   (LogicRepr id1 sublogics1 basic_spec1 sentence1 
-              symb_items1 symb_map_items1
-              local_env1 sign1 morphism1 symbol1 raw_symbol1
-              id2 sublogics2 basic_spec2 sentence2 
-              symb_items2 symb_map_items2
-              local_env2 sign2 morphism2 symbol2 raw_symbol2)
-   morphism2 
-
 data G_symbol = forall id sublogics
         basic_spec sentence symb_items symb_map_items
         local_env sign morphism symbol raw_symbol .
@@ -132,8 +109,8 @@ instance Show G_symbol where
   show (G_symbol _ s) = show s
 
 instance Eq G_symbol where
-  (G_symbol _ s1) == (G_symbol _ s2) =
-     coerce s1 == Just s2
+  (G_symbol i1 s1) == (G_symbol i2 s2) =
+     coerce i1 i2 s1 == Just s2
 
 data G_symb_items_list = forall id sublogics
         basic_spec sentence symb_items symb_map_items
@@ -151,8 +128,8 @@ instance PrettyPrint G_symb_items_list where
       fcat $ punctuate (comma<>space) $ map (printText0 ga) l
 
 instance Eq G_symb_items_list where
-  (G_symb_items_list _ s1) == (G_symb_items_list _ s2) =
-     coerce s1 == Just s2
+  (G_symb_items_list i1 s1) == (G_symb_items_list i2 s2) =
+     coerce i1 i2 s1 == Just s2
 
 data G_symb_map_items_list = forall id sublogics
         basic_spec sentence symb_items symb_map_items
@@ -170,8 +147,8 @@ instance PrettyPrint G_symb_map_items_list where
       fcat $ punctuate (comma<>space) $ map (printText0 ga) l
 
 instance Eq G_symb_map_items_list where
-  (G_symb_map_items_list _ s1) == (G_symb_map_items_list _ s2) =
-     coerce s1 == Just s2
+  (G_symb_map_items_list i1 s1) == (G_symb_map_items_list i2 s2) =
+     coerce i1 i2 s1 == Just s2
 
 data G_diagram = forall id sublogics
         basic_spec sentence symb_items symb_map_items
@@ -189,12 +166,3 @@ data G_sublogics = forall id sublogics
          local_env sign morphism symbol raw_symbol =>
         G_sublogics id sublogics 
 
-{-
-homogenize_symb_items :: G_symb_items -> Maybe G_symb_items
-homogenize_symb_items [] = Nothing
-homogenize_symb_items (G_symb_items i (s::symb_map_items) : rest) = 
-  maybe Nothing 
-        (\l -> Just (G_symb_items i l))  
-        ( sequence (Just s : 
-                    map (\(G_symb_items _ si) -> coerce si::Maybe symb_map_items) rest) )
--}
