c7a4ab02b311167ffb02092337169c42f85aa544
Klaus Luettich <luettich@informatik.uni-bremen.de>
2002-07-25 13:08:49 +0000

    A module containing several reading / parsing finctions for various input formats. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@391 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ hetcats/ReadFn.hs
@@ -0,0 +1,22 @@
+
+{- HetCATS/hetcats/ReadFn.hs
+   $Id$
+   Author: Klaus Lüttich
+   Year:   2002
+
+   "reading" ATerms, CASL, HetCASL files and parsing them into an
+   appropiate data structure 
+ 
+-}
+
+module ReadFn where
+
+import Options
+
+import ATC_sml_cats
+import AS_Library
+
+read_LIB_DEFN :: HetcatsOpts -> IO LIB_DEFN
+read_LIB_DEFN opt = read_sml_ATerm (infile opt)
+
+
