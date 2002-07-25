07de5f60926d99ad7546151466c262aa113f6d94
Klaus Luettich <luettich@informatik.uni-bremen.de>
2002-07-25 13:14:20 +0000

    The main function now uses the new comandline interface for hetcats. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@394 cec4b9c1-7d33-0410-9eda-942365e851bb

--- hetcats/hetcats.hs
+++ hetcats/hetcats.hs
@@ -13,23 +13,17 @@ module Main where
 
 import Options
 import System
-import IO
-import ATC_sml_cats
-import Print_HetCASL
 
--- import ReadFn
--- import WriteFn
+import ReadFn
+import WriteFn
 -- import ProcessFn
 
-main = do as <- getArgs
-	  (inp,oup) <- case as of 
-		        [inp,oup] -> return (inp,oup)
-			_   -> error "give a filename to read and a filename to write"
-	  ld <- read_sml_ATerm inp
-	  putStrLn $ "In: " ++ inp ++ "\nOut: " ++ oup ++ "\n" 
-	  putStrLn "Starting to write the output..."
-	  hout <- openFile oup WriteMode
-	  hPutStr hout $ printLIB_DEFN_text ld
-
-m = putStrLn "*** hetcats: Not yet implented"
+main :: IO ()
+main = do opt <- getArgs >>= hetcatsOpts
+	  if (verbose opt >= 3) then putStr "Options: " >> print opt
+	   else return ()
+	  ld <- read_LIB_DEFN opt
+	  -- (env,ld') <- analyse_LIB_DEFN opt
+	  write_LIB_DEFN opt ld
+	  -- write_GLOBAL_ENV env
 
