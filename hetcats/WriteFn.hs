4e3bfd2f2b86112f7b5016f8a0f5d7988f99605e
Klaus Luettich <luettich@informatik.uni-bremen.de>
2002-07-29 13:21:56 +0000

    Changed the documentation to the style required by HDoc. Provided another implementation of stripOf. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@396 cec4b9c1-7d33-0410-9eda-942365e851bb

--- hetcats/WriteFn.hs
+++ hetcats/WriteFn.hs
@@ -1,32 +1,44 @@
 
-{- HetCATS/hetcats/WriteFn.hs
-   $Id$
-   Author: Klaus Lüttich
-   Year:   2002
-
-   This module provides functions to write a pretty printed abstract
-   syntax and all the other formats.
-
+{---
+  HetCATS/hetcats/WriteFn.hs
+  @version $Id$
+  @author Klaus L&uuml;ttich<BR>
+  Year:   2002
+  <p>
+  This module provides functions to write a pretty printed abstract
+  syntax and all the other formats.
+  </p>
 -}
-
 module WriteFn where
 
 import Options
 import List
 
-import IO
+import IO 
+import IOExts (trace)
 import Print_HetCASL
-import AS_Library (LIB_DEFN(..))
+import AS_Library (LIB_DEFN()) 
 
+{---
+  Write the given LIB_DEFN in every format that HetcatsOpts includes.
+  Filenames are determined by the output formats.
+  @param opt - Options Either default or given on the comandline
+  @param ld  - a LIB_DEFN read as ATerm or parsed
+-}
 write_LIB_DEFN :: HetcatsOpts -> LIB_DEFN -> IO ()
 write_LIB_DEFN opt ld = sequence_ $ map write_type $ outtypes opt
     where write_type :: OutType -> IO ()
 	  write_type t = 
 	      case t of 
 	      CASLOut -> write_casl_asc (casl_asc_filename opt) ld
-	      _ -> error $ "the outtype \"" ++ 
-		           show t ++ "\" is not implemented"
-
+	      _ -> trace ( "the outtype \"" ++ 
+		           show t ++ "\" is not implemented")
+		         (return ())
+{---
+  Produces the filename of the pretty printed CASL-file.
+  @param opt   - Options from the command line 
+  @return path - full path to the generated file
+-}
 casl_asc_filename :: HetcatsOpts -> FilePath
 casl_asc_filename opt =
     (outdir opt) ++ "/" ++ basename (infile opt) ++ ".pp.casl"
@@ -37,8 +49,20 @@ basename fp | ".tree.gen_trm" `isSuffixOf` fp = stripOf ".tree.gen_trm" fp
 	    | ".casl"         `isSuffixOf` fp = stripOf ".casl" fp
 	    | otherwise = fp
 
-stripOf :: [a] -> [a] -> [a]
-stripOf suf = reverse . drop (length suf) . reverse
+stripOf :: (Show a, Eq a) => [a] -> [a] -> [a]
+stripOf suf inp = reverse $ stripOf' (reverse suf) (reverse inp)
+    where stripOf' []     i  = i
+	  stripOf' (x:xs) [] = error $ 
+			       concat ["suffix is longer than input string\n"
+				      ,"input was: ", show suf, " ",show inp ]
+	  stripOf' (x:xs) (y:ys) | x == y    = stripOf' xs ys
+				 | otherwise = 
+				     error $ concat ["suffix don't match input"
+						    ," at "
+						    ,show $ reverse (x:xs)
+						    ," ",show $ reverse (y:ys)]
+
+-- stripOfx suf = reverse . drop (length suf) . reverse
 
 write_casl_asc :: FilePath -> LIB_DEFN -> IO ()
 write_casl_asc oup ld =
