fabab0eccff87042949f49040c5b2997f923f42e
Klaus Luettich <luettich@informatik.uni-bremen.de>
2002-10-02 15:18:42 +0000

    Added a variant to Diagnosis called FatalError. Used for fail and fatal_error. Added a nice Show instance for Diagnosis. Added functions warning, non_fatal_error and fatal_error to produce the various levels of diagnosis. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@475 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Result.hs
+++ Result.hs
@@ -1,12 +1,23 @@
--- error messages
+{-| 
+   
+ > HetCATS/hetcats/Result.hs
+ > $Id$
+ > Authors: Till Mossakowski, Klaus Lüttich
+ > Year:   2002
+
+   This module provides a Result type and some monadic functions to
+   use this type for accumulation of errors and warnings occuring
+   during the analyse phases.
+
+-}
 
 module Result where
 
 import Id
 
 data Diagnosis = Error String Pos
+	       | FatalError String Pos
                | Warning String Pos
-		 deriving (Show)
 
 newtype Result a = Result ([Diagnosis],Maybe a)
     deriving (Show)
@@ -16,4 +27,33 @@ instance Monad Result where
   Result (errs, Nothing) >>= f = Result (errs,Nothing)
   Result (errs1, Just x) >>= f = Result (errs1++errs2,y)
      where Result (errs2,y) = f x
-  fail s = Result ([Error ("Fail: "++s) nullPos],Nothing)
+  fail s = Result ([FatalError s nullPos],Nothing) -- better use fatal_error
+
+fatal_error :: (String,Pos) -> Result a
+fatal_error (s,p) = Result ([FatalError s p], Nothing)  
+
+non_fatal_error :: a -> (String,Pos) -> Result a
+non_fatal_error x (s,p) = Result ([Error s p], Just x)  
+
+warning :: a -> (String,Pos) -> Result a
+warning x (s,p) = Result ([Warning s p], Just x)  
+
+instance Show Diagnosis where
+    showsPrec _ d = case d of
+		    Error s p      -> 
+			showString "Error: "      . showPosString p s
+		    FatalError s p -> 
+			showString "FatalError: " . showPosString p s
+		    Warning s p    -> 
+			showString "Warning: "    . showPosString p s
+	where 
+	showPosString :: Pos -> String -> String -> String 
+	showPosString p s = showString p' . showString s 
+	    where p' = case p of 
+		       (l,c) -> (showString "in line " .
+				 showsPrec 0 l .
+				 showString " at char " .
+				 showsPrec 0 c) ": "
+    showList [] = showString ""
+    showList [d] = showsPrec 0 d
+    showList (d:ds) = showsPrec 0 d . showString "\n" . showList ds
