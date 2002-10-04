fd40e201b7277427113c89724d8a2389c18e9cbd
Christian Maeder <Christian.Maeder@dfki.de>
2002-10-04 11:54:15 +0000

    converted lit annos git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@489 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CASL/MixfixParser.hs
+++ CASL/MixfixParser.hs
@@ -1,22 +1,16 @@
 module MixfixParser where
 
 import AS_Basic_CASL 
-import LocalEnv
+import Sign
 import GlobalAnnotations
 import Result
 import Id
+import Lexer (caslChar)
+import ParsecPrim
 import qualified Char as C
 
 
--- analyse Mixfix_token
-convertMixfixToken:: GlobalAnnos -> [varDecl] -> [OpItem] -> Token
-         -> ([Diagnosis], [TERM]) 
-
-isChar :: Token -> Bool
-isChar t = head (tokStr t) == '\''
-
-isString t = head (tokStr t) == '\"'
-isNumber t = C.isDigit $ head $ tokStr t
+-- convert LiteralAnnos from Ids to OpItems
 
 data LiteralOpItems = LitItem 
     { emptyString :: Maybe OpItem
@@ -30,17 +24,91 @@ data LiteralOpItems = LitItem
     , negExponent :: Maybe OpItem
     } deriving (Show)
 
-makeStringTerm is c f tok = 
-	 case filter (\x -> opId x == c && null (opArgs (opType x))) is of
-	 [e] -> let ty = opType e
-                    ot = (case opKind ty of 
-			     Total -> Total_op_type 
-			     Partial -> Partial_op_type) [] (opRes ty) []    
-		    a = Application (Qual_op_name c ot []) [] [tokPos tok]
+convertLitAnnos is la = 
+    LitItem {emptyString = case string_lit la of 
+	                   Nothing -> Nothing
+	                   Just (c, _) -> case lookupId is 0 c of
+	                                  [ci] -> Just ci
+	                                  _ -> Nothing
+            ,consString  = case string_lit la of 
+	                   Nothing -> Nothing
+	                   Just (_, f) -> case lookupId is 2 f of
+	                                  [fi] -> Just fi
+	                                  _ -> Nothing
+            ,emptyList   = case list_lit la of 
+	                   Nothing -> Nothing
+	                   Just (_, c, _) -> case lookupId is 0 c of
+	                                  [ci] -> Just ci
+	                                  _ -> Nothing
+            ,consList    = case list_lit la of 
+	                   Nothing -> Nothing
+	                   Just (_, _, f) -> case lookupId is 2 f of
+	                                  [fi] -> Just fi
+	                                  _ -> Nothing
+            ,listBrackets = case list_lit la of 
+	                    Nothing -> ([], [])
+	                    Just(Id bs _ _, _, _) -> 
+	                       let (b1, rt) = span (not . isPlace) bs
+			       in if null rt || any isPlace (tail rt) 
+				  then ([], []) 
+				  else (b1, tail rt)
+	    ,numberLit   = case number_lit la of 
+                           Nothing -> Nothing
+			   Just f -> case lookupId is 2 f of 
+	                                  [fi] -> Just fi
+	                                  _ -> Nothing
+            ,decimalOp   = case float_lit la of 
+	                   Nothing -> Nothing
+	                   Just (f, _) -> case lookupId is 2 f of
+	                                  [fi] -> Just fi
+	                                  _ -> Nothing
+            ,exponentOp  = case float_lit la of 
+	                   Nothing -> Nothing
+	                   Just (_, g) -> case lookupId is 2 g of
+	                                  [gi] -> Just gi
+	                                  _ -> Nothing
+            ,negExponent = case lookupId is 1 (Id [Token "-" nullPos] [] []) of
+				          [fi] -> Just fi
+	                                  _ -> Nothing
+	    }
+
+-- only check for the correct number of arguments
+lookupId is args i =
+	 filter (\x -> opId x == i && args == length(opArgs(opType x))) is
+
+isChar :: Token -> Bool
+isChar t = head (tokStr t) == '\''
+
+isString t = head (tokStr t) == '\"'
+isNumber t = C.isDigit $ head (tokStr t)
+
+split ::  GenParser Char () String -> String -> (String, String)
+split p s = let ph = do hd <- p;
+		        tl <- getInput;
+                        return (hd, tl) 
+            in case parse ph "" s of
+               Left _ -> error"split" 
+	       Right x -> x
+
+{-
+makeStringTerm ga tok = 
+    let c = emptyString ga in
+	    case c of 
+	    Nothing -> ([Error "no proper %string annotation" (tokPos tok)], [])
+            Just x -> let l = init (tail (tokStr tok))
+	              in if null l then ([], [x])
+			 else let f = consString ga
+                                  (hd, tl) = split caslChar l
+				  real = "'" ++ hd ++ "'" 
+
+
                     l = (init (tail (tokStr tok)))
                 in ([],[a]) -- continue for l
          _ -> error "not unique" 
-
+-}
+-- analyse Mixfix_token
+{-convertMixfixToken:: GlobalAnnos -> [varDecl] -> [OpItem] -> Token
+         -> ([Diagnosis], [TERM]) 
 
 convertMixfixToken ga vs is t = 
      if isPlace t then ([], [Mixfix_token t])
@@ -49,3 +117,4 @@ convertMixfixToken ga vs is t =
 	  Nothing -> ([Error "missing %string annotation" (tokPos t)], [])
           Just (c, f) -> makeStringTerm is c f t
      else error "not implemented yet"
+-}
