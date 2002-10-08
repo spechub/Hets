6e5180855658f12f9059d9041f447bf0935de344
Christian Maeder <Christian.Maeder@dfki.de>
2002-10-08 18:54:56 +0000

    moved duplicate parts from HToken.hs to Token.hs git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@497 cec4b9c1-7d33-0410-9eda-942365e851bb

--- HasCASL/HToken.hs
+++ HasCASL/HToken.hs
@@ -14,7 +14,7 @@ import Id
 import Keywords
 import Lexer
 import Token(casl_reserved_ops, casl_reserved_words
-	    , formula_words, formula_ops)
+	    , formula_words, formula_ops, start, mixId)
 import Parsec
 
 -- ----------------------------------------------
@@ -44,6 +44,8 @@ derivingS = "deriving"
 hascasl_reserved_ops = [dotS++exMark, cDot++exMark, asP, assignS, lamS] 
 		       ++ casl_reserved_ops
 
+hascasl_reserved_fops = formula_ops ++ hascasl_reserved_ops
+
 hascasl_type_ops = [funS, pFun, contFun, pContFun, prodS, timesS, quMark, 
 		   lessS] 
 
@@ -51,61 +53,20 @@ hascasl_reserved_words =
     [classS, instanceS, programS, caseS, ofS, letS, derivingS] 
 			 ++ casl_reserved_words
 
-scanWords = reserved (formula_words ++ hascasl_reserved_words) scanAnyWords 
+hascasl_reserved_fwords = formula_words ++ hascasl_reserved_words
+
+scanWords = reserved hascasl_reserved_fwords scanAnyWords 
 scanTermWords = reserved hascasl_reserved_words scanAnyWords
 
--- ----------------------------------------------
--- bracket-token (for ids)
--- ----------------------------------------------
-
--- simple id (but more than only words)
-sid l = pToken (scanQuotedChar <|> scanDotWords 
-		<|> scanDigit <|> reserved l scanAnySigns 
-		<|> scanWords <?> "simple-id")
-
--- balanced mixfix-components {...}, [...]
-braced l = begDoEnd oBraceT (innerList l) cBraceT
-noComp l = begDoEnd oBracketT (innerList l) cBracketT
-
--- alternating sid and other mixfix components (including places)
--- no two sid stand side by side
-innerMix1 l = sid l <:> option [] (innerMix2 l)
-innerMix2 l = flat (many1 (braced l <|> noComp l<|> many1 placeT))
-            <++> option [] (innerMix1 l)
-
--- ingredients starting either with an sid or brackets, braces or place 
-innerList l =  option [] (innerMix1 l <|> innerMix2 l <?> "token")
-
--- a mixfix component starting with an sid (outside innerList)
-topMix1 l = sid l <:> option [] (topMix2 l)
-
--- following an sid only braced mixfix-components are acceptable
--- square brackets after an sid will be taken as compound part
-topMix2 l = flat (many1 (braced l)) <++> option [] (topMix1 l)
-
--- square brackets (as mixfix component) are ok following a place 
-topMix3 l = noComp l <++> flat (many (braced l)) <++> option [] (topMix1 l)
-
-afterPlace l = topMix1 l <|> topMix2 l<|> topMix3 l
-
--- places and something balanced possibly including places as well  
-middle l = many1 placeT <++> option [] (afterPlace l)  
-
--- balanced stuff interspersed with places  
-tokStart l = afterPlace l <++> flat (many (middle l))
-
--- at least two places on its own or a non-place possibly preceded by places 
-start l = tokStart l <|> placeT <:> (tokStart l <|> 
-				 many1 placeT <++> option [] (tokStart l))
-				     <?> "id"
 -- ----------------------------------------------
 -- non-compound mixfix ids (variables)
 -- ----------------------------------------------
 var :: GenParser Char st Id
-var = fmap (\l -> Id l [] []) (start hascasl_reserved_ops)
+var = fmap (\l -> Id l [] []) (start (hascasl_reserved_fwords,
+				      hascasl_reserved_ops))
 
 -- ----------------------------------------------
--- compound/bracketed lists
+-- bracketed lists
 -- ----------------------------------------------
 bracketParser :: GenParser Char st a -> GenParser Char st Token 
 	 -> GenParser Char st Token -> GenParser Char st Token
@@ -121,35 +82,22 @@ bracketParser parser op cl sep k =
 brackets parser k = bracketParser parser oBracketT cBracketT commaT k
 
 -- ----------------------------------------------
--- mixfix and compound ids
+-- mixIds
 -- ----------------------------------------------
 
--- a compound list
-comps :: GenParser Char st ([Id], [Pos])
-comps =     do { o <- oBracketT
-	       ; (ts, ps) <-  mixId (formula_ops ++ hascasl_reserved_ops)
-		 `separatedBy` commaT
-	       ; c <- cBracketT
-	       ; return (ts, toPos o ps c)
-	       } <?> "[<id>,...,<id>]"
+-- allow type_ops as op_symbols (like "*")
+uninstOpName = mixId (hascasl_reserved_fwords, hascasl_reserved_fops)
+	       (hascasl_reserved_fwords, hascasl_reserved_fops)
 
--- a compound list does not follow a place
--- but after a compound list further places may follow
-mixId :: [String] -> GenParser Char st Id
-mixId keys = do { l <- start keys
-		; if isPlace (last l) then return (Id l [] [])
-		  else (do { (c, p) <- option ([], []) comps
-			   ; u <- many placeT
-			   ; return (Id (l++u) c p)
-			   })
-		}
-
-uninstOpName = mixId (formula_ops ++ hascasl_reserved_ops)
-typeName = mixId (hascasl_type_ops ++ formula_ops ++ hascasl_reserved_ops)
+-- prohibit type_ops on the top-level
+typeName = mixId (hascasl_reserved_fwords,
+		  hascasl_type_ops ++ hascasl_reserved_fops)
+	   (hascasl_reserved_fwords, hascasl_reserved_fops)
 
 -- ----------------------------------------------
 -- TYPE-VAR Ids
 -- ----------------------------------------------
+
 -- no compound ids (just a word) 
 typeVar :: GenParser Char st Token
 typeVar = pToken scanWords
