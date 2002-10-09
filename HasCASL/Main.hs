b6237a01273b45e8cf739186d5228fb5a0ab723d
Christian Maeder <Christian.Maeder@dfki.de>
2002-10-09 15:53:54 +0000

    used RunParsers git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@501 cec4b9c1-7d33-0410-9eda-942365e851bb

--- HasCASL/Main.hs
+++ HasCASL/Main.hs
@@ -4,49 +4,19 @@ import ParseItem
 import ParseTerm
 import PrintAs
 import HToken
-import Parsec
-import ParsecPos
-import Pretty
-import PrettyPrint
-import System
-import GlobalAnnotationsFunctions
+import RunParsers
 
-main = do {l <- getArgs;
-	   if length l < 2 then print 
-	   "usage: main <opt> <filename>"
-	   else let opt = head l 
-	            file = head (tail l)
-	   in if opt == "id" then checkLines uninstOpName file
-	   else if opt == "typename" then checkLines typeName file
-	   else if opt == "type" then checkLines parseType file
-	   else if opt == "term" then checkLines term file
-	   else if opt == "typepattern" then checkLines typePattern file
-	   else if opt == "pattern" then checkLines pattern file
-	   else if opt == "item" then checkLines basicSpec file
-	   else if opt == "items" then parseSpec file
-	   else print ("unknown option: " ++ opt) 
-	  }
+main :: IO ()
+main = exec lineParser fileParser
 
-checkLines p fileName = do { s <- readFile fileName
-			   ; putStr (unlines (scanLines p (lines s) 1))
-			   }
+lineParser, fileParser :: [(String, HetParser)]
+lineParser = [
+ ("id", HetParser uninstOpName),
+ ("typename", HetParser typeName),
+ ("type", HetParser parseType),
+ ("term", HetParser term),
+ ("typepattern", HetParser typePattern),
+ ("pattern", HetParser pattern),
+ ("item", HetParser basicSpec)]
 
-scanLines _ [] _ = []
-scanLines p (x:l) n = (parseLine p x n) : (scanLines p l (n+1))
-
-parseLine p line n = let pos = setSourceLine (initialPos "") n
-			 parser = do { setPosition pos
-				     ; i <- p
-				     ; eof
-				     ; return i
-				     }
-		     in result (parse parser "" line)
-
-
-parseSpec fileName =  do { r <- parseFromFile basicSpec fileName
-			 ; putStrLn (result r)
-			 }
-   
-result r = case r of Left err -> "parse error at " ++ show err ++ "\n"
-		     Right x  -> renderText (printText0 emptyGlobalAnnos x)
-
+fileParser = [("items", HetParser basicSpec)]
