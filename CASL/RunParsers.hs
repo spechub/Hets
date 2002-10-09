735c3f6e283bae475c450668177ede573e4ab89e
Christian Maeder <Christian.Maeder@dfki.de>
2002-10-09 15:27:33 +0000

    reorganized Main.hs into RunParsers.hs to be used for HasCASL as well git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@499 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ CASL/RunParsers.hs
@@ -0,0 +1,66 @@
+
+{- HetCATS/CASL/RunParsers.hs
+   $Id$
+   Authors: Christian Maeder
+   Year:    2002
+   
+   test some parsers (and printers)
+-}
+
+module RunParsers (exec, HetParser(HetParser)) where 
+
+import Parsec
+import ParsecPos
+import PrettyPrint
+import System
+import GlobalAnnotationsFunctions
+
+data HetParser = forall a. PrettyPrint a => HetParser (Parser a)
+
+exec :: [(String, HetParser)] -> [(String, HetParser)] -> IO ()
+exec lps fps = do l <- getArgs
+		  if length l == 1 then case snd $ head $ fps of
+					HetParser p -> parseSpec (head l) p
+		     else if length l == 2 then
+		         let opt = head l
+			     file = head (tail l)
+                             lps' = filter (\(s, _) -> s == opt) lps
+                             fps' = filter (\(s, _) -> s == opt) fps
+		         in if null lps' && null fps' then
+			    print ("unknown option: " ++ opt) 
+			    else if null lps' then 
+				 case snd $ head $ fps' of 
+					  HetParser p -> parseSpec file p 
+				 else case snd $ head $ lps' of
+				      HetParser p -> checkLines p file
+			 else do p <- getProgName
+                                 print ("Usage: " ++ p ++ " [OPTIONS] <file>\n"
+                                   ++ "where OPTIONS is one of\n"
+                                   ++ unwords (map fst lps ++ map fst fps)) 
+
+checkLines :: (PrettyPrint a) => Parser a -> FilePath -> IO ()
+checkLines p fileName = do s <- readFile fileName
+			   putStr (unlines (scanLines p (lines s) 1))
+
+scanLines :: (Show tok, PrettyPrint a) =>
+             GenParser tok () a -> [[tok]] -> Line -> [String]
+scanLines _ [] _ = []
+scanLines p (x:l) n = (parseLine p x n) : (scanLines p l (n+1))
+
+parseLine :: (Show tok, PrettyPrint a) =>
+             GenParser tok () a -> [tok] -> Line -> String
+parseLine p line n = let pos = setSourceLine (initialPos "") n
+			 parser = do setPosition pos
+				     i <- p
+				     eof
+				     return i
+		       in result (parse parser "" line)
+
+parseSpec :: (PrettyPrint a) => SourceName -> Parser a -> IO ()
+parseSpec fileName p  =  do r <- parseFromFile p fileName
+			    putStrLn (result r)
+
+result :: (Show a, PrettyPrint b) => Either a b -> String
+result r = case r of Left err -> "parse error at " ++ show err ++ "\n"
+		     Right x  -> renderText (printText0 emptyGlobalAnnos x) 
+
