2854019fc62b252e01999e2c7796e914764367ed
Klaus Luettich <luettich@informatik.uni-bremen.de>
2002-11-04 14:07:35 +0000

    Added various funtions to PrettyPrint in LaTeX. Not ready to use but in a consistent state with minor formatting errors. No correct indentation yet! git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@581 cec4b9c1-7d33-0410-9eda-942365e851bb

--- PPUtils.hs
+++ PPUtils.hs
@@ -12,14 +12,17 @@
      - Add your own functions.
 -}
 
-module PPUtils where
+module PPUtils (module PPUtils,module LaTeX_funs) where
  
+import Id
 import AS_Annotation
 import GlobalAnnotations
 
 import Print_AS_Annotation
 import Pretty
 import PrettyPrint
+
+import LaTeX_funs 		  
 
 -- | 
 -- a helper class for calculating the pluralS of e.g. ops
@@ -33,17 +36,22 @@ instance ListCheck a => ListCheck (Annoted a) where
 
 -- |
 -- pluralS checks a list with elements in class ListCheck for a list
--- greater than zero. It returns empty if the list and all nested lists
--- have only one element. If the list or an nested list has more than one
--- element a Doc containig one "s" is returned. 
-pluralS :: ListCheck a => [a] -> Doc
+-- greater than zero. It returns an empty String if the list and all
+-- nested lists have only one element. If the list or an nested list
+-- has more than one element a String containig one "s" is returned.
+pluralS :: ListCheck a => [a] -> String
 pluralS l = 
     if length l > 1 then lastS 
     else case l of 
 	     [x] -> if x `innerListGT` 1 then lastS 
-		    else empty
+		    else ""
 	     _ -> error "pluralS do not accept list with zero elements"
-    where lastS = char 's' 
+    where lastS = "s"
+
+pluralS_doc :: ListCheck a => [a] -> Doc
+pluralS_doc l = case pluralS l of
+		"" -> empty
+		s  -> ptext s
 
 --------------------------------------------------------------------------
 
@@ -63,8 +71,8 @@ sp_brackets p = sp_hang (sp_hang lbrack 1 p) 0 rbrack
 sp_braces :: Doc -> Doc 
 sp_braces p = sp_hang (sp_hang lbrace 1 p) 0 rbrace
 
-sp_between :: String -> String -> Doc -> Doc
-sp_between lb rb p = sp_hang (sp_hang (ptext lb) 1 p) 0 (ptext rb)
+sp_between :: Doc -> Doc -> Doc -> Doc
+sp_between lb rb p = sp_hang (sp_hang lb 1 p) 0 rb
 
 -- |
 -- like punctuate from Pretty, but prepends symbol to every element 
@@ -72,6 +80,10 @@ sp_between lb rb p = sp_hang (sp_hang (ptext lb) 1 p) 0 (ptext rb)
 prepPunctuate :: Doc -> [Doc] -> [Doc]
 prepPunctuate _ [] = []
 prepPunctuate symb (x:xs) = x:map (\e -> symb <> e) xs
+
+-- | 
+-- the functions 'commaT', 'semiT', 'crossT' and 'semiAnno' are good
+-- for ASCII pretty printing but don't work well for LaTeX output.
 
 commaT,semiT,crossT :: PrettyPrint a => (GlobalAnnos -> a -> Doc) -> 
 		       GlobalAnnos -> [a] -> Doc
@@ -91,3 +103,62 @@ semiAnno pf ga l = if null l then
 			       $$ pf ga (item x) <> semi 
 			       <+> pf ga (r_annos x)) 
 		              (init l) ++ [pf ga (last l)]
+
+--------------------------------------------------------------------
+
+-- | 
+-- LaTeX variants of 'commaT' ...
+commaT_latex,semiT_latex,crossT_latex 
+    :: PrettyPrint a => (GlobalAnnos -> a -> Doc) -> 
+                         GlobalAnnos -> [a] -> Doc
+commaT_latex pf ga l = fsep_latex $ punctuate comma_latex $ map (pf ga) l
+
+semiT_latex pf ga l = fsep_latex $ punctuate semi_latex $ map (pf ga) l
+
+crossT_latex pf ga l = 
+    fsep_latex $ punctuate (space_latex <> hc_sty_axiom "\\times") $ 
+	       map (pf ga) l
+
+semiAnno_latex :: (PrettyPrint a) => 
+		  (forall b .PrettyPrint b => GlobalAnnos -> b -> Doc) ->  
+		  GlobalAnnos -> [Annoted a] -> Doc
+semiAnno_latex pf ga l = if null l then 
+		   empty 
+		else 
+		   vcat $ map (\x -> pf ga (l_annos x) 
+			       $$ pf ga (item x) <> semi_latex 
+			       <\+> pf ga (r_annos x)) 
+		              (init l) ++ [pf ga (last l)]
+
+hc_sty_casl_keyword :: String -> Doc
+hc_sty_casl_keyword = hc_sty_keyword (Just "preds")
+
+sp_hang_latex :: Doc -> Int -> Doc -> Doc
+sp_hang_latex d1 n d2 = cat [d1, nest_latex n d2]
+
+sp_between_latex :: Doc -> Doc -> Doc -> Doc
+sp_between_latex lb rb p = sp_hang_latex (sp_hang_latex lb 1 p) 0 rb
+
+sp_braces_latex :: Doc -> Doc
+sp_braces_latex = 
+    sp_between_latex (casl_normal_latex "\\{") (casl_normal_latex "\\}")
+
+sp_brackets_latex :: Doc -> Doc
+sp_brackets_latex =
+    sp_between_latex (casl_normal_latex "[") (casl_normal_latex "]")
+
+simple_id_latex :: SIMPLE_ID -> Doc 
+simple_id_latex = hc_sty_structid . tokStr
+
+-- |
+-- functions for nice indentation
+hetcasl_nest_latex :: Doc -> Doc
+hetcasl_nest_latex = nest (7 * indent_mult)
+
+casl_nest_latex :: Doc -> Doc
+casl_nest_latex = nest (5 * indent_mult)
+
+params_nest_latex :: Doc -> Doc
+params_nest_latex = nest (3 * indent_mult)
+
+
--- PrettyPrint.hs
+++ PrettyPrint.hs
@@ -16,29 +16,22 @@
 
 module PrettyPrint where
 
+import Char (isSpace)
+
 import Id
 import Pretty
 import GlobalAnnotations
+import LaTeX_funs
+
+import IOExts (trace)
 
 -- This type class allows pretty printing of instantiating Datatypes
 -- for infomation on the predefined functions see above
 class Show a => PrettyPrint a where
     printLatex, printLatex0, printText, printText0 :: GlobalAnnos -> a -> Doc
     printLatex0 ga a = printText0 ga a
-    printLatex  ga a = printText  ga a
+    printLatex  ga a = printLatex0  ga a
     printText   ga a = printText0 ga a
-
--- copied instance from Id.hs (to avoid cyclic imports via GlobalAnnotations)
-instance PrettyPrint Token where
- printText0 _ t = ptext (tokStr t)
-
-instance PrettyPrint Id where
- printText0 _ i = text (showId i "")
-
--- some useful instances ---------------------------------------------
-instance PrettyPrint String where
-    printText0  _ = ptext
-    printLatex0 _ = ptext
 
 {-instance (PrettyPrint a) => PrettyPrint [a] where
     printText0  ga l = cat $ map (printText0  ga) l
@@ -48,20 +41,118 @@ instance PrettyPrint String where
 -}
 ----------------------------------------------------------------------
 -- two Styles for Formatting (Standard is Style PageMode 100 1.5)
+
 latexStyle, textStyle :: Style
-latexStyle = style {lineLength=80, ribbonsPerLine= 1.19} 
+textStyle = style {lineLength=80, ribbonsPerLine= 1.19} 
 -- maximum line length 80 with 67 printable chars (up to 13 indentation chars) 
-textStyle = latexStyle
+latexStyle = textStyle {ribbonsPerLine = 1.2,lineLength = 121095}
+
+latex_txt :: TextDetails -> String -> String
+latex_txt (Chr c)   s  = c:s
+latex_txt (Str s1)  s2
+    | null s1        = s2
+    | all isSpace s1 = sub_space s1 ++ s2
+    | otherwise      = s1 ++ s2
+latex_txt (PStr s1) s2 = -- case s1 of 
+			 -- s | s == "[" || s == "PowerTheorems" 
+			    --   -> trace ("gotcha " ++ s) (s1 ++ s2) 
+			   -- | otherwise ->  
+			       s1 ++ s2
+-- | 
+-- A constant for calculating indentation
+indent_mult :: Int
+indent_mult = 13
+
+-- |
+-- sub_space is a sophisticatecd way to determine the right
+-- indentation. So therefor the latex_nest [5,6,7] are reserved for
+-- this purpose
+
+sub_space :: String -> String
+sub_space s1 = -- trace ("number of Spaces " ++ show len_s1) $
+    -- indent_sign ((len_s1 `mod` space_latex_width) `div` indent_mult) ++
+	 replicate (len_s1 `div` space_latex_width) ' '
+    where indent_sign x
+	      | x - 15 >= 0 = indent_HC ++ indent_CA ++ indent_PA
+	      | x - 12 >= 0 = indent_HC ++ indent_CA 
+	      | x - 10 >= 0 = indent_HC ++ indent_PA
+	      | x -  7 >= 0 = indent_HC
+	      | x -  5 >= 0 = indent_CA
+	      | x -  3 >= 0 = indent_PA
+	      | otherwise  = "" 
+	      where  indent_HC = "\\IndHC{}"
+		     indent_CA = "\\IndCA{}"
+ 		     indent_PA = "\\IndPA{}"
+          len_s1 :: Int
+	  len_s1 = cntSpc s1 0
+	  cntSpc [] l     = l
+	  cntSpc (x:xs) l 
+	      | x == '\t' = cntSpc xs (l+8)
+	      | otherwise = cntSpc xs (l+1)
+    {- add_space = if len_s1 `mod` space_latex_width > 0 
+		      then "\\hspace{" ++ rem_space ++ "mm}"
+		      else ""
+	  rem_space = (\(s,f) -> reverse f ++ "." ++ reverse s) $ 
+		      splitAt 3 $ reverse $ 
+			      show (len_s1 `mod` space_latex_width)-}
+
+
+latex_io :: TextDetails -> IO () -> IO ()
+latex_io (Chr c)   io  = putChar c >> io
+latex_io (Str s) io 
+    | null s        = io
+    | all isSpace s = putStr (sub_space s) >> io
+    | otherwise      = putStr s >> io
+latex_io (PStr s) io = putStr s >> io
+
+
 
 -- functions for producing IO printable Strings
-renderLatex,renderText,renderLatexVerb :: Doc -> String
+renderLatex,renderText,renderLatexVerb :: Maybe Int -> Doc -> String
 
-renderLatex d = renderStyle latexStyle d
+renderLatex mi d = fullRender (mode           latexStyle')
+		              (lineLength     latexStyle')
+			      (ribbonsPerLine latexStyle')
+			      latex_txt
+			      ""
+			      d'
+    where d' = ptext "\\begin{hetcasl}" $+$ d $+$ ptext "\\end{hetcasl}"
+	  latexStyle' = latexStyle {lineLength = 
+				     (case mi of
+				      Nothing -> lineLength latexStyle
+				      Just l  -> l) }
+
 -- this lacks some environment starting and closing LaTeX commands
 
-renderLatexVerb d = renderStyle latexStyle d'
+renderLatexVerb mi d = renderStyle textStyle' d'
     where d' = text "\\begin{verbatim}" $+$ d $+$ text "\\end{verbatim}"
+	  textStyle' = textStyle {lineLength = 
+				     (case mi of
+				      Nothing -> lineLength textStyle
+				      Just l  -> l) }
 
-renderText d = renderStyle textStyle d
+renderText mi d = renderStyle textStyle' d
+    where textStyle' = textStyle {lineLength = 
+				     (case mi of
+				      Nothing -> lineLength textStyle
+				      Just l  -> l) }
+
+-- moved instance from Id.hs (to avoid cyclic imports via GlobalAnnotations)
+instance PrettyPrint Token where
+    printText0 _ = ptext . tokStr
+
+    printLatex0 _ = hc_sty_id . escape_latex . tokStr 
+
+instance PrettyPrint Id where
+    printText0 _ i = text (showId i "")
+    printLatex0 _ i = hc_sty_id $ escape_latex $ showId i ""
+
+-- some useful instances ---------------------------------------------
+
+
+instance PrettyPrint String where
+    printText0  _ = ptext 
+    printLatex0  _ = error "use a 'casl_*_text' function directly" 
+
 
 
