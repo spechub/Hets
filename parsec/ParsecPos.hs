0c8691719cb3595d5590047c54eba818f9f07501
Christian Maeder <Christian.Maeder@dfki.de>
2003-01-30 13:49:43 +0000

    removed incSourceLine for \n and removed warnings git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@803 cec4b9c1-7d33-0410-9eda-942365e851bb

--- parsec/ParsecPos.hs
+++ parsec/ParsecPos.hs
@@ -6,6 +6,7 @@
 -- $Revision$
 -- $Author$
 -- $Date$
+-- $Id$
 ----------------------------------------------------------------
 module ParsecPos  ( SourceName, Line, Column                 
                   , SourcePos
@@ -24,27 +25,32 @@ type SourceName     = String
 type Line           = Int
 type Column         = Int
 
-data SourcePos      = SourcePos SourceName !Line !Column
+data SourcePos      = SourcePos { sourceName :: SourceName 
+				, sourceLine :: !Line
+				, sourceColumn :: !Column
+				}
 		     deriving (Eq,Ord)
 		
 
 newPos :: SourceName -> Line -> Column -> SourcePos
-newPos sourceName line column
-    = SourcePos sourceName line column
+newPos name line column
+    = SourcePos name line column
 
-initialPos sourceName
-    = newPos sourceName 1 1
+initialPos :: SourceName -> SourcePos
+initialPos name
+    = newPos name 1 1
 
-sourceName   (SourcePos name line column)   = name    
-sourceLine   (SourcePos name line column)   = line    
-sourceColumn (SourcePos name line column)   = column
+incSourceLine :: SourcePos -> Line -> SourcePos
+incSourceLine   s n    = s { sourceLine = sourceLine s + n }
+incSourceColumn :: SourcePos -> Column -> SourcePos
+incSourceColumn s n    = s { sourceColumn = sourceColumn s + n }
 
-incSourceLine   (SourcePos name line column) n    = SourcePos name (line+n) column
-incSourceColumn (SourcePos name line column) n    = SourcePos name line (column+n)
-
-setSourceName   (SourcePos name line column) n    = SourcePos n line column
-setSourceLine   (SourcePos name line column) n    = SourcePos name n column
-setSourceColumn (SourcePos name line column) n    = SourcePos name line n
+setSourceName :: SourcePos -> SourceName -> SourcePos
+setSourceName   s n    = s { sourceName = n }
+setSourceLine :: SourcePos -> Line -> SourcePos
+setSourceLine   s n    = s { sourceLine = n }
+setSourceColumn :: SourcePos -> Column -> SourcePos
+setSourceColumn s n    = s { sourceColumn = n }
 
 -----------------------------------------------------------
 -- Update source positions on characters
@@ -54,17 +60,16 @@ updatePosString pos string
     = forcePos (foldl updatePosChar pos string)
 
 updatePosChar   :: SourcePos -> Char -> SourcePos
-updatePosChar pos@(SourcePos name line column) c   
+updatePosChar (SourcePos name line column) c   
     = forcePos $
       case c of
         '\n' -> SourcePos name (line+1) 1
-        '\r' -> SourcePos name (line+1) 1
         '\t' -> SourcePos name line (column + 8 - ((column-1) `mod` 8))
         _    -> SourcePos name line (column + 1)
         
 
 forcePos :: SourcePos -> SourcePos      
-forcePos pos@(SourcePos name line column)
+forcePos pos@(SourcePos _ line column)
     = seq line (seq column (pos))
 
 -----------------------------------------------------------
