9aaaef25f7a8b5ac24915d1f830d39740446c587
Christian Maeder <Christian.Maeder@dfki.de>
2003-02-05 14:57:28 +0000

    adapted to Lexer, Keywords and Token rather than CaslLanguage git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@820 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Anno_Parser.hs
+++ Anno_Parser.hs
@@ -1,14 +1,11 @@
 
 {- HetCATS/Anno_Parser.hs
-   Author: Klaus Lüttich
-   Year:   2002
+   Authors: Klaus Lüttich, Christian Maeder
+   Year:   2002/2003
 
    This file implements parsers for annotations and annoted items.
 
-   todo:
-     - parsing contents of annotations
-     - annoted parser
- 
+   used Lexer, Keywords and Token rather than CaslLanguage 
 -}
 
 module Anno_Parser where
@@ -17,12 +14,14 @@ import ParsecError
 import ParsecPos
 import ParsecPerm
 
-import Utils
+import Lexer
+import Token
 
-import CaslLanguage
 import Id
 import AS_Annotation
 
+
+comment, commentLine, commentGroup :: GenParser Char st Annotation
 comment = commentLine <|> commentGroup
 
 commentLine = do try (string "%%")
@@ -32,13 +31,14 @@ commentLine = do try (string "%%")
 commentGroup = do try (string "%{")
 		  text_lines <- manyTill anyChar (try (string "}%"))
 		  sp <- getPosition
-		  return (Comment_group (lines text_lines) [c sp])
-    where c sp = (\(l,c) -> (l,c-2)) (convToPos sp)
+		  return (Comment_group (lines text_lines) [conv sp])
+    where conv sp = (\(l,c) -> (l,c-2)) (convToPos sp)
 
+annote, label :: GenParser Char st Annotation
 annote = Anno_Parser.label <|> 
 	 do start_source_pos <- getPosition
-	    id <- anno_ident
-	    anno <- ((annote_group id) <|> (annote_line id))
+	    ide <- anno_ident
+	    anno <- ((annote_group ide) <|> (annote_line ide))
 	    end_pos <- getPosition
 	    case (parse_anno (add_pos end_pos anno) start_source_pos) of
 	        Left  err -> do setPosition (errorPos err)
@@ -49,43 +49,50 @@ annote = Anno_Parser.label <|>
 					    (errorMessages err)))
 	        Right pa -> return pa
     where add_pos sp a =
-	      up_pos_l (\l -> l++[c sp a]) a
-	  c sp a = (\(l,c) -> case a of
+	      up_pos_l (\l -> l++[conv sp a]) a
+	  conv sp a = (\(l,c) -> case a of
 			   Annote_group _ _ _ -> (l,c-2)
 			   Annote_line  _ _ _ -> (l,c)
 			   _ -> error "nothing to be done for other Annos"
 		   ) (convToPos sp)
+
 label = do try(string "%(")
 	   label_lines <- manyTill anyChar (string ")%")
 	   sp <- getPosition
-	   return (Label (lines label_lines) [c sp])
-    where c sp = (\(l,c) -> (l,c-2)) (convToPos sp)
+	   return (Label (lines label_lines) [conv sp])
+    where conv sp = (\(l,c) -> (l,c-2)) (convToPos sp)
 
-anno_ident = string "%" >> casl_words
+anno_ident :: GenParser Char st String
+anno_ident = string "%" >> scanAnyWords
 
-annote_group id = do char '(' -- ) 
-		     annote_lines <- manyTill anyChar (string ")%")
-		     return (Annote_group id (lines annote_lines) [])
+annote_group, annote_line :: String -> GenParser Char st Annotation
+annote_group ide = 
+    do char '(' -- ) 
+       annote_lines <- manyTill anyChar (string ")%")
+       return (Annote_group ide (lines annote_lines) [])
 
-annote_line id = do anno <- do char ' '
-			       annote_line <- manyTill anyChar 
+annote_line ide = 
+                 do anno <- do char ' '
+			       line <- manyTill anyChar 
 			                               (newline <|>
 							(eof >> return '\n'))
-			       return (Annote_line id annote_line [])
+			       return (Annote_line ide line [])
 			       -- AnnoteWord (%implies ...)
 			    <|> do newline
-				   return (Annote_line id "" [])
+				   return (Annote_line ide "" [])
 		    return(anno)
 
+annotationL, annotation :: GenParser Char st Annotation
 annotationL = do start_source_pos <- getPosition
 		 anno <- (comment <|> annote)
 		 return (add_pos anno (convToPos start_source_pos))
     where add_pos an p = up_pos_l (\l -> p:l) an
 
 annotation = do a <- annotationL  
-		whiteSpace -- cause all parsers above are not lexeme
+		skip -- cause all parsers above are not lexeme
 		return a
 
+annotations :: GenParser Char st [Annotation]
 annotations = many annotation
 
 -----------------------------------------
@@ -101,7 +108,7 @@ parse_anno anno sp = case anno of
 			 "cons"    -> semantic_anno Conservative kw as sp pos
 			 "mono"    -> semantic_anno Monomorph    kw as sp pos
 			 _         -> parse_anno (Annote_group kw [as] pos) sp 
-		     Annote_group kw as pos -> 
+		     Annote_group kw as _ -> 
 			 case kw of
 			  "prec"    -> parse_internal prec_anno    nsp inp
 			  "display" -> parse_internal display_anno nsp inp
@@ -126,22 +133,26 @@ parse_anno anno sp = case anno of
 					     return (Lassoc_anno res [])     
 			       rassoc p = do res <- p
 					     return (Rassoc_anno res [])
-			       assoc_anno = (sepBy1 casl_id comma)
+			       assoc_anno = (sepBy1 parseId commaT)
 
 
 		     _ -> Right anno
 
+parse_internal :: GenParser Char () a -> SourcePos -> [Char] 
+	       -> Either ParseError a
 parse_internal p sp inp = parse (do setPosition sp
-				    whiteSpace
+				    skip
 				    res <- p
 				    return res
 				)
 			        (sourceName sp)
 			        inp 
 
-prec_anno = do left_ids <- braces (sepBy1 casl_id comma)
-	       sign <- (try (symbol "<>") <|> (symbol "<"))
-	       right_ids <- braces (sepBy1 casl_id comma)
+prec_anno, number_anno, string_anno, list_anno, floating_anno 
+    :: GenParser Char st Annotation
+prec_anno = do left_ids <- oBraceT >> sepBy1 parseId commaT << cBraceT
+	       sign <- (try (string "<>") <|> (string "<")) << skip
+	       right_ids <- oBraceT >> (sepBy1 parseId commaT) << cBraceT
 	       return ( Prec_anno (sign == "<")
 				  left_ids
 				  right_ids
@@ -161,36 +172,42 @@ list_anno     = literal_anno f 3 "list"
     where f [x1,x2,x3] = List_anno x1 x2 x3
 	  f _          = error "wrong_number of ids"
 
-literal_anno con count conStr = 
-    do ids <- sepBy1 casl_id comma
-       if length ids == count then return $ con ids $ []
+literal_anno :: ([Id] -> [a] -> b) -> Int -> [Char] -> GenParser Char st b
+literal_anno con nums conStr = 
+    do ids <- sepBy1 parseId commaT
+       if length ids == nums then return $ con ids $ []
 	else unexpected $ "Annotation \"" ++ conStr ++ 
 		          "\" malformed: wrong number of ids, " ++ 
-		          show count ++ " id(s) expected!" 
+		          show nums ++ " id(s) expected!" 
 
 
-display_anno = do ident <- casl_id
-		  tls <- permute ( mklst <$?> (disp_symb ident "HTML")
-				         <|?> (disp_symb ident "LATEX")
-				         <|?> (disp_symb ident "RTF") )
+display_anno :: GenParser Char st Annotation
+display_anno = do ident <- parseId
+		  tls <- permute ( mklst <$?> (disp_symb "HTML")
+				         <|?> (disp_symb "LATEX")
+				         <|?> (disp_symb "RTF") )
 		  return (Display_anno ident tls [])
     where mklst a b c = [a,b,c] 
-	  disp_symb ident sym = ((ready_symb,""),
-				 do { symb <- lexeme (try (string 
+	  disp_symb sym = ((ready_symb,""),
+				 do { symb <- (try (string 
 							   ready_symb))
+				              << skip
 				    ; str <- manyTill anyChar 
 				      ((fmap 
 					(\_->())
 					(lookAhead (string "%")))
 				       <|>
 				       eof)
-				    ; return (symb,chomp str)
+				    ; return (symb, reverse $
+					      dropWhile (`elem` whiteChars)
+					      $ reverse str)
 				    }
 				) where ready_symb = "%"++sym
 
-
+semantic_anno :: (a -> b) -> [Char] -> [Char] -> SourcePos -> a 
+	      -> Either ParseError b
 semantic_anno anno kw as sp pos = 
-    if (`elem` "\r\t\v\f \160") `all` as then 
+    if (`elem` whiteChars) `all` as then 
        Right (anno pos)
     else 
        Left (newErrorMessage (Expect("only whitespaces after %" ++ kw)) sp)
