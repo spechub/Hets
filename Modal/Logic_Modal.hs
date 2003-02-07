684ada8af5c3e6da5c1a69edb6f233c9f2db4ebd
Wiebke Herding <wiebke@informatik.uni-bremen.de>
2003-02-07 13:53:00 +0000

    Logic instance for Modal logic git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@847 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ Modal/Logic_Modal.hs
@@ -0,0 +1,52 @@
+{-# OPTIONS -fno-warn-missing-methods #-}
+{- HetCATS/Modal/Logic_Modal.hs
+   $Id
+   Authors: Wiebke Herding
+   Year:    2003
+-}
+module Logic_Modal where
+
+import Logic
+import Id
+import FiniteMap
+import Set
+import Maybe
+import AS_Modal
+
+data Modal = Modal deriving (Show)
+
+instance Language Modal where  -- default definition is okay
+
+type Sign = Set Id 
+instance Show Sign where
+
+type Morphism = (Sign, FiniteMap Id Id, Sign)
+
+
+instance Category Modal Sign Morphism  
+    where
+       -- ide :: id -> object -> morphism
+       ide Modal sigma = (sigma, listToFM [(i,i) | i<- setToList sigma], sigma)
+       -- o :: id -> morphism -> morphism -> Maybe morphism
+       o Modal (sigma1 ,m1,_) (_,m2,sigma2) =  
+	   Just (sigma1,plusFM m1 m2,sigma2)
+       -- dom, cod :: id -> morphism -> object
+       dom Modal (sigma,_,_) = sigma
+       cod Modal (_,_,sigma) = sigma
+       -- legal_obj :: id -> object -> Bool
+       legal_obj Modal _ = True
+       -- legal_mor :: id -> morphism -> Bool
+       legal_mor Modal (sigma,m,_)
+		| keysFM m == setToList sigma	 = True 
+		| True	 = False
+
+-- abstract syntax, parsing (and printing)
+
+instance Syntax Modal BASIC_SPEC 
+		SYMB_ITEMS SYMB_MAP_ITEMS
+      where 
+--         parse_basic_spec :: id -> Maybe(ParseFun basic_spec)
+--         parse_symb_items :: id -> Maybe(ParseFun symb_items)
+--         parse_symb_map_items :: id -> Maybe(ParseFun symb_map_items)
+
+
