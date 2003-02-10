190afe6ff67913ae82bb20fc2440d55eb66fe635
Till Mossakowski <mossakow@iws.cs.uni-magdeburg.de>
2003-02-10 15:46:34 +0000

    Refs to imported libs added git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@852 cec4b9c1-7d33-0410-9eda-942365e851bb

--- DevGraph.hs
+++ DevGraph.hs
@@ -30,14 +30,20 @@ import FiniteMap
 import Id
 
 data DGNode = DGNode {
-              dgn_sign :: G_sign, -- only the delta
-              dgn_sens :: G_l_sentence_list,  -- or better [(String,G_sentence)] ???
-              dgn_origin :: DGOrigin
+                dgn_sign :: G_sign, -- only the delta
+                dgn_sens :: G_l_sentence_list, 
+                                -- or better [(String,G_sentence)] ???
+                dgn_origin :: DGOrigin
+              }   
+            | DGRef { 
+                dgn_name :: SIMPLE_ID,
+                dgn_libname :: SIMPLE_ID, 
+                dgn_node :: Node
               }
             
 data DGLink = DGLink {
-              dgl_name :: String,
-              dgl_src, dgl_tar :: DGNode,
+              -- dgl_name :: String,
+              -- dgl_src, dgl_tar :: DGNode,  -- already in graph structure
               dgl_morphism :: G_morphism,
               dgl_type :: DGLinkType,
               dgl_origin :: DGOrigin }
@@ -55,10 +61,11 @@ data DGLinkType = LocalDef
               -- corresponds to a span of morphisms
               -- S1 <--m1-- S --m2--> S2
 
-data DGOrigin = DGBasic | DGExtension | DGTranslation | DGUnion | DGHiding | DGRevealing 
-              | DGRevealTranslation | DGFree | DGCofree | DGLocal | DGClosed 
-              | DGFormalParams | DGImports | DGSpecInst SIMPLE_ID | DGFitSpec | DGView
-              | DGFitView | DGFitViewImp | DGFitViewA | DGFitViewAImp
+data DGOrigin = DGBasic | DGExtension | DGTranslation | DGUnion | DGHiding 
+              | DGRevealing | DGRevealTranslation | DGFree | DGCofree 
+              | DGLocal | DGClosed 
+              | DGFormalParams | DGImports | DGSpecInst SIMPLE_ID | DGFitSpec 
+              | DGView | DGFitView | DGFitViewImp | DGFitViewA | DGFitViewAImp
 
 type DGraph = Graph DGNode DGLink
 
@@ -73,3 +80,10 @@ data GlobalEntry = SpecEntry ExtGenSig
                  | UnitEntry UnitSig
 
 type GlobalEnv = FiniteMap SIMPLE_ID GlobalEntry
+
+type LibEntry = (GlobalEnv,DGraph)
+
+type LibEnv = FiniteMap SIMPLE_ID LibEntry
+
+
+get_dgn_name :: DGNode -> Maybe String
