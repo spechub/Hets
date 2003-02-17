de53ae09bf9750c7c8754673a3a4dc7431ae9760
Daniel Pratsch <coldsky@tzi.de>
2003-02-17 13:30:08 +0000

    DP: Token-Parser git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@888 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CSP-CASL/CCToken.hs
+++ CSP-CASL/CCToken.hs
@@ -1,21 +1,37 @@
+{- **********************************************************************
+
+   $Source$
+
+   $Date$
+   $Revision$
+   Author: Daniel Pratsch (Last modification by $Author$)
+
+  ************************************************************************** 
+-}
+
 module CCToken 
 
 where
-
-import Id (Id(Id), Token(..), Pos, toPos, isPlace)
+{-import Id (Id(Id), Token(..), Pos, toPos, isPlace)-}
+import Id (Token(..))
 import Token
 import Lexer
-import Keywords
 import CCKeywords
 
-import Parsec
+--import Parsec
+
+import AnnoState
 
 csp_casl_reserved_words :: [String]
 csp_casl_reserved_words = casl_reserved_words ++
-                          [dataS, channelS, processS] 
+                          [dataS, channelS, processS] ++
+                          csp_casl_keywords
 
-channelName :: GenParser Char st Token
+channelName, var, namedProcess :: AParser Token
 channelName = pToken (reserved csp_casl_reserved_words scanAnyWords)
 
-namedProcess :: GenParser Char st Token
+var = pToken (reserved csp_casl_reserved_words scanAnyWords)
+
 namedProcess = pToken (reserved csp_casl_reserved_words scanAnyWords)
+
+--opList = pToken (reserved csp_casl_reserved_words scanAnyWords)
