bcfc9a09a6e6a1d47d63324e29dc1330f4deb5c8
Daniel Pratsch <coldsky@tzi.de>
2003-02-17 13:28:12 +0000

    DP: Parser fuer die Keywords git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@886 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CSP-CASL/CCLexer.hs
+++ CSP-CASL/CCLexer.hs
@@ -1,14 +1,61 @@
+{- **********************************************************************
+
+   $Source$
+
+   $Date$
+   $Revision$
+   Author: Daniel Pratsch (Last modification by $Author$)
+
+  ************************************************************************** 
+-}
+
 module CCLexer where
 
 import CCKeywords
 
-import Parsec
+-- import Parsec
 import Id (Token(..))
 
-import Lexer
+-- import Lexer
 
+import ItemList (asKey)
+import AnnoState
 
+dataT, endT, channelT, processT, skipT, stopT, ifT, thenT, elseT, whenT, varT,
+  multiPreT, prefixT, oRBracketT, cRBracketT, sendT, receiveT, extChoiceT, commaT,
+  intChoiceT, synParaT, interParaT, oAlPaT, cAlPaT, oGenPaT, mGenPaT, semicolonT,
+  cGenPaT, hidingT, oRenamingT, cRenamingT, colonT, chanRenT :: AParser Token
 
-extChoiceT, intChoiceT :: GenParser Char st Token
-extChoiceT = asKey extChoiceS
-intChoiceT = asKey intChoiceS
+dataT       = asKey dataS
+endT        = asKey endS
+channelT    = asKey channelS
+processT    = asKey processS
+skipT       = asKey skipS
+stopT       = asKey stopS
+ifT         = asKey ifS
+thenT       = asKey thenS
+elseT       = asKey elseS
+whenT       = asKey whenS
+varT        = asKey varS
+prefixT     = asKey prefixS
+multiPreT   = asKey multiPreS
+oRBracketT  = asKey oRBracketS
+cRBracketT  = asKey cRBracketS
+extChoiceT  = asKey extChoiceS
+intChoiceT  = asKey intChoiceS
+synParaT    = asKey synParaS
+interParaT  = asKey interParaS
+oAlPaT      = asKey oAlPaS
+cAlPaT      = asKey cAlPaS
+oGenPaT     = asKey oGenPaS
+mGenPaT     = asKey mGenPaS
+cGenPaT     = asKey cGenPaS
+hidingT     = asKey hidingS
+oRenamingT  = asKey oRenamingS 
+cRenamingT  = asKey cRenamingS
+sendT       = asKey sendS
+receiveT    = asKey receiveS
+commaT      = asKey commaS
+colonT      = asKey colonS
+semicolonT  = asKey semicolonS
+chanRenT    = asKey chanRenS 
