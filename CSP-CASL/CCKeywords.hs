127bd5978e305da10d0ada5f82fdbe1a8512a97f
Daniel Pratsch <coldsky@tzi.de>
2003-02-17 12:38:57 +0000

    DP: zweite Revision - Keyworddefinition + reserved CSP-CASL-keywords git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@884 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CSP-CASL/CCKeywords.hs
+++ CSP-CASL/CCKeywords.hs
@@ -1,12 +1,12 @@
-{-# OPTIONS -fno-warn-missing-signatures #-} 
--- ?
-{- 
-  File:           HetCATS/CSP-CASL/CCKeywords.hs
-  Authors:        Daniel Pratsch
-  last modified:  4.11.2002
-   
-  string constants for CSP-CASL w Channels keywords to be used for parsing 
-  all exported identifiers are mixed case (usually keyword plus capital S)
+{- **********************************************************************
+
+   $Source$
+
+   $Date$
+   $Revision$
+   Author: Daniel Pratsch (Last modification by $Author$)
+
+  ************************************************************************** 
 -}
 
 module CCKeywords where
@@ -17,105 +17,52 @@ import Prelude(String)
 -- csp-casl keywords
 -- ----------------------------------------------
 
-dataS      :: String = "data"
-channelS   :: String = "channel"
-processS   :: String = "process"
-endS       :: String = "end"
-semicolonS :: String = ";"
-commaS     :: String = ","
-colonS     :: String = ":"
-skipS      :: String = "skip"
-stopS      :: String = "stop"
-ifS        :: String = "if"
-thenS      :: String = "then"
-oRBracketS :: String = "("
-cRBracketS :: String = ")"
-oSBracketS :: String = "["
-cSBracketS :: String = "]" 
-extChoiceS :: String = "[]"
-intChoiceS :: String = "|~|"
+dataS, channelS, processS, endS, semicolonS, commaS,
+  colonS, skipS, stopS, ifS, thenS, elseS, whenS, varS,
+  oRBracketS, cRBracketS, oSBracketS, cSBracketS, 
+  multiPreS, extChoiceS, intChoiceS, oAlPaS, cAlPaS, 
+  oGenPaS, mGenPaS, cGenPaS, synParaS, interParaS,
+  hidingS, oRenamingS, cRenamingS, prefixS, sendS,
+  receiveS , chanRenS   :: String
 
+dataS      = "data"
+channelS   = "channel"
+processS   = "process"
+endS       = "end"
+semicolonS = ";"
+commaS     = ","
+colonS     = ":"
+skipS      = "skip"
+stopS      = "stop"
+ifS        = "if"
+thenS      = "then"
+elseS      = "else"
+whenS      = "when"
+varS       = "var"
+oRBracketS = "("
+cRBracketS = ")"
+oSBracketS = "["
+cSBracketS = "]"
+multiPreS  = "[]" 
+extChoiceS = "[]"
+intChoiceS = "|~|"
+oAlPaS     = "[|"
+cAlPaS     = "|]"
+oGenPaS    = "["
+mGenPaS    = "||"
+cGenPaS    = "]"
+synParaS   = "||"
+interParaS = "|||"
+hidingS    = "\\"
+oRenamingS = "[["
+cRenamingS = "]]"
+prefixS    = "->"
+sendS      = "!"
+receiveS   = "?"
+chanRenS   = "<-"
 
-
-
-
-
-
-
-
-
-
-{- CASL stuff
-exMark :: String = "!" -- in "exists!"
-quMark :: String = "?"
-funS :: String = "->"
-prodS :: String = "*"
-timesS :: String = "\215"
-lessS :: String = "<"
-colonS :: String = ":"
-
--- ----------------------------------------------
--- casl keywords
--- ----------------------------------------------
-
-dotS :: String = "."
-cDot :: String = "\183"
-barS :: String = "|"
-mapsTo :: String = "|->"
-defnS :: String = "::="
-
-equalS :: String = "="
-exEqual :: String = "=e="  -- unusual keyword 
-implS :: String = "=>"
-equivS :: String = "<=>"
-negS :: String = "\172"
-lAnd :: String = "/\\"   -- logical and/or
-lOr :: String = "\\/"
-
-defS :: String = "def"
-elseS :: String = "else"
-falseS :: String = "false"
-ifS :: String = "if"
-notS :: String = "not"
-trueS :: String = "true"
-whenS :: String = "when"
-
-andS :: String = "and"
-archS :: String = "arch"
-asS :: String = "as"
-assocS :: String = "assoc"
-axiomS :: String = "axiom"
-sS :: String = "s" 
-varS :: String = "var"
-predS :: String = "pred"
-opS :: String = "op"
-closedS :: String = "closed"
-commS :: String = "comm"
-endS :: String = "end"
-existsS :: String = "exists"
-forallS :: String = "forall"
-fitS :: String = "fit"
-freeS :: String = "free"
-fromS :: String = "from"
-generatedS :: String = "generated"
-getS :: String = "get"
-givenS :: String = "given" 
-hideS :: String = "hide"
-idemS :: String = "idem"
-inS :: String = "in"
-lambdaS :: String = "lambda"
-libraryS :: String = "library"
-localS :: String = "local"
-resultS :: String = "result"
-revealS :: String = "reveal" 
-sortS :: String = "sort"
-specS :: String = "spec"
-thenS :: String = "then"
-toS :: String = "to"
-typeS :: String = "type"
-unitS :: String = "unit"
-versionS :: String = "version"
-viewS :: String = "view"
-withS :: String = "with"
-withinS :: String = "within"
--}
+csp_casl_keywords :: [String]
+csp_casl_keywords = [dataS, channelS, processS, endS, semicolonS, commaS, colonS, skipS, stopS, ifS,
+                     thenS, elseS, whenS, varS, oRBracketS, cRBracketS, oSBracketS, cSBracketS, multiPreS, 
+                     extChoiceS, intChoiceS, oAlPaS, cAlPaS, oGenPaS, mGenPaS, cGenPaS, synParaS, 
+                     interParaS, hidingS, oRenamingS, cRenamingS, multiPreS, sendS, receiveS, prefixS]
