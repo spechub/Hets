e3d632aeac72e0a9c05f05bb6762535cdb5b3432
Daniel Pratsch <coldsky@tzi.de>
2003-02-17 13:27:02 +0000

    DP: Wrapping des Parsers im Main-Modul git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@885 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CSP-CASL/Main.hs
+++ CSP-CASL/Main.hs
@@ -1,3 +1,14 @@
+{- **********************************************************************
+
+   $Source$
+
+   $Date$
+   $Revision$
+   Author: Daniel Pratsch (Last modification by $Author$)
+
+  ************************************************************************** 
+-}
+
 {-------------------------------------------------------
  testing file for CSP-CASL semantics
 --------------------------------------------------------}
@@ -6,16 +17,20 @@ module Main where
 import Parse_hugo
 import Parsec
 import IO
+import System
+
+import AnnoState
+
+run :: Show a => AParser a -> String -> IO ()
+run p input
+        = case (runParser p emptyAnnos "" input) of
+            Left err -> do { putStr "parse error at "
+                           ; print err
+                           }
+            Right x -> print x                        
 
 main :: IO ()
-main = parseTest cspCaslCSpec 
-        "data 
-           sort s 
-           op c:s
-         channel 
-           n,m: s; 
-           k,l: s 
-         process 
-           P(c)
-        " 
-
+main = do { l <- getArgs
+          ; c <- readFile (head l)
+          ; run cspCaslCSpec c
+          }
