7a2a781652286f4898e9b868934e41ff5b1f855c
Till Mossakowski <mossakow@iws.cs.uni-magdeburg.de>
2003-02-18 15:08:03 +0000

    __* empty log message __* git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@900 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ DotGraph.hs
@@ -0,0 +1,43 @@
+{- HetCATS/DotGraph.hs
+   $Id$
+   Till Mossakowski
+
+   Display of development graphs using dot
+-}
+
+module DotGraph where
+
+import Graph
+import DevGraph
+import Print_HetCASL
+
+
+edgeAttribute LocalDef = " [style=bold]"
+edgeAttribute GlobalDef = " [style=bold]"
+edgeAttribute HidingDef = " [style=bold]"
+edgeAttribute (FreeDef _) = " [style=bold]"
+edgeAttribute (CofreeDef _) = " [style=bold]"
+edgeAttribute (LocalThm _)  = " [style=dotted]"
+edgeAttribute (GlobalThm _) = " [style=dotted]"
+edgeAttribute (HidingThm _ _) = " [style=dotted]"
+edgeAttribute (FreeThm _ _) = " [style=dotted]"
+
+showNode :: DGraph -> Node -> String
+showNode dg n = 
+  case get_dgn_name (lab' $ context n dg) of
+    Nothing -> "N"++show n
+    Just x -> show (printText0_eGA x)
+
+dotEdge dg (n1,n2,link) = 
+  showNode dg n1 ++ " -> " ++ showNode dg n2++ edgeAttribute (dgl_type link) ++ ";"
+    
+nodeAttribute (DGNode _ _ _ _) = ""
+nodeAttribute (DGRef _ _ _) =  " [shape=box]"
+
+dotNode dg (n,ncontents) =
+  showNode dg n ++ nodeAttribute ncontents ++ ";"
+
+dot :: DGraph -> [String]
+dot dg =
+  ["digraph G {","    size = \"8,6\"","  "] ++
+  map (dotNode dg) (labNodes dg) ++ map (dotEdge dg) (labEdges dg)++["}"]
