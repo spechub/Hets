25d38daad44104d4dab6cf16c692eec26c972966
Christian Maeder <Christian.Maeder@dfki.de>
2003-02-19 15:56:52 +0000

    __* empty log message __* git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@919 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ CSP-CASL/Print_AS_CSP_CASL.hs
@@ -0,0 +1,11 @@
+module Print_AS_CSP_CASL where
+
+import AS_CSP_CASL
+import PrettyPrint
+import Pretty
+import Print_AS_Basic
+
+instance PrettyPrint CSP_CASL_C_SPEC where
+    printText0 ga (Csp_casl_c_spec dd _cd _pd) =
+	ptext "data" $$
+	nest 2 (printText0 ga dd)
