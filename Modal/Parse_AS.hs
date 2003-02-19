59d823de481014f68b8b024474bffac150b56e1e
Wiebke Herding <wiebke@informatik.uni-bremen.de>
2003-02-19 11:32:05 +0000

    __* empty log message __* git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@910 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Modal/Parse_AS.hs
+++ Modal/Parse_AS.hs
@@ -1,4 +1,4 @@
-{- Spickzettel für's Parsen -}
+{- Spickzettel für's Parsen 
 
 -- Definition aus Logic.hs
 type ParseFun a = FilePath -> Int -> Int -> String -> (a,String,Int,Int)
@@ -11,11 +11,36 @@ toParseFun :: GenParser Char st a -> st -> ParseFun a
 -- Klassenfunktion, die meine Instanz implementiert
 parse_basic_spec :: id -> Maybe(ParseFun basic_spec)
 --                                       ^ Rückgabetyp
+-}
 
 module Parse_AS where
+import AnnoState
+import Id
+--import Keywords
+import Lexer
+import AS_Modal
+import AS_Annotation
+import Maybe
+import Parsec
+--import Formula
+--import SortItem
+--import OpItem
+--import TypeItem
+import ItemList
 
--- aus CASL
+-- aus CASL, kann bleiben
 basicSpec :: GenParser Char st BASIC_SPEC
 basicSpec = (fmap Basic_spec $ many1 $ -- 
-	    try $ bind addLeftAnno annos basicItems)
-            <|> try (oBraceT >> cBraceT >> return (Basic_spec []))
+	    try $ bind addLeftAnno annos basicItems)               --BasicItem
+            <|> try (oBraceT >> cBraceT >> return (Basic_spec [])) --Klammern
+
+basicItems :: AParser BASIC_ITEMS
+basicItems = fmap Sig_items sigItems
+	    -- <|> dotFormulae    -- später!
+
+sigItems :: AParser SIG_ITEMS
+sigItems = sortItems <|> opItems <|> predItems <|> typeItems
+            
+
+
+
