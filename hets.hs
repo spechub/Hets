f66fcd981f556c238df7dd6dfa42123745e3b1d2
Christian Maeder <Christian.Maeder@dfki.de>
2003-02-21 22:16:43 +0000

    new top-level target git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@964 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ hets.hs
@@ -0,0 +1,28 @@
+{- |
+   > HetCATS/hets.hs
+   $Id$
+   Author: Klaus Lüttich
+   Year:   2003
+
+   The Main module of the hetcats system. It provides the main function
+   to call.
+
+-}
+module Main where
+
+import Options
+import System
+
+import ReadFn
+import WriteFn
+-- import ProcessFn
+
+main :: IO ()
+main = do opt <- getArgs >>= hetcatsOpts
+	  if (verbose opt >= 3) then putStr "Options: " >> print opt
+	   else return ()
+	  ld <- read_LIB_DEFN opt
+	  -- (env,ld') <- analyse_LIB_DEFN opt
+	  write_LIB_DEFN opt ld
+	  -- write_GLOBAL_ENV env
+
