5f27bb9e79d7c717e2c08135f3d4a11b2cee7a7d
Christian Maeder <Christian.Maeder@dfki.de>
2003-02-21 16:24:48 +0000

    copies from haterm-1.0/src git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@938 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ Common/ATerm/ATermLib.hs
@@ -0,0 +1,14 @@
+{-# OPTIONS -fallow-overlapping-instances #-}
+module ATermLib (
+
+	module ATermAbstractSyntax,
+	module ATermReadWrite,
+	module ATermConversion,
+	module ATermLib 
+
+) where
+
+import ATermAbstractSyntax
+import ATermReadWrite
+import ATermConversion
+
