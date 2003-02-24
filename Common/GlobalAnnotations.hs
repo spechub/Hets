444189087da505374efd0b4fbb1fc73f673ef801
Christian Maeder <Christian.Maeder@dfki.de>
2003-02-24 10:21:08 +0000

    moved down git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@969 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ Common/GlobalAnnotations.hs
@@ -0,0 +1,59 @@
+
+{- HetCATS/GlobalAnnotations.hs
+   $Id$
+   Author: Klaus Lüttich
+   Year:   2002
+
+   Some datastructures for fast access of GlobalAnnotations
+
+   todo:
+   did: 12.7.02
+   removed PrettyPrint from Id to avoid cyclic imports
+
+-}
+
+module GlobalAnnotations where
+
+import Id
+
+import Common.Lib.Graph
+import FiniteMap
+
+data GlobalAnnos = GA { prec_annos     :: PrecedenceGraph
+		      , assoc_annos    :: AssocMap
+		      , display_annos  :: DisplayMap
+		      , literal_annos  :: LiteralAnnos
+		      , literal_map    :: LiteralMap
+		      } deriving (Show)
+
+type PrecedenceGraph = (FiniteMap Id Node,Graph Id Int)
+
+data PrecRel = Higher | Lower | ExplGroup Direct
+	       deriving (Show)
+
+data Direct = BothDirections | NoDirection
+	      deriving (Show)
+
+type AssocMap = FiniteMap Id AssocEither
+
+data AssocEither = ALeft | ARight deriving (Show,Eq)
+
+type DisplayMap = FiniteMap Id [(String,String)]
+
+type LiteralMap = FiniteMap Id LiteralType
+
+data LiteralType = StringCons | StringNull
+		 | ListBrackets | ListCons | ListNull
+		 | Number
+		 | Fraction | Floating
+		   deriving (Show,Eq)
+
+data LiteralAnnos = LA { string_lit :: Maybe (Id,Id)
+		       , list_lit   :: Maybe (Id,Id,Id)
+		       , number_lit :: Maybe Id
+		       , float_lit  :: Maybe (Id,Id)
+		       } deriving (Show)
+
+instance (Show a,Show b) => Show (FiniteMap a b) where
+    show = show . fmToList
+
