ca9198b0e57aedc7bcd94d68ef25f138b05f428c
Carsten Fischer <cfi@informatik.uni-bremen.de>
2003-02-24 14:05:10 +0000

    erweitert git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@975 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CASL/ShowMixfix.hs
+++ CASL/ShowMixfix.hs
@@ -9,6 +9,17 @@
 module CASL.ShowMixfix where
 
 import CASL.AS_Basic_CASL
+import CASL.Formula
+import Common.Lib.Parsec
+import Common.AnnoState
+import Common.Id
 
-showTERM :: TERM -> String
-showTERM t = "bla"
+showTerm :: TERM -> String
+
+showTerm (Simple_id s) = tokStr s
+showTerm (Application op_s args _) = showOps op_s ++ "(" ++ ")"
+showTerm t = "bla"
+
+showOps :: OP_SYMB -> String
+showOps(Op_name i) = showId i ""
+showOps(Qual_op_name i t _) = showId i "" ++ "..."
