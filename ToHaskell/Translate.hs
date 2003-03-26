d9a80dc6fd8d6b0c45fcd7aabca438bd6157552b
Katja Abu-dib <adk@tzi.de>
2003-03-26 12:54:48 +0000

    Continued with HsFunBind and HsPatBInd git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@1140 cec4b9c1-7d33-0410-9eda-942365e851bb

--- ToHaskell/Translate.hs
+++ ToHaskell/Translate.hs
@@ -5,18 +5,17 @@ import Haskell.Language.Syntax
 import Common.AS_Annotation
 import Common.Id
 import Common.Lib.Parsec.Pos
+import Char
 
 
 translate :: BasicSpec -> HsModule
 -- From where can I get the Module's name?
-translate (BasicSpec []) = 
-          HsModule (SrcLoc "NewModul.hs" 1 1) (Module "NewModul") Nothing [] []
+--translate x = error (show x)
 translate (BasicSpec annotedBasicItems) = 
-          HsModule (SrcLoc "NewModul.hs" 1 1) (Module "NewModul") Nothing [] 
+          HsModule (SrcLoc "" 1 1) (Module "HasCASLModul") Nothing [] 
                    (translateBasicItems annotedBasicItems)
 
-
-
+-------------------------------------------------------------
 translateBasicItems :: [Annoted BasicItem] -> [HsDecl]
 translateBasicItems [] = []
 translateBasicItems (bi:bis) = 
@@ -25,43 +24,121 @@ translateBasicItems (bi:bis) =
 translateBasicItem :: BasicItem -> [HsDecl]
 translateBasicItem i = 
   case i of
+    SigItems si -> translateSigItems si
     ProgItems eqList _posList -> translateProgEqs eqList
-    _ -> error "not yet implemented"
+    _ -> error ("translateBasicItem: " ++ show i)
 
-
-
-
+-------------------------------------------------------------
+translateSigItems :: SigItems -> [HsDecl]
+translateSigItems (TypeItems _inst _annotedTypeItems _poslist) = []
+translateSigItems _ = []
+  
+-------------------------------------------------------------
 translateProgEqs :: [Annoted ProgEq] -> [HsDecl]
-translateProgEqs [] = []
-translateProgEqs (eq:eqs) = 
-         ((translateProgEq (item eq)):(translateProgEqs eqs))
+translateProgEqs eqs = map translateProgEq eqs
 
-translateProgEq :: ProgEq -> HsDecl
-translateProgEq (ProgEq pat term pos) = 
+
+translateProgEq :: Annoted ProgEq -> HsDecl
+translateProgEq (Annoted (ProgEq pat term pos) _ _ _) = 
   case pat of
-    MixfixPattern p -> if length p == 1 
-                          then patBind pat term pos
-		          else funBind pat term pos
-    _ -> error "not yet implemented"
+    PatternToken _ptok -> case term of
+                           TermToken _ttok -> patBind pat term pos
+			   _ -> error ("translateProgEq " ++ show term)
+    MixfixPattern (p:_ps) -> case p of
+                              PatternToken ptok -> 
+                              -- erstes PatternToken klein deutet auf HsFunBind
+                                 if isLower $ head $ tokStr ptok then
+                                   funBind pat term pos
+		                 else patBind pat term pos
+			      _ -> error ("unexpected pattern in " ++
+                                          "translateProgEq: " ++ show pat)
+    _ -> error ("translateProgEq" ++ show pat)
 
-
-
-
+-------------------------------------------------------------
 patBind :: Pattern -> Term -> Pos -> HsDecl
-patBind (MixfixPattern [PatternToken tok]) term pos =
-  HsPatBind (SrcLoc {srcFilename = sourceName pos, srcLine = sourceLine pos, srcColumn = sourceColumn pos})
-      (HsPVar (HsSymbol (tokStr tok))) (translatePatBindTerm term) []
-       -- muss die HsPVar hier symbol oder id sein?
+-- x = y
+patBind (PatternToken ptok) (TermToken ttok) pos = 
+  HsPatBind (SrcLoc {srcFilename = sourceName pos, srcLine = sourceLine pos, 
+                     srcColumn = sourceColumn pos}) 
+            (HsPVar (HsIdent (tokStr ptok))) 
+            (HsUnGuardedRhs (HsVar (UnQual (HsIdent (tokStr ttok))))) 
+            []
+--
+--patBind (MixfixPattern (p:ps)) term pos = error "not yet implemented"
+patBind _ _ _ = error "not yet implemented"
 
 
-translatePatBindTerm :: Term -> HsRhs
-translatePatBindTerm term = 
-  case term of
-    TermToken tok -> HsUnGuardedRhs (HsVar (UnQual (HsSymbol (tokStr tok))))
-    -- ist die HsVar hier qualifiziert oder nicht?
-    _ -> error "not yet implemented"
-
-
-
+-------------------------------------------------------------
 funBind :: Pattern -> Term -> Pos -> HsDecl
-funBind pat term pos = HsFunBind []
+-- f a b ...  = y
+funBind (MixfixPattern (p:ps)) term pos = 
+  case p of 
+    PatternToken ptok -> 
+      HsFunBind [HsMatch (SrcLoc {srcFilename = "", srcLine = sourceLine pos,
+                                  srcColumn = sourceColumn pos})
+                         (HsIdent (tokStr ptok))
+	                 (hsPats ps)
+	                 (hsRhs term)
+                         []
+                ]
+    _ -> error ("unexpected pattern: " ++ show p)
+funBind _ _ _ = HsFunBind []
+
+-------------------------------------------------------------
+hsPats :: [Pattern] -> [HsPat]
+hsPats pats = map hsPat pats
+
+hsPat :: Pattern -> HsPat
+hsPat p = case p of 
+	    PatternToken ptok ->
+              if isLower $ head $ tokStr ptok then
+		HsPVar (HsIdent (tokStr ptok))
+	      else 
+	        if isUpper $ head $ tokStr ptok then
+                  HsPApp (UnQual (HsIdent (tokStr ptok))) []
+		else
+		  if and $ map isDigit (tokStr ptok) then
+		    HsPLit (HsInt ((read (tokStr ptok))::Integer))
+		  -- andere Typen wie z.B. Rational fehlen
+		  else error ("unexpected pattern: " ++ show p)
+	    _ -> error ("unexpected pattern: " ++ show p)
+--BracketPattern fehlen!!
+
+-------------------------------------------------------------
+hsRhs :: Term -> HsRhs
+hsRhs term = case term of
+    TermToken ttok -> if isLower $ head $ tokStr ttok then
+		        HsUnGuardedRhs (HsVar (UnQual 
+                          (HsIdent (tokStr ttok))))
+		      else 
+                        if isUpper $ head $ tokStr ttok then
+                          HsUnGuardedRhs (HsCon (UnQual 
+                            (HsIdent (tokStr ttok))))
+			else
+			  if and $ map isDigit (tokStr ttok) then
+			    HsUnGuardedRhs (HsLit (
+                              HsInt ((read (tokStr ttok))::Integer)))
+			  else error ("Unexpected term: " ++ show ttok)
+    MixfixTerm tlist -> HsUnGuardedRhs (hsExps tlist (length tlist))
+    _ -> error "hsRhs is not yet implemented"
+
+-------------------------------------------------------------
+hsExps :: [Term] -> Int -> HsExp
+hsExps (t:ts) l = case t of
+  TermToken ttok -> if l == 1 then
+		      if isLower $ head $ tokStr ttok then
+			HsVar (UnQual ((HsIdent (tokStr ttok))))
+		      else 
+		        if isUpper $ head $ tokStr ttok then
+			  HsCon (UnQual (HsIdent (tokStr ttok)))
+			else
+			  if and $ map isDigit (tokStr ttok) then
+			    HsLit (HsInt ((read (tokStr ttok))::Integer))
+			  -- andere Typen wie z.B. Rational fehlen
+			  -- Operationsanwendungen fehlen
+			  else error ("Unexpected term: " ++ show ttok)
+		    else HsApp (hsExps (t:[]) 1) (hsExps ts (l-1))
+  _ -> error "hsExps is not yet implemented"
+hsExps [] _ = error "Missing term"
+--BracketTerm fehlt!!
+		    
