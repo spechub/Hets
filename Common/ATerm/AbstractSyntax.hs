e9c78ff9c17a466f082917c87a2b97b3fee8df67
Felix Reckers <felix@informatik.uni-bremen.de>
2003-03-27 12:02:09 +0000

    __* empty log message __* git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@1145 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Common/ATerm/AbstractSyntax.hs
+++ Common/ATerm/AbstractSyntax.hs
@@ -1,108 +1,108 @@
+{- todo:
+    * Datentyp auf Annotationen umstellen
+      dazu einiges auskommentieren ..done
+      Weiter mit 'writeATerm'  ..done
+  
+    * Zugriffsfunktionen für ATermTable auf Annotationen anpassen ..done
+
+    * Neuen Datentyp für shared ATerms: ShATerm mit Annotationen
+       (ungefähr so: data ShATerm = ShAAppl String [Int]) ..done
+
+    * Umkehrfuntion zu getATermFull programmieren, Signatur und Name 
+      siehe toATermTable
+
+-}
+
 module Common.ATerm.AbstractSyntax where
 
 import Common.Lib.Map hiding (map)
 
-data ATerm = AAppl String [ATerm]
-           | AList [ATerm]
-           | AInt Integer
-	   | AIndex Int    -- Used for maximal sharing
-             deriving (Read,Show,Eq,Ord)
+data ATerm = AAppl String [ATerm] [ATerm]
+           | AList [ATerm]        [ATerm]
+           | AInt  Integer        [ATerm]
+	     deriving (Read,Show,Eq,Ord)
 
+data ShATerm = ShAAppl String [Int] [Int]
+	     | ShAList [Int]        [Int]
+	     | ShAInt  Integer      [Int]  
+	       deriving (Read,Show,Eq,Ord)
 
-type ATermTable = (Map ATerm Int, Map Int ATerm, Int)
+type ATermTable = (Map ShATerm Int,Map Int ShATerm, Int)
 
 emptyATermTable :: ATermTable
 emptyATermTable =  (empty,empty,0)
 
-addATerm :: ATerm -> ATermTable -> (ATermTable,ATerm)
-addATerm t tbl@(a_iFM, i_aFM, i)= 
-    if hasATerm t tbl then 
-       (tbl, AIndex (getATermIndex t tbl))
+addATerm :: ShATerm -> ATermTable -> (ATermTable,Int)
+addATerm t tbl = 
+{-   if hasATerm t tbl then 
+       (tbl,getATermIndex t tbl)
     else
-       addATerm'
-    where addATerm' = 
+       addATerm' t tbl
+-}
+  case i of
+    -1 -> addATerm' t tbl
+    _ -> (tbl,i)
+    where
+    i = getATermIndex t tbl 
+    addATerm' t (a_iFM,i_aFM,i) = 
 	      if member i i_aFM then
 		 error err_destruct_up
 	      else
-	         ((insert t i a_iFM, insert i t i_aFM, i+1), AIndex i)
+	         ((insert t i a_iFM,insert i t i_aFM,i+1),i)
 
-addATerm1 :: ATerm -> ATermTable -> ATermTable
-addATerm1 t tbl = let (tbl',_) = addATerm t tbl in tbl'
 
-addATermSp :: ATerm -> ATermTable -> (ATermTable,ATerm)
-addATermSp t tbl = 
-    case t of 
-    (AAppl _ [])   -> addATerm t tbl
-    (AAppl s [t']) -> let (tbl',t'') = case t' of 
-				       (AIndex _) -> (tbl,t')
-				       _  -> addATermSp t' tbl
-		      in addATerm (AAppl s [t'']) tbl'
-    (AAppl _ _)   -> addATerm t tbl
-    (AIndex _)     -> error err_index_store
-    _              -> addATerm t tbl
+addATerm1 :: ShATerm -> ATermTable -> ATermTable
+addATerm1 t tbl = fst $ addATerm t tbl 
 
-addATermSp1 :: ATerm -> ATermTable -> ATermTable
-addATermSp1 t tbl = let (tbl',_) = addATermSp t tbl in tbl'
 
-hasATerm :: ATerm -> ATermTable -> Bool
-hasATerm t (a_iFM,_,_) = member t a_iFM 
+{-
+hasATerm :: ShATerm -> ATermTable -> Bool
+hasATerm t (a_iFM,_,_) = elemFM t a_iFM 
+-}
 
-getATerm :: ATermTable -> ATerm
-getATerm (_,i_aFM,i) = findWithDefault (AIndex (-1)) (i-1) i_aFM
+getATerm :: ATermTable -> ShATerm
+getATerm (_,i_aFM,i) = findWithDefault (ShAInt (-1) []) (i-1) i_aFM  
+
+
 
 getATermFull :: ATermTable -> ATerm
 getATermFull at = 
     let t = getATerm at
     in case t of
-       (AInt _)    -> t
-       (AIndex _)  -> error err_wrong_store
-       (AList l)   -> AList (map conv l)
-       (AAppl c l) -> AAppl c (map conv l)
-    where conv t = getATermFull (getATermByIndexSp1 t at)
+       (ShAInt i as)    -> AInt i (map conv as)
+       (ShAList l as)   -> AList (map conv l) (map conv as)
+       (ShAAppl c l as) -> AAppl c (map conv l) (map conv as)
+    where conv t = getATermFull (getATermByIndex1 t at) 
 
-getATermSp :: ATermTable -> ATerm -> Maybe ATerm
-getATermSp at (AAppl s [])   = case getATerm at of 
-			       aterm@(AAppl s' _) ->
-			              if s == s' then Just aterm
-				      else Nothing
-			       _ -> error $ "*** getATermSp: " ++
-				            (show $ getATermFull at)
-getATermSp at (AAppl s [t']) = let (AAppl s' ts) = getATerm at
-				   mat = case ts of
-					 [t]  -> Just (getATermByIndexSp1 t at)
-					 _    -> Nothing
-			       in if s == s' then 
-				    case mat of 
-					   (Just at') -> 
-					       case getATermSp at' t' of 
-							(Just t'') -> 
-							 Just (AAppl s' [t''])
-							Nothing   -> Nothing
-					   Nothing    -> Nothing
-				  else
-				    Nothing
-getATermSp _at _              = error err_wrong_sp_call
+toATermTable :: ATerm -> ATermTable
+toATermTable at = fst $ addToTable at emptyATermTable
+                  where
+                  addToTable :: ATerm -> ATermTable -> (ATermTable,Int) 
+                  addToTable (AAppl s ats anns) att = let (att1,ats')  = addToTableList ats att
+							  (att2,anns') = addToTableList anns att1
+						      in addATerm (ShAAppl s ats' anns') att2
+                  addToTable (AList ats anns)   att = let (att1,ats')  = addToTableList ats att
+							  (att2,anns') = addToTableList anns att1
+                                                      in addATerm (ShAList ats' anns') att2
+                  addToTable (AInt i anns)      att = let (att1,anns') = addToTableList anns att
+                                                      in addATerm (ShAInt i anns') att1
+                  addToTableList :: [ATerm] -> ATermTable -> (ATermTable,[Int])
+		  addToTableList []       att = (att,[])
+                  addToTableList (at:ats) att = let (att1,i)  = addToTable at att
+                                                    (att2,is) = addToTableList ats att1
+                                                in (att2,i:is)
 
-getATermIndex :: ATerm -> ATermTable -> Int
-getATermIndex t (a_iFM,_,_) = findWithDefault (-1) t a_iFM
+getATermIndex :: ShATerm -> ATermTable -> Int
+getATermIndex t (a_iFM,_,_) = findWithDefault (-1) t a_iFM 
 
-getATermByIndex :: Int -> ATermTable -> (ATermTable,ATerm)
+getATermByIndex :: Int -> ATermTable -> (ATermTable,ShATerm)
 getATermByIndex i (a_iFM,i_aFM,_) = 
-    ((a_iFM, i_aFM,i+1), findWithDefault (AIndex (-1)) i i_aFM)
+    ((a_iFM,i_aFM,i+1),findWithDefault (error "getATermByIndex: No entry for ATerm in ATermTable") i i_aFM) 
 
 getATermByIndex1 :: Int -> ATermTable -> ATermTable
-getATermByIndex1 i at = let (at',_) = getATermByIndex i at in at'
+getATermByIndex1 i at = fst $ getATermByIndex i at
 
-getATermByIndexSp :: ATerm -> ATermTable -> (ATermTable,ATerm)
-getATermByIndexSp t tbl = getATermByIndex (atermIndexToInt t) tbl
 
-getATermByIndexSp1 :: ATerm -> ATermTable -> ATermTable
-getATermByIndexSp1 t tbl = getATermByIndex1 (atermIndexToInt t) tbl
-
-atermIndexToInt            :: ATerm -> Int
-atermIndexToInt (AIndex i) = i
-atermIndexToInt _          = error (err_wrong_store++
-				    "\n*** atermIndexToInt: cannot convert non index")
 --- some error messages --------
 err_ref_index   = "*** ATermTable: reference  points to reference"
 
@@ -117,6 +117,41 @@ err_wrong_sp_call =
 err_const_no_match = "*** getATermSp: constructors don't match:"
 
 err_index_store = "*** addATermSp: attempt to add an AIndex"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
