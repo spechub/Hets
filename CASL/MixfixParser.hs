0cc809b79c4276013d0325bedbc33c4244d32705
Christian Maeder <Christian.Maeder@dfki.de>
2003-04-30 13:48:08 +0000

    made isListId test total git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@1299 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CASL/MixfixParser.hs
+++ CASL/MixfixParser.hs
@@ -80,7 +80,7 @@ listId :: Id -> Id
 listId i = Id [listToken] [i] []
 
 isListId :: Id -> Bool
-isListId (Id ts cs _) = head ts == listToken && length cs == 1
+isListId (Id ts cs _) = not (null ts) && head ts == listToken && length cs == 1
 
 listStates :: GlobalAnnos -> Int -> [State]
 listStates g i = 
