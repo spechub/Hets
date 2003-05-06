5214cf3742dc626a7efc5ec851db09bf0ff1f579
Christian Maeder <Christian.Maeder@dfki.de>
2003-05-06 20:08:05 +0000

    recollected tests git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@1351 cec4b9c1-7d33-0410-9eda-942365e851bb

--- HasCASL/test/runcheck.sh
+++ HasCASL/test/runcheck.sh
@@ -6,7 +6,20 @@ ANNOS=../../Common/test/Standard.annos
 
 . ../../Common/test/checkFunctions.sh
 
-for i in *.hascasl
+runchecker MixIds ../../Common/test/MixIds.casl MixIds.casl.output
+runchecker MixIds ../../Common/test/WrongMixIds.casl WrongMixIds.casl.output
+runchecker BasicSpec ../../CASL/test/BasicSpec.casl BasicSpec.casl.output
+
+for i in Kinds Types Terms Items
+do
+    runmycheck $i hascasl
+    runwrongcheck $i hascasl
+done
+
+runchecker BasicSpec BasicSpec.hascasl BasicSpec.hascasl.parser.output
+runwrongcheck BasicSpec hascasl
+
+for i in [A-HL-SX-Z]*.hascasl
 do
   echo "processing $i"
   runchecker "analysis" $i $i.output
