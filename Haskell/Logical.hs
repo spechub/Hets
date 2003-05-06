688136b8b8f1bdd5780a8dbe8b1769731d32de48
Till Mossakowski <mossakow@iws.cs.uni-magdeburg.de>
2003-05-06 14:53:33 +0000

    Prelude for XHaskell git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@1335 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ Haskell/Logical.hs
@@ -0,0 +1,77 @@
+module Locigal where
+
+not_implemented :: a
+not_implemented = error "not implemented"
+
+data Logical = T | F deriving Show
+
+infix 2 \/
+infix 3 /\
+infix 1 ==>
+infix 1 <=> 
+(\/), (/\), (==>), (<=>) :: Logical -> Logical -> Logical
+neg :: Logical -> Logical
+-- unfortunately, already not :: Bool -> Bool
+(/\) = not_implemented
+(\/) = not_implemented
+(==>) = not_implemented
+(<=>) = not_implemented
+neg = not_implemented
+
+-- for any f which is declared but not defined,
+-- f = not_implemented should be addded automatically
+-- This allows for loose specifications
+
+
+allof, ex :: (a->Logical) -> Logical
+allof = not_implemented
+ex = not_implemented
+
+infix 4 ===
+(===) :: a->a->Logical
+(===) = not_implemented
+
+{-
+
+{-# AXIOMS
+         "map_law"       forall f g xs. map f (map g xs) = map (f.g) xs
+         "deMorgan"      forall p q. neg p /\ neg q <=> neg (p\/q)
+ #-}
+
+-- This is syntactical sugar for 
+map_law = allof(\f-> allof(\g -> map (g.f) === map g . map f))
+deMorgan = allof(\p -> allof(\q -> neg p /\ neg q <=> neg (p\/q)))
+
+
+Syntax of AXIOM pragmas:
+
+topdecl : ...
+    | '{-# AXIOMS' axioms '#-}'
+
+axiom : STRING formula
+
+formula : quantification formula
+        | formula '/\' formula
+        | formula '\/' formula
+        | formula '=>' formula
+        | formula '<=>' formula
+        | 'not' formula
+        | infixexp '=' srcloc exp  -- srcloc just gets source location
+
+quantification : 'forall' rule_var_list '.'   
+        | 'exists' rule_var_list '.'
+        | 'exists!' rule_var_list '.'
+
+
+-- from Haskell parser:
+
+rule_var_list 
+        : rule_var
+        | rule_var rule_var_list
+
+rule_var 
+	: varid                              	
+       	| '(' varid '::' ctype ')' 
+
+
+-}
