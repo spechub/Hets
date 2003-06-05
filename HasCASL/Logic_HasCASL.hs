c1b444094def2286653f8a420f97c75b080593e5
Christian Maeder <Christian.Maeder@dfki.de>
2003-06-05 18:49:40 +0000

    passed global annotations to anaBasicSpec git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@1475 cec4b9c1-7d33-0410-9eda-942365e851bb

--- HasCASL/Logic_HasCASL.hs
+++ HasCASL/Logic_HasCASL.hs
@@ -82,8 +82,8 @@ instance StaticAnalysis HasCASL BasicSpec Sentence ()
                Sign 
                M.Morphism 
                Symbol RawSymbol where
-    basic_analysis HasCASL = Just ( \ (b, e, _) ->
-		let ne = snd $ (runState (anaBasicSpec b)) e 
+    basic_analysis HasCASL = Just ( \ (b, e, ga) ->
+		let ne = snd $ (runState (anaBasicSpec ga b)) e 
 		    in return (b, ne, initialEnv, [])) 
   
     signature_union HasCASL = merge
