45ebd7609f0dcf87ccd8ebd47dcf597c64bf63a6
Christian Maeder <Christian.Maeder@dfki.de>
2003-06-12 13:16:49 +0000

    new output for corrected mixfix analysis git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@1494 cec4b9c1-7d33-0410-9eda-942365e851bb

--- ToHaskell/test/OpDecls.hascasl.hs
+++ ToHaskell/test/OpDecls.hascasl.hs
@@ -5,20 +5,11 @@ data A_s = A_s
  
 data A_t = A_t
  
-_2_P_23 :: (A_s, A_s) -> A_s
-_2_P_23 = undefined
- 
-_2_P_22 :: A_t -> A_t -> A_t
-_2_P_22 = undefined
- 
-_2_P_2 :: (A_t, A_t) -> A_t
+_2_P_2 :: (A_s, A_s) -> A_t
 _2_P_2 = undefined
  
-a :: A_s
-a = undefined
+x :: A_s
+x = undefined
  
-b :: A_s
-b = undefined
- 
-y :: A_s
-y = _2_P_23 (x :: s, x :: s)
+y :: A_t
+y = _2_P_2 (x, x)
