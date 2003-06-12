21a5dc6f0c7be507b8091d65cf77392208e5807f
Katja Abu-dib <adk@tzi.de>
2003-06-12 11:57:32 +0000

    __* empty log message __* git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@1491 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ ToHaskell/test/OpDecls.hascasl.hs
@@ -0,0 +1,24 @@
+module HasCASLModul where
+import Prelude (undefined)
+ 
+data A_s = A_s
+ 
+data A_t = A_t
+ 
+_2_P_23 :: (A_s, A_s) -> A_s
+_2_P_23 = undefined
+ 
+_2_P_22 :: A_t -> A_t -> A_t
+_2_P_22 = undefined
+ 
+_2_P_2 :: (A_t, A_t) -> A_t
+_2_P_2 = undefined
+ 
+a :: A_s
+a = undefined
+ 
+b :: A_s
+b = undefined
+ 
+y :: A_s
+y = _2_P_23 (x :: s, x :: s)
