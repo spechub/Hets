2696a56c9b517a0d98db4c61ac69c27d467a9772
Klaus Luettich <luettich@informatik.uni-bremen.de>
2003-07-18 12:46:59 +0000

    improved the printing git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@1559 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Syntax/Print_AS_Library.hs
+++ Syntax/Print_AS_Library.hs
@@ -20,10 +20,12 @@ module Syntax.Print_AS_Library where
 import Common.Lib.Pretty
 import Common.PrettyPrint
 import Common.PPUtils
+import Common.GlobalAnnotations
 
 import Common.Id
 import qualified Syntax.AS_Structured as AS_Struct 
 import Syntax.AS_Library
+import Common.AS_Annotation
 
 import Common.Print_AS_Annotation
 import Syntax.Print_AS_Architecture
@@ -34,20 +36,22 @@ instance PrettyPrint LIB_DEFN where
 	let aa' = printText0 ga aa              -- lib name
 	    ab' = vcat $ map (printText0 ga) ab -- LIB_ITEMs
 	    ad' = vcat $ map (printText0 ga) ad -- global ANNOTATIONs
-	in ptext "library" <+> aa' $$ ad' $$ ptext "\n" $$ ab'
+	in ptext "library" <+> aa' <> ptext "\n" $$ ad' $$ ptext "\n" $$ ab'
 
     printLatex0 ga (Lib_defn aa ab _ ad) =
-	let aa' = printLatex0 ga aa              -- lib name
-	    ab' = vcat $ map (printLatex0 ga) ab -- LIB_ITEMs
-	    ad' = vcat $ map (printLatex0 ga) ad -- global ANNOTATIONs
-	in hc_sty_plain_keyword "library" <\+> aa' $$ ad' 
+	let aa' = printLatex0 ga' aa              -- lib name
+	    ab' = vcat $ map (printLatex0 ga') ab -- LIB_ITEMs
+	    ad' = vcat $ map (printLatex0 ga') ad -- global ANNOTATIONs
+	    ga' = set_latex_print True ga
+	in hc_sty_plain_keyword "library" <\+> aa' 
+	       <> latex_macro "\n" $$ ad' 
 	       $$ latex_macro "\n" $$ ab'
 
 instance PrettyPrint LIB_ITEM where
     printText0 ga (Spec_defn aa ab ac _) =
 	let aa' = printText0 ga aa
 	    ab' = printText0 ga ab
-	    spec_head = (hang (ptext "spec" <+> aa' <+> ab') 2 equals)
+	    spec_head = ptext "spec" <+> aa' <+> ab' <+> equals
 	    ac' = spAnnotedPrint printText0 (<+>) ga (spec_head) ac
          -- nesting is done by the instance for SPEC now
 	in ac' $$ ptext "end\n"
@@ -58,8 +62,10 @@ instance PrettyPrint LIB_ITEM where
 	    -- ac' = printText0 ga ac
 	    (frm,to) = case ac of 
 		       AS_Struct.View_type vaa vab _ -> 
-			   (condBracesGroupSpec printText0 sp_braces ga vaa, 
-			    condBracesGroupSpec printText0 sp_braces ga vab)
+			   (condBracesGroupSpec printText0 braces 
+			        Nothing ga vaa, 
+			    condBracesGroupSpec printText0 braces 
+			        Nothing ga vab)
 	    ad' = sep $ punctuate comma $ map (printText0 ga) ad
 	    eq' = if null ad then empty else equals
 	in (hang (hang (hang (hang (ptext "view" <+> aa' <+> ab') 
@@ -93,12 +99,14 @@ data VIEW_DEFN = View_defn VIEW_NAME GENERICITY VIEW_TYPE
 	let aa' = printText0 ga aa
 	in ptext "logic" <+> aa' 
 
+
+
     printLatex0 ga (Spec_defn aa ab ac _) =
-	let aa' = simple_id_latex aa
-	    ab' = setTab_latex <> (tabbed_nest_latex $ printLatex0 ga ab)
-	    spec_head = (hang_latex (hc_sty_hetcasl_keyword "spec" 
-				         <\+> setTab_latex <> aa' <\+> ab') 
-			     2 equals_latex)
+	let aa' = simple_id_indexed_latex aa
+	    ab' = printLatex0 ga ab -- only PARAMS are nested/hang after 
+				    -- the spec name
+	    spec_head = hc_sty_hetcasl_keyword "spec" 
+			<\+> setTab_latex <> aa' <\+> ab' <\+> equals_latex
 	    ac' = spAnnotedPrint printLatex0 (<\+>) ga (spec_head) ac
 	in ac' $$ latexEnd 
     printLatex0 ga (View_defn aa ab ac ad _) =
@@ -106,31 +114,27 @@ data VIEW_DEFN = View_defn VIEW_NAME GENERICITY VIEW_TYPE
 	    ab' = case printLatex0 ga ab of
 		  ab_d 
 		      | isEmpty ab_d -> empty
-		      | otherwise    -> setTab_latex <> tabbed_nest_latex ab_d
-	    condGrpSp = condBracesGroupSpec printLatex0 sp_braces_latex ga
+		      | otherwise    -> ab_d
+	    condGrpSp = condBracesGroupSpec4View_defn
+			     printLatex0 
+			     sp_braces_latex2 ga
 	    (frm,to) = case ac of 
 		       AS_Struct.View_type vaa vab _ -> 
-			    (condGrpSp vaa, condGrpSp vab)
-	    ad' = cat $ punctuate (comma_latex<>space_latex) $ 
+			    (condGrpSp vaa, 
+			     condGrpSp vab)
+	    ad' = fcat $ punctuate (comma_latex<>space_latex) $ 
 	                 map (printLatex0 ga) ad
 	    eq' = if null ad then empty else equals_latex
 	    vhead = hc_sty_hetcasl_keyword "view" <\+> 
 		   setTab_latex <> aa' <\+> ab' 
-	    head_with_type = tab_hang_latex 
-			            (tab_hang_latex vhead 8 
-				              (colon_latex <~> setTab_latex 
-					       <> frm 
-					       <\+> hc_sty_plain_keyword "to"))
-		                    8
-		                    to
+	    to' = if isEmpty eq' then to else to <\+> eq'
+	    head_with_type = 
+		fsep [vhead <\+> colon_latex, 
+		      fsep [frm <\+> hc_sty_plain_keyword "to",
+		      to']]	                  
 	    in (if isEmpty eq' 
 		then head_with_type
-		else (tab_hang_latex
-		        (tab_hang_latex
-		           head_with_type
-		           8 eq')
-		        8
-		        ad')
+		else head_with_type $$ tabbed_nest_latex ad'
 	       ) 
 		   $$ latexEnd
 
@@ -179,6 +183,26 @@ data VIEW_DEFN = View_defn VIEW_NAME GENERICITY VIEW_TYPE
     printLatex0 ga (Syntax.AS_Library.Logic_decl aa _) =
 	let aa' = printLatex0 ga aa
 	in hc_sty_plain_keyword "logic" <\+> aa' 
+
+condBracesGroupSpec4View_defn :: 
+    (GlobalAnnos -> (Annoted AS_Struct.SPEC) -> Doc)
+	-> (Doc -> Doc) -- ^ a function enclosing the Doc
+                        -- in braces
+	-> GlobalAnnos -> (Annoted AS_Struct.SPEC) -> Doc
+condBracesGroupSpec4View_defn pf b_fun ga as =
+    case skip_Group $ item as of
+		 AS_Struct.Spec_inst _ _ _ -> as'
+		 AS_Struct.Union _ _       -> nested'
+		 AS_Struct.Extension _ _   -> nested'
+		 _                         -> tabbed_nest_latex 
+					         $ b_fun 
+						   (set_tabbed_nest_latex as')
+    where nested' =  tabbed_nest_latex
+		      (format_line <>
+		           b_fun (set_tabbed_nest_latex 
+				     (latex_macro "\\>"<>as')))
+          as' = pf ga as
+	  format_line = latex_macro "\\{\\=\\KW{view}~\\=\\kill\n"	
 
 instance PrettyPrint ITEM_NAME_OR_MAP where
     printText0 ga (Item_name aa) =
