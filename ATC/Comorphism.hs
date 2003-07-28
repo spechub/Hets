c90c6602dc1f2c1b69696538480d0454b25eb85e
Felix Reckers <felix@informatik.uni-bremen.de>
2003-07-28 17:03:46 +0000

    New ATermConvertible instances for the ATerm-Interface. They can't be generated automatically. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@1604 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ ATC/Comorphism.hs
@@ -0,0 +1,71 @@
+module ATC.Comorphism where
+
+import Logic.Comorphism
+import Logic.Logic
+import Logic.LogicGraph
+import Common.ATerm.Lib
+import Common.Lib.Set
+import Data.Maybe
+import Data.Dynamic
+--import ATC.Grothendieck
+
+data IdMonad a = IdMonad a
+
+instance Monad IdMonad where
+   return = IdMonad
+   IdMonad x >>= f = f x
+
+unpackIdMonad (IdMonad x) = x
+
+{-getLogicId :: (lid -> a) -> AnyLogic -> a
+getLogicId constr_fun l =
+   unpackIdMonad (
+      do Logic lid <- IdMonad l
+         return (constr_fun lid)
+    )-}
+
+
+instance (Comorphism cid1
+            lid1 sublogics1 basic_spec1 sentence1 symb_items1 symb_map_items1
+                sign1 morphism1 symbol1 raw_symbol1 proof_tree1
+            lid2 sublogics2 basic_spec2 sentence2 symb_items2 symb_map_items2
+                sign2 morphism2 symbol2 raw_symbol2 proof_tree2, 
+          Comorphism cid2
+            lid2 sublogics2 basic_spec2 sentence2 symb_items2 symb_map_items2
+                sign2 morphism2 symbol2 raw_symbol2 proof_tree2
+            lid3 sublogics3 basic_spec3 sentence3 symb_items3 symb_map_items3
+                sign3 morphism3 symbol3 raw_symbol3 proof_tree3) =>
+          ATermConvertible (CompComorphism cid1
+            lid1 sublogics1 basic_spec1 sentence1 symb_items1 symb_map_items1
+                sign1 morphism1 symbol1 raw_symbol1 proof_tree1 
+            cid2
+            lid2 sublogics2 basic_spec2 sentence2 symb_items2 symb_map_items2
+                sign2 morphism2 symbol2 raw_symbol2 proof_tree2
+            lid3 sublogics3 basic_spec3 sentence3 symb_items3 symb_map_items3
+                sign3 morphism3 symbol3 raw_symbol3 proof_tree3) where
+     toShATerm att0 (CompComorphism cid1 cid2) = 
+	 let (att1,i1) = toShATerm att0 (language_name cid1)
+             (att2,i2) = toShATerm att1 (language_name cid2)
+         in addATerm (ShAAppl "CompComorphism" [i1,i2] []) att2
+     fromShATerm att =  error "bla" {-
+         case aterm of
+	    (ShAAppl "CompComorphism" [i1,i2] _) ->
+		let i1'  = fromShATerm (getATermByIndex1 i1 att)
+                    cid1 = getLogicId $ lookupLogic_in_LG ("couldn't find Logic " ++ i1' ++ " in Logic Graph") i1'
+                    i2'  = fromShATerm (getATermByIndex1 i2 att)
+		    cid2 = getLogicId $ lookupLogic_in_LG ("couldn't find Logic " ++ i2' ++ " in Logic Graph") i2'
+                in (CompComorphism cid1 cid2)
+         where
+         aterm = getATerm att -}
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ ATC/Grothendieck.hs
@@ -0,0 +1,254 @@
+{-
+   File: ATC/Grothendieck.hs
+   ShATermConvertible instances for the datatypes in Logic/Grothendieck.hs
+   Author: Felix Reckers
+   Year: 2003
+-}
+
+module ATC.Grothendieck where
+
+import Logic.Grothendieck
+import Common.ATerm.Lib
+import Logic.LogicGraph
+import Logic.Logic
+import ATC.Comorphism
+import ATC.Named
+import Logic.Logic
+import Logic.Comorphism
+import ATC.Graph
+
+instance ATermConvertible G_basic_spec where
+     toShATerm att0 (G_basic_spec lid basic_spec) = 
+	 let (att1,i1) = toShATerm att0 (language_name lid)
+             (att2,i2) = toShATerm att1 basic_spec
+         in addATerm (ShAAppl "G_basic_spec" [i1,i2] []) att2
+     fromShATerm att = 
+         case aterm of
+	    (ShAAppl "G_basic_spec" [i1,i2] _) ->
+		let i1' = fromShATerm (getATermByIndex1 i1 att)
+                    att' = (getATermByIndex1 i2 att)
+		    l = lookupLogic_in_LG ("couldn't find Logic " ++ i1' ++ " in Logic Graph") i1' 
+                in case l of
+		    Logic lid -> 
+			G_basic_spec lid (fromShATerm_basic_spec lid att') 
+         where
+         aterm = getATerm att
+
+instance ATermConvertible G_sentence where
+     toShATerm att0 (G_sentence lid sentence) = 
+	 let (att1,i1) = toShATerm att0 (language_name lid)
+             (att2,i2) = toShATerm att1 sentence
+         in addATerm (ShAAppl "G_sentence" [i1,i2] []) att2
+     fromShATerm att = 
+         case aterm of
+	    (ShAAppl "G_sentence" [i1,i2] _) ->
+		let i1' = fromShATerm (getATermByIndex1 i1 att)
+                    att' = getATermByIndex1 i2 att
+		    l = lookupLogic_in_LG ("couldn't find Logic " ++ i1' ++ " in Logic Graph") i1' 
+                in case l of
+                    Logic lid -> (G_sentence lid (fromShATerm_sentence lid att'))
+         where
+         aterm = getATerm att
+
+instance ATermConvertible G_l_sentence_list where
+     toShATerm att0 (G_l_sentence lid n_sentence) = 
+	 let (att1,i1) = toShATerm att0 (language_name lid)
+             (att2,i2) = toShATerm att1 n_sentence
+         in addATerm (ShAAppl "G_l_sentence" [i1,i2] []) att2
+     fromShATerm att = 
+         case aterm of
+	    (ShAAppl "G_l_sentence" [i1,i2] _) ->
+		let i1' = fromShATerm (getATermByIndex1 i1 att)
+                    att' = getATermByIndex1 i2 att
+		    l = lookupLogic_in_LG ("couldn't find Logic " ++ i1' ++ " in Logic Graph") i1' 
+                in case l of
+                    Logic lid -> (G_l_sentence lid (fromShATerm_l_sentence_list lid att'))
+         where
+         aterm = getATerm att
+
+instance ATermConvertible G_sign where
+     toShATerm att0 (G_sign lid sign) = 
+	 let (att1,i1) = toShATerm att0 (language_name lid)
+             (att2,i2) = toShATerm att1 sign
+         in addATerm (ShAAppl "G_sign" [i1,i2] []) att2
+     fromShATerm att = 
+         case aterm of
+	    (ShAAppl "G_sign" [i1,i2] _) ->
+		let i1' = fromShATerm (getATermByIndex1 i1 att)
+                    att' = getATermByIndex1 i2 att
+                    l = lookupLogic_in_LG ("couldn't find Logic " ++ i1' ++ " in Logic Graph") i1' 
+                in case l of
+                    Logic lid -> (G_sign lid (fromShATerm_sign lid att'))
+         where
+         aterm = getATerm att
+
+instance ATermConvertible G_sign_list where
+     toShATerm att0 (G_sign_list lid signs) = 
+	 let (att1,i1) = toShATerm att0 (language_name lid)
+             (att2,i2) = toShATerm att1 signs
+         in addATerm (ShAAppl "G_sign_list" [i1,i2] []) att2
+     fromShATerm att =
+         case aterm of
+	    (ShAAppl "G_sign_list" [i1,i2] _) ->
+		let i1' = fromShATerm (getATermByIndex1 i1 att)
+                    att' = getATermByIndex1 i2 att
+                    l = lookupLogic_in_LG ("couldn't find Logic " ++ i1' ++ " in Logic Graph") i1' 
+                in case l of
+                    Logic lid -> (G_sign_list lid (fromShATerm_sign_list lid att'))
+         where
+         aterm = getATerm att
+
+instance ATermConvertible G_symbol where
+     toShATerm att0 (G_symbol lid symbol) = 
+	 let (att1,i1) = toShATerm att0 (language_name lid)
+             (att2,i2) = toShATerm att1 symbol
+         in addATerm (ShAAppl "G_symbol" [i1,i2] []) att2
+     fromShATerm att = 
+         case aterm of
+	    (ShAAppl "G_symbol" [i1,i2] _) ->
+		let i1' = fromShATerm (getATermByIndex1 i1 att)
+                    att' = getATermByIndex1 i2 att
+                    l = lookupLogic_in_LG ("couldn't find Logic " ++ i1' ++ " in Logic Graph") i1' 
+                in case l of 
+                    Logic lid -> (G_symbol lid (fromShATerm_symbol lid att'))
+         where
+         aterm = getATerm att 
+
+instance ATermConvertible G_symb_items_list where
+     toShATerm att0 (G_symb_items_list lid symb_items) = 
+	 let (att1,i1) = toShATerm att0 (language_name lid)
+             (att2,i2) = toShATerm att1 symb_items
+         in addATerm (ShAAppl "G_symb_items_list" [i1,i2] []) att2
+     fromShATerm att = 
+         case aterm of
+	    (ShAAppl "G_symb_items_list" [i1,i2] _) ->
+		let i1' = fromShATerm (getATermByIndex1 i1 att)
+                    att' = getATermByIndex1 i2 att
+                    l = lookupLogic_in_LG ("couldn't find Logic " ++ i1' ++ " in Logic Graph") i1' 
+                in case l of
+                    Logic lid -> (G_symb_items_list lid (fromShATerm_symb_items_list lid att'))
+         where
+         aterm = getATerm att
+
+instance ATermConvertible G_symb_map_items_list where
+     toShATerm att0 (G_symb_map_items_list lid symb_map_items) = 
+	 let (att1,i1) = toShATerm att0 (language_name lid)
+             (att2,i2) = toShATerm att1 symb_map_items
+         in addATerm (ShAAppl "G_symb_map_items_list" [i1,i2] []) att2
+     fromShATerm att = 
+         case aterm of
+	    (ShAAppl "G_symb_map_items_list" [i1,i2] _) ->
+		let i1' = fromShATerm (getATermByIndex1 i1 att)
+                    att' = getATermByIndex1 i2 att
+                    l = lookupLogic_in_LG ("couldn't find Logic " ++ i1' ++ " in Logic Graph") i1' 
+                in case l of
+                    Logic lid -> (G_symb_map_items_list lid (fromShATerm_symb_map_items_list lid att'))
+         where
+         aterm = getATerm att
+
+instance ATermConvertible G_diagram where
+     toShATerm att0 (G_diagram lid diagram) = 
+	 let (att1,i1) = toShATerm att0 (language_name lid)
+             (att2,i2) = toShATerm att1 diagram
+         in addATerm (ShAAppl "G_diagram" [i1,i2] []) att2
+     fromShATerm att = 
+         case aterm of
+	    (ShAAppl "G_diagram" [i1,i2] _) ->
+		let i1' = fromShATerm (getATermByIndex1 i1 att)
+                    att' = getATermByIndex1 i2 att
+                    l = lookupLogic_in_LG ("couldn't find Logic " ++ i1' ++ " in Logic Graph") i1' 
+                in case l of
+                    Logic lid -> (G_diagram lid (fromShATerm_diagram lid att'))
+         where
+         aterm = getATerm att
+
+
+instance ATermConvertible G_sublogics where
+     toShATerm att0 (G_sublogics lid sublogics) = 
+	 let (att1,i1) = toShATerm att0 (language_name lid)
+             (att2,i2) = toShATerm att1 sublogics
+         in addATerm (ShAAppl "G_sublogics" [i1,i2] []) att2
+     fromShATerm att = 
+         case aterm of
+	    (ShAAppl "G_sublogics" [i1,i2] _) ->
+		let i1' = fromShATerm (getATermByIndex1 i1 att)
+                    att' = getATermByIndex1 i2 att
+                    l = lookupLogic_in_LG ("couldn't find Logic " ++ i1' ++ " in Logic Graph") i1' 
+                in case l of 
+                    Logic lid -> (G_sublogics lid (fromShATerm_sublogics lid att'))
+         where
+         aterm = getATerm att 
+
+instance ATermConvertible G_morphism where
+     toShATerm att0 (G_morphism lid morphism) = 
+	 let (att1,i1) = toShATerm att0 (language_name lid)
+             (att2,i2) = toShATerm att1 morphism
+         in addATerm (ShAAppl "G_morphism" [i1,i2] []) att2
+     fromShATerm att = 
+         case aterm of
+	    (ShAAppl "G_morphism" [i1,i2] _) ->
+		let i1' = fromShATerm (getATermByIndex1 i1 att)
+                    att' = getATermByIndex1 i2 att
+                    l = lookupLogic_in_LG ("couldn't find Logic " ++ i1' ++ " in Logic Graph") i1' 
+                in case l of 
+                    Logic lid -> (G_morphism lid (fromShATerm_morphism lid att'))
+         where
+         aterm = getATerm att
+
+---------------------------------------------------------------
+
+instance ATermConvertible AnyComorphism where
+     toShATerm att0 (Comorphism cid) = 
+	 let (att1,i1) = toShATerm att0 (language_name cid)
+         in addATerm (ShAAppl "Comorphism" [i1] []) att1
+     fromShATerm att = 
+         case aterm of
+	    (ShAAppl "Comorphism" [i1] _) -> 
+		let i1' = fromShATerm (getATermByIndex1 i1 att) 
+                in lookupComorphism_in_LG ("couldn't find Comorphims " ++ i1' ++ " in Logic Graph") i1'
+         where
+         aterm = getATerm att
+
+instance ATermConvertible GMorphism where
+     toShATerm att0 (GMorphism cid sign1 morphism2) = 
+	 let (att1,i1) = toShATerm att0 (language_name cid)
+             (att2,i2) = toShATerm att1 sign1 
+             (att3,i3) = toShATerm att2 morphism2
+         in addATerm (ShAAppl "GMorphism" [i1,i2,i3] []) att3
+     fromShATerm att = 
+           case aterm of
+	    (ShAAppl "GMorphism" [i1,i2,i3] _) ->
+	 	let i1'  = fromShATerm (getATermByIndex1 i1 att)
+                    c  =  lookupComorphism_in_LG ("couldn't find Comorphism " ++ i1' ++ " in Logic Graph") i1'
+                    att' = getATermByIndex1 i2 att
+                    att'' = getATermByIndex1 i3 att
+                in case c of
+		     Comorphism cid ->
+                       (GMorphism cid (fromShATerm_sign1 cid att') (fromShATerm_morphism2 cid att'') )
+         where
+         aterm = getATerm att
+
+instance ATermConvertible Grothendieck where
+     toShATerm att0 Grothendieck = addATerm (ShAAppl "Grothendieck" [] []) att0
+     fromShATerm att = case aterm of
+                      (ShAAppl "Grothendieck" [] _) -> Grothendieck
+                     where aterm = getATerm att
+
+
+instance ATermConvertible AnyLogic where
+     toShATerm att0 (Logic lid) = 
+	 let (att1,i1) = toShATerm att0 (language_name lid)
+         in addATerm (ShAAppl "Logic" [i1] []) att1
+     fromShATerm att = 
+         case aterm of
+	    (ShAAppl "Logic" [i1] _) ->
+		let i1' = fromShATerm (getATermByIndex1 i1 att)
+                in lookupLogic_in_LG ("couldn't find Logic " ++ i1' ++ " in Logic Graph") i1'
+         where
+         aterm = getATerm att
+
+
+
+
+
+
