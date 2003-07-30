1f731af0cbb8adca8f47ec62a292dadfa1c0c123
Christian Maeder <Christian.Maeder@dfki.de>
2003-07-30 17:22:31 +0000

    haddock does not like hashmarks in comments git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@1635 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Proofs/Proofs.hs
+++ Proofs/Proofs.hs
@@ -134,7 +134,7 @@ globSubsumeAux dGraph (rules,changes) ((ledge@(source,target,edgeLab)):list) =  
        globSubsumeAux dGraph (rules,changes) list
   where
     auxGraph = delLEdge ledge dGraph
-               -- ### wieder reinnehmen, sobald Eq definiert #####
+               -- wieder reinnehmen, sobald Eq definiert #####
 	       -- let auxGraph = delLEdge ledge dGraph
     newEdge = (source,
 	       target,
