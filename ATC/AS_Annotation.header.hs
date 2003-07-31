c57c1d611b60818decbcda194c0c87c0de5b55d3
Felix Reckers <felix@informatik.uni-bremen.de>
2003-07-31 10:24:10 +0000

    header file for ATC/AS_Annotation.hs git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@1636 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ ATC/AS_Annotation.header.hs
@@ -0,0 +1,25 @@
+
+-- begin header
+
+instance (ATermConvertible a) => ATermConvertible (Annoted a) where
+    toShATerm att0 (Annoted aa bb cc dd) =
+	let (att1,aa') = toShATerm att0 aa
+            (att2,bb') = toShATerm att1 bb
+            (att3,cc') = toShATerm att2 cc
+            (att4,dd') = toShATerm att3 dd
+            lat = [aa',bb',cc',dd']
+	in addATerm (ShAAppl "Annoted"  lat []) att4
+    fromShATerm att =
+	case aterm of
+	    (ShAAppl "Annoted" [ aa, bb, cc, dd ] _) ->
+		let aa' = fromShATerm (getATermByIndex1 aa att)
+		    bb' = fromShATerm (getATermByIndex1 bb att)
+		    cc' = fromShATerm (getATermByIndex1 cc att)
+		    dd' = fromShATerm (getATermByIndex1 dd att)
+                in (Annoted aa' bb' cc' dd')
+            _ -> fromShATermError "Annoted" aterm
+	where
+	    aterm = getATerm att
+
+-- end header
+
