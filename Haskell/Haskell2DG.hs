d0bf54f2ae867b718e6395a672fe1709b72f8601
Sonja Gröning <sonja@informatik.uni-bremen.de>
2003-11-03 13:07:54 +0000

    Added: recursive graph creation               recursive libEnv creation git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@1815 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Haskell/Haskell2DG.hs
+++ Haskell/Haskell2DG.hs
@@ -3,10 +3,10 @@
    Year:    2003
 -}
 
-module Haskell.Haskell2DG where
+module Haskell.Haskell2DG (anaHaskellFile) where
 
 import Options
-import Haskell.Hatchet.MultiModule  (readModuleInfo, readOneImportSpec)
+import Haskell.Hatchet.MultiModule  (readModuleInfo)
 import Haskell.Hatchet.HsParseMonad (ParseResult (..))
 import Haskell.Hatchet.SynConvert   (toAHsModule)
 import Haskell.Hatchet.HsParsePostProcess (fixFunBindsInModule)
@@ -37,6 +37,9 @@ import Static.DevGraph          (DGNodeLab (..),
                                  DGOrigin (..),
                                  DGraph,
                                  LibEnv,
+                                 GlobalEntry(..),
+                                 NodeSig(..),
+                                 getNode,
                                  emptyLibEnv)
 import Syntax.AS_Library        (LIB_NAME (..),
                                  LIB_ID (..))
@@ -45,22 +48,26 @@ import Logic.Grothendieck        (G_sign (..),
                                   G_l_sentence_list (..),
                                   G_morphism (..),
                                   gEmbed)
+import Logic.Logic               
 import Common.Lib.Graph          (Node,
                                   empty,
                                   insNode,
                                   insEdge,
-                                  newNodes)
+                                  newNodes,
+                                  match)
 
 import Common.Id                 (Token (..),
                                   SIMPLE_ID,
                                   nullPos)
 import Haskell.Logic_Haskell     (Haskell (..))
 import Haskell.HaskellUtils      (extractSentences)
-import GHC.IOBase                (unsafePerformIO)
+import qualified Common.Lib.Map as Map
+import Common.GlobalAnnotations  (emptyGlobalAnnos)
 
--- - Datentyp anlegen für HaskellEnv (Resultat von tiModule)
+
 data HaskellEnv = HasEnv Timing              -- timing values for each stage
-                         (Env Scheme)        -- output variable assumptions (may be local, and pattern variables) 
+                         (Env Scheme)        -- output variable assumptions (may be 
+                                             --        local, and pattern variables) 
                          (Env Scheme)        -- output data cons assumptions 
                          ClassHierarchy      -- output class Hierarchy 
                          KindEnv             -- output kinds 
@@ -68,11 +75,12 @@ data HaskellEnv = HasEnv Timing              -- timing values for each stage
                          AHsModule           -- renamed module 
                          [AHsDecl]           -- synonyms defined in this module
 
-
+-- toplevel function: Creates DevGraph and LibEnv from a .hs file (including all imported modules)
 anaHaskellFile :: HetcatsOpts -> String -> IO (Maybe (LIB_NAME, -- filename
                                                       HsModule, -- as tree
                                                       DGraph,   -- development graph
-                                                      LibEnv    -- DGraphs für importierte Module 
+                                                      LibEnv    -- DGraphs for imported modules 
+                                                                --  incl. main module
                                                       ))
 anaHaskellFile _ srcFile = 
    do
@@ -123,20 +131,20 @@ anaHaskellFile _ srcFile =
                          moduleIds
                          moduleRenamed
                          moduleSynonyms
-     --in 
-     return (Just(Lib_id(Indirect_link srcFile []), moduleSyntax, hasEnv2DG hasEnv, 
-             hasEnv2LG hasEnv))
+     let libName = Lib_id(Indirect_link srcFile [])
+     (dg,le) <- hasEnv2DG libName hasEnv
+     return (Just(libName, moduleSyntax, dg, le))
 
-hasEnv2DG :: HaskellEnv -> DGraph
-hasEnv2DG (HasEnv 
-             _
-             moduleEnv
-             dataConEnv
-             classHier
-             kindInfoTable
-             moduleIds
-             (AHsModule name exps imps decls)
-             moduleSynonyms) =
+hasEnv2DG :: LIB_NAME -> HaskellEnv -> IO (DGraph, LibEnv)
+hasEnv2DG ln (HasEnv 
+               _
+               moduleEnv
+               dataConEnv
+               classHier
+               kindInfoTable
+               _
+               (AHsModule name exps imps decls)
+               moduleSynonyms) =
 
                let modInfo = ModuleInfo {
                      moduleName = name,
@@ -148,12 +156,15 @@ hasEnv2DG (HasEnv
                      infixDecls = [],
                      tyconsMembers = [] }
                    aMod = AHsModule name exps imps decls
-               in
-                   hsMod2DG aMod modInfo
+                in
+                   dGraphAndLibEnv ln aMod modInfo
+                   
 
--- main Module and main ModuleInfo
-hsMod2DG :: AHsModule -> ModuleInfo -> DGraph
-hsMod2DG (AHsModule name exps imps decls) modInfo = 
+-- input: main module's abstract syntax and its ModuleInfo
+-- generates a DGraph and 'first node' of this DGraph
+-- checks whether there are imported modules or not
+dGraphAndLibEnv :: LIB_NAME -> AHsModule -> ModuleInfo -> IO(DGraph, LibEnv)
+dGraphAndLibEnv ln (AHsModule name exps imps decls) modInfo = 
            -- create the first node, representing the main Module
        let node_contents  | imps == [] =       -- no imports
                              DGNode {
@@ -171,30 +182,38 @@ hsMod2DG (AHsModule name exps imps decls) modInfo =
                                dgn_origin = DGExtension }
            dg = empty
            [node] = newNodes 0 dg
-           dg' = insNode (node,node_contents) dg
+           dg' = insNode(node, node_contents) dg
         in
            case imps of
-             [] -> dg                       -- no imports, no other nodes and edges
-             _  -> insImports dg node imps  -- imports -> add imported Modules
+             -- no imports, no other nodes and edges
+             [] -> return (dg', (addLibEnv emptyLibEnv ln node dg'))
+             -- imports -> add imported Modules
+             _  -> insImports ln dg' node emptyLibEnv imps
 
--- DGraph consisting in one node (the main Module), this node and ModuleInfo 
--- about imported Modules
-insImports :: DGraph ->  Node -> [AHsImportDecl] -> DGraph
-insImports dg _ [] = dg
-insImports dg n ((AHsImportDecl src name b mbm mayBeHiding):idecls) = 
-        let idecl = AHsImportDecl src name b mbm mayBeHiding
-            node_contents = DGNode {
-                               dgn_name = aHsMod2SimpleId name,
-                               dgn_sign = G_sign Haskell (unsafePerformIO 
-                                                          (readOneImportSpec idecl)),
-                               dgn_sens = G_l_sentence Haskell [],
-                               dgn_origin = DGBasic }
-            [node] = newNodes 0 dg
-            dg' = insNode(node,node_contents) dg
-            link = createDGLinkLabel idecl
-            dg'' = insEdge (node,n,link) dg'
-        in
-           insImports dg'' n idecls
+-- input: LibName of 'main module', growing DGraph, node of 'main module'
+--        growing LibEnv, List of importDecls (from 'main module')
+insImports :: LIB_NAME -> DGraph ->  Node -> LibEnv -> [AHsImportDecl] -> IO (DGraph,LibEnv)
+insImports ln dg n le [] = return (dg, (addLibEnv le ln n dg))
+insImports ln dg n le ((AHsImportDecl src name b mbm mayBeHiding):idecls) = 
+        do -- analyse imported module just like the main module
+           Just(libName, _, dg', libEnv) <- anaHaskellFile defaultHetcatsOpts (fileName name)
+
+           let idecl = AHsImportDecl src name b mbm mayBeHiding
+           -- unite the old 'big' LibEnv le with the one for the imported module
+           let le' = le `Map.union` libEnv
+           -- unite the main DGraph with the one for the imported module
+           let dg'' = joinDG dg dg'
+           -- lookup node for imported module
+           let node = lookupNode libName libEnv
+           -- create LinkLabel
+           let link = createDGLinkLabel idecl
+           -- insert new edge with LinkLabel
+           let linkedDG = insEdge (node,n,link) dg''
+
+           insImports ln linkedDG n le' idecls
+
+
+-- --------------- utilities --------------- --
 
 createDGLinkLabel :: AHsImportDecl -> DGLinkLab
 createDGLinkLabel idecl = 
@@ -214,16 +233,65 @@ createDGLinkLabel idecl =
                        dgl_morphism = gEmbed (G_morphism Haskell ()),
                        dgl_type = HidingDef,
                        dgl_origin = DGExtension }
+                              
+-- GlobalContext:
+-- GlobalAnnos können erstmal leer sein, weil es in Haskell da nichts
+-- vergleichbares gibt. (Evtl. könnten später die Deklarationen von
+-- Infix-Operatoren und Prioritäten da rein kommen).
+-- Das GlobalEnv ordnet in CASL jeder Spezifikation einen Knoten
+-- im DGraph zu. Allerdings haben wir bei Haskell pro File nur eine
+-- "Spezifikation" (nämlich das Programm-Modul). Also muss das GlobalEnv
+-- hier einfach nur einen Eintrag haben, der dem Namen des Programm-Moduls
+-- den Knoten des Programm-Moduls im DGraph zuordnet. Das kannst du
+-- erzeugen mit
+
+-- Map.insert name (SpecEntry (imp,params,parsig,body)) Map.empty
+
+-- wobei name der Namen des Programm-Moduls ist, und
+-- imp = EmptyNode und parsig als empty_signature Haskell gewählt werden können,
+-- und params = []. body = NodeSig(n,sig), wobei n der Knoten im Entwicklungsgraph
+-- und sig die Signatur ist (beides jeweils die für das Programm-Modul).
+
+
+addLibEnv :: LibEnv -> LIB_NAME -> Node -> DGraph -> LibEnv
+addLibEnv le libName n dg =
+          let 
+            nodeLab = getNodeContent n dg
+            imp = EmptyNode (Logic Haskell)
+            params = []
+            parsig = dgn_sign nodeLab -- empty_signature Haskell
+            body = NodeSig (n, (dgn_sign nodeLab))
+            globalEnv = Map.insert (getDgn_name nodeLab) (SpecEntry (imp,params,parsig,body)) Map.empty
+          in
+            Map.insert libName (emptyGlobalAnnos, globalEnv, dg) le
+
+lookupNode :: LIB_NAME -> LibEnv -> Node
+lookupNode ln le = 
+           let Just (_, globalEnv, _) = Map.lookup ln le
+               (_, (SpecEntry (_, _, _, body))) = Map.elemAt 0 globalEnv
+           in
+               case (getNode body) of
+                 Just n -> n
+                 Nothing -> (-1)
 
 aHsMod2SimpleId :: AModule -> Maybe SIMPLE_ID
 aHsMod2SimpleId (AModule name) = Just (Token { tokStr = name, tokPos = nullPos })
-                              
-hasEnv2LG :: HaskellEnv -> LibEnv
-hasEnv2LG _ = emptyLibEnv
 
--- utilities
+fileName :: AModule -> String
+fileName (AModule name) = name ++ ".hs"
 
--- call the haskell parser and check for errors
+joinDG :: DGraph -> DGraph -> DGraph
+joinDG _ _ = empty
+
+getNodeContent :: Node -> DGraph -> DGNodeLab
+getNodeContent n dg =
+               case (match n dg) of
+                 (Just (_,_,nodeLab,_), _) -> nodeLab
+--               | otherwise = ??
+
+getDgn_name :: DGNodeLab -> SIMPLE_ID
+getDgn_name nl = let Just(n) = dgn_name nl
+                 in  n
 
 parseHsSource :: String -> HsModule
 parseHsSource s = case parse s (SrcLoc 1 1) 0 [] of
