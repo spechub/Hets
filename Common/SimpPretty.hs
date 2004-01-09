1a99f9f2be9c305c2a8a45b3a0f0e191028588f5
Klaus Luettich <luettich@informatik.uni-bremen.de>
2004-01-09 10:36:59 +0000

    A simplified Pretty printing system. Only oneliner are possible. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@1928 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ Common/SimpPretty.hs
@@ -0,0 +1,208 @@
+{- 
+   > HetCATS/Common/SimpPretty.hs
+   > $Id$
+   > Authors: Hughes, Peyton Jones, Klaus Lüttich
+   > Year:    2002/2003
+
+   An imported and simplified version of GHC module
+   
+   GHCs documentation follows 
+
+-}
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Common.Lib.Pretty
+-- Copyright   :  (c) The University of Glasgow 2001
+-- License     :  BSD-style (see the file libraries/base/LICENSE)
+-- 
+-- Maintainer  :  libraries@haskell.org
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-- John Hughes's and Simon Peyton Jones's Pretty Printer Combinators
+-- 
+-- Based on /The Design of a Pretty-printing Library/
+-- in Advanced Functional Programming,
+-- Johan Jeuring and Erik Meijer (eds), LNCS 925
+-- <http://www.cs.chalmers.se/~rjmh/Papers/pretty.ps>
+--
+-- Heavily modified by Simon Peyton Jones, Dec 96
+--
+-----------------------------------------------------------------------------
+
+
+module Common.SimpPretty (
+
+	-- * The document type
+        SDoc,            -- Abstract
+
+	-- * Primitive SDocuments
+        empty,comma,
+
+	-- * Converting values into documents
+        text, char, integer,
+
+	-- * Wrapping documents in delimiters
+        parens, brackets, braces,
+
+	-- * Combining documents
+        (<>), punctuate,
+
+	-- * Predicates on documents
+	isEmpty,
+
+	-- * Rendering documents
+
+	render, fullRender,
+
+        TextDetails(..)
+
+  ) where
+
+
+import Prelude
+
+infixl 6 <> 
+
+-- ---------------------------------------------------------------------------
+-- The interface
+
+-- The primitive SDoc values
+
+isEmpty :: SDoc    -> Bool;  -- ^ Returns 'True' if the document is empty
+
+empty   :: SDoc;			-- ^ An empty document
+comma   :: SDoc;                 -- ^ A ',' character
+
+text	 :: String   -> SDoc
+char 	 :: Char     -> SDoc
+integer  :: Integer  -> SDoc
+
+
+parens       :: SDoc -> SDoc; 	-- ^ Wrap document in @(...)@
+brackets     :: SDoc -> SDoc;  	-- ^ Wrap document in @[...]@
+braces	     :: SDoc -> SDoc;   	-- ^ Wrap document in @{...}@
+
+-- Combining @SDoc@ values
+
+(<>)   :: SDoc -> SDoc -> SDoc;     -- ^Beside
+punctuate :: SDoc -> [SDoc] -> [SDoc];      -- ^ @punctuate p [d1, ... dn] = [d1 \<> p, d2 \<> p, ... dn-1 \<> p, dn]@
+
+
+-- Displaying @SDoc@ values. 
+
+instance Show SDoc where
+  showsPrec _prec doc cont = showSDoc doc cont
+
+-- | Renders the document as a string using the default style
+render     :: SDoc -> String
+
+fullRender :: (TextDetails -> a -> a)   -- ^What to do with text
+           -> a                         -- ^What to do at the end
+           -> SDoc			-- ^The document
+           -> a                         -- ^Result
+
+comma = char ','
+
+integer  n = text (show n)
+
+parens p        = char '(' <> p <> char ')'
+brackets p      = char '[' <> p <> char ']'
+braces p        = char '{' <> p <> char '}'
+
+punctuate _ []     = []
+punctuate p (d:ds) = go d ds
+                   where
+                     go d1 [] = [d1]
+                     go d1 (e:es) = (d1 <> p) : go e es
+
+-- ---------------------------------------------------------------------------
+-- The SDoc data type
+
+-- A SDoc represents a *set* of layouts.  A SDoc with
+-- no occurrences of Union or NoSDoc represents just one layout.
+
+-- | The abstract type of documents
+data SDoc
+ = Empty                                -- empty
+ | TextBeside TextDetails SDoc      -- text s <> x  
+ | Beside SDoc SDoc                       
+
+type RSDoc = SDoc         -- RSDoc is a "reduced SDoc", guaranteed not to have a top-level Above or Beside
+
+
+reduceSDoc :: SDoc -> RSDoc
+reduceSDoc (Beside p q) = beside p (reduceSDoc q)
+reduceSDoc p              = p
+
+
+data TextDetails = Chr  Char
+                 | Str  String
+
+        -- Arg of a TextBeside is always an RSDoc
+
+textBeside_ :: TextDetails -> SDoc -> SDoc
+textBeside_ s p = TextBeside s p
+
+-- ---------------------------------------------------------------------------
+-- @empty@, @text@, @nest@, @union@
+
+empty = Empty
+
+isEmpty Empty = True
+isEmpty _     = False
+
+char  c = textBeside_ (Chr c) Empty
+text  s = textBeside_ (Str s) Empty
+
+-- ---------------------------------------------------------------------------
+-- Horizontal composition @<>@
+
+p <>  q = Beside p q
+
+beside :: SDoc -> RSDoc -> RSDoc
+-- Specification: beside g p q = p <g> q
+ 
+beside Empty             q   = q
+beside (TextBeside s p) q   = textBeside_ s rest
+                               where
+                                  rest = case p of
+                                           Empty -> q
+                                           _     -> beside p q
+beside p q2 = beside (reduceSDoc p) q2
+
+-- ---------------------------------------------------------------------------
+-- Displaying the best layout
+
+
+render doc       = showSDoc doc ""
+
+showSDoc :: SDoc -> String -> String
+showSDoc doc rest = fullRender string_txt_comp rest doc
+
+{-
+string_txt (Chr c)   s  = c:s
+string_txt (Str s1)  s2 = s1 ++ s2
+string_txt (PStr s1) s2 = s1 ++ s2
+-}
+string_txt_comp :: TextDetails -> String -> String
+string_txt_comp td = case td of
+		     Chr  c -> showChar   c
+		     Str  s -> showString s
+
+
+fullRender txt end doc = easy_display txt end (reduceSDoc doc)
+
+easy_display :: (TextDetails -> a -> a)
+           -> a                        
+           -> SDoc			
+           -> a                         
+easy_display txt end doc 
+  = lay doc 
+  where
+    lay Empty            = end
+    lay (TextBeside s p) = s `txt` lay p
+    lay _ = error "lay: Beside found"
+
+
