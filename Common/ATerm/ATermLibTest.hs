09ca01bcf4700ec3ea86d06ec08b9485e4a908d5
Klaus Luettich <luettich@informatik.uni-bremen.de>
2004-01-13 09:49:36 +0000

    A test Program for the ATerm.AbstractSyntax and ATerm.ReadWrite modules git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@1943 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ Common/ATerm/ATermLibTest.hs
@@ -0,0 +1,18 @@
+module Main where
+
+import System
+import Common.ATerm.AbstractSyntax
+import Common.ATerm.ReadWrite
+import Common.SimpPretty
+
+main :: IO ()
+main = do args <- getArgs
+	  mapM_ testATC args
+
+testATC :: FilePath -> IO ()
+testATC fp = do str <- readFile fp
+		let att = readATerm str
+		putStrLn ("Reading File "++fp++" ...")
+		let fp' = fp++".ttttt"
+		putStrLn ("Writing File "++fp'++" ...")                
+		writeFileSDoc fp' (writeSharedATermSDoc att)
