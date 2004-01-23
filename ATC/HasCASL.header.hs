312a70761a32914061444e383df34b8d3c85fe65
Christian Maeder <Christian.Maeder@dfki.de>
2004-01-23 17:56:55 +0000

    added component git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@1996 cec4b9c1-7d33-0410-9eda-942365e851bb

--- ATC/HasCASL.header.hs
+++ ATC/HasCASL.header.hs
@@ -38,24 +38,26 @@ instance (ATermConvertible t) => ATermConvertible (Qual t)where
 
  
 instance ATermConvertible Env where
-     toShATerm att0 (Env cm tm as sen env cou) = 
+     toShATerm att0 (Env cm tm as sen env mis cou) = 
 	 let (att1,i1) = toShATerm att0 cm
              (att2,i2) = toShATerm att1 tm
              (att3,i3) = toShATerm att2 as
              (att4,i4) = toShATerm att3 sen 
              (att5,i5) = toShATerm att4 env
-             (att6,i6) = toShATerm att5 cou
-         in addATerm (ShAAppl "Env" [i1,i2,i3,i4,i5,i6] []) att6
+             (att6,i6) = toShATerm att5 mis
+             (att7,i7) = toShATerm att6 cou
+         in addATerm (ShAAppl "Env" [i1,i2,i3,i4,i5,i6,i7] []) att6
      fromShATerm att = 
          case aterm of
-	    (ShAAppl "Env" [i1,i2,i3,i4,i5,i6] _) ->
+	    (ShAAppl "Env" [i1,i2,i3,i4,i5,i6,i7] _) ->
 		let i1'  = fromShATerm (getATermByIndex1 i1 att)
 		    i2'  = fromShATerm (getATermByIndex1 i2 att)
                     i3'  = fromShATerm (getATermByIndex1 i3 att)
                     i4' = fromShATerm (getATermByIndex1 i4 att)
                     i5' = fromShATerm (getATermByIndex1 i5 att)
                     i6' = fromShATerm (getATermByIndex1 i6 att)
-                in (Env i1' i2' i3' i4' i5' i6')
+                    i7' = fromShATerm (getATermByIndex1 i7 att)
+                in (Env i1' i2' i3' i4' i5' i6' i7')
 	    u -> fromShATermError "Env" u
          where
          aterm = getATerm att
