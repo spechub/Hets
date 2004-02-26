53ee92d630257c55f79cc322db940737fec9deb2
Maciek Makowski <maciek@mmakowski.com>
2004-02-26 21:51:29 +0000

    Implemented unit declaration analysis Implemented the computation of diagram extension (extendDiagram) Adjusted existing functions according to modifications of types from DevGraph Added further arguments to analysis functions Updated Haddock comments git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@2228 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Static/AnalysisArchitecture.hs
+++ Static/AnalysisArchitecture.hs
@@ -1,16 +1,17 @@
-{-| 
-  Module      :  $Header$
-  Author      :  Maciek Makowski
-  Maintainer  :  hets@tzi.de
-  Year        :  2004
-  Copyright   :  (c) Maciek Makowski, Warsaw University 2004
-  Licence     :  similar to LGPL, see HetCATS/LICENCE.txt or LIZENZ.txt
-  Stability   :  provisional
-  Portability :  non-portable (via imports)
+{- | 
+   Module      :  $Header$
+   Author      :  Maciek Makowski
+   Year        :  2004
+   Copyright   :  (c) Maciek Makowski, Warsaw University 2004
+   Licence     :  similar to LGPL, see HetCATS/LICENCE.txt or LIZENZ.txt
 
-  Analysis of architectural specifications.
-  Follows the extended static semantics sketched in Chap. III:5.6
-  of the CASL Reference Manual.
+   Maintainer  :  hets@tzi.de
+   Stability   :  provisional
+   Portability :  non-portable (via imports)
+
+   Analysis of architectural specifications.
+   Follows the extended static semantics sketched in Chap. III:5.6
+   of the CASL Reference Manual.
 -}
 
 module Static.AnalysisArchitecture (ana_ARCH_SPEC, ana_UNIT_SPEC)
@@ -19,7 +20,7 @@ where
 import Maybe 
 import Logic.Logic
 import Logic.Grothendieck
--- import Common.Lib.Graph
+import Common.Lib.Graph
 import Static.DevGraph
 -- import Syntax.Parse_AS_Structured (lookupLogicName)
 -- import Syntax.Parse_AS_Library(library)
@@ -51,17 +52,20 @@ import qualified Common.Lib.Map as Map
 -- @
 -- ARCH-SPEC ::= BASIC-ARCH-SPEC | GROUP-ARCH-SPEC | ARCH-SPEC-NAME
 -- @
-ana_ARCH_SPEC :: LogicGraph -> AnyLogic -> GlobalContext 
-	      -> AnyLogic -> Bool -> ARCH_SPEC
-              -> IOResult ArchSig
+ana_ARCH_SPEC :: LogicGraph -> AnyLogic    -- ^ the default logic
+	      -> GlobalContext -> AnyLogic -- ^ current logic
+	      -> Bool                      -- ^ should only the structure be analysed?
+	      -> ARCH_SPEC -> IOResult ArchSig 
+-- ^ returns the architectural signature of given ARCH-SPEC
+
 -- BASIC-ARCH-SPEC
-ana_ARCH_SPEC lgraph defl gctx@(gannos, genv, dg) l just_struct asp@(Basic_arch_spec udd uexpr pos) = 
-    do uctx <- ana_UNIT_DECL_DEFNS defl gctx udd
+ana_ARCH_SPEC lgraph defl gctx@(gannos, genv, dg) curl justStruct asp@(Basic_arch_spec udd uexpr pos) = 
+    do uctx <- ana_UNIT_DECL_DEFNS lgraph defl gctx curl justStruct udd
        (_, usig, _) <- ana_UNIT_EXPRESSION defl gctx uctx (item uexpr)
        return (ctx uctx, usig)
 -- GROUP-ARCH-SPEC
-ana_ARCH_SPEC lgraph defl gctx@(gannos, genv, dg) l just_struct gsp@(Group_arch_spec asp pos) = 
-    ana_ARCH_SPEC lgraph defl gctx l just_struct (item asp)
+ana_ARCH_SPEC lgraph defl gctx@(gannos, genv, dg) curl justStruct gsp@(Group_arch_spec asp pos) = 
+    ana_ARCH_SPEC lgraph defl gctx curl justStruct (item asp)
 -- ARCH-SPEC-NAME
 ana_ARCH_SPEC lgraph defl gctx@(gannos, genv, dg) l just_struct asp@(Arch_spec_name asn@(Token _ pos)) = 
     do case Map.lookup asn genv of
@@ -75,61 +79,109 @@ ana_ARCH_SPEC lgraph defl gctx@(gannos, genv, dg) l just_struct asp@(Arch_spec_n
 
 
 -- | Analyse a list of unit declarations and definitions
-ana_UNIT_DECL_DEFNS :: AnyLogic -> GlobalContext -> [Annoted UNIT_DECL_DEFN] -> IOResult ExtStUnitCtx
-ana_UNIT_DECL_DEFNS defl gctx udds = ana_UNIT_DECL_DEFNS' defl gctx emptyExtStUnitCtx udds
+ana_UNIT_DECL_DEFNS :: LogicGraph -> AnyLogic -> GlobalContext -> AnyLogic 
+		    -> Bool -> [Annoted UNIT_DECL_DEFN] -> IOResult ExtStUnitCtx
+ana_UNIT_DECL_DEFNS lgraph defl gctx curl justStruct udds = 
+    ana_UNIT_DECL_DEFNS' lgraph defl gctx curl justStruct emptyExtStUnitCtx udds
 
-ana_UNIT_DECL_DEFNS' :: AnyLogic -> GlobalContext -> ExtStUnitCtx 
-		     -> [Annoted UNIT_DECL_DEFN] -> IOResult ExtStUnitCtx
-ana_UNIT_DECL_DEFNS' _ _ uctx [] =
+ana_UNIT_DECL_DEFNS' :: LogicGraph -> AnyLogic -> GlobalContext -> AnyLogic -> Bool 
+		     -> ExtStUnitCtx -> [Annoted UNIT_DECL_DEFN] -> IOResult ExtStUnitCtx
+-- TODO: pass the dg' and udd' further
+ana_UNIT_DECL_DEFNS' _ _ _ _ _ uctx [] =
     do return uctx
-ana_UNIT_DECL_DEFNS' defl gctx uctx (udd : udds) =
-    do uctx' <- ana_UNIT_DECL_DEFN defl gctx uctx (item udd)
-       uctx'' <- ana_UNIT_DECL_DEFNS' defl gctx uctx' udds
+ana_UNIT_DECL_DEFNS' lgraph defl gctx@(gannos, genv, dg) curl justStruct uctx (udd : udds) =
+    do (uctx', dg', udd') <- ana_UNIT_DECL_DEFN lgraph defl gctx curl justStruct uctx (item udd) 
+       uctx'' <- ana_UNIT_DECL_DEFNS' lgraph defl (gannos, genv, dg') curl justStruct uctx' udds
        return uctx''
 
 
 -- | Analyse unit declaration or definition
-ana_UNIT_DECL_DEFN :: AnyLogic -> GlobalContext -> ExtStUnitCtx -> UNIT_DECL_DEFN -> IOResult ExtStUnitCtx
-ana_UNIT_DECL_DEFN defl gctx@(gannos, genv, dg) uctx ud@(Unit_decl un usp uts pos) =
-    -- TODO
-    do resToIORes (warning emptyExtStUnitCtx "Ignoring unit declaration" (head (fromJust (get_pos_l ud))))
-       return uctx
-ana_UNIT_DECL_DEFN defl gctx uctx@(ps, bs, d) 
+ana_UNIT_DECL_DEFN :: LogicGraph -> AnyLogic -> GlobalContext -> AnyLogic 
+		   -> Bool -> ExtStUnitCtx -> UNIT_DECL_DEFN -> IOResult (ExtStUnitCtx, DGraph, UNIT_DECL_DEFN)
+-- ^ returns 1. extended static unit context 2. possibly modified development graph 
+-- 3. possibly modified UNIT_DECL_DEFN
+
+-- unit declaration
+ana_UNIT_DECL_DEFN lgraph defl gctx@(gannos, genv, dg) curl justStruct 
+		   uctx@(buc, diag) ud@(Unit_decl un@(Token _ unpos) usp uts pos) =
+    do (dns@(Diag_node_sig n impSig), diag') <- ana_UNIT_IMPORTED defl uctx uts
+       (usig, dg', usp') <- ana_UNIT_SPEC lgraph defl gctx curl justStruct impSig usp
+       let ud' = Unit_decl un usp' uts pos
+       if Map.member un buc 
+	  then
+	  resToIORes (plain_error (uctx, dg', ud')
+		                  ("Unit " ++ showPretty un " already declared/defined")
+		                  unpos)
+	  else
+	  case usig of
+    	       Par_unit_sig (argSigs, resultSig) -> 
+		   do (resultSig', dg'') <- resToIORes (nodeSigUnion lgraph dg' (resultSig : [impSig]) DGImports)
+		      let basedParUSig = Based_par_unit_sig (dns, (argSigs, resultSig'))
+   		      return ((Map.insert un basedParUSig buc, diag'), dg'', ud')
+	       Unit_sig nsig -> 
+		   do (nsig', dg'') <- resToIORes (nodeSigUnion lgraph dg' (impSig : [nsig]) DGImports)
+		      (dn', diag'') <- extendDiagram diag' [dns] nsig'
+		      return ((Map.insert un (Based_unit_sig dn') buc, diag''), dg'', ud')
+
+-- unit definition
+ana_UNIT_DECL_DEFN lgraph defl gctx@(_, _, dg) curl justStruct uctx@(buc, d) 
 		   ud@(Unit_defn un@(Token _ unpos) uexp pos) =
     do (p, usig, diag) <- ana_UNIT_EXPRESSION defl gctx uctx uexp
-       if Map.member un (ctx uctx)
+       {- it's sufficient to check that un is not mapped in buc, we don't need
+          to convert the ExtStUnitCtx to StUnitCtx as the domain will be preserved -}
+       if Map.member un buc 
 	  then
-	  resToIORes (plain_error uctx
-		                  ("Unit " ++ showPretty un " already defined")
+	  resToIORes (plain_error (uctx, dg, ud)
+		                  ("Unit " ++ showPretty un " already defined/declared")
 		                  unpos)
 	  else
 	  case usig of
 	       -- we can use Map.insert as there are no mappings for un in ps and bs
 	       -- (otherwise there would have been a mapping in (ctx uctx))
-	       Unit_sig _ -> return (ps, Map.insert un p bs, d)
-	       Par_unit_sig parusig -> return (Map.insert un (p, parusig) ps, bs, d)
+	       Unit_sig _ -> return ((Map.insert un (Based_unit_sig p) buc, diag),
+				     dg, ud)
+	       Par_unit_sig parusig -> return ((Map.insert un (Based_par_unit_sig (p, parusig)) buc, diag),
+					       dg, ud)
 
 
 -- | Analyse unit imports
 -- TODO
-ana_UNIT_IMPORTED :: ExtStUnitCtx -> [Annoted UNIT_TERM] -> IOResult (Item, Diag)
-ana_UNIT_IMPORTED uctx uts =
-    do resToIORes (warning ((), emptyDiag) "Ignoring unit imports" nullPos)
-       return (0, emptyDiag)
+ana_UNIT_IMPORTED :: AnyLogic -> ExtStUnitCtx -> [Annoted UNIT_TERM] -> IOResult (DiagNodeSig, Diag)
+ana_UNIT_IMPORTED defl uctx uts =
+    do resToIORes (warning (emptyDiagNodeLab defl, emptyDiag) "Ignoring unit imports" nullPos)
+       extendDiagram emptyDiag [] (EmptyNode defl)
+       -- return (emptyDiagNodeSig defl, emptyDiag)
 
 
 -- | Analyse an unit expression
 -- TODO
 ana_UNIT_EXPRESSION :: AnyLogic -> GlobalContext -> ExtStUnitCtx 
-		    -> UNIT_EXPRESSION -> IOResult (Item, UnitSig, Diag)
+		    -> UNIT_EXPRESSION -> IOResult (DiagNodeSig, UnitSig, Diag)
 ana_UNIT_EXPRESSION defl gctx@(gannos, genv, dg) uctx uexp@(Unit_expression ubs ut pos) =
     do resToIORes (warning ((), emptyUnitSig defl, emptyDiag) "Ignoring unit expression" (headPos pos))
-       return (0, emptyUnitSig defl, emptyDiag)
+       return (emptyDiagNodeSig defl, emptyUnitSig defl, emptyDiag)
+
+
+-- | Analyse an unit term
+ana_UNIT_TERM :: LogicGraph -> AnyLogic -> GlobalContext -> AnyLogic 
+	      -> Bool -> ExtStUnitCtx -> UNIT_TERM 
+	      -> IOResult (DiagNodeSig, Diag, UNIT_TERM)
+-- TODO
+ana_UNIT_TERM lgraph defl gctx curl justStruct uctx@(buc, diag) ut =
+    do resToIORes (warning (emptyDiagNodeSig defl, diag, ut) "Ignoring unit term" nullPos)
+       return (emptyDiagNodeSig defl, diag, ut)
 
 
 -- | Analyse unit specification
-ana_UNIT_SPEC :: LogicGraph -> AnyLogic -> GlobalContext -> AnyLogic 
-	      -> Bool -> NodeSig -> UNIT_SPEC -> IOResult (UnitSig, DGraph, UNIT_SPEC)
+ana_UNIT_SPEC :: LogicGraph -> AnyLogic    -- ^ the default logic
+	      -> GlobalContext -> AnyLogic -- ^ current logic
+	      -> Bool                      -- ^ should only the structure be analysed?
+	      -> NodeSig                   -- ^ the signature of imports
+	      -> UNIT_SPEC -> IOResult (UnitSig, DGraph, UNIT_SPEC)
+-- ^ returns 1. unit signature 2. the development graph resulting from 
+-- structred specs inside the unit spec and 3. a UNIT_SPEC after possible
+-- conversions.
+
 -- UNIT-TYPE
 {- if argspecs are empty and resultspec is a name of unit spec then this
    should be converted to a Spec_name -}
@@ -180,3 +232,22 @@ ana_argSpecs lgraph defl gctx@(gannos, genv, dg) justStruct (argSpec : argSpecs)
 					               justStruct (item argSpec))
        (argSigs, dg'', argSpecs') <- ana_argSpecs lgraph defl (gannos, genv, dg') justStruct argSpecs
        return (argSig : argSigs, dg'', (replaceAnnoted argSpec' argSpec) : argSpecs')
+
+
+-- | Build a diagram that extends given diagram with a node containing
+-- given signature and with edges from given set of nodes to the new node.
+extendDiagram :: Diag          -- ^ the diagram to be extended
+	      -> [DiagNodeSig] -- ^ the nodes which should be linked to the new node
+	      -> NodeSig       -- ^ the signature with which the new node should be labelled
+	      -> IOResult (DiagNodeSig, Diag)
+-- ^ returns the new node and the extended diagram
+extendDiagram diag srcNodes newNodeSig = 
+  do let nodeContents = DiagNode {dn_sig = newNodeSig}
+	 [node] = newNodes 0 diag
+	 diag' = insNode (node, nodeContents) diag
+	 inslink diag dns = do d <- diag
+			       case dns of
+			            Empty_node _ -> return d
+				    Diag_node_sig n _ -> return (insEdge (n, node, DiagLink) d)
+     diag'' <- foldl inslink (return diag') srcNodes
+     return (Diag_node_sig node newNodeSig, diag'') 
