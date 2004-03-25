e7d13921362412a57c88a7be7e257b74cb9122cc
Christian Maeder <Christian.Maeder@dfki.de>
2004-03-25 16:26:47 +0000

    simplified printing git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@2326 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Static/DotGraph.hs
+++ Static/DotGraph.hs
@@ -15,9 +15,9 @@ module Static.DotGraph where
 
 import Common.Lib.Graph
 import Static.DevGraph
-import Syntax.Print_HetCASL
+import Common.PrettyPrint
 
-
+edgeAttribute :: DGLinkType -> String
 edgeAttribute LocalDef = " [style=bold]"
 edgeAttribute GlobalDef = " [style=bold]"
 edgeAttribute HidingDef = " [style=bold]"
@@ -32,14 +32,18 @@ showNode :: DGraph -> Node -> String
 showNode dg n = 
   case get_dgn_name (lab' $ context n dg) of
     Nothing -> "N"++show n
-    Just x -> show (printText0_eGA x)
+    Just x -> showPretty x ""
 
+dotEdge :: DGraph -> (Node, Node, DGLinkLab) -> String
 dotEdge dg (n1,n2,link) = 
-  showNode dg n1 ++ " -> " ++ showNode dg n2++ edgeAttribute (dgl_type link) ++ ";"
+  showNode dg n1 ++ " -> " ++ showNode dg n2
+	       ++ edgeAttribute (dgl_type link) ++ ";"
     
+nodeAttribute :: DGNodeLab -> String
 nodeAttribute (DGNode _ _ _ _) = ""
 nodeAttribute (DGRef _ _ _) =  " [shape=box]"
 
+dotNode :: DGraph -> (Node, DGNodeLab) -> String
 dotNode dg (n,ncontents) =
   showNode dg n ++ nodeAttribute ncontents ++ ";"
 
