175b14d84d455a1b8e56302bdc49a2296d0e9682
Klaus Luettich <luettich@informatik.uni-bremen.de>
2004-04-02 13:30:40 +0000

    small Makefile useful for moving the daily binary snapshot to the download directory. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@2351 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ utils/cp_binary.mk
@@ -0,0 +1,5 @@
+
+
+cp_binary:
+	/bin/cp hets /home/www/agbkb/forschung/formal_methods/CoFI/hets/linux/daily/hets
+	/bin/chmod g+w /home/www/agbkb/forschung/formal_methods/CoFI/hets/linux/daily/hets
