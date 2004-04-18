cbd084e49e0321158315491472aeb3695e03ec55
Maciek Makowski <maciek@mmakowski.com>
2004-04-18 13:02:58 +0000

    Changed the return types of architectural spec analysis functions from IOResult to Result git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@2406 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Static/AnalysisLibrary.hs
+++ Static/AnalysisLibrary.hs
@@ -239,7 +239,7 @@ ana_LIB_ITEM lgraph defl opts libenv gctx@(gannos, genv, dg) l
              asd@(Arch_spec_defn asn asp pos) = do
   let just_struct = analysis opts == Structured
   ioToIORes (putIfVerbose opts 1  ("Analyzing arch spec " ++ showPretty asn ""))
-  (archSig, dg', asp') <- ana_ARCH_SPEC lgraph defl gctx l just_struct (item asp)
+  (archSig, dg', asp') <- resToIORes (ana_ARCH_SPEC lgraph defl gctx l just_struct (item asp))
   let asd' = Arch_spec_defn asn (replaceAnnoted asp' asp) pos
       gctx' = (gannos, genv, dg')
   if Map.member asn genv 
@@ -260,7 +260,7 @@ ana_LIB_ITEM lgraph defl opts libenv gctx@(gannos, genv, _) l
              usd@(Unit_spec_defn usn usp pos) = do
   let just_struct = analysis opts == Structured
   ioToIORes (putIfVerbose opts 1 ("Analyzing unit spec " ++ showPretty usn ""))
-  (unitSig, dg', usp') <- ana_UNIT_SPEC lgraph defl gctx l just_struct (EmptyNode defl) usp
+  (unitSig, dg', usp') <- resToIORes (ana_UNIT_SPEC lgraph defl gctx l just_struct (EmptyNode defl) usp)
   let usd' = Unit_spec_defn usn usp' pos
   if Map.member usn genv 
      then
