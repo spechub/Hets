7d2a64e9f65c7d5de7f28d13f7610b0c86f2709e
Klaus Luettich <luettich@informatik.uni-bremen.de>
2004-04-19 10:54:37 +0000

    added case for EnvOut to avoid wrong error message git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@2408 cec4b9c1-7d33-0410-9eda-942365e851bb

--- hetcats/WriteFn.hs
+++ hetcats/WriteFn.hs
@@ -54,6 +54,8 @@ write_LIB_DEFN ga file opt ld = sequence_ $ map write_type $ outtypes opt
             PrettyOut PrettyLatex -> do
                 putIfVerbose opt 3 ("Generating OutType: " ++ (show t))
                 write_casl_latex opt ga (casl_latex_filename file opt) ld
+	    EnvOut -> return () -- implemented in hets.hs 
+				-- (requires environment)
             _ -> do putStrLn ( "Error: the OutType \"" ++ 
                         show t ++ "\" is not implemented")
                     return ()
