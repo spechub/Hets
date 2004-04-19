4c39a1007edcbd0b65d598d468e2a26b80380a58
Till Mossakowski <mossakow@iws.cs.uni-magdeburg.de>
2004-04-19 12:06:55 +0000

    __* empty log message __* git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@2410 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ Modal/AS_Modal.der.hs
@@ -0,0 +1,53 @@
+{-
+Module      :  $Header$
+Copyright   :  (c) Till Mossakowski, Wiebke Herding and Uni Bremen 2003
+Licence     :  All rights reserved.
+
+Maintainer  :  hets@tzi.de
+Stability   :  provisional
+Portability :  portable
+
+  Abstract syntax for modal logic extension of CASL
+  Only the added syntax is specified
+-}
+
+module Modal.AS_Modal where
+
+import Common.Id
+import Common.AS_Annotation 
+import CASL.AS_Basic_CASL
+
+-- DrIFT command
+{-! global: UpPos !-}
+
+data M_BASIC_SPEC =
+     M_Basic_spec [Annoted (BASIC_ITEMS M_BASIC_ITEM () M_FORMULA)]
+		  deriving (Show,Eq)
+
+data M_BASIC_ITEM =
+	         Flexible_Op_items [Annoted (OP_ITEM M_FORMULA)] [Pos]
+		 -- pos: op, semi colons
+	       | Flexible_Pred_items [Annoted (PRED_ITEM M_FORMULA)] [Pos]
+		 -- pos: pred, semi colons
+             deriving (Eq, Show)
+
+data M_FORMULA = 
+	       Box (Either Id (TERM M_FORMULA)) (FORMULA M_FORMULA) [Pos]
+               -- The identifier and the term specify the kind of the modality
+	       -- pos: "[]"	    
+	     | Diamond (Either Id (TERM M_FORMULA)) (FORMULA M_FORMULA) [Pos]
+               -- The identifier and the term specify the kind of the modality
+               -- pos: "<>"
+             deriving (Eq, Show)
+
+instance PrettyPrint M_FORMULA where
+    printText0 ga (Box t f _) = 
+       ptext "[" <> pprintModality ga t <+> ptext"]" <+> printText0 ga f
+    printText0 ga (Diamond t f _) = 
+       ptext "<" <> pprintModality ga t <+> ptext">" <+> printText0 ga f
+
+pprintModality :: GlobalAnnos -> Either Id (TERM M_FORMULA) -> Doc
+pprintModality ga (Left ident) = 
+  if show ident=="empty" then empty
+     else printText0 ga ident
+pprintModality ga (Right t) = printText0 ga t
