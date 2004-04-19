6c9e4e723bbd13835c967b1ca4237e8cda92d6b2
Christian Maeder <Christian.Maeder@dfki.de>
2004-04-19 16:31:56 +0000

    fixed AS git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@2418 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Modal/AS_Modal.der.hs
+++ Modal/AS_Modal.der.hs
@@ -18,40 +18,78 @@ import Common.AS_Annotation
 import Common.GlobalAnnotations
 import CASL.AS_Basic_CASL
 import CASL.Print_AS_Basic
+import Common.Keywords
 import Common.Lib.Pretty
 import Common.PrettyPrint
+import Common.PPUtils
 
 -- DrIFT command
 {-! global: UpPos !-}
 
-type M_BASIC_SPEC = BASIC_SPEC M_BASIC_ITEM () M_FORMULA
+type M_BASIC_SPEC = BASIC_SPEC M_BASIC_ITEM M_SIG_ITEM M_FORMULA
 
-data M_BASIC_ITEM =
-	         Rigid_Op_items [Annoted (OP_ITEM M_FORMULA)] [Pos]
+data M_BASIC_ITEM = Mod_decl [Annoted MOD_DECL] [Pos]
+
+data MOD_DECL = Simple_mod_decl SIMPLE_ID
+	      | Term_mod_decl SORT
+		deriving (Eq, Show)
+
+data RIGOR = Rigid | Flexible deriving (Eq, Show)
+
+data M_SIG_ITEM =
+	  Rigid_op_items RIGOR [Annoted (OP_ITEM M_FORMULA)] [Pos]
 		 -- pos: op, semi colons
-	       | Rigid_Pred_items [Annoted (PRED_ITEM M_FORMULA)] [Pos]
+	| Rigid_pred_items RIGOR [Annoted (PRED_ITEM M_FORMULA)] [Pos]
 		 -- pos: pred, semi colons
              deriving (Eq, Show)
 
-type ModalFORMULA = FORMULA M_FORMULA
+-- type ModalFORMULA = FORMULA M_FORMULA
+
+data MODALITY = Simple_mod SIMPLE_ID | Term_mod (TERM M_FORMULA)
+             deriving (Eq, Show)
 
 data M_FORMULA = 
-	       Box (Either Id (TERM M_FORMULA)) () [Pos]
+	       Box MODALITY (FORMULA M_FORMULA) [Pos]
                -- The identifier and the term specify the kind of the modality
 	       -- pos: "[]"	    
-	     | Diamond (Either Id (TERM M_FORMULA)) (ModalFORMULA) [Pos]
+	     | Diamond MODALITY (FORMULA M_FORMULA) [Pos]
                -- The identifier and the term specify the kind of the modality
                -- pos: "<>"
              deriving (Eq, Show)
 
+
+modalityS, flexibleS, rigidS, termS :: String 
+modalityS = "modality"
+flexibleS = "flexible"
+rigidS = "rigid"
+termS = "term"
+
+instance PrettyPrint MOD_DECL where
+    printText0 ga (Simple_mod_decl i) = 
+	ptext modalityS <+> printText0 ga i
+    printText0 ga (Term_mod_decl s) = 
+	ptext termS <+> ptext modalityS <+> printText0 ga s
+
+instance PrettyPrint RIGOR where
+    printText0 _ Rigid = ptext rigidS
+    printText0 _ Flexible = ptext flexibleS
+
+instance PrettyPrint M_SIG_ITEM where
+    printText0 ga (Rigid_op_items r ls _) =
+	hang (printText0 ga r <+> ptext opS <> pluralS_doc ls) 4 $ 
+	     semiAnno_text ga ls
+    printText0 ga (Rigid_pred_items r ls _) =
+	hang (printText0 ga r <+> ptext predS <> pluralS_doc ls) 4 $ 
+	     semiAnno_text ga ls
+
 instance PrettyPrint M_FORMULA where
     printText0 ga (Box t f _) = 
-       ptext "[" <> pprintModality ga t <+> ptext"]" <+> printText0 ga f
+       ptext "[" <> printText0 ga t <+> ptext"]" <+> printText0 ga f
     printText0 ga (Diamond t f _) = 
-       ptext "<" <> pprintModality ga t <+> ptext">" <+> printText0 ga f
+       ptext "<" <> printText0 ga t <+> ptext">" <+> printText0 ga f
 
-pprintModality :: GlobalAnnos -> Either Id (TERM M_FORMULA) -> Doc
-pprintModality ga (Left ident) = 
-  if show ident=="empty" then empty
-     else printText0 ga ident
-pprintModality ga (Right t) = printText0 ga t
+instance PrettyPrint MODALITY where
+    printText0 ga (Simple_mod ident) = 
+	if tokStr ident == "empty" then empty
+	   else printText0 ga ident
+    printText0 ga (Term_mod t) = printText0 ga t
