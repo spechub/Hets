2664434a58ddc2e7084c54d649216004d29d8062
Christian Maeder <Christian.Maeder@dfki.de>
2004-05-14 16:56:19 +0000

    slightly improved pretty print git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@2612 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Haskell/PrintModuleInfo.hs
+++ Haskell/PrintModuleInfo.hs
@@ -13,36 +13,38 @@ pretty printing Haskell's ModuleInfo
 module Haskell.PrintModuleInfo where
 
 import Common.PrettyPrint
+import Common.PPUtils
 import Common.Lib.Pretty as Pretty
 
 import Haskell.Hatchet.MultiModuleBasics
 import Haskell.Hatchet.AnnotatedHsSyn
+import Haskell.Hatchet.Env
+import Haskell.Hatchet.PPrint
 
 instance PrettyPrint ModuleInfo where
-  printText0 _ modInfo = text "Module name" 
-                                <+> text (show (moduleName modInfo)) 
-                                <> comma
-                          $+$ text "Variable Assumptions" 
-                                <+> text (show (varAssumps modInfo))
-                                <> comma
-                          $+$ text "Data Constructor Assumptions" 
-                                <+> text (show (dconsAssumps modInfo))
-                                <> comma
-                          $+$ text "Class Hierarchy" 
-                                <+> text (show (classHierarchy modInfo))
-                                <> comma
-                          $+$ text "Kinds" 
-                                <+> text (show (kinds modInfo))
-                                <> comma
-                          $+$ text "Synonyms" 
-                                <+> text (show (synonyms modInfo))
-                                <> comma
-                          $+$ text "Infix Declarations" 
-                                <+> text (show (infixDecls modInfo))
-                                <> comma
-                          $+$ text "Type Constructor Members" 
-                                <+> text (show (tyconsMembers modInfo))
-                                <> comma
+  printText0 ga modInfo = text "module" 
+                                <+> pprint (moduleName modInfo)
+                                <+> text "where" 
+                          $$ text "-- Functions" 
+                          $$ pprintEnv (varAssumps modInfo)
+                          $$ text "-- Constructors" 
+			  $$ pprintEnv (dconsAssumps modInfo)
+                          $$ text "-- Class Hierarchy" 
+ 			  $$ pprintEnv (classHierarchy modInfo)
+                          $$ text "-- Kinds" 
+                          $$ pprintEnv (kinds modInfo)
+                          $$ text "-- Synonyms" 
+                          $$ vcat (map (printText0 ga) $ synonyms modInfo)
+                          $$ text "-- Infix Declarations" 
+                          $$ vcat (map (printText0 ga) $ infixDecls modInfo)
+                          $$ text "-- Type Constructor Members" 
+                          $$ vcat (map ( \ (t, cs) -> 
+				printText0 ga t 
+				<+> char '='
+				<+> listSep_text (space<>char '|') ga cs)
+				   $ tyconsMembers modInfo)
 
 instance PrettyPrint AHsDecl where
     printText0 _ = ptext . show
+instance PrettyPrint AHsName where
+    printText0 _ = pprint
