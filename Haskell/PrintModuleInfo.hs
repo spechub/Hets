cf2f0d5e1c64137546fafbddfc3b17dcef205812
Christian Maeder <Christian.Maeder@dfki.de>
2004-05-18 13:30:56 +0000

    prettier git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@2639 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Haskell/PrintModuleInfo.hs
+++ Haskell/PrintModuleInfo.hs
@@ -14,12 +14,13 @@ module Haskell.PrintModuleInfo where
 
 import Common.PrettyPrint
 import Common.PPUtils
-import Common.Lib.Pretty as Pretty
+import Common.Lib.Pretty
 
 import Haskell.Hatchet.MultiModuleBasics
 import Haskell.Hatchet.AnnotatedHsSyn
 import Haskell.Hatchet.Env
 import Haskell.Hatchet.PPrint
+import Haskell.Hatchet.AHsPretty as AP
 
 instance PrettyPrint ModuleInfo where
   printText0 ga modInfo = text "module" 
@@ -28,23 +29,23 @@ instance PrettyPrint ModuleInfo where
                           $$ text "-- Functions" 
                           $$ pprintEnv (varAssumps modInfo)
                           $$ text "-- Constructors" 
-			  $$ pprintEnv (dconsAssumps modInfo)
-                          $$ text "-- Class Hierarchy" 
- 			  $$ pprintEnv (classHierarchy modInfo)
-                          $$ text "-- Kinds" 
-                          $$ pprintEnv (kinds modInfo)
+                          $$ pprintEnv (dconsAssumps modInfo)
+--                          $$ text "-- Class Hierarchy" 
+--                          $$ pprintEnv (classHierarchy modInfo)
+--                          $$ text "-- Kinds" 
+--                          $$ pprintEnv (kinds modInfo)
                           $$ text "-- Synonyms" 
                           $$ vcat (map (printText0 ga) $ synonyms modInfo)
                           $$ text "-- Infix Declarations" 
                           $$ vcat (map (printText0 ga) $ infixDecls modInfo)
                           $$ text "-- Type Constructor Members" 
                           $$ vcat (map ( \ (t, cs) -> 
-				printText0 ga t 
-				<+> char '='
-				<+> listSep_text (space<>char '|') ga cs)
-				   $ tyconsMembers modInfo)
+                                printText0 ga t 
+                                <+> char '='
+                                <+> listSep_text (space<>char '|') ga cs)
+                                   $ tyconsMembers modInfo)
 
 instance PrettyPrint AHsDecl where
-    printText0 _ = ptext . show
+    printText0 _ = text . AP.render . ppAHsDecl
 instance PrettyPrint AHsName where
     printText0 _ = pprint
