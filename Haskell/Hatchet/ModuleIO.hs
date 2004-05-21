7a719d0af63c4b3e6dc1f69f300f8b4b62275be0
Christian Maeder <Christian.Maeder@dfki.de>
2004-05-21 09:57:03 +0000

    removed warnings git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@2667 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Haskell/Hatchet/ModuleIO.hs
+++ Haskell/Hatchet/ModuleIO.hs
@@ -1,4 +1,4 @@
-{-------------------------------------------------------------------------------
+{------------------------------------------------------------------------------
 
         Copyright:              The Hatchet Team (see file Contributors)
 
@@ -13,7 +13,7 @@
 
         Notes:                  See the file License for license information
 
--------------------------------------------------------------------------------}
+------------------------------------------------------------------------------}
 
 module Haskell.Hatchet.ModuleIO(
     writeModuleInfo,
@@ -23,21 +23,14 @@ module Haskell.Hatchet.ModuleIO(
   toString,       -- eventually we shouldn't export these
     ) where
 
-import List                     (intersect)
 import Haskell.Hatchet.MultiModuleBasics
 import Haskell.Hatchet.MultiModule
 import Haskell.Hatchet.PlainShowParse
-import Haskell.Hatchet.Env                      (Env, listToEnv)
-import Haskell.Hatchet.KindInference            (KindEnv)
-import Haskell.Hatchet.HsSyn
 import Haskell.Hatchet.FiniteMaps               (toListFM, listToFM)
 import Haskell.Hatchet.AnnotatedHsSyn  
-import Haskell.Hatchet.Utils                    (getUnQualName, qualifyName)
-import Haskell.Hatchet.Rename                   (getAHsNamesAndASrcLocsFromAHsDecl, unRename)
-import Haskell.Hatchet.Representation           (Scheme)
-import Haskell.Hatchet.Class                    (ClassHierarchy)
+import Haskell.Hatchet.Utils                    (qualifyName)
 
---------------------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
 readModuleInfo :: AHsModule -> IO ModuleInfo
 readModuleInfo (AHsModule _ _ imports _) 
@@ -47,16 +40,18 @@ readModuleInfo (AHsModule _ _ imports _)
 --    where
     -- take one declaration and build a ModuleInfo from it
 readOneImportSpec :: AHsImportDecl -> IO ModuleInfo
-readOneImportSpec (AHsImportDecl _ mod _ _ maybeListOfIdents)
+readOneImportSpec (AHsImportDecl _ m _ _ maybeListOfIdents)
         = do
-          moduleInfoUnParsed <- readFile (modToFilePath mod)
+          moduleInfoUnParsed <- readFile (modToFilePath m)
           let modInfoUnFiltered :: ModuleInfo
               modInfoUnFiltered = fromString moduleInfoUnParsed
           case maybeListOfIdents of
-               Nothing -> return modInfoUnFiltered -- we're not imposing restrictions
+               Nothing -> return modInfoUnFiltered 
+	               -- we're not imposing restrictions
                Just (_, importSpecs) -> 
-                   let ans = filterModuleInfo mod modInfoUnFiltered $
-                             expandDotsInTyCons mod (tyconsMembers modInfoUnFiltered) $
+                   let ans = filterModuleInfo m modInfoUnFiltered $
+                             expandDotsInTyCons m 
+				    (tyconsMembers modInfoUnFiltered) $
                              map importSpecToExportSpec importSpecs
                    in  do return ans
 
@@ -66,33 +61,41 @@ writeModuleInfo alternateFileName (AHsModule amod exports _ tree) modInfo
     = do
 
       -- figure out what needs to go into the file
-      let topLevel :: [AHsExportSpec]    -- this is the maximum that we can export
+      let topLevel :: [AHsExportSpec]    
+          -- this is the maximum that we can export
           topLevel = getTopLevelBindings tree
         
           actualExports :: [AHsExportSpec]
           actualExports = 
-              case exports of Nothing -> topLevel  -- no restrictions
-                              Just list -> intersectAHsExportSpecs topLevel list
+              case exports of 
+			   Nothing -> topLevel  -- no restrictions
+			   Just list -> intersectAHsExportSpecs topLevel list
 
       let filteredVarAssumps 
              = filterSchemes amod actualExports $ toListFM $ varAssumps modInfo
           filteredDConsAssumps 
-             = filterSchemes amod actualExports $ toListFM $ dconsAssumps modInfo
+             = filterSchemes amod actualExports $ toListFM 
+	       $ dconsAssumps modInfo
           newKinds
-             = filterInOnlyThisModuleAndQualifyIfNecessary $ toListFM $ kinds modInfo
+             = filterInOnlyThisModuleAndQualifyIfNecessary $ toListFM 
+	       $ kinds modInfo
           newClassHierarchy
-             = filterInOnlyThisModuleAndQualifyIfNecessary $ toListFM $ classHierarchy modInfo
+             = filterInOnlyThisModuleAndQualifyIfNecessary $ toListFM 
+	       $ classHierarchy modInfo
 
-          {- to get the new kind table just include the kinds from the kind table
-             returned and qualify if necessary (and don't include ones from other
-             modules). -}
-          -- this is Bryn's attempt to beat Bernie in the longest identifier competition 
-          filterInOnlyThisModuleAndQualifyIfNecessary :: [(AHsName, a)] -> [(AHsName, a)]
+          {- to get the new kind table just include the kinds from the
+             kind table returned and qualify if necessary (and don't
+             include ones from other modules). This is Bryn's
+             attempt to beat Bernie in the longest identifier
+             competition -}
+
+          filterInOnlyThisModuleAndQualifyIfNecessary 
+	      :: [(AHsName, a)] -> [(AHsName, a)]
           filterInOnlyThisModuleAndQualifyIfNecessary name_pairs
              = [ (qualifyName amod ahsname, kind)
                     | (ahsname, kind) <- name_pairs,
                       case ahsname of (AUnQual _) -> True
-                                      (AQual mod _) -> mod == amod
+                                      (AQual m _) -> m == amod
                ]
                  
           writableModInfo 
@@ -110,18 +113,3 @@ writeModuleInfo alternateFileName (AHsModule amod exports _ tree) modInfo
                  Just f  -> f
 
       writeFile intermediateFilePath stuffForFile
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
