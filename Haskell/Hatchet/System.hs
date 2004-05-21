35fef78198e451e957ee655ca4b2c8348c094f33
Christian Maeder <Christian.Maeder@dfki.de>
2004-05-21 09:44:53 +0000

    replaced by library modules git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@2666 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Haskell/Hatchet/System.hs
+++
@@ -1,9 +0,0 @@
-module Haskell.Hatchet.System (
-    module System.Exit,
-    module System.Environment,
-    module System.Cmd
-  ) where
-
-import System.Exit
-import System.Environment
-import System.Cmd
