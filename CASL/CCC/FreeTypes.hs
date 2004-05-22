df7df5721afc394a81472be37e8495e260a46a08
Mingyi Liu <xinga@tzi.de>
2004-05-22 23:05:57 +0000

    __* empty log message __* git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@2680 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CASL/CCC/FreeTypes.hs
+++ CASL/CCC/FreeTypes.hs
@@ -21,9 +21,44 @@ import qualified Common.Lib.Rel as Rel
 import CASL.CCC.SignFuns
 
 checkFreeType :: Morphism f e m -> [FORMULA f] -> Maybe Bool
-checkFreeType m fs=
-
+checkFreeType m fs=if any (\s->not $ elem s srts) sorts then Nothing
+                   else if all (\s->not $ elem s f_Inhabited) sorts then Just False
+                        else if elem Nothing $ map leadingSym fs then Nothing
+                             else Just True
    where sig = imageOfMorphism m
-         sorts=sortSet sig 
-    
-      
+         sorts= Set.toList (sortSet sig)
+         fconstrs= concat $ map fc fs
+         fc f= case f of
+                     Sort_gen_ax constrs True -> constrs
+                     _->[]
+         (srts,_,_)=recover_Sort_gen_ax fconstrs 
+         f_Inhabited=inhabited fconstrs
+         leadingSym f=case f of
+                       Quantification Universal _ f' _  -> leadingSym f'
+                       Implication _ f' _ _ -> leadingSym f'
+                       Equivalence f' _ _ -> leadingSym f'
+                       Predication predS _ _ -> return (Right predS)
+                       Strong_equation t1 t2 _ -> case t1 of
+                                                   Application opS _ _ -> case t2 of
+                                                                           Sorted_term _ s _ ->
+                                                                             if elem s sorts then return (Left opS)
+                                                                             else Nothing
+                                                                           _ -> Nothing
+                                                   _ -> Nothing
+                       _ -> Nothing                                           
+{-         
+leadingSym :: FORMULA f -> Maybe(Either OP_SYMB PRED_SYMB)
+leadingSym f= case f of
+                Quantification Universal _ f' _  -> leadingSym f'
+                Implication _ f' _ _ -> leadingSym f'
+                Equivalence f' _ _ -> leadingSym f' 
+                Predication predS _ _ -> return (Right predS)
+                Strong_equation t1 t2 _ -> case t1 of
+                                             Application opS _ _ -> case t2 of
+                                                                      Sorted_term _ s _ ->
+                                                                        if elem s sorts then return (Left opS)
+                                                                        else Nothing
+                                                                      _ -> Nothing                  
+                                             _ -> Nothing
+                _ -> Nothing
+-}
