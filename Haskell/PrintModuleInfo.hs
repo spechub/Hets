b71db6323c9a3c44625d099911ddd7e5c0199758
Christian Maeder <Christian.Maeder@dfki.de>
2004-05-26 20:44:51 +0000

    only show defs git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@2724 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Haskell/PrintModuleInfo.hs
+++ Haskell/PrintModuleInfo.hs
@@ -26,24 +26,7 @@ instance PrettyPrint ModuleInfo where
   printText0 ga modInfo = text "module" 
                                 <+> pprint (moduleName modInfo)
                                 <+> text "where" 
-                          $$ text "-- Functions" 
                           $$ pprintEnv (varAssumps modInfo)
-                          $$ text "-- Constructors" 
-                          $$ pprintEnv (dconsAssumps modInfo)
---                          $$ text "-- Class Hierarchy" 
---                          $$ pprintEnv (classHierarchy modInfo)
---                          $$ text "-- Kinds" 
---                          $$ pprintEnv (kinds modInfo)
-                          $$ text "-- Synonyms" 
-                          $$ vcat (map (printText0 ga) $ synonyms modInfo)
-                          $$ text "-- Infix Declarations" 
-                          $$ vcat (map (printText0 ga) $ infixDecls modInfo)
-                          $$ text "-- Type Constructor Members" 
-                          $$ vcat (map ( \ (t, cs) -> 
-                                printText0 ga t 
-                                <+> char '='
-                                <+> listSep_text (space<>char '|') ga cs)
-                                   $ tyconsMembers modInfo)
 
 instance PrettyPrint AHsDecl where
     printText0 _ = text . AP.render . ppAHsDecl
