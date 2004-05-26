bab1df035a5b5cb9087c445ecbcb34359f9e4387
Sonja Gröning <sonja@informatik.uni-bremen.de>
2004-05-26 15:40:25 +0000

    Moved all instances concerning (Pretty) Printing to IsaPrint.hs git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@2716 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Isabelle/IsaSign.hs
+++ Isabelle/IsaSign.hs
@@ -16,11 +16,6 @@ module Isabelle.IsaSign where
 
 import qualified Common.Lib.Map as Map
 import Common.Id
-import Common.PrettyPrint
-import Common.Lib.Pretty
-
-bracketize :: Bool -> String -> String
-bracketize b s = if b then "("++s++")" else s
 
 ---------------- from src/Pure/Syntax/syntax.ML -------------------
 
@@ -41,15 +36,6 @@ data Typ = Type (String,[Typ])
              | TFree (String, Sort)
              -- TVar  (Indexname, Sort)
            deriving (Eq, Ord)
-
-instance Show Typ where
-  show = showTyp 1000
-
-showTyp _ (Type (t,[])) = t
-showTyp pri (Type ("fun",[s,t])) = 
-  bracketize (pri<=10) (showTyp 10 s ++ "=>" ++ showTyp 11 t)
-showTyp pri (Type (t,args)) = "("++concat (map ((" "++).show) args)++")"++t
-showTyp pri (TFree (v,_)) = v
 
 infix -->
 infix --->
@@ -89,24 +75,6 @@ data Term =
       | App Term  Term
       deriving (Eq, Ord)
 
-instance Show Term where
-  show = showTerm 1000
-
-showTerm :: Int -> Term -> String
-showTerm pri (Const (c,_)) = c
-showTerm pri (Free (v,_)) = v
-showTerm pri (Abs (v,_,t)) = "%"++showTerm pri v++" . "++showTerm pri t
-showTerm pri (Abs (v,_,t)) = "%"++showTerm pri v++" . "++showTerm pri t
-showTerm pri (Const ("All",_) `App` Abs (v,ty,t)) = 
-  bracketize (pri<=10) ("! "++showTerm pri v++" :: "++show ty++" . "++showTerm pri t)
-showTerm pri (Const ("Ex",_) `App` Abs (v,_,t)) = 
-  bracketize (pri<=10) ( "? "++showTerm pri v++" . "++showTerm pri t)
-showTerm pri (Const ("Ex1",_) `App` Abs (v,_,t)) = 
-  bracketize (pri<=10) ( "?! "++showTerm pri v++" . "++showTerm pri t)
-showTerm pri (t1 `App` t2) = 
-  bracketize (pri<=10) (showTerm 11 t1 ++ " " ++ showTerm 10 t2)
-
-
 data Sentence = Sentence { senTerm :: Term
                            }
 instance Eq Sentence where
@@ -114,15 +82,6 @@ instance Eq Sentence where
 
 instance Ord Sentence where
   compare s1 s2 = compare (senTerm s1) (senTerm s2)
-
-instance Show Sentence where
-  show s = show (senTerm s)
-
-instance PrettyPrint Sentence where
-    printText0 _ = ptext . show
-
-instance PrintLaTeX Sentence where
-    printLatex0 = printText0
 
 
 -------------------- from src/Pure/sorts.ML ------------------------
@@ -184,17 +143,6 @@ emptyTypeSig = TySg {
     abbrs = Map.empty,
     arities = Map.empty }
 
-instance Show TypeSig where
-  show tysig =
-    if Map.isEmpty (tycons tysig) then ""
-     else Map.foldWithKey showTycon "" (tycons tysig) 
-     where showTycon t arity rest =
-             "typedecl "++
-             (if arity>0 then "("++concat (map ((" 'a"++).show) [1..arity])++")"
-               else "") 
-            ++ show t
-            ++"\n"++rest
-
 -------------------- from src/Pure/sign.ML ------------------------
 
 data Sign = Sign { baseSig :: String, -- like Pure, HOL, Main etc.
@@ -211,31 +159,3 @@ emptySign = Sign { baseSig = "Pure",
                    constTab = Map.empty,
                    dataTypeTab = [],
                    syn = () }
-
-instance Show Sign where
-  show sig =
-    baseSig sig ++":\n"++
-    shows (tsig sig) (showDataTypeDefs (dataTypeTab sig))
-      ++ (showsConstTab (constTab sig))
-    where
-    showsConstTab tab =
-     if Map.isEmpty tab then ""
-      else "consts\n" ++ Map.foldWithKey showConst "" tab
-    showConst c t rest = show c ++ " :: " ++ "\"" ++ show t ++ "\"\n" ++ rest
-    showDataTypeDefs dtDefs = concat $ map showDataTypeDef dtDefs
-    showDataTypeDef [] = ""
-    showDataTypeDef (dt:dts) = 
-       "datatype " ++ showDataType dt
-       ++ (concat $ map (("and "++) . showDataType) dts)
-    showDataType (t,op:ops) =
-       show t ++ " = " ++ showOp op 
-       ++ (concat $ map ((" | "++) . showOp) ops)
-    showOp (opname,args) =
-       opname ++ (concat $ map (((" ")++) . show) args)
-
-
-instance PrettyPrint Sign where
-    printText0 _ = ptext . show
-
-instance PrintLaTeX Sign where
-    printLatex0 = printText0
