1019115b4ecad6cd2ab2098ce160b8f485b31b37
Heng Jiang <jiang@tzi.de>
2004-05-29 13:32:46 +0000

    the graph is now with round nodes, unique edge and the information Menu git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@2750 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GUI/ShowLogicGraph.hs
+++ GUI/ShowLogicGraph.hs
@@ -14,14 +14,13 @@ import Channels
 
 --  data structure
 import Comorphisms.LogicGraph
-import Comorphisms.LogicList
 import Logic.Grothendieck
 import Logic.Logic
 import Logic.Comorphism
--- import Logic.Provers
+import Logic.Prover
 
 import qualified Common.Lib.Map as Map
-import Concurrent
+-- import Concurrent
 import Maybe
 import Debug(debug)
 import Events
@@ -50,13 +49,16 @@ showLogicGraph
        
        (killChannel :: Channel ()) <- newChannel   
 
-       let logicNodeMenu = LocalMenu(Button "Type" (disp "Type"))  -- ??
+       let logicNodeMenu = LocalMenu(Menu (Just "Info") 
+               [Button "Parser" (\lg -> createTextDisplay ("Parsers of " ++ showTitle lg) (showParse lg) [size(80,40)]),
+                Button "Sublogic" (\lg -> createTextDisplay ("Sublogics of " ++ showTitle lg) (showSubLogic lg) [size(80,40)]),
+                Button "Prover" (\lg -> createTextDisplay ("Provers of " ++ showTitle lg) (showProver lg) [size(80,40)]),
+                Button "Cons_checker" (\lg -> createTextDisplay ("Cons_Checkers of " ++ showTitle lg) (showConsChecker lg) [size(80,40)])])
+           
            logicNodeTypeParms = 
                logicNodeMenu $$$
-               Triangle $$$
-               ValueTitle (\l -> case l of
-                                   Logic lid -> 
-                                    return (language_name lid)) $$$
+               Ellipse $$$
+               ValueTitle (return . showTitle) $$$
                Color "green" $$$
                nullNodeParms
       
@@ -69,7 +71,7 @@ showLogicGraph
        let namesAndNodes = Map.fromList (zip (Map.keys(logics logicGraph)) nodeList)
        
            lookupLogic(Logic lid) = fromJust (Map.lookup (language_name lid) namesAndNodes)
-           logicArcMenu = LocalMenu(Button "Type" (disp "Type"))
+           logicArcMenu = LocalMenu(Button "Como" (disp "Como"))
            normalArcTypeParms = logicArcMenu $$$
                                 Color "black" $$$
                                 nullArcTypeParms
@@ -91,8 +93,11 @@ showLogicGraph
                 case i of
                   Comorphism cid -> 
                     newArc logicG inclArcType "ArcType" (lookupLogic(Logic(sourceLogic cid))) (lookupLogic(Logic(targetLogic cid)))) 
-       arcList1 <- mapM insertComo (Map.elems(comorphisms logicGraph))
-       arcList2 <- mapM insertIncl (Map.elems(inclusions logicGraph)) 
+
+       arcList1 <- mapM insertIncl (Map.elems(inclusions logicGraph))
+       arcList2 <- mapM insertComo  [rest | rest <- Map.elems(comorphisms logicGraph), not (rest `elem` (Map.elems(inclusions logicGraph)))] 
+                  --(drop (length arcList1) (Map.elems (comorphisms logicGraph)))
+       
        
        redraw logicG
        
@@ -104,3 +109,41 @@ showLogicGraph
                
          +> (destroyed logicG)
         )
+
+  where showParse l = 
+	    case l of 
+               Logic lid -> let s1 =  case parse_basic_spec lid of
+				      Just _  -> "Parser for basic specifications.\n"
+				      Nothing -> ""
+				s2 =  case parse_symb_items lid of
+				      Just _ ->  "Parser for symbol lists.\n"
+				      Nothing -> ""
+				s3 =  case parse_symb_map_items lid of
+				      Just _ ->  "Parser for symbol maps.\n"
+				      Nothing -> ""
+				s4 =  case parse_sentence lid of
+				      Just _ ->  "Parser for sentences.\n"
+				      Nothing -> ""
+				s5 =  case basic_analysis lid of
+				      Just _ ->  "Analysis of basic specifications.\n"
+                                      Nothing -> ""
+                                s6 =  case data_logic lid of	       
+				      Just _ ->  "is a process logic.\n" 
+				      Nothing -> ""
+			     in  (s1 ++ s2 ++ s3 ++ s4 ++ s5 ++ s6)
+	 
+        showSubLogic l = 
+            case l of
+              Logic lid -> unlines (map unwords $ 
+                                map (sublogic_names lid) (all_sublogics lid))
+        showTitle l = 
+            case l of
+              Logic lid -> language_name lid
+
+        showProver l = 
+            case l of
+              Logic lid -> unlines $ map prover_name (provers lid)
+
+        showConsChecker l = 
+            case l of
+              Logic lid -> unlines $ map cons_checker_name (cons_checkers lid)
