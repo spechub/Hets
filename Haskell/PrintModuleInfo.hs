c575c685ea05f27231c6c5243463e151ce10f838
Christian Maeder <Christian.Maeder@dfki.de>
2004-06-02 15:36:51 +0000

    only print a dummy header git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@2805 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Haskell/PrintModuleInfo.hs
+++ Haskell/PrintModuleInfo.hs
@@ -13,20 +13,17 @@ pretty printing Haskell's ModuleInfo
 module Haskell.PrintModuleInfo where
 
 import Common.PrettyPrint
-import Common.PPUtils
 import Common.Lib.Pretty
 
 import Haskell.Hatchet.MultiModuleBasics
 import Haskell.Hatchet.AnnotatedHsSyn
-import Haskell.Hatchet.Env
 import Haskell.Hatchet.PPrint
 import Haskell.Hatchet.AHsPretty as AP
 
 instance PrettyPrint ModuleInfo where
-  printText0 ga modInfo = text "module" 
-                                <+> pprint (moduleName modInfo)
-                                <+> text "where" 
-                          $$ pprintEnv (varAssumps modInfo)
+  printText0 _ga _modInfo = text "module Dummy where"
+             $$ text "import Prelude (undefined, Show, Eq, Ord, Bool)"
+             $$ text "import MyLogic"
 
 instance PrettyPrint AHsDecl where
     printText0 _ = text . AP.render . ppAHsDecl
