769ae58dc009791fd4c56fa9152c2db64516f1e4
Christian Maeder <Christian.Maeder@dfki.de>
2004-07-07 15:32:33 +0000

    shortened implementations git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@2931 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Common/ListUtils.hs
+++ Common/ListUtils.hs
@@ -12,36 +12,29 @@ Portability :  portable
 
 module Common.ListUtils where
 
-import Data.List                ( partition )
+import Data.List                ( groupBy )
 
+-- | split list at separator elements, avoid empty sublists
 splitBy :: Eq a => a -> [a] -> [[a]]
-splitBy x [] = []
-splitBy x xs = let (l,rest) = break (==x) xs
-                 in l : case rest of
-                          []        -> []
-                          (_:rest') -> splitBy x rest' 
+splitBy x xs = let (l, r) = break (==x) xs in 
+    (if null l then [] else [l]) ++ (if null r then [] else splitBy x $ tail r)
+-- suffix "By" usually indicates a (a -> a -> Bool) argument instead of Eq
 
 -- | Divide a Set (List) into equivalence classes w.r. to eq
 equivalence_Classes         :: (a -> a -> Bool) -> [a] -> [[a]]
-equivalence_Classes _ []     = []
-equivalence_Classes eq (x:l) = xs':(equivalence_Classes eq ys)
-    where
-        (xs, ys) = partition (eq x) l
-        xs'      = (x:xs)
+equivalence_Classes = groupBy
 
 -- | Transform a list [l1,l2, ... ln] to (in sloppy notation)
 -- [[x1,x2, ... ,xn] | x1<-l1, x2<-l2, ... xn<-ln]
 permute      :: [[a]] -> [[a]]
 permute []    = [[]]
-permute [x]   = map (\y -> [y]) x
-permute (x:l) = concat (map (distribute (permute l)) x)
-    where
-        distribute perms y = map ((:) y) perms
+permute (x:l) = concatMap ((`map` permute l) . (:)) x
+-- a better name would be "combine"
 
 -- | Like 'and (zipWith p as bs)',
 -- but must return False if lengths don't match
 -----------------------------------------------------------}
 zipped_all                :: (a -> b -> Bool) -> [a] -> [b] -> Bool
 zipped_all _ []     []     = True
-zipped_all p (a:as) (b:bs) = (p a b) && (zipped_all p as bs)
+zipped_all p (a:as) (b:bs) = p a b && zipped_all p as bs
 zipped_all _  _      _     = False
