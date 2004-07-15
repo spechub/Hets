4c83c593dd73c3675ac59c063c4b9a07e9216de8
Sonja Gröning <sonja@informatik.uni-bremen.de>
2004-07-15 09:21:31 +0000

    Added new application function for total functions git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@2939 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Comorphisms/MainHC.thy
+++ Comorphisms/MainHC.thy
@@ -1,14 +1,38 @@
-theory MainHC = Main
+theory MainHC = Main:
 
 consts app :: "('a => 'b option) option => 'a option => 'b option"
-
 primrec
   "app None a = None"
-  "app (Some f) a = (case a of 
+  "app (Some f) x = (case x of 
                             None => None
-                          | Some x => f x)"
+                          | Some x' => f x')"
 
-consts  defOp :: "'a option=> bool"
+consts apt :: "('a => 'b) option => 'a option => 'b option"
+primrec
+  "apt None a = None"
+  "apt (Some f) x = (case x of 
+                            None => None
+                          | Some x' => Some (f x'))"
+
+consts pApp :: "('a => bool) option => 'a option => bool"
+primrec
+  "pApp None a = False"
+  "pApp (Some f) x = (case x of
+                             None => False
+                           | Some x' => True)"
+
+
+consts defOp :: "'a option => bool"
 primrec
   "defOp (Some x) = True"
   "defOp None     = False"
+
+
+consts pair :: "'a option => 'b option => ('a * 'b) option"
+primrec
+  "pair None a = None"
+  "pair (Some x) z = (case z of
+                             None => None
+                           | Some y => Some (x,y))"
+
+end
