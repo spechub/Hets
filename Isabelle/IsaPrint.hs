a74b97a1e9615b64664dbea33f3ac9e6e7b2f2ac
Till Mossakowski <mossakow@iws.cs.uni-magdeburg.de>
2004-08-09 11:19:46 +0000

    outerShowTerm also for nested --&gt; git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@3027 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Isabelle/IsaPrint.hs
+++ Isabelle/IsaPrint.hs
@@ -114,7 +114,7 @@ outerShowTerm :: Term -> String
 outerShowTerm (Const ("All",_) `App` Abs (v,ty,t)) = 
   outerShowQuant "!!" v ty t
 outerShowTerm (Const ("op -->",_) `App` t1 `App` t2) =
-  showTerm t1 ++ " ==> " ++ showTerm t2
+  showTerm t1 ++ " ==> " ++ outerShowTerm t2
 outerShowTerm t = showTerm t
 
 outerShowQuant :: String -> Term -> Typ -> Term -> String
