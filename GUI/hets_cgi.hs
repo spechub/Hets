bca91e67afa41275caef8d24c85c0855f5e9f626
Heng Jiang <jiang@tzi.de>
2004-08-26 09:32:30 +0000

    improved the error handler git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@3089 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GUI/hets_cgi.hs
+++ GUI/hets_cgi.hs
@@ -27,8 +27,8 @@ Portability  : non-portable
      - when the HTML-PrettyPrinting is available: generated HTML-Code 
        instead of poorly rendered ASCII has to be inserted as response
        .. ???
+     - a handler for warning message
 -}
-
 module Main where
 
 import CGI
@@ -42,6 +42,7 @@ import Static.AnalysisLibrary
 import Comorphisms.LogicGraph
 import Static.DevGraph
 import Syntax.AS_Library
+import Syntax.Parse_AS_Structured
 import Maybe
 import Random
 import IO
@@ -70,9 +71,9 @@ mainCGI =
 	       	      CGI.body $ makeForm $ page1 ("Hets " ++ hetcats_version)) 
 
 page1 :: String -> WithHTML x CGI ()
-page1 title =
+page1 title1 =
     do    
-      h1 $ text title
+      h1 $ text title1
       p (text "Enter a CASL 1.0.1 specification or library in the input zone, then press SUBMIT:")
       -- Input field
       input   <- p (makeTextarea "" (attr "rows" "22" ## attr "cols" "68"))
@@ -105,10 +106,10 @@ handle (F5 input box1 box2 box3 box4) =
 	tex  = value box3
 	achiv= value box4 
     in  do
-	random <- io $ getRandom
+	random1 <- io $ getRandom
 	processID <- io $ getProcessID
 	let outputfile = "/home/www/cofi/hets-tmp/result" ++ (show processID) ++
-			    (show random)
+			    (show random1)
 	res <- io $ anaInput str (tree, env, tex, achiv) outputfile
 	ask $ html ( do CGI.head (title $ text "HETS results")
 			CGI.body $ printR str res (tree, env, tex, achiv) outputfile)
@@ -120,46 +121,55 @@ handle (F5 input box1 box2 box3 box4) =
 anaInput :: String -> SelectedBox -> FilePath -> IO([DiagStr],[(HtmlTitle, ResAna)])
 anaInput contents showS@(_,_,_,willAchiv) outputfiles =
    do 
-   setEnv "HETS_LIB" "/home/cofi/jiang/HetCATS/CASL-lib" True
+   setEnv "HETS_LIB" "/home/cofi/CASL-lib" True
    (parseErrors, ast) <- read_LIB_DEFN_M_WI defaultLogic "<stdin>" contents
-   Common.Result.Result ds res <- ioresToIO 
-				  (ana_LIB_DEFN logicGraph defaultLogic 
-				   defaultHetcatsOpts{outputToStdout = False} emptyLibEnv ast)
-   let diagStrs  = filterDiagStr [] $ take ((length ds)-1) (Prelude.map show ds) 
-   if parseErrors == "" then
-      if hasErrors ds then
-	 return (diagStrs, [])
-	 else
-	 let Lib_defn _ alibItems _ _ = ast
-	     items     = Prelude.map item alibItems
-	     diagStrs' = addSpnToDiags items diagStrs
-	 in  do
-	     saveLog willAchiv contents
-	     anaInput_aux (reverse diagStrs') res outputfiles showS
-      else return (parseErrors:diagStrs, [])
+   if parseErrors == "" then 
+      do
+      Common.Result.Result ds res <- ioresToIO 
+				     (ana_LIB_DEFN logicGraph defaultLogic 
+				      defaultHetcatsOpts{outputToStdout = False} emptyLibEnv ast)
+      let diagStrs1  = Prelude.map show ds
+          diagStrs2  = reverse $ filterDiagStr [] diagStrs1
+      -- putStrLn $ show $ length $ lines $ Prelude.head diagStrs1
+      case res of
+	       Just (_, libdefn1, _, _) ->  
+		   do
+		   let Lib_defn _ alibItems _ _ = libdefn1
+		       items = reverse $ Prelude.map item alibItems
+		   -- diagStrs' <- addSpnToDiags items diagStrs
+		   if hasErrors ds then
+		      return (diagStrs2, [])
+		      else do
+			   saveLog willAchiv contents
+			   anaInput_aux diagStrs2 res outputfiles showS
+	       Nothing -> return(diagStrs2,[])
+      else return (parseErrors:[], [])
 
    where 
       filterDiagStr :: [String] -> [DiagStr] -> [DiagStr]
       filterDiagStr str [] = str
-      filterDiagStr str (hdiags:rdiags) 
-		    | "*** FatalError" == take 14 hdiags = hdiags:str
-		    | otherwise = filterDiagStr (hdiags:str) rdiags
-      
-      addSpnToDiags :: [LIB_ITEM] -> [DiagStr] -> [DiagStr] 
-      addSpnToDiags [] ds = ds
-      addSpnToDiags (Spec_defn spn _ _ _:re) ds = addSpnToDiags re (("Analyzing spec " ++ showPretty spn ""):ds )
-      addSpnToDiags (View_defn vn _ _ _ _:re) ds = addSpnToDiags re (("Analyzing view " ++ showPretty vn ""):ds )
-      addSpnToDiags (Arch_spec_defn asn _ _:re) ds = addSpnToDiags re (("Analyzing arch spec " ++ showPretty asn ""):ds)
-      addSpnToDiags (Unit_spec_defn usn _ _:re) ds =  addSpnToDiags re (("Analyzing unit spec " ++ showPretty usn ""):ds)
-      addSpnToDiags (Logic_decl _ _:re) ds =  addSpnToDiags re ("":ds)
-      addSpnToDiags (Download_items ln _ _:re) ds = addSpnToDiags re (("Analyzing from " ++ showPretty ln "\n"):ds)
-						    
+      filterDiagStr str d@(hdiags:rdiags)
+	| (length $ lines hdiags) < 2 = 
+	        if "*** Hint" == take 8 hdiags then  filterDiagStr str rdiags
+		   else if "*** FatalError" == take 14 hdiags then hdiags:str
+			   else if "*** MessageW ," == take 14 hdiags then
+			           filterDiagStr ((drop 14 hdiags):str) rdiags
+			           else filterDiagStr (hdiags:str) rdiags
+        | otherwise  = filterDiagStr (filterDiagStr' str $ lines hdiags) rdiags 
+
+      filterDiagStr' :: [String] -> [DiagStr] -> [DiagStr]
+      filterDiagStr' str' [] = str'
+      filterDiagStr' str' (hd:rd)
+	  | "*** Hint"       == take 8 hd = filterDiagStr' str' rd
+	  | "*** MessageW ," == take 14 hd = filterDiagStr' ((drop 14 hd):str') rd
+	  | otherwise = filterDiagStr' (hd:str') rd
+		    
       anaInput_aux :: [DiagStr]
 		      -> Maybe(LIB_NAME, LIB_DEFN, DGraph, LibEnv)
 		      -> FilePath
                       -> SelectedBox 
 		      -> IO([DiagStr],[(HtmlTitle, ResAna)])
-      anaInput_aux diags res outputfiles (showTree, showEnv, showTex, _) =
+      anaInput_aux diags1 res outputfiles1 (showTree, showEnv, showTex, _) =
 	  case res of 
 	       Just (libName, libDefn, _, libEnv) ->
 	        do 
@@ -170,10 +180,10 @@ anaInput contents showS@(_,_,_,willAchiv) outputfiles =
 		 resTex <- write_casl_latex_stdout defaultHetcatsOpts globalAnnos libDefn
 		 if showTex then
 		    do
-		    let pptexFile = outputfiles ++ ".pp.tex"
-			latexFile = outputfiles ++ ".tex"
-			pdfFile   = outputfiles ++ ".pdf"
-			tmpFile   = outputfiles ++ ".tmp"
+		    let pptexFile = outputfiles1 ++ ".pp.tex"
+			latexFile = outputfiles1 ++ ".tex"
+			pdfFile   = outputfiles1 ++ ".pdf"
+			tmpFile   = outputfiles1 ++ ".tmp"
 		    write_casl_latex defaultHetcatsOpts globalAnnos (pptexFile) libDefn
 		    rawSystem "cp" ["/home/www/cofi/hets-tmp/mould.m", latexFile]
 		    setFileMode pptexFile fileMode
@@ -189,23 +199,23 @@ anaInput contents showS@(_,_,_,willAchiv) outputfiles =
 		    else return()
 		 if showEnv then
 		    do
-		    let txtFile = outputfiles ++ ".txt"
+		    let txtFile = outputfiles1 ++ ".txt"
 		    write_casl_asc defaultHetcatsOpts globalAnnos txtFile libDefn
 		    setFileMode txtFile fileMode
 		    else return()
-		 return $ (diags, selectOut [showTree, showEnv, showTex] libDefn resAsc resTex)
+		 return $ (diags1, selectOut [showEnv, showTex, showTree] libDefn resAsc resTex)
 	       Nothing -> return ([],[])
 
       selectOut :: [Bool] -> LIB_DEFN -> String -> String -> [(HtmlTitle, ResAna)]
       selectOut [] _ _ _ = []
       selectOut (hb:rb) libDefn ra rt 
 		| length rb == 2 && hb = 
-			("Parse tree:", show libDefn):(selectOut rb libDefn ra rt)
+		    ("ASCII code:", ra):(selectOut rb libDefn ra rt)
 		| length rb == 1 && hb = 
-			("ASCII code:", ra):(selectOut rb libDefn ra rt)
+		    ("LaTeX code:", ""):(selectOut rb libDefn ra rt)
+		    -- ("LaTeX code:", rt):(selectOut rb libDefn ra rt)
 		| length rb == 0 && hb = 
-		        ("LaTeX code:", ""):(selectOut rb libDefn ra rt)
-		      	-- ("LaTeX code:", rt):(selectOut rb libDefn ra rt) 
+		    ("Parse tree:", show libDefn):(selectOut rb libDefn ra rt)
 		| otherwise = selectOut rb libDefn ra rt
       -- log file
       saveLog :: Bool -> String -> IO()
@@ -236,12 +246,12 @@ anaInput contents showS@(_,_,_,willAchiv) outputfiles =
 -- Print the result		    
 printR :: String -> ([DiagStr], [(HtmlTitle, ResAna)]) -> SelectedBox -> FilePath 
           -> WithHTML x CGI ()
-printR str (diags, result) selectedBoxes outputFiles =
+printR str (diags2, result) selectedBoxes outputFiles =
     do 
        h1 $ text "You have submitted the HetCASL library:"
        mapM_ (\l -> text l >> br CGI.empty) $ lines str
        h1 $ text "Result of parsing and static checking:"
-       mapM_ (\l -> h3 $ text l >> br CGI.empty) $ diags 
+       mapM_ (\l -> h3 $ text l >> br CGI.empty) $ diags2 
        printRes selectedBoxes outputFiles result 
        hr_S $ CGI.empty
        p (do text "Not the result you expected ? Please check the "
@@ -261,29 +271,29 @@ printR str (diags, result) selectedBoxes outputFiles =
 		     ((title_ana, text_ana):rR) =
           do h3 $ text title_ana
        	     p $ mapM_ (\l -> text l >> br CGI.empty) $ lines text_ana
-	     if isTree then
-		printRes (False, isEnv, isTex, willAchiv) outputfiles rR
-		else if isEnv then
-		        do  
-			p $ i(do text "You can download the " 
-			         hlink (read ("http://www.informatik.uni-bremen.de/cofi/hets-tmp/" ++
-					      (drop 24 (outputfiles++".txt")))) $ text "ACSII file" 
-			         text " here. The file will be deleted after 30 minutes.\n" 
-			      )
-			printRes (isTree, False, isTex, willAchiv) outputfiles rR
-			else if isTex then
-			        do
-				p $ i(do text "You can download the "
-				         hlink (read ("http://www.informatik.uni-bremen.de/cofi/hets-tmp/" ++
-						       (drop 24 (outputfiles++".pp.tex")))) $ text "LaTeX file" 
-				         text " here. For compiling the LaTeX output, you need " 
-				         hlink (read "http://www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets/hetcasl.sty") $ text "hetcasl.sty" 
-				         text "."
-				     )
-				p $ i(do  text "You can also download the " 
-				          hlink (read ("http://www.informatik.uni-bremen.de/cofi/hets-tmp/" ++
-						       (drop 24 (outputfiles++".pdf")))) $ text "PDF file" 
-				          text ". All files will be deleted after 30 minutes.\n" 
-				     )
-				printRes (isTree, isEnv, False, willAchiv) outputfiles rR
-				else printRes (isTree, isEnv, isTex, willAchiv) outputfiles rR
+	     if isEnv then
+		do  
+		p $ i(do text "You can download the " 
+		         hlink (read ("http://www.informatik.uni-bremen.de/cofi/hets-tmp/" ++
+				      (drop 24 (outputfiles++".txt")))) $ text "ACSII file" 
+		         text " here. The file will be deleted after 30 minutes.\n" 
+		     )
+		printRes (isTree, False, isTex, willAchiv) outputfiles rR
+		else if isTex then
+		     do
+		     p $ i(do text "You can download the "
+			      hlink (read ("http://www.informatik.uni-bremen.de/cofi/hets-tmp/" ++
+					   (drop 24 (outputfiles++".pp.tex")))) $ text "LaTeX file" 
+			      text " here. For compiling the LaTeX output, you need " 
+			      hlink (read "http://www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets/hetcasl.sty") $ text "hetcasl.sty" 
+			      text "."
+			  )
+		     p $ i(do text "You can also download the " 
+			      hlink (read ("http://www.informatik.uni-bremen.de/cofi/hets-tmp/" ++
+					   (drop 24 (outputfiles++".pdf")))) $ text "PDF file" 
+			      text ". All files will be deleted after 30 minutes.\n" 
+			  )
+		     printRes (isTree, isEnv, False, willAchiv) outputfiles rR
+		     else if isTree then
+			  printRes (False, isEnv, isTex, willAchiv) outputfiles rR
+			  else printRes (isTree, isEnv, isTex, willAchiv) outputfiles rR
