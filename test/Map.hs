dc1d3a347e0262b8eba085a6d1ff126bfba1a4da
Christian Maeder <Christian.Maeder@dfki.de>
2004-09-07 15:49:48 +0000

    replaced ++ with : git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@3141 cec4b9c1-7d33-0410-9eda-942365e851bb

--- test/Map.hs
+++ test/Map.hs
@@ -2,7 +2,7 @@ module Map where
 
 map1 :: (a -> b) -> [a] -> [b]
 map1 f [] = []
-map1 f (x:xs) = f x ++ map1 f xs
+map1 f (x:xs) = f x : map1 f xs
 
 {-# AXIOMS
          "map1" forall f g xs ->  map1 f (map1 g xs) = map1 (f.g) xs
