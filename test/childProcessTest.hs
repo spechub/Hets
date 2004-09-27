b474ebd97dae002a787a0092a9334b866e0aa368
Till Mossakowski <mossakow@iws.cs.uni-magdeburg.de>
2004-09-27 08:12:09 +0000

    __* empty log message __* git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@3190 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ test/childProcessTest.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import ChildProcess
+import Computation
+import Concurrent
+
+main = do
+  p <- newChildProcess "isabelle" [arguments ["HOL"]]
+  sendMsg p "3+4;"
+  forkIO $ forever $ do m <- readMsg p; putStrLn m
+  return p
+  
