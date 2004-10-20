d2a7d862bdfd22777c74f52363eee3067510827c
Christian Maeder <Christian.Maeder@dfki.de>
2004-10-20 14:37:06 +0000

    use equality rather than unification git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@3276 cec4b9c1-7d33-0410-9eda-942365e851bb

--- HasCASL/Symbol.hs
+++ HasCASL/Symbol.hs
@@ -16,7 +16,6 @@ module HasCASL.Symbol where
 import HasCASL.Le
 import HasCASL.PrintLe
 import HasCASL.As
-import HasCASL.Unify
 import HasCASL.RawSym
 import Common.Id
 import Common.Result
@@ -38,8 +37,8 @@ checkSymbols s1 s2 r =
 dependentSyms :: Symbol -> Env -> SymbolSet
 dependentSyms sym sig = 
     Set.fold ( \ op se -> 
-	       if Set.member sym $ subSymsOf op then
-	       Set.insert op se else se) Set.empty $ symOf sig
+               if Set.member sym $ subSymsOf op then
+               Set.insert op se else se) Set.empty $ symOf sig
 
 hideRelSymbol :: Symbol -> Env -> Env
 hideRelSymbol sym sig = 
@@ -49,39 +48,39 @@ hideRelSymbol sym sig =
 hideSymbol :: Symbol -> Env -> Env
 hideSymbol sym sig = 
     let i = symName sym
-	tm = typeMap sig
-	as = assumps sig in
+        tm = typeMap sig
+        as = assumps sig in
     case symType sym of 
     ClassAsItemType _ -> sig
     TypeAsItemType _ -> sig { typeMap = 
-			      Map.delete i tm }
+                              Map.delete i tm }
     OpAsItemType ot -> 
-	let OpInfos os = Map.findWithDefault (OpInfos []) i as
-	    rs = filter (not . isUnifiable tm 1 ot . opType) os
+        let OpInfos os = Map.findWithDefault (OpInfos []) i as
+            rs = filter (not . (== ot) . opType) os
         in sig { assumps = if null rs then Map.delete i as
-		          else Map.insert i (OpInfos rs) as }
+                          else Map.insert i (OpInfos rs) as }
 
 plainHide :: SymbolSet -> Env -> Env
 plainHide syms sigma = 
     let (opSyms, otherSyms) = Set.partition (\ sy -> case symType sy of
-					      OpAsItemType _ -> True
-					      _ -> False) syms
+                                              OpAsItemType _ -> True
+                                              _ -> False) syms
     in Set.fold hideSymbol (Set.fold hideSymbol sigma otherSyms) opSyms 
 
 -- | type ids within a type
 subSyms :: Env -> Type -> SymbolSet
 subSyms e t = case t of
-	   TypeName i k n ->
-	       if n == 0 then if i == unitTypeId then Set.empty 
+           TypeName i k n ->
+               if n == 0 then if i == unitTypeId then Set.empty 
                   else Set.single $ idToTypeSymbol e i k
-	       else Set.empty
-	   TypeAppl t1 t2 -> Set.union (subSyms e t1) (subSyms e t2)
-	   ExpandedType _ t1 -> subSyms e t1
-	   KindedType tk _ _ -> subSyms e tk
-	   LazyType tl _ -> subSyms e tl
-	   ProductType l _ -> Set.unions $ map (subSyms e) l
+               else Set.empty
+           TypeAppl t1 t2 -> Set.union (subSyms e t1) (subSyms e t2)
+           ExpandedType _ t1 -> subSyms e t1
+           KindedType tk _ _ -> subSyms e tk
+           LazyType tl _ -> subSyms e tl
+           ProductType l _ -> Set.unions $ map (subSyms e) l
            FunType t1 _ t2 _ -> Set.union (subSyms e t1) (subSyms e t2)
-	   _ -> error ("subSyms: " ++ show t)
+           _ -> error ("subSyms: " ++ show t)
 
 subSymsOf :: Symbol -> SymbolSet
 subSymsOf sy = case symType sy of
@@ -94,17 +93,17 @@ closeSymbSet s = Set.unions (s : map subSymsOf (Set.toList s))
 symOf :: Env -> SymbolSet
 symOf sigma = 
     let classes = Map.foldWithKey ( \ i ks s -> 
-			Set.insert (idToClassSymbol sigma i $ 
-				    Intersection (classKinds ks) []) s)
-		  Set.empty $ classMap sigma
-	types = Map.foldWithKey ( \ i ti s -> 
-			Set.insert (idToTypeSymbol sigma i $
-				    typeKind ti) s) 
-		classes $ typeMap sigma
+                        Set.insert (idToClassSymbol sigma i $ 
+                                    Intersection (classKinds ks) []) s)
+                  Set.empty $ classMap sigma
+        types = Map.foldWithKey ( \ i ti s -> 
+                        Set.insert (idToTypeSymbol sigma i $
+                                    typeKind ti) s) 
+                classes $ typeMap sigma
         ops = Map.foldWithKey ( \ i ts s0 ->
-		      foldr ( \ t s1 -> 
-			  Set.insert (idToOpSymbol sigma i $
-				      opType t) s1) s0 $ opInfos ts)
-	      types $ assumps sigma
-	in ops
+                      foldr ( \ t s1 -> 
+                          Set.insert (idToOpSymbol sigma i $
+                                      opType t) s1) s0 $ opInfos ts)
+              types $ assumps sigma
+        in ops
 
