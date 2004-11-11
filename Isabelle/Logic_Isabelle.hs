6b8f813c46b719a37e9a61cdfba85339952abbed
Christian Maeder <Christian.Maeder@dfki.de>
2004-11-11 11:20:37 +0000

    cleaned up git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@3420 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Isabelle/Logic_Isabelle.hs
+++ Isabelle/Logic_Isabelle.hs
@@ -1,5 +1,4 @@
 {-# OPTIONS -cpp #-}
-
 {- |
 Module      :  $Header$
 Copyright   :  (c) Till Mossakowski, Uni Bremen 2002-2004
@@ -7,7 +6,7 @@ Licence     :  similar to LGPL, see HetCATS/LICENCE.txt or LIZENZ.txt
 
 Maintainer  :  hets@tzi.de
 Stability   :  provisional
-Portability :  portable
+Portability :  non-portable (imports Logic)
 
    Instance of class Logic for Isabelle (including Pure, HOL etc.).
 -}
@@ -25,8 +24,6 @@ import Isabelle.IsaProve
 #endif
 
 import Logic.Logic
-import Common.Result
-import Common.Id
 import Common.Lib.Pretty
 import Common.AS_Annotation
 import Common.PrettyPrint
@@ -37,10 +34,11 @@ import ATC.IsaSign
 data Isabelle = Isabelle deriving (Show)
 instance Language Isabelle where
  description _ = 
-  "Isabelle - a generic theorem prover\n"++
-  "This logic corresponds to the logic of Isabelle,\n"++
-  "a weak intuitionistic type theory\n"++
-  "Also, the logics encoded in Isabelle, like FOL, HOL; HOLCF, ZF are made available\n"++
+  "Isabelle - a generic theorem prover\n" ++
+  "This logic corresponds to the logic of Isabelle,\n" ++
+  "a weak intuitionistic type theory\n" ++
+  "Also, the logics encoded in Isabelle, like FOL, HOL, HOLCF, ZF " ++ 
+  "are made available\n" ++
   "See http://www.cl.cam.ac.uk/Research/HVG/Isabelle/"
 
 sentenceTc, signTc :: TyCon
@@ -67,62 +65,30 @@ instance Category Isabelle Sign ()
          -- legal_mor :: id -> morphism -> Bool
 	 legal_mor Isabelle _ = False
 
-
 -- abstract syntax, parsing (and printing)
 
 instance Logic.Logic.Syntax Isabelle () () ()
-      where 
-         parse_basic_spec Isabelle = Nothing
-	 parse_symb_items Isabelle = Nothing
-	 parse_symb_map_items Isabelle = Nothing
+    -- default implementation is fine!
+
 
 instance Sentences Isabelle Sentence () Sign () ()  where
-      map_sen Isabelle _morphism s = return s
+      map_sen Isabelle () s = return s
       print_named Isabelle ga (NamedSen lab sen) = 
 	(if null lab then empty 
 	else text lab <+> colon <> space) <> printText0 ga sen
-      parse_sentence Isabelle = Nothing
 #ifdef UNI_PACKAGE
       provers Isabelle = [isabelleProver] 
-#else
-      provers Isabelle = [] 
 #endif
-      cons_checkers Isabelle = []
+    -- other default implementations are fine
 
 instance StaticAnalysis Isabelle () Sentence ()
                () ()
                Sign 
                () () ()  where
-         basic_analysis Isabelle = Nothing
-         stat_symb_map_items Isabelle _ = 
-            fatal_error "No symbol map analysis for Isabelle" nullPos
-         stat_symb_items Isabelle _ = 
-            fatal_error "No symbol map analysis for Isabelle" nullPos
-	 ensures_amalgamability Isabelle _ = 
-            fail "ensures_amalgamability nyi" -- ???
-
          sign_to_basic_spec Isabelle _sigma _sens = ()
-
          empty_signature Isabelle = emptySign
-         signature_union Isabelle sigma1 sigma2 = 
-            fatal_error "No signature union for Isabelle" nullPos
-         morphism_union Isabelle _ _ = 
-            fatal_error "No morphism union for Isabelle" nullPos
-	 final_union Isabelle _ _ = 
-            fatal_error "No signature union for Isabelle" nullPos
-{-
-         is_subsig Isabelle = isSubSig
-         inclusion Isabelle = sigInclusion
-         cogenerated_sign Isabelle = cogeneratedSign
-         generated_sign Isabelle = generatedSign
-         induced_from_morphism Isabelle = inducedFromMorphism
-         induced_from_to_morphism Isabelle = inducedFromToMorphism
--}
 
 instance Logic Isabelle () () Sentence () ()
                Sign 
-               () () () () where
-         sublogic_names Isabelle _ = ["Isabelle"]
-         all_sublogics Isabelle = [()]
-
-         data_logic Isabelle = Nothing
+               () () () ()
+    -- again default implementations are fine
