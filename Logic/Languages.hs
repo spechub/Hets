5fb008da20da798ad652207d16b9aaeed712af61
Till Mossakowski <mossakow@iws.cs.uni-magdeburg.de>
2004-12-19 14:10:27 +0000

    Moved language and coercion stuff to a separate module git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@3575 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ Logic/Languages.hs
@@ -0,0 +1,54 @@
+{- |
+Module      :  $Header$
+Copyright   :  (c) Till Mossakowski, and Uni Bremen 2002-2003
+Licence     :  similar to LGPL, see HetCATS/LICENCE.txt or LIZENZ.txt
+
+Maintainer  :  till@tzi.de
+Stability   :  provisional
+Portability :  non-portable (various -fglasgow-exts extensions)
+
+
+-- languages, define like "data CASL = CASL deriving Show" 
+-}
+
+module Logic.Languages where
+
+-- for coercion used in Grothendieck.hs and Analysis modules
+
+import UnsafeCoerce
+import Data.Dynamic
+import Common.DynamicUtils 
+
+import Common.Id
+import Common.Result
+
+
+
+class Show lid => Language lid where
+    language_name :: lid -> String
+    language_name i = show i
+    description :: lid -> String
+    -- default implementation
+    description _ = "No description available"
+
+-- (a bit unsafe) coercion using the language name
+mcoerce :: (Typeable a, Typeable b, Language lid1, Language lid2,
+          Monad m) => lid1 -> lid2 -> String -> a -> m b
+mcoerce i1 i2 err a = 
+  if language_name i1 == language_name i2 
+     then return $ unsafeCoerce a 
+     else fail (err1++"Logic "++ language_name i1 ++ " expected, but "
+                ++ language_name i2 ++ " found")
+  where err1 = if err=="" then "" else err++": "
+   
+coerce :: (Typeable a, Typeable b, Language lid1, Language lid2,
+          Monad m) => lid1 -> lid2 -> a -> m b
+coerce i1 i2 a = mcoerce i1 i2 "" a 
+
+rcoerce :: (Typeable a, Typeable b, Language lid1, Language lid2) => 
+           lid1 -> lid2 -> Pos -> a -> Result b
+rcoerce i1 i2 pos a = adjustPos pos $ coerce i1 i2 a
+
+mrcoerce :: (Typeable a, Typeable b, Language lid1, Language lid2) => 
+           lid1 -> lid2 -> Pos -> String -> a -> Result b
+mrcoerce i1 i2 pos err a = adjustPos pos $ mcoerce i1 i2 err a
