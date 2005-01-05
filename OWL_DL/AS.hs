dcc8afbab7f71fcebe80b820c78d8c8d84a7c7de
Heng Jiang <jiang@tzi.de>
2005-01-05 12:20:04 +0000

    better formatting git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@3618 cec4b9c1-7d33-0410-9eda-942365e851bb

--- OWL_DL/AS.hs
+++ OWL_DL/AS.hs
@@ -1,8 +1,20 @@
+{- |
+Module      :  $Header$
+Copyright   :  (c) Heng Jiang, Uni Bremen 2004-2005
+Licence     :  similar to LGPL, see HetCATS/LICENCE.txt or LIZENZ.txt
+
+Maintainer  :  hets@tzi.de
+Stability   :  provisional
+Portability :  portable
+
+   Here is the place where the class Logic is instantiated for CASL.
+   Also the instances for Syntax an Category.
+-}
+
 module OWL_DL.AS where
 
 type URIreference = String
 
--- Data structur for Ontologies
 type DatatypeID = URIreference
 type ClassID = URIreference
 type IndividualID = URIreference
@@ -12,30 +24,45 @@ type IndividualvaluedPropertyID = URIreference
 type AnnotationPropertyID = URIreference
 type OntologyPropertyID = URIreference
 
+-- | Data structur for Ontologies
 data Ontology = Ontology (Maybe OntologyID) [Directive]
                 deriving (Show, Eq)
 data Directive = Anno Annotation | Ax Axiom | Fc Fact
                  deriving (Show, Eq)
-data Annotation = URIAnnotation AnnotationPropertyID URIreference
-                | DLAnnotation AnnotationPropertyID DataLiteral
-                | IndivAnnotation AnnotationPropertyID Individual
+data Annotation = URIAnnotation 
+                         AnnotationPropertyID 
+                         URIreference
+                | DLAnnotation 
+                         AnnotationPropertyID 
+                         DataLiteral
+                | IndivAnnotation 
+                         AnnotationPropertyID 
+                         Individual
                   deriving (Show, Eq)
 
--- Data structur for facts
-data DataLiteral = TypedL TypedLiteral | PlainL PlainLiteral
+-- | Data literal
+data DataLiteral = TypedL TypedLiteral 
+                 | PlainL PlainLiteral
                    deriving (Show, Eq)
-type TypedLiteral = (LexicalForm, URIreference) -- consist of a lexical representatoin and a URI.
-                   
-type PlainLiteral =  -- Unicode string in Normal Form C and an optional language tag
-                    (LexicalForm, LanguageTag)  
-
+type TypedLiteral = (LexicalForm, URIreference)  
+                    -- ^ consist of a lexical representatoin and a URI.                   
+type PlainLiteral = (LexicalForm, LanguageTag)  
+                    -- ^ Unicode string in Normal Form C and an optional language tag
 type LexicalForm = String        
-type LanguageTag = String      -- XMLLT?? an XML language tag
+type LanguageTag = String
 
+-- | Data structur for facts
 data Fact = Indiv Individual 
-          | SameIndividual IndividualID IndividualID [IndividualID]
-          | DifferentIndividuals IndividualID IndividualID [IndividualID]
+          | SameIndividual 
+                  IndividualID 
+                  IndividualID 
+                  [IndividualID]
+          | DifferentIndividuals 
+                  IndividualID 
+                  IndividualID 
+                  [IndividualID]
             deriving (Show, Eq)
+
 data Individual = Individual (Maybe IndividualID) [Annotation] (Maybe Type) (Maybe Value)
                   deriving (Show, Eq)
 data Value = ValueID    IndividualvaluedPropertyID IndividualID
@@ -44,41 +71,74 @@ data Value = ValueID    IndividualvaluedPropertyID IndividualID
              deriving (Show, Eq)
 type Type = Description
 
--- Axiom (Class Axioms, Descriptions, Restrictions, Property Axioms)
-data Axiom = Class ClassID (Maybe Deprecated) Modality [Annotation] [Description]
-           | EnumeratedClass ClassID (Maybe Deprecated) [Annotation] [IndividualID]
-           | DisjointClasses Description Description [Description]
-           | EquivalentClasses Description [Description]
-           | SubClassOf Description Description
-           | Datatype DatatypeID (Maybe Deprecated) [Annotation]
-           | DatatypeProperty DatavaluedPropertyID (Maybe Deprecated) [Annotation] [Super DatavaluedPropertyID] (Maybe Func) [Domain Description] [Range DataRange]          -- ??
+-- | Axiom (Class Axioms, Descriptions, Restrictions, Property Axioms)
+data Axiom = Class 
+                   ClassID 
+                   Bool -- ^ True == deprecated
+                   Modality 
+                   [Annotation] 
+                   [Description]
+           | EnumeratedClass 
+                   ClassID 
+                   Bool -- ^ True == deprecated
+                   [Annotation] 
+                   [IndividualID]
+           | DisjointClasses 
+                   Description 
+                   Description 
+                   [Description]
+           | EquivalentClasses 
+                   Description 
+                   [Description]
+           | SubClassOf 
+                   Description 
+                   Description
+           | Datatype 
+                   DatatypeID 
+                   Bool -- ^ True == deprecated  
+                   [Annotation]
+           | DatatypeProperty 
+                   DatavaluedPropertyID 
+                   Bool -- ^ True == deprecated  
+                   [Annotation] 
+                   [DatavaluedPropertyID]  -- ^ super properties 
+                   (Maybe Func) 
+                   [Description] -- ^ Domain 
+                   [DataRange] -- ^ Range
            | ObjectProperty IndividualvaluedPropertyID 
-                    Bool -- ^ True == deprecated 
-                    [Annotation] 
-                    [IndividualvaluedPropertyID] -- ^ super properties 
-                    (Maybe IndividualvaluedPropertyID)
-                       -- ^ inverse of property 
-                    Bool -- ^ True == symmetric
-                    (Maybe Func) 
-                    [Description] -- ^ Domain 
-                    [Description] -- ^ Range
-           | AnnotationProperty AnnotationPropertyID [Annotation]
-           | OntologyProperty OntologyPropertyID [Annotation]
-           | DEquivalentProperties DatavaluedPropertyID DatavaluedPropertyID [DatavaluedPropertyID]
-           | DSubPropertyOf DatavaluedPropertyID DatavaluedPropertyID
-           | IEquivalentProperty IndividualvaluedPropertyID IndividualvaluedPropertyID [IndividualvaluedPropertyID]
-           | ISubPropertyOf IndividualvaluedPropertyID IndividualvaluedPropertyID
+                   Bool -- ^ True == deprecated 
+                   [Annotation] 
+                   [IndividualvaluedPropertyID] -- ^ super properties 
+                   (Maybe IndividualvaluedPropertyID)
+                      -- ^ inverse of property 
+                   Bool -- ^ True == symmetric
+                   (Maybe Func) 
+                   [Description] -- ^ Domain 
+                   [Description] -- ^ Range
+           | AnnotaionProperty 
+                   AnnotationPropertyID 
+                   [Annotation]
+           | OntologyProperty 
+                   OntologyPropertyID 
+                   [Annotation]
+           | DEquivalentProperties 
+                   DatavaluedPropertyID 
+                   DatavaluedPropertyID 
+                   [DatavaluedPropertyID]
+           | DSubPropertyOf 
+                   DatavaluedPropertyID 
+                   DatavaluedPropertyID
+           | IEquivalentProperty 
+                   IndividualvaluedPropertyID 
+                   IndividualvaluedPropertyID 
+                   [IndividualvaluedPropertyID]
+           | ISubPropertyOf 
+                   IndividualvaluedPropertyID 
+                   IndividualvaluedPropertyID
              deriving (Show, Eq)
 
 data Func = Functional | InverseFunctional | Functional_InverseFunctional | Transitive
             deriving (Show, Eq)
-data Deprecated = Deprecated  
-                  deriving (Show, Eq)
-data Symm =  Symmetric  
-             deriving (Show, Eq)
-data Super a = Super a  deriving (Show, Eq)
-data Domain a = Domain a deriving (Show, Eq)
-data Range a = Range a deriving (Show, Eq)
 
 data Modality = Complete | Partial
                 deriving (Show, Eq)
@@ -113,5 +173,6 @@ data Cardinality = MinCardinality Int
 
 data DataRange = DID DatatypeID 
                | OneOfData [DataLiteral]
-               | Rdfs_Literal       ------- ????
+               | RL DataLiteral       -- ^ rdfs:literal
                  deriving (Show, Eq)
+
