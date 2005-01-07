c317382ade6e0fd973cddf0554c7033c5a189f02
Christian Maeder <Christian.Maeder@dfki.de>
2005-01-07 18:08:41 +0000

    use hana instead of hapa git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@3639 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Haskell/hana.hs
+++ Haskell/hana.hs
@@ -1,36 +1,55 @@
 {- |
 Module      :  $Header$
-Copyright   :  (c) Till Mossakowski, Uni Bremen 2002-2004
+Copyright   :  (c) Christian Maeder, Uni Bremen 2002-2004
 Licence     :  similar to LGPL, see HetCATS/LICENCE.txt or LIZENZ.txt
 
 Maintainer  :  hets@tzi.de
 Stability   :  experimental
-Portability :  portable 
+Portability :  non-portable 
    
-test the haskell parser
+test the programatica parser and (HatAna) analysis
 
 -}
 
 module Main where
 
-import System.Environment
 import Haskell.HatParser
 import Haskell.HatAna
-import Text.ParserCombinators.Parsec
+import ParseMonad
+import LexerOptions
+import PropLexer
+import PropParser as HP
+import PropPosSyntax
+
+import System.Environment
+
+import Common.Result
+import Common.AnnoState
+import Common.AS_Annotation
+import Common.Print_AS_Annotation()
 import Common.GlobalAnnotations
+import Common.PrettyPrint
+
+hParser :: AParser () (Sign, [Named (HsDeclI PNT)])
+hParser = do 
+   b <- hatParser
+   let res@(Result _ m) = hatAna(b, emptySign, emptyGlobalAnnos)
+   case m of 
+      Nothing -> error $ show res
+      Just (_, _, sig, sens) -> return (sig, sens)
 
 main :: IO ()
-main = do
-    l <- getArgs
-    if length l == 1 
-       then do
-	    let f = head l
-	    r <- parseFromFile hatParser f 
-	    putStrLn $ case r of
-			 Right decls -> 
-                           show $ hatAna (decls, emptySign, emptyGlobalAnnos)
-			 Left msg -> show msg
-	else do 
-	     p <- getProgName
-             putStrLn("Usage: "++p++" <file>")
-
+main = do l <- getArgs
+	  if length l >= 1 then
+	     do let fn = head l 
+                s <- readFile fn
+                let ts = pLexerPass0 lexerflags0 s
+                    Result ds m = do 
+                      HsModule _ _ _ _ b <- parseTokens HP.parse fn ts
+                      hatAna(HsDecls b, emptySign, emptyGlobalAnnos) 
+	        case m of 
+		       Just (_, _, sig, hs) -> do
+		           putStrLn $ showPretty sig ""
+			   mapM_ (putStrLn . flip showPretty "") hs
+		       _ -> mapM_ (putStrLn . show) ds
+	     else putStrLn "missing argument"
