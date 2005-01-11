a1143543d96ddc73ce467477730658d5c63720bc
Christian Maeder <Christian.Maeder@dfki.de>
2005-01-11 09:18:00 +0000

    3 arguments example git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@3651 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ ToHaskell/test/Foldl.hascasl.hs
@@ -0,0 +1,29 @@
+{-
+
+types:
+List :: (*->*, data)
+
+values:
+foldl ::
+    forall a b . a -> (a -> b -> a) -> (List b) -> a
+A__cons :: forall a . a -> (List a) -> List a
+A__nil :: forall a . List a
+
+scope:
+Prelude.A__cons |-> Prelude.A__cons, con of List
+Prelude.A__nil |-> Prelude.A__nil, con of List
+Prelude.List |-> Prelude.List, Type [A__nil,
+				     A__cons] []
+Prelude.foldl |-> Prelude.foldl, Value
+A__cons |-> Prelude.A__cons, con of List
+A__nil |-> Prelude.A__nil, con of List
+List |-> Prelude.List, Type [A__nil, A__cons] []
+foldl |-> Prelude.foldl, Value
+-}
+module Dummy where
+import Prelude (error, Show, Eq, Ord)
+import MyLogic
+foldl :: b -> (b -> a -> b) -> (List a) -> b
+data List a = A__nil | A__cons !a !(List a)
+foldl z f A__nil = z
+foldl z f (A__cons x l) = foldl (f z x) f l
