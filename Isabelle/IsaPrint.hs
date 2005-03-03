72a80e31851373a6a01f12c7ffa1b27c8b610ea2
Paolo Torrini <paolot@informatik.uni-bremen.de>
2005-03-03 13:45:43 +0000

    non-terminating showTerm clause removed git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@3902 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Isabelle/IsaPrint.hs
+++ Isabelle/IsaPrint.hs
@@ -118,7 +118,8 @@ showTerm (If t1 t2 t3) =
 showTerm (Let pts t) = lb ++ "let" ++ sp ++ showPat False (head pts) 
                                 ++ concat (map (showPat True) (tail pts))
                                 ++ sp ++ "in" ++ sp ++ showTerm t ++ rb
-showTerm t = showPTree (toPrecTree t)
+-- showTerm t = showPTree (toPrecTree t)     NO GOOD - it loops
+showTerm t = "(IsaPrint.showTerm, error: unsupported term)"
 
 
 showPat :: Bool -> (Term, Term) -> String
