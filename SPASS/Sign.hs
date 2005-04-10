2d3418c043b78f839bb782eb49ba241b2adfb442
René Wagner <Rene.Wagner@dfki.de>
2005-04-10 19:54:33 +0000

    initial data structure for SPASS git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4030 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ SPASS/Sign.hs
@@ -0,0 +1,56 @@
+{- |
+Module      :  $Header$
+Copyright   :  (c) Rene Wagner, Uni Bremen 2005
+Licence     :  similar to LGPL, see HetCATS/LICENCE.txt or LIZENZ.txt
+
+Maintainer  :  rwagner@tzi.de
+Stability   :  provisional
+Portability :  unknown
+
+   Data structures representing SPASS signatures.
+
+-}
+
+module SPASS.Sign where
+
+import Common.AS_Annotation
+
+type SPIdentifier = String
+
+type SPFormula = Named SPTerm
+
+data SPOriginType =
+        SPOriginAxioms
+      | SPOriginConjectures
+      deriving (Eq, Show)
+
+data SPTerm = 
+        SPQuantTerm { termList :: SPTermList,
+                      term     :: SPTerm }
+      | SPSimpleTerm SPSymbol
+      | SPComplexTerm { symbol    :: SPSymbol,
+                        arguments :: [SPTerm]}
+      deriving (Eq, Show)
+
+data SPTermList = 
+        SPTermList [SPTerm]
+      deriving (Eq, Show)
+
+data SPQuantSym =
+        SPForall
+      | SPExists
+      | SPCustomQuantSym SPIdentifier
+      deriving (Eq, Show)
+
+data SPSymbol =
+        SPEqual
+      | SPTrue 
+      | SPFalse 
+      | SPOr 
+      | SPAnd
+      | SPNot
+      | SPImplies
+      | SPImplied
+      | SPEquiv
+      | SPCustomSymbol SPIdentifier
+      deriving (Eq, Show)
