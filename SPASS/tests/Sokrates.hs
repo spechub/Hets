ddaca64f6aaa5b46081c95c57f4f192f1e3d4f4a
René Wagner <Rene.Wagner@dfki.de>
2005-04-17 22:53:05 +0000

    example from the SPASS tutorial git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4073 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ SPASS/tests/Sokrates.hs
@@ -0,0 +1,67 @@
+{- |
+Module      :  $Header$
+Copyright   :  (c) Rene Wagner, Uni Bremen 2005
+Licence     :  similar to LGPL, see HetCATS/LICENCE.txt or LIZENZ.txt
+
+Maintainer  :  rwagner@tzi.de
+Stability   :  provisional
+Portability :  unknown
+
+   The Sokrates example from the SPASS tutorial at
+   <http://spass.mpi-sb.mpg.de/tutorial/> as a Haskell data structure.
+
+-}
+
+import SPASS.Sign
+
+import Common.AS_Annotation
+import Common.PrettyPrint
+import Common.Lib.Pretty
+
+sokratesProblem :: SPProblem
+sokratesProblem = SPProblem { identifier = "Sokrates1",
+                              description = sokratesDescription,
+                              logicalPart = sokratesLogicalPart }
+
+sokratesDescription :: SPDescription
+sokratesDescription = SPDescription { name = "Sokrates",
+                                      author = "Christoph Weidenbach",
+                                      version = Nothing,
+                                      logic = Nothing,
+                                      status = SPStateUnsatisfiable,
+                                      desc = "Sokrates is mortal and since all humans are mortal, he is mortal too.",
+                                      date = Nothing }
+
+sokratesLogicalPart :: SPLogicalPart
+sokratesLogicalPart = SPLogicalPart { symbolList = Just sokratesSymbolList,
+                                      declarationList = [],
+                                      formulaLists = [sokratesAxiomFormulae, sokratesConjectureFormulae] }
+
+sokratesSymbolList :: SPSymbolList
+sokratesSymbolList = SPSymbolList { functions = [SPSignSym {sym= "sokrates", arity= 0}],
+                                    predicates = [SPSignSym {sym= "Human", arity= 1}, SPSignSym {sym= "Mortal", arity= 1}],
+                                    sorts= [],
+                                    operators= [],
+                                    quantifiers= [] }
+
+sokratesAxiomFormulae :: SPFormulaList
+sokratesAxiomFormulae = SPFormulaList {originType= SPOriginAxioms,
+                                       formulae= [f1, f2] }
+  where
+    f1 = NamedSen {senName= "1",
+                   sentence= SPComplexTerm {symbol= SPCustomSymbol "Human",
+                                            arguments= [SPSimpleTerm (SPCustomSymbol "sokrates")] } }
+    f2 = NamedSen {senName= "2",
+                   sentence= SPQuantTerm {quantSym= SPForall, 
+                                          termTermList= [SPSimpleTerm (SPCustomSymbol "x")],
+                                          termTerm= SPComplexTerm { symbol= SPImplies,
+                                                                    arguments= [SPComplexTerm {symbol= SPCustomSymbol "Human", arguments= [SPSimpleTerm (SPCustomSymbol "x")]},
+                                                                                SPComplexTerm {symbol= SPCustomSymbol "Mortal", arguments= [SPSimpleTerm (SPCustomSymbol "x")]}] } } }
+
+sokratesConjectureFormulae :: SPFormulaList
+sokratesConjectureFormulae = SPFormulaList { originType= SPOriginConjectures,
+                                             formulae= [f3] }
+  where
+    f3 = NamedSen {senName= "3",
+                   sentence= SPComplexTerm {symbol= SPCustomSymbol "Mortal",
+                                            arguments= [SPSimpleTerm (SPCustomSymbol "sokrates")] } }
