a0fb21aefc6fb5e1fd0d9baeb1cec2eab00b947e
René Wagner <Rene.Wagner@dfki.de>
2005-04-19 10:35:44 +0000

    move printing functions from SPASS/Sign.hs to SPASS/Print.hs git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4083 cec4b9c1-7d33-0410-9eda-942365e851bb

--- SPASS/Sign.hs
+++ SPASS/Sign.hs
@@ -15,13 +15,9 @@ Portability :  unknown
 
 module SPASS.Sign where
 
-import Maybe
 import Char
 
 import Common.AS_Annotation
-import Common.PrettyPrint
-import Common.GlobalAnnotations
-import Common.Lib.Pretty
 
 {- |
   A SPASS Identifier is a String for now. See also 'checkIdentifier' function
@@ -85,7 +81,9 @@ data SPSignSym =
       | SPSimpleSignSym SPIdentifier
       deriving (Eq, Show)
 
---DOCUMENTME
+{- |
+  SPASS Declarations allow for the introduction of sorts.
+-}
 data SPDeclaration =
         SPSubsortDecl { sortSymA :: SPIdentifier,
                         sortSymB :: SPIdentifier }
@@ -180,151 +178,3 @@ data SPLogState =
       | SPStateUnsatisfiable
       | SPStateUnknown
       deriving (Eq, Show)
-
-{- |
-  Helper function. Generates a "." as a Doc.
--}
-dot :: Doc
-dot = char '.'
-
-{- |
-  Creates a Doc from a SPASS Problem.
--}
-printProblem :: SPProblem-> Doc
-printProblem p = text "begin_problem" <> parens (text (identifier p)) <> dot
-  $$ printDescription (description p)
-  $$ printLogicalPart (logicalPart p)
-  $$ text "end_problem."
-
-{- |
-  Creates a Doc from a SPASS Logical Part.
--}
-printLogicalPart :: SPLogicalPart-> Doc
-printLogicalPart lp =
-  (if isJust (symbolList lp) then printSymbolList (fromJust (symbolList lp)) else empty)
-  $$ (if not $ null (declarationList lp) then printDeclarationList (declarationList lp) else empty)
-  $$ (if not $ null (formulaLists lp) then printFormulaLists (formulaLists lp) else empty)
-  where
-    printDeclarationList xs = text "list_of_declarations."
-      $$ foldl (\d x-> d $$ printDeclaration x) empty xs
-      $$ text "end_of_list."
-    printFormulaLists = foldl (\d x-> d $$ printFormulaList x) empty
-
-{- |
-  Creates a Doc from a SPASS Symbol List.
--}
-printSymbolList :: SPSymbolList-> Doc
-printSymbolList sl = text "list_of_symbols."
-  $$ printSignSymList "functions" (functions sl)
-  $$ printSignSymList "predicates" (predicates sl)
-  $$ printSignSymList "sorts" (sorts sl)
-  $$ printSignSymList "operators" (operators sl)
-  $$ printSignSymList "quantifiers" (quantifiers sl)
-  $$ text "end_of_list."
-  where 
-    printSignSymList name list =
-      if not $ null list
-        then text name <> brackets (foldl (\d x-> if isEmpty d then printSignSym x else d <> comma $$ printSignSym x) empty list) <> dot
-        else empty
-
-{-|
-  Helper function. Creates a Doc from a Signature Symbol.
--}
-printSignSym :: SPSignSym-> Doc
-printSignSym (SPSimpleSignSym s) = text s
-printSignSym ssym = parens (text (sym ssym) <> comma <> int (arity ssym))
-
--- FIXME
-printDeclaration :: SPDeclaration-> Doc
-printDeclaration _ = empty
-
-{- |
-  Creates a Common.Lib.Pretty.Doc from a SPASS Formula List
--}
-printFormulaList :: SPFormulaList-> Doc
-printFormulaList l = text "list_of_formulae" <> parens (printOriginType (originType l)) <> dot
-  $$ printFormulae (formulae l)
-  $$ text "end_of_list."
-  where
-    printFormulae = foldl (\fl x-> fl <> printFormula x <> dot) empty
-
-{- |
-  Creates a Doc from a SPASS Origin Type
--}
-printOriginType :: SPOriginType-> Doc
-printOriginType t = case t of
-  SPOriginAxioms      -> text "axioms"
-  SPOriginConjectures -> text "conjectures"
-
-{- |
-  Creates a Doc from a SPASS Formula.
--}
-printFormula :: SPFormula-> Doc
-printFormula f = (text "formula") <> parens (printTerm (sentence f) <> comma <> text (senName f))
-
-{- |
-  Creates a Doc from a SPASS Term.
--}
-printTerm :: SPTerm-> Doc
-printTerm t = case t of
-  SPQuantTerm{quantSym= qsym, termTermList= tlist, termTerm= t} -> printQuantSym qsym <> parens (brackets (printTermList tlist) <> comma <> printTerm t)
-  SPSimpleTerm sym -> printText0 emptyGlobalAnnos sym
-  SPComplexTerm{symbol= sym, arguments= args} -> printText0 emptyGlobalAnnos sym <> parens (printTermList args)
-  where
-    printTermList = foldl (\tl x-> if isEmpty tl then printTerm x else tl <> comma <> (printTerm x)) empty
-
-{- |
-  Creates a Doc from a SPASS Quantifier Symbol.
--}
-printQuantSym :: SPQuantSym-> Doc
-printQuantSym qs = case qs of
-  SPForall             -> text "forall"
-  SPExists             -> text "exists"
-  SPCustomQuantSym cst -> text cst
-
-{- |
-  Creates a Doc from a SPASS Symbol.
--}
--- printSymbol :: SPSymbol-> Doc
-instance PrettyPrint SPSymbol where
-    printText0 ga s = case s of
-     SPEqual            -> text "equal"
-     SPTrue             -> text "true"
-     SPFalse            -> text "false"
-     SPOr               -> text "or"
-     SPAnd              -> text "and"
-     SPNot              -> text "not"
-     SPImplies          -> text "implies"
-     SPImplied          -> text "implied"
-     SPEquiv            -> text "equiv"
-     SPCustomSymbol cst -> text cst
-
-{- |
-  Creates a Doc from a SPASS description.
--}
-printDescription :: SPDescription-> Doc
-printDescription d = text "list_of_descriptions."
-  $$ text "name" <> parens (spText (name d)) <> dot
-  $$ text "author" <> parens (spText (author d)) <> dot
-  $$ (if isJust (version d) then text "version" <> parens (spText (fromJust (version d))) <> dot else empty)
-  $$ (if isJust (logic d) then text "logic" <> parens (spText (fromJust (logic d))) <> dot else empty)
-  $$ text "status" <> parens (printLogState (status d)) <> dot
-  $$ text "description" <> parens (spText (desc d)) <> dot
-  $$ (if isJust (date d) then text "date" <> parens (text (fromJust (date d))) <> dot else empty)
-  $$ text "end_of_list."
-
-{- |
-  Helper function. Wraps a String in "{*  *}" as required for some of the
-  description fields.
--}
-spText :: String-> Doc
-spText s = text "{* " <> text s <> text " *}"
-
-{- |
-  Creates a Doc from an 'SPLogState'.
--}
-printLogState :: SPLogState-> Doc
-printLogState s = case s of
-  SPStateSatisfiable   -> text "satisfiable"
-  SPStateUnsatisfiable -> text "unsatisfiable"
-  SPStateUnknown       -> text "unknown"
