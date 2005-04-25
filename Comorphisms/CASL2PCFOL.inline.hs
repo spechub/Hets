0a700aab8abf4c402bef027055759f298182a7f3
Christian Maeder <Christian.Maeder@dfki.de>
2005-04-25 15:43:25 +0000

    avoided equivalence classes computation git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4120 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Comorphisms/CASL2PCFOL.inline.hs
+++ Comorphisms/CASL2PCFOL.inline.hs
@@ -20,7 +20,6 @@ import qualified Common.Lib.Map as Map
 import qualified Common.Lib.Set as Set
 import qualified Common.Lib.Rel as Rel
 import Common.AS_Annotation
-import Common.ListUtils
 import Data.List
 
 -- CASL
@@ -142,14 +141,10 @@ monotonicities sig =
     ++ concatMap (makePredMonos sig) (Map.toList $ predMap sig)
 
 makeMonos :: Sign f e -> (Id, Set.Set OpType) -> [Named (FORMULA f)]
-makeMonos sig (o, ts) = 
-   concatMap (makeEquivMonos o sig) $ equivalence_Classes (leqF sig) 
-             $ Set.toList ts
+makeMonos sig (o, ts) = makeEquivMonos o sig $ Set.toList ts
 
 makePredMonos :: Sign f e -> (Id, Set.Set PredType) -> [Named (FORMULA f)]
-makePredMonos sig (p, ts) = 
-   concatMap (makeEquivPredMonos p sig) $ equivalence_Classes (leqP sig) 
-             $ Set.toList ts
+makePredMonos sig (p, ts) = makeEquivPredMonos p sig $ Set.toList ts
 
 makeEquivMonos :: Id -> Sign f e -> [OpType] -> [Named (FORMULA f)]
 makeEquivMonos o sig ts = 
@@ -168,7 +163,11 @@ makeEquivPredMonos o sig ts =
 makeEquivMono :: Id -> Sign f e -> OpType -> OpType -> [Named (FORMULA f)]
 makeEquivMono o sig o1 o2 =     
       let rs = minimalSupers sig (opRes o1) (opRes o2)
-          args = permute $ zipWith (maximalSubs sig) (opArgs o1) (opArgs o2)
+          a1 = opArgs o1
+          a2 = opArgs o2
+          args = if length a1 == length a2 then 
+                 combine $ zipWith (maximalSubs sig) a1 a2
+                 else []
       in concatMap (makeEquivMonoRs o o1 o2 rs) args 
 
 makeEquivMonoRs :: Id -> OpType -> OpType -> 
@@ -194,8 +193,11 @@ makeEquivMonoR o o1 o2 args res =
 makeEquivPredMono :: Id -> Sign f e -> PredType -> PredType 
                   -> [Named (FORMULA f)]
 makeEquivPredMono o sig o1 o2 =     
-      let args = permute $ zipWith (maximalSubs sig) (predArgs o1) 
-                 $ predArgs o2
+      let a1 = predArgs o1
+          a2 = predArgs o2
+          args = if length a1 == length a2 then 
+                 combine $ zipWith (maximalSubs sig) a1 a2
+                 else []
       in map (makeEquivPred o o1 o2) args 
 
 makeEquivPred :: Id -> PredType -> PredType -> [SORT] -> Named (FORMULA f)
