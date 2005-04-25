dcd3711ceec112fd1f27deb94934d9f47b4071a9
Till Mossakowski <mossakow@iws.cs.uni-magdeburg.de>
2005-04-25 22:23:23 +0000

    documentation git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4126 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Comorphisms.hs
+++ Comorphisms.hs
@@ -1,1 +1,28 @@
+{- |
+
+This folder contains various comorphisms, which are then
+collected to a logic graph in "Comorphisms.LogicGraph".
+The latter is based on the list of logics collected in 	
+"Comorphisms.LogicList".
+
+The individual comorphisms are:
+
+"Comorphisms.CASL2CoCASL"
+"Comorphisms.CASL2CspCASL"
+"Comorphisms.CASL2HasCASL"
+"Comorphisms.CASL2IsabelleHOL"
+"Comorphisms.CASL2Modal"
+"Comorphisms.CASL2PCFOL"
+"Comorphisms.CASL2TopSort"
+"Comorphisms.CoCASL2IsabelleHOL"
+"Comorphisms.CspCASL2Modal"
+"Comorphisms.HasCASL2HasCASL"
+"Comorphisms.HasCASL2Haskell"
+"Comorphisms.HasCASL2IsabelleHOL"
+"Comorphisms.Haskell2IsabelleHOLCF"
+"Comorphisms.Modal2CASL"
+"Comorphisms.PCFOL2FOL"
+
+-}
+
 module Comorphisms where
