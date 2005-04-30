3b0131264d807b989d1e805c4aff0691afba8f51
Heng Jiang <jiang@tzi.de>
2005-04-30 14:18:33 +0000

    removed errors, ignored validation, messages and namespaces git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4166 cec4b9c1-7d33-0410-9eda-942365e851bb

--- OWL_DL/ToHaskellAS.hs
+++ OWL_DL/ToHaskellAS.hs
@@ -2,11 +2,26 @@ module Main where
 
 import OWL_DL.ReadWrite
 import Common.ATerm.Lib
-import System
+import System(getArgs)
 
 main :: IO()
 main =
-    do filename <- getArgs 
-       str <- readFile $ head filename  
-       let atermTable = readATerm str
-       putStrLn $ show $ getATermFull atermTable
+    do filename <- getArgs
+       if null filename then
+          putStrLn "Usage: runhugs ToHaskellAS.hs <filename>"
+          else do 
+                  str <- readFile $ head filename  
+                  let aterm = getATermFull $ readATerm str
+                  case aterm of
+                      AAppl "OWLParserOutput" [valid, msg, ns, onto] _ ->
+                          putStrLn $ show ((fromShATerm $ toATermTable onto)::Ontology)
+                      _ -> error "false file."
+
+processor :: String -> IO()
+processor filename = 
+    do d <- readFile filename
+       let aterm = getATermFull $ readATerm d
+       case aterm of
+          AAppl "OWLParserOutput" [valid, msg, ns, onto] _ ->
+             putStrLn $ show ((fromShATerm $ toATermTable onto)::Ontology)
+          _ -> error "false file."
