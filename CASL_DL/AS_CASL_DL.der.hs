0040ce6dda80a33e3911735bc4ecd9cb955ca8ee
Klaus Luettich <luettich@informatik.uni-bremen.de>
2005-05-04 14:39:21 +0000

    Two preliminary versins of CASL_DLs Abstract Syntax and signature datastructures. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4202 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ CASL_DL/AS_CASL_DL.der.hs
@@ -0,0 +1,45 @@
+{-
+Module      :  $Header$
+Copyright   :  (c) Klaus Lüttich, Uni Bremen 2004
+Licence     :  similar to LGPL, see HetCATS/LICENCE.txt or LIZENZ.txt
+
+Maintainer  :  hets@tzi.de
+Stability   :  provisional
+Portability :  portable
+
+  Abstract syntax for CASL_DL logic extension of CASL
+  Only the added syntax is specified
+-}
+
+module CASL_DL.AS_CASL_DL where
+
+import Common.Id
+import Common.AS_Annotation 
+import CASL.AS_Basic_CASL
+
+-- DrIFT command
+{-! global: UpPos !-}
+
+type DL_BASIC_SPEC = BASIC_SPEC () () DL_FORMULA
+
+type AnDLFORM = Annoted (FORMULA DL_FORMULA)
+
+data CardType = CMin | CMax | CExact deriving (Eq, Ord, Show)
+
+data DL_FORMULA = Cardinality CardType PRED_NAME 
+                              (TERM DL_FORMULA) (TERM DL_FORMULA) [Pos]
+               -- the PRED_NAME refers to a declared binary predicate; 
+               -- the first term is restricted to constructors
+               -- denoting a variable;
+               -- the second term is restricted to an Application denoting 
+               -- a literal of type nonNegativeInteger (Nat)
+	       -- pos: position of keyword, brackets, parens and comma
+             deriving (Eq, Ord, Show)
+
+minCardinalityS,maxCardinalityS,cardinalityS :: String 
+cardinalityS = "cardinality"
+minCardinalityS = "minC" ++ tail cardinalityS
+maxCardinalityS = "maxC" ++ tail cardinalityS
+
+casl_DL_reserved_words :: [String]
+casl_DL_reserved_words = minCardinalityS:maxCardinalityS:cardinalityS:[]
---
+++ CASL_DL/Sign.hs
@@ -0,0 +1,69 @@
+{- |
+Module      :  $Header$
+Copyright   :  (c) Klaus Lüttich, Uni Bremen 2004
+Licence     :  similar to LGPL, see HetCATS/LICENCE.txt or LIZENZ.txt
+
+Maintainer  :  hets@tzi.de
+Stability   :  provisional
+Portability :  portable
+
+   Signatures for modal logic, as extension of CASL signatures.
+-}
+
+module CASL_DL.Sign where
+
+import qualified Common.Lib.Map as Map
+import Common.Id
+import Common.Utils
+
+import CASL.AS_Basic_CASL
+import CASL_DL.AS_CASL_DL
+
+import Data.List
+import Text.XML.HXT.DOM.XmlTreeTypes (QName)          
+
+data CASL_DLSign = 
+    CASL_DLSign { annoProperties  :: Map.Map SIMPLE_ID PropertyType
+                , annoPopertySens :: [AnnoAppl]
+                } deriving (Show, Eq)
+
+data PropertyType = AnnoProperty
+                  | OntoProperty deriving (Show,Eq)
+
+data AnnoAppl = AnnoAppl SIMPLE_ID Id AnnoLiteral 
+              deriving (Show,Eq)
+
+data AnnoLiteral = AL_Term (TERM DL_FORMULA)
+                 | AL_URI  QName
+                 | AL_Id   Id
+              deriving (Show,Eq)
+
+emptyCASL_DLSign :: CASL_DLSign
+emptyCASL_DLSign = CASL_DLSign Map.empty []
+
+addCASL_DLSign :: CASL_DLSign -> CASL_DLSign -> CASL_DLSign
+addCASL_DLSign a b = a
+     { annoProperties = 
+           Map.unionWithKey (throwAnnoError "CASL_DL.Sign.addCASL_DLSign:") 
+                  (annoProperties a) (annoProperties b)
+     , annoPopertySens = union (annoPopertySens a) (annoPopertySens b)
+     } 
+
+throwAnnoError :: String -> SIMPLE_ID 
+               -> PropertyType -> PropertyType -> PropertyType
+throwAnnoError s k e1 e2 = 
+    if e1 == e2 
+       then e1
+       else error (s++" Annotation Properties and Ontology Properties \
+                          \must have distinct names! ("++show k++")")
+
+diffCASL_DLSign :: CASL_DLSign -> CASL_DLSign -> CASL_DLSign
+diffCASL_DLSign a b = a
+     { annoProperties = Map.difference (annoProperties a) (annoProperties b)
+     , annoPopertySens = (annoPopertySens a) \\ (annoPopertySens b)
+     }
+
+isSubCASL_DLSign :: CASL_DLSign -> CASL_DLSign -> Bool
+isSubCASL_DLSign a b = 
+    Map.subset (annoProperties a) (annoProperties b) &&
+    (annoPopertySens a `isSublistOf` annoPopertySens b)
