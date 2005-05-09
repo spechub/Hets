ac04ce60093a9eb5e6117bf9b7749a1586debc6e
Christian Maeder <Christian.Maeder@dfki.de>
2005-05-09 09:24:04 +0000

    recovered unshared ATerms git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4221 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ Common/ATerm/Unshared.hs
@@ -0,0 +1,55 @@
+{- |
+Module      :  $Header$
+Copyright   :  (c) Klaus Lüttich, Uni Bremen 2002-2004
+Licence     :  similar to LGPL, see HetCATS/LICENCE.txt or LIZENZ.txt
+
+Maintainer  :  hets@tzi.de
+Stability   :  provisional
+Portability :  portable
+
+-}
+
+module Common.ATerm.Unshared
+    (ATerm(..),
+     getATermFull,
+     toATermTable)
+    where
+
+import Common.ATerm.AbstractSyntax
+
+data ATerm = AAppl String [ATerm] [ATerm]
+           | AList [ATerm]        [ATerm]
+           | AInt  Integer        [ATerm]
+             deriving (Eq,Ord)
+
+getATermFull :: ATermTable -> ATerm
+getATermFull at = 
+    let t = getATerm at
+    in case t of
+       (ShAInt i as)    -> AInt i (map conv as)
+       (ShAList l as)   -> AList (map conv l) (map conv as)
+       (ShAAppl c l as) -> AAppl c (map conv l) (map conv as)
+    where conv t = getATermFull (getATermByIndex1 t at) 
+
+toATermTable :: ATerm -> ATermTable
+toATermTable at = fst $ addToTable at emptyATermTable
+    where
+    addToTable :: ATerm -> ATermTable -> (ATermTable,Int) 
+    addToTable (AAppl s ats anns) att = 
+        let (att1,ats')  = addToTableList ats att
+            (att2,anns') = addToTableList anns att1
+        in addATerm (ShAAppl s ats' anns') att2
+    addToTable (AList ats anns)   att = 
+        let (att1,ats')  = addToTableList ats att
+            (att2,anns') = addToTableList anns att1
+        in addATerm (ShAList ats' anns') att2
+    addToTable (AInt i anns)      att = 
+        let (att1,anns') = addToTableList anns att
+        in addATerm (ShAInt i anns') att1
+    addToTableList :: [ATerm] -> ATermTable -> (ATermTable,[Int])
+    addToTableList []       att = (att,[])
+    addToTableList (at1:ats) att = 
+        let (att1,i)  = addToTable at1 att
+            (att2,is) = addToTableList ats att1
+        in (att2,i:is)
+
