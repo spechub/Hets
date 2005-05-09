09be7cbcb48dcf26eaeefb637df83027cb842e0b
Christian Maeder <Christian.Maeder@dfki.de>
2005-05-09 09:24:24 +0000

    derived order git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4222 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Common/ATerm/AbstractSyntax.hs
+++ Common/ATerm/AbstractSyntax.hs
@@ -9,20 +9,6 @@ Portability :  portable
 
 -}
 
-{- todo:
-    * Datentyp auf Annotationen umstellen
-      dazu einiges auskommentieren ..done
-      Weiter mit 'writeATerm'  ..done
-  
-    * Zugriffsfunktionen für ATermTable auf Annotationen anpassen ..done
-
-    * Neuen Datentyp für shared ATerms: ShATerm mit Annotationen
-       (ungefähr so: data ShATerm = ShAAppl String [Int]) ..done
-
-    * Umkehrfuntion zu getATermFull programmieren, Signatur und Name 
-      siehe toATermTable
--}
-
 module Common.ATerm.AbstractSyntax 
     (ShATerm(..),
      ATermTable,
@@ -34,28 +20,11 @@ module Common.ATerm.AbstractSyntax
 
 import qualified Common.Lib.Map as Map
 import qualified Common.Lib.Map as DMap
--- import UnsafeCoerce(unsafePtrEq)
 
 data ShATerm = ShAAppl String [Int] [Int]
              | ShAList [Int]        [Int]
              | ShAInt  Integer      [Int] 
---               deriving (Eq,Ord)
-
-compareShATerm :: ShATerm -> ShATerm -> Ordering
-compareShATerm a b = -- if unsafePtrEq a b then EQ else 
-    case (a, b) of
-    (ShAAppl s1 i1 j1, ShAAppl s2 i2 j2) -> compare (s1, i1, j1) (s2, i2, j2)
-    (ShAAppl _ _ _, _) -> LT
-    (ShAList i1 j1, ShAList i2 j2) -> compare (i1, j1) (i2, j2)
-    (ShAList _ _, ShAInt _ _) -> LT
-    (ShAInt i1 j1, ShAInt i2 j2) -> compare (i1, j1) (i2, j2)
-    _ -> GT
-
-instance Eq ShATerm where
-  a == b = compareShATerm a b == EQ
-
-instance Ord ShATerm where
-  compare a b = compareShATerm a b
+               deriving (Eq,Ord)
 
 data ATermTable = ATT !(Map.Map ShATerm Int) !(DMap.Map Int ShATerm) Int
 
