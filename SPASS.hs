bc8e696520fdbd56ec590d1ba795f48a91b30c7b
René Wagner <Rene.Wagner@dfki.de>
2005-05-16 11:31:28 +0000

    preliminary description based on the one from Isabelle.hs git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4279 cec4b9c1-7d33-0410-9eda-942365e851bb

--- SPASS.hs
+++ SPASS.hs
@@ -1,1 +1,24 @@
+{- |
+
+This folder contains the interface to the SPASS theorem prover.
+
+The SPASS project homepage is located at <http://spass.mpi-sb.mpg.de/>.
+
+"SPASS.Sign" provides data structures for SPASS signatures,
+formulae and problems.
+
+The emphasis is on outputting theories
+with the pretty printer ("SPASS.Print"); hence, not only the
+kernel language of SPASS is supported. Because the SPASS
+logic is only used for proving, no parser and static analysis are provided.
+
+"SPASS.Prove" is a non-interactive (SPASS is fully automated) interface to
+the SPASS prover.
+
+"SPASS.Logic_SPASS" provides the SPASS instance of
+type class 'Logic.Logic.Logic'.
+-}
+
+--TODO: reference relevant papers
+
 module SPASS where
