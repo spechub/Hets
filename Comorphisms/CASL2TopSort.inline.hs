af68be22d5ea72f0e71c368631c49d2135dd7ed0
Christian Maeder <Christian.Maeder@dfki.de>
2005-05-18 17:13:56 +0000

    changed empty relation git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4296 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Comorphisms/CASL2TopSort.inline.hs
+++ Comorphisms/CASL2TopSort.inline.hs
@@ -181,7 +181,7 @@ transSig sig
                               `Set.union`
                               (sortSet sig `Set.difference`
                                Rel.keysSet subSortMap) 
-                        , sortRel = Rel.fromList []
+                        , sortRel = Rel.empty
                         , opMap   = transOpMap subSortMap (opMap sig)
                         , assocOps= transOpMap subSortMap (assocOps sig)
                         , predMap = newPredMap
