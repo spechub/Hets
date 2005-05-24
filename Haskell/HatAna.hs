5a3cb88234f5d5054db903bec4d04ba8130220af
Christian Maeder <Christian.Maeder@dfki.de>
2005-05-24 12:27:24 +0000

    type Instance changed git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4317 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Haskell/HatAna.hs
+++ Haskell/HatAna.hs
@@ -75,7 +75,7 @@ instance PrettyPrint Sign where
                         values = vs, fixities = fs, scope = sc }
         = text "{-" $$ (if null is then empty else
               text "instances:" $$ 
-                   vcat (map (text . pp) is)) $$ 
+                   vcat (map (text . show) is)) $$ 
           (if Map.null ts then empty else
               text "\ntypes:" $$ 
                    vcat (map (text . pp) 
