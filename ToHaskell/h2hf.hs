4d00f5283ed21ad24a6f47174b867e4e58b3e21c
Paolo Torrini <paolot@informatik.uni-bremen.de>
2005-05-25 15:11:08 +0000

    executable source calling directly Hs2HOLCF, to translate Haskell into Isabelle-HOLCF while minimising dependency on Hets git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4327 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ ToHaskell/h2hf.hs
@@ -0,0 +1,65 @@
+{- |
+Module      :  $Header$
+Copyright   :  (c) Christian Maeder, Uni Bremen 2002-2004
+Licence     :  similar to LGPL, see HetCATS/LICENCE.txt or LIZENZ.txt
+
+Maintainer  :  hets@tzi.de
+Stability   :  experimental
+Portability :  portable 
+   
+test translation
+-}
+
+module Main where
+
+import System.Environment
+
+import Text.ParserCombinators.Parsec
+import Common.Result
+import Common.AnnoState
+import Common.AS_Annotation
+import Common.GlobalAnnotations
+-- import Common.PrettyPrint
+import Common.Lib.Pretty
+
+-- import Comorphisms.HasCASL2Haskell
+import Comorphisms.Hs2HOLCF
+
+import Isabelle.CreateThy
+import Isabelle.IsaSign as IsaSign
+
+import Haskell.HatAna as HatAna
+import Haskell.HatParser
+
+-- import HasCASL.Le
+-- import HasCASL.AsToLe
+-- import HasCASL.ParseItem(basicSpec)
+-- import HasCASL.ProgEq
+
+hParser :: AParser () (IsaSign.Sign, [Named IsaSign.Sentence])
+hParser = do 
+   b <- hatParser
+   let res@(Result _ m) = do 
+          (_, _, sig, sens) <- hatAna (b, HatAna.emptySign, emptyGlobalAnnos)
+--          sens <- return [x | Named _ True x <- nsens]
+          transTheory sig sens
+   case m of 
+      Nothing -> error $ show res
+      Just x -> return x
+
+main :: IO ()
+main = do l <- getArgs
+	  if length l >= 1 then
+	     do s <- readFile $ head l
+		let r = runParser hParser (emptyAnnos ()) (head l) s 
+	        case r of 
+		       Right (sig, hs) -> let 
+                         tn = (takeWhile (/= '.') $ reverse (takeWhile (/= '/') $ reverse $ show $ head l))
+                            ++ "_theory" 
+                         doc = text "theory" <+> text tn <+> text "=" $$
+                            createTheoryText sig hs
+                         in writeFile (tn ++ ".thy") (shows doc "\n")
+		       Left err -> putStrLn $ show err
+	   else putStrLn "missing argument"
+--       _ -> return ()
+
