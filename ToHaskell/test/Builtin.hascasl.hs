9efe0ca32bd86630c33e73d5c7bd3f1c0642ebff
Christian Maeder <Christian.Maeder@dfki.de>
2005-05-30 15:55:20 +0000

    translate builtin ops git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4342 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ ToHaskell/test/Builtin.hascasl.hs
@@ -0,0 +1,28 @@
+{-
+
+types:
+A__dummy :: (*, data)
+
+values:
+p :: A__dummy -> Bool
+A__dummy :: A__dummy
+
+scope:
+Prelude.A__dummy |-> Prelude.A__dummy, Type [A__dummy] []
+Prelude.A__dummy |-> Prelude.A__dummy, con of A__dummy
+Prelude.p |-> Prelude.p, Value
+A__dummy |-> Prelude.A__dummy, Type [A__dummy] []
+A__dummy |-> Prelude.A__dummy, con of A__dummy
+p |-> Prelude.p, Value
+-}
+module Dummy where
+data A__dummy = A__dummy
+p :: A__dummy -> Bool
+p x = True
+p x = False
+p x =   (\ (a, b, c) -> if b then a else c)
+            (p x,
+             uncurry{-Bool Bool Bool-} (||)
+                 (flip{-Bool Bool Bool-} seq{-Bool Bool-} True (p x),
+                  False),
+             p x)
