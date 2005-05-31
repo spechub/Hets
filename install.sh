2b7c1b16ebf5159361f25b155e128805537bd653
Christian Maeder <Christian.Maeder@dfki.de>
2005-05-31 12:02:25 +0000

    removed linking and added src-distribution git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4344 cec4b9c1-7d33-0410-9eda-942365e851bb

--- install.sh
+++ install.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
-#change daily snapshot into a release 
-#make sure the file version_nr exists
+#change daily snapshot into a backup or release
+#the first argument should be a version number or a date  
 
 INSTALLDIR=/home/www/agbkb/forschung/formal_methods/CoFI/hets 
-VERSION=`cat version_nr`
+VERSION=$1
 for i in linux solaris mac
 do
-  (cd $INSTALLDIR/$i; cp -p daily/hets versions/hets-$VERSION;
-      rm  -f hets; ln -s versions/hets-$VERSION hets)
+  (cd $INSTALLDIR/$i; cp -p daily/hets versions/hets-$VERSION)
 done
       
-  
+(cd $INSTALLDIR/src-distribution; cp -p daily/Het*.t* versions/Hets-src-$VERSION.tgz) 
 
