5915d4c7483046bc80e6d8a5c038b51d7ab31e4b
Christian Maeder <Christian.Maeder@dfki.de>
2005-06-02 16:19:14 +0000

    splitted Proofs.Proofs git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4360 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Proofs.hs
+++ Proofs.hs
@@ -2,8 +2,9 @@
   Proofs in development graphs.
    Follows Sect. IV:4.4 of the CASL Reference Manual.
 
-"Proofs.Proofs" main module for proof calculus
+"Proofs.InferBasic" main module for proof calculus
 
+"Proofs.Proofs" data types for proofs 
 -}
 
 module Proofs where
