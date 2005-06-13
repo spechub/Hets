b06a3cc7b4530695532f2a42e1229f220b49276e
Christian Maeder <Christian.Maeder@dfki.de>
2005-06-13 13:41:43 +0000

    __* empty log message __* git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4409 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ ATC/BasicProof.hs
@@ -0,0 +1,42 @@
+module ATC.BasicProof where
+
+import Proofs.Proofs
+import Common.ATerm.Lib
+import Comorphisms.LogicGraph
+import Logic.Logic
+import ATC.Prover()
+
+instance ShATermConvertible BasicProof where
+     toShATerm att0 (BasicProof lid p) = 
+	 case toShATerm att0 (language_name lid) of { (att1,i1) ->
+         case toShATerm att1 p of { (att2,i2) ->
+            addATerm (ShAAppl "BasicProof" [i1,i2] []) att2}}
+     toShATerm att0 Guessed =
+         case toShATerm att0 (show Guessed) of { (att1, i1) ->
+            addATerm (ShAAppl "BasicProof" [i1] []) att1}
+     toShATerm att0 Conjectured =
+         case toShATerm att0 (show Conjectured) of { (att1, i1) ->
+            addATerm (ShAAppl "BasicProof" [i1] []) att1}
+     toShATerm att0 Handwritten =
+         case toShATerm att0 (show Handwritten) of { (att1, i1) ->
+            addATerm (ShAAppl "BasicProof" [i1] []) att1}
+
+     fromShATerm att = 
+         case aterm of
+	    (ShAAppl "BasicProof" [i1,i2] _) ->
+	       case fromShATerm (getATermByIndex1 i1 att) of { i1' ->
+               case getATermByIndex1 i2 att of { att' ->
+               case lookupLogic_in_LG 
+                        ("ShATermConvertible BasicProof") i1'  of {
+                    Logic lid -> (BasicProof lid (fromShATerm att'))}}}
+            v@(ShAAppl "BasicProof" [i1] _) ->
+               case fromShATerm (getATermByIndex1 i1 att) of { i1' ->
+               case i1' of
+                 "Guessed" -> Guessed
+                 "Conjectured" -> Conjectured
+                 "Handwritten" -> Handwritten
+                 _ -> fromShATermError "BasicProof" v}
+	    u     -> fromShATermError "BasicProof" u
+         where
+         aterm = getATerm att
+
