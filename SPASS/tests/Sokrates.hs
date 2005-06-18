135c06ffde57eac8671a977d812d8037f38616d1
René Wagner <Rene.Wagner@dfki.de>
2005-06-18 11:23:16 +0000

    SPFormula is a Named SPTerm now git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4461 cec4b9c1-7d33-0410-9eda-942365e851bb

--- SPASS/tests/Sokrates.hs
+++ SPASS/tests/Sokrates.hs
@@ -7,8 +7,8 @@ Maintainer  :  rwagner@tzi.de
 Stability   :  provisional
 Portability :  unknown
 
-   The Sokrates example from the SPASS tutorial at
-   <http://spass.mpi-sb.mpg.de/tutorial/> as a Haskell data structure.
+The Sokrates example from the SPASS tutorial at
+<http://spass.mpi-sb.mpg.de/tutorial/> as a Haskell data structure.
 
 -}
 
@@ -49,20 +49,23 @@ sokratesAxiomFormulae :: SPFormulaList
 sokratesAxiomFormulae = SPFormulaList {originType= SPOriginAxioms,
                                        formulae= [f1, f2] }
   where
-    f1 = SPFormula {formulaLabel = "1",
-                    formulaTerm  = SPComplexTerm {symbol= SPCustomSymbol "Human",
-                                                  arguments= [SPSimpleTerm (SPCustomSymbol "sokrates")] } }
-    f2 = SPFormula {formulaLabel = "2",
-                   formulaTerm   = SPQuantTerm {quantSym= SPForall, 
-                                                termTermList= [SPSimpleTerm (SPCustomSymbol "x")],
-                                                termTerm= SPComplexTerm { symbol= SPImplies,
-                                                                          arguments= [SPComplexTerm {symbol= SPCustomSymbol "Human", arguments= [SPSimpleTerm (SPCustomSymbol "x")]},
-                                                                                      SPComplexTerm {symbol= SPCustomSymbol "Mortal", arguments= [SPSimpleTerm (SPCustomSymbol "x")]}] } } }
+    f1 = NamedSen { senName  = "f1",
+                    isAxiom  = True,
+                    sentence = SPComplexTerm {symbol= SPCustomSymbol "Human",
+                                              arguments= [SPSimpleTerm (SPCustomSymbol "sokrates")] } }
+    f2 = NamedSen { senName  = "f2",
+                    isAxiom  = True,
+                    sentence = SPQuantTerm {quantSym= SPForall, 
+                                            termTermList= [SPSimpleTerm (SPCustomSymbol "x")],
+                                            termTerm= SPComplexTerm { symbol= SPImplies,
+                                                                      arguments= [SPComplexTerm {symbol= SPCustomSymbol "Human", arguments= [SPSimpleTerm (SPCustomSymbol "x")]},
+                                                                                  SPComplexTerm {symbol= SPCustomSymbol "Mortal", arguments= [SPSimpleTerm (SPCustomSymbol "x")]}] } } }
 
 sokratesConjectureFormulae :: SPFormulaList
 sokratesConjectureFormulae = SPFormulaList { originType= SPOriginConjectures,
                                              formulae= [f3] }
   where
-    f3 = SPFormula {formulaLabel = "3",
-                    formulaTerm  = SPComplexTerm {symbol= SPCustomSymbol "Mortal",
-                                                  arguments= [SPSimpleTerm (SPCustomSymbol "sokrates")] } }
+    f3 = NamedSen { senName  = "f3",
+                    isAxiom  = False,
+                    sentence = SPComplexTerm {symbol= SPCustomSymbol "Mortal",
+                                              arguments= [SPSimpleTerm (SPCustomSymbol "sokrates")] } }
