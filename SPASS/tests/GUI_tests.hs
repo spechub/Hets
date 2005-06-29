352ca8f780511079c82f079f9789f26d6a45b62d
Klaus Luettich <luettich@informatik.uni-bremen.de>
2005-06-29 16:49:18 +0000

    corrected test1 git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4529 cec4b9c1-7d33-0410-9eda-942365e851bb

--- SPASS/tests/GUI_tests.hs
+++ SPASS/tests/GUI_tests.hs
@@ -2,6 +2,8 @@
 -- | some GUI tests to use from ghci
 module GUI_tests where 
 
+import qualified Common.Lib.Map as Map 
+
 import Common.AS_Annotation
 import Logic.Prover
 
@@ -9,9 +11,28 @@ import SPASS.Sign
 import SPASS.Prove
 
 
+printStatus :: IO [Proof_status ()] -> IO ()
+printStatus act = do st <- act
+                     putStrLn (show st)
+
+sign1 :: SPASS.Sign.Sign
+sign1 = emptySign {sortMap = Map.insert "s" Nothing Map.empty,
+                  predMap = Map.fromList [("P",["s"]),("Q",["s"])] }
+
 term_x :: SPTerm 
 term_x = SPSimpleTerm (SPCustomSymbol "x")
 
-test1 = spassProveGUI "Foo" (Theory SPASS.Sign.emptySign 
-      [NamedSen "Ax" True (SPQuantTerm SPForall [term_x] (SPComplexTerm SPEquiv [SPComplexTerm (SPCustomSymbol "P") [term_x],SPComplexTerm (SPCustomSymbol "Q") [term_x]])), 
-       NamedSen "Go" False (SPQuantTerm SPForall [term_x] (SPComplexTerm SPImplies [SPComplexTerm (SPCustomSymbol "Q") [term_x],SPComplexTerm (SPCustomSymbol "P") [term_x] ]))])
+axiom1 :: Named SPTerm
+axiom1 = NamedSen "Ax" True (SPQuantTerm SPForall [term_x] (SPComplexTerm SPEquiv [SPComplexTerm (SPCustomSymbol "P") [term_x],SPComplexTerm (SPCustomSymbol "Q") [term_x]]))
+
+goal1 :: Named SPTerm
+goal1 = NamedSen "Go" False (SPQuantTerm SPForall [term_x] (SPComplexTerm SPImplies [SPComplexTerm (SPCustomSymbol "Q") [term_x],SPComplexTerm (SPCustomSymbol "P") [term_x] ]))
+
+theory1 :: Theory SPASS.Sign.Sign SPTerm
+theory1 = (Theory sign1 [axiom1, goal1])
+
+test1 :: IO ()
+test1 = printStatus (spassProveGUI "Foo" theory1)
+
+test1b :: IO ()
+test1b = printStatus (spassProveBatch "Foo" theory1)
