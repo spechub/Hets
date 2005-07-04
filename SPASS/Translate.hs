cd58b69b2ac1a723aef1c424c259de31b773f23b
Klaus Luettich <luettich@informatik.uni-bremen.de>
2005-07-04 11:47:39 +0000

    Functions for the translation of SPASS Identifiewrs git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4550 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ SPASS/Translate.hs
@@ -0,0 +1,57 @@
+{- |
+Module      :  $Header$
+Copyright   :  (c) Klaus Lüttich, Uni Bremen 2005
+License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
+
+Maintainer  :  luettich@tzi.de
+Stability   :  provisional
+Portability :  portable
+
+ collection of functions used by Comorphisms.CASL2SPASS and SPASS.Prove
+ for the translation of CASL identifiers and axiom labels into
+ valid SPASS identifiers 
+-}
+
+module SPASS.Translate where 
+
+import Data.Char
+
+import Common.Id
+import Common.PrettyPrint
+import qualified Common.Lib.Set as Set
+import qualified Common.Lib.Map as Map
+
+import SPASS.Sign
+import SPASS.Print ()
+
+import Common.ProofUtils
+
+-- | collect all keywords of SPASS
+reservedWords :: Set.Set SPIdentifier 
+reservedWords = Set.fromList (map ((flip showPretty) "") [SPEqual
+                                          , SPTrue 
+                                          , SPFalse 
+                                          , SPOr 
+                                          , SPAnd
+                                          , SPNot
+                                          , SPImplies
+                                          , SPImplied
+                                          ,SPEquiv])
+
+
+transSenName :: String -> String
+transSenName = transId . simpleIdToId . mkSimpleId
+
+transId :: Id -> SPIdentifier
+transId iden 
+    | checkIdentifier str = if Set.member str reservedWords 
+                            then "X_"++str
+                            else str
+    | otherwise = concatMap transToSPChar str
+    where str = show iden
+
+transToSPChar :: Char -> SPIdentifier
+transToSPChar c
+    | checkSPChar c = [c]
+    | otherwise = Map.findWithDefault def c charMap
+    where def = "Slash_"++ show (ord c)
