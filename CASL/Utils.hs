950cfa4052e5934bbd4e4e51d72b7b5ebd09598c
Christian Maeder <Christian.Maeder@dfki.de>
2005-07-08 17:20:20 +0000

    used Fold for noMixfix and replacePropPredication git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4598 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CASL/Utils.hs
+++ CASL/Utils.hs
@@ -21,8 +21,8 @@ import qualified Common.Lib.Set as Set
 import qualified Common.Lib.Map as Map
 
 import Common.Id
-
 import CASL.AS_Basic_CASL
+import CASL.Fold
 
 -- |
 -- replacePropPredication replaces a propositional predication of a
@@ -38,89 +38,60 @@ replacePropPredication :: Maybe (PRED_NAME,VAR,TERM f)
 		       -> FORMULA f -- ^ Formula to insert
 		       -> FORMULA f -- ^ Formula with placeholder
 		       -> FORMULA f
-replacePropPredication mTerm pSymb frmIns frmToChn =
-    case frmToChn of
-    Quantification q vs frm ps ->
-	Quantification q vs (replacePropPredication mTerm pSymb frmIns frm) ps
-    Conjunction fs ps -> 
-	Conjunction (map (replacePropPredication mTerm pSymb frmIns) fs) ps 
-    Implication f1 f2 b ps ->
-	Implication (replacePropPredication mTerm pSymb frmIns f1) 
-		    (replacePropPredication mTerm pSymb frmIns f2) b ps
-    Predication (Qual_pred_name symb (Pred_type [] []) []) [] [] 
-	| symb == pSymb -> frmIns
-    Predication qpn@(Qual_pred_name symb _ _) (arg1:args) ps
-	| (isJust mTerm) && symb == pSymbT -> 
-	    case arg1 of
-	    Sorted_term (Qual_var v1 _ _) _ _ 
-		| v1 == var -> Predication qpn (term:args) ps 
-	    _ -> error "replacePropPredication: unknown term to replace"
-          where (pSymbT,var,term) = fromJust mTerm 
-    p@(Predication _ _ _) -> p 
-    _ -> error "replacePropPredication: unknown formula to replace"
+replacePropPredication mTerm pSymb frmIns =
+    mapFormula (idRecord $ const $ error 
+             "replacePropPredication: unexpected extended formula")
+        { foldPredication = \ (Predication qpn ts ps) _ _ _ -> 
+              let (pSymbT,var,term) = fromJust mTerm in case qpn of
+              Qual_pred_name symb (Pred_type s _) _ 
+                 | symb == pSymb && null ts && null s -> frmIns
+                 | isJust mTerm && symb == pSymbT -> case ts of
+                   Sorted_term (Qual_var v1 _ _) _ _ : args 
+                       |  v1 == var -> Predication qpn (term:args) ps 
+	           _ -> error "replacePropPredication: unknown term to replace"
+              _ -> error "replacePropPredication: unknown formula to replace"
+         , foldConditional = \ t _ _ _ _ -> t }
+
+
+noMixfixRecord :: (f -> Bool) -> Record f Bool Bool
+noMixfixRecord cef = (constRecord cef and True)
+    { foldMixfix_formula = \ _ _ -> False
+    , foldMixfix_qual_pred = \ _ _ -> False
+    , foldMixfix_term = \ _ _ -> False
+    , foldMixfix_token = \ _ _ -> False
+    , foldMixfix_sorted_term = \ _ _ _ -> False
+    , foldMixfix_cast = \ _ _ _ -> False
+    , foldMixfix_parenthesized = \ _ _ _ -> False
+    , foldMixfix_bracketed = \ _ _ _ -> False
+    , foldMixfix_braced = \ _ _ _ -> False
+    }
 
 -- | 
 -- noMixfixF checks a 'FORMULA' f for Mixfix_*. 
 -- A logic specific helper has to be provided for checking the f.
-
-noMixfixF :: (Show f) => (f -> Bool) -> FORMULA f -> Bool
-noMixfixF cef form = 
-    let rec = noMixfixF cef 
-	tchk = noMixfixT cef
-    in
-    case form of
-    ExtFORMULA f -> cef f
-    Quantification _ _ f _ -> rec f
-    Conjunction fs _ -> and $ map rec fs
-    Disjunction fs _ -> and $ map rec fs
-    Implication f1 f2 _ _ -> rec f1 && rec f2
-    Equivalence f1 f2 _   -> rec f1 && rec f2
-    Negation f _ -> rec f
-    Predication _ ts _  -> and $ map tchk ts
-    Definedness t _ -> tchk t
-    Existl_equation t1 t2 _ -> tchk t1 && tchk t2
-    Strong_equation t1 t2 _ -> tchk t1 && tchk t2
-    Membership t _ _ -> tchk t
-    Mixfix_formula _ -> False
-    Unparsed_formula _ _ -> error ("CASL.Utils.noMixfixF: should not occur: "
-				   ++show form)
-    _ -> True -- {True,False}_atom
+noMixfixF :: (f -> Bool) -> FORMULA f -> Bool
+noMixfixF = mapFormula . noMixfixRecord
 
 -- | 
 -- noMixfixT checks a 'TERM' f for Mixfix_*. 
 -- A logic specific helper has to be provided for checking the f.
-    
-noMixfixT :: (Show f) => (f -> Bool) -> TERM f -> Bool
-noMixfixT cef term = 
-    let fchk = noMixfixF cef 
-	tchk = noMixfixT cef
-    in
-    case term of
-    Simple_id _ -> True
-    Qual_var _ _ _ -> True
-    Application _ ts _ -> and $ map tchk ts
-    Sorted_term t _ _  -> tchk t
-    Cast t _ _ -> tchk t
-    Conditional t1 f t2 _ -> tchk t1 && fchk f && tchk t2
-    Unparsed_term _ _ -> error ("CASL.Utils.noMixfixT: should not occur: "
-				   ++show term)
-    _ -> False
+noMixfixT :: (f -> Bool) -> TERM f -> Bool
+noMixfixT = mapTerm . noMixfixRecord
 
 -- |
 -- isMixfixTerm checks the 'TERM' f for Mixfix_*, 
 -- but performs no recusive lookup
-isMixfixTerm :: Show f => TERM f -> Bool
+isMixfixTerm :: TERM f -> Bool
 isMixfixTerm term =
     case term of
-    Simple_id _ -> False
+    Simple_id _ -> False -- error "CASL.Utils.isMixfixTerm"
     Qual_var _ _ _ -> False
     Application _ _ _ -> False
     Sorted_term _ _ _  -> False
     Cast _ _ _ -> False
     Conditional _ _ _ _ -> False
-    Unparsed_term _ _ -> 
-        error ("CASL.Utils.isMixfixTerm: should not occur: "
-	       ++show term)
+    Unparsed_term s _ -> 
+        error $ "CASL.Utils.isMixfixTerm: should not occur: " ++ s
     _ -> True
 
 type FreshVARMap f = Map.Map VAR (TERM f)
