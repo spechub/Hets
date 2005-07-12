c58a94c44b76b072ace930f2126c889c0b64cb2a
Christian Maeder <Christian.Maeder@dfki.de>
2005-07-12 12:02:37 +0000

    added year 2005 git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4630 cec4b9c1-7d33-0410-9eda-942365e851bb

--- HasCASL/As.der.hs
+++ HasCASL/As.der.hs
@@ -1,6 +1,6 @@
 {- |
 Module      :  $Header$
-Copyright   :  (c) Christian Maeder and Uni Bremen 2003
+Copyright   :  (c) Christian Maeder and Uni Bremen 2003-2005
 License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
 
 Maintainer  :  maeder@tzi.de
