6db74966feafb10defa39f689c1b7d176432a9d8
Christian Maeder <Christian.Maeder@dfki.de>
2005-07-12 11:08:38 +0000

    commented git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4627 cec4b9c1-7d33-0410-9eda-942365e851bb

--- HasCASL/As.der.hs
+++ HasCASL/As.der.hs
@@ -23,9 +23,11 @@ import HasCASL.HToken
 -- * abstract syntax entities 
 {-! global: UpPos !-}
 
+-- | annotated basic items
 data BasicSpec = BasicSpec [Annoted BasicItem]
                   deriving (Show, Eq)
 
+-- | the possible items
 data BasicItem = SigItems SigItems
                | ProgItems [Annoted ProgEq] [Pos]
                -- pos "program", dots
@@ -44,6 +46,7 @@ data BasicItem = SigItems SigItems
                -- pos "internal" "{", ";"s, "}"
                  deriving (Show, Eq)
 
+-- | signature items types and functions
 data SigItems = TypeItems Instance [Annoted TypeItem] [Pos] -- including sort
               -- pos "type", ";"s
               | OpItems OpBrand [Annoted OpItem] [Pos]
@@ -59,6 +62,7 @@ instance Show OpBrand where
         Op -> opS
         Fun -> functS
 
+-- | test if the function was declared as predicate
 isPred :: OpBrand -> Bool
 isPred b = case b of Pred -> True
                      _ -> False
@@ -71,14 +75,17 @@ instance Show Instance where
         Instance -> instanceS
         Plain -> ""
 
+-- | a class item
 data ClassItem = ClassItem ClassDecl [Annoted BasicItem] [Pos] 
                  -- pos "{", ";"s "}"
                  deriving (Show, Eq)
 
+-- | declaring class identifiers
 data ClassDecl = ClassDecl [ClassId] Kind [Pos]
                -- pos ","s
                  deriving (Show, Eq)
-                          
+
+-- | co- or contra- variance indicator                          
 data Variance = CoVar | ContraVar deriving (Eq, Ord)
 
 instance Show Variance where
@@ -86,7 +93,7 @@ instance Show Variance where
         CoVar -> plusS
         ContraVar -> minusS
 
--- | kind or an extended kind
+-- | (higher) kind or an extended kind (with a variance)
 data Kind = ClassKind ClassId           -- ^ plus the declared kind
           | FunKind Kind Kind [Pos]     -- ^ only argument may be an 'ExtKind' 
             -- pos "->" 
@@ -94,6 +101,7 @@ data Kind = ClassKind ClassId           -- ^ plus the declared kind
              -- pos "+" or "-" 
             deriving Show
 
+-- | the string for the universe type
 typeUniverseS :: String 
 typeUniverseS = "Type"
 
@@ -119,6 +127,7 @@ prodKind :: Int -> Kind
 prodKind n = if n > 1 then mkFunKind (replicate n starPlus) star
              else error "prodKind"
 
+-- | the possible type items 
 data TypeItem  = TypeDecl [TypePattern] Kind [Pos]
                -- pos ","s
                | SubtypeDecl [TypePattern] Type [Pos]
@@ -135,6 +144,7 @@ data TypeItem  = TypeDecl [TypePattern] Kind [Pos]
 -- | a tuple pattern for 'SubtypeDefn' 
 data Vars = Var Var | VarTuple [Vars] [Pos] deriving Show
 
+-- | the lhs of most type items 
 data TypePattern = TypePattern TypeId [TypeArg] [Pos]
                  -- pos "("s, ")"s 
                  | TypePatternToken Token
@@ -145,18 +155,22 @@ data TypePattern = TypePattern TypeId [TypeArg] [Pos]
                  -- pos "(", ")"
                    deriving (Show, Eq)
 
+-- | a type synonym for raw kinds (just for readability)
 type RawKind = Kind
 
+-- | types based on variable or constructor names and applications
 data Type = TypeName TypeId RawKind Int  -- (Int == 0 means constructor,
                                       -- negative are bound variables)
           | TypeAppl Type Type
-          | ExpandedType Type Type
+          | ExpandedType Type Type    -- an alias type with its expansion
+            -- only the following variants are parsed
+          | KindedType Type Kind [Pos]
           | TypeToken Token
           | BracketType BracketKind [Type] [Pos]
           -- pos "," (between type arguments)
-          | KindedType Type Kind [Pos]
           -- pos ":"
           | MixfixType [Type] 
+          -- the following variants should be convert to type applications
           | LazyType Type [Pos]
           -- pos "?"
           | ProductType [Type] [Pos]
@@ -165,17 +179,20 @@ data Type = TypeName TypeId RawKind Int  -- (Int == 0 means constructor,
           -- pos arrow
             deriving Show
 
+-- | through away the user's type alias
 unalias :: Type -> Type
 unalias ty = case ty of 
     ExpandedType _ t -> unalias t 
     _ -> ty
 
+-- | construct a product type
 mkProductType :: [Type] -> [Pos] -> Type
 mkProductType ts ps = case ts of
     [] -> unitType
     [t] -> t
     _ -> ProductType ts ps
 
+-- | the builtin function arrows
 data Arrow = FunArr| PFunArr | ContFunArr | PContFunArr 
              deriving (Eq, Ord)
 
@@ -186,48 +203,63 @@ instance Show Arrow where
         ContFunArr -> contFun
         PContFunArr -> pContFun 
 
+-- | construct an infix identifier for a function arrow
 arrowId :: Arrow -> Id
 arrowId a = mkId $ map mkSimpleId [place, show a, place]
 
+-- | construct a mixfix product identifier with n places 
 productId :: Int -> Id
 productId n = if n > 1 then
   mkId $ map mkSimpleId $ place : concat (replicate (n-1) [prodS, place])
   else error "productId"
 
--- no curried notation for bound variables 
+{- | a type with bound type variables. The bound variables within the
+scheme should have negative numbers in the order given by the type
+argument list. The type arguments store proper kinds (including
+downsets) whereas the kind within the type names are only raw
+kinds. -}
 data TypeScheme = TypeScheme [TypeArg] Type [Pos]
                 -- pos "forall", ";"s,  dot (singleton list)
                 -- pos "\" "("s, ")"s, dot for type aliases
                   deriving Show
 
+-- | convert a type with unbound variables to a scheme
 simpleTypeScheme :: Type -> TypeScheme
 simpleTypeScheme t = TypeScheme [] t []
 
+-- | the name for the Unit (or empty product) type 
 unitTypeS :: String
 unitTypeS = "Unit"
 
+-- | the identifier for the Unit type
 unitTypeId :: Id
 unitTypeId = simpleIdToId $ mkSimpleId unitTypeS
 
+-- | the Unit type (name)
 unitType :: Type 
 unitType = TypeName unitTypeId star 0
-              -- ProductType [] [] 
 
+-- | add an additional Unit type argument to a type 
 liftType :: Type -> [Pos] -> Type
 liftType t qs = FunType unitType PFunArr t qs
 
-mapTypeOfScheme :: (Type -> Type) -> TypeScheme -> TypeScheme
-mapTypeOfScheme f (TypeScheme args t ps) =
-    TypeScheme args (f t) ps
-
-predTypeScheme :: TypeScheme -> TypeScheme
-predTypeScheme = mapTypeOfScheme predType
-
+{- | add the Unit type as result type or convert a parsed empty tuple
+   to the unit type -}
 predType :: Type -> Type
 predType t = case t of
                     BracketType Parens [] _ -> unitType
                     _ -> FunType t PFunArr unitType []
 
+-- | change the type within a scheme
+mapTypeOfScheme :: (Type -> Type) -> TypeScheme -> TypeScheme
+mapTypeOfScheme f (TypeScheme args t ps) =
+    TypeScheme args (f t) ps
+
+-- | change the type of the scheme to a 'predType'
+predTypeScheme :: TypeScheme -> TypeScheme
+predTypeScheme = mapTypeOfScheme predType
+
+-- | indicator for partial or total functions
 data Partiality = Partial | Total deriving (Eq, Ord)
 
 instance Show Partiality where
@@ -235,12 +267,14 @@ instance Show Partiality where
         Partial -> quMark
         Total -> exMark
 
+-- | function declarations or definitions
 data OpItem = OpDecl [OpId] TypeScheme [OpAttr] [Pos]
                -- pos ","s, ":", ","s, "assoc", "comm", "idem", "unit"
             | OpDefn OpId [[VarDecl]] TypeScheme Partiality Term [Pos]
                -- pos "("s, ";"s, ")"s, ":" or ":?", "="
               deriving (Show, Eq)
 
+-- | attributes without arguments for binary functions 
 data BinOpAttr = Assoc | Comm | Idem deriving Eq
 
 instance Show BinOpAttr where
@@ -249,9 +283,11 @@ instance Show BinOpAttr where
         Comm -> commS
         Idem -> idemS
 
+-- | possible function attributes (including a term as a unit element)
 data OpAttr = BinOpAttr BinOpAttr [Pos] 
             | UnitOpAttr Term [Pos] deriving Show
 
+-- | a polymorphic data type declaration with a deriving clause
 data DatatypeDecl = DatatypeDecl 
                     TypePattern 
                     Kind
@@ -261,17 +297,23 @@ data DatatypeDecl = DatatypeDecl
                      -- pos "::=", "|"s, "deriving"
                      deriving (Show, Eq)
 
+{- | Alternatives are subtypes or a constructor with a list of
+(curried) tuples as arguments. Only the components of the first tuple
+can be addressed by the places of the mixfix constructor. -}
 data Alternative = Constructor UninstOpId [[Component]] Partiality [Pos]
                    -- pos: "("s, ";"s, ")"s, "?"
                  | Subtype [Type] [Pos]
                    -- pos: "type", ","s
                    deriving Show
 
+{- | A component is a type with on optional (only pre- or postfix) 
+   selector function. -}
 data Component = Selector UninstOpId Partiality Type SeparatorKind [Pos] 
                 -- pos ",", ":" or ":?"
                 | NoSelector Type
                   deriving Show
 
+-- | the possible quantifiers
 data Quantifier = Universal | Existential | Unique
                   deriving (Eq, Ord)
 
@@ -281,6 +323,7 @@ instance Show Quantifier where
         Existential -> existsS 
         Unique -> existsS ++ exMark
 
+-- | the possibly type annotations of terms
 data TypeQual = OfType | AsType | InType | Inferred deriving (Eq, Ord)
 
 instance Show TypeQual where
@@ -290,30 +333,32 @@ instance Show TypeQual where
         InType -> inS
         Inferred -> colonS
 
+-- | an indicator of (otherwise equivalent) let or where equations
 data LetBrand = Let | Where | Program deriving (Show, Eq, Ord)
 
+-- | the possible kinds of brackets (that should match when parsed) 
 data BracketKind = Parens | Squares | Braces deriving (Show, Eq, Ord)
 
+-- | the brackets as strings for printing
 getBrackets :: BracketKind -> (String, String)
 getBrackets b = case b of
                        Parens -> ("(", ")")
                        Squares -> ("[", "]")
                        Braces -> ("{", "}")
 
--- parse quantified formulae as terms first
--- eases also parsing of formulae in parenthesis
-
+{- | The possible terms and patterns. Formulas are also kept as terms. Local variables and constants are kept separatetly. The variant 'ResolvedMixTerm' is an intermediate representation for type checking only. -}
 data Term = QualVar VarDecl
           -- pos "(", "var", ":", ")"
           | QualOp OpBrand InstOpId TypeScheme [Pos]
           -- pos "(", "op", ":", ")" 
-          | ResolvedMixTerm Id [Term] [Pos]
           | ApplTerm Term Term [Pos]  -- analysed
           -- pos?
-          | TupleTerm [Term] [Pos]
+          | TupleTerm [Term] [Pos]    -- special application
           -- pos "(", ","s, ")"
           | TypedTerm Term TypeQual Type [Pos]
           -- pos ":", "as" or "in"
+          | AsPattern VarDecl Pattern [Pos]          
+          -- pos "@"
           | QuantifiedTerm Quantifier [GenVarDecl] Term [Pos]
           -- pos quantifier, ";"s, dot
           -- only "forall" may have a TypeVarDecl
@@ -323,59 +368,68 @@ data Term = QualVar VarDecl
           -- pos "case", "of", "|"s 
           | LetTerm LetBrand [ProgEq] Term [Pos]
           -- pos "where", ";"s
+          | ResolvedMixTerm Id [Term] [Pos]
           | TermToken Token
           | MixTypeTerm TypeQual Type [Pos]
           | MixfixTerm [Term]
           | BracketTerm BracketKind [Term] [Pos]
           -- pos brackets, ","s 
-          | AsPattern VarDecl Pattern [Pos]          
-          -- pos "@"
             deriving (Show, Eq, Ord)
 
+-- | patterns are terms constructed by the first six variants
 type Pattern = Term
 
+-- | construct a tuple from non-singleton lists
 mkTupleTerm :: [Term] -> [Pos] -> Term
 mkTupleTerm ts ps = if isSingle ts then head ts else TupleTerm ts ps
 
+-- | an equation or a case as pair of a pattern and a term 
 data ProgEq = ProgEq Pattern Term [Pos] deriving (Show, Eq, Ord)
             -- pos "=" (or "->" following case-of)
--- ----------------------------------------------------------------------------
--- (type) var decls
--- ----------------------------------------------------------------------------
 
+
+{- | an indicator if variables were separated by commas or by separate
+declarations -}
 data SeparatorKind = Comma | Other deriving (Show, Eq, Ord)
 
+-- | a variable with its type
 data VarDecl = VarDecl Var Type SeparatorKind [Pos] deriving Show
                -- pos "," or ":" 
 
+-- | the kind of a type variable (or a type argument in schemes) 
 data VarKind = VarKind Kind | Downset Type | MissingKind 
                deriving (Show, Eq, Ord)
 
+-- | a (simple) type variable with its kind (or supertype)
 data TypeArg = TypeArg TypeId VarKind SeparatorKind [Pos]
                -- pos "," or ":" ("+" or "-" pos is moved to ExtClass)
                deriving Show
 
+-- | a value or type variable
 data GenVarDecl = GenVarDecl VarDecl
                 | GenTypeVarDecl TypeArg
                   deriving (Show, Eq, Ord)
 
--- ----------------------------------------------------------------------------
--- op names
--- ----------------------------------------------------------------------------
-
+-- | a polymorphic function identifier with type arguments 
 data OpId = OpId UninstOpId [TypeArg] [Pos] deriving (Show, Eq, Ord)
      -- pos "[", ";"s, "]" 
+
+-- | an instantiated function identifiers 
 data InstOpId = InstOpId UninstOpId [Type] [Pos] deriving (Show, Eq, Ord)
 
--- ----------------------------------------------------------------------------
--- ids
--- ----------------------------------------------------------------------------
+-- * synonyms for identifiers
 
+{- | type variable are expected to be simple whereas type constructors may be 
+     mixfix- and compound identifiers -} 
 type TypeId = Id
 type UninstOpId = Id
 
+{- | variables are non-compound identifiers but may be mixfix if their
+types permit -}
 type Var = Id
-type ClassId = Id -- TOKEN-ID (one token with compound list, like CASL sorts)
+
+-- | class identifier are simple but may be compound (like CASL sorts)
+type ClassId = Id
 
 -- * symbol data types
 -- | symbols 
@@ -408,7 +462,7 @@ data SymbOrMap = SymbOrMap Symb (Maybe Symb) [Pos]
                    deriving (Show, Eq)
 
 -- ----------------------------------------------------------------------------
--- equality instances while ignoring positions
+-- equality instances ignoring positions
 -- ----------------------------------------------------------------------------
 
 instance Eq Kind where
@@ -473,13 +527,13 @@ instance Ord Type where
     ProductType _ _<= _ = True
     _ <= ProductType _ _ = False
 
+-- is this equality only needed for the equality of schemes? 
 instance Eq TypeArg where
     TypeArg i1 k1 _ _ == TypeArg i2 k2 _ _ = (i1, k1) == (i2, k2)
 instance Ord TypeArg where
     TypeArg i1 k1 _ _ <= TypeArg i2 k2 _ _ = (i1, k1) <= (i2, k2)
 
--- this is strict syntactic equality
--- unification only captures analysed types and ignores lazy types! 
+-- when are analyzed schemes equal? The name of variables should not matter
 instance Eq TypeScheme where
     TypeScheme v1 t1 _ == TypeScheme v2 t2 _ = (v1, t1) == (v2, t2) 
 instance Ord TypeScheme where
