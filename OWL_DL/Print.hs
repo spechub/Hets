80fe4512fa950c47fa8b2b03f090cd633dc64d5b
Klaus Luettich <luettich@informatik.uni-bremen.de>
2005-07-15 12:14:21 +0000

    provisional implementation of PrettyPrinting for OWL DL. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4677 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ OWL_DL/Print.hs
@@ -0,0 +1,39 @@
+{- |
+Module      :  $Header$
+Copyright   :  (c) Heng Jiang, Uni Bremen 2005
+License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
+
+Maintainer  :  jiang@tzi.de
+Stability   :  provisional
+Portability :  unknown
+
+Pretty printing for OWL DL theories.
+
+-}
+
+{-
+    todo:
+     - invent ASCII display syntax for OWL_DL theories (Klaus)
+     - implement printing of a theory
+-}
+
+module OWL_DL.Print where
+
+import Common.AS_Annotation
+import Common.GlobalAnnotations
+import Common.Lib.Pretty
+import Common.PrettyPrint
+
+import OWL_DL.Sign
+
+instance PrintLaTeX Sign where
+  printLatex0 = printText0
+
+instance PrettyPrint Sign where
+  printText0 _ = text . show
+
+instance PrintLaTeX Sentence where
+  printLatex0 = printText0
+
+instance PrettyPrint Sentence where
+ printText0 _ = text . show
