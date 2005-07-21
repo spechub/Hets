aa81865f7d27013d79ad2b577d96a23a56233bea
Christian Maeder <Christian.Maeder@dfki.de>
2005-07-21 16:10:06 +0000

    further simplify git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4736 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CASL/Simplify.hs
+++ CASL/Simplify.hs
@@ -27,13 +27,13 @@ simplifyRecord mf = (mapRecord mf)
       True_atom _ -> qf 
       False_atom _ -> qf
       _ -> Quantification q vs qf ps
-    , foldConjunction = 
-      \ _ fs ps -> case nub $ filter (not . is_True_atom) fs of 
+    , foldConjunction =  \ _ fs ps -> if any is_False_atom fs 
+      then False_atom ps else case nub $ filter (not . is_True_atom) fs of 
       [] -> True_atom ps
       [f] -> f 
       rs -> Conjunction rs ps
-    , foldDisjunction =  
-      \ _ fs ps -> case nub $ filter (not . is_False_atom) fs of 
+    , foldDisjunction = \ _ fs ps -> if any is_True_atom fs
+      then True_atom ps else case nub $ filter (not . is_False_atom) fs of 
       [] -> False_atom ps
       [f] -> f 
       rs -> Disjunction rs ps
