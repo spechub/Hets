c002ab3bdc9eabe039ba7fb06e67da6e87ac39f6
Till Mossakowski <mossakow@iws.cs.uni-magdeburg.de>
2005-07-26 16:02:00 +0000

    New modules git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4789 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Proofs.hs
+++ Proofs.hs
@@ -1,10 +1,30 @@
 {- |
   Proofs in development graphs.
    Follows Sect. IV:4.4 of the CASL Reference Manual.
+See also  /Heterogeneous
+specification and the heterogeneous tool set/
+(<http://www.tzi.de/~till/papers/habil.ps>), section 5.6.
 
-"Proofs.InferBasic" main module for proof calculus
+"Proofs.Proofs" contains data types for proofs 
 
-"Proofs.Proofs" data types for proofs 
+"Proofs.EdgeUtils" and "Proofs.StatusUtils" contain
+utilities.
+
+The proof calculus for development graphs is contained
+in the following modules:
+
+"Proofs.InferBasic" rule /basic inference/
+
+"Proofs.Global" rules /global decomposition/, /global subsumption/
+ 
+"Proofs.HideTheoremShift" rule /Hide-Theorem-Shift/
+
+"Proofs.Local" rules /local decomposition/, /local subsumption/ 
+(these are derived rules in the calculus)
+
+"Proofs.TheoremHideShift" rule /Theorem-Hide-Shift/
+
+The remaining rules have not been implemented yet.
 -}
 
 module Proofs where
