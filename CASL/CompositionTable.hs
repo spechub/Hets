d000bb5dc93012a91aa276dd5745b8463392e1bc
Till Mossakowski <mossakow@iws.cs.uni-magdeburg.de>
2005-07-29 15:07:52 +0000

    __* empty log message __* git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4827 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ CASL/CompositionTable.hs
@@ -0,0 +1,24 @@
+module CompositionTable where
+
+type BaseRel = String
+
+type CompositionTable = [(BaseRel,BaseRel,[BaseRel])]
+type ConverseTable = [(BaseRel,BaseRel)]
+
+data Table = Table { name :: String,
+                     compositionTable :: CompositionTable,
+                     converseTable :: ConverseTable,
+		     identity :: BaseRel,
+                     models :: [(String,String)]
+		   }
+
+-- hets --spec=RCC8 -o comptable.xml Calculi/Space/RCC8.het
+-- writes Calculi/Space/RCC8.comptable.xml
+
+-- add general hets option that omits analysis of current lib if output files/env are up-to-date
+--  (also important for GUI)
+
+{-
+eliminate ops on rhs, resulting in list of base relations
+add equations for id
+-}
