e66aed8cdfda9c38f18bc97460b793c1a18a643b
Mingyi Liu <xinga@tzi.de>
2005-08-28 17:06:09 +0000

    Format git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@4987 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CASL/CCC/SignFuns.hs
+++ CASL/CCC/SignFuns.hs
@@ -50,41 +50,7 @@ imageOfMorphism m =
               Nothing -> Map.insert ident (Set.singleton pt) predM
               Just pts -> Map.insert ident (Set.insert pt pts) predM
 
-{-
-imageOfMorphism :: Morphism f e m  -> Sign f e
-imageOfMorphism m = 
-        sig {sortSet = Map.image sortMap (sortSet src),
-             sortRel = Rel.image (\a->Map.find a sortMap) (sortRel src), 
-             opMap = Map.foldWithKey 
-                       (\ident ots l ->  
-                           Set.fold (\ot l' -> 
-                             case mapOpSym sortMap funMap (ident,ot) of
-                               Nothing -> l'
-                               Just id_ot -> insertOp id_ot l') l ots) 
-                       Map.empty (opMap src),
-             predMap = Map.foldWithKey                                          
-                         (\ident pts l ->                                         
-                             Set.fold (\pt l' ->                                  
-                               case mapPredSym sortMap pMap (ident,pt) of      
-                                 Nothing -> l'                                    
-                                 Just id_pt -> insertPred id_pt l') l pts)        
-                         Map.empty (predMap src)              
-            }
-    where sig = mtarget m
-          src = msource m
-          sortMap = sort_map m
-          funMap = fun_map m
-          insertOp (ident,ot) opM = 
-            case Map.lookup ident opM of
-              Nothing -> Map.insert ident (Set.singleton ot) opM
-              Just ots -> Map.insert ident (Set.insert ot ots) opM
-          pMap = pred_map m
-          insertPred (ident,pt) predM = 
-            case Map.lookup ident predM of
-              Nothing -> Map.insert ident (Set.singleton pt) predM
-              Just pts -> Map.insert ident (Set.insert pt pts) predM
 
--}
 inhabited :: [SORT] -> [Constraint] -> [SORT]
 inhabited sorts constrs = iterateInhabited sorts
       where (_,ops,_)=recover_Sort_gen_ax constrs
@@ -96,9 +62,10 @@ inhabited sorts constrs = iterateInhabited sorts
                                     ) ops
             iterateInhabited l =
 	            if l==newL then newL else iterateInhabited newL
-		             where newL =foldr (\ (as,rs) l'->
+		             where newL =foldr (\(as,rs) l'->
                                                   if (all (\s->elem s l') as)
-                                                      && (not (elem rs l'))then rs:l'
+                                                      && (not (elem rs l'))
+                                                  then rs:l'
 					          else l') l argsAndres
 
 {-
