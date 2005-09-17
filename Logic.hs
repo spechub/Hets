be408ef9713fbbbaf7bde82618f7d3f204fe806c
Till Mossakowski <mossakow@iws.cs.uni-magdeburg.de>
2005-09-17 11:44:07 +0000

    Improved documentation git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5022 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Logic.hs
+++ Logic.hs
@@ -1,18 +1,35 @@
 {- |
 The folder "Logic" contains the infrastructure needed for
-presenting a logic to Hets.
-The module "Logic.Logic" contains all
-the type classes for interfacing institutions mentioned above,
-including the type class 'Logic.Logic.Logic'.  The module
-"Logic.Prover" is for the interface to theorem provers,
-rewriters, consistency checkers, model checkers.  The data types
-'Logic.Prover.Proof_Status' and 'Logic.Prover.Prover' provides the interface to
-provers.  In case of a successful proof, also the list of axioms that
-have been used in the proof can be returned. 
+presenting a logic to Hets. A logic here is understood in
+the theory of institutions. More precisely, a logic is
+an entailment system in the sense of Meseguer.
+An entailment system consists of a category
+of signatures and signature morphisms, a set of sentences
+for each signature, a sentence translation map of each
+signature morphism, and an entailment relation
+between sets of sentences and sentences for each signature.
+
+The module "Logic.Logic" contains all the type classes for interfacing
+entailment systems in this sense, including the type class
+'Logic.Logic.Logic'.  The entailment relation is given by one or more
+theorem provers, rewriters, consistency checkers, model checkers. The
+module "Logic.Prover" is for the interface to these.  The data types
+'Logic.Prover.Proof_Status' and 'Logic.Prover.Prover' provides the
+interface to provers.  In case of a successful proof, also the list of
+axioms that have been used in the proof can be returned.
+
+Note that the type class 'Logic.Logic.Logic' contains much more than
+just an entailment system. There is infrastructure for parsing,
+printing, static analysis (of both basic specifications and symbols
+maps) , conversion to ATerms, sublogic recognition etc. You see that
+in order to really work with it, one needs much more than just
+an entailment system in the mathematical sense.
+We will use the term /logic/ synonymously with an extended entailment
+system in this sense.
 
  Module "Logic.Comorphism" provides type classes for the various kinds
-of mappings between institutions, and module "Logic.Grothendieck"
-realizes the Grothendieck institution and also contains a type
+of mappings between logics, and module "Logic.Grothendieck"
+realizes the Grothendieck logic and also contains a type
 'Logic.Grothendieck.LogicGraph'. 
 
 /How to add a new logic to Hets/
