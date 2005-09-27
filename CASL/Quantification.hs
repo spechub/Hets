97eee08901ba334f0b9f68592980e75b93b492e9
Christian Maeder <Christian.Maeder@dfki.de>
2005-09-27 17:10:30 +0000

    used CASL.Fold for freeVars and stripQuant git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5087 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CASL/Quantification.hs
+++ CASL/Quantification.hs
@@ -1,6 +1,6 @@
 {- |
 Module      :  $Header$
-Copyright   :  (c) Till Mossakowski and Uni Bremen 2002-2003
+Copyright   :  (c) Till Mossakowski and Uni Bremen 2002-2005
 License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
 
 Maintainer  :  maeder@tzi.de
@@ -13,6 +13,7 @@ Free variables; getting rid of superfluous quantifications
 module CASL.Quantification where
 
 import CASL.AS_Basic_CASL
+import CASL.Fold
 import Common.Id
 import Data.List(nubBy)
 import qualified Common.Lib.Set as Set
@@ -20,34 +21,20 @@ import qualified Common.Lib.Set as Set
 flatVAR_DECLs :: [VAR_DECL] -> [(VAR, SORT)]
 flatVAR_DECLs = concatMap (\ (Var_decl vs s _) -> map (\ v -> (v, s)) vs)
 
-freeVars :: FORMULA f -> Set.Set (VAR, SORT)
-freeVars f = case f of 
-    Quantification _ vdecl phi _ -> freeVars phi Set.\\
-                    Set.fromList (flatVAR_DECLs vdecl)
-    Conjunction phis _ -> Set.unions $ map freeVars  phis
-    Disjunction phis _ -> Set.unions $ map freeVars phis
-    Implication phi1 phi2 _ _ -> freeVars phi1 `Set.union` freeVars phi2
-    Equivalence phi1 phi2 _ -> freeVars phi1 `Set.union` freeVars phi2
-    Negation phi _ -> freeVars phi
-    Predication _ args _ -> Set.unions $ map freeTermVars args
-    Definedness t _ -> freeTermVars t
-    Existl_equation t1 t2 _ -> freeTermVars t1 `Set.union` freeTermVars t2
-    Strong_equation t1 t2 _ -> freeTermVars t1 `Set.union` freeTermVars t2
-    Membership t _ _ -> freeTermVars t
-    _ -> Set.empty
+type VarSet = Set.Set (VAR, SORT)
 
+freeVarsRecord :: (f -> VarSet) -> Record f VarSet VarSet
+freeVarsRecord mf = (constRecord mf Set.unions Set.empty) 
+    { foldQual_var = \ _ v s _ -> Set.singleton (v, s)
+    , foldQuantification = \ _ _ vdecl phiVars _ -> 
+           Set.difference phiVars $ 
+                    Set.fromList $ flatVAR_DECLs vdecl
+    }
 
-freeTermVars :: TERM f -> Set.Set (VAR, SORT)
-freeTermVars t = case t of 
-    Qual_var v s _ -> Set.singleton (v, s)
-    Application _ args _ -> Set.unions $ map freeTermVars args
-    Sorted_term st _ _ -> freeTermVars st
-    Cast st _ _ -> freeTermVars st
-    Conditional t1 phi t2 _ -> freeVars phi `Set.union` 
-			       freeTermVars t1 `Set.union` freeTermVars t2
-    _ -> Set.empty
+freeVars :: FORMULA f -> VarSet
+freeVars = foldFormula $ freeVarsRecord $ const Set.empty
 
--- quantify only over free variables (and only once)
+-- | quantify only over free variables (and only once)
 effQuantify :: QUANTIFIER -> [VAR_DECL] -> FORMULA f -> Range -> FORMULA f
 effQuantify q vdecls phi pos =
     let fvs = freeVars phi 
@@ -57,28 +44,22 @@ effQuantify q vdecls phi pos =
 	    map (\v -> Var_decl [v] s ps) vs
 	newDecls = concatMap (flatVAR_DECL . filterVAR_DECL) vdecls
 	myNub = nubBy (\ (Var_decl v1 _ _) (Var_decl v2 _ _) -> v1 == v2)
-	in if null newDecls then phi else 
+	in if null newDecls && q /= Unique_existential then phi else 
 	   Quantification q (reverse $ myNub $ reverse newDecls) phi pos
-	
--- strip superfluous (or nested) quantifications
-stripQuant :: FORMULA f -> FORMULA f
-stripQuant (Quantification quant vdecl phi pos) =
-    let newF = stripQuant phi 
-	qF = effQuantify quant vdecl phi pos in 
-	case newF of 
+
+
+stripRecord :: (f -> f) -> Record f (FORMULA f) (TERM f) 
+stripRecord mf = (mapRecord mf) 
+    { foldQuantification = \ _ quant vdecl newF pos -> 
+      let qF = effQuantify quant vdecl newF pos in 
+      case newF of 
 		 Quantification quant2 vd2 f2 ps -> 
 		     if quant == quant2 then 
 			effQuantify quant (vdecl ++ vd2) f2 (pos `appRange` ps)
 		     else qF
 		 _ -> qF
-stripQuant (Conjunction phis pos) =
-  Conjunction (map stripQuant phis) pos
-stripQuant (Disjunction phis pos) =
-  Disjunction (map stripQuant phis) pos
-stripQuant (Implication phi1 phi2 b pos) =
-  Implication (stripQuant phi1) (stripQuant phi2) b pos
-stripQuant (Equivalence phi1 phi2 pos) =
-  Equivalence (stripQuant phi1) (stripQuant phi2) pos
-stripQuant (Negation phi pos) =
-  Negation (stripQuant phi) pos
-stripQuant phi = phi
+    }
+	
+-- | strip superfluous (or nested) quantifications
+stripQuant :: FORMULA f -> FORMULA f
+stripQuant = foldFormula $ stripRecord id
