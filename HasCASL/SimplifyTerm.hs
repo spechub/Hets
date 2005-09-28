e9bf7f57bae0e7d357d15de7ba308842d4438fa5
Christian Maeder <Christian.Maeder@dfki.de>
2005-09-28 16:57:14 +0000

    rewrote simplify using foldTerm and improved output for envs without the builtin operators git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5100 cec4b9c1-7d33-0410-9eda-942365e851bb

--- HasCASL/SimplifyTerm.hs
+++ HasCASL/SimplifyTerm.hs
@@ -17,29 +17,24 @@ import HasCASL.As
 import HasCASL.AsUtils
 import HasCASL.VarDecl
 import HasCASL.Le
+import HasCASL.FoldTerm
 import qualified Common.Lib.Map as Map
 import Common.Lib.State
 
--- | remove qualification of unique identifiers
-simplifyTerm :: Env -> Term -> Term
-simplifyTerm env trm = case trm of
-   QualVar (VarDecl v _ _ ps) -> if Map.member v $ assumps env then
-         trm else ResolvedMixTerm v [] ps
-   QualOp _ (InstOpId i _ _) _ ps -> 
-       if Map.member i $ localVars env then trm else 
+simplifyRec :: Env -> MapRec
+simplifyRec env = mapRec
+    { foldQualVar = \ t (VarDecl v _ _ ps) -> 
+      if Map.member v $ assumps env then t else ResolvedMixTerm v [] ps
+    , foldQualOp = \ trm _ (InstOpId i _ _) _ ps -> 
+      if Map.member i $ localVars env then trm else 
           case Map.lookup i $ assumps env of 
-          Just (OpInfos [_]) -> ResolvedMixTerm i [] ps
-          _ -> trm
-   ApplTerm t1 t2 ps ->
-       ApplTerm (simplifyTerm env t1) (simplifyTerm env t2) ps
-   TupleTerm ts ps -> TupleTerm (map (simplifyTerm env) ts) ps
-   TypedTerm te q ty ps -> let 
-      nt = simplifyTerm env te 
-      ntyped = TypedTerm nt q ty ps 
-      in case q of
-      InType -> ntyped
-      AsType -> ntyped
-      _ -> case nt of 
+          Just (OpInfos (_ : _ : _)) -> trm
+          _ -> ResolvedMixTerm i [] ps
+    , foldTypedTerm = \ _ nt q ty ps -> 
+        let ntyped = TypedTerm nt q ty ps in case q of
+        InType -> ntyped
+        AsType -> ntyped
+        _ -> case nt of 
            QualVar (VarDecl v oty _ qs) | oty == ty -> 
               if Map.member v $ assumps env then ntyped
               else TypedTerm (ResolvedMixTerm v [] qs) OfType ty ps
@@ -47,36 +42,34 @@ simplifyTerm env trm = case trm of
               if Map.member i $ localVars env then ntyped
               else TypedTerm (ResolvedMixTerm i [] qs) OfType ty ps
            _ -> ntyped
-   QuantifiedTerm q vs te ps -> 
+    , foldQuantifiedTerm = \ (QuantifiedTerm q vs te ps) _ _ _ _ -> 
        let nEnv = execState (mapM_ ( \ vd -> 
               case vd of 
               GenVarDecl v -> addLocalVar False v
               _ -> return ()) vs) env
        in QuantifiedTerm q vs (simplifyTerm nEnv te) ps
-   LambdaTerm ps p te qs -> 
+    , foldLambdaTerm = \ (LambdaTerm ls p te qs) ps _ _ _ -> 
        let nEnv = execState (mapM_ (addLocalVar False) 
-                     $ concatMap extractVars ps) env
-       in LambdaTerm (map (simplifyTerm env) ps) p (simplifyTerm nEnv te) qs
-   CaseTerm te es ps -> 
-       CaseTerm (simplifyTerm env te) (map (simplifyEq env) es) ps
-   LetTerm b es te ps ->
+                     $ concatMap extractVars ls) env
+       in LambdaTerm ps p (simplifyTerm nEnv te) qs
+    , foldLetTerm = \ (LetTerm b es te ps) _ _ _ _ ->
        let addEqVars = mapM_ ( \ (ProgEq p _ _) -> 
                        mapM_ (addLocalVar False) $ extractVars p)
            nEnv = execState (addEqVars es) env
        in LetTerm b (map (simplifyEq nEnv) es) (simplifyTerm nEnv te) ps
-   AsPattern vd pa ps -> AsPattern vd (simplifyTerm env pa) ps
-   TermToken _ -> trm
-   MixfixTerm ts -> MixfixTerm $ map (simplifyTerm env) ts
-   BracketTerm b ts ps -> BracketTerm b (map (simplifyTerm env) ts) ps
-   MixTypeTerm q ty ps -> MixTypeTerm q ty ps  
-   ResolvedMixTerm i ts ps -> ResolvedMixTerm i (map (simplifyTerm env) ts) ps
+    , foldProgEq = \ (ProgEq p t r) q _ _ ->   
+        let nEnv = execState (mapM_ (addLocalVar False) $ extractVars p) env
+            s = simplifyTerm nEnv t 
+        in ProgEq q s r
+    }
+
+
+-- | remove qualification of unique identifiers
+simplifyTerm :: Env -> Term -> Term
+simplifyTerm = foldTerm . simplifyRec 
   
 simplifyEq :: Env -> ProgEq -> ProgEq
-simplifyEq env (ProgEq p t r) = 
-    let nEnv = execState (mapM_ (addLocalVar False) $ extractVars p) env
-        q = simplifyTerm env p 
-        s = simplifyTerm nEnv t 
-    in ProgEq q s r
+simplifyEq = foldEq . simplifyRec
 
 simplifySentence :: Env -> Sentence -> Sentence
 simplifySentence env s = case s of
