9753523d7bfe7c9413207e8f0c7548bd3dc15322
Christian Maeder <Christian.Maeder@dfki.de>
2005-10-05 13:11:39 +0000

    generalized disambiguation git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5130 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Common/ProofUtils.hs
+++ Common/ProofUtils.hs
@@ -1,22 +1,18 @@
 {- |
 Module      :  $Header$
-Copyright   :  (c) various people and Klaus Lüttich, Uni Bremen 2005
+Copyright   :  (c) Klaus Lüttich, C. Maeder, Uni Bremen 2005
 License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
 
 Maintainer  :  lüttich@tzi.de
 Stability   :  provisional
-Portability :  needs POSIX
+Portability :  portable
 
-this module collects functions useful for all prover connections in Hets
-some are moved from Isabelle.Translate and some others are moved from
+this module collects functions useful for all prover connections in Hets.
+Some were moved from Isabelle.Translate and some others from
 Isabelle.IsaProve.
 -}
 
-
 module Common.ProofUtils where
-
-import Data.Maybe
-import Data.List 
 
 import qualified Common.Lib.Map as Map
 import qualified Common.Lib.Set as Set
@@ -36,38 +32,45 @@ If that does not work for a certain reasoner you can hand in a function
 which uses a different alghorithm.
 -}
 prepareSenNames :: (String -> String) -> [Named a] -> [Named a]
-prepareSenNames trFun = 
+prepareSenNames trFun =
     disambiguateSens Set.empty . transSens trFun . nameSens
 
 -- | translate special characters in sentence names
 transSens :: (String -> String) -> [Named a] -> [Named a]
-transSens trFun = map (\ax -> ax{senName = trFun (senName ax)}) 
+transSens trFun = map (\ax -> ax{senName = trFun (senName ax)})
 
 -- | disambiguate sentence names
 disambiguateSens :: Set.Set String -> [Named a] -> [Named a]
-disambiguateSens _ [] = []
-disambiguateSens nameSet (ax : rest) = 
-  let name = senName ax in case Set.splitMember name nameSet of
-  (_, False, _) -> ax : disambiguateSens (Set.insert name nameSet) rest
-  (_, _, greater) -> let 
-      name' = fromJust $ find (not . flip Set.member greater) 
+disambiguateSens =
+    genericDisambigSens senName ( \ n s -> s { senName = n })
+
+-- | generically disambiguate lists with names
+genericDisambigSens :: (a -> String) -> (String -> a -> a) -> Set.Set String
+                    -> [a] -> [a]
+genericDisambigSens _ _ _ [] = []
+genericDisambigSens sel upd nameSet (ax : rest) =
+  let name = sel ax in case Set.splitMember name nameSet of
+  (_, False, _) ->
+      ax : genericDisambigSens sel upd (Set.insert name nameSet) rest
+  (_, _, greater) -> let
+      name' = head $ filter (not . flip Set.member greater)
                           [name++show (i :: Int) | i<-[1..]]
-      ax' = ax {senName = name'}
-      in ax' : disambiguateSens (Set.insert name' nameSet) rest
+      in upd name' ax :
+         genericDisambigSens sel upd (Set.insert name' nameSet) rest
 
 -- | name unlabeled axioms with "Axnnn"
 nameSens :: [Named a] -> [Named a]
-nameSens sens = 
+nameSens sens =
   map nameSen (zip sens [1..length sens])
-  where nameSen (sen,no) = if senName sen == "" 
+  where nameSen (sen,no) = if senName sen == ""
                               then sen{senName = "Ax"++show no}
                               else sen
- 
--- | collect the mapping of new to old names 
+
+-- | collect the mapping of new to old names
 collectNameMapping :: [Named a] -> [Named a] -> Map.Map String String
-collectNameMapping n o = Map.fromList (zipWith toPair n o) 
-    where toPair nSen oSen = (senName nSen, 
-                              if null oName 
+collectNameMapping n o = Map.fromList (zipWith toPair n o)
+    where toPair nSen oSen = (senName nSen,
+                              if null oName
                                  then "<unnamed>"++senName nSen
                                  else oName)
               where oName = senName oSen
@@ -83,7 +86,7 @@ charMap = Map.fromList
   ('&' , "Amp"),
   ('(' , "OBr"),
   (')' , "CBr"),
-  ('*' , "x"), 
+  ('*' , "x"),
   ('+' , "Plus"),
   (',' , "Comma"),
   ('-' , "Minus"),
@@ -98,7 +101,7 @@ charMap = Map.fromList
   ('@' , "At"),
   ('[' , "OSqBr"),
   ('\\' , "Bslash"),
-  (']' , "CSqBr"),        
+  (']' , "CSqBr"),
   ('^' , "Caret"), -- Hat?
   ('`' , "Grave"),
   ('{' , "LBrace"),
@@ -120,7 +123,7 @@ charMap = Map.fromList
   ('¬' , "not"),
   ('­' , "shy"),
   ('®' , "reg"),
-  ('\175', "macr"), 
+  ('\175', "macr"),
   ('°' , "deg"),
   ('±' , "plusmn"),
   ('²' , "sup2"),
