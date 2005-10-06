d8b6628f877d9a58885e1ea8d2203333811013d7
Christian Maeder <Christian.Maeder@dfki.de>
2005-10-06 15:25:53 +0000

    filled in some meaningful code for ghci git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5156 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Test.hs
+++ Test.hs
@@ -1,67 +1,42 @@
-module Test
 
-where
+import Comorphisms.LogicGraph
+import Syntax.AS_Library
+import Static.AnalysisLibrary
+import Static.DevGraph
+import Static.PrintDevGraph
+import Driver.Options
+import qualified Common.Lib.Map as Map
+import Common.Lib.Pretty
 
 import System.Environment
-import Comorphisms.LogicGraph
-import Static.AnalysisLibrary
-import System.IO
-import Static.DotGraph
-import Static.DevGraph
-import Driver.Options
-import CASL.Logic_CASL
-import Common.Lib.Graph
-import Logic.Logic
-import Logic.Grothendieck
-import Common.Result
-import Common.Id
-import Common.AS_Annotation
 
---proceed :: String -> IO()
-proceed fname = do
-  anaFile logicGraph defaultLogic defaultHetcatsOpts emptyLibEnv fname
-{-  case res of
-    Just(_,_,dg,_) -> do
-      putStrLn ("Successfully analyzed.")
-      putStrLn ("Writing development graph to "++fname++".dot")
-      h <- openFile (fname++".dot") WriteMode
-      sequence (map (hPutStrLn h) (dot dg))
-      hClose h
-    _ -> return ()
+process :: FilePath -> IO (Maybe (LIB_NAME, LibEnv))
+process = anaFileOrGetEnv logicGraph defaultHetcatsOpts emptyLibEnv
+
+printLibEnv :: LibEnv -> Doc
+printLibEnv le = vcat $ map (printLibrary le) $ Map.toList le
+
+{- Call this function as follows
+make
+make ghci
+:l Test.hs
+Just (_, lenv) <- process "../CASL-lib/List.casl"
+printLibEnv lenv
 -}
 
-getCASLSig :: String -> IO CASLSign
-getCASLSig fname = do
-  res <- proceed fname
+-- sample code
+getDevGraph :: FilePath -> IO DGraph
+getDevGraph fname = do
+  res <- process fname
   case res of
-    Just (_,_,dg,_) -> do
-      case match 1 dg of
-        (Just ctx,_) -> case dgn_sign $ lab' ctx of
-          G_sign lid sig ->
-            case maybeResult $ rcoerce lid CASL nullPos sig of
-               Just sig' -> return sig'
-               Nothing -> error "Not a CASL sig"
-        _ -> error "Node 1 no in development graph" 
-    Nothing -> error "Error occured"
-
-getCASLSens :: String -> IO [Named CASLFORMULA]
-getCASLSens fname = do
-  res <- proceed fname
-  case res of
-    Just (_,_,dg,_) -> do
-      case match 1 dg of
-        (Just ctx,_) -> case dgn_sens $ lab' ctx of
-          G_l_sentence_list lid sens ->
-            case maybeResult $ rcoerce lid CASL nullPos sens of
-               Just sens' -> return sens'
-               Nothing -> error "Not the CASL logic"
-        _ -> error "Node 1 no in development graph" 
-    Nothing -> error "Error occured"
+    Nothing -> error "getDevGraph: process"
+    Just (ln, lenv) -> case Map.lookup ln lenv of
+        Nothing -> error "getDevGraph: lookup"
+        Just (_,_,dg) -> return dg
 
 main :: IO()
 main = do
   files <- getArgs
-  sequence (map proceed files)
+  mapM_ process files
   return ()
-
 
