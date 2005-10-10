0215149f58d9b822bcb37317e9da6dfa434f4192
Christian Maeder <Christian.Maeder@dfki.de>
2005-10-10 13:48:33 +0000

    fixed for recent programatica git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5158 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Haskell/hana.hs
+++ Haskell/hana.hs
@@ -17,7 +17,6 @@ import Haskell.HatParser
 import Haskell.HatAna
 import Haskell.PreludeString
 import ParseMonad
-import LexerOptions
 import PropLexer
 import PropParser as HP
 import PropPosSyntax
@@ -25,35 +24,25 @@ import PropPosSyntax
 import System.Environment
 
 import Common.Result
-import Common.AnnoState
-import Common.AS_Annotation
 import Common.Print_AS_Annotation()
 import Common.GlobalAnnotations
 import Common.PrettyPrint
 
-
-hParser :: AParser () (Sign, [Named (TiDecl PNT)])
-hParser = do 
-   b <- hatParser
-   let res@(Result _ m) = hatAna(b, emptySign, emptyGlobalAnnos)
-   case m of 
-      Nothing -> error $ show res
-      Just (_, _, sig, sens) -> return (sig, sens)
-
 main :: IO ()
-main = do l <- getArgs
-          if length l >= 1 then
-             do let fn = head l 
-                s <- readFile fn
-                let ts = pLexerPass0 lexerflags0 s
-                    Result ds m = do 
-                      HsModule _ _ _ _ b <- parseTokens HP.parse fn ts
-                      hatAna(HsDecls (preludeDecls ++ b), 
+main = getArgs >>= mapM_ process
+          
+-- try with files ToHaskell/test/*.hascasl.hs
+process :: FilePath -> IO () 
+process fn = do
+    s <- readFile fn
+    let ts = pLexerPass0 True s
+        Result ds m = do 
+            HsModule _ _ _ _ b <- parseTokens HP.parse fn ts
+            hatAna(HsDecls (preludeDecls ++ b), 
                                      emptySign, emptyGlobalAnnos) 
-                case m of 
-                       Just (_, _, sig, hs) -> do
-                           putStrLn $ showPretty sig ""
-                           mapM_ (putStrLn . flip showPretty "") hs
-                       _ -> mapM_ (putStrLn . show) ds
-             else putStrLn "missing argument"
+    case m of 
+        Just (_, _, sig, hs) -> do
+            putStrLn $ showPretty sig ""
+            mapM_ (putStrLn . flip showPretty "") hs
+        _ -> mapM_ (putStrLn . show) ds
 
