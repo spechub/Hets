628b2e9314d8232ed12c217e41b0c0e3de634d66
Christian Maeder <Christian.Maeder@dfki.de>
2005-10-19 18:54:04 +0000

    process many files git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5214 cec4b9c1-7d33-0410-9eda-942365e851bb

--- ToHaskell/h2hf.hs
+++ ToHaskell/h2hf.hs
@@ -1,13 +1,13 @@
 {- |
 Module      :  $Header$
-Copyright   :  (c) Christian Maeder, Uni Bremen 2002-2004
+Copyright   :  (c) Christian Maeder, Uni Bremen 2002-2005
 License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
 
 Maintainer  :  meader@tzi.de
 Stability   :  experimental
-Portability :  portable 
+Portability :  non-portable(programatica and isabelle) 
 
-test translation
+test translation from Haskell to Isabelle
 -}
 
 module Main where
@@ -19,10 +19,9 @@ import Common.Result
 import Common.AnnoState
 import Common.AS_Annotation
 import Common.GlobalAnnotations
--- import Common.PrettyPrint
+
 import Common.Lib.Pretty
 
--- import Comorphisms.HasCASL2Haskell
 import Comorphisms.Hs2HOLCF
 
 import Isabelle.CreateThy
@@ -31,39 +30,31 @@ import Isabelle.IsaSign as IsaSign
 import Haskell.HatAna as HatAna
 import Haskell.HatParser
 
--- import HasCASL.Le
--- import HasCASL.AsToLe
--- import HasCASL.ParseItem(basicSpec)
--- import HasCASL.ProgEq
-
 hParser :: AParser () (IsaSign.Sign, [Named IsaSign.Sentence])
 hParser = do 
    b <- hatParser
    let res@(Result _ m) = do 
           (_, _, sig, sens) <- hatAna (b, HatAna.emptySign, emptyGlobalAnnos)
---          sens <- return [x | Named _ True x <- nsens]
           transTheory sig sens
    case m of 
       Nothing -> error $ show res
       Just x -> return x
 
 main :: IO ()
-main = do l <- getArgs
-          if length l >= 1 then
-             do s <- readFile $ head l
+main = getArgs >>= mapM_ process
+
+process :: FilePath -> IO ()
+process fn = do s <- readFile fn
                 s1 <- return $ dropWhile (\x -> x == '\n' || x == ' ') s
                 s2 <- return $ if takeWhile (/= ' ') s1 == "module" 
                                then dropWhile (/= '\n') s1 else s1  
-                let r = runParser hParser (emptyAnnos ()) (head l) s2  
+                let r = runParser hParser (emptyAnnos ()) fn s2  
                 case r of 
                        Right (sig, hs) -> let 
                          tn = dropWhile (== '"') $ (takeWhile (/= '.') 
                               $ reverse (takeWhile (\x -> x /= '/') $ reverse 
-                              $ show $ head l)) ++ "_theory" 
+                              $ show fn)) ++ "_theory" 
                          doc = text "theory" <+> text tn <+> text "=" $$
                             createTheoryText sig hs
                          in writeFile (tn ++ ".thy") (shows doc "\n")
                        Left err -> putStrLn $ show err
-           else putStrLn "missing argument"
---       _ -> return ()
-
