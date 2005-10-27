5d6bbd2eb3b7f5422bae53d8a95f23627ad8eeb6
Klaus Luettich <luettich@informatik.uni-bremen.de>
2005-10-27 16:20:40 +0000

    * changed maintainer and added author to Copyright * Added fields to the stat and updated initialState function git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5287 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Proofs/GUIState.hs
+++ Proofs/GUIState.hs
@@ -1,10 +1,10 @@
 {- |
 Module      :  $Header$
 Description :  State data structure used by the goal management GUI.
-Copyright   :  (c) Rene Wagner, Uni Bremen 2005
+Copyright   :  (c) Rene Wagner, Klaus Lüttich, Uni Bremen 2005
 License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
 
-Maintainer  :  rwagner@tzi.de
+Maintainer  :  luettich@tzi.de
 Stability   :  provisional
 Portability :  needs POSIX
 
@@ -15,39 +15,74 @@ away by allowing callback function to use it as the sole input and output.
 
 module Proofs.GUIState where
 
-import Static.DevGraph
+import qualified Common.OrderedMap as OMap
+import qualified Common.Lib.Map as Map
+
+import Logic.Logic 
+import Logic.Prover
+import Logic.Grothendieck
+
 import Comorphisms.KnownProvers
+
+import Static.DevGraph
 
 {- |
   Represents the global state of the prover GUI.
 -}
-data ProofGUIState = ProofGUIState { -- | theory name
-                                     theoryName :: String,
-				     -- | Grothendieck theory
-				     theory :: G_theory,
-				     -- | currently known provers
-				     provers :: KnownProversMap,
-                                     -- | currently selected goal or Nothing
-                                     selectedGoals :: [String],
-                                     -- | whether a prover is running or not
-                                     proverRunning :: Bool,
-                                     -- | which prover (if any) is currently selected
-                                     selectedProver :: Maybe String
-                                   }
+data ProofGUIState lid sentence =  
+     ProofGUIState 
+      { -- | theory name
+        theoryName :: String,
+	-- | Grothendieck theory
+	theory :: G_theory,
+        -- | logic id associated with following maps
+        logic_id :: lid,
+        -- | axioms are stored in a separate map
+        axiomMap :: ThSens sentence (AnyComorphism,BasicProof),
+        -- | goals are stored in a separate map
+        goalMap :: ThSens sentence (AnyComorphism,BasicProof),
+	-- | currently known provers
+	proversMap :: KnownProversMap,
+        -- | all goals in the theory
+        allGoals :: [String],
+        -- | currently selected goals
+        selectedGoals :: [String],
+        -- | whether a prover is running or not
+        proverRunning :: Bool,
+        -- | wether all goals are selected or not
+        allGoalsSelected :: Bool,
+        -- | which prover (if any) is currently selected
+        selectedProver :: Maybe String
+      }
 
 {- |
   Creates an initial State.
 -}
-initialState :: String
+initialState :: 
+    (Logic  lid1 sublogics1 basic_spec1 sentence1 symb_items1 symb_map_items1
+                 sign1 morphism1 symbol1 raw_symbol1 proof_tree1,
+     Monad m) =>
+                lid1
+              -> String
              -> G_theory
 	     -> KnownProversMap
-             -> ProofGUIState
-initialState thN th pm = 
-  ProofGUIState { theoryName = thN,
-		  theory = th,
-		  provers = pm,
-                  selectedGoals = [],
-                  proverRunning = False,
-                  selectedProver = Nothing
-                }
-
+             -> m (ProofGUIState lid1 sentence1)
+initialState lid1 thN th@(G_theory lid2 _ thSens) pm = 
+    do thSens' <- coerceThSens lid2 lid1 "creating initial GUI State" thSens
+       let (gMap,aMap) = Map.partition (isAxiom . OMap.ele) thSens'
+       return $ 
+           ProofGUIState { theoryName = thN,
+		           theory = th,
+                           logic_id = lid1,
+                           goalMap = gMap,
+                           axiomMap = aMap,
+		           proversMap = pm,
+                           allGoals = OMap.keys gMap,
+                           selectedGoals = [],
+                           proverRunning = False,
+                           allGoalsSelected = False,
+                           selectedProver = if null (Map.keys pm) 
+                                            then Nothing
+                                            else Just (last $ Map.keys pm)
+                         }
+      
