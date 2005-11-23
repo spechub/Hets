9b8c63ff208e064b99d4feeb87fd21a7b2df7e30
Heng Jiang <jiang@tzi.de>
2005-11-23 16:56:58 +0000

    removed debug code. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5443 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Static/DevGraph.hs
+++ Static/DevGraph.hs
@@ -60,7 +60,7 @@ import Data.Dynamic
 import Data.List as List
 import Control.Monad
 import Control.Exception
-import Debug.Trace
+-- import Debug.Trace
 
 getNewNode :: Tree.Gr a b -> Node
 getNewNode g = case newNodes 1 g of
@@ -631,7 +631,7 @@ joinG_sentences :: Monad m => G_theory -> G_theory -> m G_theory
 joinG_sentences (G_theory lid1 sig1 sens1) (G_theory lid2 sig2 sens2) = do
   sens2' <- coerceThSens lid2 lid1 "joinG_sentences" sens2
   sig2' <- coerceSign lid2 lid1 "joinG_sentences" sig2
-  return $ assert (let t = sig1 == sig2' in if not t then trace ("What's wrong:\n"++show sig1++"\n"++show sig2') t else t) $ G_theory lid1 sig1 $ joinSens sens1 sens2'
+  return $ assert (sig1 == sig2') $ G_theory lid1 sig1 $ joinSens sens1 sens2'
 
 -- | flattening the sentences form a list of G_theories
 flatG_sentences :: Monad m => G_theory -> [G_theory] -> m G_theory
