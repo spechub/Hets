e3e78f297ff0934537cb0437d8d347576b159573
Christian Maeder <Christian.Maeder@dfki.de>
2005-11-23 17:11:35 +0000

    added {} git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5448 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Isabelle/IsaStrings.hs
+++ Isabelle/IsaStrings.hs
@@ -167,7 +167,7 @@ mainS :: IsaSets
 mainS = 
   IsaSets {
   types =  mkIsaSet ["Int"],
-  consts = mkIsaSet ["inject n [__] >= >",
+  consts = mkIsaSet ["inject n [__] {__} >= >",
   "!   !!   #   %   &   (   ()   (]   (|   (}   )   *   +   ++   ,",
   "-   -->   -`   .   ..   ...   ..}   /   //   0   1   :   ::   :=   ;   <",
   "<*>   <*lex*>   <+>   <=   =   ==   ==>   =>   ?   ?!   @   ALL   BIT",
