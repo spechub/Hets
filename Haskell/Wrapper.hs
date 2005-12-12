84f4a3859d625c9d69a7af998ebb8d512d465389
Christian Maeder <Christian.Maeder@dfki.de>
2005-12-12 16:18:53 +0000

    used generic parser for nested comments git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5512 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Haskell/Wrapper.hs
+++ Haskell/Wrapper.hs
@@ -1,8 +1,8 @@
 {- |
 Module:  $Header$
 Copyright   :  (c) C. Maeder, Uni Bremen 2003
-License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt          
-                                                                               
+License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
+
 Maintainer  :  maeder@tzi.de
 Stability   :  provisional
 Portability :  portable
@@ -11,12 +11,12 @@ extract Haskell code from String
 -}
 
 {-
-   stops at unbalanced "}" 
+   stops at unbalanced "}"
 
    "then" may be recognized if it is not preceded by "if"
    but that's not worth the trouble, because ...
 
-   "and" is used by Haskell, 
+   "and" is used by Haskell,
    "with", "hide", "reveal", "within", "end"
    may be userdefined in Haskell
 -}
@@ -25,14 +25,14 @@ module Haskell.Wrapper where
 
 import Text.ParserCombinators.Parsec
 import Common.Lexer
- 
+
 hStuff, stuff :: GenParser Char st String
-hStuff = flat $ many stuff 
+hStuff = flat $ many stuff
 
 stuff = lineComment <|> nestComment <|> stringLit <|> charLit
-        <|> balanced "{}" 
-        <|> balanced "()" 
-        <|> balanced "[]" 
+        <|> balanced "{}"
+        <|> balanced "()"
+        <|> balanced "[]"
         <|> single (noneOf "])}") <?> ""
 
 balanced :: String -> GenParser Char st String
@@ -40,16 +40,11 @@ balanced [o, c] = char o <:> hStuff <++> string [c]
 balanced _ = error "balanced"
 
 nestComment :: GenParser Char st String
-nestComment = try (string "{-") <++> 
-                 flat (many (single (noneOf "-{") 
-                             <|> try (string "-" << notFollowedBy (char '}'))
-                             <|> nestComment 
-                             <|> string "{" ))
-                 <++> string "-}"
+nestComment = nestedComment "{-" "-}"
 
 lineComment, stringLit, charLit :: GenParser Char st String
 lineComment = try (string "--") <++> many (noneOf "\n\r")
-              <++> many (oneOf "\n\r")  
+              <++> many (oneOf "\n\r")
 
 stringLit = char '\"' <:> flat (many (single (noneOf "\\\"")
                                  <|> char '\\' <:> single anyChar))
@@ -59,5 +54,3 @@ charLit = try (string "'''") <|>
           char '\'' <:> flat (many (single (noneOf "\\\'")
                                  <|> char '\\' <:> single anyChar))
           <++> string "\'"
- 
-
