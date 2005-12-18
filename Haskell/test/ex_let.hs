d8392f2672587cd186c87d576e13a91b00763b7a
Paolo Torrini <paolot@informatik.uni-bremen.de>
2005-12-18 19:10:01 +0000

    __* empty log message __* git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5541 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ Haskell/test/ex_let.hs
@@ -0,0 +1,11 @@
+
+myEqual        :: Eq a => a -> a -> Bool
+myEqual x y    =   if x == y then True else False
+
+foon :: Bool -> Bool -> Bool -> Bool
+foon a b c = let 
+ testName1 = myEqual a b 
+ testName2 = myEqual b c 
+ in
+ testName1 && testName2
+
