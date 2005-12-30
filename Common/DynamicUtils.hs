cca5af4c02f17711e8a331cfa71325acec5fc564
Christian Maeder <Christian.Maeder@dfki.de>
2005-12-30 19:54:24 +0000

    make it even usable for ghc-5 git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5575 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Common/DynamicUtils.hs
+++ Common/DynamicUtils.hs
@@ -1,8 +1,7 @@
 {-# OPTIONS -cpp #-}
-{-|
-
+{- |
 Module      :  $Header$
-Copyright   :  (c) Till Mossakowski, Uni Bremen 2002-2004
+Copyright   :  (c) Till Mossakowski, Uni Bremen 2002-2005
 License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
 
 Maintainer  :  maeder@tzi.de
@@ -18,6 +17,7 @@ import Data.Dynamic
 #if __GLASGOW_HASKELL__<=602
 import qualified Common.Lib.Map as Map
 import qualified Common.Lib.Set as Set
+import Data.Ratio
 
 mkTyConApp :: TyCon -> [TypeRep] -> TypeRep
 mkTyConApp = mkAppTy
@@ -34,4 +34,11 @@ mapTc = mkTyCon "Common.Lib.Map.Map"
 instance (Typeable a, Typeable b) => Typeable (Map.Map a b) where
   typeOf m = mkTyConApp mapTc [typeOf ((undefined :: Map.Map a b -> a) m),
                             typeOf ((undefined :: Map.Map a b -> b) m)]
+#endif
+#if __GLASGOW_HASKELL__<=504
+ratioTc :: TyCon
+ratioTc = mkTyCon "Data.Ratio"
+
+instance Typeable a => Typeable (Ratio a) where
+  typeOf s = mkTyConApp ratioTc [typeOf ((undefined:: Ratio a -> a) s)]
 #endif
