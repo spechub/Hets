9c8492f33911385ff9714bb6efc12fef1af037da
Christian Maeder <Christian.Maeder@dfki.de>
2006-01-26 21:26:58 +0000

    added a line break before preceding annotations git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5723 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Common/Print_AS_Annotation.hs
+++ Common/Print_AS_Annotation.hs
@@ -21,20 +21,20 @@ import Common.Lexer(whiteChars)
 
 instance PrettyPrint Annotation where
     printText0 _ (Unparsed_anno aw at _) =
-        case at of 
-                Line_anno str -> 
-                    (case aw of 
+        case at of
+                Line_anno str ->
+                    (case aw of
                             Comment_start -> ptext "%%"
-                            Annote_word w ->  ptext $ "%" ++ w) 
-                    <> (if all (`elem` whiteChars) str 
+                            Annote_word w ->  ptext $ "%" ++ w)
+                    <> (if all (`elem` whiteChars) str
                         then empty else ptext str)
-                Group_anno strs -> 
+                Group_anno strs ->
                     let docs = map ptext strs
-                        (o, c) = case aw of 
+                        (o, c) = case aw of
                             Comment_start -> (ptext "%{",  ptext "}%")
-                            Annote_word w -> (ptext ("%" ++ w ++ "("), 
+                            Annote_word w -> (ptext ("%" ++ w ++ "("),
                                               ptext ")%")
-                        in case docs of 
+                        in case docs of
                            [] -> empty
                            [h] ->  o <> h <> c
                            h : t -> vcat ((o <> h) : init t ++ [last t <> c])
@@ -42,7 +42,7 @@ instance PrettyPrint Annotation where
         let aa' = printText0 ga aa
             ab' = fcat $ punctuate space $ map printPair $ filter nullSnd ab
         in printGroup (ptext "display") $ aa' <+> ab'
-           where printPair (s1,s2) = ptext ("%" ++ lookupDisplayFormat s1) 
+           where printPair (s1,s2) = ptext ("%" ++ lookupDisplayFormat s1)
                                      <+> ptext s2
                  nullSnd (_,s2) = not $ null s2
     printText0 ga (String_anno aa ab _) =
@@ -68,8 +68,8 @@ instance PrettyPrint Annotation where
         in  printGroup (ptext "prec") $ braces ab' <+> aa' <+> braces ac'
     printText0 ga (Assoc_anno as aa _) =
         printGroup (case as of ARight -> ptext "right_assoc"
-                               ALeft -> ptext "left_assoc") $ fcat $ 
-                                punctuate (comma <> space) $ 
+                               ALeft -> ptext "left_assoc") $ fcat $
+                                punctuate (comma <> space) $
                                           map (printText0 ga) aa
     printText0 _ (Label [] _) = empty
     printText0 _ (Label aa _) =
@@ -97,12 +97,13 @@ printLine key line = if isEmpty line then pkey else pkey <+> line
     where pkey = ptext "%" <> key
 
 printAnnotationList_Text0 :: GlobalAnnos -> [Annotation] -> Doc
-printAnnotationList_Text0 ga l = (vcat $ map (printText0 ga) l) 
+printAnnotationList_Text0 ga l = (vcat $ map (printText0 ga) l)
 
 instance (PrettyPrint a) => PrettyPrint (Annoted a) where
     printText0 ga (Annoted i _ las ras) =
         let i'   = printText0 ga i
-            las' = printAnnotationList_Text0 ga las
+            las' = if null las then empty else
+                       text "" $+$ printAnnotationList_Text0 ga las
             (la,rras) = case ras of
                         []     -> (empty,[])
                         r@(l:xs)
@@ -113,26 +114,26 @@ instance (PrettyPrint a) => PrettyPrint (Annoted a) where
 
 instance PrettyPrint s => PrettyPrint (Named s) where
     printText0 ga = printText0 ga . sentence
--- other stuff must be printed logic dependent 
+-- other stuff must be printed logic dependent
 
--- | print sentence with label and non-axioms with implied annotation 
+-- | print sentence with label and non-axioms with implied annotation
 printLabelledSen :: PrettyPrint s => GlobalAnnos -> Named s -> Doc
-printLabelledSen ga s@NamedSen{senName = label, isAxiom = isAx} =  
+printLabelledSen ga s@NamedSen{senName = label, isAxiom = isAx} =
     printText0 ga s <> (if null label then empty else
            space <> printText0 ga (Label [label] nullRange))
         <> if isAx then empty else
         space <> printText0 ga (Semantic_anno SA_implied nullRange)
 
 -- | function to split the annotation to the right of an item
--- * fst contains printed label and implied annotion 
+-- * fst contains printed label and implied annotion
 --   if any at the begining of the list of annotations
 -- * snd contains the remaining annos
 splitAndPrintRAnnos :: (GlobalAnnos -> Annotation -> d)
                        -> (GlobalAnnos -> [Annotation] -> d)
-                       -> (d -> d -> d)    -- ^ a beside with space 
+                       -> (d -> d -> d)    -- ^ a beside with space
                                                  -- like <+> or <\+>
-                       -> (d -> d) -- ^ for Latex something to move the label 
-                              -- and \/ or implied annotation to the right 
+                       -> (d -> d) -- ^ for Latex something to move the label
+                              -- and \/ or implied annotation to the right
                               -- margin
                        -> d   -- ^ empty doc
                        -> GlobalAnnos -> [Annotation] -> (d, d)
@@ -144,10 +145,10 @@ splitAndPrintRAnnos pf pf_list sepF move nil ga ras =
                  | isImplied l -> (move $ pf ga l, nil)
                  | otherwise -> (nil,pf_list ga r)
              r@(l:impl:xs)
-                 | isLabel l && not (isImplied impl) 
+                 | isLabel l && not (isImplied impl)
                      -> (pf ga l, pf_list ga (impl:xs))
-                 | isLabel l && isImplied impl 
+                 | isLabel l && isImplied impl
                      -> (pf ga l `sepF` pf ga impl, pf_list ga xs)
-                 | isImplied l 
+                 | isImplied l
                      -> (move $ pf ga l, pf_list ga (impl:xs))
                  | otherwise -> (nil,pf_list ga r)
