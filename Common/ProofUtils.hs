e62ad09bc17d184562ee184746e80e438c070e68
Klaus Luettich <luettich@informatik.uni-bremen.de>
2006-02-13 17:05:06 +0000

    corrected maintainer email address git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5810 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Common/ProofUtils.hs
+++ Common/ProofUtils.hs
@@ -3,7 +3,7 @@ Module      :  $Header$
 Copyright   :  (c) Klaus Lüttich, C. Maeder, Uni Bremen 2005
 License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
 
-Maintainer  :  lüttich@tzi.de
+Maintainer  :  luettich@tzi.de
 Stability   :  provisional
 Portability :  portable
 
