010c2ff527dcdcdec75ae1389a1fdc528d36602b
Christian Maeder <Christian.Maeder@dfki.de>
2006-02-16 13:40:46 +0000

    my solaris wrapper script git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5826 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ utils/nightly/solaris/cronjob.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+PATH=/usr/local/bin:/usr/bin/:/usr/local/X11/bin:/home/pub-bkb/bin:/usr/ccs/bin
+MAKE=gmake
+UDG_HOME=/home/pub-bkb/uDrawGraph-3.1
+HETS_LIB=/local/home/maeder/haskell/CASL-lib
+
+export PATH
+export MAKE
+export UDG_HOME
+export HETS_LIB
+
+cd /local/home/maeder/haskell
+
+. ../cronjob.sh
+
+#rm -rf uni
+#ln -s /home/cofi/uni/solaris-ghc-6.4.1/uni .
+
+#makeProgramatica
+makeHets
+makeLibCheck
+
+cd CASL-lib
+chmod 775 hets
+chgrp wwwbkb hets
+bzip2 hets
+\cp -fp hets.bz2 /home/www/agbkb/forschung/formal_methods/CoFI/hets/solaris/daily/
