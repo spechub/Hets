5674c32995ec708ca0866242df6a466b3f05c52b
Christian Maeder <Christian.Maeder@dfki.de>
2006-02-16 10:15:30 +0000

    remove versions installed by install.sh git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5821 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ utils/uninstall.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -x
+
+#remove an installed version
+#the first argument should be a version number or a date  
+#compute date by "date -Idate"
+
+INSTALLDIR=/home/www/agbkb/forschung/formal_methods/CoFI/hets 
+VERSION=$1
+for i in linux solaris mac
+do
+  (cd $INSTALLDIR/$i; rm versions/hets-$VERSION.bz2)
+done
+      
+(cd $INSTALLDIR/src-distribution; rm versions/Hets-src-$VERSION.tgz) 
+
