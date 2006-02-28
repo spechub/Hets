a84fcef461d6ef423fa3bdefc29d745bd34cb331
Christian Maeder <Christian.Maeder@dfki.de>
2006-02-28 15:33:51 +0000

    changed diffing git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5881 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/nightly/linux/weekly.sh
+++ utils/nightly/linux/weekly.sh
@@ -13,19 +13,16 @@ cd $HETS_LIB
 #\cp -fp hets /home/linux-bkb/bin/
 
 # check differences of *.pp.het and *.pp.pp.het
-mkdir pp1
-cp Basic/*.pp.het pp1/
+cat Basic/*.pp.het pp1.txt
 \rm -f Basic/*.env
 hets -v2 -o pp.het Basic/*.pp.het
-mkdir pp2
-mv Basic/*.pp.pp.het pp2/
-diff pp1 pp2
-\rm -rf pp1 pp2
+cat Basic/*.pp.pp.het pp2.txt
+diff pp1.txt pp2.txt
 
 # try consistency check with SPASS
 hets -v2 -o dfg.c Basic/*.casl
 date
-/local/home/maeder/haskell/runSPASSconsistency.sh > /dev/null
+/local/home/maeder/haskell/runSPASSconsistency.sh
 date
 
 # check other examples
