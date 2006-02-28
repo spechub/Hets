806fd9e08b8b68eeeb0eccfd8892046a2985e594
Christian Maeder <Christian.Maeder@dfki.de>
2006-02-28 14:50:32 +0000

    changed order and used hets in PATH git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5879 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/nightly/linux/weekly.sh
+++ utils/nightly/linux/weekly.sh
@@ -8,30 +8,32 @@ export HETS_LIB
 
 cd $HETS_LIB
 
+# install a newer binary 
+#chgrp linuxbkb hets
+#\cp -fp hets /home/linux-bkb/bin/
+
 # check differences of *.pp.het and *.pp.pp.het
 mkdir pp1
 cp Basic/*.pp.het pp1/
 \rm -f Basic/*.env
-./hets -v2 -o pp.het Basic/*.pp.het
+hets -v2 -o pp.het Basic/*.pp.het
 mkdir pp2
 mv Basic/*.pp.pp.het pp2/
 diff pp1 pp2
 \rm -rf pp1 pp2
 
-# check other examples
-date
-for i in */*/*.het */*/*.casl; \
-   do ./hets -v2 -o prf,th,pp.het,pp.tex $i; done
-date
-for i in */*/*.prf; do ./hets -v2 -o th $i; done
-date
-
 # try consistency check with SPASS
-./hets -v2 -o dfg.c Basic/*.casl
+hets -v2 -o dfg.c Basic/*.casl
 date
 /local/home/maeder/haskell/runSPASSconsistency.sh > /dev/null
 date
 
-# install a newer binary 
-#chgrp linuxbkb hets
-#\cp -fp hets /home/linux-bkb/bin/
+# check other examples
+date
+for i in */*/*.het */*/*.casl; \
+   do hets -v2 -o prf,th,pp.het,pp.tex $i; done
+date
+for i in */*/*.prf; do hets -v2 -o th $i; done
+date
+
+
