449dec898e52d1ce9b809df70cf26cd567ae8518
Christian Maeder <Christian.Maeder@dfki.de>
2006-03-09 10:48:01 +0000

    use isabelle scripts git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5942 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Haskell/test/run.sh
+++ Haskell/test/run.sh
@@ -2,18 +2,18 @@
 
 CVSROOT=:pserver:cvsread@cvs-agbkb.informatik.uni-bremen.de:/repository
 
+isabellescriptspath=../../../utils/nightly/linux/
+
 run ()
 {
   cd $1
   ../../../ToHaskell/h2hf $1 *.hs
-  ./runisabelle.sh > log 2>&1
-  fgrep '***' log 
+  $isabellescriptspath/$2.sh *.thy > log 2>&1
+  fgrep '***' log
   cvs diff -u log
 cd ..
 }
 
-run HOL
+run HOL runisabelle
 \cp -f HOL/*.hs HOLCF/
-run HOLCF
-
-
+run HOLCF runHsIsabelle
