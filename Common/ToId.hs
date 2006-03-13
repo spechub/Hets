89d5f8c60a011ca3f5a63ccf64e6c779763d9f80
Christian Maeder <Christian.Maeder@dfki.de>
2006-03-13 16:03:59 +0000

    changed illegal identifiers to dot-words git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5971 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Common/ToId.hs
+++ Common/ToId.hs
@@ -15,11 +15,15 @@ module Common.ToId where
 import Common.Id
 import Common.ProofUtils
 import Common.Token
+import Common.Lexer
+import qualified Common.Lib.Map as Map 
+import Text.ParserCombinators.Parsec
 
--- todo: check for CASL keywords and legal underline words
-
--- | convert 
+-- | convert a string to a legal CASL identifier
 toId :: String -> Id
-toId s = mkId [mkSimpleId s]
-
-
+toId s = simpleIdToId $ mkSimpleId $ 
+    case parse (reserved casl_reserved_words scanAnyWords) "Common.ToId" s of
+    Left _ -> if null s then error "Common.ToId" else 
+              '.' : tail (concatMap ( \ c -> '_' : Map.findWithDefault [c] c 
+                              (Map.insert '_' "U" charMap)) s) 
+    Right _ -> s
