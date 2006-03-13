549fcb59564bcd22136d9250bbf733d978ae5fce
Christian Maeder <Christian.Maeder@dfki.de>
2006-03-13 16:17:15 +0000

    removed unused imports git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5973 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CASL/fromKif.hs
+++ CASL/fromKif.hs
@@ -5,7 +5,7 @@ License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
 
 Maintainer  :  maeder@tzi.de
 Stability   :  experimental
-Portability :  portable 
+Portability :  portable
 
 convert .kif to .casl
 -}
@@ -13,15 +13,12 @@ module Main where
 
 import CASL.Kif
 import CASL.Kif2CASL
-import CASL.Sign
-import CASL.AS_Basic_CASL
 import CASL.Print_AS_Basic()
 
 import Common.Utils
 import Common.GlobalAnnotations
 import Common.PrettyPrint
 import Common.Lib.Pretty
-import Common.AS_Annotation
 
 import Text.ParserCombinators.Parsec
 import System.Environment
@@ -30,15 +27,14 @@ main :: IO ()
 main = getArgs >>= mapM_ process
 
 process :: String -> IO ()
-process s = do 
-  e <- parseFromFile kifProg s 
-  case e of 
+process s = do
+  e <- parseFromFile kifProg s
+  case e of
     Left err -> putStrLn $ show err
-    Right l -> do 
+    Right l -> do
         let f = fst (stripSuffix [".kif"] s) ++ ".casl"
             (sign, sens) = kif2CASL l
             pp :: PrettyPrint a => a -> Doc
             pp = printText0 emptyGlobalAnnos
-            d = pp sign $+$ vcat (map pp sens) 
+            d = pp sign $+$ vcat (map pp sens)
         writeFile f $ shows d "\n"
-  
