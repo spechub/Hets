289eee1eab171c85d86c94f3c4076bb485ba8d32
Florian Mossakowski <fmossa@web.de>
2006-03-14 09:05:16 +0000

    Abstrakte Syntax + Parser implemntiert git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@5986 cec4b9c1-7d33-0410-9eda-942365e851bb

--- ConstraintCASL/AS_ConstraintCASL.der.hs
+++ ConstraintCASL/AS_ConstraintCASL.der.hs
@@ -24,12 +24,15 @@ type ConstraintCASLBasicSpec = BASIC_SPEC () () ConstraintFORMULA
 
 type ConstraintCASLFORMULA = FORMULA ConstraintFORMULA
 
-data ConstraintFORMULA = Implication_ConstraintFormula ATOMCONJUNCTION ATOMCONJUNCTION 
-		       | Equivalence_ConstraintFormula ATOMCONJUNCTION ATOMCONJUNCTION 
+data ConstraintFORMULA = Implication_ConstraintFormula 
+			 ATOMCONJUNCTION ATOMCONJUNCTION 
+		       | Equivalence_ConstraintFormula 
+			 ATOMCONJUNCTION ATOMCONJUNCTION 
 		       | Axiom_ConstraintFormula ATOMCONJUNCTION
 			 deriving (Eq, Ord, Show)
 
-data RELATION = Empty_Relation | Equal_Relation | Id_Relation Id | Relation_Disjunction [RELATION] | Inverse_Relation RELATION 
+data RELATION = Empty_Relation | Equal_Relation | Id_Relation Id 
+	      | Relation_Disjunction [RELATION] | Inverse_Relation RELATION 
 		deriving (Eq, Ord, Show)
 
 
@@ -37,5 +40,9 @@ data ATOMCONJUNCTION = Atom_Conjunction [ATOM]
 		   deriving (Eq, Ord, Show)
 
 
-data ATOM = Prefix_Atom RELATION [(TERM ())] | Infix_Atom (TERM ()) RELATION (TERM ())
+data ATOM = Prefix_Atom RELATION [(ConstraintTERM)] 
+	  | Infix_Atom (ConstraintTERM) RELATION (ConstraintTERM)
 	    deriving (Eq, Ord, Show)
+
+data ConstraintTERM = Atomar_Term Id | Composite_Term Id [ConstraintTERM]
+		      deriving (Eq, Ord, Show)
--- ConstraintCASL/Formula.hs
+++ ConstraintCASL/Formula.hs
@@ -35,8 +35,14 @@ cformula k =
         c2 <- conjunction k 
         return (Implication_ConstraintFormula c1 c2)
   <|> 
-    do { c1 <- conjunction k; equivalentT; c2 <- conjunction k; return (Equivalence_ConstraintFormula c1 c2)} <|> 
-    do { impliesT; c <- conjunction k; return (Axiom_ConstraintFormula c)}
+    do c1 <- conjunction k 
+       equivalentT
+       c2 <- conjunction k 
+       return (Equivalence_ConstraintFormula c1 c2)} 
+  <|> 
+    do impliesT 
+       c <- conjunction k
+       return (Axiom_ConstraintFormula c)
 
 conjunction :: [String] -> AParser st ATOMCONJUNCTION
 conjunction k = 
@@ -46,21 +52,45 @@ atom :: [String] -> AParser st ATOM
 atom k =
     try (do r <- relation k
             oParenT
-            terms <- many1 (term k)
+            terms <- many1 (constraintterm k)
             cParenT `notFollowedWith` (relation k)
             trace ("atom1: "++show (Prefix_Atom r terms)) $
              return (Prefix_Atom r terms))
   <|>
-    do {t1 <- term k; r <- trace ("atom2:"++show t1) $ relation k; t2 <- term k; return (Infix_Atom t1 r t2)} 
+    do t1 <- constraintterm k 
+       r <- trace ("atom2:"++show t1) $ relation k
+       t2 <- constraintterm k
+       return (Infix_Atom t1 r t2)} 
 
 relation :: [String] -> AParser st RELATION
 relation k =  
-    do {emptyRelationT; return Empty_Relation} <|>
-    do {equalityRelationT; return Equal_Relation} <|>
-    do {braceOpenT; r <- relation k; braceCloseT; return r} <|>
-    do {id <- parseId k; return (Id_Relation id)} <|> 
-    do {inverseT; r <- relation k; return (Inverse_Relation r)} -- <|>
+    do emptyRelationT 
+       return Empty_Relation 
+   <|>
+    do equalityRelationT 
+       return Equal_Relation 
+   <|>
+    do oParenT 
+       r <- relation k
+       cParenT 
+       return r 
+   <|>
+    do id <- parseId k 
+       return (Id_Relation id) 
+   <|> 
+    do inverseT 
+       r <- relation k 
+       return (Inverse_Relation r)} -- <|>
     --do {rels <- many1 (relation k); return (Relation_Disjunction rels)}
+
+constraintterm :: [String] -> AParser st ConstraintTERM
+constraintterm k =
+    do id <- parseId k 
+       return (Atomar_Term id)
+   <|>
+    do id <- parseId k 
+       terms <- many1 (constraintterm k)
+       return Composite_Term id terms  
 
 formula :: [String] -> AParser st ConstraintCASLFORMULA
 formula k = do x <- cformula k
@@ -83,19 +113,6 @@ inverse = "~"
 
 inverseT :: GenParser Char st Token
 inverseT = pToken $ toKey inverse
-
-braceOpen :: String
-braceOpen = "("
-
-braceOpenT :: GenParser Char st Token
-braceOpenT = pToken $ toKey braceOpen
-
-
-braceClose :: String
-braceClose = ")"
-
-braceCloseT :: GenParser Char st Token
-braceCloseT = pToken $ toKey braceClose
 
 implies :: String
 implies = "|-"
