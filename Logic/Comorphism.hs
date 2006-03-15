53310804002cd9e3c9c5844db3b984abcf001788
Christian Maeder <Christian.Maeder@dfki.de>
2006-03-15 20:13:50 +0000

    added disambiguation git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6024 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Logic/Comorphism.hs
+++ Logic/Comorphism.hs
@@ -1,5 +1,5 @@
-{-| 
-   
+{-|
+
 Module      :  $Header$
 Copyright   :  (c) Till Mossakowski, Uni Bremen 2002-2004
 License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
@@ -8,7 +8,7 @@ Maintainer  :  till@tzi.de
 Stability   :  provisional
 Portability :  non-portable (via Logic)
 
-Provides data structures for institution comorphisms. 
+Provides data structures for institution comorphisms.
    These are just collections of
    functions between (some of) the types of logics.
 
@@ -31,6 +31,7 @@ import Logic.Logic
 import Logic.Coerce
 import qualified Common.Lib.Set as Set
 import Common.Result
+import Common.ProofUtils
 import Common.AS_Annotation
 
 class (Language cid,
@@ -38,7 +39,7 @@ class (Language cid,
         basic_spec1 sentence1 symb_items1 symb_map_items1
         sign1 morphism1 symbol1 raw_symbol1 proof_tree1,
        Logic lid2 sublogics2
-        basic_spec2 sentence2 symb_items2 symb_map_items2 
+        basic_spec2 sentence2 symb_items2 symb_map_items2
         sign2 morphism2 symbol2 raw_symbol2 proof_tree2) =>
   Comorphism cid
             lid1 sublogics1 basic_spec1 sentence1 symb_items1 symb_map_items1
@@ -59,7 +60,7 @@ class (Language cid,
     mapSublogic :: cid -> sublogics1 -> sublogics2
     -- default implementation
     mapSublogic cid _ = targetSublogic cid
-    -- the translation functions are partial 
+    -- the translation functions are partial
     -- because the target may be a sublanguage
     -- map_basic_spec :: cid -> basic_spec1 -> Result basic_spec2
     -- cover theoroidal comorphisms as well
@@ -83,37 +84,37 @@ map_sign :: Comorphism cid
          => cid -> sign1 -> Result (sign2,[Named sentence2])
 map_sign cid sign = map_theory cid (sign,[])
 
-simpleTheoryMapping :: (sign1 -> sign2) -> (sentence1 -> sentence2) 
-                    -> (sign1, [Named sentence1]) 
+simpleTheoryMapping :: (sign1 -> sign2) -> (sentence1 -> sentence2)
+                    -> (sign1, [Named sentence1])
                     -> (sign2, [Named sentence2])
-simpleTheoryMapping mapSig mapSen (sign,sens) = 
+simpleTheoryMapping mapSig mapSen (sign,sens) =
     (mapSig sign, map (mapNamed mapSen) sens)
 
 mkTheoryMapping :: (Monad m) => (sign1 -> m (sign2, [Named sentence2]))
-                   -> (sign1 -> sentence1 -> m sentence2) 
-                   -> (sign1, [Named sentence1]) 
+                   -> (sign1 -> sentence1 -> m sentence2)
+                   -> (sign1, [Named sentence1])
                    -> m (sign2, [Named sentence2])
 mkTheoryMapping mapSig mapSen (sign,sens) = do
        (sign',sens') <- mapSig sign
        sens'' <- mapM (mapNamedM $ mapSen sign) sens
-       return (sign',sens'++sens'')
+       return (sign', disambiguateSens Set.empty . nameSens $ sens' ++ sens'')
 
-data IdComorphism lid sublogics = 
-     IdComorphism lid sublogics 
+data IdComorphism lid sublogics =
+     IdComorphism lid sublogics
 
 instance Logic lid sublogics
         basic_spec sentence symb_items symb_map_items
         sign morphism symbol raw_symbol proof_tree =>
    Show (IdComorphism lid sublogics)
-   where 
+   where
    show = language_name
 
 instance Logic lid sublogics
         basic_spec sentence symb_items symb_map_items
         sign morphism symbol raw_symbol proof_tree =>
          Language (IdComorphism lid sublogics) where
-           language_name (IdComorphism lid sub) = 
-               case sublogic_names lid sub of 
+           language_name (IdComorphism lid sub) =
+               case sublogic_names lid sub of
                [] -> error "language_name IdComorphism"
                h : _ -> "id_" ++ language_name lid ++ "." ++ h
 
@@ -126,7 +127,7 @@ instance Logic lid sublogics
           sign morphism symbol raw_symbol proof_tree
           lid sublogics
           basic_spec sentence symb_items symb_map_items
-          sign morphism symbol raw_symbol proof_tree 
+          sign morphism symbol raw_symbol proof_tree
          where
            sourceLogic (IdComorphism lid _sub) = lid
            targetLogic (IdComorphism lid _sub) = lid
@@ -143,7 +144,7 @@ data CompComorphism cid1 cid2 = CompComorphism cid1 cid2 deriving Show
 
 instance (Language cid1, Language cid2)
           => Language (CompComorphism cid1 cid2) where
-   language_name (CompComorphism cid1 cid2) = 
+   language_name (CompComorphism cid1 cid2) =
      language_name cid1++ ";" ++language_name cid2
 
 
@@ -151,7 +152,7 @@ instance (Comorphism cid1
             lid1 sublogics1 basic_spec1 sentence1 symb_items1 symb_map_items1
                 sign1 morphism1 symbol1 raw_symbol1 proof_tree1
             lid2 sublogics2 basic_spec2 sentence2 symb_items2 symb_map_items2
-                sign2 morphism2 symbol2 raw_symbol2 proof_tree2, 
+                sign2 morphism2 symbol2 raw_symbol2 proof_tree2,
           Comorphism cid2
             lid4 sublogics4 basic_spec4 sentence4 symb_items4 symb_map_items4
                 sign4 morphism4 symbol4 raw_symbol4 proof_tree4
@@ -162,48 +163,48 @@ instance (Comorphism cid1
               sign1 morphism1 symbol1 raw_symbol1 proof_tree1
               lid3 sublogics3 basic_spec3 sentence3 symb_items3 symb_map_items3
               sign3 morphism3 symbol3 raw_symbol3 proof_tree3 where
-   sourceLogic (CompComorphism cid1 _) = 
+   sourceLogic (CompComorphism cid1 _) =
      sourceLogic cid1
-   targetLogic (CompComorphism _ cid2) = 
+   targetLogic (CompComorphism _ cid2) =
      targetLogic cid2
-   sourceSublogic (CompComorphism cid1 _) = 
+   sourceSublogic (CompComorphism cid1 _) =
      sourceSublogic cid1
-   targetSublogic (CompComorphism cid1 cid2) = 
-     mapSublogic cid2 
-      (coerceSublogic (targetLogic cid1) (sourceLogic cid2) 
+   targetSublogic (CompComorphism cid1 cid2) =
+     mapSublogic cid2
+      (coerceSublogic (targetLogic cid1) (sourceLogic cid2)
         (targetSublogic cid1))
-   mapSublogic (CompComorphism cid1 cid2) = 
-     mapSublogic cid2 
-     . coerceSublogic (targetLogic cid1) (sourceLogic cid2) 
+   mapSublogic (CompComorphism cid1 cid2) =
+     mapSublogic cid2
+     . coerceSublogic (targetLogic cid1) (sourceLogic cid2)
      . mapSublogic cid1
-   map_sentence (CompComorphism cid1 cid2) = 
-       \si1 se1 -> 
+   map_sentence (CompComorphism cid1 cid2) =
+       \si1 se1 ->
          do (si2,_) <- map_sign cid1 si1
-            se2 <- map_sentence cid1 si1 se1 
-            (si2', se2') <- coerceBasicTheory 
-                (targetLogic cid1) (sourceLogic cid2) 
+            se2 <- map_sentence cid1 si1 se1
+            (si2', se2') <- coerceBasicTheory
+                (targetLogic cid1) (sourceLogic cid2)
                 "Mapping sentence along comorphism" (si2, [emptyName se2])
-            case se2' of 
+            case se2' of
                 [x] -> map_sentence cid2 si2' $ sentence x
                 _ -> error "CompComorphism.map_sentence"
 
-   map_theory (CompComorphism cid1 cid2) = 
-       \ti1 -> 
+   map_theory (CompComorphism cid1 cid2) =
+       \ti1 ->
          do ti2 <- map_theory cid1 ti1
-            ti2' <- coerceBasicTheory (targetLogic cid1) (sourceLogic cid2) 
-                        "Mapping theory along comorphism" ti2 
+            ti2' <- coerceBasicTheory (targetLogic cid1) (sourceLogic cid2)
+                        "Mapping theory along comorphism" ti2
             map_theory cid2 ti2'
 
-   map_morphism (CompComorphism cid1 cid2) = \ m1 -> 
-       do m2 <- map_morphism cid1 m1 
+   map_morphism (CompComorphism cid1 cid2) = \ m1 ->
+       do m2 <- map_morphism cid1 m1
           m3 <- coerceMorphism (targetLogic cid1) (sourceLogic cid2)
                   "Mapping signature morphism along comorphism"m2
           map_morphism cid2 m3
 
-   map_symbol (CompComorphism cid1 cid2) = \ s1 -> 
+   map_symbol (CompComorphism cid1 cid2) = \ s1 ->
          let mycast = coerceSymbol (targetLogic cid1) (sourceLogic cid2)
          in Set.unions
-                (map (map_symbol cid2 . mycast) 
+                (map (map_symbol cid2 . mycast)
                  (Set.toList (map_symbol cid1 s1)))
-   constituents (CompComorphism cid1 cid2) = 
+   constituents (CompComorphism cid1 cid2) =
       constituents cid1 ++ constituents cid2
