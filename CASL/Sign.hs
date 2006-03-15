bc48a75c0a968be3d6cb31768a4120cafba0d202
Christian Maeder <Christian.Maeder@dfki.de>
2006-03-15 16:43:50 +0000

    changed shadowed warning to a hint git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6019 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CASL/Sign.hs
+++ CASL/Sign.hs
@@ -1,21 +1,20 @@
-
 {- |
 Module      :  $Header$
-Copyright   :  (c) Christian Maeder and Uni Bremen 2002-2004
+Copyright   :  (c) Christian Maeder and Uni Bremen 2002-2006
 License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
 
 Maintainer  :  maeder@tzi.de
 Stability   :  provisional
 Portability :  portable
 
-CASL signature
-    
+This module provides CASL signatures that also serve as local
+  environments for the basic static analysis.  
 -}
 
 module CASL.Sign where
 
 import CASL.AS_Basic_CASL
-import CASL.Print_AS_Basic()
+import CASL.Print_AS_Basic ()
 import qualified Common.Lib.Map as Map
 import qualified Common.Lib.Set as Set
 import qualified Common.Lib.Rel as Rel
@@ -29,8 +28,8 @@ import Common.Result
 import Common.AS_Annotation
 import Common.GlobalAnnotations
 
--- constants have empty argument lists 
-data OpType = OpType {opKind :: FunKind, opArgs :: [SORT], opRes :: SORT} 
+-- constants have empty argument lists
+data OpType = OpType {opKind :: FunKind, opArgs :: [SORT], opRes :: SORT}
               deriving (Show, Eq, Ord)
 
 data PredType = PredType {predArgs :: [SORT]} deriving (Show, Eq, Ord)
@@ -38,12 +37,12 @@ data PredType = PredType {predArgs :: [SORT]} deriving (Show, Eq, Ord)
 type OpMap = Map.Map Id (Set.Set OpType)
 
 data Sign f e = Sign { sortSet :: Set.Set SORT
-               , sortRel :: Rel.Rel SORT         
+               , sortRel :: Rel.Rel SORT
                , opMap :: OpMap
                , assocOps :: OpMap
                , predMap :: Map.Map Id (Set.Set PredType)
                , varMap :: Map.Map SIMPLE_ID SORT
-               , sentences :: [Named (FORMULA f)]        
+               , sentences :: [Named (FORMULA f)]
                , envDiags :: [Diagnosis]
                , globAnnos :: GlobalAnnos
                , extendedInfo :: e
@@ -51,7 +50,7 @@ data Sign f e = Sign { sortSet :: Set.Set SORT
 
 -- better ignore assoc flags for equality
 instance (Eq f, Eq e) => Eq (Sign f e) where
-    e1 == e2 = 
+    e1 == e2 =
         sortSet e1 == sortSet e2 &&
         sortRel e1 == sortRel e2 &&
         opMap e1 == opMap e2 &&
@@ -67,7 +66,7 @@ emptySign e = Sign { sortSet = Set.empty
                , varMap = Map.empty
                , sentences = []
                , envDiags = []
-               , globAnnos = emptyGlobalAnnos 
+               , globAnnos = emptyGlobalAnnos
                , extendedInfo = e }
 
 -- | proper subsorts (possibly excluding input sort)
@@ -98,12 +97,12 @@ instance PrettyPrint PredType where
   printText0 ga pt = printText0 ga $ toPRED_TYPE pt
 
 instance (PrettyPrint f, PrettyPrint e) => PrettyPrint (Sign f e) where
-    printText0 ga s = 
-        ptext (sortS++sS) <+> commaT_text ga (Set.toList $ sortSet s) 
-        $$ 
+    printText0 ga s =
+        ptext (sortS++sS) <+> commaT_text ga (Set.toList $ sortSet s)
+        $$
         (if Rel.null (sortRel s) then empty
-            else ptext (sortS++sS) <+> 
-             (fsep . punctuate semi $ map printRel $ Map.toList 
+            else ptext (sortS++sS) <+>
+             (fsep . punctuate semi $ map printRel $ Map.toList
                        $ Rel.toMap $ Rel.transpose $ sortRel s))
         $$ printSetMap (ptext opS) empty ga (opMap s)
         $$ printSetMap (ptext predS) space ga (predMap s)
@@ -111,46 +110,46 @@ instance (PrettyPrint f, PrettyPrint e) => PrettyPrint (Sign f e) where
      where printRel (supersort, subsorts) =
              printSet ga subsorts <+> ptext lessS <+> printText0 ga supersort
 
-printSetMap :: (PrettyPrint k, PrettyPrint a, Ord k, Ord a) => Doc 
+printSetMap :: (PrettyPrint k, PrettyPrint a, Ord k, Ord a) => Doc
             -> Doc -> GlobalAnnos -> Map.Map k (Set.Set a) -> Doc
-printSetMap header sepa ga m = 
-    vcat $ map (\ (i, t) -> 
+printSetMap header sepa ga m =
+    vcat $ map (\ (i, t) ->
                header <+>
                printText0 ga i <+> colon <> sepa <>
-               printText0 ga t) 
+               printText0 ga t)
              $ concatMap (\ (o, ts) ->
                           map ( \ ty -> (o, ty) ) $ Set.toList ts)
-                   $ Map.toList m 
+                   $ Map.toList m
 
 -- working with Sign
 
 diffSig :: Sign f e -> Sign f e -> Sign f e
-diffSig a b = 
+diffSig a b =
     a { sortSet = sortSet a `Set.difference` sortSet b
       , sortRel = Rel.transClosure $ Rel.difference (sortRel a) $ sortRel b
       , opMap = opMap a `diffMapSet` opMap b
-      , assocOps = assocOps a `diffMapSet` assocOps b   
-      , predMap = predMap a `diffMapSet` predMap b      
+      , assocOps = assocOps a `diffMapSet` assocOps b
+      , predMap = predMap a `diffMapSet` predMap b
       }
-  -- transClosure needed:  {a < b < c} - {a < c; b} 
+  -- transClosure needed:  {a < b < c} - {a < c; b}
   -- is not transitive!
 
-diffMapSet :: (Ord a, Ord b) => Map.Map a (Set.Set b) 
+diffMapSet :: (Ord a, Ord b) => Map.Map a (Set.Set b)
            -> Map.Map a (Set.Set b) -> Map.Map a (Set.Set b)
 diffMapSet =
     Map.differenceWith ( \ s t -> let d = Set.difference s t in
-                         if Set.null d then Nothing 
+                         if Set.null d then Nothing
                          else Just d )
 
-addMapSet :: (Ord a, Ord b) => Map.Map a (Set.Set b) -> Map.Map a (Set.Set b) 
+addMapSet :: (Ord a, Ord b) => Map.Map a (Set.Set b) -> Map.Map a (Set.Set b)
           -> Map.Map a (Set.Set b)
-addMapSet = Map.unionWith Set.union 
+addMapSet = Map.unionWith Set.union
 
 addOpMapSet :: OpMap -> OpMap -> OpMap
 addOpMapSet m = remPartOpsM . addMapSet m
 
 addSig :: (e -> e -> e) -> Sign f e -> Sign f e -> Sign f e
-addSig ad a b = 
+addSig ad a b =
     a { sortSet = sortSet a `Set.union` sortSet b
       , sortRel = Rel.transClosure $ Rel.union (sortRel a) $ sortRel b
       , opMap = addOpMapSet (opMap a) $ opMap b
@@ -159,10 +158,10 @@ addSig ad a b =
       , extendedInfo = ad (extendedInfo a) $ extendedInfo b
       }
 
-isEmptySig :: (e -> Bool) -> Sign f e -> Bool 
-isEmptySig ie s = 
-    Set.null (sortSet s) && 
-    Rel.null (sortRel s) && 
+isEmptySig :: (e -> Bool) -> Sign f e -> Bool
+isEmptySig ie s =
+    Set.null (sortSet s) &&
+    Rel.null (sortRel s) &&
     Map.null (opMap s) &&
     Map.null (predMap s) && ie (extendedInfo s)
 
@@ -171,51 +170,51 @@ isSubMapSet :: (Ord a, Ord b) => Map.Map a (Set.Set b) -> Map.Map a (Set.Set b)
 isSubMapSet = Map.isSubmapOfBy Set.isSubsetOf
 
 isSubOpMap :: OpMap -> OpMap -> Bool
-isSubOpMap a b = Map.isSubmapOfBy ( \ s t -> 
+isSubOpMap a b = Map.isSubmapOfBy ( \ s t ->
                Set.fold ( \ e r -> r && (Set.member e t || case opKind e of
                          Partial -> Set.member e {opKind = Total} t
                          Total -> False)) True s) a b
 
-isSubSig :: (PrettyPrint e, PrettyPrint f) => 
+isSubSig :: (PrettyPrint e, PrettyPrint f) =>
             (e -> e -> Bool) -> Sign f e -> Sign f e -> Bool
-isSubSig isSubExt a b = 
-  Set.isSubsetOf (sortSet a) (sortSet b) 
+isSubSig isSubExt a b =
+  Set.isSubsetOf (sortSet a) (sortSet b)
           && Rel.isSubrelOf (sortRel a) (sortRel b)
           && isSubOpMap (opMap a) (opMap b)
-          -- ignore associativity properties! 
+          -- ignore associativity properties!
           && isSubMapSet (predMap a) (predMap b)
-          && isSubExt (extendedInfo a) (extendedInfo b) 
+          && isSubExt (extendedInfo a) (extendedInfo b)
 
 partOps :: Set.Set OpType -> [OpType]
-partOps s = map ( \ t -> t { opKind = Partial } ) 
+partOps s = map ( \ t -> t { opKind = Partial } )
          $ Set.toList $ Set.filter ((==Total) . opKind) s
 
-remPartOps :: Set.Set OpType -> Set.Set OpType 
+remPartOps :: Set.Set OpType -> Set.Set OpType
 remPartOps s = foldr Set.delete s $ partOps s
 
-remPartOpsM :: Ord a => Map.Map a (Set.Set OpType) 
-            -> Map.Map a (Set.Set OpType) 
+remPartOpsM :: Ord a => Map.Map a (Set.Set OpType)
+            -> Map.Map a (Set.Set OpType)
 remPartOpsM = Map.map remPartOps
 
 addDiags :: [Diagnosis] -> State.State (Sign f e) ()
-addDiags ds = 
+addDiags ds =
     do e <- State.get
        State.put e { envDiags = reverse ds ++ envDiags e }
 
 addSort :: SORT -> State.State (Sign f e) ()
-addSort s = 
+addSort s =
     do e <- State.get
        let m = sortSet e
-       if Set.member s m then 
-          addDiags [mkDiag Hint "redeclared sort" s] 
+       if Set.member s m then
+          addDiags [mkDiag Hint "redeclared sort" s]
           else State.put e { sortSet = Set.insert s m }
 
 hasSort :: Sign f e -> SORT -> [Diagnosis]
-hasSort e s = if Set.member s $ sortSet e then [] 
+hasSort e s = if Set.member s $ sortSet e then []
                 else [mkDiag Error "unknown sort" s]
 
 checkSorts :: [SORT] -> State.State (Sign f e) ()
-checkSorts s = 
+checkSorts s =
     do e <- State.get
        addDiags $ concatMap (hasSort e) s
 
@@ -223,44 +222,44 @@ addSubsort :: SORT -> SORT -> State.State (Sign f e) ()
 addSubsort = addSubsortOrIso True
 
 addSubsortOrIso :: Bool -> SORT -> SORT -> State.State (Sign f e) ()
-addSubsortOrIso b super sub = 
+addSubsortOrIso b super sub =
     do if b then checkSorts [super, sub] else return ()
        e <- State.get
-       let r = sortRel e  
-       State.put e { sortRel = (if b then id else 
+       let r = sortRel e
+       State.put e { sortRel = (if b then id else
                          Rel.insert super sub) $ Rel.insert sub super r }
        let p = posOfId sub
            rel = " '" ++ showPretty sub (if b then " < "
                                          else " = ") ++ showPretty super "'"
-       if super == sub then 
-          addDiags [mkDiag Warning 
+       if super == sub then
+          addDiags [mkDiag Warning
                     "void reflexive subsort" sub]
-          else if b then 
-              if Rel.path super sub r then 
+          else if b then
+              if Rel.path super sub r then
                   if  Rel.path sub super r then
-                  addDiags [Diag Warning 
+                  addDiags [Diag Warning
                             ("sorts are isomorphic" ++ rel) p]
-                  else addDiags [Diag Warning 
+                  else addDiags [Diag Warning
                                  ("added subsort cycle by" ++ rel) p]
-              else if Rel.path sub super r then 
+              else if Rel.path sub super r then
                   addDiags [Diag Hint ("redeclared subsort" ++ rel) p]
               else return ()
-          else if Rel.path super sub r then 
+          else if Rel.path super sub r then
                   if Rel.path sub super r then
-                       addDiags [Diag Hint 
+                       addDiags [Diag Hint
                                  ("redeclared isomoprhic sorts" ++ rel) p]
-                  else addDiags [Diag Warning 
-                                 ("subsort '" ++ showPretty super 
-                                  "' made isomorphic by" ++ rel) 
+                  else addDiags [Diag Warning
+                                 ("subsort '" ++ showPretty super
+                                  "' made isomorphic by" ++ rel)
                                  $ posOfId super]
                else if Rel.path sub super r then
-                  addDiags [Diag Warning 
+                  addDiags [Diag Warning
                             ("subsort  '" ++ showPretty sub
                              "' made isomorphic by" ++ rel) p]
                   else return()
 
 closeSubsortRel :: State.State (Sign f e) ()
-closeSubsortRel= 
+closeSubsortRel=
     do e <- State.get
        State.put e { sortRel = Rel.transClosure $ sortRel e }
 
@@ -268,34 +267,34 @@ alsoWarning :: String -> Id -> [Diagnosis]
 alsoWarning msg i = [mkDiag Warning ("also known as " ++ msg) i]
 
 checkWithOtherMap :: String -> Map.Map Id a -> Id -> [Diagnosis]
-checkWithOtherMap msg m i = 
-    case Map.lookup i m of 
+checkWithOtherMap msg m i =
+    case Map.lookup i m of
     Nothing -> []
     Just _ -> alsoWarning msg i
 
 addVars :: VAR_DECL -> State.State (Sign f e) ()
-addVars (Var_decl vs s _) = do 
+addVars (Var_decl vs s _) = do
     checkSorts [s]
     mapM_ (addVar s) vs
 
 addVar :: SORT -> SIMPLE_ID -> State.State (Sign f e) ()
-addVar s v = 
+addVar s v =
     do e <- State.get
        let m = varMap e
-           i = simpleIdToId v 
+           i = simpleIdToId v
            ds = case Map.lookup v m of
-                Just _ -> [mkDiag Warning "known variable shadowed" v] 
+                Just _ -> [mkDiag Hint "known variable shadowed" v]
                 Nothing -> []
        State.put e { varMap = Map.insert v s m }
        addDiags $ ds ++ checkWithOtherMap "operation" (opMap e) i
                 ++ checkWithOtherMap "predicate" (predMap e) i
 
-addOpTo :: Id -> OpType -> OpMap -> OpMap 
-addOpTo k v m = 
+addOpTo :: Id -> OpType -> OpMap -> OpMap
+addOpTo k v m =
     let l = Map.findWithDefault Set.empty k m
-        n = Map.insert k (Set.insert v l) m   
+        n = Map.insert k (Set.insert v l) m
     in case opKind v of
-     Total -> let vp =  v { opKind = Partial } in 
+     Total -> let vp =  v { opKind = Partial } in
               if Set.member vp l then
               Map.insert k (Set.insert v $ Set.delete vp l) m
               else n
