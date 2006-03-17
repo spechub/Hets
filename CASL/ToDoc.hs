690e4ab8f298d9cff3803316cda70ad9b98e9c43
Christian Maeder <Christian.Maeder@dfki.de>
2006-03-17 12:26:13 +0000

    check for simple terms git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6043 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CASL/ToDoc.hs
+++ CASL/ToDoc.hs
@@ -49,8 +49,7 @@ printOpType (Op_type p l r _) =
 printOpSymb :: OP_SYMB -> Doc
 printOpSymb o = case o of
     Op_name i -> idDoc i
-    Qual_op_name i t _ ->
-        parens $ fsep [text opS, idDoc i, colon <> printOpType t]
+    Qual_op_name i t _ -> fsep [text opS, idDoc i, colon <> printOpType t]
 
 printPredType :: PRED_TYPE -> Doc
 printPredType (Pred_type l _) = case l of
@@ -66,23 +65,19 @@ printPredSymb p = case p of
 printRecord :: (f -> Doc) -> Record f Doc Doc
 printRecord mf = Record
     { foldQuantification = \ _ q l r _ ->
-          printQuant q <+> fsep (punctuate semi (map printVarDecl l)
-                                ++ [bullet, r])
+          fsep $ printQuant q : punctuate semi (map printVarDecl l)
+                                ++ [bullet, r]
     , foldConjunction = \ (Conjunction ol _) l _ ->
-          fsep $ punctuate (space <> andDoc) $ zipWith
-                   ( \ o d -> if isJunct o then parens d else d) ol l
+          fsep $ punctuate (space <> andDoc) $ zipWith mkJunctDoc ol l
     , foldDisjunction = \ (Disjunction ol _) l _ ->
-          fsep $ punctuate (space <> orDoc) $ zipWith
-                   ( \ o d -> if isJunct o then parens d else d) ol l
+          fsep $ punctuate (space <> orDoc) $ zipWith mkJunctDoc ol l
     , foldImplication = \ (Implication oL oR b _) l r _ _ ->
           let nl = if isAnyImpl oL then parens l else l
               nr = if isImpl b oR then parens r else r
           in if b then fsep [nl, implies, nr]
              else fsep [nr, text ifS, nl]
     , foldEquivalence = \ (Equivalence oL oR _) l r _ ->
-          let nl = if isEquiv oL then parens l else l
-              nr = if isEquiv oR then parens r else r
-          in fsep [nl, equiv, nr]
+          fsep [mkEquivDoc oL l, equiv, mkEquivDoc oR r]
     , foldNegation = \ _ r _ -> fsep [notDoc, r]
     , foldTrue_atom = \ _ _ -> text trueS
     , foldFalse_atom = \ _ _ -> text falseS
@@ -111,10 +106,12 @@ printRecord mf = Record
           parens $ fsep [text varS, sidDoc v, colon, idDoc s]
     , foldApplication = \ _ o l _ -> case o of
           Op_name i -> idApplDoc i l
-          Qual_op_name _ _ _ -> if null l then printOpSymb o else
-              fcat [printOpSymb o, parens $ fsep $ punctuate comma l]
-    , foldSorted_term = \ _ r t _ -> fsep [r, colon, idDoc t]
-    , foldCast = \ _ r t _ -> fsep [r, text asS, idDoc t]
+          Qual_op_name _ _ _ -> let d = parens $ printOpSymb o in 
+              if null l then d else fcat [d, parens $ fsep $ punctuate comma l]
+    , foldSorted_term = \ (Sorted_term o _ _) r t _ -> 
+          fsep [mkSimpleDoc o r <> colon, idDoc t]
+    , foldCast = \ (Cast o _ _) r t _ -> 
+          fsep [mkSimpleDoc o r, text asS, idDoc t]
     , foldConditional = \ (Conditional ol _ _ _) l f r _ ->
           fsep [if isCond ol then parens l else l,
                 text whenS, f, text elseS, r]
@@ -133,6 +130,15 @@ printRecord mf = Record
 printFormula :: FORMULA f -> Doc
 printFormula = foldFormula $ printRecord (error "printFormula")
 
+mkSimpleDoc :: TERM f -> Doc -> Doc
+mkSimpleDoc t = if isSimpleTerm t then id else parens
+
+isSimpleTerm :: TERM f -> Bool
+isSimpleTerm t = case t of
+    Application (Op_name i) (_ : _) _ -> not $ isMixfix i
+    Mixfix_term _ -> False
+    _ -> True
+
 isQuant, isEquiv, isAnyImpl, isJunct :: FORMULA f -> Bool
 isQuant f = case f of
     Quantification _ _ _ _ -> True
@@ -148,6 +154,12 @@ isJunct f = case f of
     Conjunction _ _ -> True
     Disjunction _ _ -> True
     _ -> isAnyImpl f
+
+mkJunctDoc :: FORMULA f -> Doc -> Doc
+mkJunctDoc f = if isJunct f then parens else id
+
+mkEquivDoc :: FORMULA f -> Doc -> Doc
+mkEquivDoc f = if isEquiv f then parens else id
 
 isImpl :: Bool -> FORMULA f -> Bool
 isImpl a f = case f of
