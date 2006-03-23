5b426254dd143569e086ba3b8a823a09f2394844
Klaus Luettich <luettich@informatik.uni-bremen.de>
2006-03-23 15:32:13 +0000

    added more services and operations git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6112 cec4b9c1-7d33-0410-9eda-942365e851bb

--- SPASS/tests/soapTest.hs
+++ SPASS/tests/soapTest.hs
@@ -1,6 +1,6 @@
 
 -- | simple test program that sends a file as soap message to a 
--- MathServ Broker
+-- MathServ Broker and to other Services like Vampire
 
 module Main where
 
@@ -11,6 +11,8 @@ import System.Exit
 import Network.URI
 import Network.Service
 
+import Text.XML.HXT.Aliases
+
 import Text.XML.Serializer
 import Org.Xmlsoap.Schemas.Soap.Envelope as ENV
 
@@ -20,18 +22,73 @@ import Data.Typeable
 data MathServServices = 
     ProveProblemOpt { in0 :: String
                     , in1 :: Int}
+  | ProveProblemChoice { in0 :: String
+                       , in1 :: Int}
+  | ProveTPTPProblem { in0 :: String
+                     , in1 :: Int}
+  | ProveTPTPProblemWithOptions { in0 :: String
+                                , in1 :: Int
+                                , in2 :: String}
+  | ProveProblem { in0 :: String
+                 , in1 :: Int}
 
-$(xmlify [''MathServServices] [])
+data MathServOutput = 
+    ProveProblemOptResponse { 
+          proveProblemOptReturn :: String } 
+  | ProveProblemChoiceResponse { 
+          proveProblemChoiceReturn :: String } 
+  | ProveTPTPProblemResponse { 
+          proveTPTPProblemReturn :: String } 
+  | ProveTPTPProblemWithOptionsResponse { 
+          proveTPTPProblemWithOptionsReturn :: String } 
+  | ProveProblemResponse { 
+          proveProblemReturn :: String } 
+    deriving Show
 
 instance XMLNamespace MathServServices
+instance XMLNamespace MathServOutput
 
-mkProveProblem :: String -> Int -> MathServServices
-mkProveProblem = ProveProblemOpt
+
+$(xmlify [''MathServServices] [])
+$(xmlify [''MathServOutput] [capFieldsE])
+
+getResponse :: MathServOutput -> String
+getResponse mso = 
+    case mso of
+    ProveProblemOptResponse _ -> proveProblemOptReturn mso
+    ProveProblemChoiceResponse _ -> proveProblemChoiceReturn mso
+    ProveTPTPProblemResponse _ -> proveTPTPProblemReturn mso
+    ProveProblemResponse _ -> proveProblemReturn mso
+    ProveTPTPProblemWithOptionsResponse _ -> 
+        proveTPTPProblemWithOptionsReturn mso
+
+mkProveProblem :: Maybe String -- ^ extra options
+               -> String -- ^ Service name 
+               -> String -- ^ SOAP operation name
+               -> String -> Int -> MathServServices
+mkProveProblem mopts service operation = 
+    case service of 
+     "VampireService" -> 
+         case operation of 
+          "ProveTPTPProblem" -> maybe ProveTPTPProblem
+                                      (\ opts -> \ x y -> 
+                                          ProveTPTPProblemWithOptions x y opts)
+                                      mopts
+          "ProveProblem" -> ProveProblem
+          _ -> fail $ "unknown Operation for service Broker\n"++
+                  "known operations: ProveProblem, ProveTPTPProblem"
+     "Broker" -> case operation of 
+                 "ProveProblemOpt" -> ProveProblemOpt
+                 "ProveProblemChoice" -> ProveProblemChoice
+                 _ -> fail $ "unknown Operation for service Broker\n"++
+                       "known operations: ProveProblemOpt, ProveProblemChoice"
+     _ -> fail $ "unknown Service\nknown services: "++
+                          "VampireService, Broker"
 
 usage :: String -> IO ()
 usage m = do hPutStrLn stderr $
                  "Usage: soapTest <server-name> <port> <service-name> "++
-                 "<problem-file> <timeout>\n"++m
+                 "<operation> <problem-file> <timeout> [<options>]\n"++m
              exitWith (ExitFailure 2)
 
 makeEndPoint :: String -> Maybe HTTPTransport
@@ -44,25 +101,33 @@ makeEndPoint uriStr = maybe Nothing
 main :: IO ()
 main = do
    args <- getArgs
-   if length args == 5 
-      then doSoapCall args
-      else usage "too few arguments"
+   if length args == 6
+      then doSoapCall Nothing args
+      else if length args == 7 
+           then doSoapCall (Just $ last args) $ init args
+           else usage "too few arguments"
 
-doSoapCall :: [String] -> IO ()
-doSoapCall (server:port:service:problemFile:timeoutStr:[]) =
+doSoapCall :: Maybe String -> [String] -> IO ()
+doSoapCall mopts (server:port:service:operation:problemFile:timeoutStr:[]) =
     do problem <- readFile problemFile
        let (timeout :: Int) = read timeoutStr
            -- problem = "Egal"
        maybe (usage "please give a valid server name and port number")
              (\ endPoint -> do
-                 (res::Either Fault String) <- soapCall endPoint $
-                                           mkProveProblem problem timeout
+                 (res::Either SimpleFault MathServOutput) 
+                     <- soapCall endPoint $
+                        mkProveProblem mopts service operation problem timeout
                  case res of
-                  Left f -> do putStrLn "SOAP Fault"
-                               putStrLn $ show f
-                  Right r -> do putStrLn "Got a result"
-                                putStrLn $ show r
+                  Left err -> putStrLn $ show err
+                  Right resp -> do
+                      writeFile (problemFile++".xml") $ getResponse resp
+                      let xmlCont = getResponse resp
+                      mtrees <- parseXML xmlCont
+                      maybe (putStrLn $ "no parse of:\n"++xmlCont)
+                            putXML
+                            mtrees   
              )
              (makeEndPoint $ 
-                "http://"++server++':':port++"/axis/services/Broker")
-doSoapCall _ = fail "wrong number of arguments"
+                "http://"++server++':':port++"/axis/services/"++service)
+doSoapCall _ _ = fail $ "wrong number of arguments:\n" ++ 
+              " ./soapTest <server> <port> <service> <problemFile> <timelimit>"
