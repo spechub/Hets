b20cc520e698253354303b7bf3bc17f84240b213
Klaus Luettich <luettich@informatik.uni-bremen.de>
2006-03-31 10:34:02 +0000

    added coerceTheory git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6139 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Logic/Coerce.hs
+++ Logic/Coerce.hs
@@ -64,7 +64,17 @@ coerceBasicTheory ::
             -> (sign1, [Named sentence1]) -> m (sign2, [Named sentence2])
 coerceBasicTheory l1 l2 msg t1 = primCoerce l1 l2 msg t1
 
-coerceMorphism ::
+coerceTheory :: 
+  (Logic  lid1 sublogics1 basic_spec1 sentence1 symb_items1 symb_map_items1
+               sign1 morphism1 symbol1 raw_symbol1 proof_tree1,
+   Logic  lid2 sublogics2 basic_spec2 sentence2 symb_items2 symb_map_items2
+               sign2 morphism2 symbol2 raw_symbol2 proof_tree2,
+   Monad m) => lid1 -> lid2 -> String 
+            -> Theory sign1 sentence1 proof_tree1
+            -> m (Theory sign2 sentence2 proof_tree2)
+coerceTheory l1 l2 msg t1 = primCoerce l1 l2 msg t1
+
+coerceMorphism ::    
   (Logic  lid1 sublogics1 basic_spec1 sentence1 symb_items1 symb_map_items1
                 sign1 morphism1 symbol1 raw_symbol1 proof_tree1,
    Logic  lid2 sublogics2 basic_spec2 sentence2 symb_items2 symb_map_items2
