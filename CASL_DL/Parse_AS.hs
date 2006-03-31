d24a0ba1c91c810e7d1d674a8f8dd9870366bed9
Klaus Luettich <luettich@informatik.uni-bremen.de>
2006-03-31 10:34:02 +0000

    adapted to meet with new AS git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6143 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CASL_DL/Parse_AS.hs
+++ CASL_DL/Parse_AS.hs
@@ -25,21 +25,33 @@ import Text.ParserCombinators.Parsec
 
 import CASL.Formula
 import CASL.OpItem
-
+import CASL.AS_Basic_CASL 
 import Data.List
 
 dlFormula :: AParser st DL_FORMULA
 dlFormula = 
     do (ct,ctp) <- cardKeyword
        o <- oBracketT
-       p <- parseId casl_DL_reserved_words
+       p <- parsePredSymb
        c <- cBracketT
        op <- oParenT
        t1 <- term casl_DL_reserved_words 
        co <- anComma 
        t2 <- term casl_DL_reserved_words 
        cp <- cParenT
-       return (Cardinality ct p t1 t2 (appRange ctp (concatMapRange tokPos (o:c:op:co:[cp]))))
+       return (Cardinality ct p t1 t2 
+                   (appRange ctp (concatMapRange tokPos (o:c:op:co:[cp]))))
+
+parsePredSymb :: AParser st PRED_SYMB
+parsePredSymb = 
+   (do pi <- parseId casl_DL_reserved_words
+       return $ Pred_name pi)
+   <|>
+   (do o <- oParenT << addAnnos
+       Mixfix_qual_pred qpred <- qualPredName casl_DL_reserved_words o
+       return qpred)
+   <?> "Expecting a PRED_SYMB"
+
 
 cardKeyword :: AParser st (CardType,Range)
 cardKeyword = 
