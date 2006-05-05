e3b1339811d25eb3c79f9633327c823fef788a24
Rainer Grabbe <rainer@informatik.uni-bremen.de>
2006-05-05 10:33:24 +0000

    Printing variable names. Error checking with assert on termTermList and on number of arguments for printTermList. Successfully tested. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6285 cec4b9c1-7d33-0410-9eda-942365e851bb

--- SPASS/PrintTPTP.hs
+++ SPASS/PrintTPTP.hs
@@ -14,12 +14,14 @@ Pretty printing for SPASS signatures in TPTP syntax.
 
 -}
 
-module PrintTPTP where
+module SPASS.PrintTPTP where
 
 import Maybe
 
 import Common.AS_Annotation
 import Common.Lib.Pretty
+
+import Control.Exception
 
 import SPASS.Sign
 import SPASS.Conversions
@@ -35,18 +37,20 @@ class PrintTPTP a where
 dot :: Doc
 dot = char '.'
 
-implies :: Doc
-implies = text "=>"
-
-endOfListS :: String
-endOfListS = "end_of_list."
-
 {- |
   Helper function. Generates a separating comment line.
 -}
 separator :: String
 separator = "%-----------------------------------------------------------------"
             ++ "---------"
+
+{- |
+  Helper function. Generates a comma separated list of SPASS Terms as a Doc.
+-}
+printCommaSeparated :: [SPTerm] -> Doc
+printCommaSeparated = foldl (\vl v -> if isEmpty vl then printTPTP v
+                                      else vl <> comma <> printTPTP v) empty
+
 
 instance PrintTPTP Sign where
     printTPTP = printTPTP . signToSPLogicalPart
@@ -56,7 +60,7 @@ instance PrintTPTP Sign where
 -}
 instance PrintTPTP SPProblem where
     printTPTP p = text separator
-      $$ text "Problem" <+> colon <+> text (identifier p)
+      $$ text "% Problem" <+> colon <+> text (identifier p)
       $$ printTPTP (description p)
       $$ text separator
       $$ printTPTP (logicalPart p)
@@ -71,7 +75,7 @@ instance PrintTPTP SPLogicalPart where
         else empty)
 
 {- |
-  Creates a Common.Lib.Pretty.Doc from a SPASS Formula List
+  Creates a Doc from a SPASS Formula List
 -}
 instance PrintTPTP SPFormulaList where
     printTPTP l = foldl (\fl x-> fl $$ printFormula (originType l) x)
@@ -92,8 +96,8 @@ instance PrintTPTP SPOriginType where
 -}
 printFormula :: SPOriginType -> SPFormula -> Doc
 printFormula ot f =
-    text "fof" <> parens (quotes (text (senName f)) <> comma <> printTPTP ot <> comma <>
-                          parens (empty $$ printTPTP (sentence f))) <> dot
+    text "fof" <> parens (text (senName f) <> comma <> printTPTP ot <> comma <>
+                          parens (printTPTP (sentence f))) <> dot
 
 {- |
   Creates a Doc from a SPASS Term.
@@ -102,50 +106,61 @@ instance PrintTPTP SPTerm where
     printTPTP t = case t of
       SPQuantTerm{quantSym= qsym, termTermList= tlist, termTerm= tt}
         -> printTPTP qsym
-           <+> brackets (printVariableList tlist)
+           <+> brackets (printCommaSeparated $ getVariables tlist)
            <+> colon
-           <+> parens (printTermList SPAnd tlist
-                       <+> implies $$ printTPTP tt)
-      SPSimpleTerm stsym -> space <> printTPTP stsym
+           -- either all variables are simple terms
+           <+> if (filter isSimpleTerm tlist == tlist) then printTPTP tt
+               -- or there are none simple terms
+               else assert (null $ filter isSimpleTerm tlist)
+               -- construct premiss for implication out of termTermList
+                           printTermList SPImplies [SPComplexTerm{
+                                                      symbol=SPAnd,
+                                                      arguments=tlist}, tt]
+      SPSimpleTerm stsym -> printTPTP stsym
       SPComplexTerm{symbol= ctsym, arguments= args}
         -> if null args
-           then (space <> printTPTP ctsym)
-           else (space <> printTermList ctsym args)
+           then printTPTP ctsym
+           else printTermList ctsym args
       where
--- !! wir kriegen hier entweder SPSimpleTerm oder SPComplexTerm. Das müssen wir solange
---    durchfiltern, bis nur noch alle SPSimpleTerm übrigbleiben. Diese werden anschließend
---    in eine Menge gesteckt (also nur je einmal vorhanden). Geht das? Wie?
--- !! here the difficult part: how to get the variable names out of the tList??
---    this does something, but it won't be the filtered variable names only
-        printVariableList = foldl (\ tl v -> if isEmpty tl then printTPTP v
-                                             else tl <> comma <> printTPTP v) empty
-      
-      
+        isSimpleTerm tm = case tm of 
+            SPSimpleTerm _ -> True
+            _              -> False
+
+        -- check that every list entry is simple term
+        getSimpleVars = foldr (\v vl -> assert (isSimpleTerm v) $ v:vl) []
+        getVariables tl = if null $ filter isSimpleTerm tl
+                          then foldr (\co col -> (getSimpleVars $
+                                                  arguments co)++col)
+                                     [] tl
+                          else getSimpleVars tl
+
 {- |
   Creates a Doc from a list of SPASS Terms connected by a SPASS Symbol.
 -}
 printTermList :: SPSymbol -> [SPTerm] -> Doc
 printTermList symb terms = case symb of
--- !! maybe use terms always as last parameter?
--- !! what about unary connectors? has to be defined with SPCustomSymbol!
-
-    SPEqual            -> connectTerms (char '=') terms
-    -- true and false ignore term list
-    SPTrue             -> text "$true"
-    SPFalse            -> text "$false"
-    SPOr               -> connectTerms (char '|') terms
-    SPAnd              -> connectTerms (char '&') terms
+    SPEqual            -> assert (length terms == 2) $ associate SPEqual
+    SPTrue             -> assert (length terms == 0) $ associate SPTrue
+    SPFalse            -> assert (length terms == 0) $ associate SPFalse
+    -- variable number of terms for or/and
+    SPOr               -> assert (length terms >= 0) $ associate SPOr
+    SPAnd              -> assert (length terms >= 0) $ associate SPAnd
     -- only one term can be negated
-    SPNot              -> (char '~') <> parens (printTPTP $ head terms)
-    SPImplies          -> connectTerms implies terms
-    SPImplied          -> connectTerms (text "<=") terms
-    SPEquiv            -> connectTerms (text "<=>") terms
-    SPCustomSymbol cst -> connectTerms (text cst) terms
+    SPNot              -> assert (length terms == 1) $ applicate SPNot
+    SPImplies          -> assert (length terms == 2) $ associate SPImplies
+    SPImplied          -> assert (length terms == 2) $ associate SPImplied
+    SPEquiv            -> assert (length terms == 2) $ associate SPEquiv   
+    SPCustomSymbol cst -> applicate $ SPCustomSymbol cst
     where
-      connectTerms sb t =
-        parens (foldl (\ tl x -> if isEmpty tl then printTPTP x
-                                 else tl <+> sb $$ space <> printTPTP x)
-                      empty t)
+      associate sb = case terms of
+        []  -> empty
+        [x] -> printTPTP x
+        _   -> parens (foldl (\tl x -> if isEmpty tl then printTPTP x
+                                       else tl <+> printTPTP sb $$ printTPTP x)
+                             empty terms)
+      applicate sb =
+        if null terms then printTPTP sb
+        else printTPTP sb <> parens (printCommaSeparated terms)
 
 {- |
   Creates a Doc from a SPASS Quantifier Symbol.
@@ -160,37 +175,36 @@ instance PrintTPTP SPQuantSym where
   Creates a Doc from a SPASS Symbol.
 -}
 instance PrintTPTP SPSymbol where
--- !! maybe this doesn't work properly in all cases (if any)
     printTPTP s = case s of
-     SPEqual            -> char '='
-     SPTrue             -> text "$true"
-     SPFalse            -> text "$false"
-     SPOr               -> char '|'
-     SPAnd              -> char '&'
-     SPNot              -> char '~'
-     SPImplies          -> implies
-     SPImplied          -> text "<="
-     SPEquiv            -> text "<=>"
-     SPCustomSymbol cst -> text cst
+        SPEqual            -> char '='
+        SPTrue             -> text "$true"
+        SPFalse            -> text "$false"
+        SPOr               -> char '|'
+        SPAnd              -> char '&'
+        SPNot              -> char '~'
+        SPImplies          -> text "=>"
+        SPImplied          -> text "<="
+        SPEquiv            -> text "<=>"
+        SPCustomSymbol cst -> text cst
 
 {- |
   Creates a Doc from a SPASS description.
 -}
 instance PrintTPTP SPDescription where
-    printTPTP d = spText "Name   " (name d)
-      $$ spText "Author " (author d)
-      $$ maybe empty (spText "Version") (version d)
-      $$ maybe empty (spText "Logic  ") (logic d)
-      $$ text "Status " <+> colon <+> printTPTP (status d)
-      $$ spText "Desc   " (desc d)
-      $$ maybe empty (spText "Date   ") (date d)
+    printTPTP d = spCommentText "Name   " (name d)
+      $$ spCommentText "Author " (author d)
+      $$ maybe empty (spCommentText "Version") (version d)
+      $$ maybe empty (spCommentText "Logic  ") (logic d)
+      $$ text "% Status " <+> colon <+> printTPTP (status d)
+      $$ spCommentText "Desc   " (desc d)
+      $$ maybe empty (spCommentText "Date   ") (date d)
 
 {- |
-  Helper function. Creates a formatted output for a description field.
+  Helper function. Creates a formatted comment output for a description field.
   On left side will be displayed the field's name, on right side its content.
 -}
-spText :: String -> String -> Doc
-spText sn sd = hsep [text sn, colon, text sd]
+spCommentText :: String -> String -> Doc
+spCommentText fieldName fieldDesc = hsep [char '%', text fieldName, colon, text fieldDesc]
 
 {- |
   Creates a Doc from an 'SPLogState'.
