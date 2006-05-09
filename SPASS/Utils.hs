84870b073ef394967196baf2e1fcd819f857eb98
Klaus Luettich <luettich@informatik.uni-bremen.de>
2006-05-09 18:28:35 +0000

    Some utility functions and datatypes for SPASS and TPTP git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6300 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ SPASS/Utils.hs
@@ -0,0 +1,26 @@
+{- |
+Module      :  $Header$
+Description :  Some utilities for SPASS and TPTP.
+Copyright   :  (c) Klaus Lüttich, Uni Bremen 2005
+License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
+
+Maintainer  :  luettich@tzi.de
+Stability   :  provisional
+Portability :  portable
+
+Some utilities for SPASS and TPTP
+
+-}
+
+module SPASS.Utils where
+
+import qualified CASL.Sign as CSign
+import CASL.AS_Basic_CASL (SORT)
+
+
+-- | A data type for storing the type of a CASL Id; needed for SPASS and TPTP
+data CType = CSort
+           | CVar SORT
+           | CPred CSign.PredType
+           | COp   CSign.OpType
+             deriving (Eq,Ord,Show)
