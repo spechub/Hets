333d622fda2aed69a284e4c0eddf3aca4a857e6a
Klaus Luettich <luettich@informatik.uni-bremen.de>
2006-05-28 20:05:50 +0000

    added German clarification of todo list git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6391 cec4b9c1-7d33-0410-9eda-942365e851bb

--- SPASS/PrintTPTP.hs
+++ SPASS/PrintTPTP.hs
@@ -20,20 +20,46 @@ Pretty printing for SPASS signatures in TPTP syntax.
    - output typing for operations as sentences with name 
      * a conjunction of sorted variables for each parameter 
        forms the premise of an implication if there are any parameters
+
+       Eine Kunjunktion (&) von Typapplikationen, für jedes Argument und
+       davor eine Variable für jedes Argument. Kann einfach X1,X2,... sein. 
+
      * the sorted function application is the conclusion or the sentence is 
        just the sorted constant 
+
+       Und als rechte Seite der Implikation bzw. wenn es keine Argumente gibt, 
+       wird die Funktion noch mit dem Ergebnistyp als Funktionsapplikation 
+       ausgegeben.
+
     examples:
      1. 
        DFG: forall([a(x1),list_A(x2)],list_A(cons(x1,x2))).
 
-       TPTP: fof(ax4,declaration0,(! [U,V] : ((a(U) &
+       TPTP: fof(declaration0,axiom,(! [U,V] : ((a(U) &
                           list_A(V)) => list_A(cons(U,V))))).
 
      2.
       DFG: list_A(nil).
-      TPTP: fof(ax7,declaration1,(list_A(nil))).
+      TPTP: fof(declaration1,axiom,(list_A(nil))).
+
    - output subsorting information as implication
        sortSymA => sortSymB
+
+   Anders beschrieben:
+     Nimm die declarationList aus dem LogicalPart und erzeuge eine Formel 
+     aus jeder SPTermDecl und aus jeder SPSubsortDecl nach den beiden 
+     Schemata, die oben im Englischen Teil der Aufgabenbeschreibung als 
+     Beispiele angegeben sind:
+     - Also eine Implikation mit Typinformationen für die Typen jeder 
+       Funktion gegeben, die schon fast die fertige Formel ist: Argumente 
+       (termDeclTermList) ohne Typen als Variablen Deklaration. 
+       (Wiederverwenden Deiner Funktion, die die Deklaration von Variablen 
+       zu einem Quantifier in eine Implikation umsetzt.) Das Ergebnis 
+       (termDeclTerm) wird einfach so als rechte Seite der Implikation 
+       genutzt.
+     - Eine Implikation zu jeder SPSubsortDecl:
+       ! [X]  : sortSymA (X) => sortSymB(X)
+ 
 -}
 module SPASS.PrintTPTP where
 
