e1d881664025211ab6f2a795ee40c74a0abc5507
Rainer Grabbe <rainer@informatik.uni-bremen.de>
2006-05-29 10:44:14 +0000

    Proof status correctly mapped. Prover output is parsed, timeout check available. Used axioms are only listed when SPASS was used. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6392 cec4b9c1-7d33-0410-9eda-942365e851bb

--- SPASS/ProveMathServ.hs
+++ SPASS/ProveMathServ.hs
@@ -15,8 +15,6 @@ See <http://spass.mpi-sb.mpg.de/> for details on SPASS.
 
 {-
     To do:
-      - XML parser for MathServOutput
-      
       - update to do list and module description
       
       - check if the theorem is used in the proof;
@@ -46,7 +44,9 @@ import Network.URI
 import Network.Service
 import Org.Xmlsoap.Schemas.Soap.Envelope as ENV
 
--- import Text.XML.HXT.XPath.XPathParser
+import Text.XML.HXT.Aliases
+import Text.XML.HXT.Parser hiding (when)
+import Text.XML.HXT.XPath
 import Text.Regex
 
 import Data.List
@@ -74,7 +74,6 @@ mathServBroker =
 
 data SPASSProverState = SPASSProverState
     { initialLogicalPart :: SPLogicalPart}
-
 
 -- ** SPASS specific functions for prover GUI
 
@@ -140,8 +139,6 @@ mathServBrokerGUI thName th =
 
 -- * MathServ Interfacing Code
 
--- testwise connection to MathServ
-
 makeEndPoint :: String -> Maybe HTTPTransport
 makeEndPoint uriStr = maybe Nothing 
                             (\ uri -> Just $ HTTPTransport 
@@ -149,6 +146,10 @@ makeEndPoint uriStr = maybe Nothing
                                       , httpSOAPAction = Just nullURI})
                             (parseURI uriStr)
 
+{- |
+  Sends a problem in TPTP format to MathServ using a given time limit.
+  Either MathServ output is returned or a simple error message (no XML).
+-}
 callMathServ :: String -- ^ Problem to prove in TPTP format
              -> Int -- ^ Time limit
              -> IO String -- ^ MathServ output or error message
@@ -161,8 +162,8 @@ callMathServ problem timeout =
                      <- soapCall endPoint $
                         mkProveProblem Nothing service operation problem timeout
                  case res of
-                  Left err -> do
-                    return $ show err
+                  Left mErr -> do
+                    return $ show mErr
                   Right resp -> do
                     return $ getResponse resp
              )
@@ -175,36 +176,26 @@ callMathServ problem timeout =
         service = "Broker"
         operation = "ProveProblemOpt"
 
-
-
 {- |
-  Reads and parses the output of SPASS or any other prover. If SPASS was used,
-  more information about goal status and used axioms is available and will be parsed.
+  Verifies if the used prover was SPASS. This is done by parsing the prover output.
 -}
-parseProverOutput :: [String] -- ^ the prover output (maybe SPASS)
-                  -> (Maybe GoalStatus, [String]) -- ^ previous goal status reported by MathServ, used axioms (initial empty)
-                  -> (Maybe GoalStatus, [String]) -- ^ (result goal status, used axioms)
-parseProverOutput [] result = result
-parseProverOutput (line:ls) (res, usedAxs) =
-    let spassMatch = matchRegex re_spass line
-    in
-       -- filter more information with parseSpassOutput if SPASS was used
-      if isJust spassMatch then parseSpassOutput ls (res, usedAxs)
-                           else (res, usedAxs)
+isSPASSOutput :: [String] -- ^ the prover output (maybe SPASS)
+              -> Bool
+isSPASSOutput out =
+    isJust $ matchRegex re_spass $ unlines out
     where
       re_spass = mkRegex "SPASS V.*$"
-  
 
 {- |
   Reads and parses the output of SPASS. The goal status will be updated (if possible),
   used axioms will be filtered and added.
 -}
-parseSpassOutput :: [String] -- ^ SPASS output, beginning with result line
+parseSPASSOutput :: [String] -- ^ SPASS output, beginning with result line
                  -> (Maybe GoalStatus, [String])
                  -> (Maybe GoalStatus, [String]) -- ^ (current goal status, currently used axioms)
-parseSpassOutput [] result = result
-parseSpassOutput (line:ls) (res, usedAxs) =
-    if null ls then (res', usedAxs') else parseSpassOutput ls (res', usedAxs')
+parseSPASSOutput [] result = result
+parseSPASSOutput (line:ls) (res, usedAxs) =
+    if null ls then (res', usedAxs') else parseSPASSOutput ls (res', usedAxs')
 
     where
       resultMatch = matchRegex re_sb line
@@ -238,49 +229,68 @@ runMSBroker sps cfg saveDFG thName nGoal = do
     let lp = initialLogicalPart sps
     prob <- genSPASSProblem thName lp (Just nGoal)
     when saveDFG
-         (writeFile (thName++'_':AS_Anno.senName nGoal++".dfg")
-                    (showPretty prob ""))
+        (writeFile (thName++'_':AS_Anno.senName nGoal++".dfg")
+                   (showPretty prob ""))
+
     mathServOut <- callMathServ (show $ printTPTP prob) tLimit
-   
-    -- put XML parser in here 
-    
-    -- some fast results using RegExp
-    let res = parseMSOutput mathServOut
+    mtrees <- parseXML mathServOut
 
-    let timeout = False
-    let output = []
-
-      -- replace tabulators occuring in output with each 8 spaces
-    let output' = lines $ foldr (\ch li ->
-                                    if ch == '\x9' then "        "++li
-                                                   else ch:li)
-                                "" $ unlines output
+    let rdfTree = maybe emptyRoot head mtrees
+    let res = mapToGoalStatus $ getXTextValue $ getXPath resultXPath rdfTree
+    let output = maybe (lines mathServOut) (lines . unTab) $
+                       getXTextValue $ getXPath outputXPath rdfTree
+    let timeout = isJust $ matchRegex re_timeout $ unlines output
 
     -- get some more infos if SPASS was used
-    let (res', usedAxs) = parseProverOutput output (res, []) 
-    let (err, retval) = proof_stat nGoal res' usedAxs timeout defaultPrStat
-    return (err,
+    let (res', usedAxs) = if isSPASSOutput output
+                             then parseSPASSOutput output (res, []) 
+                             -- the goal itself was used as an axiom
+                             else (res, [AS_Anno.senName nGoal])
+    let (atpErr, retval) = proof_stat nGoal res' usedAxs timeout defaultPrStat
+    return (atpErr,
             cfg{proof_status = retval,
-                resultOutput = output'})
+                resultOutput = output})
     where
       tLimit = maybe (guiDefaultTimeLimit) id $ timeLimit cfg
       defaultPrStat = defaultProof_status nGoal tLimit
+      -- replace tabulators with each 8 spaces
+      unTab = foldr (\ch li ->
+                        if ch == '\x9' then "        "++li
+                                       else ch:li) ""
+      outputXPath = "/mw:*[local-name()='FoAtpResult']/mw:*[local-name()='output']/text()"
+      resultXPath = "/mw:*[local-name()='FoAtpResult']/mw:*[local-"
+                     ++ "name()='status']/attribute::rdf:*/text()"
+      re_timeout = mkRegex "Terminated by signal."
       
-
-parseMSOutput :: String  -- ^ MathServ Output
-              -> Maybe GoalStatus -- ^ final parsed goal status
-parseMSOutput out =
-    let matchTheorem = matchRegex re_theorem out
-        matchCounter = matchRegex re_counter out
-    in if isJust matchTheorem then Just $ Proved Nothing
-          else if isJust matchCounter then Just Disproved
-            else Just Open
+      
+{- |
+  Maps the status message from MathServ results to GoalStatus.
+  RegExp are used.
+-}
+mapToGoalStatus :: Maybe String -- ^ MathServ output
+                -> Maybe GoalStatus -- ^ final parsed goal status
+mapToGoalStatus stat = case stat of
+    Nothing -> Nothing
+    Just st -> if isJust $ matchRegex re_theorem st then Just $ Proved Nothing
+                 else if isJust $ matchRegex re_counter st then Just Disproved
+                   else Just Open
     where
-      re_theorem = mkRegex "status.owl#Theorem"
-      re_counter = mkRegex "status.owl#Counter"
+      re_theorem = mkRegex "Theorem$"
+      re_counter = mkRegex "Counter$"
+    
+{- |
+  Helper function. Given a one-elemented [XmlTree], containing an XText element
+  in first node, the function returns value of this XText element, if existing.
+-}
+getXTextValue :: XmlTrees -- ^ XmlTrees to parse
+              -> Maybe String -- ^ value of XText element
+getXTextValue xmltrees = case xmltrees of
+    [] -> Nothing
+    xt -> let firstNode = getNode $ head xt
+          in  if isXTextNode firstNode
+                 then (\(XText s) -> Just s) firstNode
+                 else Nothing
 
-
--- !! check the states, is defaultProof_status sufficient?
 {- |
   Default proof status. Contains the goal name, prover name
   and the time limit option used by MathServ.
@@ -306,6 +316,8 @@ proof_stat :: AS_Anno.Named SPTerm -- ^ goal to prove
            -> (ATPRetval, Proof_status ()) -- ^ General return value of a prover run, used in GUI.
                                            --   Detailed proof status if information is available.
 proof_stat nGoal res usedAxs timeOut defaultPrStat
+  | isNothing res =
+      (ATPError "Internal error.", defaultPrStat)
   | (fromJust res == Proved Nothing) =
       (ATPSuccess, defaultPrStat
        { goalStatus = Proved $ if elem (AS_Anno.senName nGoal) usedAxs
@@ -317,6 +329,4 @@ proof_stat nGoal res usedAxs timeOut defaultPrStat
   | isJust res && timeOut =
       (ATPTLimitExceeded,
        defaultPrStat { goalStatus = fromJust res })
-  | isNothing res =
-      (ATPError "Internal error.", defaultPrStat)
   | otherwise = (ATPSuccess, defaultPrStat)
