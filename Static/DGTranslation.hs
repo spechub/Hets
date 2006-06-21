2ea77782eb5f7cca10c498be27af79ba98974e92
Heng Jiang <jiang@tzi.de>
2006-06-21 17:30:41 +0000

    second try git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6473 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Static/DGTranslation.hs
+++ Static/DGTranslation.hs
@@ -18,177 +18,66 @@ import Logic.Prover
 import Static.DevGraph
 import qualified Common.Lib.Map as Map
 import Common.Result
+import Maybe
 
-dg_translation :: {-(Comorphism cid lid1 sublogics1 basic_spec1 sentence1 
-                symb_items1 symb_map_items1 sign1 morphism1 symbol1 
-                raw_symbol1 proof_tree1 lid2 sublogics2 basic_spec2 
-                sentence2 symb_items2 symb_map_items2 sign2 morphism2 
-                symbol2 raw_symbol2 proof_tree2Comorphism) => -}
-               GlobalContext -> AnyComorphism -> GlobalContext
-dg_translation gc cm@(Comorphism cid) =
-    let slid = sourceLogic cidMor
-        tlid = targetLogic cidMor
-        sublogic = sublogicOfTh thForProof
-                   if lessSublogicComor subLogic cm
-    in  gc { sigMap = transSig cid (sigMap gc) 
-                  , thMap  = transTh  cid (thMap gc)
-                  , morMap = transMor cid (morMap gc)
-                  }
-{-
-    in  gc' { morMap = transMor2 cidMor (morMap gc')
-            , thMap = transTh2 cidMor (thMap gc')
-            , sigMap = transSign2 cidMor (sigMap gc')
-            }
 
--}
-transSig :: (Logic lid sublogics basic_spec sentence symb_items
-                 symb_map_items sign1 morphism symbol
-                 raw_symbol proof_tree) => 
-            cid -> Map.Map Int G_sign -> Map.Map Int (G_sign,sens)
-transSig slid gSignMap1 =
-    transSig' (Map.keys gSignMap1) gSignMap1  Map.empty
-  where
-    transSig' [] _ resMap = resMap
-    transSig' (h:r) gSignMap newMap =
-        case Map.lookup h gSignMap of
-          Just v ->
-              case fSign slid v of 
-                Just cValue -> 
-                    transSig' r gSignMap (Map.insert h cValue newMap)
-                Nothing -> transSig' r gSignMap newMap
-          Nothing -> transSig' r gSignMap newMap
-    
-    fSign :: (Logic lid1 sublogics basic_spec sentence symb_items
-                 symb_map_items sign1 morphism symbol
-                 raw_symbol proof_tree) => 
-         lid1 -> G_sign -> Maybe G_sign
-    fSign slid' (G_sign lid sign) =
-      case coerceSign lid slid' "" sign of
-        Just sign' -> Just (G_sign slid' sign')
+dg_translation :: GlobalContext -> AnyComorphism -> GlobalContext
+dg_translation gc cm@(Comorphism cidMor) =
+    mainTrans (Map.keys $ thMap gc) gc
+  where 
+        mainTrans [] gc = gc
+        mainTrans (h:r) gc =  
+            case Map.lookup h (thMap gc) of
+              Just value -> if lessSublogicComor (sublogicOfTh value) cm
+                               then mainTrans r $ updateNode h gc cm
+                               else mainTrans r gc
+
+updateNode :: Int -> GlobalContext -> AnyComorphism -> GlobalContext
+updateNode index gc cm@(Comorphism cidMor) =
+    gc { sigMap = Map.update 
+                  transSig 
+                  index (sigMap gc)
+       , thMap = Map.update
+                 transTh 
+                 index (thMap gc)
+       , morMap = Map.update
+                  transMor 
+                  index (morMap gc)
+       }
+
+   where 
+     transSig sign = fSign (sourceLogic cidMor) sign
+     fSign slid (G_sign lid sign) =
+      case coerceSign lid slid "" sign of
+        Just sign' -> Just (G_sign slid sign')
         Nothing    -> Nothing
-        
-transTh :: (Logic lid sublogics basic_spec sentence symb_items
-                 symb_map_items sign1 morphism symbol
-                 raw_symbol proof_tree) =>
-           lid -> Map.Map Int G_theory -> Map.Map Int G_theory
-transTh slid gThMap1 =
-    transTh' (Map.keys gThMap1) gThMap1 Map.empty
-  where
-    transTh' [] _ resMap = resMap
-    transTh' (h:r) gThMap newMap =
-        case Map.lookup h gThMap of
-          Just v -> 
-              case fTh slid v of
-                Just cValue -> 
-                    transTh' r gThMap (Map.insert h cValue newMap) 
-                Nothing     -> transTh' r gThMap newMap
-          Nothing -> transTh' r gThMap newMap
-    
-    fTh :: (Logic lid1 sublogics basic_spec sentence symb_items
-                 symb_map_items sign1 morphism symbol
-                 raw_symbol proof_tree) =>
-         lid1 -> G_theory -> Maybe G_theory
-    fTh slid' (G_theory lid sign thSens) =
-      case coerceSign lid slid' "" sign of
+
+     transTh th = fTh (sourceLogic cidMor) th
+     fTh slid (G_theory lid sign thSens) =
+      case coerceSign lid slid "" sign of
         Just sign' -> 
-            case coerceThSens lid slid' "" thSens of
-              Just thSens' -> Just (G_theory slid' sign' thSens')
+            case coerceThSens lid slid "" thSens of
+              Just thSens' -> Just (G_theory slid sign' thSens')
               Nothing    -> Nothing
         Nothing -> Nothing
-                
 
-transMor :: (Logic lid sublogics basic_spec sentence symb_items
-                 symb_map_items sign1 morphism symbol
-                 raw_symbol proof_tree) =>
-            lid -> Map.Map Int G_morphism -> Map.Map Int G_morphism
-transMor slid gMorMap1 =
-    transMor' (Map.keys gMorMap1) gMorMap1 Map.empty
-    where  
-      transMor' [] _ resMap = resMap
-      transMor' (h:r) gMorMap newMap =
-          case Map.lookup h gMorMap of
-              Just v -> 
-                  case fMor slid v of 
-                    Just cValue ->
-                        transMor' r gMorMap (Map.insert h cValue newMap)
-                    Nothing -> transMor' r gMorMap newMap
-              Nothing   -> transMor' r gMorMap newMap
+     transMor mor = fMor (sourceLogic cidMor) mor
+     fMor slid (G_morphism lid mor) =
+        case coerceMorphism lid slid "" mor of
+          Just mor' -> 
+              case map_morphism cidMor mor' of
+                Result diags maybeResult ->
+                    maybeResult
+          Nothing   -> Nothing    
 
-      fMor :: (Logic lid1 sublogics basic_spec sentence symb_items
-                 symb_map_items sign1 morphism symbol
-                 raw_symbol proof_tree) =>
-           lid1 -> G_morphism -> Maybe G_morphism
-      fMor slid' (G_morphism lid mor) =
-        case coerceMorphism lid slid' "" mor of
-          Just mor' -> Just (G_morphism slid' mor')
-          Nothing   -> Nothing
-        
 {-
-transMor2 :: (Comorphism cid
-                         lid1
-                         sublogics1
-                         basic_spec1
-                         sentence1
-                         symb_items1
-                         symb_map_items1
-                         sign1
-                         G_morphism
-                         symbol1
-                         raw_symbol1
-                         proof_tree1
-                         lid2
-                         sublogics2
-                         basic_spec2
-                         sentence2
-                         symb_items2
-                         symb_map_items2
-                         sign2
-                         G_morphism
-                         symbol2
-                         raw_symbol2
-                         proof_tree2)
-             => cid -> Map.Map Int G_morphism -> Map.Map Int G_morphism
-transMor2 cmid gMMap =
-    transMor2' (Map.keys gMMap) gMMap Map.empty
-    where 
-      transMor2' [] _ resGm = resGm
-      transMor2' (h:r) gm newMap =
-          case Map.lookup h gm of
-            Just m -> case map_morphism cmid m of
-                        Result diags maybeResult -> 
-                            -- show diags
-                            case maybeResult of
-                              Just m' -> 
-                                 transMor2' r gm (Map.insert h m' newMap)
-                              Nothing -> transMor2' r gm newMap
-            Nothing -> transMor2' r gm newMap
+transMor2 :: (Comorphism cid lid1 sublogics1 basic_spec1 sentence1 symb_items1 symb_map_items sign1 morphism symbol1 raw_symbol1 proof_tree1 lid2 sublogics2 basic_spec2 sentence2 symb_items2 symb_map_items2 sign2 morphism symbol2 raw_symbol2 proof_tree2) => cid -> morphism1 -> Maybe morphism2
+transMor2 cmid mor =
+    case map_morphism cmid mor of
+      Result diags maybeResult -> maybeResult
 
-transTh2 :: (Comorphism cid
-                        lid1
-                        sublogics1
-                        basic_spec1
-                        sentence
-                        symb_items1
-                        symb_map_items1
-                        sign
-                        morphism1
-                        symbol1
-                        raw_symbol1
-                        proof_tree1
-                        lid2
-                        sublogics2
-                        basic_spec2
-                        sentence
-                        symb_items2
-                        symb_map_items2
-                        sign
-                        morphism2
-                        symbol2
-                        raw_symbol2
-                        proof_tree2)
-            => 
-             cid -> Map.Map Int G_theory -> Map.Map Int G_theory
-transTh2 cmid gtMap =
+transTh2 :: AnyComorphism  -> Map.Map Int G_theory -> Map.Map Int G_theory
+transTh2 (Comorphism cmid) gtMap =
     transTh2' (Map.keys gtMap) gtMap Map.empty
     where 
       transTh2' [] _ resGm = resGm
@@ -205,32 +94,8 @@ transTh2 cmid gtMap =
                         Nothing -> transTh2' r gm newMap
             Nothing -> transTh2' r gm newMap
 
-transSign2 :: (Comorphism cid
-                          lid1
-                          sublogics1
-                          basic_spec1
-                          sentence1
-                          symb_items1
-                          symb_map_items1
-                          sign
-                          morphism1
-                          symbol1
-                          raw_symbol1
-                          proof_tree1
-                          lid2
-                          sublogics2
-                          basic_spec2
-                          sentence2
-                          symb_items2
-                          symb_map_items2
-                          sign
-                          morphism2
-                          symbol2
-                          raw_symbol2
-                          proof_tree2)
-              => 
-              cid -> Map.Map Int G_sign -> Map.Map Int G_sign
-transSign2 cmid gsMap =
+transSign2 :: AnyComorphism -> Map.Map Int G_sign -> Map.Map Int G_sign
+transSign2 (Comorphism cmid) gsMap =
     transSign2' (Map.keys gsMap) gsMap Map.empty
     where
       transSign2' [] _ resGm = resGm
@@ -246,5 +111,4 @@ transSign2 cmid gsMap =
                                               (G_sign lib sign') newMap)
                         Nothing -> transSign2' r gm newMap
             Nothing -> transSign2' r gm newMap
-
 -}
