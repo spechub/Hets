95834d47204eec8e8a076639b805928ecb8ce46e
Christian Maeder <Christian.Maeder@dfki.de>
2006-06-22 15:23:03 +0000

    make tests go through git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6483 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Common/RunParsers.hs
+++ Common/RunParsers.hs
@@ -1,6 +1,4 @@
-
 {- |
-
 Module      :  $Header$
 Copyright   :  (c) Christian Maeder and Uni Bremen 2002-2003
 License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
@@ -10,18 +8,18 @@ Stability   :  provisional
 Portability :  portable
 
 test some parsers (and printers)
-
 -}
 
-module Common.RunParsers (exec, StringParser, toStringParser, fromAParser) 
-    where 
+module Common.RunParsers (exec, StringParser, toStringParser, fromAParser)
+    where
 
 import Common.Lexer((<<), parseString)
 import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Pos
 import Common.Anno_Parser
 import Common.AnnoState
-import Common.PrettyPrint
+import Common.Doc
+import Common.DocUtils
 import Common.GlobalAnnotations
 import Common.AnalyseAnnos(addGlobalAnnos)
 import Common.Result
@@ -29,28 +27,28 @@ import System.Environment
 
 type StringParser = GlobalAnnos -> AParser () String
 
-fromAParser :: (PrettyPrint a) => AParser () a -> StringParser
-fromAParser p ga = fmap (show . printText0 ga) p
+fromAParser :: Pretty a => AParser () a -> StringParser
+fromAParser p ga = fmap (\ a -> showGlobalDoc ga a "") p
 
-toStringParser :: (PrettyPrint a) => (GlobalAnnos -> AParser () a) 
+toStringParser :: Pretty a => (GlobalAnnos -> AParser () a)
                -> StringParser
-toStringParser p ga = fmap (show . printText0 ga) $ p ga
+toStringParser p ga = fmap (\ a -> showGlobalDoc ga a "") $ p ga
 
 exec :: [(String, StringParser)] -> [(String, StringParser)] -> IO ()
 exec lps fps = do l <- getArgs
-                  if null l then 
+                  if null l then
                      parseSpec emptyGlobalAnnos $ snd  $ head $ fps
                      else do let opt = head l
                                  lps' = filter (\(s, _) -> s == opt) lps
                                  fps' = filter (\(s, _) -> s == opt) fps
-                             ga <- if not $ null $ tail l then 
+                             ga <- if not $ null $ tail l then
                                    do let annoFile = head (tail l)
                                       str <- readFile annoFile
-                                      maybe (error "run parser") 
-                                             return $ maybeResult 
-                                             $ addGlobalAnnos emptyGlobalAnnos 
+                                      maybe (error "run parser")
+                                             return $ maybeResult
+                                             $ addGlobalAnnos emptyGlobalAnnos
                                              $ parseString annotations str
-                                      -- should not fail 
+                                      -- should not fail
                                       -- but may return empty annos
                                    else return emptyGlobalAnnos
                              if null lps' && null fps' then
@@ -60,13 +58,13 @@ exec lps fps = do l <- getArgs
                                       " [OPTIONS] <Annotations> < infile")
                                    putStrLn "where OPTIONS is one of:"
                                    putStrLn $ unwords
-                                              (map fst lps ++ map fst fps) 
+                                              (map fst lps ++ map fst fps)
                                 else if null lps'
                                      then parseSpec ga $ snd $ head fps'
                                      else checkLines ga $ snd $ head lps'
 
 checkLines :: GlobalAnnos -> StringParser -> IO ()
-checkLines ga p = 
+checkLines ga p =
     do s <- getContents
        putStr (unlines (scanLines ga p (lines s) 1))
 
@@ -75,7 +73,7 @@ scanLines _ _ [] _ = []
 scanLines ga p (x:l) n = (parseLine ga p x n) : (scanLines ga p l (n+1))
 
 parseLine :: GlobalAnnos -> StringParser -> String -> Line -> String
-parseLine ga p line n = 
+parseLine ga p line n =
     let pos = setSourceLine (initialPos "") n
         parser = do setPosition pos
                     i <- p ga
@@ -85,10 +83,10 @@ parseLine ga p line n =
 
 parseSpec :: GlobalAnnos -> StringParser -> IO ()
 parseSpec ga p = do str <- getContents
-                    putStrLn $ showParse $ 
+                    putStrLn $ showParse $
                              runParser (p ga << eof) (emptyAnnos ()) "" str
 
 showParse :: Either ParseError String -> String
-showParse e = case e of 
+showParse e = case e of
                      Left err -> "parse error at " ++ showErr err ++ "\n"
                      Right x -> x
