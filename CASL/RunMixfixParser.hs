85eb04d6a37a86571432210135d3d9f3185e3e4a
Christian Maeder <Christian.Maeder@dfki.de>
2006-06-22 15:28:12 +0000

    make tests go through git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6484 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CASL/RunMixfixParser.hs
+++ CASL/RunMixfixParser.hs
@@ -1,11 +1,11 @@
 {- |
 Module      :  $Header$
-Copyright   :  (c) Klaus Lüttich, Christian Maeder and Uni Bremen 2002-2003 
+Copyright   :  (c) Klaus Lüttich, Christian Maeder and Uni Bremen 2002-2003
 License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
 
 Maintainer  :  maeder@tzi.de
 Stability   :  experimental
-Portability :  portable 
+Portability :  portable
 
 make mixfix analysis checkable by RunParsers
 
@@ -21,8 +21,8 @@ import qualified Common.Lib.Set as Set
 import Common.Id
 import Common.Result
 import Common.Lexer
-import Common.PPUtils
-import Common.PrettyPrint
+import Common.Doc
+import Common.DocUtils
 
 import Common.Token
 import CASL.Formula
@@ -31,17 +31,17 @@ import CASL.ShowMixfix
 -- start testing
 stdOpsL, stdPredsL :: [String]
 
-stdOpsL = ["__^__", "__*__", "__+__", "[__]","__div__","__mod__", "__rem__", 
+stdOpsL = ["__^__", "__*__", "__+__", "[__]","__div__","__mod__", "__rem__",
         "__-__", "+__", "__E__", "__@@__", "[]", "__::__", "__:::__",
-        "-__", "__!"] ++ 
+        "-__", "__!"] ++
           [ "____p", "q____","____x____", "{____}",
-          "repeat__until__", "while__do__od", 
+          "repeat__until__", "while__do__od",
             "__where__but__", "__where__done",
            "__ --> __", "__{__}--__-->{__}__"]
-         ++ ["A[a[c,d],b]", "B[a[c,d],b]", "__B[a[c,d],b]__", 
-             "a[c,d]", "__a[c,d]__", "A[a]", "A__B", 
-             "A__", "__[a]", "__p", 
-             "__[__]__", "[__]__", "__[__]"] 
+         ++ ["A[a[c,d],b]", "B[a[c,d],b]", "__B[a[c,d],b]__",
+             "a[c,d]", "__a[c,d]__", "A[a]", "A__B",
+             "A__", "__[a]", "__p",
+             "__[__]__", "[__]__", "__[__]"]
 
 stdPredsL = ["__<__", "__<=__", "__>__", "__>=__", "__!=__", "__<>__",
              "__/=__", "even__", "odd__", "__isEmpty",
@@ -52,39 +52,47 @@ mkIds = Set.fromList . map (parseString $ parseId [])
 
 stdOps, stdPreds :: Set.Set Id
 stdOps = mkIds stdOpsL
-stdPreds = mkIds stdPredsL 
+stdPreds = mkIds stdPredsL
 
 myIdSets :: IdSets
 myIdSets = mkIdSets (mkIds stdOpsL) $ mkIds stdPredsL
 
 resolveForm :: GlobalAnnos -> AParser () (Result (FORMULA ()))
-resolveForm ga = 
-      resolveFormula id (const $ const return) ga (makeRules ga myIdSets) 
+resolveForm ga =
+      resolveFormula id (const $ const return) ga (makeRules ga myIdSets)
                          `fmap` formula []
 
 resolveTerm :: GlobalAnnos -> AParser () (Result (TERM ()))
-resolveTerm ga = 
+resolveTerm ga =
       resolveMixfix id (const $ const return) ga (makeRules ga myIdSets)
                         `fmap` term []
 
+-- | a helper type to pretty print (wrapped) strings
+data WrapString = WrapString String
+instance Show WrapString where
+    showsPrec _ (WrapString s) _ = s
+
+instance Pretty WrapString where
+    pretty (WrapString s) = text s
+
 testTerm ::  AParser () WrapString
 testTerm = do t <- term [] :: AParser () (TERM ())
-              return $ WrapString $ showPretty (mapTerm id t) ""
+              return $ WrapString $ showDoc (mapTerm id t) ""
 
 testTermMix :: GlobalAnnos -> AParser () WrapString
 testTermMix ga = do Result ds mt <- resolveTerm ga
-                    return $ WrapString $ 
-                        case mt of 
-                        Just t -> show $ printText0 ga (mapTerm id t)
+                    return $ WrapString $
+                        case mt of
+                        Just t -> showGlobalDoc ga (mapTerm id t) ""
                         _ -> show ds
 
 testFormula :: AParser () WrapString
 testFormula = do f <- formula [] :: AParser () (FORMULA ())
-                 return $ WrapString $ showPretty (mapFormula id  f) ""
+                 return $ WrapString $ showDoc (mapFormula id  f) ""
 
 testFormulaMix :: GlobalAnnos -> AParser () WrapString
 testFormulaMix ga = do Result ds m <- resolveForm ga
-                       return $ WrapString $ 
-                           case m of 
-                           Just f -> show $ printText0 ga (mapFormula id f)
+                       return $ WrapString $
+                           case m of
+                           Just f -> showGlobalDoc ga (mapFormula id f) ""
                            _ -> show ds
