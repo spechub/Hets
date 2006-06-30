93f8523526f2caa11b0738a83b20a92cbdb82899
Christian Maeder <Christian.Maeder@dfki.de>
2006-06-30 09:16:51 +0000

    removed unused files git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6548 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Syntax/ADoc.hs
+++ Syntax/ADoc.hs
@@ -1,39 +1,35 @@
-{- | The folder Syntax contains abstract syntax, parsing and printing
+{- |
+Module      :  $Header$
+Description :  folder description
+Copyright   :  (c) Christian Maeder and Uni Bremen 2006
+License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
+
+Maintainer  :  maeder@tzi.de
+Stability   :  provisional
+Portability :  portable
+
+The folder Syntax contains abstract syntax, parsing and printing
 for heterogeneous structured and architectural specifications and
-specification libraries.  Parsing is based on 
+specification libraries.  Parsing is based on
 <http://www.cs.uu.nl/people/daan/parsec.html>,
-pretty printing on "Common.Lib.Pretty".
-Some modules deal with global annotations (see "Common.AS_Annotation").
+pretty printing on "Common.Doc".
 
 /Abstract syntax/
 
-"Syntax.AS_Structured"  
-"Syntax.AS_Architecture"        
-"Syntax.AS_Library"     
-
-/Global annotations/
-
-"Syntax.GlobalLibraryAnnotations"       
-"Syntax.Print_HetCASL"  
+"Syntax.AS_Structured"
+"Syntax.AS_Architecture"
+"Syntax.AS_Library"
 
 /Parsing/
 
-"Syntax.Parse_AS_Structured"    
-"Syntax.Parse_AS_Architecture"  
-"Syntax.Parse_AS_Library"       
+"Syntax.Parse_AS_Structured"
+"Syntax.Parse_AS_Architecture"
+"Syntax.Parse_AS_Library"
 
 /Pretty printing/
-        
+
 "Syntax.Print_AS_Structured"
-"Syntax.Print_AS_Architecture"  
+"Syntax.Print_AS_Architecture"
 "Syntax.Print_AS_Library"
-
-/LaTeX pretty printing/
-
-"Syntax.LaTeX_AS_Structured"
-"Syntax.LaTeX_AS_Architecture"  
-"Syntax.LaTeX_AS_Library"       
-
-
 -}
 module Syntax.ADoc where
