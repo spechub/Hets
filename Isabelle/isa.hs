a6a495df32cfb03dada6f298b9777dece008ffac
Christian Maeder <Christian.Maeder@dfki.de>
2006-07-04 15:28:53 +0000

    print nicer body git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6579 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Isabelle/isa.hs
+++ Isabelle/isa.hs
@@ -15,6 +15,10 @@ module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec
 import Isabelle.IsaParse
+import Isabelle.IsaConsts
+import qualified Common.Lib.Map as Map
+import Common.Doc as Doc
+import Common.DocUtils
 
 main :: IO ()
 main = getArgs >>= mapM_ process
@@ -23,5 +27,30 @@ process :: String -> IO ()
 process f = do
   s <- readFile f
   putStrLn $ case parse parseTheory f s of
-             Right (_, b) -> show b
+             Right (_, b) -> show $ printBody b
              Left err -> show err
+
+printBody :: Body -> Doc
+printBody f = let
+    axs = axiomsF f
+    gls = goalsF f
+    cns = constsF f
+    dts = datatypesF f
+    col a b = a <+> colon <+> b
+    dcol a b = a <+> text "::" <+> b
+    in (if Map.null axs then empty else keyword axiomsS)
+       $+$ 
+       printMap id vcat col axs
+       $+$
+       (if Map.null gls then empty else keyword lemmaS)
+       $+$
+       ppMap pretty (sep . prepPunctuate (text andS <> Doc.space) . map pretty)
+             id vcat col gls
+       $+$
+       (if Map.null cns then empty else keyword constsS)
+       $+$
+       printMap id vcat dcol cns
+       $+$
+       (if Map.null dts then empty else keyword datatypeS)
+       $+$
+       printMap id vcat dcol dts
