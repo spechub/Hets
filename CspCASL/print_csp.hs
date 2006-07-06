de6cc8c6eda51b77fc1259a9d9652c42266ed275
Christian Maeder <Christian.Maeder@dfki.de>
2006-07-06 15:15:37 +0000

    made it go through git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6621 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CspCASL/print_csp.hs
+++ CspCASL/print_csp.hs
@@ -1,38 +1,34 @@
-{- **********************************************************************
+{- |
+Module      :  $Header$
+Copyright   :  (c) Daniel Pratsch and Uni Bremen 2002-2006
+License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
 
-   $Source$
+Maintainer  :  maeder@tzi.de
+Stability   :  provisional
+Portability :  portable
 
-   $Date$
-   $Revision$
-   Author: Daniel Pratsch (Last modification by $Author$)
-
-  ************************************************************************** 
+testing file for CSP-CASL semantics
 -}
 
-{-------------------------------------------------------
- testing file for CSP-CASL semantics
---------------------------------------------------------}
 module Main where
 
 import CspCASL.Parse_AS_CSP_CASL
+import CspCASL.Print_AS_CSP_CASL ()
+
+import Common.DocUtils
+import Common.AnnoState
+
 import Text.ParserCombinators.Parsec
 import System.IO
 import System.Environment
-import Common.PrettyPrint
-import Common.Lib.Pretty
-import CspCASL.Print_AS_CSP_CASL
-import Common.GlobalAnnotations
 
-import Common.AnnoState
-
-run :: PrettyPrint a => AParser () a -> String -> IO ()
+run :: Pretty a => AParser () a -> String -> IO ()
 run p input
         = case (runParser p (emptyAnnos ()) "" input) of
             Left err -> do { putStr "parse error at "
                            ; print err
                            }
-            Right x -> putStrLn $ renderText Nothing $
-                       printText0 emptyGlobalAnnos x                        
+            Right x -> putStrLn $ showDoc x ""
 
 main :: IO ()
 main = do { l <- getArgs
