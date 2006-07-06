3724989bb692e0ae7eeed1f78914f41b429601b8
Christian Maeder <Christian.Maeder@dfki.de>
2006-07-06 15:41:06 +0000

    removed PrettyPrint and untabified git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6623 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CASL_DL/Print_AS.hs
+++ CASL_DL/Print_AS.hs
@@ -1,29 +1,25 @@
-{-
+{- |
 Module      :  $Header$
 Copyright   :  (c) Klaus Lüttich, Uni Bremen 2005
 Licence     :  similar to LGPL, see HetCATS/LICENCE.txt or LIZENZ.txt
 
-Maintainer  :  luettichtzi.de
+Maintainer  :  luettich@tzi.de
 Stability   :  provisional
 Portability :  portable
 
-pretty print AS of CASL_DL in ASCII
+pretty print AS of CASL_DL
 -}
 
 module CASL_DL.Print_AS where
 
 import Common.Doc
 import Common.DocUtils
-import Common.PrettyPrint
 
-import CASL.ToDoc()
+import CASL.ToDoc ()
 import CASL_DL.AS_CASL_DL
-			     
-instance PrettyPrint DL_FORMULA where
-    printText0 = toOldText
 
 instance Pretty DL_FORMULA where
-    pretty (Cardinality ct pn varTerm natTerm _) = 
+    pretty (Cardinality ct pn varTerm natTerm _) =
         text (case ct of
               CMin   -> minCardinalityS
               CMax   -> maxCardinalityS
