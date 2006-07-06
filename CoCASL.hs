92a8d7a035c4ee1054cd2ef112bb404f9b3b50d5
Christian Maeder <Christian.Maeder@dfki.de>
2006-07-06 12:32:19 +0000

    removed LaTeX file and cleaned up git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6611 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CoCASL.hs
+++ CoCASL.hs
@@ -1,4 +1,16 @@
-{- | CoCASL is the coalgebraic extension of CASL.  See /Till
+{- |
+Module      :  $Header$
+Description :  folder description
+Copyright   :  (c) Christian Maeder and Uni Bremen 2006
+License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
+
+Maintainer  :  maeder@tzi.de
+Stability   :  provisional
+Portability :  portable (except CoCASL.Logic_CoCASL)
+
+This folder contains the files for CoCASL basic specs
+
+CoCASL is the coalgebraic extension of CASL.  See /Till
 Mossakowski, Lutz Schröder, Markus Roggenbach, Horst
 Reichel. Algebraic-co-algebraic specification in CoCASL. Journal of
 Logic and Algebraic Programming. To appear./
@@ -7,26 +19,23 @@ The modules for CoCASL largely are built on top of those for "CASL",
 using the holes for future extensions that have been left in the
 datatypes for CASL.
 
-"CoCASL.AS_CoCASL" abstract syntax of CoCASL specifications
-
-"CoCASL.ATC_CoCASL" ATerm conversion
-
-"CoCASL.CoCASLSign" CoCASL signatures
-
-"CoCASL.LaTeX_CoCASL" CoCASL LaTeX pretty printing
-
-"CoCASL.Logic_CoCASL" the CoCASL instance of type class 'Logic.Logic.Logic'
-
-"CoCASL.Parse_AS" CoCASL parser
-
-"CoCASL.Print_AS" CoCASL pretty printer
-
-"CoCASL.StatAna" CoCASL static analysis
-
 Special proof tactics for the CoCASL encoding into Isabelle
 are delivered in the CASL-lib folder <http://www.cofi.info/Libraries/>
 of basic libraries.
 
--}
+* "CoCASL.AS_CoCASL"    abstract syntax of CoCASL specifications
 
+* "CoCASL.ATC_CoCASL"   ATerm conversion
+
+* "CoCASL.CoCASLSign"   CoCASL signatures
+
+* "CoCASL.Logic_CoCASL" the CoCASL instance of type class 'Logic.Logic.Logic'
+
+* "CoCASL.Parse_AS"     CoCASL parser
+
+* "CoCASL.Print_AS"     CoCASL pretty printer
+
+* "CoCASL.StatAna"      CoCASL static analysis
+
+-}
 module CoCASL where
--- Modal.hs
+++ Modal.hs
@@ -1,28 +1,38 @@
-{- | ModalCASL is the modal logic extension of CASL.  See 
-/Heterogeneous
-specification and the heterogeneous tool set/
+{- |
+Module      :  $Header$
+Description :  folder description
+Copyright   :  (c) Christian Maeder and Uni Bremen 2006
+License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
+
+Maintainer  :  maeder@tzi.de
+Stability   :  provisional
+Portability :  portable (except Modal.Logic_Modal)
+
+This folder contains the files for ModalCASL basic specs
+
+ModalCASL is the modal logic extension of CASL.  See
+/Heterogeneous specification and the heterogeneous tool set/
 (<http://www.tzi.de/~till/papers/habil.ps>), section 3.2.
 
 The modules for ModalCASL largely are built on top of those for "CASL",
 using the holes for future extensions that have been left in the
 datatypes for CASL.
 
-"Modal.AS_Modal" abstract syntax
-        
-"Modal.Parse_AS" parser
+* "Modal.AS_Modal"     abstract syntax
 
-"Modal.Print_AS" , "Modal.LaTeX_Modal"  pretty printing
+* "Modal.Parse_AS"     parser
 
-"Modal.ModalSign" signatures
+* "Modal.Print_AS"     pretty printing
 
-"Modal.StatAna" static analysis
+* "Modal.ModalSign"    signatures
 
-"Modal.ModalSystems" recognition of various systems such as S4, S5 etc.
+* "Modal.StatAna"      static analysis
 
-"Modal.ATC_Modal" ATerm conversion
+* "Modal.ModalSystems" recognition of various systems such as S4, S5 etc.
 
-"Modal.Logic_Modal" the ModalCASL instance of type class 'Logic.Logic.Logic'
+* "Modal.ATC_Modal"    ATerm conversion
+
+* "Modal.Logic_Modal"  the ModalCASL instance of type class 'Logic.Logic.Logic'
 
 -}
-
 module Modal where
