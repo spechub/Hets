1b6f750799a9dd7499734164b4d3f18734eb935a
Christian Maeder <Christian.Maeder@dfki.de>
2006-07-25 16:12:37 +0000

    marked non-portabel git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6713 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Comorphisms/CASL2SubCFOL.inline.hs
+++ Comorphisms/CASL2SubCFOL.inline.hs
@@ -5,7 +5,7 @@ License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
 
 Maintainer  :  maeder@tzi.de
 Stability   :  provisional
-Portability :  portable
+Portability :  non-portable (imports Logic.Comorphism)
 
 Coding out partiality (SubPCFOL= -> SubCFOL=),
 -}
