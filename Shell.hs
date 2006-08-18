e65ab991958d18e23195d2b9d2bd102c65381a11
Razvan Pascanu <r.pascanu@jacobs-university.de>
2006-08-18 09:17:02 +0000

    shellac git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6839 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ Shell.hs
@@ -0,0 +1,107 @@
+{-
+ -
+ -  Copyright 2005-2006, Robert Dockins.
+ -
+ -}
+
+{- | This module implements a framework for creating read-eval-print style
+     command shells.  Shells are created by declaratively defining evaluation
+     functions and \"shell commands\".  Input is read using a plugable backend.
+     The shell framework handles command history and word completion if the
+     backend supports it.
+
+     The basic idea is for creating a shell is:
+
+      (1) Create a list of shell commands and an evaluation function
+
+      (2) Create a shell description (using 'mkShellDescription')
+
+      (3) Set up the initial shell state
+
+      (4) Run the shell (using 'runShell')
+-}
+
+
+module Shell (
+
+-- * Shell Descriptions
+  ShellDescription (..)
+, initialShellDescription
+, mkShellDescription
+, defaultExceptionHandler
+
+-- * Executing Shells
+, runShell
+
+-- * Creating Shell Commands
+, exitCommand
+, useCommand
+, helpCommand
+, toggle
+, cmd
+, CommandFunction (..)
+, File (..)
+, Username (..)
+, Completable (..)
+, Completion (..)
+, ShellCommand
+
+-- * Subshells
+, Subshell
+, simpleSubshell
+
+-- * Printing Help Messages
+, showShellHelp
+, showCmdHelp
+
+-- * Generating text output
+, shellPutStr
+, shellPutStrLn
+, shellPutInfo
+, shellPutInfoLn
+, shellPutErr
+, shellPutErrLn
+
+-- * Auxiliary Types
+, CommandStyle (..)
+, ShellSpecial (..)
+, OutputCommand
+, CommandResult
+) where
+
+import Shell.Types
+import Shell.ShellMonad
+import Shell.Commands
+import Shell.RunShell
+
+
+-- | A basic shell description with sane initial values
+initialShellDescription :: ShellDescription String
+initialShellDescription =
+  let wbc = " \t\n\r\v`~!@#$%^&*()=[]{};\\\'\",<>" in
+    ShDesc
+       { shellCommands      = []
+       , commandStyle       = OnlyCommands
+       , evaluateFunc       = \_ -> return ()
+       , wordBreakChars     = wbc
+       , beforePrompt       = return ()
+       , prompt             = \_ -> return "> "
+       , exceptionHandler   = defaultExceptionHandler
+       , defaultCompletions = Just (\_ _ -> return [])
+       , historyFile        = Nothing
+       , maxHistoryEntries  = 100
+       , historyEnabled     = True
+       }
+
+
+-- | Creates a simple shell description from a list of shell commmands and
+--   an evalation function.
+mkShellDescription :: [ShellCommand String]
+                   -> (String -> Sh String ())
+                   -> ShellDescription String 
+
+mkShellDescription cmds func =
+   initialShellDescription
+      { shellCommands = cmds
+      , evaluateFunc  = func
+      }
---
+++ Shell/Backend/Readline.hs
@@ -0,0 +1,124 @@
+{-
+ - 
+ -  Copyright 2005-2006, Robert Dockins.
+ -  
+ -}
+
+{- | This module implements a Shellac backend based on the GNU readline
+     and GNU history modules.  For readline, we use the bindings
+     from the standard library.  For history, we import directily using
+     FFI.  This Shellac backend supports command completion, history buffer
+     and all the line editing and character binding features of GNU readline.
+
+     Beware that while the code for this Shellac binding is licensed under a BSD3
+     license, GNU readline itself is licensed under the GPL.  This means that your
+     project needs to be GPL compatible for this Shellac backend to be useful to you.
+-}
+
+
+module Shell.Backend.Readline
+( readlineBackend
+) where
+
+import System.IO            ( stdin, stdout, stderr, hFlush, hPutStr, hPutStrLn, hGetChar
+                            , hSetBuffering, hGetBuffering
+                            , BufferMode(..)
+                            )
+import Foreign.Ptr          ( Ptr )
+import Foreign.C            ( CInt, CString, withCString )
+import Foreign.C.Error      ( Errno, eOK, errnoToIOError )
+import Foreign.Storable     ( peek )
+
+import qualified Control.Exception as Ex
+import qualified System.Console.Readline as RL
+
+import Shell.Backend 
+
+readlineBackend :: ShellBackend String
+readlineBackend = ShBackend
+  { initBackend                      = return ""
+  , outputString                     = \x -> readlineOutput x
+  , flushOutput                      = \_ -> hFlush stdout
+  , getInput                         = \_ -> RL.readline
+  , getSingleChar                    = \_ -> readlineGetSingleChar
+  , addHistory                       = \_ -> RL.addHistory
+  , getWordBreakChars                = \_ -> RL.getBasicWordBreakCharacters
+  , setWordBreakChars                = \_ -> RL.setBasicWordBreakCharacters
+  , onCancel                         = \_ -> hPutStrLn stdout "canceled..."
+  , setAttemptedCompletionFunction   = \_ -> readlineCompletionFunction
+  , setDefaultCompletionFunction     = \_ -> RL.setCompletionEntryFunction
+  , completeFilename                 = \_ -> RL.filenameCompletionFunction
+  , completeUsername                 = \_ -> RL.filenameCompletionFunction
+  , clearHistoryState                = \_ -> doClearHistoryState
+  , setMaxHistoryEntries             = \_ -> doSetMaxHistoryEntries
+  , getMaxHistoryEntries             = \_ -> doGetMaxHistoryEntries
+  , readHistory                      = \_ -> doReadHistory
+  , writeHistory                     = \_ -> doWriteHistory
+  }
+
+
+readlineCompletionFunction :: CompletionFunction -> IO ()
+readlineCompletionFunction f = RL.setAttemptedCompletionFunction (Just complete)
+
+ where complete word begin end = do
+          buffer <- RL.getLineBuffer
+          let before = take begin buffer
+          let after  = drop end buffer
+
+          f (before,word,after)
+
+
+readlineGetSingleChar :: String -> IO (Maybe Char)
+readlineGetSingleChar prompt = do
+   hPutStr stdout prompt
+   hFlush stdout
+   Ex.bracket (hGetBuffering stdin) (hSetBuffering stdin) $ \_ -> do
+      hSetBuffering stdin NoBuffering
+      c <- hGetChar stdin
+      hPutStrLn stdout ""
+      return (Just c)
+
+-- foreign import ccall "readline/history.h history_get_history_state" history_get_history_state :: IO (Ptr ())
+-- foreign import ccall "readline/history.h history_set_history_state" history_set_history_state :: Ptr () -> IO ()
+foreign import ccall "readline/history.h clear_history" clear_history :: IO ()
+foreign import ccall "readline/history.h stifle_history" stifle_history :: CInt -> IO ()
+foreign import ccall "readline/history.h unstifle_history" unstifle_history :: IO CInt
+foreign import ccall "readline/history.h read_history" read_history :: CString -> IO Errno
+foreign import ccall "readline/history.h write_history" write_history :: CString -> IO Errno
+foreign import ccall "readline/history.h history_max_entries" history_max_entries :: Ptr CInt
+
+doClearHistoryState :: IO ()
+doClearHistoryState = clear_history
+
+doSetMaxHistoryEntries :: Int -> IO ()
+doSetMaxHistoryEntries m = stifle_history (fromIntegral m)
+
+doGetMaxHistoryEntries :: IO Int
+doGetMaxHistoryEntries = peek history_max_entries >>= return . fromIntegral
+
+doReadHistory :: FilePath -> IO ()
+doReadHistory path = do
+  err <- withCString path read_history
+  if err == eOK
+     then return ()
+     else ioError $ errnoToIOError 
+              "System.Console.Shell.Backend.Readline.doReadHistory"
+              err
+              Nothing
+              (Just path)
+
+doWriteHistory :: FilePath -> IO ()
+doWriteHistory path = do
+  err <- withCString path write_history
+  if err == eOK
+     then return ()
+     else ioError $ errnoToIOError
+              "System.Console.Shell.Backend.Readline.doWriteHistory"
+              err
+              Nothing
+              (Just path)
+
+readlineOutput :: String -> BackendOutput -> IO ()
+readlineOutput x (RegularOutput str)  = hPutStr stdout ("\n"++str++"\n")
+readlineOutput x (InfoOutput str)     = hPutStr stdout ("\n"++str++"\n")
+readlineOutput x (ErrorOutput str)    = hPutStr stderr ("\n"++str++"\n")
