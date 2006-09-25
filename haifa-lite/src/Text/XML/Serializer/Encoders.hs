41ef630cb2bda0b7a1b04fca052dd57ae0abb155
Christian Maeder <Christian.Maeder@dfki.de>
2006-09-25 15:32:22 +0000

    added -fth OPTION git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6975 cec4b9c1-7d33-0410-9eda-942365e851bb

--- haifa-lite/src/Text/XML/Serializer/Encoders.hs
+++ haifa-lite/src/Text/XML/Serializer/Encoders.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS -fglasgow-exts -fallow-undecidable-instances #-}
+{-# OPTIONS -fglasgow-exts -fth -fallow-undecidable-instances #-}
 ----------------------------------------------------------------------------
 -- |
 -- Module      :  Text.XML.Serializer.Encoders
