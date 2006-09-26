c290de646846ce532d2e1eeb648f07c20407417b
Christian Maeder <Christian.Maeder@dfki.de>
2006-09-26 11:05:37 +0000

    make script callable from any directory git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@6979 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/nightly/linux/runHsIsabelle.sh
+++ utils/nightly/linux/runHsIsabelle.sh
@@ -3,5 +3,5 @@
 for i in $*
 do
    j=`basename $i .thy`
-   isabelle -q -e "use_thy \"$j\";print_depth 300;theory \"$j\";axioms_of it;quit();" HOLCF
+   (cd `dirname $i`; isabelle -q -e "use_thy \"$j\";print_depth 300;theory \"$j\";axioms_of it;quit();" HOLCF)
 done
