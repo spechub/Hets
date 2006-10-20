f57279093718fac174b661b128d03b5049ac467c
Till Mossakowski <mossakow@iws.cs.uni-magdeburg.de>
2006-10-20 10:58:35 +0000

    changed result to Result (FORMULA f) git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@7075 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CASL/Induction.hs
+++ CASL/Induction.hs
@@ -4,7 +4,7 @@ Description :  Derive induction schemes from sort generation constraints
 Copyright   :  (c) Till Mossakowski and Uni Bremen 2002-2006
 License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
 
-Maintainer  :  maeder@tzi.de
+Maintainer  :  till@tzi.de
 Stability   :  provisional
 Portability :  portable
 
@@ -26,7 +26,7 @@ import Common.Result
 -- | derive a second-order induction scheme from a sort generation constraint
 -- | the second-order predicate variables are represented as predicate
 -- | symbols P[s], where s is a sort
-inductionScheme :: [Constraint] -> FORMULA f
+inductionScheme :: [Constraint] -> Result (FORMULA f)
 inductionScheme constrs =
   induction constrs (map predSubst constrs)
   where predSubst constr t =
@@ -43,11 +43,13 @@ inductionScheme constrs =
 -- | sort generation constraint for this list of formulas
 -- | It is assumed that the (original) sorts of the constraint
 -- | match the sorts of the free variables
-instantiateSortGen :: [Constraint] -> [(FORMULA f,VAR,SORT)] -> FORMULA f
+instantiateSortGen :: [Constraint] -> [(FORMULA f,VAR,SORT)] 
+                        -> Result (FORMULA f)
 instantiateSortGen constrs phis =
   induction constrs (map substFormula phis)
   where substFormula (phi,v,_) t = substitute v t phi
 
+-- | substitute a term for a variable in a formula
 substitute :: VAR -> TERM f -> FORMULA f -> FORMULA f
 substitute v t = foldFormula $
  (mapRecord id) {foldQual_var = \ t2 v2 s _ ->
@@ -55,9 +57,30 @@ substitute v t = foldFormula $
 
 -- | derive an induction scheme from a sort generation constraint
 -- | using substitutions as induction predicates
-induction :: [Constraint] -> [TERM f -> FORMULA f] -> FORMULA f
-induction constrs subst =
-  Implication inductionPremises inductionConclusion True nullRange
+induction :: [Constraint] -> [TERM f -> FORMULA f] -> Result (FORMULA f)
+induction constrs substs =
+  if not (length constrs == length substs)
+    then fail "CASL.Induction.induction: argument lists must have equal length"
+    else return $
+          Implication inductionPremises inductionConclusion True nullRange
   where
-  inductionConclusion = True_atom nullRange -- Quantification Universal [Var_decl [v] s nullRange] phi
-  inductionPremises = True_atom nullRange
+  sortInfo = zip3 constrs substs 
+                  (zip (map mkVar [1..length constrs]) (map newSort constrs))
+  inductionConclusion = mkConj $ map mkConclusion sortInfo
+  mkConclusion (_,subst,(v,s)) = 
+    Quantification Universal [Var_decl [v] s nullRange] 
+                   (subst (var (v,s))) nullRange
+  mkVar i = mkSimpleId ("x_"++show i)
+  var (v,s) = Qual_var v s nullRange
+  inductionPremises = mkConj $ map mkPrem sortInfo
+
+-- | construct a premise for the induction scheme 
+-- | (i.e. for one sort in the constraint)
+mkPrem :: (Constraint, TERM f -> FORMULA f, (VAR,SORT)) -> FORMULA f
+mkPrem (constr,subst,(v,s)) = True_atom nullRange
+
+-- | optimized conjunction
+mkConj :: [FORMULA f] -> FORMULA f
+mkConj [] = False_atom nullRange
+mkConj [phi] = phi
+mkConj phis = Conjunction phis nullRange
