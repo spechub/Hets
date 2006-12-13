eae398d41705bf1ce5d3214c6e13152a817f4853
Christian Maeder <Christian.Maeder@dfki.de>
2006-12-13 16:20:18 +0000

    used ghc-6.6 and build in ~/haskell/V240-solaris git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@7257 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/nightly/solaris/cronjob.sh
+++ utils/nightly/solaris/cronjob.sh
@@ -1,20 +1,19 @@
 #!/bin/bash -x
 
-PATH=/usr/local/bin:/usr/bin/:/usr/local/X11/bin:/home/pub-bkb/bin:/usr/ccs/bin
+PATH=/home/maeder/bin:/home/pub-bkb/bin:/usr/local/bin:/usr/bin/:/usr/local/X11/bin:/usr/ccs/bin
 MAKE=gmake
 UDG_HOME=/home/pub-bkb/uDrawGraph-3.1
-HETS_LIB=/local/home/maeder/haskell/CASL-lib
+HETS_LIB=/home/maeder/haskell/V240-solaris/haskell/CASL-lib
 
 export PATH
 export MAKE
 export UDG_HOME
 export HETS_LIB
 
-cd /local/home/maeder/haskell
+cd /home/maeder/haskell/V240-solaris/haskell
 
 . ../cronjob.sh
 
-makeProgramatica
 makeHets
 makeLibCheck
 
