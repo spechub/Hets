80165d62acd4644f062affcd5968ebed7b5b4b51
Christian Maeder <Christian.Maeder@dfki.de>
2007-03-01 18:12:28 +0000

    added export list and removed unused functions git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@7579 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Common/Utils.hs
+++ Common/Utils.hs
@@ -12,7 +12,18 @@ Utility functions that can't be found in the libraries
                but should be shared across Hets.
 -}
 
-module Common.Utils where
+module Common.Utils
+        ( joinWith
+        , splitOn
+        , basename
+        , dirname
+        , fileparse
+        , stripDir
+        , stripSuffix
+        , safeContext
+        , getEnvSave
+        , filterMapWithList
+        ) where
 
 import Data.List
 import Data.Graph.Inductive.Graph
@@ -20,11 +31,7 @@ import Data.Graph.Inductive.Graph
 import qualified Data.Map as Map
 import qualified Data.Set as Set
 
-import System.Posix.Process
-import System.Time
-import System.Random
-import System.IO
-import System
+import System.Environment
 import System.IO.Error
 
 import qualified Control.Exception as Exception
@@ -92,92 +99,6 @@ stripSuffix suf fp = case filter justs $ map (stripSuf fp) suf of
           justs (Nothing) = False
           justs (Just _)  = True
 
-isSublistOf :: (Eq a) => [a] -> [a] -> Bool
-isSublistOf [] _ = True
-isSublistOf _ [] = False
-isSublistOf ys l@(_:l')
-    | length ys <= length l = (ys `isPrefixOf` l) || (ys `isSublistOf` l')
-    | otherwise = False
-
--- IgnoreMaybe datatype
--- extension to Maybe for use in computations over recursive types
--- that need a "don't care" result
---
--- RealJust a means result a was computed
--- RealNothing means computation failed
--- IgnoreNothing means the rest of the computation should not be
---               influenced by this result
---
-data IgnoreMaybe a = RealJust a
-                   | RealNothing
-                   | IgnoreNothing
-
--- drop IgnoreNothing from a list to get Maybe list
---
-dropIgnore :: [IgnoreMaybe a] -> [Maybe a]
-dropIgnore []                = []
-dropIgnore ((RealJust x):t)  = (Just x):(dropIgnore t)
-dropIgnore (RealNothing:t)   = Nothing:(dropIgnore t)
-dropIgnore (IgnoreNothing:t) = dropIgnore t
-
--- convert from Maybe to IgnoreMaybe
---
-toIgnore :: Maybe a -> IgnoreMaybe a
-toIgnore (Just x) = RealJust x
-toIgnore _        = RealNothing
-
--- convert from IgnoreMaybe to Maybe
--- IgnoreNothing is propagated (wrt to the meaning given above) to Nothing
---
-toMaybe :: IgnoreMaybe a -> Maybe a
-toMaybe (RealJust x) = Just x
-toMaybe _            = Nothing
-
--- map over IgnoreMaybe taking (Maybe a -> b) function
---
-mapIgnore :: (Maybe a -> b) -> [IgnoreMaybe a] -> [b]
-mapIgnore _ [] = []
-mapIgnore f (IgnoreNothing:t) = mapIgnore f t
-mapIgnore f (h:t) = (f $ toMaybe h):(mapIgnore f t)
-
--- map over IgnoreMaybe taking (a -> b) function
---
-mapIgnoreMaybe :: (a -> b) -> [IgnoreMaybe a] -> [b]
-mapIgnoreMaybe _ [] = []
-mapIgnoreMaybe f (RealJust x:t) = (f x):(mapIgnoreMaybe f t)
-mapIgnoreMaybe f (_:t) = mapIgnoreMaybe f t
-
--- add element to list as if it were a set
---  addition is to the front if element wasn't already in the list, in which
---  case the list is not modified
---
-setAddOne :: Eq a => [a] -> a -> [a]
-setAddOne set x = if (x `elem` set) then set else (x:set)
-
--- concat two lists as if they were sets
--- adds each element from the second list using setAddOne
---
-setAdd :: Eq a => [a] -> [a] -> [a]
-setAdd set add = foldl setAddOne set add
-
--- find out whether all the elements of a list occur only once
---
-allUnique :: Eq a => [a] -> Bool
-allUnique [] = False
-allUnique [_] = True
-allUnique (h:t) = ([ x | x<-t, x == h ]==[]) && allUnique t
-
--- compute members of a list occuring more than once
---
-notUnique :: Eq a => [a] -> [a]
-notUnique [] = []
-notUnique (h:t) = let
-                    diff = [ x | x<-t, x/=h ]
-                    rest = notUnique diff
-                  in
-                    case (h `elem` t) of True ->  h : rest
-                                         False -> rest
-
 -- safe context for graphs
 safeContext :: (Show a, Show b,Graph gr) => String -> gr a b -> Node
             -> Context a b
@@ -202,13 +123,6 @@ filterMapWithSet :: Ord k => Set.Set k -> Map.Map k e -> Map.Map k e
 filterMapWithSet s = Map.filterWithKey selected
     where selected k _ = Set.member k s
 
-{- |
-  advice from <http://haskell.org/hawiki/ThingsToAvoid> use this instead of
-  direct application of selector function
--}
-comparing :: (Ord b) => (a -> b) -> a -> a -> Ordering
-comparing selector x y = compare (selector x) (selector y)
-
 {- | get, parse and check an environment variable; provide the default
   value, only if the envionment variable is not set or the
   parse-check-function returns a Left value
@@ -227,28 +141,3 @@ getEnvSave defValue envVar readFun = do
                           else Exception.throwIO e
                       _ -> Exception.throwIO e)
    return (either id (\s -> (either (const defValue) id (readFun s))) is)
-
-{- |
-  create a temp file.
--}
-createTempFile :: FilePath      -- ^ parent path, but no separator (\/)
-               -> FilePath     -- ^ name of file (prefix)
-               -> FilePath     -- ^ suffix of file (no point)
-               -> IO Handle
-createTempFile parent preName sufName =
-    do randm <- getRandom
-       pid <- getProcessID
-       time <- getClockTime
-       ctime <- toCalendarTime time
-       let outTime = show (ctDay ctime) ++ show (ctHour ctime)
-                     ++ show (ctMin ctime) ++ show (ctSec ctime)
-           parentPath = (if length parent == 0
-                          then "/tmp/tmp" ++ show randm
-                          else parent)
-           separator = "/"
-           randomName = show pid ++ outTime ++ show randm
-           abPath = parentPath ++ separator ++ preName ++ randomName ++
-                    (if length sufName == 0 then "" else "." ++ sufName)
-       openFile abPath WriteMode
-    where getRandom :: IO Int
-          getRandom = getStdRandom (randomR (10000, 99999))
