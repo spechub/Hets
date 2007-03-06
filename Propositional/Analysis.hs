648e803bb79a00b1c246d093fe2a2707c6b2cf7b
Dominik Luecke <luecke@informatik.uni-bremen.de>
2007-03-06 10:40:53 +0000

    Fixed bug 286 (hopefully) git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@7607 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Propositional/Analysis.hs
+++ Propositional/Analysis.hs
@@ -29,6 +29,14 @@ import qualified Common.Result as Result
 import qualified Common.Id as Id
 import qualified Data.List as List
 import qualified Data.Set as Set
+import Common.Doc()
+import Common.DocUtils
+
+data DIAG_FORM = DiagForm 
+    {
+      formula :: AS_Anno.Named (AS_BASIC.FORMULA),
+      diagnosis :: Result.Diagnosis
+    }
 
 -- | Retrieves the signature out of a basic spec
 makeSig :: 
@@ -53,34 +61,85 @@ retrieveBasicItem sig x =
 makeFormulas :: 
     AS_BASIC.BASIC_SPEC
     -> Sign.Sign
-    -> [AS_Anno.Named (AS_BASIC.FORMULA)]
+    -> [DIAG_FORM]
 makeFormulas (AS_BASIC.Basic_spec bspec) sig = List.foldl (\xs bs -> retrieveFormulaItem xs bs sig) []  bspec
 
 -- Helper for makeFormulas
 retrieveFormulaItem :: 
-    [AS_Anno.Named (AS_BASIC.FORMULA)]
+    [DIAG_FORM]
     -> AS_Anno.Annoted (AS_BASIC.BASIC_ITEMS)
     -> Sign.Sign
-    -> [AS_Anno.Named (AS_BASIC.FORMULA)]
+    -> [DIAG_FORM]
 retrieveFormulaItem axs x sig = 
     case (AS_Anno.item x) of 
       (AS_BASIC.Pred_decl _)    -> axs 
-      (AS_BASIC.Axiom_items ax) -> List.foldl (\xs bs -> addFormula xs bs sig) axs ax
+      (AS_BASIC.Axiom_items ax) -> List.foldl (\xs bs -> addFormula xs bs sig) axs $ numberFormulae ax 0
+
+-- Number formulae
+numberFormulae :: [AS_Anno.Annoted (AS_BASIC.FORMULA)] -> Integer -> [(AS_Anno.Annoted (AS_BASIC.FORMULA), Integer)]
+numberFormulae [] _ = []
+numberFormulae (x:xs) i 
+    | label == "" =  (x, i) : (numberFormulae xs $ i + 1)
+    | otherwise   =  (x, 0) : (numberFormulae xs $ i)
+    where
+      label = AS_Anno.getRLabel x
 
 --  Add a formula to a named list of formulas
-addFormula :: [AS_Anno.Named (AS_BASIC.FORMULA)]
-           -> AS_Anno.Annoted (AS_BASIC.FORMULA) 
+addFormula :: [DIAG_FORM]
+           -> (AS_Anno.Annoted (AS_BASIC.FORMULA), Integer) 
            -> Sign.Sign
-           -> [AS_Anno.Named (AS_BASIC.FORMULA)]             
-addFormula formulae f sign
-    | isLegal = formulae ++ [AS_Anno.emptyName nakedFormula] 
-    | otherwise = error "Unknown proposition"
+           -> [DIAG_FORM]             
+addFormula formulae (f, i) sign
+    | isLegal == True = formulae ++ [DiagForm
+                                     {
+                                       formula   = makeNamed f i
+                                     , diagnosis = Result.Diag 
+                                                   {
+                                                     Result.diagKind = Result.Hint
+                                                   , Result.diagString = "All fine"
+                                                   , Result.diagPos    = Id.nullRange
+                                                   }
+                                     }] 
+    | otherwise       = formulae ++ [DiagForm
+                                     {
+                                       formula   = makeNamed f i
+                                     , diagnosis = Result.Diag 
+                                                   {
+                                                     Result.diagKind = Result.Error
+                                                   , Result.diagString = "Unknown propositions "
+                                                     ++ (show $ pretty difference)
+                                                     ++ "in formula "
+                                                     ++ (show $ pretty nakedFormula)
+                                                   , Result.diagPos    = Id.nullRange
+                                                   }
+                                     }] 
     where
       nakedFormula  = AS_Anno.item f
       varsOfFormula = propsOfFormula nakedFormula
       isLegal       = Sign.isSubSigOf varsOfFormula sign
       difference    = Sign.sigDiff varsOfFormula sign
 
+-- generates a named formula
+makeNamed :: AS_Anno.Annoted (AS_BASIC.FORMULA) -> Integer -> AS_Anno.Named (AS_BASIC.FORMULA)
+makeNamed f i 
+    | label == "" = AS_Anno.NamedSen
+                    {
+                      AS_Anno.senName = "Ax_" ++ show i
+                    , AS_Anno.isAxiom = True
+                    , AS_Anno.isDef   = False
+                    , AS_Anno.sentence = AS_Anno.item f
+                    }
+    | otherwise   = AS_Anno.NamedSen
+                    {
+                      AS_Anno.senName = label
+                    , AS_Anno.isAxiom = True
+                    , AS_Anno.isDef   = False
+                    , AS_Anno.sentence = AS_Anno.item f
+                    }
+    where 
+      label = AS_Anno.getRLabel f
+
+-- Retrives the signature of a formula
 propsOfFormula :: AS_BASIC.FORMULA -> Sign.Sign
 propsOfFormula (AS_BASIC.Negation form _) = propsOfFormula form
 propsOfFormula (AS_BASIC.Implication form1 form2 _) = Sign.unite (propsOfFormula form1)
@@ -107,12 +166,15 @@ basicAnalysis :: AS_BASIC.BASIC_SPEC
                                ,Sign.Sign
                                ,[AS_Anno.Named (AS_BASIC.FORMULA)]
                                )
-basicAnalysis bs sig _ = 
-    let 
-        bsSig = makeSig bs sig
-        bsForm = makeFormulas bs bsSig
-    in
-    Result.Result [] $ Just (bs, bsSig, bsForm)
+basicAnalysis bs sig _ 
+    | exErrs == False =  Result.Result diags $ Just (bs, bsSig, formulae)
+    | otherwise       =  Result.Result diags $ Nothing
+    where 
+      bsSig     = makeSig bs sig
+      bsForm    = makeFormulas bs bsSig
+      formulae  = map (\x -> formula x) bsForm
+      diags     = map (\x -> diagnosis x) bsForm   
+      exErrs    = Result.hasErrors diags
 
 -- | Wrapper for the interface defined in Logic.Logic
 basicPropositionalAnalysis :: (
