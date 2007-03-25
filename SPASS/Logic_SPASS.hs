47ad79f4c6b3f9d816b81b700891cdf24425c99c
Till Mossakowski <mossakow@iws.cs.uni-magdeburg.de>
2007-03-25 14:13:20 +0000

    provided symbols using a hack git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@7755 cec4b9c1-7d33-0410-9eda-942365e851bb

--- SPASS/Logic_SPASS.hs
+++ SPASS/Logic_SPASS.hs
@@ -16,7 +16,7 @@ Instance of class Logic for SoftFOL.
 module SPASS.Logic_SPASS where
 
 import Common.DefaultMorphism
-
+import Data.Set as Set
 import Logic.Logic
 
 import SPASS.ATC_SPASS ()
@@ -60,6 +60,7 @@ instance Logic.Logic.Syntax SoftFOL () () ()
 
 instance Sentences SoftFOL Sentence ATP_ProofTree Sign SoftFOLMorphism () where
       map_sen SoftFOL _ s = return s
+      sym_of SoftFOL _ = Set.empty
       print_named SoftFOL = printFormula
 -- the prover uses HTk and IO functions from uni
 #ifdef UNI_PACKAGE
