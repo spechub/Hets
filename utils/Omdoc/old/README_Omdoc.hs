fb3faa201e16ab9f0f020546fb76b0ead641541f
Hendrik Iben <hiben@informatik.uni-bremen.de>
2007-04-02 18:57:34 +0000

    removed very old omdoc code git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@7770 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/Omdoc/old/README_Omdoc.hs
+++
@@ -1,9 +0,0 @@
-This is the first attempt at creating a Haskell-Datatype for Omdoc-structures.
-
-To read in an Omdoc-document import 'Omdoc.hs' and do something similar to :
-	myOmdoc <- (fReadXml "file.omdoc")::(IO Omdoc)
-
-To write an Omdoc-structure to a file :
-	fWriteXml "file.omdoc" myOmdoc
-
-The state of developement is currently experimental. Reading in a file and writing it back does not always result in equivalent data (see 'examples' directory for some information).
