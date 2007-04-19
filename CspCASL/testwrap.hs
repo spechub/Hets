34a4c8c6f861104cdc198282f30fae36cf3858ad
Andy Gimblett <a.m.gimblett@swansea.ac.uk>
2007-04-19 19:50:34 +0000

    CspCASL - more work on testing git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@7829 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CspCASL/testwrap.hs
+++ CspCASL/testwrap.hs
@@ -13,31 +13,72 @@ This is a standalone `main' wrapper for CspCASL-related tests
 performed locally to the CspCASL codebase.  It's probably only of
 interest to the CspCASL maintainers.
 
-The "test" directory contains (negative & positive) test cases.  Each
-test case is identified by a *.testcase file, with the following
-structure:
+Usage:
 
-  - The first line is the name of the file containing the input source
-    to use for this test case (also in the test directory).
+  testwrap targets
 
-  - The second line identifies which parser is being tested (eg
-    "Process", "Core-CspCASL").
+  where each target can be:
 
-  - The third line identifies whether this is a positive (++) or
-    negative (--) test case.
+    - a .cspcasl file; parse the file as a Core-CspCASL specification,
+      unparse the parse tree, and print out the result of the unparse.
+      In case of parse error, report the error.
 
-  - If it's a positive test case, the remaining lines contain the
-    expected output of the pretty printer.
+    - a .testcase file; execute the test and report the outcome.  A
+      testcase file specifies one test case, whose source is contained
+      in another file, and whose output we will check against expected
+      contents.  See below for the file format.
 
-  - If it's a negative test case, the remaining lines contain
-    information about the expected error.
+    - a .testcases file; execute the tests and report their outcomes.
+      A testcases file specifies multiple test cases, with source
+      integrated with each test case, and outputs we will check
+      against expected contents.  See below for the file format.
+
+    - a directory; read all .testcase and .testcases files in the
+      directory (non-recursively) and operate on them as described
+      above.
+
+Postive & negative tests:
+
+  A positive test is one where we expect the parse to succeed; here
+  the expected output is the result of unparsing the resultant parse
+  tree.  The test can fail with a parse error, or with unexpected
+  output.
+
+  A negative test is one where we expect the parse to fail; here the
+  expected output is the error message produced.  The test can fail
+  with a successful parse, or with unexpected output.
+
+Format of .testcase files:
+
+  A .testcase file contains a single test case.  The first line is the
+  name of the file containing the source to be parsed/tested (it also
+  acts as the name of the test case).  The second line identifies the
+  test sense ("++" is positive, "--" is negative). The third line is
+  the name of the parser to be used.  The remaining lines contain the
+  expected output of the test.
+
+Format of .testcases files:
+
+  A .testcases file contains multiple test cases including their
+  source.  Individual test cases are separated by lines containing
+  twenty '-' characters and nothing else.  The format of an individual
+  test case is similar but not identical to the format of a standalone
+  test case (above).  The first line is the name of the test (used for
+  reporting).  The second line identifies the test sense ("++" is
+  positive, "--" is negative). The third line is the name of the
+  parser to be used.  This is followed by the source of the test and
+  the expected outcome of the test, both of which may span multiple
+  lines; they are separated by a line containing ten '-' characters
+  and nothing else.
 
 -}
 
 module Main where
 
-import Directory
 import List
+import Monad
+import System.Directory
+import System.Environment (getArgs)
 import System.IO
 import Text.ParserCombinators.Parsec
 
@@ -47,6 +88,53 @@ import Common.DocUtils
 import CspCASL.Parse_CspCASL(basicCspCaslSpec)
 import CspCASL.Parse_CspCASL_Process(csp_casl_process)
 import CspCASL.Print_CspCASL()
+
+main :: IO ()
+main = do args <- getArgs 
+          ccs <- filterSuffix args ".cspcasl"
+          tests <- (liftM2 (++) (filterSuffix args ".testcase")
+                                (filterSuffix args ".testcases"))
+          parseCspCASLs ccs
+          --print tests
+    where
+      -- Filter existing files with given suffix
+      filterSuffix fs sfx = filterM doesFileExist (filter (sfx `isSuffixOf`) fs)
+
+-- | Recursive file lister adapted from
+-- http://therning.org/magnus/archives/228
+listFilesR :: FilePath -> IO [FilePath]
+listFilesR path =
+    do allfiles <- getDirectoryContents path
+       no_dots <- filterM (return . isDODD) (map (joinFN path) allfiles)
+       dirs <- listDirs no_dots
+       subdirfiles <- (mapM listFilesR dirs >>= return . concat)
+       files <- listFiles no_dots
+       return $ files ++ subdirfiles
+    where
+      isDODD f = not $ ("/." `isSuffixOf` f) || ("/.." `isSuffixOf` f)
+      listDirs = filterM doesDirectoryExist
+      listFiles = filterM doesFileExist
+      joinFN p1 p2 = p1 ++ "/" ++ p2
+
+-- | Given a list of paths to .cspcasl files, parse each in turn,
+-- printing results as you go.
+parseCspCASLs :: [FilePath] -> IO ()
+parseCspCASLs [] = do putStr ""
+parseCspCASLs (f:fs) = do putStrLn "--------------------"
+                          prettyCspCASLFromFile f
+                          parseCspCASLs fs
+
+-- | Parse one .cspcasl file; print error or pretty print parse tree.
+prettyCspCASLFromFile :: FilePath -> IO ()
+prettyCspCASLFromFile fname
+  = do putStrLn ("Parsing " ++ fname)
+       input <- readFile fname
+       case (runParser basicCspCaslSpec (emptyAnnos ()) fname input) of
+           Left err -> do putStr "parse error at "
+                          print err
+           Right x  -> do putStrLn $ showDoc x ""
+
+
 
 -- | Test sense: do we expect parse success or failure?  What is the
 -- nature of the expected output?
@@ -79,16 +167,17 @@ instance Show TestCase where
 data TestOutcome = TestPass TestCase
                  | TestFail TestCase String
 
-main :: IO ()
-main = do contents <- readAllTests "test"
-          printResults (map performTest (sort contents))
+tcMain :: IO ()
+tcMain = do contents <- readAllTests "test"
+            printResults (map performTest (sort contents))
 
 printResults :: [TestOutcome] -> IO ()
 printResults [] = do putStr ""
-printResults ((TestPass tc):xs) = do putStr ((show tc) ++ " passed\n")
+printResults ((TestPass tc):xs) = do putStrLn ((show tc) ++ " passed")
                                      printResults xs
-printResults ((TestFail tc o):xs) = do putStr ((show tc) ++ " failed\n")
-                                       putStr (o ++ "\nvs\n" ++ (expected tc) ++ "\n--\n")
+printResults ((TestFail tc o):xs) = do putStrLn ((show tc) ++ " failed")
+                                       putStrLn (o ++ "\nvs\n" ++ (expected tc))
+                                       putStrLn "--"
                                        printResults xs
 
 -- | Perform a test and record its outcome.  There are six
@@ -144,12 +233,9 @@ readAllTests path = do tests <- listTestCases path
 listTestCases :: FilePath -> IO [FilePath]
 listTestCases path =
     do files <- getDirectoryContents path
-       let cases = (map (\x -> path ++ "/" ++ x) (filter isTestCase files))
+       let cases = (map (\x -> path ++ "/" ++ x)
+                            (filter (".testcase" `isSuffixOf`) files))
        return cases
-    where
-      endswith sub tgt = drop (length sub - length tgt) sub == tgt
-      isTestCase f = endswith f ".testcase"
-
 
 -- | Given the path to a .testcase file, return TestCase value
 -- described therein.
