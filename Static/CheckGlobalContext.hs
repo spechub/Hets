a5aa566f43f5bec72e84816df8f5c766ad8c9b4d
Jian Chun Wang <wjch868@informatik.uni-bremen.de>
2007-05-11 12:36:16 +0000

    total Numbers git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@7891 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Static/CheckGlobalContext.hs
+++ Static/CheckGlobalContext.hs
@@ -23,12 +23,15 @@ import qualified Data.Map as Map
 import Common.Lib.State
 
 data Statistics = Statistics
-    { zeroSign, wrongSign, zeroMor, wrongMor, zeroTh, wrongTh :: Int } 
+    { zeroSign, wrongSign, totalSign,
+      zeroMor, wrongMor, totalMor,
+      zeroTh, wrongTh, totalTh :: Int } 
     deriving (Show)
 
 initStat :: Statistics
-initStat = Statistics { zeroSign = 0, wrongSign = 0, zeroMor = 0,
-                        wrongMor = 0, zeroTh = 0, wrongTh = 0 } 
+initStat = Statistics { zeroSign = 0, wrongSign = 0, totalSign = 0, 
+                        zeroMor = 0, wrongMor = 0, totalMor = 0,
+                        zeroTh = 0, wrongTh = 0, totalTh = 0} 
 
 incrZeroSign :: Statistics -> Statistics
 incrZeroSign s = s { zeroSign = zeroSign s + 1 }
@@ -36,11 +39,17 @@ incrZeroSign s = s { zeroSign = zeroSign s + 1 }
 incrWrongSign :: Statistics -> Statistics
 incrWrongSign s = s { wrongSign = wrongSign s + 1 } 
 
+incrTotalSign ::Statistics -> Statistics 
+incrTotalSign s = s { totalSign = totalSign s + 1 }
+
 incrZeroG_theory :: Statistics -> Statistics
 incrZeroG_theory s = s { zeroTh = zeroTh s + 1 }
 
 incrWrongG_theory :: Statistics -> Statistics
 incrWrongG_theory s = s { wrongTh = wrongTh s + 1 }
+
+incrTotalG_theory :: Statistics -> Statistics
+incrTotalG_theory s = s { totalTh = totalTh s + 1 } 
 
 incrZeroGMorphism :: Statistics -> Statistics
 incrZeroGMorphism s = s { zeroMor = zeroMor s +1 }
@@ -48,18 +57,21 @@ incrZeroGMorphism s = s { zeroMor = zeroMor s +1 }
 incrWrongGMorphism :: Statistics -> Statistics
 incrWrongGMorphism s = s { wrongMor = wrongMor s + 1 }
 
+incrTotalGMorphism :: Statistics -> Statistics
+incrTotalGMorphism s = s { totalMor = totalMor s + 1 }
+
 checkG_theory :: G_theory -> GlobalContext -> State Statistics ()
 checkG_theory g@(G_theory _ _ si _ ti) ctxt = do 
     if si == 0 then modify incrZeroSign
        else case Map.lookup si $ sigMap ctxt of
           Nothing -> error "checkG_theory: Sign"
           Just signErg -> if signOf g /= signErg then modify incrWrongSign
-                          else return ()
+                          else modify incrTotalSign
     if ti == 0 then modify incrZeroG_theory
        else case Map.lookup ti $ thMap ctxt of
           Nothing -> error "checkG_theory: Theory"
           Just thErg -> if g /= thErg then modify incrWrongG_theory
-                        else return ()
+                        else modify incrTotalG_theory
 
 checkG_theoryInNode :: GlobalContext -> DGNodeLab -> State Statistics () 
 checkG_theoryInNode ctxt dg = checkG_theory (dgn_theory dg) ctxt
@@ -75,12 +87,12 @@ checkGMorphism g@(GMorphism cid sign si _ mi) ctxt = do
            Nothing -> error "checkGMorphism: Sign"
            Just signErg -> if  G_sign (sourceLogic cid) sign si /= signErg 
                            then modify incrWrongSign
-                           else return ()
+                           else modify incrTotalSign
     if mi == 0 then modify incrZeroGMorphism
        else case Map.lookup mi $ morMap ctxt of
            Nothing -> error "checkGMorphism: Morphism"
            Just morErg -> if g /= gEmbed morErg then modify incrWrongGMorphism
-                          else return ()
+                          else modify incrTotalGMorphism
    
 getDGLinkLab :: DGraph -> [DGLinkLab]
 getDGLinkLab dgraph = map (\(_,_,label) -> label) $ labEdges dgraph 
@@ -113,4 +125,5 @@ checkGlobalContext ctxt = do
 
 printStatistics :: GlobalContext -> String
 printStatistics ctxt = show $ execState (checkGlobalContext ctxt) initStat
+
 
