f78ff42037086a58af7b785eabcda899f5d30de5
Christian Maeder <Christian.Maeder@dfki.de>
2007-06-01 16:40:44 +0000

    based mapping on folding git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@7969 cec4b9c1-7d33-0410-9eda-942365e851bb

--- HasCASL/MapTerm.hs
+++ HasCASL/MapTerm.hs
@@ -8,59 +8,51 @@ Stability   :  provisional
 Portability :  portable
 
 rename symbols of terms according to a signature morphisms
-
 -}
 
 module HasCASL.MapTerm where
 
 import HasCASL.As
 import HasCASL.Le
+import HasCASL.FoldTerm
 import Common.Id
 
 type Rename = ((Id, TypeScheme) -> (Id, TypeScheme), Type -> Type)
 
-mapTerm :: Rename -> Term -> Term
-mapTerm m t = case t of
-   QualVar vd -> QualVar $ mapVar (snd m) vd
-   QualOp b (InstOpId i ts ps) sc qs -> 
+renameRec :: Rename -> MapRec
+renameRec m = mapRec
+   { foldQualVar = \ _ vd -> QualVar $ mapVar (snd m) vd
+   , foldQualOp = \ _ b (InstOpId i ts ps) sc qs ->
         let (i2, sc2) = fst m (i, sc)
             in QualOp b (InstOpId i2 (map (snd m) ts) ps) sc2 qs
-   ApplTerm t1 t2 ps ->
-       ApplTerm (mapTerm m t1) (mapTerm m t2) ps
-   TupleTerm ts ps -> TupleTerm (map (mapTerm m) ts) ps
-   TypedTerm te q ty ps -> TypedTerm (mapTerm m te) q (snd m ty) ps
-   QuantifiedTerm q vs te ps -> 
-       QuantifiedTerm q (map (mapGenVar $ snd m) vs) (mapTerm m te) ps
-   LambdaTerm ps p te qs ->     
-       LambdaTerm (map (mapTerm m) ps) p (mapTerm m te) qs
-   CaseTerm te es ps -> 
-       CaseTerm (mapTerm m te) (map (mapEq m) es) ps
-   LetTerm b es te ps ->
-       LetTerm b (map (mapEq m) es) (mapTerm m te) ps
-   AsPattern vd pa ps -> AsPattern (mapVar (snd m) vd) (mapTerm m pa) ps
-   TermToken _ -> t
-   MixfixTerm ts -> MixfixTerm $ map (mapTerm m) ts
-   BracketTerm b ts ps -> BracketTerm b (map (mapTerm m) ts) ps
-   MixTypeTerm q ty ps -> MixTypeTerm q (snd m ty) ps  
-   ResolvedMixTerm i ts ps -> ResolvedMixTerm i (map (mapTerm m) ts) ps
+   , foldTypedTerm = \ _ te q ty ps -> TypedTerm te q (snd m ty) ps
+   , foldQuantifiedTerm = \ _ q vs te ps ->
+       QuantifiedTerm q (map (mapGenVar $ snd m) vs) te ps
+   , foldAsPattern = \ _ vd pa ps -> AsPattern (mapVar (snd m) vd) pa ps
+   , foldMixTypeTerm = \ _ q ty ps -> MixTypeTerm q (snd m ty) ps }
 
-mapGenVar :: (Type -> Type) -> GenVarDecl -> GenVarDecl
-mapGenVar m (GenVarDecl vd) = GenVarDecl $ mapVar m vd
-mapGenVar _ tv = tv 
+mapTerm :: Rename -> Term -> Term
+mapTerm m = foldTerm $ renameRec m
+
+mapEq :: Rename -> ProgEq -> ProgEq
+mapEq m = foldEq $ renameRec m
 
 mapVar :: (Type -> Type) -> VarDecl -> VarDecl
 mapVar m (VarDecl v ty q ps) = VarDecl v (m ty) q ps
 
-mapEq :: Rename -> ProgEq -> ProgEq
-mapEq m (ProgEq p t ps) = ProgEq (mapTerm m p) (mapTerm m t) ps
+mapGenVar :: (Type -> Type) -> GenVarDecl -> GenVarDecl
+mapGenVar m g = case g of
+    GenVarDecl vd -> GenVarDecl $ mapVar m vd
+    _ -> g
 
 mapOpInfo :: Rename -> OpInfo -> OpInfo
-mapOpInfo m oi = oi { opType = mapTypeOfScheme (snd m) $ opType oi
-                    , opAttrs = map (mapOpAttr m) $ opAttrs oi
-                    , opDefn = renameOpDefn m $ opDefn oi }
+mapOpInfo m oi = oi
+    { opType = mapTypeOfScheme (snd m) $ opType oi
+    , opAttrs = map (mapOpAttr m) $ opAttrs oi
+    , opDefn = renameOpDefn m $ opDefn oi }
 
 mapOpAttr :: Rename -> OpAttr -> OpAttr
-mapOpAttr m o = case o of 
+mapOpAttr m o = case o of
     UnitOpAttr t p -> UnitOpAttr (mapTerm m t) p
     _ -> o
 
@@ -72,4 +64,3 @@ renameOpDefn m d = case d of
 
 renameConstrInfo :: (Type -> Type) -> ConstrInfo -> ConstrInfo
 renameConstrInfo m c = c { constrType = mapTypeOfScheme m $ constrType c }
-
