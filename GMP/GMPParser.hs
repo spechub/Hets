f36b9db17fd6c5a36f1a0549968f4d7748080a15
Georgel Calin <g.calin@jacobs-university.de>
2007-06-11 13:57:17 +0000

    GMPParser updated &amp; input slightly modified git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8014 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GMP/GMPParser.hs
+++ GMP/GMPParser.hs
@@ -13,17 +13,59 @@ import Text.ParserCombinators.Parsec.Language
 import GMPAS
 
 -----------------------------------------------------------
--- A program is simply a formulae
+-- The Parser Things
 -----------------------------------------------------------
-program
-    = do{ whiteSpace
-        ; f <- form
-        ; return f
-        }
-{-
+par5er :: Parser String
+par5er = do
+    do try(string "F"); return "F"
+    <|> do try(string "T"); return "T"
+    <|> do try(string "~"); return "Not"
+    <|> do try(string"/\\") ; return "And"
+    <|> do try(string "\\/"); return "Or"
+    <|> do try(string "->"); return "If"
+    <|> do try(string "<-"); return "Fi"
+    <|> do try(string "<->"); return "Iff"
+    <?> "GMPParser.par5er"
+
+{- the parser should actually look somewhat like the following
+par5er :: Parser Formula
+par5er = do Text.ParserCombinators.Parsec.try(string "F"); 
+            return F
+     <|> do Text.ParserCombinators.Parsec.try(string "T"); 
+            return T
+     <|> do Text.ParserCombinators.Parsec.try(string "~"); 
+            return Neg
+     <|> do Text.ParserCombinators.Parsec.try(string "/\\"); 
+            return And
+     <|> do Text.ParserCombinators.Parsec.try(string "\\/"); 
+            return Or
+     <|> do Text.ParserCombinators.Parsec.try(string "->"); 
+            return If
+     <|> do try(string "<-"); 
+            return Fi
+
+-}
+
+runLex :: Show a => Parser a -> String -> IO ()
+runLex p input = run (do {
+    whiteSpace
+    ; x <- p
+    ; eof
+    ; return x
+    }) input
+
+run :: Show a => Parser a -> String -> IO ()
+run p input
+        = case (parse p "" input) of
+                Left err -> do{ putStr "parse error at "
+                                ; print err
+                              }
+                Right x -> print x
+
 ----------------------------------------------------------------
 -- Formulae
 ----------------------------------------------------------------
+{-
 form :: Parser Formula
 form = choice
 	[ not
--- GMP/Main.hs
+++ GMP/Main.hs
@@ -10,13 +10,9 @@ import IO
 import GMPAS
 import GMPParser
 
-test input 
-	= do result <- parseFromFile program name
-		;case result of
-			Left err -> do putStr "parse error at: " ;print err
-			Right x  -> print x
 main = do
     hSetBuffering stdin LineBuffering
     putStrLn "Give the name of the test file: "
     name <- getLine
-    test name
+    input <- readFile name
+    runLex par5er input
