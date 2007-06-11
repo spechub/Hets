570e76db4a9af54fb440cdb1e218649bddd9aa56
Christian Maeder <Christian.Maeder@dfki.de>
2007-06-11 14:03:03 +0000

    the functor example ticket 191 works now git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8015 cec4b9c1-7d33-0410-9eda-942365e851bb

--- HasCASL/TypeAna.hs
+++ HasCASL/TypeAna.hs
@@ -114,10 +114,11 @@ inferKinds b ty te@Env{classMap = cm} = case ty of
         return ((rk, l), KindedType t k ps)
     ExpandedType t1 t2 -> do
         let Result _ mk = inferKinds b t1 te
-        (kp, t4) <- inferKinds b t2 te
-        return (kp, case mk of
-                Just (_, t3) -> ExpandedType t3 t4
-                Nothing -> ExpandedType t1 t4)
+        (kp@(rk, ks), t4) <- inferKinds b t2 te
+        return $ case mk of
+                Just ((_, aks), t3) -> ((rk, keepMinKinds cm $ aks ++ ks)
+                                       , ExpandedType t3 t4)
+                Nothing -> (kp, ExpandedType t1 t4)
     _ -> error "inferKinds"
 
 -- * converting type terms
