c07d153a76b4e67e39c8977788ac446ec3a7255c
Georgel Calin <g.calin@jacobs-university.de>
2007-06-14 08:56:54 +0000

    Test files and short README added git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8036 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GMP/Main.hs
+++ GMP/Main.hs
@@ -10,9 +10,22 @@ import IO
 -- import GMPAS
 import GMPParser
 
+askForInput = do
+    putStrLn ("Please enter the name of the test file (or \"\" to stop):")
+    name <- getLine
+    if name == "" 
+        then return []
+        else do
+            input <- readFile name
+            runLex par5er input
+            askForInput
+            return []
 main = do
     hSetBuffering stdin LineBuffering
+    askForInput
+{-
     putStrLn "Give the name of the test file: "
     name <- getLine
     input <- readFile name
     runLex par5er input
+-}
