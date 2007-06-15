2556f6ccd99ff75bed2260be9e98bfdd4db68a4c
Georgel Calin <g.calin@jacobs-university.de>
2007-06-15 13:01:47 +0000

    Bit-string parsing successfull. Module portable. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8047 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GMP/GMPParser.hs
+++ GMP/GMPParser.hs
@@ -23,21 +23,42 @@ instance ModalLogic () where        -- unit parsed Mindex
     parseIndex = return ()
 instance ModalLogic Integer where   -- integer parsed Mindex
     parseIndex = natural
+--- characters parseIndex----------------------------------
 instance ModalLogic Kars where
-    parseIndex =  do l <- letter; Kars i <- parseIndex; return (Kars (l:i))
+    parseIndex =  do l <- letter 
+                     ;Kars i <- parseIndex 
+                     ;return (Kars (l:i))
               <|> do return (Kars [])
+--- bit-string parseIndex ---------------------------------
+revbInt x size
+    = let
+        revaux (x,size,y,i)
+            = if (i == (size+1))
+                then 0
+                else let y = revaux(x,size,y,i+1)
+                        in if (testBit x i)
+                            then setBit y (size-i)
+                            else clearBit y (size-i)
+      in revaux(x,size,0,0)
 
-bitParse i =  do try(char('0')); BitString n <- bitParse (i+1); return(BitString(clearBit n i))
-          <|> do try(char('1')); BitString n <- bitParse (i+1); return(BitString(setBit n i))
-          <|> return (BitString 0)
+bitParse i =  do try(char('0'))
+                 ;(BitString n, size) <- bitParse (i+1)
+                 ;return((BitString(clearBit n i), size))
+          <|> do try(char('1'))
+                 ;(BitString n, size) <- bitParse (i+1)
+                 ;return((BitString(setBit n i), size))
+          <|> return ((BitString 0), i-1)
           <?> "GMPParse.bitParse"
+
 instance ModalLogic BitString where
-    parseIndex = do res <- bitParse 0; return res
+    parseIndex = do (BitString rres,size) <- bitParse 0 
+                    ;let res = revbInt rres size
+                    ;return (BitString res)
 
 -----------------------------------------------------------
 -- The Different Parsers for general Formula Type
 -----------------------------------------------------------
-par5er :: ModalLogic a => Parser (Formula a) -- main parser to parse the infix/primitive formulae
+par5er :: ModalLogic a => Parser (Formula a) -- main parser
 par5er = do f <- prim; option (f) (inf f)
       <?> "GMPParser.par5er"
 
@@ -56,12 +77,39 @@ inf f1 =
 
 prim :: ModalLogic a => Parser (Formula a)  -- primitive parser
 prim = 
-        do try(string "F"); whiteSpace; return F;
-    <|> do try(string "T"); whiteSpace; return T;
-    <|> do try(string "~"); whiteSpace; f <- par5er; return $ Neg f
-    <|> do try(char '('); whiteSpace; f <- par5er; whiteSpace; char ')'; whiteSpace; return f
-    <|> do try(char '['); whiteSpace; i <- parseIndex; whiteSpace; char ']'; whiteSpace; f <-par5er; return $ Mapp (Mop i Square) f
-    <|> do try(char '<'); whiteSpace; i <- parseIndex; whiteSpace; char '>'; whiteSpace; f <-par5er; return $ Mapp (Mop i Angle) f
+        do try(string "F")
+           ;whiteSpace
+           ;return F
+    <|> do try(string "T")
+           ;whiteSpace
+           ;return T
+    <|> do try(string "~")
+           ;whiteSpace
+           ;f <- par5er
+           ;return $ Neg f
+    <|> do try(char '(')
+           ;whiteSpace
+           ;f <- par5er
+           ;whiteSpace
+           ;char ')'
+           ;whiteSpace
+           ;return f
+    <|> do try(char '[')
+           ;whiteSpace
+           ;i <- parseIndex
+           ;whiteSpace
+           ;char ']'
+           ;whiteSpace
+           ;f <-par5er
+           ;return $ Mapp (Mop i Square) f
+    <|> do try(char '<')
+           ;whiteSpace
+           ;i <- parseIndex
+           ;whiteSpace
+           ;char '>'
+           ;whiteSpace
+           ;f <- par5er
+           ;return $ Mapp (Mop i Angle) f
     <?> "GMPParser.prim"
 ----------------------------------------------------------------
 -- Run GMP parser & print
@@ -77,9 +125,8 @@ runLex p input = run (do
 run :: Show a => Parser a -> String -> IO ()
 run p input
         = case (parse p "" input) of
-                Left err -> do{ putStr "parse error at "
-                                ; print err
-                              }
+                Left err -> do putStr "parse error at "
+                               ;print err
                 Right x -> print x
 
 ----------------------------------------------------------------
