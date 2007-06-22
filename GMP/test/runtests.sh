ccf13bcf804ce7dbdec1876662fd8f2759f1bf8d
Georgel Calin <g.calin@jacobs-university.de>
2007-06-22 11:05:48 +0000

    Makefile system changed for tests. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8114 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ GMP/test/runtests.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# testing String-indexed formulae
+for i in *.s
+do
+    echo "  processing Generic f from $i"
+    ../main 0 $i
+done
+# testing K modal logic formulae
+for i in *.k
+do
+    echo "  processing K f from $i" 
+    ../main 1 $i
+done
+# testing KD modal logic formulae
+for i in *.kd
+do
+    echo "  processing KD f from $i"
+    ../main 2 $i
+done
+#testing CL formulae
+for i in *.cl
+do
+    echo "  processing CL f from $i"
+    ../main 3 $i
+done
+# testing GML formulae
+for i in *.g
+do
+    echo "  processing GML f from $i"
+    ../main 4 $i
+done
