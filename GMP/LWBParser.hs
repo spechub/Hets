ad077aad4f2b87ebc63470db2663570b70b2118c
Georgel Calin <g.calin@jacobs-university.de>
2007-07-02 14:54:53 +0000

    Parser for LWB formulae works :D, hence more tests for the GMP to come ... git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8214 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GMP/LWBParser.hs
+++ GMP/LWBParser.hs
@@ -1,10 +1,11 @@
-module LWBParser where
+module Main where
 
+import System.Environment
 import Text.ParserCombinators.Parsec
 import Lexer
 
 lwbjunc :: Parser String
-lwbjunc =  do try(string "&");   whiteSpace; return "/"
+lwbjunc =  do try(string "&");   whiteSpace; return "/\\"
        <|> do try(string "v");   whiteSpace; return "\\/"
        <|> do try(string "->");  whiteSpace; return "->"
        <|> do try(string "<->"); whiteSpace; return "<->"
@@ -16,43 +17,70 @@ inf :: String -> Parser String
 inf f = do iot <- lwbjunc; ff <- lwb2sf; return $ "("++f++iot++ff++")"
 
 prim :: Parser String
-prim =  do try(string "False")
+prim =  do try(string "false")
            whiteSpace
            return "F"
-    <|> do try(string "True")
+    <|> do try(string "true")
            whiteSpace
            return "T"
     <|> do try(string "~")
            whiteSpace
-           return "~"
+           f <- lwb2sf
+           return $ "~"++f
+    <|> do try(string "box(")
+           f <- lwb2sf
+           char ')'
+           return $ "[]"++f
     <|> do try(string "box")
            whiteSpace
-           return "[]"
+           f <- prim
+           return $ "[]"++f
+    <|> do try (string "dia(")
+           f <- lwb2sf
+           char ')'
+           return $ "<>"++f
     <|> do try (string "dia")
            whiteSpace
-           return "<>"
-    <|> do try(char '(')
+           f <- prim
+           return $ "<>"++f
+    <|> do try(string "p")
+           i <- natural
            whiteSpace
+           return $ "p" ++ show i
+    <|> do try(char '(')
            f <- lwb2sf
            char ')'
            whiteSpace
            return f
-    <|> do try(string "p")
-           i <- natural
-           return $ "p" ++ show i
+    <?> "prim"
 
-run :: Show a => Parser a -> String -> IO ()
-run p input
+run :: String -> Parser String -> String -> IO ()
+run path p input
         = case (parse p "" input) of
             Left err -> do putStr "parse error at "
                            print err
-            Right x  -> print x
+            Right x  -> writeFile path x
 
 
-runLex :: Show a => Parser a -> String -> IO ()
-runLex p
-        = run (do whiteSpace
-                  x <- p
-                  eof
-                  return x
-              )
+runLex :: String -> Parser String -> String -> IO ()
+runLex path p
+        = run path (do whiteSpace
+                       x <- p
+                       eof
+                       return x)
+
+help :: IO()
+help = do
+    putStrLn ("Usage:\n" ++
+               "./<exe> <path>\n" ++
+               "<exe>  : executable file\n" ++
+               "<path> : path to file to write into\n")
+main :: IO()
+main = do
+    args <- getArgs
+    if (args==[])||(head args == "--help")
+      then help
+      else do let p = head args
+              putStrLn "Give the input formula:"
+              line <- getLine
+              runLex p lwb2sf line
