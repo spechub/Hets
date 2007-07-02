3487c9758783e796946a47a1e94888b4288880c6
Georgel Calin <g.calin@jacobs-university.de>
2007-07-02 13:42:04 +0000

    LWB parser works git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8211 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GMP/LWBParser.hs
+++ GMP/LWBParser.hs
@@ -1,4 +1,4 @@
-module BParser where
+module LWBParser where
 
 import Text.ParserCombinators.Parsec
 import Lexer
@@ -17,29 +17,29 @@ inf f = do iot <- lwbjunc; ff <- lwb2sf; return $ "("++f++iot++ff++")"
 
 prim :: Parser String
 prim =  do try(string "False")
-             whiteSpace
-             return "F"
-      <|> do try(string "True")
-             whiteSpace
-             return "T"
-      <|> do try(string "~")
-             whiteSpace
-             return "~"
-      <|> do try(string "box")
-             whiteSpace
-             return "[]"
-      <|> do try (string "dia")
-             whiteSpace
-             return "<>"
-      <|> do try(char '(')
-             whiteSpace
-             f <- lwb2sf
-             char ')'
-             whiteSpace
-             return f
-      <|> do try(string "p")
-             i <- natural
-             return $ "p" ++ show i
+           whiteSpace
+           return "F"
+    <|> do try(string "True")
+           whiteSpace
+           return "T"
+    <|> do try(string "~")
+           whiteSpace
+           return "~"
+    <|> do try(string "box")
+           whiteSpace
+           return "[]"
+    <|> do try (string "dia")
+           whiteSpace
+           return "<>"
+    <|> do try(char '(')
+           whiteSpace
+           f <- lwb2sf
+           char ')'
+           whiteSpace
+           return f
+    <|> do try(string "p")
+           i <- natural
+           return $ "p" ++ show i
 
 run :: Show a => Parser a -> String -> IO ()
 run p input
