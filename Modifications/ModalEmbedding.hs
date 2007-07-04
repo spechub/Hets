a06981102ed71198f062c391dd06e7c1770ffdfe
Mihai Codescu <codescu@iws.cs.uni-magdeburg.de>
2007-07-04 13:18:01 +0000

    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8242 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ Modifications/ModalEmbedding.hs
@@ -0,0 +1,57 @@
+{-# OPTIONS -fglasgow-exts -fallow-undecidable-instances #-}
+-- institution modification 
+-- CASL -------------------------id------------------> CASL
+-- CASL ---CASL2Modal----> ModalCASL --Modal2CASL----> CASL
+ 
+module Modifications.ModalEmbedding where
+
+import Logic.Modification
+import Logic.Logic
+import Logic.Comorphism
+
+import Comorphisms.CASL2Modal
+import Comorphisms.Modal2CASL
+
+
+import CASL.Logic_CASL
+import CASL.Sign
+import CASL.Morphism
+import CASL.AS_Basic_CASL
+import CASL.Sublogic as SL
+
+data MODAL_EMBEDDING = MODAL_EMBEDDING deriving Show
+
+instance Language MODAL_EMBEDDING
+
+instance Modification MODAL_EMBEDDING (IdComorphism CASL CASL_Sublogics) (CompComorphism CASL2Modal Modal2CASL) 
+        CASL CASL_Sublogics
+        CASLBasicSpec CASLFORMULA SYMB_ITEMS SYMB_MAP_ITEMS
+        CASLSign
+        CASLMor
+        Symbol RawSymbol ()
+        CASL CASL_Sublogics
+        CASLBasicSpec CASLFORMULA SYMB_ITEMS SYMB_MAP_ITEMS
+        CASLSign
+        CASLMor
+        Symbol RawSymbol ()
+        CASL CASL_Sublogics
+        CASLBasicSpec CASLFORMULA SYMB_ITEMS SYMB_MAP_ITEMS
+        CASLSign
+        CASLMor
+        Symbol RawSymbol ()
+        CASL CASL_Sublogics
+        CASLBasicSpec CASLFORMULA SYMB_ITEMS SYMB_MAP_ITEMS
+        CASLSign
+        CASLMor 
+        Symbol RawSymbol () 
+ 
+
+ where
+  sourceComorphism MODAL_EMBEDDING = mkIdComorphism CASL (top_sublogic CASL)
+  targetComorphism MODAL_EMBEDDING = CompComorphism CASL2Modal Modal2CASL
+  tauSigma MODAL_EMBEDDING sigma = do (sigma1,_ )<- wrapMapTheory CASL2Modal (sigma,[])
+                                      (sigma2,_ )<- wrapMapTheory Modal2CASL (sigma1,[])                                       
+                                      return (embedMorphism dummy sigma sigma2)
+
+
+
