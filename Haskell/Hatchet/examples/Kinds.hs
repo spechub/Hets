8dd33a794b7ab4f0da70b290e424e4fae162b3a4
Christian Maeder <Christian.Maeder@dfki.de>
2007-07-13 08:16:50 +0000

    unused git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8353 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Haskell/Hatchet/examples/Kinds.hs
+++
@@ -1,8 +0,0 @@
-module Kinds where
-
--- this should produce a kind error
-
-data C x = Foo (B x) (x MyType)
-data B y = Bar 
-
-data MyType = MyType
--- Haskell/Hatchet/examples/NegAppAndSection.hs
+++
@@ -1,8 +0,0 @@
--- NegAppAndSection.hs
--- Output: []
-
-main :: [Bool]
-main = map (`elem` [4,5,6]) foo 
-
-foo :: [Int]
-foo = filter ((-3)>) [1..10]
