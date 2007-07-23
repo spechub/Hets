0b12cf4c1326ce391d8129f823af1b23bad2ce7e
Georgel Calin <g.calin@jacobs-university.de>
2007-07-23 10:51:13 +0000

    First implementation of the inequality solver. It might need adjustment. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8445 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GMP/GradedML.hs
+++ GMP/GradedML.hs
@@ -38,78 +38,37 @@ eccContent (Mimplies n p) =
       w = 1 + (length l1) + (length l2) + sum (map size l1) + sum (map size l2)
   in (Coeffs l1 l2, w)
 -------------------------------------------------------------------------------
-{- recursion over the unkowns preceeded by positive coefficients
- - @ param p : positive coefficients
- - @ param y : current instantiation of the unknowns
- - @ param s : current sum (initially of unknowns corresponding to neg coeff)
- - @ param lim : the upper limit for the unknowns
- - @ return : solutions of the "partial" inequality i.e. solutions for the 
- -            unknowns preceeded by pos coeff for a certain instantiation of 
- -            the unknowns preceeded by neg coeff-}
-posCoeff :: [Int] -> [Int] -> Int -> Int -> [[Int]]
-posCoeff p y s lim =
-  let news = s + sum [a*b| a <- p, b <- y]
-      step l c w q =
-        case l of
-          [] -> []
-          h:t -> let (u,v) = (head c, tail c)
-                 in if (size h == w)||(q + u > -1)
-                    then 1:(step t v w (q - (h-1)*u))
-                    else (h + 1):t
-  in if (news > -1)
-     then []
-     else let newy = step y p lim news
-          in y:(posCoeff p newy s lim)
-{- recursion on the unknowns preceeded by negative coefficients
- - @ param n : negative coefficients
- - @ param p : positive coefficients
- - @ param x : current instantiation of the unknowns
- - @ lim : the limit up to which to look for unknowns (2^18W^4)
- - @ return : solutions of the inequality from the "x" solution/instantiation 
- -            on -}
-negCoeff :: [Int] -> [Int] -> [Int] -> Int -> [([Int],[Int])]
-negCoeff n p x lim =
-  let s = sum [a*b| a <- n, b <- x]
-      y = map (\_ -> 1) p
-      step l w = 
-        case l of
-          []  -> []
-          h:t -> if (h == w)
-                 then 1:(step t w)
-                 else (h + 1):t
-      newx = step x lim
-  in if (newx == map (\_ -> 1) n)
-     then []
-     else (map (\w -> (x,w)) (posCoeff p y s lim)) ++ (negCoeff n p newx lim)
--- alternative ...
 {- generate all lists of given length and with elements between 1 and a limit
  - @ param n : fixed length
  - @ param lim : upper limit of elements
  - @ return : a list of all lists described above -}
 negCands :: Int -> Int -> [[Int]]
 negCands n lim =
-  let first = map (\_ -> 1) [1..n]
-      next l x = 
-        case l of
-          []  -> []
-          h:t -> if (h == x) 
-                 then 1 : (next t x)
-                 else (h + 1) : t
-      aux l l0 x = 
-        let nextl = next l x
-        in if (nextl == l0) 
-           then [l]
-           else l : (aux nextl l0 x)
-  in aux first first lim
+  case n of
+    0 -> [[]]
+    _ -> [i:l| i <- [1..lim], l <- negCands (n-1) lim]
+{- generate all lists of given length with elems between 1 and a limit such
+ - that the side condition of Graded ML rule is satisfied
+ - @ param n : fixed length
+ - @ param lim : upper limit of elements
+ - @ param s : sum (negative) which is previously computed
+ - @ param p : positive coefficients
+ - @ return : list of all lists described above -}
+posCands :: Int -> Int -> Int -> [Int] -> [[Int]]
+posCands n lim s p =
+ let doSum l1 l2 =
+       if (l1 == [])||(l2 == [])
+       then 0
+       else (head l1)*(head l2) + doSum (tail l1) (tail l2)
+ in case n of
+      0 -> [[]]
+      _ -> [i:l|i<-[1..lim],l<-posCands (n-1) lim s p,s+doSum p (i:l) < 0]
 {- gives all solutions in (1,lim) of the inequality with coeff n and p
  - @ param (Coeff n p) : negative and positive coefficients
  - @ param lim : limit for solution searching
  - @ return : solutions of the inequality, each stored as a pair -}
 ineqSolver :: Coeffs -> Int -> [([Int],[Int])]
-ineqSolver (Coeffs n p) lim = let x = map (\_ -> 1) n
-                              in negCoeff n p x lim
+ineqSolver (Coeffs n p) lim = 
+  let x = negCands (length n) lim
+  in [(a,b)|a<-x,b<-posCands (length p) lim (sum [u*v|u<-a,v<-n]) p]
 -------------------------------------------------------------------------------
-
-
-negCands 0 lim = []
-negCands (n+1) lim = [i:l| i<-[1..lim], l <-negCand n lim]
