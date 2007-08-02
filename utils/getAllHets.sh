c894e2405a49b865ee9c393b551080b892b9eaeb
Heng Jiang <jiang@tzi.de>
2007-08-02 12:43:51 +0000

    use released hets instead of daliy hets. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8533 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/getAllHets.sh
+++ utils/getAllHets.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-## This script downloads, bunzips daily hets;
-## afterwards it adds excutable permissions for everyone
+## This script downloads, bunzips released hets;
 
 HETS_VER=$1
 HETS_TMP=$HOME/tmp/hets_installer
@@ -10,40 +9,48 @@ HETS_DOWNLOAD=$HETS_TMP/hets
 HETS_LINUX=$HETS_DOWNLOAD/linux/hets-$HETS_VER
 HETS_MAC=$HETS_DOWNLOAD/mac/hets-$HETS_VER
 HETS_SOL=$HETS_DOWNLOAD/solaris/hets-$HETS_VER
+HETS_MACINTEL=$HETS_DOWNLOAD/intel-mac/hets-$HETS_VER
 
-DAILY_HETS_LINUX=$HETS_LINUX.bz2
-DAILY_HETS_MAC=$HETS_MAC.bz2
-DAILY_HETS_SOL=$HETS_SOL.bz2
+RELEASED_HETS_LINUX=$HETS_LINUX.bz2
+RELEASED_HETS_MAC=$HETS_MAC.bz2
+RELEASED_HETS_SOL=$HETS_SOL.bz2
+RELEASED_HETS_INTELMAC=$HETS_MACINTEL.bz2
 
 if [ -d $HETS_DOWNLOAD ] ;
 then rm -fr $HETS_DOWNLOAD/* 2>/dev/null ; 
 fi ;
 
 mkdir -p $HETS_DOWNLOAD/linux ; mkdir $HETS_DOWNLOAD/mac; \
-                                mkdir $HETS_DOWNLOAD/solaris
+           mkdir $HETS_DOWNLOAD/solaris ; mkdir $HETS_DOWNLOAD/intel-mac
 
 ARCH_DIR=""
-DOWNLOADCMD_L="wget --output-document=$DAILY_HETS_LINUX"
-DOWNLOADCMD_M="wget --output-document=$DAILY_HETS_MAC"
-DOWNLOADCMD_S="wget --output-document=$DAILY_HETS_SOL"
+DOWNLOADCMD_L="wget --output-document=$RELEASED_HETS_LINUX"
+DOWNLOADCMD_M="wget --output-document=$RELEASED_HETS_MAC"
+DOWNLOADCMD_S="wget --output-document=$RELEASED_HETS_SOL"
+DOWNLOADCMD_IM="wget --output-document=$RELEASED_HETS_INTELMAC"
 
-DAILY_HETS_URL_LINUX="http://www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets/linux/releasedhets.bz2"
-DAILY_HETS_URL_MAC="http://www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets/mac/releasedhets.bz2"
-DAILY_HETS_URL_SOLARIS="http://www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets/solaris/releasedhets.bz2"
+
+RELEASED_HETS_URL_LINUX="http://www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets/linux/releasedhets.bz2"
+RELEASED_HETS_URL_MAC="http://www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets/mac/releasedhets.bz2"
+RELEASED_HETS_URL_SOLARIS="http://www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets/solaris/releasedhets.bz2"
+RELEASED_HETS_URL_INTELMAC="http://www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets/intel-mac/releasedhets.bz2"
 
 # main
-$DOWNLOADCMD_L $DAILY_HETS_URL_LINUX
+$DOWNLOADCMD_L $RELEASED_HETS_URL_LINUX
 #bunzip2 $DAILY_HETS_LINUX
 #chmod a+x $HETS_LINUX 
 
-$DOWNLOADCMD_M $DAILY_HETS_URL_MAC
+$DOWNLOADCMD_M $RELEASED_HETS_URL_MAC
 #bunzip2 $DAILY_HETS_MAC
 #chmod a+x $HETS_MAC 
 
-$DOWNLOADCMD_S $DAILY_HETS_URL_SOLARIS
+$DOWNLOADCMD_S $RELEASED_HETS_URL_SOLARIS
 #bunzip2 $DAILY_HETS_SOL
 #chmod a+x $HETS_SOL
 
-echo "Downloaded and saved daily hets to: " $DAILY_HETS_LINUX
-echo "Downloaded and saved daily hets to: " $DAILY_HETS_MAC
-echo "Downloaded and saved daily hets to: " $DAILY_HETS_SOL
+$DOWNLOADCMD_S $RELEASED_HETS_URL_INTELMAC
+
+echo "Downloaded and saved released hets to: " $RELEASED_HETS_LINUX
+echo "Downloaded and saved released hets to: " $RELEASED_HETS_MAC
+echo "Downloaded and saved released hets to: " $RELEASED_HETS_SOL
+echo "Downloaded and saved released hets to: " $RELEASED_HETS_INTELMAC
