7cfcecda7cc66d80e1acc2c5687b0a5574bca1c5
Christian Maeder <Christian.Maeder@dfki.de>
2007-08-07 12:47:46 +0000

    remove auxiliary function git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8547 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GUI/ShowGraph.hs
+++ GUI/ShowGraph.hs
@@ -8,7 +8,6 @@ Stability   :  provisional
 Portability :  non-portable (Logic)
 
 display the final graph
-
 -}
 
 module GUI.ShowGraph
@@ -16,9 +15,7 @@ module GUI.ShowGraph
 where
 
 import Driver.Options
-
 import Syntax.AS_Library(LIB_NAME)
-
 import Static.DevGraph
 
 import GUI.GraphDisplay
@@ -30,27 +27,23 @@ import Events
 import Destructible
 import DialogWin (useHTk)
 
-
 showGraph :: FilePath -> HetcatsOpts ->
              Maybe (LIB_NAME, -- filename
                     LibEnv    -- DGraphs for imported modules
                    )  -> IO ()
 showGraph file opts env = case env of
-  Just (ln, le) ->
-    showGraphAux file opts ln le -- "development graph"
+  Just (ln, le) -> do
+    putIfVerbose opts 2 $ "Trying to display " ++ file
+                     ++ " in a graphical window"
+    putIfVerbose opts 3 "Initializing Converter"
+    (gInfo,wishInst) <- initializeConverter
+    useHTk -- All messages are displayed in TK dialog windows
+    -- from this point on
+    gInfo' <- setGInfo gInfo ln le opts
+    graph <- showLibGraph gInfo'
+    mShowGraph gInfo' ln
+    sync(destroyed graph)
+    destroy wishInst
+    InfoBus.shutdown
   Nothing -> putIfVerbose opts 1 $ "Error: Basic Analysis is neccessary "
     ++ "to display graphs in a graphical window"
-
-showGraphAux :: FilePath -> HetcatsOpts -> LIB_NAME -> LibEnv -> IO ()
-showGraphAux file opts ln le = do
-  putIfVerbose opts 2 $ "Trying to display "++file++" in a graphical window"
-  putIfVerbose opts 3 "Initializing Converter"
-  (gInfo,wishInst) <- initializeConverter
-  useHTk -- All messages are displayed in TK dialog windows
-  -- from this point on
-  gInfo' <- setGInfo gInfo ln le opts          
-  graph <- showLibGraph gInfo'
-  mShowGraph gInfo' ln
-  sync(destroyed graph)
-  destroy wishInst
-  InfoBus.shutdown
