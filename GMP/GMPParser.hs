4eb2574f464f85c4514ac3a5dae9dac4855ae9ba
Georgel Calin <g.calin@jacobs-university.de>
2007-08-27 08:59:44 +0000

    Parser modified so that it supports binding order but needs debugging. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8711 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GMP/GMPParser.hs
+++ GMP/GMPParser.hs
@@ -2,117 +2,116 @@
 -- the Generic Model Parser Abstract Syntax
 -- Copyright 2007, Lutz Schroeder and Georgel Calin
 -------------------------------------------------------------------------------
-
 module GMP.GMPParser where
 
-import Text.ParserCombinators.Parsec
-import GMP.Lexer
 import GMP.GMPAS
-{- tentative of making binding order work
-import Text.ParserCombinators.Parsec.Expr
+import GMP.Lexer
+import Text.ParserCombinators.Parsec
+--import Text.ParserCombinators.Parsec.Expr
+--import Text.ParserCombinators.Parsec.Token
+--import Text.ParserCombinators.Parsec.Language
 
-fparser pa =
-  let table = [[Prefix negParser]
-              ,[Infix andParser AssocLeft]
-              ,[Infix orParser AssocLeft]
-              ,[Infix ifParser AssocLeft, Infix fiParser AssocRight]
-              ,[Infix iffParser AssocLeft]
-              ,[Prefix angParser, Prefix sqrParser]]
-      negParser = do parens (fparser pa)
-                     string "~"
---                     f <- fparser pa
-                     return $ Neg --f
-      andParser = do parens (fparser pa)
-                     string "/\\" 
-                     return And
-      orParser  = do parens (fparser pa)
-                     string "\\/" 
-                     return Or
-      ifParser  = do parens (fparser pa)
-                     string "->"  
-                     return If
-      fiParser  = do parens (fparser pa)
-                     string "<-"  
-                     return Fi
-      iffParser = do parens (fparser pa)
-                     string "<->" 
-                     return Iff
-      angParser = do parens (fparser pa)
-                     char '<'
+par5er :: GenParser Char st a -> GenParser Char st (Formula a)
+par5er pa = do f <- primFormula pa; whiteSpace; option (f) ({-iff-}implFormula pa f)
+
+parenFormula :: GenParser Char st a -> GenParser Char st (Formula a)
+parenFormula pa =  do try (char '(')
+                      whiteSpace
+                      f <- par5er pa
+                      whiteSpace
+                      char ')'
+                      whiteSpace
+                      return f
+               <?> "GMPParser.parenFormula"
+
+primFormula :: GenParser Char st a -> GenParser Char st (Formula a)
+primFormula pa =  do try (string "T")
+                     whiteSpace
+                     return T
+              <|> do try (string "F")
+                     whiteSpace
+                     return F
+              <|> do f <- parenFormula pa
+                     return f
+              <|> do try (string "~")
+                     whiteSpace
+                     f <- primFormula pa
+                     return $ Neg f
+              <|> do try (char '<')
+                     whiteSpace
                      i <- pa
+                     whiteSpace
                      char '>'
-                     return $ Mapp (Mop i Angle)
-      sqrParser = do parens (fparser pa)
-                     char '['
+                     whiteSpace
+                     f <- primFormula pa
+                     return $ Mapp (Mop i Angle) f
+              <|> do try (char '[')
+                     whiteSpace
                      i <- pa
+                     whiteSpace
                      char ']'
-                     return $ Mapp (Mop i Square)
-  in buildExpressionParser table pa
+                     whiteSpace
+                     f <- primFormula pa
+                     return $ Mapp (Mop i Square) f
+              <?> "GMPParser.primFormula"
 
+andFormula :: GenParser Char st a -> Formula a -> GenParser Char st (Formula a)
+andFormula pa f =  do try (string "/\\")
+                      whiteSpace
+                      g <- primFormula pa
+                      h <- andFormula pa g
+                      whiteSpace
+                      return $ Junctor f And h
+               <|> do try (string "/\\")
+                      whiteSpace
+                      g <- primFormula pa
+                      h <- andFormula pa g
+                      whiteSpace
+                      return $ Junctor f And h
+               <|> do return f
+               <?> "GMPParser.andFormula"
+
+orFormula :: GenParser Char st a -> Formula a -> GenParser Char st (Formula a)
+orFormula pa f =  do try (string "\\/")
+                     whiteSpace
+                     g <- primFormula pa
+                     h <- andFormula pa g
+                     return $ Junctor f Or h
+              <|> do return f
+              <?> "GMPParser.orFormula"
+
+implFormula :: GenParser Char st a -> Formula a -> GenParser Char st (Formula a)
+implFormula pa f =  do try (string "->")
+                       whiteSpace
+                       g <- primFormula pa
+                       h <- andFormula pa g
+                       i <- orFormula pa h
+                       return $ Junctor f If i
+                <|> do try (string "<-")
+                       whiteSpace
+                       g <- primFormula pa
+                       h <- andFormula pa g
+                       i <- orFormula pa h
+                       return $ Junctor f Fi i
+                <|> do try (string "<->")
+                       whiteSpace
+                       g <- primFormula pa
+                       h <- andFormula pa g
+                       i <- orFormula pa h
+                       return $ Junctor f Iff i
+                <|> do return f
+                <?> "GMPParser.implFormula"
+{-
+iffFormula :: GenParser Char st a -> Formula a -> GenParser Char st (Formula a)
+iffFormula pa f =  do try (string "<->")
+                      whiteSpace
+                      g <- primFormula pa
+                      h <- andFormula pa g
+                      i <- orFormula pa h
+                      j <- implFormula pa i
+                      return $ Junctor f Iff j
+               <|> return f
+               <?> "GMPParser.iffFormula"
 -}
--------------------------------------------------------------------------------
--- Parser for polymorphic (Formula a) Type
--------------------------------------------------------------------------------
-par5er :: Parser a -> Parser (Formula a)                         -- main parser
-par5er pa = do f <- prim pa; option (f) (inf pa f)
-         <?> "GMPParser.par5er"
-
-junc :: Parser Junctor                                        -- junctor parser
-junc =  do try(string "/\\"); whiteSpace; return And
-    <|> do try(string "\\/"); whiteSpace; return Or
-    <|> do try(string "->");  whiteSpace; return If
-    <|> do try(string "<->"); whiteSpace; return Iff
-    <|> do try(string "<-");  whiteSpace; return Fi
-    <?> "GMPParser.junc"
-
-inf :: Parser a -> (Formula a) -> Parser (Formula a)            -- infix parser
-inf pa f1 =
-    do iot <- junc; f2 <-par5er pa; return $ Junctor f1 iot f2
-    <?> "GMPParser.inf"
-
-varp :: CharParser st Char                               -- lower letter parser
-varp = let isAsciiLower c = c >= 'a' && c <= 'z'
-       in satisfy isAsciiLower
-    
-prim :: Parser a -> Parser (Formula a)                      -- primitive parser
-prim pa = 
-        do try(string "F")
-           whiteSpace
-           return F
-    <|> do try(string "T")
-           whiteSpace
-           return T
-    <|> do try(string "~")
-           whiteSpace
-           f <- par5er pa
-           return $ Neg f
-    <|> do try(char '(')
-           whiteSpace
-           f <- par5er pa
-           whiteSpace
-           char ')'
-           whiteSpace
-           return f
-    <|> do try(char '[')
-           whiteSpace
-           i <- pa
-           whiteSpace
-           char ']'
-           whiteSpace
-           f <-par5er pa
-           return $ Mapp (Mop i Square) f
-    <|> do try(char '<')
-           whiteSpace
-           i <- pa
-           whiteSpace
-           char '>'
-           whiteSpace
-           f <- par5er pa
-           return $ Mapp (Mop i Angle) f
-    <|> do v <- varp
-           i <- natural
-           whiteSpace
-           return $ Var v i
-    <?> "GMPParser.prim"
 -------------------------------------------------------------------------------
 -------------------------------------------------------------------------------
