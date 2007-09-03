f2e3a08e1f838140e2c533d22e178837934a230d
Christian Maeder <Christian.Maeder@dfki.de>
2007-09-03 12:50:04 +0000

    parser is able to reparse Basic/*.dfg files git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8782 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ SoftFOL/dfg.hs
@@ -0,0 +1,31 @@
+{- |
+Module      :  $Id$
+Description :  a standalone dfg parser
+Copyright   :  (c) C. Maeder and Uni Bremen 2007
+License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
+
+Maintainer  :  Christian.Maeder@dfki.de
+Stability   :  provisional
+Portability :  portable
+
+parse dfg files
+-}
+
+module Main where
+
+import System.Environment
+import Text.ParserCombinators.Parsec
+import SoftFOL.DFGParser
+import SoftFOL.Print ()
+import Common.DocUtils
+
+main :: IO ()
+main = getArgs >>= mapM_ process
+
+process :: String -> IO ()
+process f = do
+  s <- readFile f
+  case parse parseSPASS f s of
+    Right b -> writeFile (f ++ ".dfg") $ shows (pretty b) "\n"
+    Left err -> print err
+
