cc4a7869d78fc46c64b6d94534474486397f4c2d
Christian Maeder <Christian.Maeder@dfki.de>
2007-09-03 10:43:41 +0000

    listed current modules git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8779 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GUI.hs
+++ GUI.hs
@@ -1,4 +1,14 @@
-{- | Graphical user interface for Hets. The GUI is based on the
+{- |
+Module      :  $Id$
+Description :  folder description
+Copyright   :  (c) Uni Bremen 2005-2007
+License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
+
+Maintainer  :  raider@informatik.uni-bremen.de
+Stability   :  provisional
+Portability :  non-portable (uses uni and development graphs)
+
+Graphical user interface for Hets. The GUI is based on the
 UniForM Workbench <http://www.informatik.uni-bremen.de/uniform/wb>.
 The UniForm Workbench provides an event system and encapsulations of
 TclTk <http://www.informatik.uni-bremen.de/htk/> and uDraw(Graph)
@@ -9,34 +19,43 @@ module "GraphDisp").
 encapsulation of uDraw(Graph). Provides additional functions
 for hiding and redisplaying (groups of) nodes and edges.
 
-"GUI.ConvertDevToAbstractGraph"
-displays development graphs, using abstract graph interface.
+"GUI.ConsistencyChecker"
 
-"GUI.GenericATP" is a generic graphical interface for automatic theorem provers.
+"GUI.ConsoleUtils" are similar utilities for using without "HTk" (only
+console).
+
+"GUI.DGTranslations"
+
+"GUI.GenericATP" is a generic graphical interface for automatic
+theorem provers.
 
 "GUI.GenericATPState" provides data structures and initialising functions for
 prover state and configurations.
 
-"GUI.ProofManagement" is a goal management GUI for the structured level.
+"GUI.GraphDisplay"
+
+"GUI.GraphLogic"
+
+"GUI.GraphMenu"
+
+"GUI.GraphTypes"
+
+"GUI.HTkUtils" provides some utilities on top of "HTk".
+
+"GUI.PrintUtils" are pretty printing functions used by "GUI.GenericATP".
 
 "GUI.ProofDetails" sets an additional window used by "GUI.ProofManagement" for
 displaying and saving proof details (prover output, tactic script, proof tree).
 
-"GUI.HTkUtils" provides some utilities on top of "HTk".
-
-"GUI.ConsoleUtils" are similar utilities for using without "HTk" (only console).
-
-"GUI.PrintUtils" are pretty printing functions used by "GUI.GenericATP".
-
-"GUI.Utils" are either "GUI.HTkUtils" or "GUI.ConsoleUtils".
+"GUI.ProofManagement" is a goal management GUI for the structured level.
 
 "GUI.ShowGraph" displays the final graph.
 
 "GUI.ShowLogicGraph" displays the logic graph.
 
-"GUI.DGTranslations" displays the logic graph.
-
 "GUI.Taxonomy" displays a subsort relation (taxonomy).
+
+"GUI.Utils" are either "GUI.HTkUtils" or "GUI.ConsoleUtils".
 -}
 
 module GUI where
