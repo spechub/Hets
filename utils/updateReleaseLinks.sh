437c137e15db3bf69a24b8a47423f6e545cda738
Christian Maeder <Christian.Maeder@dfki.de>
2007-09-07 15:48:31 +0000

    script to set all release links git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8812 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ utils/updateReleaseLinks.sh
@@ -0,0 +1,17 @@
+#!/bin/sh -x
+
+#link to a new release version
+#the link type (2nd arg) is either "pre" or "released"  
+
+INSTALLDIR=/home/www/agbkb/forschung/formal_methods/CoFI/hets 
+VERSION=$1
+LINKTYPE=$2
+for i in linux solaris mac pc-solaris intel-mac
+do
+  (cd $INSTALLDIR/$i; f=${LINKTYPE}hets.bz2; rm $f; \
+   ln -s versions/hets-$VERSION.bz2 $f)
+done
+      
+(cd $INSTALLDIR/src-distribution; \
+ f=${LINKTYPE}Hets-src.tgz; rm $f; 
+ ln -s versions/Hets-src-$VERSION.tgz $f)
