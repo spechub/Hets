669f3d93f3cce89c6a9ec0a33aa406e1f1f901a2
Christian Maeder <Christian.Maeder@dfki.de>
2007-09-10 13:29:22 +0000

    updated git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8825 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Comorphisms.hs
+++ Comorphisms.hs
@@ -1,9 +1,17 @@
 {- |
+Module      :  $Id$
+Description :  folder description
+Copyright   :  (c) Uni Bremen 2005-2007
+License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
+
+Maintainer  :  Christian.Maeder@dfki.de
+Stability   :  provisional
+Portability :  non-portable (existential types)
 
 This folder contains various comorphisms (implemented using
 the type class 'Logic.Comorphism.Comorphism'), which are then
 collected to a logic graph in "Comorphisms.LogicGraph".
-The latter is based on the list of logics collected in  
+The latter is based on the list of logics collected in
 "Comorphisms.LogicList".
 
 The individual comorphisms are on the one hand trivial embeddings:
@@ -16,34 +24,40 @@ The individual comorphisms are on the one hand trivial embeddings:
 
 "Comorphisms.CASL2Modal"
 
-On the ohter hand, there are a number of real encodings:
+"Comorphisms.Prop2CASL"
 
+
+On the other hand, there are a number of real encodings:
 
 "Comorphisms.CASL2PCFOL", "Comorphisms.CASL2TopSort"  encodings of subsorting
 
-"Comorphisms.PCFOL2FOL" encoding of partiality
+"Comorphisms.CASL2SubCFOL" encoding of partiality
+
+"Comorphisms.Sule2SoftFOL" translating a CASL subset to SoftFOL
 
 "Comorphisms.Modal2CASL" encoding of Kripke worlds
 
 
-"Comorphisms.HasCASL2Haskell" translation of executable HasCASL subset to Haskell
+"Comorphisms.HasCASL2Haskell"
+  translation of executable HasCASL subset to Haskell
 
+"Comorphisms.CspCASL2Modal"
+  unfinished coding of CSP-CASL LTS semantics as Kripke models
 
-"Comorphisms.CspCASL2Modal" unfinished coding of CSP-CASL LTS semantics as Kripke models
-
-"Comorphisms.HasCASL2HasCASL" unfinished mapping of HasCASL subset to HsCASL program blocks
+"Comorphisms.HasCASL2HasCASL"
+  unfinished mapping of HasCASL subset to HasCASL program blocks
 
 
 Finally, encodings to the theorem prover Isabelle:
 
-"Comorphisms.CASL2IsabelleHOL"
+"Comorphisms.CFOL2IsabelleHOL"
 
-"Comorphisms.CoCASL2IsabelleHOL"
+"Comorphisms.CoCFOL2IsabelleHOL"
 
-"Comorphisms.HasCASL2IsabelleHOL"
+"Comorphisms.PCoClTyCons2IsabelleHOL"
+ unfinished translation of HasCASL subset to Isabelle
 
 "Comorphisms.Haskell2IsabelleHOLCF"
-
 -}
 
 module Comorphisms where
