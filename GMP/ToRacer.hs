87b08d7a93d17077e9c90cbb0643ca1baa6cd47e
Georgel Calin <g.calin@jacobs-university.de>
2007-09-14 12:27:15 +0000

    Idea for implementation Racer/Formula equivalence formulae revised git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8852 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GMP/ToRacer.hs
+++ GMP/ToRacer.hs
@@ -1,57 +1,102 @@
 module Main where
 
-import GMP.GMPParser
-import GMP.GradedML
+--import GMP.GMPParser
+--import GMP.GradedML
+import GMP.GMPAS
 import GMP.Lexer
 import Text.ParserCombinators.Parsec
 import System.Environment
-import IO
+import qualified IO as IO
 
-{--
-runLex :: (Ord a, Show a, ModalLogic a b) => String -> Parser (Formula a) -> String -> IO ()
-runLex path p input = run (do
-    whiteSpace
-    ; x <- p
-    ; eof
-    ; return x
-    ) input
---}
-run :: (Ord a, Show a, ModalLogic a b) => String -> Parser (Formula a) -> String -> IO ()
-run path p input
-        = case (parse p "" input) of
-                Left err -> do putStr "parse error at "
-                               print err
-                Right x ->  do return x
--- run ::
-run path p f
-        = case (parse p "" input) of
-                Left err -> putStr "parse error at "
-                            print err
-                Right x -> do writeFile path (toString x)
+data RFormula = N RFormula            -- negation
+              | A RFormula RFormula   -- logical and
+              | O  RFormula RFormula  -- logical or
+              | I RFormula RFormula   -- implication
+              | AL Int RFormula       -- at least
+              | AM Int RFormula       -- at most
+              | V Char                -- variables
+    deriving (Eq, Ord)
+
+instance Show RFormula where
+    show f = case f of
+        I g h   -> "(concept-subsumes?\n" ++ show g ++ "\n" ++ show h ++ ")"
+        N g     -> "(not " ++ show g ++ ")"
+        A g h   -> "(and " ++ show g ++ " " ++ show h ++ ")"
+        O g h   -> "(or " ++ show g ++ " " ++ show h ++ ")"
+        AL i g  -> "\nat-least " ++ show i ++ " R " ++ show g
+        AM i g  -> "\nat-most " ++ show i ++ " R " ++ show g
+        V c     -> [c]
+
+varP :: CharParser st Char
+varP = let isAsciiLower c = c >= 'a' && c <= 'z'
+       in satisfy isAsciiLower
+
+rfParser :: forall st. GenParser Char st RFormula
+rfParser =  do try (char '~')
+               whiteSpace
+               f <- rfParser
+               return $ N f
+        <|> do try (char '&')
+               whiteSpace
+               f <- rfParser
+               whiteSpace
+               g <- rfParser
+               return $ A f g
+        <|> do try (char '|')
+               whiteSpace
+               f <- rfParser
+               whiteSpace
+               g <- rfParser
+               return $ O f g
+        <|> do try (string "al")
+               whiteSpace
+               n <- natural
+               whiteSpace
+               f <- rfParser
+               return $ AL (fromInteger n) f
+        <|> do try (string "am")
+               whiteSpace
+               n <- natural
+               whiteSpace
+               f <- rfParser
+               return $ AM (fromInteger n) f
+        <|> do c <- varP
+               return $ V c
+        <?> "ToRacer.rfParser"
+
+toFormula :: RFormula -> Formula GML
+toFormula f =
+    case f of
+        N g    -> Neg (toFormula g)
+        A g h  -> Junctor (toFormula g) And (toFormula h)
+        O g h  -> Junctor (toFormula g) Or (toFormula h)
+        I g h  -> Junctor (toFormula g) If (toFormula h)
+        V c    -> Var c Nothing
+        AL i g -> Mapp (Mop (GML i) Square) (toFormula g)
+        AM i g -> let aux = toFormula g
+                  in Junctor (Mapp (Mop (GML (i-1)) Angle) aux) And (Neg (Mapp (Mop (GML i) Angle) aux))
 
 help :: IO()
 help = do
     putStrLn ("Usage:\n" ++
-               "./<exe> <patho> <pathi>\n" ++
-               "<exe>  : executable file\n" ++
-               "<patho> : path to file to write into\n" ++
-               "<pathi> : path to file to read from\n")
--- runGMP ::
-runGMP p input = 
-    run ( do whiteSpace
-             x <- p
-             eof
-             return x ) input
+               "./<exe> <pathi> <pathof> <pathor>\n" ++
+               "<exe>    : executable file\n" ++
+               "<pathof> : path to file to write formula into\n" ++
+               "<pathor> : path to file to write racer formula into\n" ++
+               "<pathi>  : path to file to read from\n")
 
-runLex po f p =
-    run 
 main :: IO()
 main = do
     args <- getArgs
-    if (args==[])||(head args == "--help")||(length args < 2)
+    if (args==[])||(head args == "--help")||(length args < 3)
       then help
-      else do let po = head args
-                  pi = head (tail args)
-              input <- readFile pi
-              f <- runGMP ((par5er parseIndex) :: Parser (Formula GML)) input
-              runLex po f toRparse
+      else do let pi = head args
+                  aux = tail args
+                  pf = head aux
+                  pr = head (tail aux)
+              input <- IO.readFile pi
+              print input
+--              IO.writeFile pf (show (toFormula input))
+--              IO.writeFile pr (show input)
+--              f <- runGMP ((par5er parseIndex) :: Parser (Formula GML)) input
+--              runLex po f toRparse
