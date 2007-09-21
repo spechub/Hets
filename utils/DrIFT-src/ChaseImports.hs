cbb8f80ee5707c9832d005b7dc46094c60169a0e
Christian Maeder <Christian.Maeder@dfki.de>
2007-09-21 12:51:44 +0000

    silently ignore imported files not found git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8888 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/DrIFT-src/ChaseImports.hs
+++ utils/DrIFT-src/ChaseImports.hs
@@ -1,17 +1,17 @@
 {- this module coordinates the whole shebang.
-  Parses input and plucks out data and newtype declarations and 
+  Parses input and plucks out data and newtype declarations and
   processor commands.
-  The commands are combined with the parsed data, and if any data is missing, 
+  The commands are combined with the parsed data, and if any data is missing,
   derive goes hunting for it, looking in
-  the path variable DERIVEPATH. Derive searches recusively though modules 
-  imported until all the types needed are found, or it runs out of links, 
+  the path variable DERIVEPATH. Derive searches recusively though modules
+  imported until all the types needed are found, or it runs out of links,
   which causes an error -}
-  
+
 module ChaseImports where
 
 import RuleUtils (Tag)
 import DataP
-import CommandP 
+import CommandP
 import ParseLib2
 import System
 import List
@@ -19,22 +19,24 @@ import Monad
 import GenUtil
 
 --GHC version
+try :: IO a -> IO (Either IOError a)
 try x = catch (x >>= return . Right) (return . Left)
 
--- Parser - extract data and newtypes from code  
+-- Parser - extract data and newtypes from code
 
-type ToDo = [([Tag],Data)]
+type ToDo = [([Tag], Data)]
 
 parser :: String -> ToDo
-parser = sanitycheck . papply p (0,0) . \s -> ((0,0),s)
-        where    
-       p = parse . skipUntilOff $ statement +++ command
-       statement = do d <- datadecl +++ newtypedecl
-                      ts <- opt local
-                      return (ts,d)
-       sanitycheck [] = error "***Error: no DrIFT directives found\n"
-       sanitycheck [(x,_)] = x
-       sanitycheck ((x,_):_) = error "***Error: ambiguous DriFT directives?"
+parser = sanitycheck . papply p (0, 0) . \ s -> ((0, 0), s)
+        where
+       p = parse . skipUntilOff $ statemnt +++ command
+       statemnt = do
+         d <- datadecl +++ newtypedecl
+         ts <- opt local
+         return (ts, d)
+       sanitycheck l = case l of
+         [(x, _)] -> x
+         _ -> error "***Error: no or ambiguous DriFT directives"
 
 -------Go Hunting for files, recursively ----------------------------------
 
@@ -44,70 +46,64 @@ chaseImports txt dats = do
         if (not . null) left then error ("can't find type " ++ show left)
          else return found
 
-chaseImports' :: String -> ToDo ->  IO (ToDo,ToDo)
-chaseImports' text dats = 
-  case papply (parse header) (0,-1) ((0,0),text) of
-        [] -> return (dats,[])
-        (modnames:_) -> foldM action (dats,[]) (fst modnames)
+chaseImports' :: String -> ToDo ->  IO (ToDo, ToDo)
+chaseImports' text indats =
+  case papply (parse header) (0, -1) ((0, 0), text) of
+        [] -> return (indats, [])
+        (modnames:_) -> foldM action (indats, []) (fst modnames)
     where
-        action :: (ToDo,ToDo) -> FilePath -> IO (ToDo,ToDo)     
-        action (dats,done) m | null dats = return ([],done)
-                             | otherwise = do 
+        action :: (ToDo, ToDo) -> FilePath -> IO (ToDo, ToDo)
+        action (dats, done) m = if null dats then return ([], done) else do
              mp <- ioM $ getEnv "DERIVEPATH"
-             let paths = maybe [] breakPaths mp 
-             c <- findModule paths m
-             let (found,rest) = scanModule m dats c
-             return (rest, done ++ found) -- finished
+             let paths = maybe [] breakPaths mp
+             mc <- findModule paths m
+             return $ case mc of
+               Nothing -> (dats, done)
+               Just c -> let (found, rest) = scanModule m dats c in
+                 (rest, done ++ found)
              -- only one level of imports is now supported for more speed!
-                        
--- break DERIVEPATH into it's components                        
-breakPaths :: String -> [String]
-breakPaths x = case break (==':') x of
-        (p,(_:pp)) -> p: breakPaths pp
-        (p,[]) -> [p]
 
--- search though paths, using try
-findModule :: [String] -> String -> IO String
-findModule paths modname = let
-        action p = try $ do 
-                            h <- readFile p 
-                            return (h,p)
+-- break DERIVEPATH into it's components
+breakPaths :: String -> [String]
+breakPaths x = case break (== ':') x of
+        (p, _ : pp) -> p : breakPaths pp
+        (p, []) -> [p]
+
+-- search through paths, using try
+findModule :: [String] -> String -> IO (Maybe String)
+findModule paths modname = do
+    let action p = try $ readFile p
         fnames = combine paths hiracle_modname
-        hiracle_modname = map (\x -> if x == '.' then '/' else x) modname
+        hiracle_modname = map (\ x -> if x == '.' then '/' else x) modname
         isLeft (Left _ ) = True
         isLeft _ = False
-     in do
-        hh <- mapM action fnames
-        let (h,p) = case dropWhile (isLeft) hh of
-                   ((Right h):_) -> h
-                   _ -> error ("can't find module " ++ modname)
-        return h
+    hh <- mapM action fnames
+    case dropWhile (isLeft) hh of
+              Right h : _ -> return $ Just h
+              _ -> return Nothing
 
 -- generate filepaths by combining module names with different suffixes.
 -- Note : Dedicated Hugs-only users may wish to remove ".hi" from the list of
--- file types to search.  
-combine :: [String] -> String -> [FilePath]     
-combine paths modname = [p++'/':f| f <- toFile modname, p <- ("." :paths)]
-        where
-             toFile :: String -> [String]
-             toFile l = [l++".hs",l++".lhs",l++".hi"] 
+-- file types to search.
+combine :: [String] -> String -> [FilePath]
+combine paths modname = [p ++ '/' : modname ++ ".hs" | p <- "." : paths]
 
 -- pluck out the bits of interest
-scanModule :: String -> ToDo -> String -> (ToDo,ToDo)
-scanModule modname dats txt = let 
-        newDats = filter isData . parse $ txt
-        parse l = map snd . parser $ l
-        in (resolve modname newDats dats ([],[]))
+scanModule :: String -> ToDo -> String -> (ToDo, ToDo)
+scanModule modname dats txt = let
+        newDats = filter isData . myparse $ txt
+        myparse l = map snd . parser $ l
+        in resolve modname newDats dats ([], [])
 
 -- update what's still missing
 resolve :: String -> [Data] -> ToDo -> (ToDo,ToDo) -> (ToDo,ToDo)
 resolve _ _ [] acc = acc
-resolve modname parsed (tv:tt) p@(local, imports) = 
+resolve modname parsed (tv:tt) p@(local, imports) =
     case tv of
     (tags, TypeName t) ->
-        case filter ( \ d -> name d == t || 
+        case filter ( \ d -> name d == t ||
                              modname ++ "." ++ name d == t) parsed of
-          [x] -> resolve modname parsed tt 
+          [x] -> resolve modname parsed tt
                  ( (tags, x { name = modname ++ "." ++ name x }) : local
                  , imports)
           _ -> resolve modname parsed tt (local, tv : imports)
