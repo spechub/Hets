4a647326d6083cd2e4a88f2c9f2be639e14530dc
Christian Maeder <Christian.Maeder@dfki.de>
2007-09-25 14:25:01 +0000

    added testing for generated axioms and put all conditional equations into the simplifier without loop-check git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8905 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Isabelle/MarkSimp.hs
+++ Isabelle/MarkSimp.hs
@@ -19,9 +19,19 @@ import Isabelle.IsaConsts
 
 import Common.AS_Annotation
 
+import Data.List (isPrefixOf)
+
 markSimp :: Named Sentence -> Named Sentence
-markSimp s = if isDef s then s else
-             mapNamed (markSimpSen isSimpRuleSen) s
+markSimp s = let prefixIsin = any (flip isPrefixOf $ senAttr s) in
+  if isDef s || prefixIsin excludePrefixes
+    then s else mapNamed
+    (markSimpSen $ \ ns -> isSimpRuleSen ns || prefixIsin includePrefixes) s
+
+excludePrefixes :: [String]
+excludePrefixes = ["ga_transitive"]
+
+includePrefixes :: [String]
+includePrefixes = ["ga_predicate_monotonicity", "ga_function_monotonicity"]
 
 markSimpSen :: (Sentence -> Bool) -> Sentence -> Sentence
 markSimpSen f s = case s of
@@ -31,31 +41,33 @@ markSimpSen f s = case s of
 isSimpRuleSen :: Sentence -> Bool
 isSimpRuleSen sen = case sen of
     RecDef {} -> False
-    _ -> isSimpRule $ senTerm sen
+    _ -> isCondEq $ senTerm sen
 
--- | test whether a formula should be put into the simpset
-isSimpRule :: Term -> Bool
--- only universal quantifications
-isSimpRule trm = case trm of
-    App (Const q _) arg@Abs{} _
-        | new q == exS || new q == ex1S -> False
-        | new q == allS  -> isSimpRule (termId arg)
+isSimplAppl :: Term -> Bool
+isSimplAppl trm = case trm of
+    Free {} -> True
+    Const q _ -> not $ elem (new q)
+      [allS, exS, ex1S, eq, impl, disj, conj, cNot]
+    App f a _ -> isSimplAppl f && isSimplAppl a
+    Tuplex ts _ -> all isSimplAppl ts
+    _ -> False
+
+isEqOrSimplAppl :: Term -> Bool
+isEqOrSimplAppl trm = case trm of
+    App (App (Const q _) a1 _) a2 _ | new q == eq ->
+        isSimplAppl a1 && isSimplAppl a2
+    _ -> isSimplAppl trm
+
+isEqOrNeg :: Term -> Bool
+isEqOrNeg trm = case trm of
+    App (Const q _) arg _  | new q == cNot -> isEqOrNeg arg
+    _ -> isEqOrSimplAppl trm
+
+isCondEq :: Term -> Bool
+isCondEq  trm = case trm of
+    App (Const q _) arg@Abs{} _ | new q == allS  -> isCondEq (termId arg)
     App (App (Const q _) a1 _) a2 _
-        | new q == eq -> sizeOfTerm a1 > sizeOfTerm a2
-        | new q == impl -> sizeOfTerm a1 < sizeOfTerm a2
-    _ -> True
+        | new q == impl -> isEqOrNeg a1 && isCondEq a2
+        | new q == conj -> isCondEq a1 && isCondEq a2
+    _ -> isEqOrNeg trm
 
-sizeOfTerm :: Term -> Int
-sizeOfTerm trm = case trm of
-    Abs { termId = t } -> sizeOfTerm t + 1
-    App { funId = t1, argId = t2 } -> sizeOfTerm t1 + sizeOfTerm t2
-    If { ifId = t1, thenId = t2, elseId = t3 } ->
-        sizeOfTerm t1 + max (sizeOfTerm t2) (sizeOfTerm t3)
-    Case { termId = t1, caseSubst = cs } ->
-        sizeOfTerm t1 + foldr max 0 (map (sizeOfTerm . snd) cs)
-    Let { letSubst = es, inId = t } ->
-        sizeOfTerm t + sum (map (sizeOfTerm . snd) es)
-    IsaEq { firstTerm = t1, secondTerm = t2 } ->
-        sizeOfTerm t1 + sizeOfTerm t2 + 1
-    Tuplex ts _ -> sum $ map sizeOfTerm ts
-    _ -> 1
