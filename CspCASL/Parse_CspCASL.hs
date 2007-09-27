63f8854d42e766ff77125c6e532c5fce3b5ebdef
Christian Maeder <Christian.Maeder@dfki.de>
2007-09-27 14:50:29 +0000

    added an option channel parser and cleaned up do-notation git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8945 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CspCASL/Parse_CspCASL.hs
+++ CspCASL/Parse_CspCASL.hs
@@ -1,59 +1,63 @@
 {- |
 Module      :  $Id$
 Description :  Parser for CspCASL specifications
-Copyright   :  (c) 
+Copyright   :  (c) Uni Bremen 2007
 License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
 
 Maintainer  :  a.m.gimblett@swan.ac.uk
 Stability   :  experimental
-Portability :  
+Portability :  portable
 
 Parser for CSP-CASL specifications.
-
 -}
 
 module CspCASL.Parse_CspCASL (
     cspBasicSpec
 ) where
 
-import Control.Monad (liftM)
-import Text.ParserCombinators.Parsec (many1, try, (<|>))
+import Text.ParserCombinators.Parsec (many1, try, (<|>), option, sepBy)
 
-import Common.AnnoState (AParser, asKey, equalT)
-import Common.Id (Id, mkId, mkSimpleId)
-import Common.Token (parseId)
+import Common.AnnoState (AParser, asKey, equalT, anSemi)
+import Common.Id (genName)
+import CASL.Formula
 
 import CspCASL.AS_CspCASL
 import CspCASL.CspCASL_Keywords
 import CspCASL.Parse_CspCASL_Process
 
 cspBasicSpec :: AParser st CspBasicSpec
-cspBasicSpec = do pp <- processPart
-                  return (CspBasicSpec [] pp)
+cspBasicSpec = do
+    option [] $ do
+      asKey "channel"
+      varDecl csp_casl_keywords `sepBy` anSemi
+      return []
+    pp <- processPart
+    return (CspBasicSpec [] pp)
 
 processPart :: AParser st [PROC_EQ]
-processPart = do asKey processS
-                 peqs <- procEqs <|> liftM toDummyEq csp_casl_process
-                 return peqs
-    where toDummyEq p = [(ProcEq dummyName p)]
-          dummyName = ParmProcname singletonProcessName []
-
--- Default name allocated to unnamed singleton processes.
-singletonProcessName :: Id
-singletonProcessName = mkId [(mkSimpleId "P")]
+processPart = do
+    asKey processS
+    procEqs <|> fmap toDummyEq csp_casl_process
+    where
+      toDummyEq p = [(ProcEq dummyName p)]
+      dummyName = ParmProcname singletonProcessName []
+      -- Default name allocated to unnamed singleton processes.
+      singletonProcessName = genName "P"
 
 procEqs :: AParser st [PROC_EQ]
 procEqs = many1 procEq
 
 procEq :: AParser st PROC_EQ
-procEq = do pn <- try (do pn <- parmProcname
-                          equalT
-                          return pn)
-            p <- csp_casl_process
-            return (ProcEq pn p)
+procEq = do
+    pn <- try (do
+      pn <- parmProcname
+      equalT
+      return pn)
+    p <- csp_casl_process
+    return (ProcEq pn p)
 
 parmProcname :: AParser st PARM_PROCNAME
-parmProcname = do pn <- process_name
-                  pa <- procArgs
-                  return (ParmProcname pn pa)
-  
+parmProcname = do
+    pn <- process_name
+    pa <- procArgs
+    return (ParmProcname pn pa)
