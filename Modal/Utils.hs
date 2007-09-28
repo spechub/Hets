e40703d2d736c2d40801607dfe6a66a3c63ad192
Christian Maeder <Christian.Maeder@dfki.de>
2007-09-28 09:17:16 +0000

    removed warning, trace and some commented out code git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@8953 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Modal/Utils.hs
+++ Modal/Utils.hs
@@ -10,14 +10,12 @@ import CASL.AS_Basic_CASL
 
 -- ModalCASL
 import Modal.AS_Modal
-
 import Data.Maybe
-import Debug.Trace
+
 getModTermSort :: Id -> Id
-getModTermSort rs =
-    case rs of
+getModTermSort rs = case rs of
     Id _ [s] _ -> s
-    _  -> error ("Modal2CASL: getTermSort: the impossible happened!!")
+    _  -> error "Modal.Utils.getModTermSort"
 
 addNonEmptyLabel :: String -> Maybe (Named a) -> Maybe (Named a)
 addNonEmptyLabel _ Nothing = Nothing
@@ -32,35 +30,14 @@ modToTerm (Term_mod t) = Just t
 addTerm :: ([VAR] -> TERM M_FORMULA -> TERM ())
         -> PRED_NAME -> [Maybe (TERM M_FORMULA)] -> [VAR]
         -> Maybe (Named CASLFORMULA) -> Maybe (Named CASLFORMULA)
-addTerm mapT rel mTerms vars nCaslFrm
+addTerm _ _ mTerms _ nCaslFrm
     | null (catMaybes mTerms) = nCaslFrm
-    | otherwise = trace "Modal.Utils.addTerm: Term-Modalities not corectly treated yet" nCaslFrm
+    | otherwise = nCaslFrm -- Term-Modalities not correctly treated yet
 
 {- - run with a State across formula
    - substitute in a nice manna the Terms into the relation symbols
 
-incomple code:
-
- let repl f = case f of
-           Quantification q vs frm ps ->
-                  Quantification q vs (repl frm) ps
-           Conjunction fs ps ->
-               Conjunction (map (repl) fs) ps
-           Disjunction fs ps ->
-               Disjunction (map (repl) fs) ps
-           Implication f1 f2 b ps ->
-               Implication (repl f1)
-                           (repl f2) b ps
-           Equivalence f1 f2 ps ->
-               Equivalence (repl f1)
-                           (repl f2) ps
-           Negation frm ps -> Negation (repl frm) ps
-           True_atom ps -> True_atom ps
-           False_atom ps -> False_atom ps
-           Existl_equation t1 t2 ps ->
-               Existl_equation t1 t2 ps
-           Strong_equation t1 t2 ps ->
-                  Strong_equation t1 t2 ps
+incomplete code:
            Predication pn as qs ->
                let term' = mapT vars term
                    (pn',as') =
@@ -74,10 +51,4 @@ incomple code:
                                        term':as)
                             | otherwise -> (pn,as)
                in Predication pn' as' qs
-           Definedness t ps -> Definedness t ps
-           Membership t ty ps -> Membership t ty ps
-           _ -> error "Modal.ModalSystems: addTerm: unsupported formula"
-        term = fromJust mTerm
-     in maybe nCaslFrm
-              (\_-> nCaslFrm {sentence = repl (sentence nCaslFrm) }) mTerm
 -}
