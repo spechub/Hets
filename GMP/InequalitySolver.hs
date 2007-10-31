22686fe00a6aa1abad91ffbbdc63dd09d9f308fd
Georgel Calin <g.calin@jacobs-university.de>
2007-10-31 15:23:55 +0000

    Bug found and solved. Nothing spectacular ;). git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@9113 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GMP/InequalitySolver.hs
+++ GMP/InequalitySolver.hs
@@ -6,49 +6,54 @@ data Coeffs = Coeffs [Int] [Int]
 
 {- | Returns the updated bound for the unknown corresponding to the negative
  - coeff. h where n & p hold the coefficients for the not yet set unknowns -}
-updateNegBound :: Int -> [Int] -> [Int] -> Int -> Int -> Int
-updateNegBound h n p c lim = 
+negBound :: Int -> [Int] -> [Int] -> Int -> Int -> Int
+negBound h n p c lim = 
         let tmp = fromIntegral (c+lim*(sum p)+sum n)/fromIntegral h :: Double
         in if (tmp>0) then max (ceiling tmp) 1 else 1
 {- | Returns the updated bound for the unknown corresponding to the positive
  - coeff. h where p holds the coefficients for the not yet set unknowns -}
-updatePosBound :: Int -> [Int] -> Int -> Int -> Int
-updatePosBound h p c lim =
+posBound :: Int -> [Int] -> Int -> Int -> Int
+posBound h p c lim =
         let tmp = fromIntegral (-c-sum p)/fromIntegral h :: Double
         in if (tmp>0) then min (floor tmp) lim else lim
 
--- | Append w to the first element of the pair result of func w
-negAddAndCall :: (a -> [([a], [a])]) -> a -> [([a], [a])]
-negAddAndCall func w = let aux = func w 
-                       in zip (map ((w:).fst) aux) (map snd aux)
--- | Append w to the second element of the pair result of func w
-posAddAndCall :: (a -> [([a], [a])]) -> a -> [([a], [a])]
-posAddAndCall func w = let aux = func w
-                       in zip (map fst aux) (map ((w:).snd) aux)
+-- | Append an element to each fst. element of each element of a list of pairs
+mapAppendFst :: a -> [([a],[a])] -> [([a],[a])]
+mapAppendFst x list = map (\e->(x:(fst e), snd e)) list
 
-{- | Returns all solutions (x,y) with 1<=x_i,y_j<=lim of the inequality
+-- | Append an element to each snd. element of each element of a list of pairs
+mapAppendSnd :: a -> [([a],[a])] -> [([a],[a])]
+mapAppendSnd x list = map (\e->(fst e, x:(snd e))) list
+
+-- | Generate all possible solutions of unknowns corresp. to positive coeffs.
+getPosUnknowns :: [Int] -> Int -> Int -> [([Int], [Int])]
+getPosUnknowns p lim c =
+  if (c+lim*(sum p)<=0)
+  then [([], map (\_->lim) p)]
+  else 
+    case p of
+      h:t -> let aux = posBound h t c lim
+             in concat (map (\x->mapAppendSnd x (getPosUnknowns t lim (c+x*h)))
+                            [1..aux])
+      []  -> [([],[])]
+
+-- | Generate all posible solutions of unknowns
+getUnknowns :: [Int] -> [Int] -> Int -> Int -> [([Int], [Int])]
+getUnknowns n p lim c =
+  if (c+sum n+lim*(sum p)<=0) 
+  then [(map (\_->1) n, map (\_->lim) p)]
+  else 
+    case n of
+      h:t -> let aux = negBound (abs h) t p c lim
+             in concat (map (\x->mapAppendFst x (getUnknowns t p lim (c+x*h)))
+                            [aux..lim])
+      []  -> getPosUnknowns p lim c
+
+
+{- | Returns all solutions (x,y) with 1<=x_i,y_j<=bound of the inequality
  -              0 >= 1 + sum x_i*n_i + sum y_j*p_j
  - with coefficients n_j<0, p_j>0 known -}
 ineqSolver :: Coeffs -> Int -> [([Int],[Int])]
-ineqSolver (Coeffs x y) bound = recurseAll x y bound 1
+ineqSolver (Coeffs n p) bound = getUnknowns n p bound 1
 ------------------------------------------------------------------------------
-recursePos :: [Int] -> Int -> Int -> [([Int], [Int])]
-recursePos p lim c =
-        if (c+lim*(sum p)<=0)
-        then [([], map (\_->lim) p)]
-        else case p of
-               h:t -> let aux = updatePosBound h t c lim
-                      in concat (map (posAddAndCall (recursePos t lim))
-                                     (map ((c+).(h*)) [1..aux]))
-               []  -> [([],[])]
-
-recurseAll :: [Int] -> [Int] -> Int -> Int -> [([Int], [Int])]
-recurseAll n p lim c =
-        if (c+sum n+lim*(sum p)<=0) 
-        then [(map (\_->1) n, map (\_->lim) p)]
-        else case n of
-               h:t -> let aux = updateNegBound (abs h) t p c lim
-                      in concat (map (negAddAndCall (recurseAll t p lim))
-                                     (map ((c+).(h*)) [aux..lim]))
-               []  -> recursePos p lim c
 
