05edc725a1278623336d8b87d130b03305382db0
Georgel Calin <g.calin@jacobs-university.de>
2007-12-14 16:40:33 +0000

    New GML test added (RG1)+(A1) git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@9376 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GMP/extra/TestGenGML.hs
+++ GMP/extra/TestGenGML.hs
@@ -10,10 +10,12 @@ a1 :: Formula GML -> Formula GML -> Formula GML -> Int -> Int -> (Formula GML, F
 a1 c a b n1 n2 = (Mapp (Mop (GML (n1+n2)) Angle) c, 
                   Mapp (Mop (GML n1) Angle) a, 
                   Mapp (Mop (GML n2) Angle) b)
+
 a2 :: Formula GML -> Formula GML -> Formula GML -> Formula GML -> Int -> Int -> (Formula GML, Formula GML, Formula GML)
 a2 a b c d n1 n2 = (Junctor (Mapp (Mop (GML n1) Angle) a) And (Mapp (Mop (GML n2) Angle) b),
                     Mapp (Mop (GML (n1+n2+1)) Angle) c,
                     Mapp (Mop (GML 0) Angle) d)
+
 rn :: Formula GML -> Formula GML
 rn a = Neg (Mapp (Mop (GML 0) Angle) (Neg a))
 
