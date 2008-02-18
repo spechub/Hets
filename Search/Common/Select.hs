e04c6d430fc2f0c9ef5ede1c4bdc63f9b3c8bd70
Immanuel Normann <normann@uni-bremen.de>
2008-02-18 18:50:50 +0000

    little bug fixed; clean up; documentation added. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@9571 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Search/Common/Select.hs
+++ Search/Common/Select.hs
@@ -32,10 +32,30 @@ type Renaming a = M.Map a a
 type LineMap = M.Map Int Int
 type ProfileMorphism a = (Renaming a, LineMap)
 
--- theory = "alg_1__t10_alg_1.dfg"
+
+-- -----------------------------------------------------------
+-- *  Principle Functions
+-- -----------------------------------------------------------
+
+{- |
+   search is the main function of this module. It takes a handler function
+   to read in the source theory and a path to the file of the source theory
+   and returns all possible profile morphisms from all matching target theories
+   in the database.
+-}
+search :: (Ord p, Read p) =>
+          (FilePath -> IO [(Skel, ([p], LineNr))])
+              -> FilePath -> IO [(String, [(Renaming p, LineMap)])]
+search getSourceAxiomProfiles file =
+    do sourceList <- getSourceAxiomProfiles file 
+       targetCandidates <- allTargetCandidates (L.map fst sourceList)
+       mapM (showResults $ removeDuplicateProfiles sourceList) (M.toList targetCandidates)
+
+{- "queryProfileMorphisms' is perhaps the cleaner alternative to 'search':
+   the handler 'getSourceAxiomProfiles' is out sorced to a class function.
+-}
 class Retrievable p where
     getSourceAxiomProfiles :: FilePath -> IO [(Skel,([p],LineNr))]
-
 
 queryProfileMorphisms :: (Retrievable p, Ord p, Read p) =>
                          FilePath -> IO [(TheoryName, [(Renaming p, LineMap)])]
@@ -44,6 +64,9 @@ queryProfileMorphisms file =
        targetCandidates <- allTargetCandidates (L.map fst sourceList)
        mapM (showResults sourceList) (M.toList targetCandidates)
 
+{- |
+   showResults shows for a given target theory candidate all possible profile morphisms.
+-}
 showResults :: (Ord p, Read p) =>
                [(Skel, ([p], LineNr))]
                -> (String, Map Skel (Set ([p], LineNr)))
@@ -52,87 +75,33 @@ showResults sourceList (targetTheory,tMap) =
     do putStrLn targetTheory
        return (targetTheory,profileMorphism sourceList tMap)
 
-
-{-
-  Matching
--}
-
-
 {- |
-   profileMorphism finds all theory morphisms from profiles of a source theory
-   to profiles in a target theory.
+   two profiles are said to be duplicates if they have the same skeletons and paramters.
 -}
-profileMorphism :: (Ord p, Read p) =>
-                   [(Skel,([p], LineNr))] -> M.Map Skel (S.Set ([p], LineNr)) -> [(Renaming p, LineMap)]
-profileMorphism sourceProfiles targetProfileMap =
-    foldr merge [] (L.map matchList' sourceProfiles)
-        where matchList' (s,sourceProfile) = 
-                  case M.lookup s targetProfileMap
-                  of (Just targetProfiles) -> matchList (S.toList targetProfiles) sourceProfile
-                     Nothing -> []
 
-merge :: (Ord p, Read p) => [(Renaming p, LineMap)]
-                 -> [(Renaming p, LineMap)]
-                 -> [(Renaming p, LineMap)]
-merge ps1 ps2 = allJust [merge' p1 p2 | p1 <- ps1, p2 <- ps2]
-    where merge' (r1,l1) (r2,l2) =
-              case maybeUnion r1 r2
-              of (Just r) -> Just (r, M.union l1 l2)
-                 Nothing -> Nothing
+removeDuplicateProfiles profiles = nubBy eqProfiles profiles
+    where eqProfiles (skel1,(par1,_)) (skel2,(par2,_)) = 
+              skel1 == skel2 && par1 == par2
 
-{- |
-   matchList takes a single source pair and a set of target pairs
-   and returns (Just) a renaming together with a line number mapping.
--}
-matchList :: (Ord p, Read p) =>
-            [([p], LineNr)] -> ([p], LineNr) -> [(Renaming p, LineMap)]
-matchList targetProfiles sourceProfile =
-    foldr justInsert [] (L.map (match sourceProfile) targetProfiles)
-        where justInsert Nothing lst = lst
-              justInsert (Just x) lst = x:lst
-
-{- |
-   match takes two pairs and returns (Just) 
-   a renaming of parameters and a line number association
-   if the pairs match and Nothing otherwise.
-   Each pair has a list of parameter as first component
-   and a line number as second. Each pair represents formula
-   whose skeleton is supposed to be identical.
-   The pairs match iff their parameter do (s. 'constructRenaming').
--}
-match :: (Ord p, Read p) => ([p],LineNr) -> ([p],LineNr)
-                -> Maybe (Renaming p, LineMap)
-match (p1,l1) (p2,l2) =
-    case constructRenaming p1 p2
-    of (Just renaming) -> Just (renaming, M.singleton l1 l2)
-       _ -> Nothing
-
-{- |
-   constructRenaming takes two lists of parameters and returns (Just) a pointwise
-   mapping between these lists if this is consistently possible.
-   Otherwise it returns Nothing.
--}
-constructRenaming :: (Ord p, Read p) => [p] -> [p] -> Maybe (Renaming p)
-constructRenaming lst1 lst2 = SM.fromList $ zip lst1 lst2
-
-
-{-
-  Database Access
--}
+-- -----------------------------------------------------------
+-- *  Database Access
+-- -----------------------------------------------------------
 
 
 {- |
    allTargetCandidates takes a list of skeletons and retrieves from the database
-   target theories
+   candidates of target theories. A theory is a candidate if it contains all the
+   skeletons from the input list.
 -}
 allTargetCandidates :: (Ord p, Read p) => 
                        [Skel] -> IO (Map TheoryName (Map Skel (Set ([p], LineNr))))
-allTargetCandidates skels = 
-    do triples <- matchSkeleton skels
-       return $ (M.map fromSetSetValues (M.filter comprisesAllSkels (fromListSetValues triples)))
-           where comprisesAllSkels skelLineSet = 
-                     ((S.size $ S.map fst skelLineSet) == (length skels))
-
+allTargetCandidates skels =
+    let len = length $ nub skels
+        comprisesAllSkels skelLineSet = 
+                     ((S.size $ S.map fst skelLineSet) == len)
+    in do triples <- matchSkeleton skels
+          return $ (M.map fromSetSetValues
+                         (M.filter comprisesAllSkels (fromListSetValues triples)))
 
 {- |
    matchSkeleton takes a list of skeletons and retrieves from the database all
@@ -152,34 +121,73 @@ matchSkeleton skels =
           return $ L.map recToTuple recs
 
 
-{-
-  Query File Access
--}
+-- -----------------------------------------------------------
+-- *  Matching
+-- -----------------------------------------------------------
 
 
-{-
-getSourceAxiomProfiles file = zip md5s lines -- for testing
-    where md5s = ["1a8c5f73411bbd689bff1d9306b9da3b", "0df802cf17eb339f0bbcdd2d86c06506", "87929a4736dd0d152b4dde3c29ea213e"]
-          lines = [1,2,3]
--}
-
-{-
-getAxioms dir file =
-    getProfiles dir file >>= (sortProfiles . 
-                              removeDuplicateProfiles . 
-                              removeTrueAtoms . 
-                              removeTheorems)
--}
--- ** Filter Functions
 {- |
-   
-
-removeTrueAtoms :: [Profile t f (Skeleton c) [p]] -> [Profile t f (Skeleton c) [p]]
-removeTrueAtoms profiles = filter isNotTrueAtom profiles
-    where isNotTrueAtom (Profile _ _ (Const TrueAtom []) _) = False
-	  isNotTrueAtom _ = True
-
-removeTheorems :: [Profile t f (Skeleton c) [p]] -> [Profile t f (Skeleton c) [p]]
-removeTheorems  profiles = filter isNotAxiom profiles
-    where isNotAxiom = undefined -- todo!!!
+   profileMorphism finds all theory morphisms from profiles of a source theory
+   to profiles in a target theory.
 -}
+profileMorphism :: (Ord p, Read p) =>
+                   [(Skel,([p], LineNr))] -> M.Map Skel (S.Set ([p], LineNr)) -> [(Renaming p, LineMap)]
+profileMorphism sourceProfiles targetProfileMap =
+    let  matchList' (s,sourceProfile) = 
+             case M.lookup s targetProfileMap
+             of (Just targetProfiles) -> matchList (S.toList targetProfiles) sourceProfile
+                Nothing -> []
+    in case (L.map matchList' sourceProfiles)
+       of (h:t) -> foldr merge h t
+          [] -> []
+
+{- |
+   merge takes two lists of profile mappings and returns the list of
+   all profile mappings resulting from a admissible union out of the
+   Cartesian product of the input lists. A union is of two profile mappings
+   is admissible iff their renamings are equal on their common domain.
+-}
+merge :: (Ord p, Read p) => [(Renaming p, LineMap)]
+                 -> [(Renaming p, LineMap)]
+                 -> [(Renaming p, LineMap)]
+merge ps1 ps2 = allJust [merge' p1 p2 | p1 <- ps1, p2 <- ps2]
+    where merge' (r1,l1) (r2,l2) =
+              case maybeUnion r1 r2
+              of (Just r) -> Just (r, M.union l1 l2)
+                 Nothing -> Nothing
+
+{- |
+   matchList takes  a list of target pairs and a single source pair
+   and returns a list of renamings together with a line number mapping.
+-}
+matchList :: (Ord p, Read p) =>
+            [([p], LineNr)] -> ([p], LineNr) -> [(Renaming p, LineMap)]
+matchList targetProfiles sourceProfile =
+    foldr justInsert [] (L.map (match sourceProfile) targetProfiles)
+        where justInsert Nothing lst = lst
+              justInsert (Just x) lst = x:lst
+
+{- |
+   match takes two pairs and returns (Just) 
+   a renaming of parameters and a line number association
+   if the pairs match and Nothing otherwise.
+   Each pair has a list of parameter as first component
+   and a line number as second. Each pair represents a formula
+   whose skeleton is supposed to be identical.
+   The pairs match iff their parameter do (s. 'constructRenaming').
+-}
+match :: (Ord p, Read p) => ([p],LineNr) -> ([p],LineNr)
+                -> Maybe (Renaming p, LineMap)
+match (p1,l1) (p2,l2) =
+    case constructRenaming p1 p2
+    of (Just renaming) -> Just (renaming, M.singleton l1 l2)
+       _ -> Nothing
+
+{- |
+   constructRenaming takes two lists of parameters and returns (Just) a pointwise
+   mapping between these lists if this is consistently possible.
+   Otherwise it returns Nothing.
+-}
+constructRenaming :: (Ord p, Read p) => [p] -> [p] -> Maybe (Renaming p)
+constructRenaming lst1 lst2 = SM.fromList $ zip lst1 lst2
+
--- Search/SPASS/FeedDB.hs
+++ Search/SPASS/FeedDB.hs
@@ -8,43 +8,67 @@ Maintainer  :  inormann@jacobs-university.de
 Stability   :  provisional
 Portability :  portable
 -}
-module Search.Main where
+module Main where
 
-import Config
+--import Config
 import Data.List as L
---import Data.Set as S 
 import Search.Common.Normalization
+import Search.Common.Select -- (queryProfileMorphisms) 
 import Search.SPASS.DFGParser
 import Search.SPASS.Sign
 import Search.SPASS.FormulaWrapper
--- import System.Environment
 import System.Directory
--- import System.Posix.Files
 import System.IO
 import Text.ParserCombinators.Parsec
--- import Text.ParserCombinators.Parsec.Char
--- import MD5
-import DB.Connection
-import System.Environment
+import MD5
+import Search.DB.Connection
+import System.Environment (getArgs)
 
 main = do args <- getArgs
-          case args of -- todo: test ob file existiert
-            (lib:dir:file:_) -> treatFile lib dir file
+          case args of
+            ("search":file:_) -> searchDFG file >> return ()
+            ("insert":lib:dir:file:_) -> indexFile lib dir file  >> return ()
             _ -> error "invalid arguments!\nShould be <library> <dir> <file>"
 
---treatDir :: (ParseHandling -> IO ()) -> Handle -> FilePath -> IO ()
-treatDir lib dir =
-    do ls <- getDirectoryContents dir
-       setCurrentDirectory dir
-       rec <- mapM (treatFile lib dir) (L.filter (L.isSuffixOf "dfg") ls)
-       putStrLn (concatMap showRec rec)
-       return rec
+-- -----------------------------------------------------------
+-- * Search
+-- -----------------------------------------------------------
 
-showRec (file,ts,ds,fs) = file ++ "\t" ++ (show ts) ++ "\t" ++ (show ds) ++ "\t" ++ (show fs)
+{- |
+   searchDFG reads in a source theory from file and returns all possible profile morphisms
+   from all matching target theories in the database.
+-}
+searchDFG :: SourceName -> IO [(String, [(Renaming SPIdentifier, LineMap)])]
+searchDFG = search readProblemAxioms
+
+{- |
+   readProblemAxioms reads in from file a dfg problem and returns
+   all axioms but removes duplicates and trivially true axioms.
+-}
+readProblemAxioms :: SourceName -> IO [(Skel, ([SPIdentifier], Int))]
+readProblemAxioms filePath =
+    do result <- parseFromFile parseSPASS filePath
+       case result 
+         of Left err  -> error $ show err
+	    Right spproblem  -> 
+                let (_,_,lst) = getSPFormulas "lib" "file" spproblem
+                    isAxiom (_,_,_,role,_,skel,_,_) = 
+                        (role == "axiom") && (skel /= Const TrueAtom [])
+                    f (_,_,line,_,_,skel,params,_) = (md5s $ Str $ show skel,(params,line))
+                in return $ map f $ filter isAxiom lst
+
+-- -----------------------------------------------------------
+-- * Indexing
+-- -----------------------------------------------------------
 
 
---treatFile :: SourceName -> IO  (Int,Set SPFormula)
-treatFile lib dir file =
+{-|
+  indexFile reads the file TheoryName in directory FilePath and feeds it
+  to the database. The first argument specifies the library the theory
+  should be associated with.
+-}
+indexFile :: String -> FilePath -> TheoryName -> IO (TheoryName, Int, Int, Int)
+indexFile lib dir file =
     do result <- parseFromFile parseSPASS (dir ++ "/" ++file)
        case result 
          of Left err  -> error $ show err
@@ -58,18 +82,50 @@ treatFile lib dir file =
                                       return (file,nrOfTautologies,nrOfDuplicates,len)
 
 
-{- test:
-r <- treatFile dfgHome "zfmisc_1__t9_zfmisc_1.dfg"
+{-|
+  indexDir reads all files from the directory FilePath and feeds them
+  to the database. The first argument specifies the library the theories
+  should be associated with.
 -}
+indexDir :: String -> FilePath -> IO [(TheoryName, Int, Int, Int)]
+indexDir lib dir =
+    let showRec (file,ts,ds,fs) = file ++ "\t" ++ (show ts) ++ "\t" ++ 
+                                  (show ds) ++ "\t" ++ (show fs)
+    in do ls <- getDirectoryContents dir
+          setCurrentDirectory dir
+          rec <- mapM (indexFile lib dir) (L.filter (L.isSuffixOf "dfg") ls)
+          putStrLn (concatMap showRec rec)
+          return rec
 
-msgErr err = hPutStrLn stderr ("\t ... failed!\nerror: " ++ show err)
+-- -----------------------------------------------------------
+-- * SPProblem Normalization
+-- -----------------------------------------------------------
 
---getSPFormulas :: SPProblem -> (Int,Set SPFormula)
-getSPFormulas lib file spproblem = (nrOfTautologies,nrOfDuplicates,spformulas)
+
+{-|
+  getSPFormulas translates a SPProblem into a list of tuples
+  similiar to a profile record in the database. In particular
+  all formulas are normalized.
+  The first two arguments are intended to assign a library and theory name
+  to the SPProblem.
+-}
+getSPFormulas :: String
+                 -> String
+                 -> SPProblem
+                 -> (Int,
+                     Int,
+                     [(String, -- lib
+                       String, -- theory
+                       Int, -- line
+                       String, -- role
+                       SPTerm, 
+                       Formula (Constant SpassConst) Int, -- formula skeleton
+                       [SPIdentifier], -- formula parameter
+                       String)])
+getSPFormulas lib theory spproblem = (nrOfTautologies,nrOfDuplicates,spformulas)
     where (SPProblem _ _ (SPLogicalPart _ _ flsts) _) = spproblem
-          spformulas1 = map (normalizeSPFormula lib file)
+          spformulas1 = map (normalizeSPFormula lib theory)
                         (concatMap (\(SPFormulaList _ f) -> f) flsts)
-          --eqSpterm (_,_,spterm1,_,_,_,_) (_,_,spterm2,_,_,_,_) = spterm1 == spterm2
           eqSpterm (_,_,_,_,_,skel1,params1,_) (_,_,_,_,_,skel2,params2,_) =
               skel1 == skel2 && params1 == params2
           isNotTrueAtom (_,_,_,_,_,(Const TrueAtom []),_,_) = False
@@ -79,9 +135,14 @@ getSPFormulas lib file spproblem = (nrOfTautologies,nrOfDuplicates,spformulas)
           spformulas = L.nubBy eqSpterm spformulas2
           nrOfDuplicates = (length spformulas2) - (length spformulas)
 
---normalizeSPFormula :: String -> SPFormula -> (String, String, Bool, SPTerm,Skeleton SpassConst, [SPIdentifier], String,Int)
-normalizeSPFormula lib file (NamedSen line ax _ spterm) = 
-    (lib,file,line',role,spterm,abstractSPTerm,parameter,normalizationStrength)
+{-|
+  normalizeSPFormula normalizes a SPFormula and associates it with
+  a library and theory name.
+-}
+normalizeSPFormula :: String -> String -> SPFormula
+                   -> (String, String, Int, String, SPTerm,Skeleton SpassConst, [SPIdentifier], String)
+normalizeSPFormula lib theory (NamedSen line ax _ spterm) = 
+    (lib,theory,line',role,spterm,abstractSPTerm,parameter,normalizationStrength)
         where (abstractSPTerm,parameter,normalizationStrength) = (normalize . wrapTerm) spterm
               line' = read line::Int
               role = if ax then "axiom" else "conjecture"
