b8e9e3504e7813f9912048921b8f6de3f50a89df
Dominik Luecke <luecke@informatik.uni-bremen.de>
2008-02-25 12:32:30 +0000

    changed OWLParser to a BIG one git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@9624 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ OWL/java/org/hets/owl11/atermRender/OWLATermFormat.java
@@ -0,0 +1,15 @@
+package org.hets.owl11.atermRender;
+
+import org.semanticweb.owl.model.OWLOntologyFormat;
+
+/**
+ * Author: Heng Jiang<br>
+ * The University Of Bremen <br>
+ * Date: 10-2007<br><br>
+ */
+public class OWLATermFormat extends OWLOntologyFormat {
+
+    public String toString() {
+        return "OWL ATerm";
+    }
+}
