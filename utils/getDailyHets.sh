6588ecc93db54073b171dfa13fdb1edabaad326b
Christian Maeder <Christian.Maeder@dfki.de>
2008-02-26 09:35:53 +0000

    updated git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@9643 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/getDailyHets.sh
+++ utils/getDailyHets.sh
@@ -1,55 +1,59 @@
 #!/bin/bash
 
-## This script downloads, bunzips daily hets;
-## afterwards it adds excutable permissions for everyone
+## This script downloads a daily hets into your ~/bin directory
 
 DAILY_HETS=$HOME/bin/hets-`date +%F`
-DAILY_HETS_WGET_TARGET=$DAILY_HETS".bz2"
+DAILY_HETS_TARGET=$DAILY_HETS.bz2
 
 if [ -d "$HOME/bin" ] ; then
-    if [ -e "$DAILY_HETS" ] ; then 
-	mv $DAILY_HETS "$DAILY_HETS~"
-	echo generated backup of exisiting file '"'$DAILY_HETS'"'
+    if [ -e "$DAILY_HETS" ] ; then
+        mv $DAILY_HETS "$DAILY_HETS~"
+        echo generated backup of exisiting file '"'$DAILY_HETS'"'
     fi
-    if [ -e "$DAILY_HETS_WGET_TARGET" ] ; then 
-	rm -f "$DAILY_HETS_WGET_TARGET"
-	echo removed exisiting file '"'"$DAILY_HETS_WGET_TARGET"'"'
+    if [ -e "$DAILY_HETS_TARGET" ] ; then
+        rm -f "$DAILY_HETS_TARGET"
+        echo removed exisiting file '"'"$DAILY_HETS_TARGET"'"'
     fi
-else 
-    mkdir $HOME/bin ; 
+else
+    mkdir $HOME/bin
 fi
 
 ARCH_DIR=""
-DOWNLOADCMD="wget --output-document=$DAILY_HETS_WGET_TARGET"
+DOWNLOADCMD="curl -o $DAILY_HETS_TARGET"
 
 case `uname -s` in
-    SunOS) case `uname -p` in
-	       sparc) ARCH_DIR=solaris ;;
-               *) echo "Unsupported SunOS processor: " `uname -p`
-	          exit 2;;
-           esac;;
-    *inux) case `uname -p` in
-               i*86) ARCH_DIR=linux ;;
-	       athlon) ARCH_DIR=linux ;;
-	       *) echo "Unsupported Linux processor: " `uname -p`
-	          exit 2;;
-	   esac;;
-    Darwin) case `uname -p` in
-               powerpc) ARCH_DIR=mac 
-	                DOWNLOADCMD="ftp -o $DAILY_HETS_WGET_TARGET" ;;
-	       *86) ARCH_DIR=mac ;;
-	       *) echo "Unsupported Mac OS X processor: " `uname -p`
-	          exit 2;;
-            esac;;
-    *) echo "Unsupported system: " `uname -s`
-       exit 2;;
+    SunOS)
+        case `uname -p` in
+            sparc)
+                ARCH_DIR=solaris ;;
+            *86*)
+                ARCH_DIR=pc-solaris ;;
+            *)
+                echo "Unsupported SunOS processor: " `uname -p`
+                exit 2;;
+        esac;;
+    *inux)
+        ARCH_DIR=linux ;;
+    Darwin)
+        case `uname -p` in
+            powerpc)
+                ARCH_DIR=mac ;;
+            *86)
+                ARCH_DIR=intel-mac ;;
+            *)
+                echo "Unsupported Mac OS X processor: " `uname -p`
+                exit 2;;
+        esac;;
+    *)
+        echo "Unsupported system: " `uname -s`
+        exit 2;;
 esac
-DAILY_HETS_URL="http://www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets/$ARCH_DIR/daily/hets.bz2"
 
+DAILY_HETS_URL=http://www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets/$ARCH_DIR/daily/hets.bz2
 
 # main
 $DOWNLOADCMD $DAILY_HETS_URL || exit 1
-bunzip2 $DAILY_HETS_WGET_TARGET || exit 1
+bunzip2 $DAILY_HETS_TARGET || exit 1
 chmod a+x $DAILY_HETS || exit 1
 
 echo "Downloaded and saved daily hets to: " $DAILY_HETS
