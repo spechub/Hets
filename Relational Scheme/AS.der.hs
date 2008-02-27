5eda89432ca662da58f83ecc6d2ce8852968d247
Dominik Luecke <luecke@informatik.uni-bremen.de>
2008-02-27 14:29:10 +0000

    imminent move git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@9655 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ Relational Scheme/AS.der.hs
@@ -0,0 +1,13 @@
+{- |
+Module      :  $Header$
+Description :  abstract syntax for Relational Schemes
+Copyright   :  Dominik Luecke, Uni Bremen 2008
+License     :  similar to LGPL, see Hets/LICENSE.txt or LIZENZ.txt
+
+Maintainer  :  luecke@informatik.uni-bremen.de
+Stability   :  provisional
+Portability :  portable
+
+Abstract syntax Relational Schemes
+-}
+
