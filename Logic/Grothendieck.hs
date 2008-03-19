3209e1390e106b42e3526bd2c77261799d54cad1
Christian Maeder <Christian.Maeder@dfki.de>
2008-03-19 16:10:42 +0000

    removed unused coerceG_sign git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@9805 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Logic/Grothendieck.hs
+++ Logic/Grothendieck.hs
@@ -98,7 +98,6 @@ module Logic.Grothendieck
   , coerceProver
   , G_cons_checker (..)
   , coerceConsChecker
-  , coerceG_sign
   , Square (..)
   , LaxTriangle (..)
   , mkIdSquare
@@ -986,13 +985,6 @@ coerceConsChecker ::
       -> ConsChecker sign1 sentence1 sublogics1 morphism1 proof_tree1
       -> m (ConsChecker sign2 sentence2 sublogics2 morphism2 proof_tree2)
 coerceConsChecker l1 l2 msg m1 = primCoerce l1 l2 msg m1
-
-coerceG_sign ::
-   (Logic  lid1 sublogics1 basic_spec1 sentence1 symb_items1 symb_map_items1
-                sign1 morphism1 symbol1 raw_symbol1 proof_tree1,
-   Monad m) => lid1 -> String -> G_sign -> m sign1
-coerceG_sign l1 msg (G_sign l2 sign2 _) = primCoerce l2 l1 msg sign2
-
 
 -- * Lax triangles and weakly amalgamable squares of lax triangles
 -- a lax triangle looks like:
