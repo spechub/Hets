7631f47e074b7b273f87ad651acd84fe2540d647
Georgel Calin <g.calin@jacobs-university.de>
2008-06-18 14:20:22 +0000

    Temporary remove of script ... git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@10141 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GMP/examples/scripts/checkP.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# testing Probabilistic Modal Logic formulae
-for i in ../probabilistic/*
-do 
-    echo "~~~~~~processing probabilistic logic formula from $i"
-    ../../main 5 $i
-done
