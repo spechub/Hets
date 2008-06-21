1a14184ae8b8a3831ff58d664997dcc20d536d8c
Razvan Pascanu <r.pascanu@jacobs-university.de>
2008-06-21 01:12:10 +0000

    Modified check file for K modal logic git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@10174 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GMP/examples/scripts/checkK.sh
+++ GMP/examples/scripts/checkK.sh
@@ -4,5 +4,5 @@
 for i in ../k_and_kd/*
 do
     echo "~~~~~~processing $i"
-    ./gnutime -f "Running Time (real): %e" ./timeout 3600 ../../main 1 $i 
+    ./gnutime -f "Running Time (real): %e" ./timeout 600 ../../main 1 $i 
 done
