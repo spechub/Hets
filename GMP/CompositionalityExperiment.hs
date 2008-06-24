2ce19544de9ca1262c4b101d3d0b559aba04f5f8
Lutz Schröder <lutz.schroeder@cs.fau.de>
2008-06-24 12:07:02 +0000

    Experiment on Compositionality without code generation from M4M Paris git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@10183 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ GMP/CompositionalityExperiment.hs
@@ -0,0 +1,44 @@
+module Comp where
+
+import qualified Data.Set as Set
+import qualified Data.Map as Map
+
+data Boole a = F | T | And (Boole a) (Boole a) | Or (Boole a) (Boole a) | 
+     	      Not (Boole a) | At a deriving (Eq, Show)
+
+data K l = K (Boole l) deriving (Eq, Show)
+
+data RK = RK Int
+
+data KD l  = KD (Boole l) deriving (Eq, Show)
+
+data RKD = RKDPos Int | RKDNeg Int
+
+data Segala a = S (Boole (KD (K (Segala a)))) deriving (Eq, Show)
+
+data Clause a = Implies (Set.Set a) (Set.Set a) 
+
+data Subst a = Subst (Map.Map Int a)
+
+class Logic a b | a -> b, b -> a where
+  match :: Clause (a c) -> [(b, Subst c)]
+  clauses :: b -> [Clause Int]
+  subclauses :: Clause (a c) -> Set.Set (Clause (a c))
+
+instance Logic K RK where
+  match ((Implies n p):: Clause (K c)) = 
+    let i = Set.size n in
+    [(RK i, Subst (Map.fromList 
+  	((i+1,head (Set.elems p)): zip [1..i] (Set.elems n))))]
+  clauses (RK n) = [Implies (Set.fromList [1..n]) (Set.singleton (n+1))]
+  subclauses (Implies n p) = Set.fromList [Implies n (Set.singleton l) | l <- p]
+
+instance Logic KD RKD where
+  match ((Implies n p):: Clause (KD c)) = 
+    let i = Set.size n in
+    [(RKDPos i, Subst (Map.fromList 
+  	((i+1,head (Set.elems p)): zip [1..i] (Set.elems n))))]
+  clauses (RKDPos n) = [Implies (Set.fromList [1..n]) (Set.singleton (n+1))]
+  clauses (RKDNeg n) = [Implies (Set.fromList [1..n]) (Set.singleton (n+1))]
+  subclauses (Implies n p) = Set.fromList [Implies n (Set.singleton l) | l <- p]
+
