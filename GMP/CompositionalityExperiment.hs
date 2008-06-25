ef206c05d6a481ca61e4df4b54ec4326d94de859
Georgel Calin <g.calin@jacobs-university.de>
2008-06-25 09:41:18 +0000

    Added flag for allowing functional dependencies git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@10192 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GMP/CompositionalityExperiment.hs
+++ GMP/CompositionalityExperiment.hs
@@ -1,5 +1,7 @@
+{-# OPTIONS -fglasgow-exts #-}
 module Comp where
 
+import Debug.Trace
 import qualified Data.Set as Set
 import qualified Data.Map as Map
 
@@ -8,17 +10,17 @@ data Boole a = F | T | And (Boole a) (Boole a) | Or (Boole a) (Boole a) |
 
 data K l = K (Boole l) deriving (Eq, Show)
 
-data RK = RK Int
+data RK = RK Int deriving Show
 
 data KD l  = KD (Boole l) deriving (Eq, Show)
 
-data RKD = RKDPos Int | RKDNeg Int
+data RKD = RKDPos Int | RKDNeg Int deriving Show
 
 data Segala a = S (Boole (KD (K (Segala a)))) deriving (Eq, Show)
 
-data Clause a = Implies (Set.Set a) (Set.Set a) 
+data Clause a = Implies (Set.Set a) (Set.Set a) deriving Show
 
-data Subst a = Subst (Map.Map Int a)
+data Subst a = Subst (Map.Map Int a) deriving Show
 
 class Logic a b | a -> b, b -> a where
   match :: Clause (a c) -> [(b, Subst c)]
@@ -26,13 +28,13 @@ class Logic a b | a -> b, b -> a where
   subclauses :: Clause (a c) -> Set.Set (Clause (a c))
 
 instance Logic K RK where
-  match ((Implies n p):: Clause (K c)) = 
-    let i = Set.size n in
-    [(RK i, Subst (Map.fromList 
-  	((i+1,head (Set.elems p)): zip [1..i] (Set.elems n))))]
+  match ((Implies n p)::Clause (K c)) = 
+    let i = Set.size n
+        res = [(RK i, Subst (Map.fromList ((i+1,head (Set.elems p)): zip [1..i] (Set.elems n))))]
+    in trace("show res type"++show res) res
   clauses (RK n) = [Implies (Set.fromList [1..n]) (Set.singleton (n+1))]
   subclauses (Implies n p) = Set.fromList [Implies n (Set.singleton l) | l <- p]
-
+{-
 instance Logic KD RKD where
   match ((Implies n p):: Clause (KD c)) = 
     let i = Set.size n in
@@ -41,4 +43,4 @@ instance Logic KD RKD where
   clauses (RKDPos n) = [Implies (Set.fromList [1..n]) (Set.singleton (n+1))]
   clauses (RKDNeg n) = [Implies (Set.fromList [1..n]) (Set.singleton (n+1))]
   subclauses (Implies n p) = Set.fromList [Implies n (Set.singleton l) | l <- p]
-
+-}
