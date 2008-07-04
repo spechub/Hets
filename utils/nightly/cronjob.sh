61ed8286ea9913a9a2a762ae33039f9a1965b8f3
Christian Maeder <Christian.Maeder@dfki.de>
2008-07-04 17:20:38 +0000

    reorganized git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@10263 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/nightly/cronjob.sh
+++ utils/nightly/cronjob.sh
@@ -3,6 +3,17 @@
 GHCRTS='-H300m -M1g'
 export GHCRTS
 
+hetsdir=\
+/home/www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets
+destdir=$hetsdir/src-distribution/daily
+outtypes=env,thy,th,dfg,dfg.c,pp.het,pp.tex
+
+export hetsdir
+export destdir
+export outtypes
+
+unset LANG
+
 makeUni ()
 {
 date
@@ -31,7 +42,6 @@ date
 rm -rf Hets
 svn co https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk Hets
 cd Hets
-##$MAKE package_clean
 time $MAKE
 time $MAKE check
 $MAKE release
@@ -40,41 +50,94 @@ date
 cd ..
 }
 
+latexBasicLibraries ()
+{
+\cp ../Hets/utils/hetcasl.sty .
+pdflatex Basic-Libraries
+}
+
+checkCASLAsHasCASL ()
+{
+date
+for i in Basic/*.casl; do ./hets -v2 -l HasCASL -o th,pp.het,pp.tex $i; done
+latexBasicLibraries
+date
+for i in Basic/*.pp.het; do ./hets -v2 -l HasCASL -o pp.het,pp.tex $i; done
+\ls -l Basic/*.pp.*
+\rm -f Basic/*.pp.*
+}
+
+checkBasicCASL ()
+{
+date
+for i in Basic/*.casl; do ./hets -v2 -o $outtypes $i; done
+date
+for i in Basic/*.pp.het; do ./hets -v2 -o pp.het,pp.tex $i; done
+\ls -l Basic/*.pp.*
+}
+
+checkUserManual ()
+{
+date
+for i in UserManual/*.casl; do ./hets -v2 -o $outtypes $i; done
+}
+
+reCheckBasicCASLThs ()
+{
+date
+for i in Basic/*.th; do ./hets -v2 -o th,pp.het $i; done
+}
+
+checkHasCASL ()
+{
+date
+for i in HasCASL/*.het HasCASL/Metatheory/*.het;
+    do ./hets -v2 -o env,th,prf,pp.het,pp.tex $i; done
+pdflatex HasCASL/Metatheory/HasCASL-Metatheory-Libraries.tex
+}
+
 makeLibCheck ()
 {
 rm -rf Hets-lib
 svn co https://svn-agbkb.informatik.uni-bremen.de/Hets-lib/trunk Hets-lib
 cd Hets-lib
 mv ../Hets/Hets/hets .
-date
-for i in Basic/*.casl; do ./hets -v2 -l HasCASL -o th,pp.het,pp.tex $i; done
-
-\cp ../Hets/utils/hetcasl.sty .
-pdflatex Basic-Libraries
-
-date
-for i in Basic/*.pp.het; do ./hets -v2 -l HasCASL -o pp.het,pp.tex $i; done
-\ls -l Basic/*.pp.*
-\rm -f Basic/*.pp.*
-
-date
-for i in Basic/*.casl;
-    do ./hets -v2 -o env,thy,th,dfg,dfg.c,pp.het,pp.tex $i; done
-date
-for i in Basic/*.pp.het; do ./hets -v2 -o pp.het,pp.tex $i; done
-\ls -l Basic/*.pp.*
-
-date
-for i in UserManual/*.casl;
-    do ./hets -v2 -o env,thy,th,dfg,dfg.c,pp.het,pp.tex $i; done
-
-date
-for i in Basic/*.th; do ./hets -v2 -o th,pp.het $i; done
-
-date
-for i in HasCASL/*.het HasCASL/Metatheory/*.het;
-    do ./hets -v2 -o env,th,prf,pp.het,pp.tex $i; done
-pdflatex HasCASL/Metatheory/HasCASL-Metatheory-Libraries.tex
+checkCASLAsHasCASL
+checkBasicCASL
+checkUserManual
+reCheckBasicCASLThs
+checkHasCASL
 date
 cd ..
+}
+
+installHetsBinary ()
+{
+chmod 775 hets
+chgrp wwwbkb hets
+bzip2 -k hets
+\cp -fp hets.bz2 $hetsdir/$1/daily/
+}
+
+copyStyForCgi ()
+{
+\cp -f ../Hets/utils/hetcasl.sty \
+  /home/www.informatik.uni-bremen.de/cofi/hets-tmp/
+}
+
+createLogFiles ()
+{
+cat */*.th > ../th.log
+cat */*.pp.het > ../pp.log
+}
+
+runIsaBasic ()
+{
+../Hets/utils/nighly/runisabelle.sh Basic/*.thy > ../isa.log 2>&1
+fgrep \*\*\* ../isa.log
+}
+
+runSPASSBasic ()
+{
+../Hets/utils/nighly/runSPASS.sh Basic/*.dfg > ../spass.log 2>&1
 }
