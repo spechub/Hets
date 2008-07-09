be26aa582094f9065f21fb563944882a46eacc7b
Christian Maeder <Christian.Maeder@dfki.de>
2008-07-09 14:44:08 +0000

    also create sources and docu git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@10289 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/nightly/pc-solaris/cronjob.sh
+++ utils/nightly/pc-solaris/cronjob.sh
@@ -1,12 +1,10 @@
 #!/bin/bash -x
 
 PATH=/home/pub-bkb/bin:/usr/local/bin:/usr/bin/:/usr/local/X11/bin:/usr/ccs/bin:/home/pkgs/teTeX/3.0/bin/i686-pc-solaris2.10
-MAKE=gmake
 UDG_HOME=/home/pub-bkb/uDrawGraph-3.1
 HETS_LIB=/local/home/maeder/haskell/Hets-lib
 
 export PATH
-export MAKE
 export UDG_HOME
 export HETS_LIB
 
@@ -21,7 +19,17 @@ gmake hets.cgi
 cd ../..
 
 makeLibCheck
+installHetsBinary pc-solaris
+\cp -fp hets /home/pub-bkb/pc-solaris/bin/
+
+copyStyForCgi
+latexBasicLibraries
 createLogFiles
+
 runIsaBasic
 runSPASSBasic
-installHetsBinary pc-solaris
+
+makeSources
+makeCofiLib
+repackDocs
+updateLibForCgi
