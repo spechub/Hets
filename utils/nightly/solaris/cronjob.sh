62feb358c1c3f0ab21b91e411b125cb44315afc3
Christian Maeder <Christian.Maeder@dfki.de>
2008-07-09 10:07:43 +0000

    run fewer tests git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@10284 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/nightly/solaris/cronjob.sh
+++ utils/nightly/solaris/cronjob.sh
@@ -1,13 +1,11 @@
 #!/bin/bash -x
 
 PATH=/home/maeder/haskell/solaris/bin:/home/pub-bkb/bin:/usr/local/bin:/usr/bin/:/usr/local/X11/bin:/usr/ccs/bin
-MAKE=gmake
 UDG_HOME=/home/pub-bkb/uDrawGraph-3.1
 HETS_LIB=/home/maeder/haskell/V240-solaris/haskell/Hets-lib
 LD_LIBRARY_PATH=/home/maeder/haskell/solaris/lib:/usr/local/lib
 
 export PATH
-export MAKE
 export UDG_HOME
 export HETS_LIB
 export LD_LIBRARY_PATH
@@ -17,6 +15,6 @@ cd /home/maeder/haskell/V240-solaris/haskell
 . ../cronjob.sh
 
 makeHets
-makeLibCheck
+makeLibCheck faster
 
 installHetsBinary solaris
