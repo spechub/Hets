7b886e840402df85d5d3366b3f675b4c2cc1d2dd
Christian Maeder <Christian.Maeder@dfki.de>
2008-07-14 17:09:25 +0000

    shortened git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@10326 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/appendHaskellPreludeString.hs
+++ utils/appendHaskellPreludeString.hs
@@ -1,5 +1,5 @@
 {- |
-Module      :  $Id$
+Module      :  appendHaskellPreludeString.hs
 Copyright   :  (c) Christian Maeder, Uni Bremen 2002-2004
 License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
 
@@ -8,8 +8,9 @@ Stability   :  experimental
 Portability :  portable
 
 append a haskell Prelude string for programatica analysis
-
+  or add CASL_DL/PredDatatypes.het to CASL_DL/PredefinedSign.inline.hs
 -}
+
 module Main where
 
 import System.Environment
@@ -17,9 +18,8 @@ import System.Environment
 main :: IO ()
 main = do
     l <- getArgs
-    let preludeFileName = if null l then "Haskell/ProgramaticaPrelude.hs"
-                          else head l
-    preludeString <- readFile preludeFileName
+    preludeString <- readFile $ case l of
+      [] -> "Haskell/ProgramaticaPrelude.hs"
+      h : _ -> h
     str <- getContents
-    putStrLn (str ++ "\n " ++ show preludeString)
-
+    putStrLn $ str ++ "\n " ++ show preludeString
