3a57e77bdc4930299d938cddbe721ed2ec5bf2b1
Christian Maeder <Christian.Maeder@dfki.de>
2008-07-17 12:56:29 +0000

    use gtar under solaris git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@10350 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/install.sh
+++ utils/install.sh
@@ -1,19 +1,25 @@
 #!/bin/sh -x
 
 #change daily snapshot into a backup or release
-#the first argument should be a version number or a date  
-#compute date by "date -Idate"
+#the first argument should be a version number or a date
+#compute date by "date +%F"
 
-INSTALLDIR=/home/www/agbkb/forschung/formal_methods/CoFI/hets 
+INSTALLDIR=/home/www/agbkb/forschung/formal_methods/CoFI/hets
 VERSION=$1
+
+case `uname -s` in
+  SunOS) TAR=gtar;;
+  *) TAR=tar;;
+esac
+
 for i in linux solaris mac pc-solaris intel-mac
 do
   (cd $INSTALLDIR/$i; cp -p daily/hets.bz2 versions/hets-$VERSION.bz2)
 done
-      
+
 (cd $INSTALLDIR/src-distribution; \
  cp -p daily/Het*.t* versions/Hets-src-$VERSION.tgz; \
  cd versions; rm -rf Hets; \
- tar zxf Hets-src-$VERSION.tgz) 
+ $TAR zxf Hets-src-$VERSION.tgz)
 
 # also unpack the new release as "recent overview of the modules"
