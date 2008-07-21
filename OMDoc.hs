abcd9ba43db7f442cf51690f51e1c8bd34da76ff
Elena Digor <e.digor@jacobs-university.de>
2008-07-21 23:10:09 +0000

    OMDoc.hs description added git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@10378 cec4b9c1-7d33-0410-9eda-942365e851bb

--- OMDoc.hs
+++ OMDoc.hs
@@ -8,13 +8,28 @@ Maintainer  :
 Stability   :  provisional
 Portability :  portable
 
-The "OMDoc" folder contains the import and epxort functions for OMDoc
+The "OMDoc" folder contains the import and epxort functions from CASL to OMDoc
 (see <http://www.omdoc.org>).
-
 
 There is also an instance of "Logic.Logic" (the data for this is
 assembled in "OMDoc.Logic_OMDoc"), but this is not complete yet.
 
+The files "OMDocInput.hs" and "OMDocOutput.hs" are the main files responsible for the export and, respectively, import of the CASL files to OMDoc format. These are the central files, which are characterized by the following hierarchical absrtact layers:  
+
+1. CASL Abstract Syntax (which embeddes also the "CASL Text") is the lowest level, responsible for interpreting CASL concepts and syntax.
+
+2. OMDoc Abstract Syntax ( defined in "OMDocInterface.hs" ) is responsbile for representing the OMDoc's logic in an abstract way (with no XML tag deliminters). The communication channel between these 2 first layers is fortified by "OMDocDefs.hs" (deals exclusively with OMDoc namings) and "HetsDefs.hs" (deals with CASL data structures). This layer also communicates with "ATerms" layer via "ATerm.hs"  and "ATC_OMDoc.der.hs" (see "Logic_OMDoc.hs" for details) 
+
+3. XML Abstract Syntax is a middle layer, which adds/removes the OMDoc tag-elements, while preserving and sending the OMDoc contents to the lower layer (via "OMDocXML.hs" which is responsible for XML conversion for OMDoc model (in/out)).
+
+4. OMDoc XML is the top layer, which takes care of the final OMDoc input/or output files. It parses the OMDoc documents via an XML handler (see "XmlHandling.hs" and HXT package for details). 
+
+The "OMDoc" folder also contains some additional files, which are as well used in the transformation: 
+"Container.hs" and "Util.hs" - utility functions
+"HetsInterface.hspp"  - is a Test function
+"KeyDebug.hs" - used in debugging
+
+The "Basic" subdirectory contains the CASL's basic transformation in OMDoc format.
 
 -}
 
