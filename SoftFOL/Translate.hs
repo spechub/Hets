90a111bcb2ca1949abeda9747c9bc4d4b68a5551
Christian Maeder <Christian.Maeder@dfki.de>
2008-08-04 16:57:12 +0000

    change to lower case for non-vars git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@10444 cec4b9c1-7d33-0410-9eda-942365e851bb

--- SoftFOL/Translate.hs
+++ SoftFOL/Translate.hs
@@ -95,7 +95,7 @@ transIdString t str = case str of
       CKOp | '_' == c -> 'o' : str
       CKPred | '_' == c -> 'p' : str
       CKVar -> toUpper c : r
-      _ -> if Set.member str reservedWords then "x_" ++ str else str
+      _ -> if Set.member str reservedWords then "x_" ++ str else toLower c : r
 
 transToSPChar :: Char -> String
 transToSPChar c = if checkSPChar c then [c] else
