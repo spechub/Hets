1ab3cda9cf88318392b2c80dcf3082bfda51ec43
Christian Maeder <Christian.Maeder@dfki.de>
2008-08-19 14:01:11 +0000

    avoid trailing spaces in generated file git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@10535 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/build_version.pl
+++ utils/build_version.pl
@@ -1,4 +1,4 @@
-#!/usr/local/bin/perl 
+#!/usr/local/bin/perl
 
 # HetCATS/utils/pp_test.pl
 # $Id$
@@ -12,21 +12,19 @@ use POSIX qw(strftime);
 
 my $currdate = strftime("%d %b %Y",localtime);
 
-my $header = '
-   This file was generated 
-     by utils\/build_version.pl 
-     from Driver\/Version.in 
+my $header = 'This file was generated
+     by utils\/build_version.pl
+     from Driver\/Version.in
      on ' . $currdate . '!
 
-   Do not edit! 
-';
+   Do not edit!';
 
 my $version = "<unknown>";
 
 # check Arguments
 if (@ARGV == 1) {
     open VERS,"<".$ARGV[0] or
-	die "Coud not open $ARGV[0] for reading";
+        die "Coud not open $ARGV[0] for reading";
     $version = join "", <VERS>;
     close VERS;
     $version =~ s/\n//ogs;
