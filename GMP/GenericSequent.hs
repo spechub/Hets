dd223deccdb19969eed13be6f9254635bd0e4c65
Georgel Calin <g.calin@jacobs-university.de>
2008-08-21 12:43:43 +0000

    Idea on GeneriSequent for Compositional Logics. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@10553 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GMP/GenericSequent.hs
+++ GMP/GenericSequent.hs
@@ -1,22 +1,47 @@
+{-# OPTIONS -XMultiParamTypeClasses -XFlexibleInstances #-}
 module GMP.GenericSequent where
---   ((-->), (<-->), (\/), (/\), true, false, box, dia, neg, var,  provable)
+-- ((-->), (<-->), (\/), (/\), true, false, box, dia, neg, var,  provable)
 
-import Data.List (delete, intersect, nub)
+-- import Data.List (delete, intersect, nub)
 
--- | Data type for fomulas
-data Form = Var Int -- Var Char (Maybe Int)  -- variables
-          | Not !Form             -- ¬A
-	        | Box !Form             -- [] A
-	        | Conj !Form !Form      -- A/\B
-  deriving (Eq)
+-- | Datatype for fomulas
+data Form a b = Var Char (Maybe Int)              -- variables
+              | Not !(Form a b)                   -- negation: ¬
+	            | M a !(Form b a)                   -- modal atom: [] A
+	            | Conj !(Form a b) !(Form a b)      -- conjunction: A/\B
+  deriving (Eq, Show)
 
-type Sequent =  [Form]     -- defines a sequent
+-- | Datatype for defining a sequent
+type Sequent a b = [Form a b]
 
--- | Show instance for Form datatype
-instance Show Form where
-  show f = case f of
-            Var i    -> "p" ++ show i
-            Not g    -> "~" ++ show g
-            Box g    -> "[]" ++ show g
-            Conj g h -> "(" ++ show g ++ "&" ++ show h ++ ")"
+{- | Logic Class : two "ordered" modal logic types with "clause" matching rule
+ -}
+class (Eq a, Eq b, Show a, Show b) => Logic a b where
+  match :: Sequent a b -> [Sequent b a]
+
+-- | Datatypes for the supported logics
+data K = K deriving (Eq, Show)
+data KD = KD deriving (Eq, Show)
+
+-- | Instance of logic K & a
+instance (Show a, Eq a) => Logic K a where
+  match x = []
+
+-- ............
+
+
+
+------------------------------------------------------------------------------
+
+-- | "False" as deduced out of the Form datatype
+false :: Form a b
+false = Conj (Var ' ' Nothing) (Not (Var ' ' Nothing))
+
+-- | "Negation" of a form
+neg :: Form a b -> Form a b
+neg x = Not x
+
+-- | "True" as deduced from "False" and "Negation"
+true :: Form a b
+true = neg false
 
