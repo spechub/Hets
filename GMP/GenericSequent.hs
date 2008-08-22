dd9a95b10aff801ff0aa543dc7df69782652cdf8
Georgel Calin <g.calin@jacobs-university.de>
2008-08-22 11:12:58 +0000

    Structure change for dealing with composite logics + gentzen calculus git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@10568 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GMP/GenericSequent.hs
+++ GMP/GenericSequent.hs
@@ -1,50 +1,9 @@
-{-# OPTIONS -XMultiParamTypeClasses -XFlexibleInstances #-}
-module GMP.GenericSequent where
--- ((-->), (<-->), (\/), (/\), true, false, box, dia, neg, var,  provable)
+module GenericSequent where
 
--- import Data.List (delete, intersect, nub)
+import AbstractSyntax
+import qualified Data.List as List
+import qualified Data.Set as Set
 
--- | Datatype for fomulas
-data Form a b = Var Char (Maybe Int)              -- variables
-              | Not !(Form a b)                   -- negation: ¬
-	            | M a !(Form b a)                   -- modal atom: [] A
-	            | Conj !(Form a b) !(Form a b)      -- conjunction: A/\B
-  deriving (Eq, Show)
-
--- | Datatype for defining a sequent
-type Sequent a b = [Form a b]
-
-{- | Logic Class : two "ordered" modal logic types with "clause" matching rule
- -}
-class (Eq a, Eq b, Show a, Show b) => Logic a b where
-  match :: Sequent a b -> [Sequent b a]
-
--- | Datatypes for the supported logics
-data K = K deriving (Eq, Show)
-data KD = KD deriving (Eq, Show)
-
--- | Instance of logic K & a
-instance (Show a, Eq a) => Logic K a where
-  match x = []
-
--- ............
-
-provable :: Logic a b => Sequent a b -> Bool
-provable _ = True
-
--- ............
-
-------------------------------------------------------------------------------
-
--- | "False" as deduced out of the Form datatype
-false :: Form a b
-false = Conj (Var ' ' Nothing) (Not (Var ' ' Nothing))
-
--- | "Negation" of a form
-neg :: Form a b -> Form a b
-neg x = Not x
-
--- | "True" as deduced from "False" and "Negation"
-true :: Form a b
-true = neg false
+-- testing Segala
+-- test = S (KD (Or (At (K (Or (At (S (KD T))) F))) (Or F (At (K F)))))::Segala 
 
