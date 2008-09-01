acaf0ef7a70127ae45f77d6a5b4326c80b8bb2fa
Georgel Calin <g.calin@jacobs-university.de>
2008-09-01 11:09:07 +0000

    Changes in the CombLogic implementation git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@10612 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GMP/CombLogic.hs
+++ GMP/CombLogic.hs
@@ -4,8 +4,8 @@ module CombLogic where
 import ModalLogic
 import Data.List
 
-{-
 data Box b c = Box b (Boole c)
+{-
 data BBox b c = BBox b (Boole c) (Boole c)
 
 data K_KD = K_KD (Box K_u KD_K)
@@ -20,39 +20,35 @@ data KD_u = KD_u deriving (Show, Eq)
  -       b is the index type corresponding to a
  -       c is the "result" logic type
  -}
-
-{- initial version ...
 class Form a b c | a->b, a->c where
-  extract :: Form a b c => a -> [Box b c]
+  extract :: Form a b c => a -> Box b c
   provable :: Form a b c => a -> Bool
--}
-
-{- | Class for Formulas where
- -      a is the "input" logic
- -      b is the "result" logic
- -}
-class Form a b | a -> b where
-  extract :: Form a b => a -> [b]
-  provable :: Form a b => a -> Bool
 
 --instance Form K_KD K_u KD_K where
 --instance Form (K (KD l)) K_u (KD l) where
 --instance Eq l => Form (K (KD l)) (KD l) where
-instance Eq l => Form (K l) l where
-  extract (K f) = case f of
+instance Eq l => Form (K l) () l where
+  extract (K f) = Box () f
+  {-
+                  case f of
                     Not g   -> extract (K g)
                     And g h -> (extract (K g)) `union` (extract (K h))
                     At a    -> [a]
                     _       -> []
+   -}
 
 --instance Eq l => Form (KD (K l)) (K l) where
-instance Eq l => Form (KD l) l where
-  extract (KD f) = case f of
+instance Eq l => Form (KD l) () l where
+  extract (KD f) = Box () f
+  {-
+                   case f of
                      Not g   -> extract (KD g)
                      And g h -> (extract (KD g)) `union` (extract (KD h))
                      At a    -> [a]
                      _       -> []
+   -}
 
 --instance Form KD_K KD_u K_KD -- where
 --instance Form (KD (K l)) KD_u (K l) -- where
--- instance Form (G l) Int l
+instance Eq l => Form (G l) Int l where
+  extract (G i f) = Box i f
