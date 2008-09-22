edbee37fd0c724e09c8471d25d0dc48ac84ac12c
Georgel Calin <g.calin@jacobs-university.de>
2008-09-22 13:41:57 +0000

    Further changes on GMP-CoLoSS implementation git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@10705 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GMP/Main.hs
+++ GMP/Main.hs
@@ -10,73 +10,24 @@
  -}
 module Main where
 
-import Text.ParserCombinators.Parsec
+import GenericSequent
+import Parser
+
 import System.Environment
 import IO
 
---import Parser
-import GenericSequent
-{-
+-- | Map logic indexes from [Char] to Int
+transformLogicIndex :: [Char] -> Int
+transformLogicIndex c = case c of
+                         "K"  -> 1
+                         "KD" -> 2
+                         "C"  -> 3
+                         "G"  -> 4
+                         "P"  -> 5
+                         "HM" -> 6
+                         "M"  -> 7
+                         _    -> error "Main.transformLogicIndex"
 
--- | Replaces all conjunctions by disjunctions and normalizes negations
-preparse :: L a -> L a
-preparse f = 
-  let conj2disj w = case w of
-                      And x y -> let a = conj2disj x
-                                     b = conj2disj y
-                                 in Neg (Or (Neg a) (Neg b))
-                      Or x y  -> let a = conj2disj x
-                                     b = conj2disj y
-                                 in Or a b
-                      Neg x   -> let a = conj2disj x
-                                 in Neg a
-                      M i x   -> M i $ conj2disj x
-                      x       -> x
-      negNorm w = case w of
-                    Neg (Neg x) -> negNorm x
-                    Neg x       -> Neg $ negNorm x
-                    M i x       -> M i $ negNorm x
-                    Or x y      -> let a = negNorm x
-                                       b = negNorm y
-                                   in Or a b
-                    x           -> x -- there is no need for discussing "And"
-  in negNorm $ conj2disj f
--- | Runs the parser and the prover and prints the result(s) of obtained.
-runLex :: (Logic a, Eq a, Show a) => Parser (L a) -> String -> IO ()
-runLex p_rL input = run (do spaces
-                            x <- p_rL 
-                            eof
-                            return x
-                        ) input
-
-run :: (Logic a, Eq a, Show a) => Parser (L a) -> String -> IO ()
-run p_r input = case (parse p_r "" input) of
-                  Left err -> do putStr "parse error at "
-                                 print err
-                  Right y ->  do let x = preparse y
-                                 --putStrLn (show x{-++" <=> "++input-})
-                                 let isP = provable x
-                                 case isP of
-                                    True -> putStrLn "... is Provable"
-                                    _    -> let isS = sat x
-                                            in case isS of
-                                                 True -> putStrLn "... is not Provable but Satisfiable"
-                                                 _    -> putStrLn "... is not Satisfiable"
-
--- | Auxiliary run function for testing with the input given as string
-runTest :: [Int] -> String -> IO ()
-runTest ml input = do
-    case (head ml) of
-     1 -> runLex ((par5er Sqr parseKindex) :: Parser (L K)) input
-     2 -> runLex ((par5er Sqr parseKDindex) :: Parser (L KD)) input
-     3 -> runLex ((par5er Sqr parseCindex) :: Parser (L C)) input
-     4 -> runLex ((par5er Ang parseGindex) :: Parser (L G)) input
-     5 -> runLex ((par5er Ang parsePindex) :: Parser (L P)) input
-     6 -> runLex ((par5er Sqr parseHMindex) :: Parser (L HM)) input
-     7 -> runLex ((par5er Sqr parseMindex) :: Parser (L Mon)) input
-     _ -> showHelp
-    return ()
--}
 -- | Function for displying user help 
 showHelp :: IO()
 showHelp = do
@@ -85,16 +36,17 @@ showHelp = do
                "    ./main -t <test> <N> <L1> <L2> .. <LN>\n\n" ++
                "<N>:     a natural number >0 specifing the number of combined/embedded logics\n" ++
                "<Lx>:    each logic can be one of the following cases:\n" ++
-               "              1 - K Modal Logic\n" ++
-               "              2 - KD Modal Logic\n" ++
-               "              3 - Coalition Logic\n" ++
-               "              4 - Graded Modal Logic\n" ++
-               "              5 - Probability Logic\n" ++
-               "              6 - Hennessy-Milner Modal Logic\n" ++
-               "              7 - Monotonic Logic\n" ++
+               "              K  - K Modal Logic\n" ++
+               "              KD - KD Modal Logic\n" ++
+               "              C  - Coalition Logic\n" ++
+               "              G  - Graded Modal Logic\n" ++
+               "              P  - Probability Logic\n" ++
+               "              HM - Hennessy-Milner Modal Logic\n" ++
+               "              M  - Monotonic Logic\n" ++
                "<path>:  path to input file\n" ++
                "<test>:  test given as a string\n")
--- | main program function
+
+-- | Main program function
 main :: IO()
 main = do
     args <- getArgs
@@ -106,7 +58,7 @@ main = do
              then showHelp
              else let list = take (read n) rest
                   in case it of
-                       "-p" -> do input <- readFile test
-                                  putStrLn input
-                       "-t" -> putStrLn test
+                       "-p" -> do test <- readFile test
+                                  putStrLn test -- run prover with given input
+                       "-t" -> putStrLn test -- run prover with given input
                        _    -> showHelp
---
+++ GMP/Parser.hs
@@ -0,0 +1,233 @@
+{- | Module     : $Header$
+ -  Description : Implementation of logic formula parser
+ -  Copyright   : (c) Georgel Calin & Lutz Schroeder, DFKI Lab Bremen
+ -  License     : Similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
+ -  Maintainer  : g.calin@jacobs-university.de
+ -  Stability   : provisional
+ -  Portability : portable
+ -
+ -  Provides the implementation of the generic parser for the Boole datatype
+ -}
+module Parser where
+
+import Text.ParserCombinators.Parsec
+import ModalLogic
+import GenericSequent
+
+{-
+-- | Main parser
+par5er :: [GenParser Char st a] -> GenParser Char st (Boole a)
+par5er pa = implFormula pa
+
+-- | Parser which translates all implications in disjunctions & conjunctions
+implFormula :: [GenParser Char st a] -> GenParser Char st (Boole a)
+implFormula pa = do
+    f <- orFormula pa
+    option f (do string "->"
+                 spaces
+                 i <- implFormula pa
+                 return $ Or (Neg f) i
+          <|> do try(string "<->")
+                 spaces
+                 i <- implFormula pa
+                 return $ And (Or (Neg f) i) (Or f (Neg i))
+          <|> do string "<-"
+                 spaces
+                 i <- implFormula pa
+                 return $ Or f (Neg i)
+          <|> do return f
+          <?> "GMPParser.implFormula")
+
+-- | Parser for disjunction - used for handling binding order
+orFormula :: [GenParser Char st a] -> GenParser Char st (Boole a)
+orFormula pa = do
+    f <- andFormula pa
+    option f $ do
+      string "\\/"
+      spaces
+      g <- orFormula pa
+      return $ Or f g
+  <?> "GMPParser.orFormula"
+
+-- | Parser for conjunction - used for handling the binding order
+andFormula :: [GenParser Char st a] -> GenParser Char st (Boole a)
+andFormula pa = do
+    f <- primFormula pa
+    option f $ do
+      string "/\\"
+      spaces
+      g <- andFormula pa
+      return $ And f g
+  <?> "GMPParser.andFormula"
+
+          
+{- | Parse a primitive formula: T, F, ~f, <i>f, [i]f, p*, 
+ -   where i stands for an index, f for a formula and 
+ -   * for a series of digits i.e. and integer -}
+primFormula :: GenParser Char st a -> GenParser Char st (Boole a)
+primFormula pa =  do string "T"
+                     spaces
+                     return T
+              <|> do string "F"
+                     spaces
+                     return F
+              <|> do f <- parenFormula pa
+                     return f
+              <|> do string "~"
+                     spaces
+                     f <- primFormula pa
+                     return $ nneg f
+              <|> do char '<'
+                     spaces
+                     i <- pa
+                     spaces
+                     char '>'
+                     spaces
+                     f <- primFormula pa
+                     return $ M i f
+              <|> do char '['
+                     spaces
+                     i <- pa
+                     spaces
+                     char ']'
+                     spaces
+                     f <- primFormula flag pa
+                     return $ M i f
+              <?> "GMPParser.primFormula"
+
+-- | Parser for un-parenthesizing a formula
+parenFormula :: GenParser Char st a -> GenParser Char st (Boole a)
+parenFormula pa =  do char '('
+                      spaces
+                      f <- par5er flag pa
+                      spaces
+                      char ')'
+                      spaces
+                      return f
+               <?> "GMPParser.parenFormula"
+-}
+-- | Parse integer number
+natural :: GenParser Char st Integer
+natural = fmap read $ many1 digit 
+
+-- | Parser for Coalition Logic index
+parseCindex :: Parser [Int]
+parseCindex =  do -- checks whether there are more numbers to be parsed
+                  let stopParser =  do char ','
+                                       return False
+                                <|> do char '}'
+                                       return True
+                                <?> "Parser.parseCindex.stop"                  
+                  -- checks whether the index is of the for x1,..,x&
+                  let normalParser l =  do x <- natural
+                                           let n = fromInteger x
+                                           spaces
+                                           q <- stopParser
+                                           spaces
+                                           case q of
+                                             False -> normalParser (n:l)
+                                             _     -> return (n:l)
+                                    <?> "Parser.parseCindex.normal"
+                  char '{'
+                  res <- try(normalParser [])
+                  return $ res
+           <|> do -- checks whether the index is of the form "n..m"
+                  let shortParser =  do x <- natural
+                                        let n = fromInteger x
+                                        spaces
+                                        string ".."
+                                        spaces
+                                        y <- natural
+                                        let m = fromInteger y
+                                        return $ [n..m]
+                                 <?> "Parser.parseCindex.short"
+                  res <- try(shortParser)
+                  return $ res
+           <?> "Parser.parseCindex"
+
+-- | Parser for Graded Modal Logic index
+parseGindex :: Parser Int
+parseGindex =  do n <- natural
+                  return $ fromInteger n
+           <?> "Parser.parseGindex"
+
+-- | Parser for Hennesy-Milner Modal Logic index
+parseHMindex :: Parser Char
+parseHMindex =  do c <- letter
+                   return $ c
+            <?> "Parser.parseHMindex"
+
+-- | Parser for K Modal Logic index
+parseKindex :: Parser ()
+parseKindex = return ()
+
+-- | Parser for KD Modal Logic index
+parseKDindex ::Parser ()
+parseKDindex = return ()
+
+-- | Parser for Probability Logic index
+parsePindex :: Parser Rational
+parsePindex = 
+    do x <- natural
+       let auxP n =  do char '/'
+                        m<-natural
+                        return $ toRational (fromInteger n/fromInteger m)
+                 <|> do char '.'
+                        m<-natural
+                        let noDig n = let tmp = n<10
+                                      in case tmp of
+                                           True -> 1
+                                           _    -> 1 + noDig (div n 10)
+                        let rat n = toRational(fromInteger n / 
+                                               fromInteger (10^(noDig n)))
+                        let res = toRational n + rat m
+                        return res
+                 <|> do return $ toRational n
+                 <?> "Parser.parsePindex.auxP"
+       aux <- auxP x
+       return $ aux
+
+-- | Parser for Monotonic Modal Logic index
+parseMindex :: Parser ()
+parseMindex = return ()
+
+{-
+
+run :: Parser (Boole a) -> String -> IO ()
+run p_r input = case (parse p_r "" input) of
+                  Left err -> do putStr "parse error at "
+                                 print err
+                  Right y ->  do let x = preparse y
+                                 --putStrLn (show x{-++" <=> "++input-})
+                                 let isP = provable x
+                                 case isP of
+                                    True -> putStrLn "... is Provable"
+                                    _    -> let isS = sat x
+                                            in case isS of
+                                                 True -> putStrLn "... is not Provable but Satisfiable"
+                                                 _    -> putStrLn "... is not Satisfiable"
+
+-- | Runs the parser and the prover and prints the result(s) obtained.
+runLex :: Parser (Boole a) -> String -> IO ()
+runLex parser input = run (do spaces
+                            res <- parser
+                            eof
+                            return res
+                        ) input
+
+
+-- | Auxiliary run function for testing with the input given as string
+runTest :: [Int] -> String -> IO ()
+runTest ml input = do
+    let h = head ml; t = tail ml
+    in case h of
+      1 -> runLex ((par5er Sqr parseKindex) :: Parser (L K)) input
+      2 -> runLex ((par5er Sqr parseKDindex) :: Parser (L KD)) input
+      3 -> runLex ((par5er Sqr parseCindex) :: Parser (L C)) input
+      4 -> runLex ((par5er Ang parseGindex) :: Parser (L G)) input
+      5 -> runLex ((par5er Ang parsePindex) :: Parser (L P)) input
+      6 -> runLex ((par5er Sqr parseHMindex) :: Parser (L HM)) input
+      7 -> runLex ((par5er Sqr parseMindex) :: Parser (L Mon)) input
+      _ -> showHelp
+    return ()
+-}
