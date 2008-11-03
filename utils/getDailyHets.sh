5fe1c87d7201d2e97d4ef280c9af0c6eb026e575
Christian Maeder <Christian.Maeder@dfki.de>
2008-11-03 11:30:00 +0000

    support for arguments git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@10936 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/getDailyHets.sh
+++ utils/getDailyHets.sh
@@ -1,21 +1,38 @@
 #!/bin/bash
 
-## This script downloads a daily hets into your ~/bin directory
+## This script downloads a version of hets (second argument) 
+## into a directory (first argument) 
+## the daily hets version and your bin directory is used without arguments
 
-DAILY_HETS=$HOME/bin/hets-`date +%F`
+if [ -z "$1" ] ; then
+   INSTALLDIR="$HOME/bin"
+else
+   INSTALLDIR="$1"
+fi
+
+if [ -z "$2" ] ; then # daily
+ TARGET_HETS=hets-`date +%F`
+ SOURCE_HETS=daily/hets.bz2
+else # version
+ TARGET_HETS=hets-$2
+ SOURCE_HETS=versions/hets-$2.bz2
+fi   
+
+DAILY_HETS=$INSTALLDIR/$TARGET_HETS
 DAILY_HETS_TARGET=$DAILY_HETS.bz2
 
-if [ -d "$HOME/bin" ] ; then
+if [ -d "$INSTALLDIR" ] ; then
     if [ -e "$DAILY_HETS" ] ; then
-        mv $DAILY_HETS "$DAILY_HETS~"
-        echo generated backup of exisiting file '"'$DAILY_HETS'"'
+        mv "$DAILY_HETS" "$DAILY_HETS"~
+        echo generated backup file: $DAILY_HETS~
     fi
     if [ -e "$DAILY_HETS_TARGET" ] ; then
         rm -f "$DAILY_HETS_TARGET"
-        echo removed exisiting file '"'"$DAILY_HETS_TARGET"'"'
+        echo removed file: $DAILY_HETS_TARGET
     fi
 else
-    mkdir $HOME/bin
+    echo created directory $INSTALLDIR
+    mkdir -p "$INSTALLDIR"
 fi
 
 ARCH_DIR=""
@@ -29,7 +46,7 @@ case `uname -s` in
             *86*)
                 ARCH_DIR=pc-solaris ;;
             *)
-                echo "Unsupported SunOS processor: " `uname -p`
+                echo Unsupported SunOS processor: `uname -p`
                 exit 2;;
         esac;;
     *inux)
@@ -41,19 +58,19 @@ case `uname -s` in
             *86)
                 ARCH_DIR=intel-mac ;;
             *)
-                echo "Unsupported Mac OS X processor: " `uname -p`
+                echo Unsupported Mac OS X processor: `uname -p`
                 exit 2;;
         esac;;
     *)
-        echo "Unsupported system: " `uname -s`
+        echo Unsupported system: `uname -s`
         exit 2;;
 esac
 
-DAILY_HETS_URL=http://www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets/$ARCH_DIR/daily/hets.bz2
+DAILY_HETS_URL=http://www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets/$ARCH_DIR/$SOURCE_HETS
 
 # main
 $DOWNLOADCMD $DAILY_HETS_URL || exit 1
-bunzip2 $DAILY_HETS_TARGET || exit 1
-chmod a+x $DAILY_HETS || exit 1
+bunzip2 "$DAILY_HETS_TARGET" || exit 1
+chmod a+x "$DAILY_HETS" || exit 1
 
 echo "Downloaded and saved daily hets to: " $DAILY_HETS
