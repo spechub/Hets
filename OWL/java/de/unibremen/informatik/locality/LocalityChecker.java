cb913c16ce42ac80b1b4dffb68050f11772374a9
Dominik Luecke <luecke@informatik.uni-bremen.de>
2008-11-20 16:23:16 +0000

    stub for locality git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@11045 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ OWL/java/de/unibremen/informatik/locality/LocalityChecker.java
@@ -0,0 +1,9 @@
+package de.unibremen.informatik.locality;
+
+import com.clarkparsia.modularity.locality.LocalityClass;
+import com.clarkparsia.modularity.locality.LocalityEvaluator;
+import com.clarkparsia.modularity.locality.SyntacticLocalityEvaluator;
+
+public class LocalityChecker {
+
+}
