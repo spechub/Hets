94db5ecf4260f9150852aedd254dfc9723718a28
Christian Maeder <Christian.Maeder@dfki.de>
2008-11-21 14:18:05 +0000

    Leopard only git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@11048 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/nightly/intel-mac/cronjob.sh
+++ utils/nightly/intel-mac/cronjob.sh
@@ -3,7 +3,6 @@
 export PATH=/home/pub-bkb/bin:/usr/local/bin:/bin:/usr/bin:/usr/X11R6/bin:/opt/local/bin:/sw/bin
 export UDG_HOME=/home/mac-bkb/uDrawGraph-3.1
 export HETS_LIB=/Users/Shared/maeder/haskell/Hets-lib
-export MACOSX_DEPLOYMENT_TARGET=10.4 #this is not enough
 
 cd /Users/Shared/maeder/haskell
 
