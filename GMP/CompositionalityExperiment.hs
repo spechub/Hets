0cf60d15a70b2b9922248afc03e8685407619c8f
Lutz Schröder <lutz.schroeder@cs.fau.de>
2008-12-23 12:58:15 +0000

    old ideas on compositionality git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@11192 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ GMP/CompositionalityExperiment.hs
@@ -0,0 +1,51 @@
+{-# OPTIONS -fglasgow-exts #-}
+module Comp where
+
+import Debug.Trace
+import qualified Data.Set as Set
+import qualified Data.Map as Map
+
+data Boole a = F | T | And (Boole a) (Boole a) | Or (Boole a) (Boole a) | 
+     	      Not (Boole a) | At a deriving (Eq, Ord, Show)
+
+data K l = K (Boole l) deriving (Eq, Ord, Show)
+
+data RK = RK Int deriving Show
+
+data KD l  = KD (Boole l) deriving (Eq, Ord, Show)
+
+data RKD = RKDPos Int | RKDNeg Int deriving Show
+
+data Segala a = S (Boole (KD (Boole (K (Segala a))))) deriving (Eq, Show)
+
+data Clause a = Implies (Set.Set a) (Set.Set a) deriving (Eq, Ord, Show)
+
+data Subst a = Subst (Map.Map Int a) deriving (Eq, Show)
+
+class Logic a b | a -> b, b -> a where
+  match :: Clause (a c) -> [(b, Subst (Boole c))]
+  clauses :: b -> [Clause Int]
+  subclauses :: Ord c => Clause (a c) -> Set.Set (Clause (a c))
+
+instance Logic K RK where
+  match ((Implies n p)::Clause (K c)) = 
+    let i = Set.size n
+        strip (K x) = x
+        substHead = (i+1,strip(head(Set.elems p)))
+        substTail = zip [1..i] (map strip (Set.elems n))
+    in [(RK i, Subst (Map.fromList (substHead:substTail)))]
+  clauses (RK n) = [Implies (Set.fromList [1..n]) (Set.singleton (n+1))]
+  subclauses (Implies n p) = 
+    Set.fromList [Implies n (Set.singleton l) | l <- Set.elems p]
+
+instance Logic KD RKD where
+  match ((Implies n p):: Clause (KD c)) = 
+    let i = Set.size n 
+        strip (KD x) = x
+        substHead = (i+1,strip(head(Set.elems p)))
+        substTail = zip [1..i] (map strip (Set.elems n))
+    in [(RKDPos i, Subst (Map.fromList (substHead:substTail)))]
+  clauses (RKDPos n) = [Implies (Set.fromList [1..n]) (Set.singleton (n+1))]
+  clauses (RKDNeg n) = [Implies (Set.fromList [1..n]) (Set.singleton (n+1))]
+  subclauses (Implies n p) = 
+    Set.fromList [Implies n (Set.singleton l) | l <- Set.elems p]
