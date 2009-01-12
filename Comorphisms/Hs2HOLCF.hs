f6d6dd9e66649fcd93bbac447c395d8d8a7ca188
Christian Maeder <Christian.Maeder@dfki.de>
2009-01-12 16:49:43 +0000

    import qualified haskell modules git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@11254 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Comorphisms/Hs2HOLCF.hs
+++ Comorphisms/Hs2HOLCF.hs
@@ -13,8 +13,6 @@ theory translation for the embedding comorphism from Haskell to Isabelle.
 
 module Comorphisms.Hs2HOLCF (transTheory) where
 
-import Monad
-
 import qualified Data.Map as Map
 import Common.Result
 import Common.AS_Annotation
