28f336e639eae9e0a940a673159198d9ef4e5613
Ewaryst Schulz <Ewaryst.Schulz@dfki.de>
2009-02-19 14:11:47 +0000

    Added the Datatype definitions and the empty xmlinterface git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@11404 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ OMDoc/DataTypes.hs
@@ -0,0 +1,61 @@
+{- |
+Module      :  $Header$
+Description :  The OMDoc Data Types
+Copyright   :  (c) Ewaryst Schulz, DFKI 2008
+License     :  similar to LGPL, see HetCATS/LICENSE.txt
+
+Maintainer  :  Ewaryst.Schulz@dfki.de
+Stability   :  provisional
+Portability :  portable
+
+data types for OMDoc
+-}
+module OMDoc.DataTypes where
+
+{-
+  OMDoc represented in 3 layers:
+  1. toplevel (theory, view)
+  2. theory constitutive (axiom, symbol)
+  3. subelements (FMP, Insort) and OpenMath
+-}
+
+
+-- | Theory constitutive Elements for OMDoc
+data TCElement = TCAxiomOrTheorem Bool String OMElement
+               | TCSymbol String OMElement
+               | TCImport
+                 deriving (Show, Eq, Ord)
+
+-- | Top level Elements for OMDoc
+data TLElement = TLTheory [TCElement]
+               | TLView
+                 deriving (Show, Eq, Ord)
+
+-- | Names used for OpenMath variables and symbols
+data OMName = OMName String deriving (Show, Eq, Ord)
+
+-- | attribute-name attribute-value pair
+data OMAttribute = OMAttr OMElement OMElement
+                      deriving (Show, Eq, Ord)
+
+-- | CD contains the reference to the content dictionary
+-- | as name and eventually the CDBase entry
+data OMCD = CD String (Maybe String) deriving (Show, Eq, Ord)
+
+-- | Elements for Open Math
+data OMElement =
+    -- | Symbol
+    OMS OMCD OMName
+    -- | Simple variable
+  | OMV OMName
+    -- | Attributed element needed for type annotations of elements
+  | OMATTT OMElement OMAttribute
+    -- | Application to a list of arguments,
+    -- | first argument is usually the functionhead
+  | OMA [OMElement]
+    -- | Bindersymbol, bound vars, body
+  | OMBIND OMElement [OMElement] OMElement
+    -- | Comment
+  | OMC String
+  deriving (Show, Eq, Ord)
+
---
+++ OMDoc/XmlInterface.hs
@@ -0,0 +1,19 @@
+{- |
+Module      :  $Header$
+Description :  OMDoc-to-XML conversion
+Copyright   :  (c) Ewaryst Schulz, DFKI 2009
+License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
+
+Maintainer  :  ewaryst.schulz@dfki.de
+Stability   :  provisional
+Portability :  non-portable(Logic)
+
+Implementation of the OMDoc-Datatype to XML transformation, there and back
+-}
+
+module OMDoc.XmlInterface
+  where
+
+import OMDoc.DataTypes
+import Text.XML.Light
+
