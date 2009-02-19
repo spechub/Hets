4db8da194089494405c029a2dc133d49fe861b4f
Ewaryst Schulz <Ewaryst.Schulz@dfki.de>
2009-02-19 18:51:33 +0000

    first experiments with xml.light git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@11409 cec4b9c1-7d33-0410-9eda-942365e851bb

--- OMDoc/XmlInterface.hs
+++ OMDoc/XmlInterface.hs
@@ -16,4 +16,29 @@ module OMDoc.XmlInterface
 
 import OMDoc.DataTypes
 import Text.XML.Light
+import Data.Set
 
+
+
+
+
+
+
+
+-- TESTPART:
+-- 
+-- h <- readFile "/tmp/Numbers.xml" >>= (\x -> return $ Hide $ Data.Maybe.fromJust $ parseXMLDoc x)
+
+-- fmap (length . show . (filter (\x -> case x of (CRef _) -> True ; _ -> False)) . elContent) h
+
+collectQNames :: (Set QName) -> Element -> (Set QName)
+collectQNames s (Element q _ c _) = insert q $ unions $ Prelude.map (collectQNames s) $ onlyElems c
+
+data Hide a = Hide a
+
+instance Functor Hide where fmap f (Hide x) = Hide (f x)
+
+instance Show a => Show (Hide a) where show (Hide x) = take 300 (show x)
+
+theHidden :: (Hide a) -> a
+theHidden (Hide x) = x
