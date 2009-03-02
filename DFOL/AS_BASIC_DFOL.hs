d4cc1082b15a477fd9ebc39e2bea83fdfea26a56
Kristina Sojakova <kristinas@cmu.edu>
2009-03-02 22:55:45 +0000

    Added pretty printing for DFOL git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@11463 cec4b9c1-7d33-0410-9eda-942365e851bb

--- DFOL/AS_BASIC_DFOL.hs
+++ DFOL/AS_BASIC_DFOL.hs
@@ -3,50 +3,57 @@ Module      :  $Header$
 Description :  Abstract syntax for first-order logic with dependent types (DFOL)
 -}
 
-module DFOL.AS_BASIC_DFOL where
+module DFOL.AS_BASIC_DFOL 
+    (
+      SIG (..)                 -- datatype for signatures
+    , CONTEXT                  -- datatype for contexts
+    , TYPE	               -- datatype for types
+    , TERM                     -- datatype for terms
+    , FORMULA (..)             -- datatype for formulas
+    , BASIC_SPEC (..)          -- Basic Spec    
+    ) where
 
 import Common.Id as Id
+import Common.Doc
+import Common.DocUtils
 
 type VARIABLE = Id.Token
 type CONSTANT = Id.Token
 
 
 -- grammar for signatures
-data SIG = Sig_empty
-         | Sig SIG (CONSTANT, TYPE)
+data SIG = Sig [(CONSTANT, TYPE)]
            deriving (Show, Eq)
 
 -- grammar for contexts 
-data CONTEXT = Context_empty
-             | Context CONTEXT (VARIABLE, TYPE)
+data CONTEXT = Context [(VARIABLE, TYPE)]
                deriving (Show, Eq)
 
 -- grammar for types 
 data TYPE = Sort
           | Form
           | Univ TERM
-          | Pi (VARIABLE, TYPE) TYPE
+          | Pi [(VARIABLE, TYPE)] TYPE
             deriving (Show, Eq)
 
 -- grammar for terms
-data TERM = Term_var VARIABLE
-          | Term_appl TERM TERM
+data TERM = Var VARIABLE
+          | Appl TERM TERM
             deriving (Show, Eq)
-               
 
-
+			
 --grammar for formulas
 data FORMULA = True
              | False
              | Pred TERM 
-             | Equality TERM TERM 
-             | Negation FORMULA 
+             | Equality TYPE TERM TERM 
+             | Negation FORMULA
              | Conjunction [FORMULA]
              | Disjunction [FORMULA]
              | Implication FORMULA FORMULA
              | Equivalence FORMULA FORMULA
-             | Forall [VARIABLE] TERM FORMULA
-             | Exists [VARIABLE] TERM FORMULA
+             | Forall [(VARIABLE, TYPE)] TERM FORMULA
+             | Exists [(VARIABLE, TYPE)] TERM FORMULA
                deriving (Show, Eq)
 
 
@@ -54,5 +61,67 @@ data FORMULA = True
 data BASIC_SPEC = Basic_spec SIG [FORMULA] 
                   deriving Show
 
- 
 
+-- pretty printing
+{-instance Pretty FORMULA where
+    pretty = printFormula
+instance Pretty BASIC_SPEC where
+    pretty = printBasicSpec
+instance Pretty SIG where
+    pretty = printSignature
+instance Pretty CONTEXT where
+    pretty = printContext
+instance Pretty TYPE where
+    pretty = printType
+instance Pretty TERM where
+    pretty = printTerm
+
+
+-- pretty printing for basic specs
+printBasicSpec :: BASIC_SPEC -> Doc
+--printBasicSpec (Basic_spec sig axioms) = hsep $ pretty sig map pretty axioms
+
+-- pretty printing for formulas
+printFormula :: FORMULA -> Doc
+printFormula (Negation f) = notDoc
+                            <> lparen <> printFormula f <> rparen
+printFormula (Conjunction xs) = parens $
+                                sepByArbitrary andDoc
+                                $ map printFormula xs
+printFormula (Disjunction xs) = parens $
+                                sepByArbitrary orDoc
+                                $ map printFormula xs
+printFormula (Implication x y) = parens $ printFormula x <>
+                                 implies <> printFormula y
+printFormula (Equivalence x y) = parens $ printFormula x <>
+                                 equiv <> printFormula y
+printFormula (True) = text "True"
+printFormula (False) = text "False"
+printFormula (Pred x) = printTerm x
+printFormula (Equality _ x y) = parens $ printTerm x <>
+                                equals <> printTerm y
+printFormula (Forall xs) = parens $
+                                sepByArbitrary andDoc
+                                $ map printFormula xs 
+
+			
+-- pretty printing for terms
+printTerm :: TERM -> Doc
+--printTerm (Var var) = pretty var
+--printTerm (Appl f a) = parens $ printTerm f <> printTerm a
+
+-- pretty printing for types
+printType :: TYPE -> Doc
+--printType (Sort) = text "Sort"
+--printType (Form) = text "Form"
+--printType (Univ t) = text "Univ" <> parens $ printTerm t
+--printType (Pi xs t) = text "Pi" <> map (\(v, t) -> pretty v <> text ":" <> pretty t <> ". ") xs <> pretty x 
+
+-- pretty printing for contexts
+printContext :: CONTEXT -> Doc
+--printType (Context xs) = parens $ map (\(v, t) -> pretty v <> text ":" <> pretty t <> ".") xs)
+
+ -- pretty printing for signatures
+printSignature :: SIG -> Doc
+--printSignature (Sig xs) = hsep $ map (\(c, t) -> pretty c <> text ":" <> pretty t) xs
+-}
