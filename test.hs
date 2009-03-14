e5e0e4cfae4acd7de3576081bd25d31207783fc4
Kristina Sojakova <kristinas@cmu.edu>
2009-03-14 22:28:07 +0000

    Test file for AS_DFOL git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@11522 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ test.hs
@@ -0,0 +1,44 @@
+{- |
+Description :  Test file for AS_DFOL
+-}
+
+import DFOL.AS_DFOL
+import Common.Id
+import Common.Doc
+import Common.DocUtils
+import Common.AS_Annotation
+
+natTok, matTok, mTok, nTok, aTok, bTok, plusTok :: NAME 
+
+natTok = Token "nat" $ Range [] 
+matTok = Token "mat" $ Range []
+mTok = Token "m" $ Range []
+nTok = Token "n" $ Range []
+aTok = Token "A" $ Range []
+bTok = Token "B" $ Range []
+plusTok = Token "+" $ Range []
+
+nat, mat, m, n, a, b, plus :: TERM
+
+nat = Identifier natTok 
+mat = Identifier matTok
+m = Identifier mTok
+n = Identifier nTok
+a = Identifier aTok
+b = Identifier bTok
+plus = Identifier plusTok
+
+decl :: [([NAME], TYPE)]
+decl = [([mTok, nTok], Univ nat), 
+        ([aTok, bTok], Univ $ Appl mat [m, n])]	
+
+commut :: FORMULA
+commut = Forall decl $ Equality (Appl plus [a, b]) (Appl plus [b, a])
+
+spec :: BASIC_SPEC
+spec = Basic_spec [Annoted (Decl [natTok] Sort) (Range []) [] [],
+                   Annoted (Decl [matTok] $ Pi (take 2 decl) Sort) (Range []) [] [],
+                   Annoted (Decl [plusTok] $ Pi decl $ Univ $ Appl mat [m, n]) (Range []) [] [],
+                   Annoted (Axiom commut) (Range []) [] []]
+
+     
