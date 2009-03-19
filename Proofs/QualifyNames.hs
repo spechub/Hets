dc0ae81895771bff125b735927e1fd7f5c0b594b
Christian Maeder <Christian.Maeder@dfki.de>
2009-03-19 19:49:11 +0000

    fixed #677 git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@11551 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Proofs/QualifyNames.hs
+++ Proofs/QualifyNames.hs
@@ -13,7 +13,7 @@ however that signature will be always be complete, i.e. imported entities
 will be repeated.
 -}
 
-module Proofs.QualifyNames where
+module Proofs.QualifyNames (qualifyLibEnv) where
 
 import Proofs.EdgeUtils
 
@@ -40,27 +40,33 @@ import qualified Data.Map as Map
 import qualified Data.Set as Set
 import Control.Monad
 
-mapWithKeyM :: (Ord k, Monad m) => (k -> a -> m b) -> Map.Map k a
-            -> m (Map.Map k b)
-mapWithKeyM f = foldM (\ m (k, a) -> do
-   b <- f k a
-   return $ Map.insert k b m) Map.empty . Map.toList
-
 qualifyLibEnv :: LibEnv -> Result LibEnv
-qualifyLibEnv = mapWithKeyM qualifyDGraph
+qualifyLibEnv libEnv = fmap fst
+  $ foldM (\ (le, m) ln -> do
+    dg0 <- updateRefNodes (le, m) $ lookupDGraph ln le
+    (dg, trm) <- qualifyDGraph ln dg0
+    return (Map.insert ln dg le, Map.insert ln trm m)) (libEnv, Map.empty)
+    $ getTopsortedLibs libEnv
 
-qualifyDGraph :: LIB_NAME -> DGraph -> Result DGraph
+type RenameMap = Map.Map Int (GMorphism, GMorphism)
+
+qualifyDGraph :: LIB_NAME -> DGraph -> Result (DGraph, RenameMap)
 qualifyDGraph ln dg =
   addErrorDiag "qualification failed for" (getLIB_ID ln)
   $ do
   let es = map (\ (_, _, lb) -> dgl_id lb) $ labEdgesDG dg
   unless (Set.size (Set.fromList es) == length es) $
     fail $ "inkonsistent graph for library " ++ showDoc ln ""
-  dg1 <- foldM (qualifyLabNode ln) dg $ topsortedNodes dg
-  return $ groupHistory dg (DGRule "Qualified-Names") dg1
+  (dg1, trm) <- foldM (qualifyLabNode ln) (dg, Map.empty) $ topsortedNodes dg
+  return (groupHistory dg (DGRule "Qualified-Names") dg1, trm)
 
--- consider that loops are part of innDG and outDG that should not be handled
--- twice
+{- consider that loops are part of innDG and outDG that should not be handled
+   twice -}
+properEdge :: LEdge a -> Bool
+properEdge (x, y, _) = x /= y
+
+properInEdges :: DGraph -> Node -> [LEdge DGLinkLab]
+properInEdges dg = filter properEdge . innDG dg
 
 constructUnion :: Logic lid sublogics
          basic_spec sentence symb_items symb_map_items
@@ -72,14 +78,37 @@ constructUnion lid hd l = case l of
     Just m -> constructUnion lid m tl
     Nothing -> constructUnion lid sd tl
 
-qualifyLabNode :: LIB_NAME -> DGraph -> LNode DGNodeLab
+updateRefNodes :: (LibEnv, Map.Map LIB_NAME RenameMap) -> DGraph
                -> Result DGraph
-qualifyLabNode ln dg (n, lb) = let
-  noLoop (x, y, _) = x /= y
-  inss = filter noLoop $ innDG dg n
-  allOuts = outDG dg n in
-  case dgn_theory lb of
+updateRefNodes (le, trm) dgraph =
+  foldM (\ dg (n, lb) ->
+     if isDGRef lb then do
+     let refLn = dgn_libname lb
+         refNode = dgn_node lb
+         gp = Map.findWithDefault (error "updateRefNodes2") refNode
+           $ Map.findWithDefault (error "updateRefNodes1") refLn trm
+         refGr = lookupDGraph refLn le
+         gth = dgn_theory $ labDG refGr refNode
+         newlb = lb { dgn_theory = createGThWith gth startSigId startThId }
+     (ds, is) <- createChanges dg n (properInEdges dg n) gp
+     return $ changesDGH dg $ ds ++ SetNodeLab lb (n, newlb) : is
+     else return dg) dgraph $ labNodesDG dgraph
+
+createChanges :: DGraph -> Node -> [LEdge DGLinkLab] -> (GMorphism, GMorphism)
+              -> Result ([DGChange], [DGChange])
+createChanges dg n inss (gm1, grm) = do
+  let allOuts = outDG dg n
+  nAllouts <- mapM (composeWithMorphism False gm1 grm) allOuts
+  let (nouts, nloops) = partition properEdge nAllouts
+  nAllinss <- mapM (composeWithMorphism True gm1 grm) $ nloops ++ inss
+  return (map DeleteEdge $ allOuts ++ inss, map InsertEdge $ nAllinss ++ nouts)
+
+qualifyLabNode :: LIB_NAME -> (DGraph, RenameMap) -> LNode DGNodeLab
+               -> Result (DGraph, RenameMap)
+qualifyLabNode ln (dg, mormap) (n, lb) =
+   if isDGRef lb then return (dg, mormap) else case dgn_theory lb of
     G_theory lid (ExtSign sig _) _ sens _ -> do
+        let inss = properInEdges dg n
         hins <- foldM (\ l (GMorphism cid _ _ mor _) ->
             if isIdComorphism (Comorphism cid) && language_name lid ==
                language_name (targetLogic cid) then do
@@ -94,24 +123,25 @@ qualifyLabNode ln dg (n, lb) = let
         (m1, osens) <- qualify lid (mkSimpleId $ getDGNodeName lb)
                        (getLIB_ID ln) m sig
         rm <- inverse m1
-        nThSens <- mapThSensValueM (map_sen lid m1) sens
+        nThSens <- mapThSensValueM (map_sen lid m1) $ joinSens sens
+          $ toThSens osens
         let nlb = lb { dgn_theory = G_theory lid
                        (makeExtSign lid (cod m1)) startSigId
-                       (joinSens nThSens $ toThSens osens) startThId }
+                       nThSens startThId }
         gm1 <- return $ gEmbed $ G_morphism lid m1 startMorId
         grm <- return $ gEmbed $ G_morphism lid rm startMorId
-        nAllouts <- mapM (composeWithMorphism False gm1 grm) allOuts
-        let (nouts, nloops) = partition noLoop nAllouts
-        nAllinss <- mapM (composeWithMorphism True gm1 grm) $ nloops ++ inss
-        return $ changesDGH dg $ map DeleteEdge (allOuts ++ inss)
-              ++ SetNodeLab lb (n, nlb) : map InsertEdge (nAllinss ++ nouts)
+        let gp = (gm1, grm)
+        (ds, is) <- createChanges dg n inss gp
+        return ( changesDGH dg $ ds ++ SetNodeLab lb (n, nlb) : is
+               , Map.insert n gp mormap)
 
 -- consider that hiding edges have a reverse morphism
-
+-- special treatment for HidingThm needed, too
 composeWithMorphism :: Bool -> GMorphism -> GMorphism -> LEdge DGLinkLab
                     -> Result (LEdge DGLinkLab)
 composeWithMorphism dir mor rmor (s, t, lb) = do
     let lmor = dgl_morphism lb
-    nmor <- if dir /= isHidingEdge (dgl_type lb)
+    nmor <- addErrorDiag ("edge " ++ show (s, t, dgl_id lb)) ()
+            $ if dir /= isHidingEdge (dgl_type lb)
             then comp lmor mor else comp rmor lmor
     return (s, t, lb { dgl_morphism = nmor})
