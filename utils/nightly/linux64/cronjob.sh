a12ade166e2d6c067177f9cffb67c93439b8e402
Christian Maeder <Christian.Maeder@dfki.de>
2009-03-26 17:19:14 +0000

    pollux job git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@11605 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ utils/nightly/linux64/cronjob.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -x
+
+PATH=/bin:/usr/bin:/usr/X11R6/bin:/home/linux-bkb/Isabelle/Isabelle2008/bin:/home/linux-bkb/bin
+UDG_HOME=/home/linux-bkb/uDrawGraph-3.1
+HETS_LIB=/local/home/maeder/haskell/Hets-lib
+
+export PATH
+export HETS_LIB
+
+cd /local/home/maeder/haskell
+. ../cronjob.sh
+
+export GHCRTS='-H300m -M6g'
+
+makeHets
+makeLibCheck
+installHetsBinary linux64
+chgrp linuxbkb hets
+\cp -fp hets /home/linux64-bkb/hets/hets-latest/bin/
+
+latexBasicLibraries
+createLogFiles
+
+runIsaBasic
+runSPASSBasic
+checkIsaOf Calculi/Time/AllenHayesLadkin_TACAS.het HC
+checkIsaOf Calculi/Space/RCCVerification.het HC2
+
+checkBins
+
+cd ../Hets
+checkMoreBins
+runIsaHS
+
+moreChecks
+checkEnvs
+checkPrfs
+date
+updateOMDoc
+date
