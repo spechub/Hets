c7f1a9b3811f1ed427c57559d9af26a75a309ff1
Ewaryst Schulz <Ewaryst.Schulz@dfki.de>
2009-07-03 14:59:22 +0000

    refinement of xml export git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@11874 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Syntax/ToXml.hs
+++ Syntax/ToXml.hs
@@ -12,20 +12,27 @@ Xml printing of Hets specification libaries
 
 module Syntax.ToXml
     (
-      XmlPrintable
-    , printLibDefnXml
+      printLibDefnXml
     ) where
 
 import Syntax.AS_Structured
 import Syntax.AS_Library
 import Syntax.Print_AS_Structured()
 
-import Common.Id (Range)
+import Common.Id
+import Common.LibName
 import Common.Result ()
 import Common.AS_Annotation
 import Common.DocUtils
 
+import Logic.Logic
+import Logic.Grothendieck
+
 import Text.XML.Light
+
+import Data.Maybe
+
+import Monad
 
 -- one can add global annos if necessary
 -- | Prints the Library to an xml string
@@ -37,38 +44,70 @@ printLibDefnXml ld = case toXml ld of (Elem e) -> ppTopElement e
 -- if necessary replace Content by a custom data type
 
 {- |
-  this class defines the interface for writing XML
+  This class defines the interface for writing XML
 -}
 class XmlPrintable a where
   -- | render instance to an XML Element
   toXml :: a -> Content
 
+{- |
+  Use this class if you can render something as an xml element list
+-}
+class XmlListPrintable a where
+  -- | render instance to an xml element list
+  toLst :: a -> [Content]
+
+{- |
+  Use this class if you can render something as an xml attribute value
+-}
+class PrintableAsXmlAttr a where
+  -- | render instance to an xml attribute value
+  toStr :: a -> String
+
+class XmlAttrList a where
+  -- | Everything what can be output to an attribute list
+  mkAtts :: a -> [Attr]
+
+-- for empty lists use this datatype
+data Empty = Empty
+instance XmlAttrList Empty where mkAtts _ = []
+instance XmlListPrintable Empty where toLst _ = []
+
+
 instance XmlPrintable LIB_DEFN where
     toXml (Lib_defn n il rg an) =
-        mkEl "LIB_DEFN" ["name", toString n]
+        mkEl "Lib" ["name", toStr n]
                  $ printAnnotations rg an [] : map (toXml . item) il
 
 instance XmlPrintable LIB_ITEM where
---    toXml (Spec_defn si (Genericity aa@(Params pl) ab@(Imported il) _) ac' _) =
-    toXml (Spec_defn n _ as rg) =
-        withRg rg $ mkEl "SPEC" ["name", toString n] [toXml as]
-    toXml (View_defn n _ vt mapping rg) = withRg rg $ mkFEl "VIEW" []
-    toXml (Download_items n mapping rg) = withRg rg $ mkFEl "FROM" []
-    toXml (Logic_decl n rg) = withRg rg $ mkFEl "LOGIC" []
+    toXml (Spec_defn n g as rg)
+        = withRg rg $ mkEl "Spec" ["name", toString n]
+          $ toLst g ++ [toXml as]
+    toXml (View_defn n g (View_type from to _) mapping rg)
+        = withRg rg $ mkEl "View" ["name", toString n]
+          $ toLst g ++ [mkPEl "From" [toXml from], mkPEl "To" [toXml to]] ++
+            toLst mapping
+    toXml (Download_items n mapping rg)
+        = withRg rg $ mkEl "Import" ["name", toString n] $ toLst mapping
+    toXml (Logic_decl n rg)
+        = withRg rg $ mkFEl "Logic" ["name", toStr n]
     toXml x = mkComment $ take 15 (show x)  ++ "- not Supported"
+
 
 instance XmlPrintable (Annoted SPEC) where
     toXml a@(Annoted s toprg la ra) =
         let mkAEl x y z = withAnno a $ mkEl x y z
             mkAPEl x z = withAnno a $ mkPEl x z
         in case s of
-             Basic_spec bs rg -> withRg rg $ mkAPEl "Basic" [toText bs]
-             EmptySpec _ -> mkAPEl "EmptySpec" []
-             Translation as m -> mkAPEl "Translation" [toXml as, toText m]
-             Reduction as m -> mkAPEl "Reduction" [toXml as, toText m]
-             Union asl rg -> withRg rg $ mkAPEl "Union" $ map toXml asl
+             Basic_spec bs rg -> withRg rg $ mkAPEl "Basic" $ toLst bs
+             EmptySpec _ -> mkAPEl "Emptyspec" []
+             Translation as (Renaming m _) -> mkAPEl "Renaming"
+                                              $ toXml as : toLst m
+             Reduction as m -> mkAPEl "Restriction"
+                               $ toXml as : toLst m
+             Union asl rg -> withRg rg $ mkAPEl "Union" $ toLst asl
              Extension asl rg -> withRg rg $ mkAPEl "Extension"
-                                 $ map toXml asl
+                                 $ toLst asl
              Free_spec as rg -> withRg rg $ mkAPEl "Free" [toXml as]
              Cofree_spec as rg -> withRg rg $ mkAPEl "Cofree" [toXml as]
              Local_spec as ins rg -> withRg rg $ mkAPEl "Local"
@@ -76,9 +115,9 @@ instance XmlPrintable (Annoted SPEC) where
              Closed_spec as rg -> withRg rg $ mkAPEl "Closed" [toXml as]
              Group as rg -> withRg rg $ mkAPEl "Group" [toXml as]
              Spec_inst n fa rg ->
-                 withRg rg $ mkAEl "Inst" ["name", toString n] $ map toXml fa
-             Qualified_spec ln as rg ->
-                 withRg rg $ mkAEl "Qualified" ["logic", toString ln] [toXml as]
+                 withRg rg $ mkAEl "Inst" ["name", toString n] $ toLst fa
+             Qualified_spec ln as rg -> withRg rg $ mkAEl "Qualified"
+                                        ["logic", toString ln] [toXml as]
              Data _ _ s1 s2 _ -> mkComment "DATA not supported"
 
 instance XmlPrintable (Annoted FIT_ARG) where
@@ -89,18 +128,79 @@ instance XmlPrintable (Annoted FIT_ARG) where
              Fit_spec as _ rg ->  withRg rg $ mkAPEl "Fitspec" [toXml as]
              Fit_view n fargs rg ->
                  withRg rg $ mkAEl "Fitview" ["name", toString n]
-                            $ map toXml fargs
+                            $ toLst fargs
 
 instance XmlPrintable Annotation where
     toXml x = mkPEl "Annotation" [toText x]
 
-withAnno :: Annoted a -> Content -> Content
-withAnno a (Elem e) = Elem $ e { elContent = printAnnotated a : elContent e }
-withAnno a _ = error "withAnno only applies to elements"
+instance XmlPrintable ITEM_NAME_OR_MAP where
+    toXml (Item_name name) = mkFEl "Item" ["name", toString name]
+    toXml (Item_name_map name as _)
+        = mkFEl "Item" ["name", toString name, "as", toString as]
 
-withRg :: Range -> Content -> Content
-withRg rg (Elem e) = Elem $ add_attr (Attr (unqual "range") $ toString rg) e
-withRg rg _ = error "withRg only applies to elements"
+instance XmlPrintable G_mapping where
+    toXml (G_symb_map l) = mkPEl "Mapping" $ toLst l
+    toXml (G_logic_translation lc) = mkFEl "Logictranslation" lc
+
+instance XmlPrintable G_hiding where
+    toXml (G_symb_list l) = mkPEl "Hiding" $ toLst l
+    toXml (G_logic_projection lc) = mkFEl "Logicprojection" lc
+
+
+
+instance PrintableAsXmlAttr LIB_NAME where
+    toStr l = case l of
+                Lib_version i _ -> toString i
+                Lib_id i -> toString i
+
+instance PrintableAsXmlAttr Token where toStr = tokStr
+
+instance PrintableAsXmlAttr Logic_name where toStr (Logic_name t _) = toStr t
+
+--instance PrintableAsXmlAttr SIMPLE_ID where toStr = toString
+
+
+instance XmlPrintable a => XmlListPrintable [a] where
+    toLst = map toXml
+
+instance XmlListPrintable G_basic_spec where
+    toLst (G_basic_spec lid bs) = map printBasicSpecItem
+                                  $ toAnnotedStrings lid bs
+
+instance XmlListPrintable GENERICITY where
+    toLst (Genericity (Params pl) (Imported il) _)
+        = map (mkPEl "Param" . (: []) . toXml) pl ++
+          map (mkPEl "Given" . (: []) . toXml) il
+
+instance XmlListPrintable RESTRICTION where
+    toLst (Hidden m _) = toLst m
+    toLst (Revealed m _) = toLst m
+
+instance XmlListPrintable G_symb_items_list where
+    toLst _ = []
+
+instance XmlListPrintable G_symb_map_items_list where
+    toLst _ = []
+
+instance XmlAttrList [String] where
+    mkAtts (x:y:l) = (Attr (unqual x) y) : mkAtts l
+    mkAtts _ = []
+
+instance XmlAttrList [(String, String)] where
+    mkAtts ((x,y):l) = (Attr (unqual x) y) : mkAtts l
+    mkAtts _ = []
+
+instance XmlAttrList Logic_code where
+    mkAtts (Logic_code enc src trg _)
+        = mkAtts $ catMaybes [liftM ((,) "encoding" . toStr) enc,
+                              liftM ((,) "source" . toStr) src,
+                              liftM ((,) "target" . toStr) trg]
+
+printBasicSpecItem :: Annoted String -> Content
+printBasicSpecItem as@(Annoted s rg la ra)
+    = withAnno as
+      $ mkPEl (if notImplied as then "Theoryitem" else "Assertion") [toText s]
+
 
 printAnnotated :: Annoted a -> Content
 printAnnotated (Annoted _ rg la ra) = printAnnotations rg la ra
@@ -110,26 +210,22 @@ printAnnotations rg lan ran =
     withRg rg $ mkPEl "Annotations" 
                [printPXmlList "Left" lan, printPXmlList "Right" ran]
 
-{-
-withNewLine :: String -> String
-withNewLine s = "\n" ++ s ++ "\n"
-
-toComment :: Pretty a => a -> Content
-toComment = mkComment . toString
-
-toVerb :: Pretty a => a -> Content
-toVerb = mkVerb . toString
-
-mkVerb :: String -> Content
-mkVerb s = Text $ CData CDataVerbatim s $ Just 50
-
--}
 
 printXmlList :: XmlPrintable a => String -> [String] -> [a] -> Content
-printXmlList n attrs l = mkEl n attrs $ map toXml l
+printXmlList n attrs l = mkEl n attrs $ toLst l
 
 printPXmlList :: XmlPrintable a => String -> [a] -> Content
 printPXmlList n l = printXmlList n [] l
+
+
+withAnno :: Annoted a -> Content -> Content
+withAnno a (Elem e) = Elem $ e { elContent = printAnnotated a : elContent e }
+withAnno a _ = error "withAnno only applies to elements"
+
+withRg :: Range -> Content -> Content
+withRg rg (Elem e) = Elem $ add_attr (Attr (unqual "range") $ toString rg) e
+withRg rg _ = error "withRg only applies to elements"
+
 
 toString :: Pretty a => a -> String
 toString = show . pretty
@@ -144,18 +240,14 @@ mkText :: String -> Content
 mkText s = Text $ CData CDataText s Nothing
 
 -- make element
-mkEl :: String -> [String] -> [Content] -> Content
+mkEl :: XmlAttrList a => String -> a -> [Content] -> Content
 mkEl n a c = Elem $ node (unqual n) (mkAtts a, c)
 
 -- make final element (no children)
-mkFEl :: String -> [String] -> Content
+mkFEl :: XmlAttrList a => String -> a -> Content
 mkFEl n a = mkEl n a []
 
 -- make pure element (no attributes)
 mkPEl :: String -> [Content] -> Content
-mkPEl n c = mkEl n [] c
-
-mkAtts :: [String] -> [Attr]
-mkAtts (x:y:l) = (Attr (unqual x) y) : mkAtts l
-mkAtts _ = []
+mkPEl n c = mkEl n Empty c
 
