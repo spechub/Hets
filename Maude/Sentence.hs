e984708050804320f6e0852fc6fa4d2a09c87ad1
Martin Kühl <martin.kuehl@gmail.com>
2009-07-09 12:00:46 +0000

    Switch sentences to AS_Maude. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@11887 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Maude/Sentence.hs
+++ Maude/Sentence.hs
@@ -20,52 +20,10 @@ module Maude.Sentence (
     Sentence(..),
 ) where
 
-import Maude.Meta
-
-import Data.Typeable (Typeable)
-
--- for ShATermConvertible
-import Common.ATerm.Conversion (ShATermConvertible(..))
--- for Pretty
-import Common.DocUtils (Pretty(..))
-import qualified Common.Doc as Doc
+import Maude.AS_Maude
 
 
-data Sentence = MembAx MembAx
+data Sentence = Membership Membership
               | Equation Equation
               -- We are excluding Rules for now.
-    deriving (Show, Eq, Ord, Typeable)
-
--- TODO: Add real pretty-printing for Maude Sentences.
-instance Pretty Sentence where
-    pretty _ = Doc.empty
-
--- TODO: Replace dummy implementation for ShATermConvertible Sentence.
-instance ShATermConvertible Sentence where
-    toShATermAux table _ = return (table, 0)
-    fromShATermAux _ table = (table, error "Nope...")
-
-
-instance HasSorts Sentence where
-    getSorts sen = case sen of
-        MembAx mb   -> getSorts mb
-        Equation eq -> getSorts eq
-    mapSorts mp sen = case sen of
-        MembAx mb   -> MembAx (mapSorts mp mb)
-        Equation eq -> Equation (mapSorts mp eq)
-
-instance HasOps Sentence where
-    getOps sen = case sen of
-        MembAx mb   -> getOps mb
-        Equation eq -> getOps eq
-    mapOps mp sen = case sen of
-        MembAx mb   -> MembAx (mapOps mp mb)
-        Equation eq -> Equation (mapOps mp eq)
-
-instance HasLabels Sentence where
-    getLabels sen = case sen of
-        MembAx mb   -> getLabels mb
-        Equation eq -> getLabels eq
-    mapLabels mp sen = case sen of
-        MembAx mb   -> MembAx (mapLabels mp mb)
-        Equation eq -> Equation (mapLabels mp eq)
+    deriving (Show, Read)
