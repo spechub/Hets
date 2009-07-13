d48b3f098b0961e0be881ef66dc2dc3255a22724
Christian Maeder <Christian.Maeder@dfki.de>
2009-07-13 16:02:11 +0000

    Isabelle 2009 change git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@11902 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/nightly/runHsIsabelle.sh
+++ utils/nightly/runHsIsabelle.sh
@@ -3,5 +3,5 @@
 for i in $*
 do
    j=`basename $i .thy`
-   (cd `dirname $i`; isabelle -q -e "use_thy \"$j\";print_depth 300;theory \"$j\";axioms_of it;quit();" HOLCF)
+   (cd `dirname $i`; isabelle-process -q -e "use_thy \"$j\";print_depth 300;theory \"$j\";axioms_of it;quit();" HOLCF)
 done
