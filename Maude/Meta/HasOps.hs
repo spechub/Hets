bfc860b985ca46852edef56e0de30bd3e41dbfcf
Martin Kühl <martin.kuehl@gmail.com>
2009-07-14 11:15:23 +0000

    Update HasOps to use AS_Maude git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@11908 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Maude/Meta/HasOps.hs
+++ Maude/Meta/HasOps.hs
@@ -2,26 +2,18 @@ module Maude.Meta.HasOps (
     HasOps(..)
 ) where
 
-import Maude.Meta.Qid
-import Maude.Meta.Term
-import Maude.Meta.Module
+import Maude.AS_Maude
+import Maude.Symbol
+
+import Maude.Meta.HasName
 
 import Data.Set (Set)
 import qualified Data.Set as Set
 
 
 class HasOps a where
-    getOps :: a -> QidSet
-    mapOps :: QidMap -> a -> a
-
-
-instance HasOps Term where
-    getOps term = case term of
-        Term op ts -> Set.insert op (getOps ts)
-        _          -> Set.empty
-    mapOps mp term = case term of
-        Term op ts -> Term (mapAsFunction mp op) (mapOps mp ts)
-        _          -> term
+    getOps :: a -> SymbolSet
+    mapOps :: SymbolMap -> a -> a
 
 
 instance (HasOps a) => HasOps [a] where
@@ -41,52 +33,43 @@ instance (Ord a, HasOps a) => HasOps (Set a) where
     mapOps = Set.map . mapOps
 
 
-instance HasOps OpDecl where
-    getOps = Set.singleton . op'name
-    mapOps mp op = op {
-        op'name = mapAsFunction mp (op'name op)
-    }
+instance HasOps Operator where
+    getOps = Set.singleton . getName
+    mapOps = mapName
+
+
+instance HasOps Term where
+    getOps term = case term of
+        Apply op ts -> Set.insert (getName op) (getOps ts)
+        _           -> Set.empty
+    mapOps mp term = case term of
+        Apply op ts -> Apply (mapName mp op) (mapOps mp ts)
+        _           -> term
 
 
 instance HasOps Condition where
     getOps cond = case cond of
-        Nil               -> Set.empty
-        Equal t1 t2       -> getOps (t1, t2)
-        Member t _        -> getOps t
-        Match t1 t2       -> getOps (t1, t2)
-        Implies t1 t2     -> getOps (t1, t2)
-        Conjunction c1 c2 -> getOps (c1, c2)
+        EqCond t1 t2    -> getOps (t1, t2)
+        MbCond t s      -> getOps t
+        MatchCond t1 t2 -> getOps (t1, t2)
+        RwCond t1 t2    -> getOps (t1, t2)
     mapOps mp cond = case cond of
-        Nil               -> Nil
-        Equal t1 t2       -> Equal (mapOps mp t1) (mapOps mp t2)
-        Member t s        -> Member (mapOps mp t) s
-        Match t1 t2       -> Match (mapOps mp t1) (mapOps mp t2)
-        Implies t1 t2     -> Implies (mapOps mp t1) (mapOps mp t2)
-        Conjunction c1 c2 -> Conjunction (mapOps mp c1) (mapOps mp c2)
-
-
-instance HasOps MembAx where
-    getOps mb = case mb of
-        Mb t _ _    -> getOps t
-        Cmb t _ c _ -> getOps (t, c)
-    mapOps mp mb = case mb of
-        Mb t s as    -> Mb (mapOps mp t) s as
-        Cmb t s c as -> Cmb (mapOps mp t) s (mapOps mp c) as
+        EqCond t1 t2    -> EqCond (mapOps mp t1) (mapOps mp t2)
+        MbCond t s      -> MbCond (mapOps mp t) s
+        MatchCond t1 t2 -> MatchCond (mapOps mp t1) (mapOps mp t2)
+        RwCond t1 t2    -> RwCond (mapOps mp t1) (mapOps mp t2)
 
 
 instance HasOps Equation where
-    getOps eq = case eq of
-        Eq t1 t2 _    -> getOps (t1, t2)
-        Ceq t1 t2 c _ -> getOps (t1, t2, c)
-    mapOps mp eq = case eq of
-        Eq t1 t2 as    -> Eq (mapOps mp t1) (mapOps mp t2) as
-        Ceq t1 t2 c as -> Ceq (mapOps mp t1) (mapOps mp t2) (mapOps mp c) as
+    getOps (Eq t1 t2 cs _) = getOps (t1, t2, cs)
+    mapOps mp (Eq t1 t2 cs as) = Eq (mapOps mp t1) (mapOps mp t2) (mapOps mp cs) as
+
+
+instance HasOps Membership where
+    getOps (Mb ts _ cs _) = getOps (ts, cs)
+    mapOps mp (Mb ts ss cs as) = Mb (mapOps mp ts) ss (mapOps mp cs) as
 
 
 instance HasOps Rule where
-    getOps rl = case rl of
-        Rl t1 t2 _    -> getOps (t1, t2)
-        Crl t1 t2 c _ -> getOps (t1, t2, c)
-    mapOps mp rl = case rl of
-        Rl t1 t2 as    -> Rl (mapOps mp t1) (mapOps mp t2) as
-        Crl t1 t2 c as -> Crl (mapOps mp t1) (mapOps mp t2) (mapOps mp c) as
+    getOps (Rl t1 t2 cs _) = getOps (t1, t2, cs)
+    mapOps mp (Rl t1 t2 cs as) = Rl (mapOps mp t1) (mapOps mp t2) (mapOps mp cs) as
