f8eff59ab34565ab064b86ac7edc2d8bab498e1a
Markus Gross <Markus.Gross@dfki.de>
2009-07-14 11:44:58 +0000

    Conservativity shift rule now working. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@11912 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Proofs/Conservativity.hs
+++ Proofs/Conservativity.hs
@@ -16,26 +16,18 @@ module Proofs.Conservativity
     ( conservativity
     ) where
 
-{-import Logic.Logic
-import Logic.Prover
-import Logic.ExtSign
-import Logic.Grothendieck
 import Static.GTheory
-import Static.ArchDiagram-}
 import Static.DevGraph
 
 import Proofs.EdgeUtils
-
-{-import Common.Amalgamate
-import Common.Lib.Graph as Tree
-import Common.Result-}
+import Proofs.ComputeColimit (makeDiagram)
+import Common.Amalgamate
+import Common.Result
 import Common.LibName
 
 import Data.Graph.Inductive.Graph as Graph
 import qualified Data.Map as Map
 import Data.List (nub, nubBy)
-import Data.Maybe (fromJust)
---import Control.Monad
 
 ------------------------------------------------
 -- Conservativity rules
@@ -88,9 +80,7 @@ conservativity = Map.adjust (shift . freeIsMono)
 -- amalgamable.
 -- Afterwards the quad is positioned correctly and the edges are updated.
 shift :: DGraph -> DGraph
-shift dg = changesDGH dg changes
-  --error $ unlines $ intersperse "\n##\n" $
-  --        map (\ (x,y) -> showPair x ++ "\n" ++ showPair y) quads
+shift dg = groupHistory dg (DGRule "conservativity") $ changesDGH dg changes
   where
     edgs = filter (liftE isGlobalEdge) $ labEdgesDG dg
     globThmEdges = filter (liftE isGlobalThm) edgs
@@ -104,49 +94,27 @@ shift dg = changesDGH dg changes
             filter (isolated edgs . snd) $ map posQuad
             [ ((e1, e2), (e3, e4)) | (e1@(_,t1,_), e2@(_,t2,_))<-pairs1,
                                      (e3@(s3,_,_), e4@(s4,_,_))<-pairs2,
-                                     ((t1 == s3 && t2 == s4) ||
-                                      (t1 == s4 && t2 == s3)) ]
+                                     (t1 == s3 && t2 == s4) ||
+                                     (t1 == s4 && t2 == s3) ]
     changes = concatMap process quads
     -- Updates the e4 edge with the cons value from the e1 edge.
     -- The quads have to be positioned correctly before using this function.
     process :: Quad -> [DGChange]
-    process ((e1, _), (_, e4@(s4, t4, l4))) =
+    process ((e1, _), (_, e4)) =
       let cons = getConservativity e1
-       in [ DeleteEdge e4, InsertEdge (s4, t4, l4 {
-              dgl_type = case dgl_type l4 of
-                           ScopedLink sc dl cs -> ScopedLink sc dl (case cs of
-                              ConsStatus co pc tl -> ConsStatus (max cons co)
-                                                                pc tl)
-                           t -> t
-            , dgl_origin = DGLinkProof
-            , dgl_id = defaultEdgeId
-          })]
+       in [ DeleteEdge e4, InsertEdge $ modifyEdgeCons e4
+              (\ (ConsStatus co pc tl) -> ConsStatus (max cons co) pc tl)]
 
 -- Checks if a quad is amalgamable.
 isAmalgamable :: DGraph -> Quad -> Bool
-isAmalgamable _ _ = True
-{-isAmalgamable dg ((e1@(s1,_,_), e2), (e3@(s3,t3,l3), e4@(s4,t4,l4))) =
-  case resultToMaybe $
-       ensures_amalgamability lid ([Sharing], diag, sink, Graph.empty) of
-    Just Amalgamates -> True
-    _                -> False
+isAmalgamable dg ((e1@(s1,_,_), e2), (e3@(s3,_,l3), e4@(s4,t4,l4))) =
+  case resultToMaybe amal of
+    Just Amalgamates  -> True
+    _                 -> False
   where
-    sink = case resultToMaybe $ homogeniseSink lid
-                [(s3, dgl_morphism l3), (s4, dgl_morphism l4)] of
-             Just s -> s
-             _      -> []
-    sig = cod $ snd $ head sink
-    --lid :: StaticAnalysis lid
-    --  basic_spec sentence symb_items symb_map_items
-    --  sign morphism symbol raw_symbol => lid
-    lid :: Logic lid sublogics
-           basic_spec sentence symb_items symb_map_items
-           sign morphism symbol raw_symbol proof_tree => lid
-    lid = case sig of
-            G_sign llid _ _ -> llid
-    diag = Graph.empty
-    --diag = resultToMaybe $
-    --       homogeniseDiagram lid d-}
+    sink = [(s3, dgl_morphism l3), (s4, dgl_morphism l4)]
+    diag = makeDiagram dg [s1, s3, s4, t4] [e1, e2, e3, e4]
+    amal = gEnsuresAmalgamability [ColimitThinness,Cell] diag sink
 
 -- Checks wether a pair is duplicate.
 nubPair :: Pair -> Pair -> Bool
@@ -163,13 +131,9 @@ posQuad q@((e1@(_,t1,_), e2@(_,t2,_)), (e3@(s3,_,_), e4))
 -- Checks wether the node of the pair is isolated.
 -- Both edges of the pair have the same target node.
 isolated :: [LEdge DGLinkLab] -> Pair -> Bool
-isolated edgs (e1@(_,t1,_), e2) = not $ any
-  (\ x@(_,t,_) -> x /= e1 && x /= e2 && t == t1 ) edgs
-
--- Displays the node ids of an edge pair.
-{-showPair :: Pair -> String
-showPair ((s1,t1,_), (s2,t2,_)) = show s1 ++ " -> " ++ show t1 ++ " && " ++
-                                  show s2 ++ " -> " ++ show t2-}
+isolated edgs (e1@(_,t1,_), e2) =
+  not $ any (\ x@(_,t,_) -> x /= e1 && x /= e2 && t == t1 ) $
+  filter (liftE isGlobalDefEdge) edgs
 
 -- First get all free links in the graph. Then all cons links.
 -- When the free and cons link point to the same node, the cons link is upgraded
@@ -183,13 +147,25 @@ freeIsMono dg = changesDGH dg changes
     mono = nub [ c | c@(_, ct, _)<-cons, (_, ft, _)<-free, ct == ft ]
     changes = concatMap process mono
     process :: LEdge DGLinkLab -> [DGChange]
-    process m@(s, t, l) = [ DeleteEdge m, InsertEdge (s, t, l {
-         dgl_type = case dgl_type l of
-                      ScopedLink sc dl cs -> ScopedLink sc dl (case cs of
-                              ConsStatus _ pc tl -> ConsStatus Mono pc tl)
-                      tp -> tp
-       , dgl_origin = DGLinkProof
-       , dgl_id = defaultEdgeId
-      }) ]
+    process m = [ DeleteEdge m, InsertEdge $ modifyEdgeCons m
+                    (\ (ConsStatus _ pc tl) -> ConsStatus Mono pc tl) ]
+
+-- Modifies the ConsStatus of an edge.
+modifyEdgeCons :: LEdge DGLinkLab
+  -> (ConsStatus -> ConsStatus) -> LEdge DGLinkLab
+modifyEdgeCons (s,t,l) f =
+  (s,t, l {
+     dgl_type = case dgl_type l of
+                ScopedLink sc dl cs -> ScopedLink sc dl $ f cs
+                tp -> tp
+   , dgl_origin = DGLinkProof
+   , dgl_id = defaultEdgeId
+  })
+
+-- Checks if an edge is global but no theorem link.
+isGlobalDefEdge :: DGLinkType -> Bool
+isGlobalDefEdge edge = case edge of
+    ScopedLink Global DefLink _  -> True
+    _ -> False
 
 --monoIsFree
