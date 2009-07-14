ffd78bee7d5e1ea2d822e9a1c0a0819789b7c38c
Martin Kühl <martin.kuehl@gmail.com>
2009-07-14 11:15:19 +0000

    Update HasSorts to use AS_Maude git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@11907 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Maude/Meta/HasSorts.hs
+++ Maude/Meta/HasSorts.hs
@@ -2,9 +2,10 @@ module Maude.Meta.HasSorts (
     HasSorts(..)
 ) where
 
-import Maude.Meta.Qid
-import Maude.Meta.Term
-import Maude.Meta.Module
+import Maude.AS_Maude
+import Maude.Symbol
+
+import Maude.Meta.HasName
 
 import Data.Set (Set)
 import qualified Data.Set as Set
@@ -13,14 +14,10 @@ import Common.Lib.Rel (Rel)
 import qualified Common.Lib.Rel as Rel
 
 
+-- TODO: Maybe this class should be named `HasTypes` instead?
 class HasSorts a where
-    getSorts :: a -> SortSet
-    mapSorts :: QidMap -> a -> a
-
-
-instance HasSorts Sort where
-    getSorts = Set.singleton
-    mapSorts = mapAsFunction
+    getSorts :: a -> SymbolSet
+    mapSorts :: SymbolMap -> a -> a
 
 
 instance (HasSorts a) => HasSorts [a] where
@@ -44,73 +41,69 @@ instance (Ord a, HasSorts a) => HasSorts (Rel a) where
     mapSorts = Rel.map . mapSorts
 
 
+instance HasSorts Symbol where
+    getSorts = Set.singleton . getName
+    mapSorts = mapName
+
+
+instance HasSorts Sort where
+    getSorts = Set.singleton . getName
+    mapSorts = mapName
+
+
+instance HasSorts Kind where
+    getSorts = Set.singleton . getName
+    mapSorts = mapName
+
+
 instance HasSorts Type where
     getSorts typ = case typ of
-        Sort s  -> Set.singleton s
-        Kind ss -> Set.fromList ss
+        TypeSort sort -> getSorts sort
+        TypeKind kind -> getSorts kind
     mapSorts mp typ = case typ of
-        Sort s  -> Sort (mapSorts mp s)
-        Kind ss -> Kind (mapSorts mp ss)
+        TypeSort sort -> TypeSort $ mapSorts mp sort
+        TypeKind kind -> TypeKind $ mapSorts mp kind
+
+
+instance HasSorts Operator where
+    getSorts (Op _ dom cod _) = getSorts (dom, cod)
+    mapSorts mp (Op op dom cod as) = Op op (mapSorts mp dom) (mapSorts mp cod) as
 
 
 instance HasSorts Term where
     getSorts term = case term of
-        Constant _ t -> getSorts t
-        Variable _ t -> getSorts t
-        Term _ ts    -> getSorts ts
+        Const _ t  -> getSorts t
+        Var _ t    -> getSorts t
+        Apply _ ts -> getSorts ts
     mapSorts mp term = case term of
-        Constant c t -> Constant c (mapSorts mp t)
-        Variable v t -> Variable v (mapSorts mp t)
-        Term t ts    -> Term t (mapSorts mp ts)
-
-
-instance HasSorts OpDecl where
-    getSorts op = getSorts (op'domain op, op'range op)
-    mapSorts mp op = op {
-        op'domain = mapSorts mp (op'domain op),
-        op'range = mapSorts mp (op'range op)
-    }
+        Const c t  -> Const c (mapSorts mp t)
+        Var v t    -> Var v (mapSorts mp t)
+        Apply t ts -> Apply t (mapSorts mp ts)
 
 
 instance HasSorts Condition where
     getSorts cond = case cond of
-        Nil               -> Set.empty
-        Equal t1 t2       -> getSorts (t1, t2)
-        Member t s        -> getSorts (s, t)
-        Match t1 t2       -> getSorts (t1, t2)
-        Implies t1 t2     -> getSorts (t1, t2)
-        Conjunction c1 c2 -> getSorts (c1, c2)
+        EqCond t1 t2    -> getSorts (t1, t2)
+        MbCond t s      -> getSorts (t, s)
+        MatchCond t1 t2 -> getSorts (t1, t2)
+        RwCond t1 t2    -> getSorts (t1, t2)
     mapSorts mp cond = case cond of
-        Nil               -> Nil
-        Equal t1 t2       -> Equal (mapSorts mp t1) (mapSorts mp t2)
-        Member t s        -> Member (mapSorts mp t) (mapSorts mp s)
-        Match t1 t2       -> Match (mapSorts mp t1) (mapSorts mp t2)
-        Implies t1 t2     -> Implies (mapSorts mp t1) (mapSorts mp t2)
-        Conjunction c1 c2 -> Conjunction (mapSorts mp c1) (mapSorts mp c2)
-
-
-instance HasSorts MembAx where
-    getSorts mb = case mb of
-        Mb t s _    -> getSorts (s, t)
-        Cmb t s c _ -> getSorts (s, t, c)
-    mapSorts mp mb = case mb of
-        Mb t s as    -> Mb (mapSorts mp t) (mapSorts mp s) as
-        Cmb t s c as -> Cmb (mapSorts mp t) (mapSorts mp s) (mapSorts mp c) as
+        EqCond t1 t2    -> EqCond (mapSorts mp t1) (mapSorts mp t2)
+        MbCond t s      -> MbCond (mapSorts mp t) (mapSorts mp s)
+        MatchCond t1 t2 -> MatchCond (mapSorts mp t1) (mapSorts mp t2)
+        RwCond t1 t2    -> RwCond (mapSorts mp t1) (mapSorts mp t2)
 
 
 instance HasSorts Equation where
-    getSorts eq = case eq of
-        Eq t1 t2 _    -> getSorts (t1, t2)
-        Ceq t1 t2 c _ -> getSorts (t1, t2, c)
-    mapSorts mp eq = case eq of
-        Eq t1 t2 as    -> Eq (mapSorts mp t1) (mapSorts mp t2) as
-        Ceq t1 t2 c as -> Ceq (mapSorts mp t1) (mapSorts mp t2) (mapSorts mp c) as
+    getSorts (Eq t1 t2 cs _) = getSorts (t1, t2, cs)
+    mapSorts mp (Eq t1 t2 cs as) = Eq (mapSorts mp t1) (mapSorts mp t2) (mapSorts mp cs) as
+
+
+instance HasSorts Membership where
+    getSorts (Mb ts ss cs _) = getSorts (ts, ss, cs)
+    mapSorts mp (Mb ts ss cs as) = Mb (mapSorts mp ts) (mapSorts mp ss) (mapSorts mp cs) as
 
 
 instance HasSorts Rule where
-    getSorts rl = case rl of
-        Rl t1 t2 _    -> getSorts (t1, t2)
-        Crl t1 t2 c _ -> getSorts (t1, t2, c)
-    mapSorts mp rl = case rl of
-        Rl t1 t2 as    -> Rl (mapSorts mp t1) (mapSorts mp t2) as
-        Crl t1 t2 c as -> Crl (mapSorts mp t1) (mapSorts mp t2) (mapSorts mp c) as
+    getSorts (Rl t1 t2 cs _) = getSorts (t1, t2, cs)
+    mapSorts mp (Rl t1 t2 cs as) = Rl (mapSorts mp t1) (mapSorts mp t2) (mapSorts mp cs) as
