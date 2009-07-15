3056105b50466d8dfb3bf09cd84d434fc5aecc78
Martin Kühl <martin.kuehl@gmail.com>
2009-07-15 16:31:20 +0000

    Add Hasstuff instances for Sentence git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@11934 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Maude/Sentence.hs
+++ Maude/Sentence.hs
@@ -22,6 +22,10 @@ import Maude.AS_Maude
 
 import Maude.Printing
 
+import Maude.Meta.HasSorts
+import Maude.Meta.HasOps
+import Maude.Meta.HasLabels
+
 import Common.Doc
 import Common.DocUtils
 
@@ -29,12 +33,43 @@ data Sentence = Membership Membership
               | Equation Equation
               | Rule Rule
     deriving (Show, Read, Ord, Eq)
-    
+
+
 instance Pretty Sentence where
   pretty (Membership mb) = text $ printMb mb
   pretty (Equation eq) = text $ printEq eq
   pretty (Rule rl) = text $ printRl rl
-  
+
+instance HasSorts Sentence where
+    getSorts sen = case sen of
+        Membership mb -> getSorts mb
+        Equation eq   -> getSorts eq
+        Rule rl       -> getSorts rl
+    mapSorts mp sen = case sen of
+        Membership mb -> Membership $ mapSorts mp mb
+        Equation eq   -> Equation $ mapSorts mp eq
+        Rule rl       -> Rule $ mapSorts mp rl
+
+instance HasOps Sentence where
+    getOps sen = case sen of
+        Membership mb -> getOps mb
+        Equation eq   -> getOps eq
+        Rule rl       -> getOps rl
+    mapOps mp sen = case sen of
+        Membership mb -> Membership $ mapOps mp mb
+        Equation eq   -> Equation $ mapOps mp eq
+        Rule rl       -> Rule $ mapOps mp rl
+
+instance HasLabels Sentence where
+    getLabels sen = case sen of
+        Membership mb -> getLabels mb
+        Equation eq   -> getLabels eq
+        Rule rl       -> getLabels rl
+    mapLabels mp sen = case sen of
+        Membership mb -> Membership $ mapLabels mp mb
+        Equation eq   -> Equation $ mapLabels mp eq
+        Rule rl       -> Rule $ mapLabels mp rl
+
 
 -- | extract the Sentences of a Module
 getSentences :: Spec -> [Sentence]
