9bcebf0424e96d1c737455490f058ea62f5080a6
Christian Maeder <Christian.Maeder@dfki.de>
2009-08-04 16:09:14 +0000

    removed further haifa stuff git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@12026 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/installHetsPkgs.sh
+++ utils/installHetsPkgs.sh
@@ -3,19 +3,17 @@
 if [ -n "$1" ]
 then
 for p in \
- curl-1.3.5 \
- tagsoup-0.6 \
- hxt-8.2.0 \
- HTTP-3001.1.5 \
- hxt-filter-8.2.0 \
  fgl-5.4.2.2 \
  xml-1.3.4 \
- dataenc-0.13.0.0 \
  tabular-0.1.0.2 \
  zlib-0.5.0.0 \
  HTTP-4000.0.7 \
  cabal-install-0.6.2 \
  HaXml-1.13.3 \
+ curl-1.3.5 \
+ tagsoup-0.6 \
+ hxt-8.3.1 \
+ hxt-filter-8.3.0 \
  tar-0.3.0.0 \
  Shellac-0.9.5 \
  utf8-string-0.3.4 \
