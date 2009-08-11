64f2afbaadbbc9ffc4d10eb413325777f7e7fef8
Christian Maeder <Christian.Maeder@dfki.de>
2009-08-11 11:31:18 +0000

    regenerated instances git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@12076 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Haskell/BaseATC.hs
+++ Haskell/BaseATC.hs
@@ -17,30 +17,24 @@ import Common.ATerm.Lib
 import Data.Typeable
 import SrcLoc
 
-_tc_SrcLocTc :: TyCon
-_tc_SrcLocTc = mkTyCon "SrcLoc"
+_tcSrcLocTc :: TyCon
+_tcSrcLocTc = mkTyCon "SrcLoc.SrcLoc"
 instance Typeable SrcLoc where
-    typeOf _ = mkTyConApp _tc_SrcLocTc []
+    typeOf _ = mkTyConApp _tcSrcLocTc []
 
 instance ShATermConvertible SrcLoc where
-  toShATermAux = toShATermAux_SrcLoc
-  fromShATermAux = fromShATermAux_SrcLoc
-
-toShATermAux_SrcLoc :: ATermTable -> SrcLoc -> IO (ATermTable, Int)
-toShATermAux_SrcLoc att0 (SrcLoc aa ab ac ad) = do
-        (att1,aa') <- toShATerm' att0 aa
-        (att2,ab') <- toShATerm' att1 ab
-        (att3,ac') <- toShATerm' att2 ac
-        (att4,ad') <- toShATerm' att3 ad
-        return $ addATerm (ShAAppl "SrcLoc" [ aa',ab',ac',ad' ] []) att4
-
-fromShATermAux_SrcLoc :: Int -> ATermTable -> (ATermTable, SrcLoc)
-fromShATermAux_SrcLoc ix att0 =
-        case getShATerm ix att0 of
-            ShAAppl "SrcLoc" [ aa,ab,ac,ad ] _ ->
-                    case fromShATerm' aa att0 of { (att1, aa') ->
-                    case fromShATerm' ab att1 of { (att2, ab') ->
-                    case fromShATerm' ac att2 of { (att3, ac') ->
-                    case fromShATerm' ad att3 of { (att4, ad') ->
-                    (att4, SrcLoc aa' ab' ac' ad') }}}}
-            u -> fromShATermError "SrcLoc" u
+  toShATermAux att0 xv = case xv of
+    SrcLoc a b c d -> do
+      (att1, a') <- toShATerm' att0 a
+      (att2, b') <- toShATerm' att1 b
+      (att3, c') <- toShATerm' att2 c
+      (att4, d') <- toShATerm' att3 d
+      return $ addATerm (ShAAppl "SrcLoc" [a',b',c',d'] []) att4
+  fromShATermAux ix att0 = case getShATerm ix att0 of
+    ShAAppl "SrcLoc" [a,b,c,d] _ ->
+      case fromShATerm' a att0 of { (att1, a') ->
+      case fromShATerm' b att1 of { (att2, b') ->
+      case fromShATerm' c att2 of { (att3, c') ->
+      case fromShATerm' d att3 of { (att4, d') ->
+      (att4, SrcLoc a' b' c' d') }}}}
+    u -> fromShATermError "SrcLoc" u
