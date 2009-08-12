7e7b41d8cf398ab04838e595c9535b77f8ff5b7d
Ewaryst Schulz <Ewaryst.Schulz@dfki.de>
2009-08-12 16:22:34 +0000

    transformation with State monad git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@12099 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CASL/ToItem.hs
+++ CASL/ToItem.hs
@@ -6,21 +6,28 @@ License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
 
 Maintainer  :  Ewaryst.Schulz@dfki.de
 Stability   :  experimental
-Portability :  portable
+Portability :  non-portable
 
 get item representation of 'BASIC_SPEC'
 -}
 
 module CASL.ToItem (bsToItem) where
 
+import Control.Monad.State
+
 import Common.Doc
 import Common.DocUtils
 import Common.Id
 import Common.Item
-import Common.AS_Annotation
+--import Common.AS_Annotation
 
 import CASL.AS_Basic_CASL
 import CASL.ToDoc()
+
+-- utils
+mapF :: Functor f => (a -> b) -> [f a] -> [f b]
+mapF f = map $ fmap f
+
 
 
 instance ItemTypeable SortsKind where
@@ -28,82 +35,105 @@ instance ItemTypeable SortsKind where
     toIT PossiblyEmptySorts = Possiblyempty
 
 
-data (Pretty b, Pretty s, Pretty f) =>
-    TransState b s f = TS { fB :: (b -> Doc)
-                          , fS :: (s -> Doc)
-                          , fF :: (f -> Doc) }
+-- TS = TransState
+data TS b s f = TS { fB :: (b -> Doc)
+                   , fS :: (s -> Doc)
+                   , fF :: (f -> Doc) }
 
-bsToItem :: (Pretty b, Pretty s, Pretty f) => BASIC_SPEC b s f -> Item
-bsToItem = basicSpecToItem $ TS pretty pretty pretty
 
-basicSpecToItem :: (Pretty b, Pretty s, Pretty f) => 
-                   TransState b s f -> BASIC_SPEC b s f -> Item
-basicSpecToItem st (Basic_spec l) =
-    rootItem { items = map (fmap $ basicItem2I st) l }
+-- this function is only to unify the types of the state and the basic spec
+-- in the call of toitem and runState in bsToItem
+getTransState :: (Pretty b, Pretty s, Pretty f) => BASIC_SPEC b s f ->
+                 TS b s f
+getTransState _ = TS pretty pretty pretty
 
--- only to copy from it later
-basicItemToItem :: (Pretty b, Pretty s, Pretty f) => 
-                   TransState b s f -> BASIC_ITEMS b s f -> Item
-basicItemToItem _ sis = liftIT2I $ Abstract $ show $ pretty sis
-----
+bsToItem :: (Pretty b, Pretty s, Pretty f) => (BASIC_SPEC b s f) -> Item
+bsToItem bs = fst $ runState (toitem bs) $ getTransState bs
 
-basicItem2I :: (Pretty b, Pretty s, Pretty f) => 
-               TransState b s f -> BASIC_ITEMS b s f -> Item
-basicItem2I st (Sig_items s) = sigItem2I st s
-basicItem2I st (Free_datatype sk l rg) =
-    Item Freedatatype rg $ (liftIT2AI $ toIT sk) : [] -- map (fmap $ dtdecl2I st) l
+instance ItemConvertible (BASIC_SPEC b s f) (State (TS b s f)) where
+    toitem (Basic_spec l) = 
+        do{ l' <-  listfromAList l
+          ; return rootItem{ items = l' } }
 
-basicItem2I st (Sort_gen _ rg) = Item Sortgen rg []
-basicItem2I st (Var_items _ rg) = Item Varitems rg []
-basicItem2I st (Local_var_axioms _ _ rg) = Item Localvaraxioms rg []
-basicItem2I st (Axiom_items _ rg) = Item Axiomitems rg []
-basicItem2I st (Ext_BASIC_ITEMS _) = Item Extbasicitems nullRange []
+instance ItemConvertible (BASIC_ITEMS b s f) (State (TS b s f)) where
+    toitem bi =
+        case bi of
+          Sig_items s -> toitem s
+          _ -> return $ abstract "basicitem"
 
-sigItem2I :: (Pretty b, Pretty s, Pretty f) => 
-               TransState b s f -> SIG_ITEMS s f -> Item
+instance ItemConvertible (SIG_ITEMS s f) (State (TS b s f)) where
+    toitem si = return $ liftIT2I $ Simple Sig
 
-sigItem2I _ _ = liftIT2I Sigitem
+
+
 
 {-
-basicItemToItem :: TransState b s f -> BASIC_ITEMS b s f -> Item
-basicItemToItem st sis = let
-  rans = if null $ r_annos sis then [] else [sis { item = "", l_annos = [] }]
-  in case item sis of
-    Sig_items s -> sigItemToItem s sis
-    Free_datatype sk l _ ->
-      sis { item = show $ keyword freeS <+> keyword (typeString sk l)
-          , r_annos = [] }
-      : addSemi (map dtdeclToItem l)
-      ++ rans
-    Sort_gen l _ -> case l of
-         [Annoted (Datatype_items sk l' _) _ las ras] ->
-           let alas = l_annos sis ++ las
-               aras = ras ++ r_annos sis
-           in sis
-           { item = show $ keyword generatedS <+> keyword (typeString sk l')
-           , l_annos = alas, r_annos = [] }
-           : addSemi (map dtdeclToItem l')
-           ++ if null aras then [] else
-                  [sis { item = "", l_annos = [], r_annos = aras} ]
-         _ -> sis { item = show $ keyword generatedS <+> text "{"
-                  , r_annos = [] }
-              : concatMap (sigItemToItem fS fF) l
-              ++ [sis { item = "}", l_annos = [] }]
-    Var_items l _ -> [sis
-      { item = show $ topSigKey (varS ++ pluralS l)
-        <+> fsep (punctuate semi $ map printVarDecl l) }]
-    Local_var_axioms l f _ -> sis
-      { item = show $ fsep $ forallDoc : punctuate semi (map printVarDecl l)
-      , r_annos = [] }
-      : map (formToItem fF) f
-      ++ rans
-    Axiom_items f _ ->
-      if null $ l_annos sis then [] else [sis { item = "", r_annos = [] }]
-      ++ map (formToItem fF) f
-      ++ rans
-    Ext_BASIC_ITEMS b -> [sis { item = show $ fB b }]
 
-formToItem :: TransState b s f -> Annoted (FORMULA f) -> Annoted String
+bsToItem2 :: (Pretty b, Pretty s, Pretty f) => BASIC_SPEC b s f -> Item
+bsToItem2 = basicSpecToItem $ TS pretty pretty pretty
+
+
+basicSpecToItem :: TS b s f ->
+                   BASIC_SPEC b s f -> Item
+basicSpecToItem st (Basic_spec l) =
+    rootItem { items = mapF (basicItem2I st) l }
+
+
+basicItem2I :: TS b s f ->
+               BASIC_ITEMS b s f -> Item
+
+basicItem2I st (Sig_items s) = sigItem2I st s
+basicItem2I st (Free_datatype sk l rg) =
+    Item (IT Datatype Free) rg [] $ (liftIT2AI sk) : [] -- mapF (dtdecl2I st) l
+basicItem2I st (Sort_gen _ rg) = Item (IT SortK Gen) rg [] []
+basicItem2I st (Var_items _ rg) = Item (IT Var Items) rg [] []
+basicItem2I st (Local_var_axioms _ _ rg) = Item Localvaraxioms rg [] []
+basicItem2I st (Axiom_items _ rg) = Item Axiomitems rg [] []
+basicItem2I st (Ext_BASIC_ITEMS _) = Item (IT Basic Ext) nullRange [] []
+
+
+sigItem2I :: TS b s f ->
+             SIG_ITEMS s f -> Item
+
+sigItem2I st (Sort_items sk sis rg) =
+    Item (IT SortK Items) rg [] $ mapF (sortItem2I st) sis
+sigItem2I _ _ = liftIT2I (Simple Sig)
+
+
+sortItem2I :: TS b s f ->
+              SORT_ITEM f -> Item
+
+sortItem2I st (Sort_decl l rg) = liftIT2I (Simple SortK)
+sortItem2I st (Subsort_decl l t rg) = liftIT2I (Simple SortK)
+sortItem2I st (Subsort_defn s v s' f rg) = liftIT2I (Simple SortK)
+sortItem2I st (Iso_decl l rg) = liftIT2I (Simple SortK)
+-}
+
+{-
+
+data SORT_ITEM f = Sort_decl [SORT] Range
+                 -- pos: commas
+               | Subsort_decl [SORT] SORT Range
+                 -- pos: commas, <
+               | Subsort_defn SORT VAR SORT (Annoted (FORMULA f)) Range
+                 -- pos: "=", "{", ":", ".", "}"
+                 -- the left anno list stored in Annoted Formula is
+                 -- parsed after the equal sign
+               | Iso_decl [SORT] Range
+                 -- pos: "="s
+                 deriving Show
+
+Sort_items SortsKind [Annoted (SORT_ITEM f)] Range
+                 -- pos: sort, semi colons
+               | Op_items [Annoted (OP_ITEM f)] Range
+                 -- pos: op, semi colons
+               | Pred_items [Annoted (PRED_ITEM f)] Range
+                 -- pos: pred, semi colons
+               | Datatype_items SortsKind [Annoted DATATYPE_DECL] Range
+                 -- type, semi colons
+               | Ext_SIG_ITEMS s
+
+formToItem :: TS b s f -> Annoted (FORMULA f) -> Annoted String
 formToItem s f = f { item = show . addBullet . printFormula (fF s) $ item f }
 
 sigItemToItem :: (s -> Doc) -> (f -> Doc) -> Annoted (SIG_ITEMS s f)
