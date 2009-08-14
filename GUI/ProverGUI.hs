113723579acf4ed438b4fd17a9222c8b992f91ab
Christian Maeder <Christian.Maeder@dfki.de>
2009-08-14 13:45:14 +0000

    let haddock (make doc) go through git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@12132 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GUI/ProverGUI.hs
+++ GUI/ProverGUI.hs
@@ -23,31 +23,9 @@ import Proofs.AbstractState
 import qualified Comorphisms.KnownProvers as KnownProvers
 import Control.Concurrent
 
-{- #ifdef GTKGLADE
-import GUI.GtkProverGUI
-
-proverGUI ::
-  (Logic lid sublogics1
-             basic_spec1
-             sentence
-             symb_items1
-             symb_map_items1
-             sign1
-             morphism1
-             symbol1
-             raw_symbol1
-             proof_tree1) =>
-     lid
-  -> ProofActions lid sentence -- ^ record of possible GUI actions
-  -> String -- ^ theory name
-  -> String -- ^ warning information
-  -> G_theory -- ^ theory
-  -> KnownProvers.KnownProversMap -- ^ map of known provers
-  -> [(G_prover,AnyComorphism)] -- ^ list of suitable comorphisms to provers
-                                -- for sublogic of G_theory
-  -> IO (Result.Result G_theory)
-proverGUI = showProverGUI-}
--- #elif defined UNI_PACKAGE
+#ifdef GTKGLADE
+-- import GUI.GtkProverGUI
+#endif
 
 import GUI.ProofManagement
 
@@ -75,6 +53,4 @@ proverGUI lid prGuiAcs thName warningTxt th knownProvers comorphList = do
   guiMVar <- newMVar Nothing
   proofManagementGUI lid prGuiAcs thName warningTxt th knownProvers comorphList
                      guiMVar
--- #else
--- proverGUI = error "not implemented"
--- #endif
+
