771f3233af6306bd5028b262b6cd33248109649c
Martin Kühl <martin.kuehl@gmail.com>
2009-08-20 11:02:51 +0000

    Use pretty instead of printSign git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@12172 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Maude/Shellout.hs
+++ Maude/Shellout.hs
@@ -5,7 +5,6 @@ import System.IO
 import System.Process
 
 import Maude.AS_Maude
-import Maude.Printing (printSign)
 
 import Maude.Sign (Sign)
 import Maude.Sentence (Sentence)
@@ -13,6 +12,9 @@ import qualified Maude.Sign as Sign
 import qualified Maude.Sentence as Sen
 
 import Data.List (isPrefixOf)
+
+import Common.Doc
+import Common.DocUtils (Pretty(..))
 
 
 maudePath :: String
@@ -24,11 +26,11 @@ maudeHetsPath = "/Users/adrian/Hets/Maude/hets.prj"
 
 
 basicAnalysis :: Sign -> MaudeText -> IO (Sign, [Sentence])
-basicAnalysis sign (MaudeText text) = do
+basicAnalysis sign (MaudeText mt) = do
     (hIn, hOut, _, _) <- runInteractiveCommand maudeCmd
     hPutStrLn hIn $ unwords ["in", maudeHetsPath]
-    let printedSign = printSign (Sign.sorts sign) (Sign.subsorts sign) (Sign.ops sign)
-    hPutStrLn hIn $ unwords ["(fmod FROM-HETS is", printedSign, text, "endfm)"]
+    let printedSign = parens $ vsep [text "fmod FROM-HETS is", pretty sign, text mt, text "endfm"]
+    hPutStrLn hIn $ show printedSign
     hClose hIn
     specOut <- hGetContents hOut
     return $ convertSpec $ read $ findSpec specOut
