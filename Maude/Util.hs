deaec8dc3bb119e3e0ee6edc6a6e7be1e5db26af
Martin Kühl <martin.kuehl@gmail.com>
2009-08-20 11:02:55 +0000

    Add utility function to extract Types from Terms. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@12178 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Maude/Util.hs
+++ Maude/Util.hs
@@ -1,4 +1,19 @@
+{- |
+Module      :  $Header$
+Description :  Utility Functions
+Copyright   :  (c) Martin Kuehl, Uni Bremen 2009
+License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
+
+Maintainer  :  mkhl@informatik.uni-bremen.de
+Stability   :  experimental
+Portability :  portable
+
+Utility functions used in the Maude language module.
+-}
+
 module Maude.Util where
+
+import Maude.AS_Maude
 
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -6,3 +21,10 @@ import qualified Data.Map as Map
 
 mapAsFunction :: (Ord a) => Map a a -> (a -> a)
 mapAsFunction mp name = Map.findWithDefault name name mp
+
+
+termType :: Term -> Type
+termType term = case term of
+    Const _ typ   -> typ
+    Var _ typ     -> typ
+    Apply _ _ typ -> typ
