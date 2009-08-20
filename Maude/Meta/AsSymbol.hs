ecf79f3ba9d0ae326915831eab64b1deb1f0e672
Martin Kühl <martin.kuehl@gmail.com>
2009-08-20 11:02:53 +0000

    Define an AsSymbol type class. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@12175 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ Maude/Meta/AsSymbol.hs
@@ -0,0 +1,63 @@
+module Maude.Meta.AsSymbol (
+    AsSymbol(..),
+) where
+
+import Maude.AS_Maude
+import Maude.Symbol
+import Maude.Meta.HasName
+import Maude.Util
+
+import Data.Maybe (fromJust)
+import qualified Data.Set as Set
+
+
+class AsSymbol a where
+    asSymbol :: a -> Symbol
+    asSymbol = fromJust . asSymbolMaybe
+    asSymbolMaybe :: a -> Maybe Symbol
+    asSymbolMaybe = Just . asSymbol
+    asSymbolSet :: a -> SymbolSet
+    asSymbolSet = maybe Set.empty Set.singleton . asSymbolMaybe
+    mapAsSymbol :: (Symbol -> a) -> SymbolMap -> a -> a
+    mapAsSymbol ctor mp item = let extract = ctor . mapAsFunction mp
+        in maybe item extract $ asSymbolMaybe item
+
+
+instance AsSymbol Type where
+    asSymbol typ = case typ of
+        TypeSort sort -> asSymbol sort
+        TypeKind kind -> asSymbol kind
+
+instance AsSymbol Sort where
+    asSymbol = Sort . getName
+
+instance AsSymbol Kind where
+    asSymbol = Kind . getName
+
+instance AsSymbol LabelId where
+    asSymbol = Labl . getName
+
+instance AsSymbol StmntAttr where
+    asSymbolMaybe attr = case attr of
+        Label name -> Just $ Labl name
+        _          -> Nothing
+
+instance AsSymbol Operator where
+    asSymbol (Op op dom cod _) = let
+            op'  = getName op
+            dom' = map asSymbol dom
+            cod' = asSymbol cod
+        in Operator op' dom' cod'
+
+
+instance AsSymbol Term where
+    asSymbolMaybe term = case term of
+        Const _ _ -> Nothing
+        Var _ _   -> Nothing
+        Apply op ts tp -> Just $
+            Operator op (map (asSymbol . termType) ts) (asSymbol tp)
+
+
+instance AsSymbol Symbol where
+    asSymbol = id
+    mapAsSymbol ctor mp = ctor . mapAsFunction mp
