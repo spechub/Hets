58ce20ad60852f3b545aca0faa8de267309279f4
Martin Kühl <martin.kuehl@gmail.com>
2009-08-20 11:02:55 +0000

    Add AsSymbol to Meta. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@12177 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Maude/Meta.hs
+++ Maude/Meta.hs
@@ -1,7 +1,7 @@
 {- |
 Module      :  $Header$
 Description :  Meta information about Maude datatypes
-Copyright   :  (c) Martin Kuehl, Uni Bremen 2008
+Copyright   :  (c) Martin Kuehl, Uni Bremen 2008-2009
 License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
 
 Maintainer  :  mkhl@informatik.uni-bremen.de
@@ -10,17 +10,13 @@ Portability :  portable
 
 Meta information about Maude datatypes.
 -}
-{-
-  Ref.
-
-  ...
--}
 
 module Maude.Meta (
     module HasName,
     module HasSorts,
     module HasOps,
     module HasLabels,
+    module AsSymbol,
 ) where
 
 
@@ -28,3 +24,4 @@ import Maude.Meta.HasName as HasName
 import Maude.Meta.HasSorts as HasSorts
 import Maude.Meta.HasOps as HasOps
 import Maude.Meta.HasLabels as HasLabels
+import Maude.Meta.AsSymbol as AsSymbol
