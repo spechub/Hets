4846d677bd72a2110abc180503c969f12f1b7d84
Martin Kühl <martin.kuehl@gmail.com>
2009-08-29 11:27:41 +0000

    Formatting. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@12338 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Maude/Logic_Maude.hs
+++ Maude/Logic_Maude.hs
@@ -17,20 +17,21 @@ import Logic.Logic
 
 import Maude.AS_Maude (MaudeText(..))
 import Maude.Parse (mStuff)
-import Maude.Sign     (Sign)
-import Maude.Morphism (Morphism)
 import Maude.Symbol   (Symbol)
 import Maude.Sentence (Sentence)
-import qualified Maude.Sign     as Sign
-import qualified Maude.Morphism as Morphism
+import Maude.Sign     (Sign)
+import Maude.Morphism (Morphism)
 import qualified Maude.Symbol   as Symbol
 import qualified Maude.Sentence as Sentence
+import qualified Maude.Sign     as Sign
+import qualified Maude.Morphism as Morphism
 
 import Maude.ATC_Maude ()
 
 import Maude.Shellout
 
 import Common.AS_Annotation
+import Common.DocUtils
 import Common.ExtSign
 import System.IO.Unsafe
 
@@ -39,7 +40,7 @@ import Data.Maybe
 
 -- | Lid for Maude
 data Maude = Maude
-    deriving (Show, Eq)
+           deriving (Show, Eq)
 
 
 -- | Instance of Language for Maude
@@ -47,10 +48,10 @@ instance Language Maude where
     description _ = unlines
         [ "Maude - A High-Performance Rewriting Logic Framework"
         , "This logic is rewriting logic, a logic of concurrent change that"
-        , " can naturally deal with state and with concurrent computations."
+        , "can naturally deal with state and with concurrent computations."
         , "For an overview of Maude see <http://maude.cs.uiuc.edu/overview.html>."
-        , "For an information on rewriting logic see <http://maude.cs.uiuc.edu/rwl.html>."
-        , "For anything else about the Maude project see <http://maude.cs.uiuc.edu/>." ]
+        , "For information about rewriting logic see <http://maude.cs.uiuc.edu/rwl.html>."
+        , "For information about the Maude project see <http://maude.cs.uiuc.edu/>." ]
 
 
 -- | Instance of Category for Maude
@@ -63,7 +64,6 @@ instance Category Sign Morphism where
     isInclusion = Morphism.isInclusion
     legal_mor = Morphism.isLegal
 
-
 -- | Instance of Sentences for Maude
 instance Sentences Maude Sentence Sign Morphism Symbol where
     -- sentences --
@@ -71,29 +71,29 @@ instance Sentences Maude Sentence Sign Morphism Symbol where
     map_sen Maude = Morphism.mapSentence
     simplify_sen Maude = Sign.simplifySentence
     -- parse_sentence Maude = Nothing
-    -- print_sign Maude = pretty
+    print_sign Maude = pretty
     -- print_named Maude = printAnnoted (addBullet . pretty) . fromLabelledSen
     -- symbols --
     sym_name Maude = Symbol.toId
     sym_of Maude = Sign.symbols
     symmap_of Maude = Morphism.symbolMap
 
-
 -- | Instance of Syntax for Maude
--- TODO: Implement real instance of Syntax for Maude
 instance Syntax Maude MaudeText () () where
     parse_basic_spec Maude = Just $ fmap MaudeText mStuff
     -- parse_symb_items
     -- parse_symb_map_items
 
 -- | Instance of StaticAnalysis for Maude
--- TODO: Implement real instance of StaticAnalysis for Maude
-instance StaticAnalysis Maude MaudeText Sentence () () Sign Morphism Symbol
-    Symbol where
+instance StaticAnalysis Maude
+         MaudeText Sentence () () Sign Morphism Symbol Symbol
+  where
     -- static analysis --
-    basic_analysis Maude = Just $ \ (bs, sig, _) ->
-        let (rsig, sens) = unsafePerformIO $ basicAnalysis sig bs
-        in return (bs, mkExtSign rsig, map (makeNamed "") sens)
+    basic_analysis Maude = let
+        analyze (spec, sign, _) = let
+            (rsig, sens) = unsafePerformIO $ basicAnalysis sign spec
+            in return (spec, mkExtSign rsig, map (makeNamed "") sens)
+        in Just analyze
     -- stat_symb_map_items
     -- stat_symb_items
     -- amalgamation --
@@ -110,7 +110,7 @@ instance StaticAnalysis Maude MaudeText Sentence () () Sign Morphism Symbol
     signature_union Maude sign1 sign2 = return $ Sign.union sign1 sign2
     intersection Maude sign1 sign2 = return $ Sign.intersection sign1 sign2
     -- final_union
-    morphism_union Maude m1 m2 = return $ Morphism.union m1 m2
+    morphism_union Maude mor1 mor2 = return $ Morphism.union mor1 mor2
     subsig_inclusion Maude src tgt = return $ Morphism.inclusion src tgt
     -- generated_sign
     -- cogenerated_sign
@@ -120,11 +120,10 @@ instance StaticAnalysis Maude MaudeText Sentence () () Sign Morphism Symbol
     -- is_injective
     -- theory_to_taxonomy
 
-
 -- | Instance of Logic for Maude
--- TODO: Implement real instance of Logic for Maude
-instance Logic Maude () MaudeText Sentence () () Sign Morphism Symbol Symbol
-    () where
+instance Logic Maude
+         () MaudeText Sentence () () Sign Morphism Symbol Symbol ()
+  where
     stability Maude = Experimental
     -- data_logic
     -- top_sublogic
