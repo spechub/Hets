9f5a9f1af32e035c83740b8da11649d200cd4f60
Christian Maeder <Christian.Maeder@dfki.de>
2009-10-01 11:56:52 +0000

    moved parser module to PGIP but kept a Main module in CMDL for a stand-alone version git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@12555 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CMDL/ParseProofScript.hs
+++ CMDL/ParseProofScript.hs
@@ -10,117 +10,12 @@ Portability :  portable
 
 module Main where
 
-import Interfaces.Command
-
 import PGIP.XMLstate
-
-import CMDL.DataTypes
-import CMDL.Commands
-
-import Text.ParserCombinators.Parsec
-import Text.ParserCombinators.Parsec.Pos (initialPos)
+import PGIP.ParseProofScript (parseContent, xmlLitCmds)
 
 import Text.XML.Light as XML
 
-import Common.Utils
-
-import Data.Char
-import Data.Either
-import Data.List
-
 import System.Environment
-
-data WhiteWord = WhiteWord
-  { leading :: String -- ^ leading white space
-  , word :: String }
-
-data LitCommand = LitCommand
-  { commandWW :: WhiteWord
-  , argumentWW :: Maybe WhiteWord
-  , trailing :: String -- ^ trailing white space
-  , command :: Command }
-
-parseLine :: [Command] -> FilePath -> Int -> Parser LitCommand
-parseLine cl fp i = do
-  setPosition $ setSourceLine (initialPos fp) i
-  s1 <- many space
-  (eof >> return LitCommand
-      { commandWW = WhiteWord "" ""
-      , argumentWW = Nothing
-      , trailing = s1
-      , command = GroupCmd [] }) <|> do
-    (cs, cm) <- choice (map (\ (c, sl) -> do
-      s <- try $ parseCommand sl
-      return (s, c)) $ (GlobCmd ProveCurrent, ["prove-all"])
-        : map (\ c -> (c, words $ cmdNameStr c)) cl)
-      <|> do
-        h <- char '#'
-        r <- many anyChar
-        return (h : r, CommentCmd r)
-    s2 <- many space
-    (eof >> return LitCommand
-        { commandWW = WhiteWord s1 cs
-        , argumentWW = Nothing
-        , trailing = s2
-        , command = cm }) <|> do
-      (arg, s3) <- parseArgument
-      return LitCommand
-        { commandWW = WhiteWord s1 cs
-        , argumentWW = Just $ WhiteWord s2 arg
-        , trailing = s3
-        , command = setInputStr arg cm }
-
-parseArgument :: Parser (String, String)
-parseArgument = do
-  arg <- many1 (satisfy $ not . isSpace)
-  sp <- many space
-  (eof >> return (arg, sp)) <|> do
-    (rargs, s) <- parseArgument
-    return (arg ++ sp ++ rargs, s)
-
-parseCommand :: [String] -> Parser String
-parseCommand cmd = case cmd of
-   [] -> fail ""
-   [c] -> string c
-   c : r -> do
-     string c
-     s <- many1 space
-     t <- parseCommand r
-     return $ c ++ s ++ t
-
-parseContent :: FilePath -> String -> Either String [LitCommand]
-parseContent fp str = let
-  l = number $ lines str
-  (es, rs) = partitionEithers $ map (\ (s, i) ->
-     parse (parseLine (map cmdDescription getCommands) fp i) fp s) l
-  in if null es then Right rs else
-     Left $ unlines $ map show es
-
-whiteSpaceElems :: String -> [Element]
-whiteSpaceElems str = if null str then [] else [unode "whitespace" str]
-
-litString :: LitCommand -> String
-litString c = word (commandWW c)
-  ++ maybe "" (\ a -> leading a ++ word a) (argumentWW c)
-
-closeTheory :: Bool -> [Element]
-closeTheory isOpen = if isOpen then [unode "closetheory" ()] else []
-
-xmlLitCmds :: String -> Bool -> [LitCommand] -> [Element]
-xmlLitCmds trailer isOpen ls = case ls of
-  [] -> closeTheory isOpen ++ whiteSpaceElems trailer
-  h : r ->
-    let wsp = trailer ++ leading (commandWW h)
-        wspes = whiteSpaceElems wsp
-    in case command h of
-    SelectCmd LibFile _ ->
-      wspes ++ closeTheory isOpen ++
-      [unode "opentheory" $ litString h]
-      ++ xmlLitCmds (trailing h ++ "\n") True r
-    GroupCmd _ -> xmlLitCmds (wsp ++ "\n") isOpen r
-    CommentCmd _ -> xmlLitCmds (wsp ++ litString h ++ "\n") isOpen r
-    _ -> wspes ++ [unode "proofstep" $ litString h]
-         ++ xmlLitCmds (trailing h ++ "\n") isOpen r
 
 parseHPF :: FilePath -> IO ()
 parseHPF fp = do
