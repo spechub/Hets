ba5a19dbf443ea7d701392e6a9c3a46fd0958417
Christian Maeder <Christian.Maeder@dfki.de>
2009-12-22 13:19:28 +0000

    set Module header git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@12951 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/appendHaskellPreludeString.hs
+++ utils/appendHaskellPreludeString.hs
@@ -1,5 +1,5 @@
 {- |
-Module      :  appendHaskellPreludeString.hs
+Module      :  $Id$
 Copyright   :  (c) Christian Maeder, Uni Bremen 2002-2004
 License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
 
