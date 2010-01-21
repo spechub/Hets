ce66c00b2110ab334eb1e86e05c1c51bf4e190ba
Christian Maeder <Christian.Maeder@dfki.de>
2010-01-21 17:42:41 +0000

    adjusted to Isabelle2009 git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@12994 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/nightly/runHsIsabelle.sh
+++ utils/nightly/runHsIsabelle.sh
@@ -3,5 +3,7 @@
 for i in $*
 do
    j=`basename $i .thy`
-   (cd `dirname $i`; isabelle-process -q -e "use_thy \"$j\";print_depth 300;theory \"$j\";quit();" HOLCF)
+   ( cd `dirname $i`; \
+     echo " use_thy \"$j\"; quit;" \
+     | isabelle tty -l HOLCF )
 done
