1b8a6c29c7049d484da96110daee0896c88b87ba
Christian Maeder <Christian.Maeder@dfki.de>
2010-01-21 11:31:59 +0000

    change for Isabelle2009-1 git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@12989 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/nightly/runisabelle.sh
+++ utils/nightly/runisabelle.sh
@@ -4,8 +4,6 @@ for i in $*
 do
    j=`basename $i .thy`
    ( cd `dirname $i`; \
-     isabelle-process -q -e  \
-       " use_thy \"$j\"; \
-         theory \"$j\"; \
-         quit(); " )
+     echo " use_thy \"$j\"; quit;" \
+     | isabelle tty )
 done
