e6cc811bbde12c09370c2685bf944ce03e19b99e
Christian Maeder <Christian.Maeder@dfki.de>
2010-01-22 15:40:47 +0000

    ghc-6.12.1 packages for hets git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@13001 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/installHetsPkgs.sh
+++ utils/installHetsPkgs.sh
@@ -3,20 +3,24 @@
 if [ -n "$1" ]
 then
 for p in \
+ mtl-1.1.0.2 \
+ parsec-2.1.0.1 \
+ network-2.2.1.7 \
  fgl-5.4.2.2 \
- xml-1.3.4 \
+ xml-1.3.5 \
+ html-1.0.1.2 \
  tabular-0.1.0.2 \
- zlib-0.5.0.0 \
- HTTP-4000.0.7 \
- cabal-install-0.6.2 \
- HaXml-1.13.3 \
+ zlib-0.5.2.0 \
+ HTTP-4000.0.9 \
+ cabal-install-0.8.0 \
  curl-1.3.5 \
- tagsoup-0.6 \
- hxt-8.3.1 \
- hxt-filter-8.3.0 \
- tar-0.3.0.0 \
- utf8-string-0.3.4 \
- haskeline-0.6.2.1
+ tar-0.3.1.0 \
+ deepseq-1.1.0.0 \
+ QuickCheck-2.1.0.3 \
+ HUnit-1.2.2.1 \
+ tagsoup-0.8 \
+ hxt-8.5.0 \
+ hxt-filter-8.4.0
 do ./installPkg.sh $p $1
 done
 else
