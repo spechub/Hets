e65501997bed48428c66b45c4defa78d11343a87
Tilman Thiry <tilman.thiry@googlemail.com>
2010-01-29 12:38:10 +0000

    abstract syntax for CSPs git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@13015 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Constraints/AS_BASIC_Constraints.hs
+++ Constraints/AS_BASIC_Constraints.hs
@@ -10,3 +10,42 @@ Portability :  portable
 
 Definition of abstract syntax for constraint satisfaction problems
 -}
+
+
+
+
+import Common.Id as Id
+import Common.Doc
+import Common.DocUtils
+import Common.Keywords
+import Common.AS_Annotation as AS_Anno
+
+
+type DOMAIN_ID   = Id.Token
+type RELATION_ID = Id.Token
+type ELEMENT_ID  = Id.Token
+type VARIABLE_ID = Id.Token
+
+                            
+
+data CONSTRAINT = Atomic_Constraint RELATION_ID [VARIABLE_ID] Id.Range
+                deriving Show
+                         
+data TASK = Solve_satisfy
+
+
+data FORMULA =
+       Constraint CONSTRAINT 
+     | Task TASK
+
+
+data BASIC_ITEMS = 
+      Domain_item DOMAIN_ID [ELEMENT_ID] Id.Range
+      -- case of Domain declaration
+    | Relation_item RELATION_ID [DOMAIN_ID] Id.Range
+      -- case of relation declaration
+    | Definition_item RELATION_ID [[ELEMENT_ID]] Id.Range
+      -- case of relation definition
+    | Variable_item [VARIABLE_ID] DOMAIN_ID Id.Range
+      -- case of variable declaration
+    |  Axiom_items [AS_Anno.Annoted (FORMULA)]
