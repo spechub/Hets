4dd86e6aa3acf0f7fb08486b120a6dff23c0ad6f
Tilman Thiry <tilman.thiry@googlemail.com>
2010-01-29 12:04:27 +0000

    start work on constraint processing feature git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@13013 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ Constraints/AS_BASIC_Constraints.hs
@@ -0,0 +1,12 @@
+{- |
+Module      :  $Header$
+Description :  Abstract syntax for Constraint Satisfaction Problems
+Copyright   :  (c) Tilman Thiry, Uni Freiburg 2010
+License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
+
+Maintainer  :  thiry@informatik.uni-freiburg.de
+Stability   :  experimental
+Portability :  portable
+
+Definition of abstract syntax for constraint satisfaction problems
+-}
