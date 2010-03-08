492a92fee9b8d4da932d6bbfdede59bf39091720
Christian Maeder <Christian.Maeder@dfki.de>
2010-03-08 13:32:06 +0000

    create xupdate element git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@13176 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Static/FromXml.hs
+++ Static/FromXml.hs
@@ -16,9 +16,11 @@ import Static.DevGraph
 
 import Common.Result
 import Common.XPath
+import Common.ToXml
 
 import Text.XML.Light
 
+import Data.Either
 import Data.List
 import Data.Maybe
 
@@ -43,17 +45,38 @@ We ignore xupdate:update as this is only used to update a range, currently.
 isXUpdateQN :: QName -> Bool
 isXUpdateQN = (Just "xupdate" ==) . qPrefix
 
+hasLocalQN :: String -> QName -> Bool
+hasLocalQN s = (== s) . qName
+
+isElementQN :: QName -> Bool
+isElementQN = hasLocalQN "element"
+
+isAttributeQN :: QName -> Bool
+isAttributeQN = hasLocalQN "attribute"
+
+isTextQN :: QName -> Bool
+isTextQN = hasLocalQN "text"
+
 isAddQN :: QName -> Bool
 isAddQN q = any (flip isPrefixOf $ qName q) ["insert", "append"]
 
 isRemoveQN :: QName -> Bool
-isRemoveQN q = qName q == "remove"
+isRemoveQN = hasLocalQN "remove"
 
 getAttrVal :: String -> Element -> Maybe String
 getAttrVal = findAttr . unqual
 
 getSelectVal :: Element -> String
 getSelectVal = fromMaybe "" . getAttrVal "select"
+
+getNameAttr :: Element -> Maybe String
+getNameAttr = getAttrVal "name"
+
+str2QName :: String -> QName
+str2QName str = let (ft, rt) = break (== ':') str in
+  case rt of
+    _ : l@(_ : _) -> (unqual l) { qPrefix = Just ft }
+    _ -> unqual str
 
 anaUpdate :: LibEnv -> DGraph -> Element -> Result [Change]
 anaUpdate l g e = let q = elName e in
@@ -62,20 +85,44 @@ anaUpdate l g e = let q = elName e in
           case maybePath $ getSelectVal e of
             Just ex@(PathExpr _ (Path _ s@(_ : _)))
               | isPathExpr ex && isElementNode (last s) ->
-                  fmap concat $ mapM (anaAddElem l g) $ elChildren e
-            _ -> fail $ showElement e
+                  mapM (anaAddElem l g) $ elChildren e
+            _ -> fail $ "anaUpdate: " ++ showElement e
       else if isRemoveQN q then return [] else
           return [Change e]
   else return [Change e]
 
-anaAddElem :: LibEnv -> DGraph -> Element -> Result [Change]
-anaAddElem l g e = let q = elName e in
-  if isXUpdateQN q then undefined else undefined
+anaAddElem :: LibEnv -> DGraph -> Element -> Result Change
+anaAddElem l g e = do
+  r <- newXElem e
+  case r of
+    Left a -> fail $ "expecting element instead of attribute: " ++ showAttr a
+      ++ "\n" ++ showElement e
+    Right c -> case c of
+      Elem xe -> return $ Change xe
+      _ -> fail $ "unexpected text element: " ++ showElement e
+
+newXElem :: Monad m => Element -> m (Either Attr Content)
+newXElem e = let q = elName e in
+  if isXUpdateQN q then
+      if isTextQN q then return $ Right $ mkText $ strContent e
+      else case getNameAttr e of
+        Just n -> let qn = str2QName n in
+          if isAttributeQN q then
+               return $ Left $ Attr qn $ strContent e
+          else if isElementQN q then do
+            lrs <- mapM newXElem $ elChildren e
+            let (as, cs) = partitionEithers lrs
+            return $ Right $ Elem $ add_attrs as
+              $ node qn $ cs
+          else fail $ "no attribute or element: " ++ showElement e
+        Nothing -> fail $ "missing name attribute: " ++ showElement e
+  else fail $ "no xupdate element: " ++ showElement e
 
 {-
  xupdate:element
  xupdate:attribute
+ xupdate:text
 
-xupdate:element may contain further xupdate:element or xupdate:attribute
-tags. Maybe xupdate:text can be ignored.
--}
+xupdate:element may contain xupdate:attribute elements and further
+xupdate:element or xupdate:text elements.
+ -}
