4505f851e7c72a1ed16fc751ac03f298a8d7c46e
Dominik Luecke <luecke@informatik.uni-bremen.de>
2010-03-25 15:55:52 +0000

    fact prover working so far git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@13275 cec4b9c1-7d33-0410-9eda-942365e851bb

--- OWL/java/de/unibremen/informatik/FactProver/FactProver.java
+++ OWL/java/de/unibremen/informatik/FactProver/FactProver.java
@@ -59,17 +59,15 @@ public class FactProver
                 OWLIndividual object  = g.getObject();
                 OWLIndividual subject = g.getSubject();
                 OWLObjectPropertyExpression prop = g.getProperty();
-                boolean res = reasoner.hasObjectPropertyRelationship
+                return reasoner.hasObjectPropertyRelationship
                         (subject, prop, object);
-                return res;
             }
             if (goal instanceof OWLClassAssertionAxiom)
             {
                 OWLClassAssertionAxiom g = (OWLClassAssertionAxiom) goal;
                 OWLDescription cls = g.getDescription();
                 OWLIndividual indi = g.getIndividual();
-                boolean res = reasoner.hasType(indi, cls, false);
-                return res;
+                return reasoner.hasType(indi, cls, false);
             }
             if (goal instanceof OWLDataPropertyAssertionAxiom)
             {
@@ -78,8 +76,7 @@ public class FactProver
                 OWLDataProperty prop = g.getProperty().asOWLDataProperty();
                 OWLConstant obj      = g.getObject();
                 OWLIndividual sub    = g.getSubject();
-                boolean res = reasoner.hasDataPropertyRelationship(sub, prop, obj);
-                return res;
+                return reasoner.hasDataPropertyRelationship(sub, prop, obj);
             }
             if (goal instanceof OWLEquivalentClassesAxiom)
             {
@@ -104,21 +101,12 @@ public class FactProver
                 OWLSubClassAxiom g = (OWLSubClassAxiom) goal;
                 OWLDescription sub = g.getSubClass();
                 OWLDescription top = g.getSuperClass();
-                boolean res = reasoner.isSubClassOf(sub, top);
-                return res;
+                return reasoner.isSubClassOf(sub, top);
             }
             if (goal instanceof OWLDataPropertyRangeAxiom)
             {
                 throw new UnsupportedReasonerOperationException
                             ("Fact++ cannot determine data ranges!");
-            }
-            if (goal instanceof OWLDataPropertyCharacteristicAxiom)
-            {
-                OWLDataPropertyCharacteristicAxiom g =
-                    (OWLDataPropertyCharacteristicAxiom) goal;
-                OWLDataProperty prop = g.getProperty().asOWLDataProperty();
-                boolean res = reasoner.isFunctional(prop);
-                return res;
             }
             if (goal instanceof OWLDataPropertyDomainAxiom)
             {
@@ -161,6 +149,185 @@ public class FactProver
             {
                 throw new UnsupportedReasonerOperationException
                             ("Fact++ cannot determine antisymmetry!");
+            }
+            if (goal instanceof OWLEquivalentDataPropertiesAxiom)
+            {
+                OWLEquivalentDataPropertiesAxiom g =
+                        (OWLEquivalentDataPropertiesAxiom) goal;
+                Set<OWLDataPropertyExpression> props = g.getProperties();
+                TreeSet<OWLDataProperty> p = new TreeSet<OWLDataProperty>();
+                Iterator<OWLDataPropertyExpression> it = props.iterator();
+                p.clear();
+                while (it.hasNext())
+                {
+                  p.add(it.next().asOWLDataProperty());
+                }
+                boolean res = true;
+                Iterator<OWLDataProperty> itt = p.iterator();
+                while (itt.hasNext())
+                {
+                    Set<OWLDataProperty> cur =
+                            reasoner.getEquivalentProperties(itt.next());
+                    res &= cur.containsAll(props);
+                }
+                return res;
+            }
+            if (goal instanceof OWLEquivalentObjectPropertiesAxiom)
+            {
+                OWLEquivalentObjectPropertiesAxiom g =
+                        (OWLEquivalentObjectPropertiesAxiom) goal;
+                Set<OWLObjectPropertyExpression> props = g.getProperties();
+                TreeSet<OWLObjectProperty> p = new TreeSet<OWLObjectProperty>();
+                Iterator<OWLObjectPropertyExpression> it = props.iterator();
+                p.clear();
+                while (it.hasNext())
+                {
+                  p.add(it.next().asOWLObjectProperty());
+                }
+                boolean res = true;
+                Iterator<OWLObjectProperty> itt = p.iterator();
+                while (itt.hasNext())
+                {
+                    Set<OWLObjectProperty> cur =
+                            reasoner.getEquivalentProperties(itt.next());
+                    res &= cur.containsAll(props);
+                }
+                return res;
+            }
+            if (goal instanceof OWLFunctionalDataPropertyAxiom)
+            {
+                OWLFunctionalDataPropertyAxiom g =
+                        (OWLFunctionalDataPropertyAxiom) goal;
+                OWLDataProperty prop = g.getProperty().asOWLDataProperty();
+                return reasoner.isFunctional(prop);
+            }
+            if (goal instanceof OWLFunctionalObjectPropertyAxiom)
+            {
+                OWLFunctionalObjectPropertyAxiom g=
+                        (OWLFunctionalObjectPropertyAxiom) goal;
+                OWLObjectProperty prop = g.getProperty().asOWLObjectProperty();
+                return reasoner.isFunctional(prop);
+            }
+            if (goal instanceof OWLInverseFunctionalObjectPropertyAxiom)
+            {
+                OWLInverseFunctionalObjectPropertyAxiom g =
+                        (OWLInverseFunctionalObjectPropertyAxiom) goal;
+                OWLObjectProperty prop = g.getProperty().asOWLObjectProperty();
+                return reasoner.isInverseFunctional(prop);
+            }
+            if (goal instanceof OWLInverseObjectPropertiesAxiom)
+            {
+                OWLInverseObjectPropertiesAxiom g =
+                        (OWLInverseObjectPropertiesAxiom) goal;
+                OWLObjectProperty fst =
+                        g.getFirstProperty().asOWLObjectProperty();
+                OWLObjectProperty snd =
+                        g.getSecondProperty().asOWLObjectProperty();
+                Set<Set<OWLObjectProperty>> is =
+                        reasoner.getInverseProperties(fst);
+                boolean res = false;
+                Iterator<Set<OWLObjectProperty>> it = is.iterator();
+                while (it.hasNext())
+                {
+                    Set<OWLObjectProperty> cur = it.next();
+                    if (cur.contains(snd)) {res=true;}
+                }
+                return res;
+            }
+            if (goal instanceof OWLIrreflexiveObjectPropertyAxiom)
+            {
+                OWLIrreflexiveObjectPropertyAxiom g =
+                        (OWLIrreflexiveObjectPropertyAxiom) goal;
+                return reasoner.isIrreflexive(g.getProperty().asOWLObjectProperty());
+            }
+            if (goal instanceof OWLNegativeDataPropertyAssertionAxiom)
+            {
+                OWLNegativeDataPropertyAssertionAxiom g =
+                        (OWLNegativeDataPropertyAssertionAxiom) goal;
+                OWLIndividual sub = g.getSubject();
+                OWLConstant   obj = g.getObject();
+                OWLDataProperty p = g.getProperty().asOWLDataProperty();
+                return !reasoner.hasDataPropertyRelationship(sub, p, obj);
+            }
+            if (goal instanceof OWLNegativeObjectPropertyAssertionAxiom)
+            {
+                OWLNegativeObjectPropertyAssertionAxiom g =
+                        (OWLNegativeObjectPropertyAssertionAxiom) goal;
+                OWLIndividual sub = g.getSubject();
+                OWLIndividual obj = g.getObject();
+                OWLObjectProperty p = g.getProperty().asOWLObjectProperty();
+                return !reasoner.hasObjectPropertyRelationship(sub, p, obj);
+            }
+            if (goal instanceof OWLObjectPropertyAssertionAxiom)
+            {
+                OWLObjectPropertyAssertionAxiom g =
+                        (OWLObjectPropertyAssertionAxiom) goal;
+                OWLIndividual sub = g.getSubject();
+                OWLIndividual obj = g.getObject();
+                OWLObjectProperty p = g.getProperty().asOWLObjectProperty();
+                return reasoner.hasObjectPropertyRelationship(sub, p, obj);
+            }
+            if (goal instanceof OWLObjectPropertyDomainAxiom)
+            {
+                OWLObjectPropertyDomainAxiom g =
+                        (OWLObjectPropertyDomainAxiom) goal;
+                OWLObjectProperty prop = g.getProperty().asOWLObjectProperty();
+                OWLDescription    dom  = g.getDomain();
+                Set<Set<OWLDescription>> doms = reasoner.getDomains(prop);
+                boolean res = false;
+                Iterator<Set<OWLDescription>> it = doms.iterator();
+                while (it.hasNext())
+                {
+                    Set<OWLDescription> cur = it.next();
+                    if (cur.contains(dom)){res = true;}
+                }
+                return res;
+            }
+            if (goal instanceof OWLObjectPropertyRangeAxiom)
+            {
+                OWLObjectPropertyRangeAxiom g =
+                        (OWLObjectPropertyRangeAxiom) goal;
+                OWLObjectProperty prop = g.getProperty().asOWLObjectProperty();
+                OWLDescription    ran  = g.getRange();
+                Set<OWLDescription>  rns = reasoner.getRanges(prop);
+                return rns.contains(ran);
+            }
+            if (goal instanceof OWLObjectSubPropertyAxiom)
+            {
+                OWLObjectSubPropertyAxiom g =
+                        (OWLObjectSubPropertyAxiom) goal;
+                OWLObjectProperty sub = g.getSubProperty().asOWLObjectProperty();
+                OWLObjectProperty sup = g.getSuperProperty().asOWLObjectProperty();
+                Set<Set<OWLObjectProperty>> subs = reasoner.getSubProperties(sup);
+                boolean res = false;
+                Iterator<Set<OWLObjectProperty>> it = subs.iterator();
+                while (it.hasNext())
+                {
+                    Set<OWLObjectProperty> cur = it.next();
+                    if (cur.contains(sub)) {res = true;}
+                }
+                return res;
+            }
+            if (goal instanceof OWLReflexiveObjectPropertyAxiom)
+            {
+                OWLReflexiveObjectPropertyAxiom g =
+                        (OWLReflexiveObjectPropertyAxiom) goal;
+                OWLObjectProperty prop = g.getProperty().asOWLObjectProperty();
+                return reasoner.isReflexive(prop);
+            }
+            if (goal instanceof OWLSymmetricObjectPropertyAxiom)
+            {
+                OWLSymmetricObjectPropertyAxiom g =
+                        (OWLSymmetricObjectPropertyAxiom) goal;
+                OWLObjectProperty prop = g.getProperty().asOWLObjectProperty();
+                return reasoner.isSymmetric(prop);
+            }
+            if (goal instanceof OWLTransitiveObjectPropertyAxiom)
+            {
+                OWLTransitiveObjectPropertyAxiom g =
+                        (OWLTransitiveObjectPropertyAxiom) goal;
+                OWLObjectProperty prop = g.getProperty().asOWLObjectProperty();
+                return reasoner.isTransitive(prop);
             }
             System.out.println(goal);
             throw new UnsupportedReasonerOperationException
