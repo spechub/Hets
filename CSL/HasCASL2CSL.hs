eb8b603d56ab4beb9a66d186c430a66383588de3
Dominik Dietrich <Dominik.Dietrich@dfki.de>
2010-06-04 14:02:37 +0000

    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@13518 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ CSL/HasCASL2CSL.hs
@@ -0,0 +1,39 @@
+{- |
+Module      :  $Header$
+Description :  Helper functions for Prop2CASL
+Copyright   :  (c) Dominik Luecke and Uni Bremen 2007
+License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
+
+Maintainer  :  luecke@informatik.uni-bremen.de
+Stability   :  experimental
+Portability :  portable (imports Logic.Logic)
+
+The helpers for translating comorphism from Propositional to CASL.
+
+-}
+
+module CSL.HasCASL2Reduce
+    where
+
+import qualified Data.Set as Set
+import qualified Data.Map as Map
+import qualified Common.AS_Annotation as AS_Anno
+import qualified Common.Id as Id
+import qualified Common.Result as Result
+
+-- Reduce
+import qualified CSL.AS_BASIC_Reduce as RBasic
+-- import qualified CSL.Sublogic as PSL
+import qualified CSL.Sign as RSign
+import qualified CSL.Morphism as RMor
+import qualified CSL.Symbol as PSymbol
+
+-- HasCASL
+import HasCASL.Logic_HasCASL
+import HasCASL.As
+import HasCASL.AsUtils
+import HasCASL.Le
+import HasCASL.Builtin
+import HasCASL.Sublogic as HasSub
+import HasCASL.FoldTerm as HasFold
+
