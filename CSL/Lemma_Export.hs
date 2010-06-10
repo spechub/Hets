0c7408584182029b797b87860dbfbeafb5601d4d
Dominik Dietrich <Dominik.Dietrich@dfki.de>
2010-06-10 11:21:06 +0000

    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@13571 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ CSL/Lemma_Export.hs
@@ -0,0 +1,71 @@
+{- |
+Module      :  $Header$
+Description :  Generation of Lemmata for CMDs
+Copyright   :  (c) Dominik Dietrich, DFKI Bremen, 2010
+License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
+
+Maintainer  :  Dominik.Dietrich@dfki.de
+Stability   :  provisional
+Portability :  portable
+
+Interface for Reduce CAS system.
+-}
+
+module CSL.Lemma_Export where
+
+import Common.AS_Annotation
+import Common.Id
+
+import Data.Time (midnight)
+
+import Logic.Prover
+
+import CSL.AS_BASIC_CSL
+import CSL.Parse_AS_Basic
+
+-- | generate name for generated lemma out of name of theorem
+ganame :: Named CMD -> String
+ganame cmd = "ga_" ++ (senAttr cmd)
+
+-- | generates lemma in form of a named CMD for namedcmd that resulted in CAS answer casresult
+genLemmaFactor :: Named CMD -> EXPRESSION -> Named CMD
+genLemmaFactor namedcmd casresult = makeNamed lemmaname lemma
+    where Cmd _ exps = sentence namedcmd
+          lemma = Cmd "=" [head exps, casresult]
+          lemmaname = (ganame namedcmd)
+
+-- | generates lemma in form of a named CMD for namedcmd that resulted in CAS answer casresult
+genLemmaSolve :: Named CMD -> EXPRESSION -> Named CMD
+genLemmaSolve namedcmd casresult = makeNamed lemmaname lemma
+    where Cmd _ exps = sentence namedcmd
+          lemma = Cmd "=" [head exps, casresult]
+          lemmaname = (ganame namedcmd)
+
+-- | generates lemma in form of a named CMD for namedcmd that resulted in CAS answer casresult
+genLemmaSimplify :: Named CMD -> EXPRESSION -> Named CMD
+genLemmaSimplify namedcmd casresult = makeNamed lemmaname lemma
+    where Cmd _ exps = sentence namedcmd
+          lemma = Cmd "=" [head exps, casresult]
+          lemmaname = (ganame namedcmd)
+
+-- | generates lemma in form of a named CMD for namedcmd that resulted in CAS answer casresult
+genLemmaAsk :: Named CMD -> EXPRESSION -> Named CMD
+genLemmaAsk namedcmd casresult = makeNamed lemmaname lemma
+      where Cmd _ exps = sentence namedcmd
+            lemma = Cmd "=" [head exps, casresult]
+            lemmaname = (ganame namedcmd)
+
+-- | generates lemma in form of a named CMD for namedcmd that resulted in CAS answer casresult
+genLemmaRemainder :: Named CMD -> EXPRESSION -> Named CMD
+genLemmaRemainder namedcmd casresult = makeNamed lemmaname lemma
+    where Cmd _ exps = sentence namedcmd
+          lemma = Cmd "=" [head exps, casresult]
+          lemmaname = (ganame namedcmd)
+
+-- | generates lemma in form of a named CMD for namedcmd that resulted in CAS answer casresult
+genLemmaInt :: Named CMD -> EXPRESSION -> Named CMD
+genLemmaInt namedcmd casresult = makeNamed lemmaname lemma
+    where Cmd _ exps = sentence namedcmd
+          lemma = Cmd "=" [head exps, casresult]
+          lemmaname = (ganame namedcmd)
+
