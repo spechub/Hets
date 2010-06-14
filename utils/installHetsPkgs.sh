d0e2812daf5ff26687753c71458fac7e398606e4
Christian Maeder <Christian.Maeder@dfki.de>
2010-06-14 10:45:00 +0000

    minimal hets libs and cabal-install git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@13589 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/installHetsPkgs.sh
+++ utils/installHetsPkgs.sh
@@ -7,31 +7,11 @@ for p in \
  parsec-2.1.0.1 \
  network-2.2.1.7 \
  fgl-5.4.2.2 \
- xml-1.3.5 \
- html-1.0.1.2 \
+ xml-1.3.7 \
  zlib-0.5.2.0 \
  HTTP-4000.0.9 \
  cabal-install-0.8.2 \
- haskell-src-1.0.1.3 \
- utf8-string-0.3.6 \
- curl-1.3.5 \
- tar-0.3.1.0 \
- deepseq-1.1.0.0 \
- QuickCheck-2.1.0.3 \
- HUnit-1.2.2.1 \
- tagsoup-0.8 \
- hxt-8.5.0 \
- hxt-filter-8.4.2 \
- haskeline-0.6.2.2 \
- text-0.7.1.0 \
- parallel-2.2.0.1 \
- hexpat-0.12 \
- cgi-3001.1.7.3 \
- xhtml-3000.2.0.1 \
- stm-2.1.1.2 \
- regex-base-0.93.1 \
- regex-posix-0.94.1 \
- regex-compat-0.92
+ haskell-src-1.0.1.3
 do ./installPkg.sh $p $1
 done
 else
