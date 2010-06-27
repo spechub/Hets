f39d7216a61322865a5b3424ad1343d8317170f3
Kristina Sojakova <kristinas@cmu.edu>
2010-06-27 16:09:33 +0000

    Added missing files git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@13635 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ Comorphisms/DynLogicList.hs
@@ -0,0 +1,17 @@
+{- |
+Module      :  $Header$
+Description :  Automatically modified file, includes the user-defined
+               logics in the Hets logic list. Do not change.
+Copyright   :  (c) Kristina Sojakova, DFKI Bremen 2010
+License     :  similar to LGPL, see HetCATS/LICENSE.txt or LIZENZ.txt
+
+Maintainer  :  k.sojakova@jacobs-university.de
+Stability   :  experimental
+Portability :  portable -}
+
+module Comorphisms.DynLogicList where
+
+import Logic.Logic
+
+dynLogicList :: [AnyLogic]
+dynLogicList = []
