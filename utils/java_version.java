d257603abae3b07d74a5e97f67477a6ff1431181
Dominik Luecke <luecke@informatik.uni-bremen.de>
2010-07-07 13:10:52 +0000

    more beans git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@13691 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/java_version.java
+++ utils/java_version.java
@@ -5,7 +5,7 @@
 import java.lang.management.RuntimeMXBean;
 import java.lang.management.ManagementFactory;
 import java.util.Map;
-
+import java.lang.management.OperatingSystemMXBean;
 /**
  *
  * @author luecke
@@ -17,12 +17,16 @@ public class java_version {
      */
     public static void main(String[] args) {
         RuntimeMXBean mxBean = ManagementFactory.getRuntimeMXBean();
+        OperatingSystemMXBean mxOS = ManagementFactory.getOperatingSystemMXBean();
         Map<String, String> p = mxBean.getSystemProperties();
-        System.out.println("Vendor:   " + mxBean.getVmVendor());
-        System.out.println("VM Name:  " + mxBean.getVmName() + " " + 
+        System.out.println("Vendor:     " + mxBean.getVmVendor());
+        System.out.println("VM Name:    " + mxBean.getVmName() + " " +
                 mxBean.getVmVersion());
-        System.out.println("Runtime:  " + p.get("java.runtime.name") + " " +
+        System.out.println("Runtime:    " + p.get("java.runtime.name") + " " +
                 p.get("java.runtime.version"));
+        System.out.println("OS:         " + mxOS.getName() + " " + mxOS.getArch()
+                + " " + mxOS.getVersion());
+        System.out.println("Processors: " + mxOS.getAvailableProcessors());
     }
 
 }
