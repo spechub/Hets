46c5bfc8c02a685f404896ab071302df2c268828
Christian Maeder <Christian.Maeder@dfki.de>
2010-07-09 12:06:48 +0000

    refactored classifier git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@13698 cec4b9c1-7d33-0410-9eda-942365e851bb

--- OWL/Taxonomy.hs
+++ OWL/Taxonomy.hs
@@ -16,6 +16,7 @@ module OWL.Taxonomy ( onto2Tax ) where
 import OWL.AS
 import OWL.Sign
 import OWL.Print
+import OWL.ProvePellet
 
 import Common.AS_Annotation
 import Common.Result
@@ -24,19 +25,11 @@ import Common.Taxonomy
 
 import System.IO.Unsafe
 
-import Common.Utils
-
-import Data.Time ()
-import Data.Time.Clock (UTCTime(..), getCurrentTime)
-
-import System.Exit
-import System.IO
-import System.Directory
-
 import qualified Data.Foldable as Fold
 import qualified Common.Lib.Rel as Rel
 import qualified Data.Map as Map
 import Data.List
+import Data.Maybe
 
 import qualified Data.Set as Set
 
@@ -53,13 +46,11 @@ onto2Tax gk inOnto sig sens = case gk of
     let subRel = Rel.transReduce $ foldl Rel.union Rel.empty tree
         superRel = Rel.irreflex $ Rel.transpose subRel
         superMap = Rel.toMap superRel
-        classes  = Set.toList $ Set.map fst $ Rel.toSet subRel
+        classes = Set.toList $ Set.map fst $ Rel.toSet subRel
     makeMiss inOnto superMap classes
 
 dropClutter :: String -> String
-dropClutter a = case (stripPrefix "unamed:" a) of
-  Nothing -> a
-  Just b  -> b
+dropClutter a = fromMaybe a (stripPrefix "unamed:" a)
 
 -- | Generation of a MissOntology
 makeMiss :: MMiSSOntology
@@ -67,10 +58,10 @@ makeMiss :: MMiSSOntology
          -> [String]
          -> Result MMiSSOntology
 makeMiss o r =
-  Fold.foldlM (\x y -> fromWithError $ insertClass x (dropClutter y) ""
+  Fold.foldlM (\ x y -> fromWithError $ insertClass x (dropClutter y) ""
                 (case Map.lookup y r of
                    Nothing -> []
-                   Just z  -> Set.toList $ Set.map dropClutter z
+                   Just z -> Set.toList $ Set.map dropClutter z
                 ) Nothing) o
 
 -- | Builder for all relations
@@ -85,60 +76,27 @@ relBuilder tr =
 -- | splitter for output
 splitter :: [String] -> [[String]]
 splitter ls = case ls of
-  []    -> []
-  (h:t) -> let (l,r) = span (\x -> head x == ' ') t in (h:l) : splitter r
+  [] -> []
+  (h : t) -> let (l, r) = span (\ x -> head x == ' ') t in (h : l) : splitter r
 
 -- | builder for a single relation
 relBuild :: [String]
          -> Rel.Rel String
 relBuild s = case s of
-  []     -> Rel.empty
-  (t:ts) ->
+  [] -> Rel.empty
+  (t : ts) ->
     let nt = map (drop 3) ts
         children = splitter nt
-        ch  = foldl Rel.union Rel.empty $ map relBuild children
+        ch = foldl Rel.union Rel.empty $ map relBuild children
         suc = map head children
     in Rel.insert t t $ Rel.fromList (zip (repeat t) suc) `Rel.union` ch
 
 -- | Invocation of Pellet
 runClassifier :: Sign -> [Named Axiom] -> IO (Result String)
 runClassifier sig sen = do
-  let th      = show $ printOWLBasicTheory (sig, filter isAxiom sen)
-      options = "classify "
-      tmpFile = "PelletClassifier"
-      timeLimit = 800
-  (proTh, propEx) <- check4Pellet
-  case (proTh, propEx) of
-    (True,True) -> do
-      t <- getCurrentTime
-      tempDir <- getTemporaryDirectory
-      let timeTmpFile = tempDir ++ "/" ++ tmpFile ++ show (utctDay t) ++ "-"
-                                ++ show (utctDayTime t) ++ ".owl"
-          command = "sh pellet.sh " ++ options ++ timeTmpFile
-      writeFile timeTmpFile th
-      pPath <- getEnvDef "PELLET_PATH" ""
-      setCurrentDirectory pPath
-      (mExit, outh,_) <- timeoutCommand timeLimit command
-      outState <- case mExit of
-        Just exCode -> do
-          ls <- hGetContents outh
-          case exCode of
-            ExitSuccess -> return $ return ls
-            _           -> return $ fail ls
-        Nothing -> return $ fail "Time is up!"
-      removeFile timeTmpFile
-      return outState
-    (True,False) -> return $ fail "Pellet prover" "Pellet not executable"
-    (False,_)    -> return $ fail "Pellet prover" "Pellet not found"
-
--- | Checker if pellet is installed
-check4Pellet :: IO (Bool, Bool)
-check4Pellet = do
-  pPath <- getEnvDef "PELLET_PATH" ""
-  let path = pPath ++ "/pellet.sh"
-  progTh <- doesFileExist path
-  progEx <- if progTh then do
-      progPerms <- getPermissions path
-      return $ executable progPerms
-    else return False
-  return (progTh, progEx)
+  let th = show $ printOWLBasicTheory (sig, filter isAxiom sen)
+  res <- runTimedPellet "classify" "PelletClassifier" th (Just 800)
+  return $ case res of
+    Nothing -> fail "Time is up (800 seconds)!"
+    Just (progTh, out, _) ->
+      if progTh then return out else fail "Pellet not found"
