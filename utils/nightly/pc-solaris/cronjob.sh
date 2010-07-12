c3d7b9a38449f55e0908d7c515be3cdddebde6d0
Christian Maeder <Christian.Maeder@dfki.de>
2010-07-12 15:44:10 +0000

    avoid LD_LIBRARY_PATH git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@13715 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/nightly/pc-solaris/cronjob.sh
+++ utils/nightly/pc-solaris/cronjob.sh
@@ -1,14 +1,12 @@
 #!/bin/bash -x
 
-PATH=/home/pub-bkb/bin:/usr/bin/:/usr/ccs/bin:/opt/csw/bin:/opt/csw/gcc4/bin
+PATH=/home/pub-bkb/pc-solaris/ghc/ghc-6.12.3/bin:/home/pub-bkb/bin:/usr/bin/:/usr/ccs/bin:/opt/csw/bin:/opt/csw/gcc4/bin
 UDG_HOME=/home/pub-bkb/uDrawGraph-3.1
 HETS_LIB=/local/home/maeder/haskell/Hets-lib
-LD_LIBRARY_PATH=/home/wwwuser/maeder/cgi-bin
 
 export PATH
 export UDG_HOME
 export HETS_LIB
-export LD_LIBRARY_PATH
 
 cd /local/home/maeder/haskell
 
@@ -42,7 +40,7 @@ repackDocs
 updateLibForCgi
 
 topSortCheck
-moreChecks
+#moreChecks
 checkCalculi
 checkEnvs
 checkHpfs
