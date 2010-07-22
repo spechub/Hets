2643008447e30b6025f742eb6a661f38be756b1e
Simon Ulbricht <tekknix@informatik.uni-bremen.de>
2010-07-22 14:48:09 +0000

    initial version, not working yet git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@13747 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ GUI/GtkDisprove.hs
@@ -0,0 +1,41 @@
+
+
+module GUI.GtkDisprove where
+
+import Common.LibName
+import Common.AS_Annotation
+import qualified Common.OrderedMap as OMap
+
+import Data.Graph.Inductive.Graph (LNode)
+
+import Logic.Comorphism
+import Logic.Logic
+
+import Proofs.AbstractState
+import Proofs.ConsistencyCheck
+
+import Static.DevGraph
+import Static.GTheory
+
+
+ -- TODO write error messages with content!
+ -- TODO use return value of consistencyCheck and mark node
+ -- TODO implement in GtkProverGui
+
+disproveNode :: G_cons_checker -> AnyComorphism -> String -> LNode DGNodeLab
+             -> Int -> IO ConsistencyStatus
+disproveNode cc ac selGoal (i, l) timeout =
+  case globalTheory l of
+    Nothing -> error "GtkDisprove.disproveNode(1)"
+    Just (G_theory lid a b sens c) -> 
+      let axioms = OMap.filter isAxiom sens
+          negSen = case OMap.lookup selGoal sens of
+                     Nothing -> error "GtkDisprove.disproveNode(2)"
+                     Just sen -> 
+                       case negation lid $ sentence sen of
+                         Nothing -> error "GtkDisprove.disproveNode(3)"
+                         Just sen' -> sen { sentence = sen' }
+          sens' = OMap.insert selGoal negSen axioms
+      in consistencyCheckAux False cc ac (i, l {globalTheory = 
+        Just (G_theory lid a b sens' c) }) timeout
+          
