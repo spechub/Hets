a9a5ac69d1d35b1a8f4288f7de005b16589cc0c5
Christian Maeder <Christian.Maeder@dfki.de>
2010-08-11 18:17:18 +0000

    adjusted git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@13868 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/words.pl
+++ utils/words.pl
@@ -27,7 +27,7 @@ my $DO_PDFTOTEXT = 1; # 0 = don't do it
 
 #$ENV{'TEXINPUTS'} =$ENV{'TEXINPUTS'}."::".dirname($0);
 
-my $haskell_header = 
+my $haskell_header =
 '{- |
 Module      :  $Header$
 Copyright   :  (c) Klaus Lüttich, Uni Bremen 2002-2004
@@ -38,20 +38,17 @@ Stability   :  provisional
 Portability :  portable
 
 -}
-{- 
-    HetCATS/Common/LaTeX_maps
-    $Id$
-    Author: Klaus Lüttich
-    Year: 2002
-    
-    Created by a Perl-script (HetCATS/utils/words.pl)!
+
+{-
+
+    Created by a Perl-script (utils/words.pl)!
     DO NOT MODIFY BY HAND!!
 
 -}
 
 module Common.LaTeX_maps where
 
-import Common.Lib.Map(fromList,Map)
+import Data.Map (fromList, Map)
 
 ';
 
@@ -59,9 +56,9 @@ import Common.Lib.Map(fromList,Map)
 # main #
 ########
 
-my %used_words = &process_the_words; # the words, LaTeX makros, 
-                        # syllables (ligatures),  letters widely 
-                        # used in (Het)CASL 
+my %used_words = &process_the_words; # the words, LaTeX makros,
+                        # syllables (ligatures),  letters widely
+                        # used in (Het)CASL
 # Split into sections that give the names of the Haskell maps.
 
 my @fonts = &read_fonts; # adds an "empty font" for LaTeX-default
@@ -70,28 +67,28 @@ my %widths = (); # a table of width from various fonts
 foreach my $font (@fonts) {
    $widths{$font->[1]} = &process_one_font($font);
    # debugging
-   0 && do { 
+   0 && do {
        foreach my $sec (keys %{$widths{$font->[1]}}) {
-	   print "$sec :", join(",", @{${$widths{$font->[1]}}{$sec}}),"\n";
+           print "$sec :", join(",", @{${$widths{$font->[1]}}{$sec}}),"\n";
        }
    };
 }
 
 my %word_widths = &calc_max_width(\%widths,\%used_words);
-         # a table of sections to table of 
+         # a table of sections to table of
          # words per section to max width over all fonts
 
 # debugging
-1 && do { 
+1 && do {
     foreach my $sec (keys %word_widths) {
-	print "$sec :\n",
-	join(", ", (map {"$_: ".$word_widths{$sec}{$_}; } 
-		        (sort (keys %{$word_widths{$sec}})))),
-	"\n";
+        print "$sec :\n",
+        join(", ", (map {"$_: ".$word_widths{$sec}{$_}; }
+                        (sort (keys %{$word_widths{$sec}})))),
+        "\n";
     }
  };
 
-&generate_haskell_FM(\%word_widths); # uses %word_widths 
+&generate_haskell_FM(\%word_widths); # uses %word_widths
 
 ########
 # subs #
@@ -99,63 +96,63 @@ my %word_widths = &calc_max_width(\%widths,\%used_words);
 
 sub generate_haskell_FM {
     my $word_widths = $_[0];
-    
+
 
     open HASKELL, "> LaTeX_maps.hs" or
-	die "cannot create Haskell module \"LaTeX_maps.hs\"";
+        die "cannot create Haskell module \"LaTeX_maps.hs\"";
     print HASKELL $haskell_header;
-    
+
     my @two_letter_words = ();
     foreach my $sec (keys %{$word_widths}) {
-	# generate a list of pairs for each section named after the section 
-	print HASKELL &fm_header($sec);
-	my @words = sort (keys %{$word_widths->{$sec}});
-	push @two_letter_words, (grep {length($_) == 2;} @words);
-	print HASKELL " [",
-	join(",", (map {"(\"".&escape_String($_)."\",".
-			    int($word_widths->{$sec}{$_} * 0.351 * 1000).
-			    ')'; 
-		    } 
-		     (@words))),
-	"]\n";
-	print HASKELL &key_fm_header($sec);
-	my @long_words = sort (grep {length($_) > 2;} @words);
-	my $last_word = '';
-	my @first_letters = 
-	    grep {my $ret = $_ ne $last_word;$last_word = $_;$ret; } 
-	         (sort (map {m/^(.)/o;$1} @long_words));
-	print HASKELL " [", 
-	join(",",map {my $c = &escape_String($_);
-		      "('".$c."',[".
-			  join(",",map {"\"".
-			  &escape_String($_).
-			  "\""} 
-			        (grep {m/^$c/} 
-				      @long_words)).
-			  "])"} @first_letters), "]\n";
+        # generate a list of pairs for each section named after the section
+        print HASKELL &fm_header($sec);
+        my @words = sort (keys %{$word_widths->{$sec}});
+        push @two_letter_words, (grep {length($_) == 2;} @words);
+        print HASKELL " [",
+        join(",", (map {"(\"".&escape_String($_)."\",".
+                            int($word_widths->{$sec}{$_} * 0.351 * 1000).
+                            ')';
+                    }
+                     (@words))),
+        "]\n";
+        print HASKELL &key_fm_header($sec);
+        my @long_words = sort (grep {length($_) > 2;} @words);
+        my $last_word = '';
+        my @first_letters =
+            grep {my $ret = $_ ne $last_word;$last_word = $_;$ret; }
+                 (sort (map {m/^(.)/o;$1} @long_words));
+        print HASKELL " [",
+        join(",",map {my $c = &escape_String($_);
+                      "('".$c."',[".
+                          join(",",map {"\"".
+                          &escape_String($_).
+                          "\""}
+                                (grep {m/^$c/}
+                                      @long_words)).
+                          "])"} @first_letters), "]\n";
     }
     my $last_word = '';
     print HASKELL "\nligatures :: Map String Bool\n",
     "ligatures = fromList [",
-    join(",", map { "(\"".&escape_String($_)."\",True)";} 
-	 grep {my $ret = $_ ne $last_word;$last_word = $_;$ret; } 
-	        (sort @two_letter_words)),
+    join(",", map { "(\"".&escape_String($_)."\",True)";}
+         grep {my $ret = $_ ne $last_word;$last_word = $_;$ret; }
+                (sort @two_letter_words)),
     "]\n";
 }
 
 sub escape_String {
     return join("", map {
-	if (m/^\\$/o) {
-	    "\\$_";
-	} elsif(m/^"$/o) { # "
-	    "\\$_";
-	} else {
-	    # substitute ÄÖÜßäöü with \196\214\220\223\228\246\252
-	    $_ =~ s/Ä/\\196/o; $_ =~ s/Ö/\\214/o; $_ =~ s/Ü/\\220/o;
-	    $_ =~ s/ß/\\223/o;
-	    $_ =~ s/ä/\\228/o; $_ =~ s/ö/\\246/o; $_ =~ s/ü/\\252/o;
-	    $_;
-	}
+        if (m/^\\$/o) {
+            "\\$_";
+        } elsif(m/^"$/o) { # "
+            "\\$_";
+        } else {
+            # substitute ÄÖÜßäöü with \196\214\220\223\228\246\252
+            $_ =~ s/Ä/\\196/o; $_ =~ s/Ö/\\214/o; $_ =~ s/Ü/\\220/o;
+            $_ =~ s/ß/\\223/o;
+            $_ =~ s/ä/\\228/o; $_ =~ s/ö/\\246/o; $_ =~ s/ü/\\252/o;
+            $_;
+        }
     } split(//o,$_[0]));
 }
 
@@ -177,28 +174,28 @@ sub calc_max_width {
     my %max_widths = ();
 
     foreach my $sec (keys %{$words}) {
-	my %word_max_width = ();
-	my $width_word_index = 0;
-	print "$sec: ";
-	foreach my $word (@{$words->{$sec}}) {
-	    my $max = 0;
-	    #print "$word: ";
-	    foreach my $font (@fonts) {
-		my $cur = $font_widths->{$font}->{$sec}->[$width_word_index];
-		$cur = 0 unless defined $cur;
-		$max = &max($max,$cur);
-		print "$font: ".int($cur * 0.351 * 1000)." " if $word eq "~";
-	    }
-	    print "\n" if $word eq "~";
-	    print STDERR 
-		     "\nWarning: max length of $word is undefined or zero\n"
-		if ! defined $max || $max == 0; 
-	    $word_max_width{$word} = $max;
-	    $width_word_index++;
-	    #exit if $width_word_index >= 5;
-	}
-	$max_widths{$sec} = \%word_max_width;
-	#exit;
+        my %word_max_width = ();
+        my $width_word_index = 0;
+        print "$sec: ";
+        foreach my $word (@{$words->{$sec}}) {
+            my $max = 0;
+            #print "$word: ";
+            foreach my $font (@fonts) {
+                my $cur = $font_widths->{$font}->{$sec}->[$width_word_index];
+                $cur = 0 unless defined $cur;
+                $max = &max($max,$cur);
+                print "$font: ".int($cur * 0.351 * 1000)." " if $word eq "~";
+            }
+            print "\n" if $word eq "~";
+            print STDERR
+                     "\nWarning: max length of $word is undefined or zero\n"
+                if ! defined $max || $max == 0;
+            $word_max_width{$word} = $max;
+            $width_word_index++;
+            #exit if $width_word_index >= 5;
+        }
+        $max_widths{$sec} = \%word_max_width;
+        #exit;
     }
     return %max_widths;
 }
@@ -211,20 +208,20 @@ sub process_one_font {
     # debugging: print "font: ".join(", ", @{$_[0]})."\n";
     my ($font_cmnd,$font_name) = @{$_[0]};
     # generate two documents one human readable and one for the machine
-    my $computer_tex_filename = 
-	# &gen_tex('width-table.tex.svmono.templ',
-	&gen_tex('width-table.tex.templ',
-		 $font_name,'computer',$font_cmnd);
-    my $human_tex_filename = 
-	# &gen_tex('width-table.tex.svmono.templ',
-	&gen_tex('width-table.tex.templ',
-		 $font_name,'human',$font_cmnd);
+    my $computer_tex_filename =
+        # &gen_tex('width-table.tex.svmono.templ',
+        &gen_tex('width-table.tex.templ',
+                 $font_name,'computer',$font_cmnd);
+    my $human_tex_filename =
+        # &gen_tex('width-table.tex.svmono.templ',
+        &gen_tex('width-table.tex.templ',
+                 $font_name,'human',$font_cmnd);
     if ($DO_PDFLATEX) {
-	&pdflatex($computer_tex_filename);
-	&pdflatex($human_tex_filename);
+        &pdflatex($computer_tex_filename);
+        &pdflatex($human_tex_filename);
     }
-    my $computer_pdf_filename = 
-	basename($computer_tex_filename,'.tex').'.pdf';
+    my $computer_pdf_filename =
+        basename($computer_tex_filename,'.tex').'.pdf';
     return &get_widths($computer_pdf_filename); # seperated in sections
 }
 
@@ -238,51 +235,51 @@ sub get_widths {
     my $txt_filename = basename($pdf_filename,'.pdf').'.txt';
     my %widths = ();
     if ($DO_PDFTOTEXT) {
-	system($PDFTOTEXT_BIN,"-raw",$pdf_filename);
+        system($PDFTOTEXT_BIN,"-raw",$pdf_filename);
     }
     #open WIDTH, "pdftotext $pdf_filename | egrep 'section: |wl: ' |"
-#	or die "cannot call pdftotext or egrep or cannot fork";
-    open WIDTH, "< $txt_filename" 
-	or die "cannot read file \"$txt_filename\"";
+#       or die "cannot call pdftotext or egrep or cannot fork";
+    open WIDTH, "< $txt_filename"
+        or die "cannot read file \"$txt_filename\"";
     my $section = '';
     my @widths = ();
     while (<WIDTH>) {
-	m/section: (\w+)\+\+\+/o && do {
-	    unless ($section eq '') {
-		$widths{$section} = [@widths]; 
-		@widths = ();
-	    }
-	    $section = $1;
-	};
-	m/wl: (\d+\.\d+)pt/o && $section ne '' && do {
-	    push @widths, $1;
-	};
+        m/section: (\w+)\+\+\+/o && do {
+            unless ($section eq '') {
+                $widths{$section} = [@widths];
+                @widths = ();
+            }
+            $section = $1;
+        };
+        m/wl: (\d+\.\d+)pt/o && $section ne '' && do {
+            push @widths, $1;
+        };
     }
     close WIDTH;
     $widths{$section} = [@widths];
     # debugging
-    0 && do { 
-	foreach my $sec (keys %widths) {
-	    print "$sec :", join(",", @{$widths{$sec}}),"\n";
-	}
+    0 && do {
+        foreach my $sec (keys %widths) {
+            print "$sec :", join(",", @{$widths{$sec}}),"\n";
+        }
    };
     return \%widths;
 }
 
 sub gen_tex {
     my ($input_filename,$font_name,$purpose,$font_cmnd) = @_;
-    my $output_filename = 
-	basename($input_filename,'.tex.templ').".$font_name.".
-	substr($purpose,0,1).".tex";
+    my $output_filename =
+        basename($input_filename,'.tex.templ').".$font_name.".
+        substr($purpose,0,1).".tex";
     my $no_cols = '';
     open TEMPL, "< $input_filename";
     open OUT,   "> $output_filename";
     $purpose eq 'human' && do {$no_cols = '';};
     $purpose eq 'computer' && do {$no_cols = '% ';};
     while (<TEMPL>) {
-	s/<set-font>/$font_cmnd/;
-	s/<no-columns>/$no_cols/;
-	print OUT $_;
+        s/<set-font>/$font_cmnd/;
+        s/<no-columns>/$no_cols/;
+        print OUT $_;
     }
     close TEMPL;
     close OUT;
@@ -290,13 +287,13 @@ sub gen_tex {
 }
 
 sub read_fonts {
-    open FONTS, "< fonts.input" 
-	or die "cannot open file \"fonts.input\" for reading"; 
+    open FONTS, "< fonts.input"
+        or die "cannot open file \"fonts.input\" for reading";
     my @read_fonts = ();
     while (<FONTS>) {
-	chomp;
-	my @fnt_descrp = split /\s+:::\s+/;
-	push @read_fonts, \@fnt_descrp;
+        chomp;
+        my @fnt_descrp = split /\s+:::\s+/;
+        push @read_fonts, \@fnt_descrp;
     }
     close FONTS;
     unshift @read_fonts, [('','default')];
@@ -312,44 +309,44 @@ sub process_the_words {
     my $section = '';
 
     open WORDS, "< words.input" or die("no file named \"words.input\" found");
-    open GENWORDS, "> generated_words.tex" 
-	or die "cannot write to file \"generated_words.tex\"!";
+    open GENWORDS, "> generated_words.tex"
+        or die "cannot write to file \"generated_words.tex\"!";
     while ($line = <WORDS>) {
-	&sep_tabular,next if $line=~ m/^\s*$/o;
-	if ($line =~ m/^%/o) {
-	    chomp $line;
-	    if ($line =~ m/^\%pattern:\s*/o) {
-		$line =~ s///o;
-		$pat = $line;
-	    } elsif ($line =~ m/^%section:\s*/o) {
-		#print STDERR "$section : ",join(", ",@sec_words),"\n";
-		unless ($section eq '') {
-		    $all_words{$section} = [@sec_words];
-		    #print STDERR "pushed: $section : ",join(", ",@sec_words),"\n";
-		} 
-		@sec_words = ();
-		$line =~ s///o;
-		$section = $line;
-		&sep_tabular("\\newpage\n\\section*{section: $section+++}\n");
-	    } else {
-		print STDERR "unknown directive: $line\n";
-	    }
-	} elsif ($line =~ m/^&/o) {
-	    $line =~ s///o;
-	    print GENWORDS $line;
-	} elsif ($line =~ m/^~(.*)$/o) {
-	    &sep_tabular($1);
-	} else {
-	    my @words = split(/\s+/o, $line);
-	    push @sec_words, @words;
-	    foreach my $word (@words) {
-		my $fpat= sprintf($pat,$word);
-		print GENWORDS "\\wordline{$fpat}\n\\hline\n";
-		if(++$count_words >= 37) {
-		    &sep_tabular;
-		} 	
-	    }
-	}
+        &sep_tabular,next if $line=~ m/^\s*$/o;
+        if ($line =~ m/^%/o) {
+            chomp $line;
+            if ($line =~ m/^\%pattern:\s*/o) {
+                $line =~ s///o;
+                $pat = $line;
+            } elsif ($line =~ m/^%section:\s*/o) {
+                #print STDERR "$section : ",join(", ",@sec_words),"\n";
+                unless ($section eq '') {
+                    $all_words{$section} = [@sec_words];
+                    #print STDERR "pushed: $section : ",join(", ",@sec_words),"\n";
+                }
+                @sec_words = ();
+                $line =~ s///o;
+                $section = $line;
+                &sep_tabular("\\newpage\n\\section*{section: $section+++}\n");
+            } else {
+                print STDERR "unknown directive: $line\n";
+            }
+        } elsif ($line =~ m/^&/o) {
+            $line =~ s///o;
+            print GENWORDS $line;
+        } elsif ($line =~ m/^~(.*)$/o) {
+            &sep_tabular($1);
+        } else {
+            my @words = split(/\s+/o, $line);
+            push @sec_words, @words;
+            foreach my $word (@words) {
+                my $fpat= sprintf($pat,$word);
+                print GENWORDS "\\wordline{$fpat}\n\\hline\n";
+                if(++$count_words >= 37) {
+                    &sep_tabular;
+                }
+            }
+        }
     }
     close WORDS;
     close GENWORDS;
