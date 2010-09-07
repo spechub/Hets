33e57f95ad297df25f3873a8e1446acde262ce1b
Christian Maeder <Christian.Maeder@dfki.de>
2010-09-07 14:24:11 +0000

    added sentence analysis git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14013 cec4b9c1-7d33-0410-9eda-942365e851bb

--- GUI/GtkAddSentence.hs
+++ GUI/GtkAddSentence.hs
@@ -21,9 +21,25 @@ import qualified GUI.Glade.TextField as TextField
 import GUI.GraphTypes
 
 import Static.DevGraph
+import Static.GTheory
+
+import Logic.Logic
+import Logic.Prover
+
+import Common.AnnoState (emptyAnnos)
+import Common.Doc
+import Common.ExtSign
+import Common.GlobalAnnotations
+import Common.Parsec
+import Common.Result
+import Common.Utils
+
+import Control.Monad
+
+import Text.ParserCombinators.Parsec
 
 gtkAddSentence :: GInfo -> Int -> DGraph -> IO ()
-gtkAddSentence _ n dg = postGUIAsync $ do
+gtkAddSentence gi n dg = postGUIAsync $ do
   xml <- getGladeXML TextField.get
   -- get objects
   window <- xmlGetWidget xml castToWindow "TextField"
@@ -36,5 +52,39 @@ gtkAddSentence _ n dg = postGUIAsync $ do
   onClicked btnAbort $ widgetDestroy window
   onClicked btnAdd $ do
     sen <- entryGetText entry
-    putStrLn sen
+    abort <- anaSentence gi n dg lbl sen
+    when abort $ widgetDestroy window
   widgetShow window
+
+errorFeedback :: Bool -> String -> IO Bool
+errorFeedback abort msg =
+   errorDialogExt "Error" msg >> return abort
+
+anaSentence :: GInfo -> Int -> DGraph -> DGNodeLab -> String -> IO Bool
+anaSentence gi n dg lbl sen = case dgn_theory lbl of
+  G_theory lid sign si sens _ -> case parse_basic_spec lid of
+    Nothing -> errorFeedback True "missing basic spec parser"
+    Just p -> case basic_analysis lid of
+      Nothing -> errorFeedback True "missing basic analysis"
+      Just f -> case runParser (p << eof) (emptyAnnos ()) "" $ trimLeft sen of
+        Left err -> errorFeedback False $ show err
+        Right bs -> let
+          ps = plainSign sign
+          Result ds res = f (bs, ps, emptyGlobalAnnos)
+          in case res of
+            Nothing -> errorFeedback False $ showRelDiags 1 ds
+            Just (_, ExtSign sig2 _, sens2) ->
+              if is_subsig lid sig2 ps then case sens2 of
+                [ns] -> do
+                  print $ print_named lid ns
+                  addSentence gi n dg lbl
+                    $ G_theory lid sign si (joinSens (toThSens sens2) sens)
+                      startThId
+                  return True
+                [] -> errorFeedback False "no sentence recognized"
+                _ -> errorFeedback False $ "multiple sentences recognized\n"
+                     ++ show (vcat $ map (print_named lid) sens2)
+              else errorFeedback False "signature must not change"
+
+addSentence :: GInfo -> Int -> DGraph -> DGNodeLab -> G_theory -> IO ()
+addSentence _gi _n _dg _lbl _th = return ()
