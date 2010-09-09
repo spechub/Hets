47579588f51798de5c5f9150d371644a45acadf6
Christian Maeder <Christian.Maeder@dfki.de>
2010-09-09 16:44:13 +0000

    ignore some updates git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14041 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Static/ApplyChanges.hs
+++ Static/ApplyChanges.hs
@@ -25,6 +25,7 @@ lookupNodeByNodeName nn = lookupNodeWith ((== nn) . dgn_name . snd)
 applyChange :: Monad m => SelChangeDG -> DGraph -> m DGraph
 applyChange (SelChangeDG se ch) dg = case ch of
   RemoveChDG -> remove se dg
+  UpdateChDG str -> update str se dg
   _ -> fail "applyChange.unimplemented ChangeDG"
 
 remove :: Monad m => SelElem -> DGraph -> m DGraph
@@ -32,7 +33,7 @@ remove se dg = let err = "Static.ApplyChanges.remove: " in case se of
   NodeElem nn m -> case m of
     Nothing -> let s = showName nn in case lookupNodeByNodeName nn dg of
       [i] -> return $ changeDGH dg (DeleteNode i)
-      [] -> fail $ err ++ "node not found: " ++ s
+      [] -> return dg -- assume node is gone
       _ -> fail $ err ++ "ambiguous node: " ++ s
     Just _ -> fail $ err ++ "cannot remove node symbols"
   LinkElem eId src tar m -> let e = showEdgeId eId in case m of
@@ -43,6 +44,20 @@ remove se dg = let err = "Static.ApplyChanges.remove: " in case se of
         [le] -> return $ changeDGH dg (DeleteEdge le)
         [] -> fail $ err ++ "edge not found: " ++ e
         _ -> fail $ err ++ "ambiguous edge: " ++ e
-      _ -> fail $ err ++ "non-unique source or target for edge: " ++ e
+      (sl, tl) -> if null sl || null tl then return dg -- assume link is gone
+        else fail $ err ++ "non-unique source or target for edge: " ++ e
+          ++ "\n" ++ show (map (\ (v, l) -> (v, getDGNodeName l)) $ sl ++ tl)
     Just _ -> fail $ err ++ "cannot remove edge morphisms"
+  _ -> fail $ err ++ "unimplemented selection"
+
+update :: Monad m => String -> SelElem -> DGraph -> m DGraph
+update str se dg =
+  let err = "Static.ApplyChanges.update with: " ++ str ++ "\n"
+  in case se of
+  NodeElem nn m -> case m of
+    Nothing -> let s = showName nn in case lookupNodeByNodeName nn dg of
+      [_] -> return dg -- ignore any attribute changes
+      [] -> fail $ err ++ "node not found: " ++ s
+      _ -> fail $ err ++ "ambiguous node: " ++ s
+    Just _ -> fail $ err ++ "cannot remove node symbols"
   _ -> fail $ err ++ "unimplemented selection"
