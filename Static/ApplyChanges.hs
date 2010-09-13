972c45f3b0b7522f7d22ed680d743d0d4a629f84
Christian Maeder <Christian.Maeder@dfki.de>
2010-09-13 14:54:52 +0000

    extended git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14061 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Static/ApplyChanges.hs
+++ Static/ApplyChanges.hs
@@ -17,7 +17,10 @@ import Static.History
 
 import Data.Graph.Inductive.Graph as Graph
 
+import Control.Monad
+
 import Static.FromXml
+import Debug.Trace
 
 lookupNodeByNodeName :: NodeName -> DGraph -> [LNode DGNodeLab]
 lookupNodeByNodeName nn = lookupNodeWith ((== nn) . dgn_name . snd)
@@ -26,16 +29,47 @@ applyChange :: Monad m => SelChangeDG -> DGraph -> m DGraph
 applyChange (SelChangeDG se ch) dg = case ch of
   RemoveChDG -> remove se dg
   UpdateChDG str -> update str se dg
-  _ -> fail "applyChange.unimplemented ChangeDG"
+  AddChDG _ ls  ->
+    foldM (add se) dg ls
+
+
+add :: Monad m => SelElem -> DGraph -> AddChangeDG -> m DGraph
+add se dg ch = case ch of
+    SpecEntryDG n _fm ->
+      trace ("ignore adding spec entry " ++ show n ++ " to:\n" ++ show se)
+      $ return dg
+    ViewEntryDG n _fm s t gm ->
+      trace ("ignore adding view entry " ++ show n ++ " from " ++ show s
+             ++ " to " ++ show t ++ "\n" ++ gm
+             ++ "via:\n" ++ show se)
+      $ return dg
+    NodeDG n _refn _consStatus ds ->
+      trace ("ignore adding node " ++ showName n
+             ++ " (via:\n" ++ show se ++ ") " ++ show ds)
+      $ return dg
+    LinkDG e s t _linkTy gm ->
+      trace ("ignore adding link " ++ showEdgeId e ++ " from "
+             ++ showName s ++ " to " ++ showName t
+             ++ "\n" ++ gm
+             ++ "\nvia: " ++ show se)
+      $ return dg
+    _ ->
+      trace ("ignore adding: " ++ show ch
+             ++ "\nat: " ++ show se)
+      $ return dg
 
 remove :: Monad m => SelElem -> DGraph -> m DGraph
 remove se dg = let err = "Static.ApplyChanges.remove: " in case se of
-  NodeElem nn m -> case m of
-    Nothing -> let s = showName nn in case lookupNodeByNodeName nn dg of
+  NodeElem nn m -> let s = showName nn in case m of
+    Nothing -> case lookupNodeByNodeName nn dg of
       [i] -> return $ changeDGH dg (DeleteNode i)
-      [] -> return dg -- assume node is gone
+      [] -> trace ("cannot remove: " ++ s) $ return dg -- assume node is gone
       _ -> fail $ err ++ "ambiguous node: " ++ s
-    Just _ -> fail $ err ++ "cannot remove node symbols"
+    Just nse -> case nse of
+       DeclSymbol -> fail $ err ++ "cannot remove node symbols of: " ++ s
+       SymbolRangeAttr i ->
+          fail $ err ++ "cannot remove symbol range attribute "
+            ++ show i ++ " of: " ++ s
   LinkElem eId src tar m -> let e = showEdgeId eId in case m of
     Nothing ->
       case (lookupNodeByNodeName src dg, lookupNodeByNodeName tar dg) of
@@ -44,7 +78,11 @@ remove se dg = let err = "Static.ApplyChanges.remove: " in case se of
         [le] -> return $ changeDGH dg (DeleteEdge le)
         [] -> fail $ err ++ "edge not found: " ++ e
         _ -> fail $ err ++ "ambiguous edge: " ++ e
-      (sl, tl) -> if null sl || null tl then return dg -- assume link is gone
+      (sl, tl) -> if null sl || null tl
+        then trace ("cannot remove link " ++ showEdgeId eId
+                    ++ " from " ++ showName src
+                    ++ " to " ++ showName tar)
+             $ return dg -- assume link is gone
         else fail $ err ++ "non-unique source or target for edge: " ++ e
           ++ "\n" ++ show (map (\ (v, l) -> (v, getDGNodeName l)) $ sl ++ tl)
     Just _ -> fail $ err ++ "cannot remove edge morphisms"
@@ -56,8 +94,9 @@ update str se dg =
   in case se of
   NodeElem nn m -> case m of
     Nothing -> let s = showName nn in case lookupNodeByNodeName nn dg of
-      [_] -> return dg -- ignore any attribute changes
+      [_] -> trace ("update '" ++ str ++ "' is ignored on: " ++ show se)
+        $ return dg -- ignore any attribute changes
       [] -> fail $ err ++ "node not found: " ++ s
       _ -> fail $ err ++ "ambiguous node: " ++ s
-    Just _ -> fail $ err ++ "cannot remove node symbols"
-  _ -> fail $ err ++ "unimplemented selection"
+    Just nse -> fail $ err ++ "cannot remove node symbols\n" ++ show nse
+  _ -> fail $ err ++ "unimplemented selection:\n" ++ show se
