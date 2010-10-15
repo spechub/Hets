c8ac4ac845e6936f0b1574a7f1b1c2b21340e4a4
Christian Maeder <Christian.Maeder@dfki.de>
2010-10-15 11:49:14 +0000

    cleaned up git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14151 cec4b9c1-7d33-0410-9eda-942365e851bb

--- RelationalScheme/Sign.hs
+++ RelationalScheme/Sign.hs
@@ -12,23 +12,22 @@ Signature for Relational Schemes
 -}
 
 module RelationalScheme.Sign
-        (
-            RSIsKey
-        ,   RSDatatype(..)
-        ,   RSRawSymbol
-        ,   RSColumn(..)
-        ,   RSTable(..)
-        ,   RSTables(..)
-        ,   Sign
-        ,   RSMorphism(..)
-        ,   RSTMap(..)
-        ,   emptyRSSign
-        ,   isRSSubsig
-        ,   idMor
-        ,   rsInclusion
-        ,   uniteSig
-        ,   comp_rst_mor
-        ,   RSSymbol(..)
+        ( RSIsKey
+        , RSDatatype (..)
+        , RSRawSymbol
+        , RSColumn (..)
+        , RSTable (..)
+        , RSTables (..)
+        , Sign
+        , RSMorphism (..)
+        , RSTMap (..)
+        , emptyRSSign
+        , isRSSubsig
+        , idMor
+        , rsInclusion
+        , uniteSig
+        , comp_rst_mor
+        , RSSymbol (..)
         )
         where
 
@@ -46,10 +45,11 @@ import qualified Data.Set as Set
 
 type RSIsKey = Bool
 
-data RSDatatype = RSboolean | RSbinary | RSdate | RSdatetime | RSdecimal | RSfloat |
-                  RSinteger | RSstring | RStext | RStime | RStimestamp | RSdouble |
-                  RSnonPosInteger | RSnonNegInteger | RSlong | RSPointer
-                  deriving (Eq, Ord)
+data RSDatatype
+  = RSboolean | RSbinary | RSdate | RSdatetime | RSdecimal | RSfloat
+  | RSinteger | RSstring | RStext | RStime | RStimestamp | RSdouble
+  | RSnonPosInteger | RSnonNegInteger | RSlong | RSPointer
+    deriving (Eq, Ord)
 
 type RSRawSymbol = Id
 
@@ -59,26 +59,24 @@ data RSSymbol = STable Id |     -- id of a table
                     Id          -- id of the table
                     RSDatatype  -- datatype of the symbol
                     RSIsKey     -- is it a key?
-                deriving (Eq,Ord,Show)
+                deriving (Eq, Ord, Show)
 
 instance GetRange RSSymbol
 
 data RSColumn = RSColumn
-                    {
-                        c_name :: Id
-                    ,   c_data :: RSDatatype
-                    ,   c_key  :: RSIsKey
+                    { c_name :: Id
+                    , c_data :: RSDatatype
+                    , c_key :: RSIsKey
                     }
                 deriving (Eq, Ord, Show)
 
 data RSTable = RSTable
-                {
-                    t_name  :: Id
-                ,   columns :: [RSColumn]
-                ,   rsannos :: [Annotation]
-                ,   t_keys  :: Set.Set (Id, RSDatatype)
+                { t_name :: Id
+                , columns :: [RSColumn]
+                , rsannos :: [Annotation]
+                , t_keys :: Set.Set (Id, RSDatatype)
                 }
-                deriving (Eq, Show)
+                deriving Show
 
 data RSTables = RSTables
                     {
@@ -88,38 +86,29 @@ data RSTables = RSTables
 
 instance GetRange RSTables
 
-isRSSubsig  :: RSTables -> RSTables -> Bool
+isRSSubsig :: RSTables -> RSTables -> Bool
 isRSSubsig t1 t2 = t1 <= t2
 
 uniteSig :: (Monad m) => RSTables -> RSTables -> m RSTables
 uniteSig s1 s2 =
-    if s1 `isRSSubsig` s2
-    then
-       return $ RSTables $ (tables s1) `Set.union` (tables s2)
-    else
-        if s2 `isRSSubsig` s1
-        then
-             return $ RSTables $ (tables s1) `Set.union` (tables s2)
-        else
-            if s1 `isDisjoint` s2 then
-                 return $ RSTables $ (tables s1) `Set.union` (tables s2)
-            else
-                fail ("Tables " ++ (show s1) ++ " and " ++ (show s2) ++
-                    " cannot be united.")
+    if s1 `isRSSubsig` s2 || s2 `isRSSubsig` s1 || s1 `isDisjoint` s2
+    then return $ RSTables $ tables s1 `Set.union` tables s2
+    else fail $ "Tables " ++ showDoc s1 "\nand "
+             ++ showDoc s2 "\ncannot be united."
 
 type Sign = RSTables
 
 data RSTMap = RSTMap
                 {
-                   col_map  :: Map.Map Id Id
+                   col_map :: Map.Map Id Id
                 }
                 deriving (Eq, Ord, Show)
 
 data RSMorphism = RSMorphism
-                    {   domain     :: RSTables
-                    ,   codomain   :: RSTables
-                    ,   table_map  :: Map.Map Id Id
-                    ,   column_map :: Map.Map Id RSTMap
+                    { domain :: RSTables
+                    , codomain :: RSTables
+                    , table_map :: Map.Map Id Id
+                    , column_map :: Map.Map Id RSTMap
                     }
                     deriving (Eq, Ord, Show)
 
@@ -147,52 +136,52 @@ comp_rst_mor mor1 mor2 =
             t_set = Map.fromList $ map (\ t -> (t_name t, ())) t1
             t_map = composeMap t_set (table_map mor1) (table_map mor2)
             cm_map = Map.fromList
-              $ map (\x -> apply_comp_c_map x t_map mor1 mor2) t1
+              $ map (\ x -> apply_comp_c_map x t_map mor1 mor2) t1
         in return RSMorphism
-                {   domain     = d1
-                ,   codomain   = codomain mor2
-                ,   table_map  = t_map
-                ,   column_map = cm_map
+                { domain = d1
+                , codomain = codomain mor2
+                , table_map = t_map
+                , column_map = cm_map
                 }
 
 emptyRSSign :: RSTables
-emptyRSSign =  RSTables
+emptyRSSign = RSTables
                 {
-                    tables  = Set.empty
+                    tables = Set.empty
                 }
 
 -- ^ id-morphism for RS
 idMor :: RSTables -> RSMorphism
 idMor t = RSMorphism
-            {
-                domain   = t
-            ,   codomain = t
-            ,   table_map = foldl (\y x -> Map.insert (t_name x) (t_name x) y) Map.empty $
-                                    Set.toList $ tables t
-            ,   column_map =
+            { domain = t
+            , codomain = t
+            , table_map = foldl (\ y x -> Map.insert (t_name x) (t_name x) y)
+                          Map.empty $ Set.toList $ tables t
+            , column_map =
                     let
                         makeRSTMap i =
-                           foldl (\y x -> Map.insert (c_name x) (c_name x) y) Map.empty $
-                                    columns i
+                           foldl (\ y x -> Map.insert (c_name x) (c_name x) y)
+                                 Map.empty $ columns i
                     in
-                        foldl (\y x -> Map.insert (t_name x) (RSTMap $ makeRSTMap x) y)
+                        foldl (\ y x -> Map.insert (t_name x)
+                                        (RSTMap $ makeRSTMap x) y)
                                 Map.empty $ Set.toList $ tables t
             }
 
 rsInclusion :: RSTables -> RSTables -> Result RSMorphism
-rsInclusion t1 t2 = return $ RSMorphism
-            {
-                domain   = t1
-            ,   codomain = t2
-            ,   table_map = foldl (\y x -> Map.insert (t_name x) (t_name x) y) Map.empty $
-                                    Set.toList $ tables t1
-            ,   column_map =
+rsInclusion t1 t2 = return RSMorphism
+            { domain = t1
+            , codomain = t2
+            , table_map = foldl (\ y x -> Map.insert (t_name x) (t_name x) y)
+                          Map.empty $ Set.toList $ tables t1
+            , column_map =
                     let
                         makeRSTMap i =
-                           foldl (\y x -> Map.insert (c_name x) (c_name x) y) Map.empty $
-                                    columns i
+                           foldl (\ y x -> Map.insert (c_name x) (c_name x) y)
+                                 Map.empty $ columns i
                     in
-                        foldl (\y x -> Map.insert (t_name x) (RSTMap $ makeRSTMap x) y)
+                        foldl (\ y x -> Map.insert (t_name x)
+                                        (RSTMap $ makeRSTMap x) y)
                                 Map.empty $ Set.toList $ tables t1
             }
 
@@ -200,7 +189,7 @@ rsInclusion t1 t2 = return $ RSMorphism
 
 instance Pretty RSColumn where
     pretty c = (if c_key c then keyword rsKey else empty) <+>
-       pretty (c_name c) <+> colon <+> keyword (show $ c_data c)
+       pretty (c_name c) <+> colon <+> pretty (c_data c)
 
 instance Pretty RSTable where
     pretty t = pretty (t_name t) <> parens (ppWithCommas $ columns t)
@@ -209,60 +198,55 @@ instance Pretty RSTable where
 instance Pretty RSTables where
     pretty t = keyword rsTables $+$ vcat (map pretty $ Set.toList $ tables t)
 
+instance Pretty RSTMap where
+    pretty = pretty . col_map
+
 instance Pretty RSMorphism where
-    pretty = text . show
+    pretty m = pretty (domain m) $+$ mapsto <+> pretty (codomain m)
+                $+$ pretty (table_map m) $+$ pretty (column_map m)
 
 instance Pretty RSSymbol where
-    pretty = text . show
+    pretty s = case s of
+      STable i -> pretty i
+      SColumn i _ t k -> pretty $ RSColumn i t k
 
 instance Show RSDatatype where
     show dt = case dt of
-        RSboolean       -> rsBool
-        RSbinary        -> rsBin
-        RSdate          -> rsDate
-        RSdatetime      -> rsDatetime
-        RSdecimal       -> rsDecimal
-        RSfloat         -> rsFloat
-        RSinteger       -> rsInteger
-        RSstring        -> rsString
-        RStext          -> rsText
-        RStime          -> rsTime
-        RStimestamp     -> rsTimestamp
-        RSdouble        -> rsDouble
+        RSboolean -> rsBool
+        RSbinary -> rsBin
+        RSdate -> rsDate
+        RSdatetime -> rsDatetime
+        RSdecimal -> rsDecimal
+        RSfloat -> rsFloat
+        RSinteger -> rsInteger
+        RSstring -> rsString
+        RStext -> rsText
+        RStime -> rsTime
+        RStimestamp -> rsTimestamp
+        RSdouble -> rsDouble
         RSnonPosInteger -> rsNonPosInteger
         RSnonNegInteger -> rsNonNegInteger
-        RSlong          -> rsLong
-        RSPointer       -> rsPointer
+        RSlong -> rsLong
+        RSPointer -> rsPointer
 
--- we need an explicit instance declaration of Ord that
--- correctly deals with tables
-isSubtable :: RSTable -> RSTable -> Bool
-isSubtable t1 t2 =
-    let
-        sc1 = Set.fromList $ columns t1
-        sc2 = Set.fromList $ columns t2
-    in
-        t_name t1 == t_name t2 && sc1 `Set.isSubsetOf` sc2
+instance Pretty RSDatatype where
+  pretty = keyword . show
 
-isProperSubtable :: RSTable -> RSTable -> Bool
-isProperSubtable t1 t2 =
-    let
-        sc1 = Set.fromList $ columns t1
-        sc2 = Set.fromList $ columns t2
-    in
-        t_name t1 == t_name t2 && sc1 `Set.isProperSubsetOf` sc2
+{- we need an explicit instance declaration of Eq and Ord that
+correctly deals with tables -}
+instance Ord RSTable where
+  compare t1 t2 =
+    compare (t_name t1, Set.fromList $ columns t1)
+            (t_name t2, Set.fromList $ columns t2)
 
-isDisjoint ::RSTables -> RSTables -> Bool
+instance Eq RSTable where
+  a == b = compare a b == EQ
+
+isDisjoint :: RSTables -> RSTables -> Bool
 isDisjoint s1 s2 =
     let
-        t1 = Set.map t_name $ (tables) s1
-        t2 = Set.map t_name $ (tables) s2
+        t1 = Set.map t_name $ tables s1
+        t2 = Set.map t_name $ tables s2
     in
-        Set.fold (\x y -> y && (x `Set.notMember` t2)) True t1 &&
-        Set.fold (\x y -> y && (x `Set.notMember` t1)) True t2
-
-instance Ord RSTable where
-    x <= y = x `isSubtable` y
-    x <  y = x `isProperSubtable` y
-    x >= y = y `isProperSubtable` x
-    x >  y = y `isSubtable` x
+        Set.fold (\ x y -> y && (x `Set.notMember` t2)) True t1 &&
+        Set.fold (\ x y -> y && (x `Set.notMember` t1)) True t2
