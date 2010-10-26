6d6600a65aef923df6766fbfb4eef2fc3b1670c9
Jonathan von Schroeder <sternkinder@gmail.com>
2010-10-26 11:34:32 +0000

    New Sign for Hol Light git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14161 cec4b9c1-7d33-0410-9eda-942365e851bb

--- HolLight/Sign.hs
+++ HolLight/Sign.hs
@@ -1,52 +1,12 @@
-{- |
-Module      :  $Header$
-Description :  Signature for HolLight logic
-Copyright   :  (c) Jonathan von Schroeder, DFKI GmbH 2010
-License     :  GPLv2 or higher, see LICENSE.txt
+module HolLight.Sign where
 
-Maintainer  :  jonathan.von_schroeder@dfki.de
-Stability   :  experimental
-Portability :  portable
+import qualified Data.Map as Map
 
-Definition of signatures for HolLight logic
+data HolType = TyVar String | TyApp String [HolType]
 
-  Ref.
+data HolKind = TyAbstractApp [HolKind] | Kind
 
-  <http://www.cl.cam.ac.uk/~jrh13/hol-light/>
 
--}
-
-module HolLight.Sign
-    (Sign (..)                     -- HolLight Signatures
-    ,pretty                        -- pretty printing
-    ,emptySig                      -- empty signature
-    ,isSubSig                    -- is subsignature?
-    ,sigUnion
-    ) where
-
-import qualified Data.Set as Set
-import Common.DocUtils
-import Common.Doc
-
--- | Datatype for HolLight Signatures
---
-newtype Sign = Sign {items :: Set.Set String} deriving (Eq, Ord, Show)
-
-instance Pretty Sign where
-    pretty _ = empty
-
--- | determines whether a signature is vaild
--- all sets are ok, so glued to true
-isLegalSignature :: Sign -> Bool
-isLegalSignature _ = True
-
--- | The empty signature
-emptySig :: Sign
-emptySig = Sign {items = Set.empty}
-
--- | Determines if sig1 is subsignature of sig2
-isSubSig :: Sign -> Sign -> Bool
-isSubSig sig1 sig2 = Set.isSubsetOf (items sig1) $ items sig2
-
-sigUnion :: Sign -> Sign -> Sign
-sigUnion s1 s2 = Sign (Set.union (items s1) (items s2))
+data Sign = Sign {
+               types :: [HolType],
+               ops :: Map.Map String HolType } 
