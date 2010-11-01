dccf0c84f0cddf5086e1a383505c7331a7e56dde
Christian Maeder <Christian.Maeder@dfki.de>
2010-11-01 15:54:49 +0000

    set required packages for ghc-7 git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14178 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/installHetsPkgs.sh
+++ utils/installHetsPkgs.sh
@@ -8,15 +8,13 @@ pre=`ghc --print-libdir | sed -e 's+/lib/.*++g'`
 fi
 
 for p in \
- mtl-1.1.1.0 \
+ transformers-0.2.2.0 \
+ mtl-2.0.0.0 \
  parsec-2.1.0.1 \
- network-2.2.1.8 \
+ network-2.2.1.10 \
  fgl-5.4.2.3 \
  xml-1.3.7 \
  zlib-0.5.2.0 \
- HTTP-4000.0.9 \
- cabal-install-0.8.2 \
- tar-0.3.1.0 \
- haskell-src-1.0.1.3
+ HTTP-4000.0.10
 do ./installPkg.sh $p $pre
 done
