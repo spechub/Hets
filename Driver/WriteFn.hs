ef4c609cebc5260771dae6e4f3a54a8959e81ed9
Christian Maeder <Christian.Maeder@dfki.de>
2010-11-01 19:38:44 +0000

    write morphism to file git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14182 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Driver/WriteFn.hs
+++ Driver/WriteFn.hs
@@ -291,7 +291,8 @@ writeSpecFiles opts file lenv ln dg = do
     unless noViews $
       mapM_ ( \ i -> case Map.lookup i gctx of
         Just (ViewEntry (ExtViewSig _ (GMorphism cid _ _ m _) _)) ->
-            print $ printMorphism (targetLogic cid) m
+            writeVerbFile opts (filePrefix ++ "_" ++ show i ++ ".view")
+              $ shows (printMorphism (targetLogic cid) m) "\n"
         _ -> putIfVerbose opts 0 $ "Unknown view name: " ++ show i
         ) vs
     mapM_ ( \ n ->
