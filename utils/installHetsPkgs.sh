5e9f2835f89542a4db162963bdfced8a535394bf
Christian Maeder <Christian.Maeder@dfki.de>
2010-11-16 17:31:38 +0000

    call cabal to install packages git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14226 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/installHetsPkgs.sh
+++ utils/installHetsPkgs.sh
@@ -7,15 +7,4 @@ else
 pre=`ghc --print-libdir | sed -e 's+/lib/.*++g'`
 fi
 
-for p in \
- aterm-0.1.0.0 \
- transformers-0.2.2.0 \
- mtl-2.0.0.0 \
- parsec-2.1.0.1 \
- network-2.2.3.1 \
- fgl-5.4.2.3 \
- xml-1.3.7 \
- zlib-0.5.2.0 \
- HTTP-4000.0.10
-do ./installPkg.sh $p $pre
-done
+cabal install aterm xml fgl HTTP haskell-src -p -O --enable-documentation --global --prefix=$pre
