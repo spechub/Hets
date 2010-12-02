eb7c2e900fad04edc597cc832ffc46c4494ea142
Christian Maeder <Christian.Maeder@dfki.de>
2010-12-02 12:12:01 +0000

    return proper global annos git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14279 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Static/AnalysisLibrary.hs
+++ Static/AnalysisLibrary.hs
@@ -220,7 +220,7 @@ anaLibDefn lgraph opts topLns libenv dg (Lib_defn ln alibItems pos ans) = do
       newLD = Lib_defn ln
         (zipWith replaceAnnoted (reverse libItems') alibItems) pos ans
       dg2 = dg1 { optLibDefn = Just newLD }
-  return (ln, newLD, gannos, Map.insert ln dg2 libenv')
+  return (ln, newLD, globalAnnos dg2, Map.insert ln dg2 libenv')
 
 anaLibItemAux :: HetcatsOpts -> LNS -> LibName
   -> ([LIB_ITEM], DGraph, LibEnv, LogicGraph)
