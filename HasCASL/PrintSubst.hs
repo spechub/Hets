0497ff67efe68737d163544e55f8fca8a8877e7e
Ewaryst Schulz <Ewaryst.Schulz@dfki.de>
2011-01-10 16:52:13 +0000

    Added PrettyInEnv class for nicer hascasl terms git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14362 cec4b9c1-7d33-0410-9eda-942365e851bb

--- HasCASL/PrintSubst.hs
+++ HasCASL/PrintSubst.hs
@@ -16,13 +16,21 @@ module HasCASL.PrintSubst where
 
 import HasCASL.Subst
 
-import HasCASL.As()
+import HasCASL.As
+import HasCASL.Le
 import HasCASL.PrintAs()
+import HasCASL.SimplifyTerm
 
 import Common.Doc
 import Common.DocUtils
 
 import qualified Data.Map as Map
+
+class PrettyInEnv a where
+    prettyInEnv :: Env -> a -> Doc
+
+-- instance Pretty a => PrettyInEnv a where
+--     prettyInEnv _ x  = pretty x
 
 
 instance Pretty SubstConst where
@@ -34,8 +42,9 @@ instance Pretty a => Pretty (SRule a) where
 
 instance Pretty Subst where
     pretty (Subst (a,b,_)) =
-        vcat [ text "Substitution", text $ replicate 70 '='
-             , prettyRuleMap "Termmap" a, prettyRuleMap "Typemap" b]
+        text "Subs"
+                 <> vcat [ text "titution"
+                         , prettyRuleMap "Termmap" a, prettyRuleMap "Typemap" b]
 
 prettyRuleMap :: (Pretty key, Pretty val)
                  => String -> Map.Map key (SRule val) -> Doc
@@ -49,3 +58,37 @@ prettyRuleMap t m | Map.null m = empty
 prettyRule :: (Pretty key, Pretty val) => (key, SRule val) -> Doc
 prettyRule (k, v) = pretty k <+> pretty v
 
+
+instance PrettyInEnv Term where
+    prettyInEnv e t = pretty $ simplifyTerm e t
+
+instance PrettyInEnv Type where
+    prettyInEnv = const pretty
+
+instance PrettyInEnv a => PrettyInEnv (SRule a) where
+    prettyInEnv e (Blocked x) = mapsto <+> prettyInEnv e x <+> parens (text "BLOCKED")
+    prettyInEnv e (Ready x) = mapsto <+> prettyInEnv e x
+
+instance PrettyInEnv Subst where
+    prettyInEnv e (Subst (a,b,_)) =
+        text "Subs"
+                 <> vcat [ text "titution"
+                         , prettyInEnvRuleMap e "Termmap" a
+                         , prettyInEnvRuleMap e "Typemap" b]
+
+prettyInEnvRuleMap :: (Pretty key, PrettyInEnv val)
+                 => Env -> String -> Map.Map key (SRule val) -> Doc
+prettyInEnvRuleMap e t m | Map.null m = empty
+                         | otherwise =
+                             vcat $ (if null t then [] else 
+                                         [ text t <+> colon
+                                         , text $ map (const '-') [0..length t+1]])
+                                      ++ map (prettyInEnvRule e) (Map.toList m)
+
+prettyInEnvRule :: (Pretty key, PrettyInEnv val) => Env -> (key, SRule val) -> Doc
+prettyInEnvRule e (k, v) = pretty k <+> prettyInEnv e v
+
+
+prettyTermMapping :: Env -> [(Term, Term)] -> Doc
+prettyTermMapping e l = vcat $ map f l where
+    f (t1, t2) = prettyInEnv e t1 <+> text "=" <+> prettyInEnv e t2
