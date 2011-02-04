f54cbc3b567482a53e417faff9cd2583bfdc47df
Dominik Dietrich <Dominik.Dietrich@dfki.de>
2011-02-04 14:29:05 +0000

    git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14483 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CSL/CSLMode.el
+++ CSL/CSLMode.el
@@ -8,25 +8,78 @@
   (cons "ENCL" (make-sparse-keymap "encl menu"))
   'tools)
 
-(define-key-after global-map [menu-bar enclmenu match] 
-  (cons "Match" (make-sparse-keymap "major modes")) 'kill-buffer )
-
-(define-key global-map [menu-bar enclmenu match others] '("..." . nil))
-(define-key global-map [menu-bar enclmenu match abstractflange] '("Abstract Flange" . match-abstractflange))
-
-(defun match-abstractflange ()
+;; extract all spec definitions
+(defun extractspecs ()
   (interactive)
-  (message (concatenate 'string "asd" (buffer-file-name (current-buffer))))
-  (call-process "/bin/ls" nil (get-buffer-create "*Match-Result*") t)
-  (switch-to-buffer (get-buffer "*Match-Result*"))
+  (save-excursion
+    (let
+	(p1 specs)
+      (goto-char (point-min))
+      (while (search-forward "spec" nil t)
+	(skip-chars-forward " ")
+	(setq p1 (point))
+	(forward-word)
+	(push (buffer-substring-no-properties p1 (point)) specs)
+	)
+      specs
+      )
+    )
   )
 
+;; extract all imports
+(defun extractgets ()
+  (interactive)
+  (save-excursion    
+    (let
+	(p1
+	 specs
+	 )
+      (goto-char (point-min))
+      (while (search-forward "get" nil t)
+	(setq p1 (point))
+	(forward-word)
+	(push (buffer-substring-no-properties p1 (point)) specs)      
+	;; comma separated lisp
+	(skip-chars-forward " ")
+	(while (string= (buffer-substring-no-properties (point) (+ 1 (point))) ",")
+	  (setq p1 (+ 1 (point)))
+	  (forward-word)
+	  (push (buffer-substring-no-properties p1 (point)) specs)
+	  (skip-chars-forward " ")
+	  )
+	)
+      specs
+      )
+    )
+  )
 
+(defun refresh-specmenu ()
+  (interactive)
+  (let
+      ((entries (append (extractspecs) (extractgets)))
+       currentsym
+       )    
+    
+    ;; delete the match menu
+    (global-unset-key [menu-bar enclmenu match])
+    ;; generate match menu
+    (define-key-after global-map [menu-bar enclmenu match] (cons "Match" (make-sparse-keymap)) 'kill-buffer)
+    
+    ;; generate subentries  
+    (dolist (item entries)
+      (setq currentsym (gensym))
+      (define-key global-map (vector 'menu-bar 'enclmenu 'match currentsym) (cons item (make-sparse-keymap)))
+      ;; submenus
+      (dolist (item2 entries)
+	(define-key global-map (vector 'menu-bar 'enclmenu 'match currentsym (gensym)) (cons item2 `(lambda () (interactive) (run-csl ,item ,item2))))
+	)
+      ))
+  )
 
-(setq myvarMenu 1)
-
-(defun match-abstractflange2 ()
-  (define-key global-map [menu-bar enclmenu match others a] '((concatenate 'string "Menu " myvarMenu) . nil))
-  (setq myvarMenu (+ 1 myvarMenu))
-  (message myvarMenu)
+(defun run-csl (spec1 spec2)
+  (interactive)
+  (message "selected %s and %s" spec1 spec2)
+;  (message (concatenate 'string "asd" (buffer-file-name (current-buffer))))
+;  (call-process "/bin/ls" nil (get-buffer-create "*Match-Result*") t)
+;  (switch-to-buffer (get-buffer "*Match-Result*"))
   )
