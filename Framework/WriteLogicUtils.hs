883de13a78d6483ce5e3cb150e90c4fc9cacac42
Kristina Sojakova <kristinas@cmu.edu>
2011-02-09 20:53:26 +0000

    Added missing file git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14509 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Framework/WriteLogicUtils.hs
+++ Framework/WriteLogicUtils.hs
@@ -1,21 +1,66 @@
+{- |
+Module      :  $Header$
+Description :  Utility functions for writing object logic instances
+Copyright   :  (c) Kristina Sojakova, DFKI Bremen 2010
+License     :  GPLv2 or higher, see LICENSE.txt
+
+Maintainer  :  k.sojakova@jacobs-university.de
+Stability   :  experimental
+Portability :  portable
+-}
+
 module Framework.WriteLogicUtils where
 
-undef = error "Framework.WriteLogicUtils"
+import Data.List
 
-mkCompOpt = undef
+tab :: String
+tab = "    "
 
-multiOpt = undef
+multiOpt :: String
+multiOpt = "MultiParamTypeClasses"
 
-synOpt = undef
+synOpt :: String
+synOpt = "TypeSynonymInstances"
 
-mkModDecl = undef
+prefixBy :: String -> [String] -> [String]
+prefixBy s xs = map (\ x -> s ++ x) xs
 
-mkImports = undef
+sepHoriz :: [String] -> String
+sepHoriz = concat . (prefixBy " ")
 
-mkLid = undef
+sepTabVert :: [String] -> String
+sepTabVert = concat . (prefixBy $ "\n" ++ tab)
 
-mkImpl = undef
+mkCompOpt :: [String] -> String
+mkCompOpt opts = "{-# LANGUAGE " ++ intercalate ", " opts ++ " #-}"
 
-mkInst = undef
+mkModDecl :: String -> String
+mkModDecl n = "module " ++ n ++ " where"
 
-mkDecl = undef
+mkImports :: [String] -> String
+mkImports imps =
+  intercalate "\n" $ prefixBy "import " imps
+
+mkLid :: String -> String
+mkLid lid = "data " ++ lid ++ " = " ++ lid ++ " deriving Show"
+
+mkImpl :: String -> String -> String -> String
+mkImpl f lid imp =
+  f ++ " " ++ lid ++ " = " ++ imp
+
+mkInst :: String -> String -> [String] -> [String] -> String
+mkInst inst lid args impls =
+  let header = "instance " ++ inst ++ " " ++ lid
+      argL = length args > 1
+      impE = not $ null impls
+      in header ++
+         if argL && impE then
+            sepTabVert $ args ++ ["where"] ++ impls else
+            if argL then
+               sepTabVert args else
+               if impE then
+                  sepHoriz args ++ " where" ++ sepTabVert impls else
+                  sepHoriz args
+
+mkDecl :: String -> String -> String -> String
+mkDecl n t v = n ++ " :: " ++ t ++ "\n" ++ n ++ " = " ++ v
