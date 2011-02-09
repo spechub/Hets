b02dd8be4b427378ba009227a6c596aff51cc02d
Christian Maeder <Christian.Maeder@dfki.de>
2011-02-09 16:32:40 +0000

    add a dummy file to ensure compilation git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14508 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ Framework/WriteLogicUtils.hs
@@ -0,0 +1,21 @@
+module Framework.WriteLogicUtils where
+
+undef = error "Framework.WriteLogicUtils"
+
+mkCompOpt = undef
+
+multiOpt = undef
+
+synOpt = undef
+
+mkModDecl = undef
+
+mkImports = undef
+
+mkLid = undef
+
+mkImpl = undef
+
+mkInst = undef
+
+mkDecl = undef
