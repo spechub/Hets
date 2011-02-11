a3b8d685ae08bf3f83a6c2930e872183c487c844
Ewaryst Schulz <Ewaryst.Schulz@dfki.de>
2011-02-11 15:44:20 +0000

    added mathlink lib and started integration into EnCL framework git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14521 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CSL/MathematicaInterpreter.hs
+++ CSL/MathematicaInterpreter.hs
@@ -14,42 +14,32 @@ Mathematica as AssignmentStore based on the Mathlink interface
 
 module CSL.MathematicaInterpreter where
 
-import Common.ProverTools (missingExecutableInPath)
-import Common.Utils (getEnvDef, trimLeft)
 import Common.Doc
 import Common.DocUtils
-import Common.IOS
+
+import Common.MathLink
+
 
 import CSL.AS_BASIC_CSL
-import CSL.Parse_AS_Basic (parseExpression)
 import CSL.Interpreter
-import CSL.Transformation
 import CSL.Verification
 import CSL.Analysis
 
--- the process communication interface
-import qualified Interfaces.Process as PC
+
+import Foreign.C.Types
 
 import Control.Monad
 --import Control.Monad.Trans (MonadTrans (..), MonadIO (..))
-import Control.Monad.Error (ErrorT(..), MonadError (..))
-import Control.Monad.State.Class
-import Control.Monad.Reader
+import Control.Monad.Error
+import Control.Monad.State
 
 import Data.List hiding (lookup)
 import qualified Data.Set as Set
 import Data.Maybe
-import System.Exit (ExitCode)
 import System.IO
 
+
 import Prelude hiding (lookup)
-
-
--- ----------------------------------------------------------------------
--- * Tests
--- ----------------------------------------------------------------------
-
-
 
 
 
@@ -57,42 +47,14 @@ import Prelude hiding (lookup)
 -- * Mathematica Types and Instances
 -- ----------------------------------------------------------------------
 
-type ConnectInfo = (PC.CommandState, PC.DTime)
+-- | MathematicaInterpreter with Translator based on the MathLink interface
+type MathState = ASState String
 
--- | MathematicaInterpreter with Translator based on the Mathlink interface
-type MathState = ASState ConnectInfo
-
-updateCS :: PC.CommandState -> ConnectInfo -> ConnectInfo
-updateCS cs (_, dt) = (cs, dt)
-
-updateDT :: PC.DTime -> ConnectInfo -> ConnectInfo
-updateDT dt (cs, _) = (cs, dt)
-
-getChannelTimeout :: MathState -> PC.DTime
-getChannelTimeout = snd . getConnectInfo
-
-setChannelTimeout :: PC.DTime -> MathState -> MathState
-setChannelTimeout dt = fmap (updateDT dt)
-
-getMI :: MathState -> PC.CommandState
-getMI = fst . getConnectInfo
-
-setMI :: PC.CommandState -> MathState -> MathState
-setMI cs = fmap (updateCS cs)
-
--- Mathematica interface, built on CommandState
-type MathematicaIO = ErrorT ASError (IOS MathState)
+-- Mathematica interface, built on MathLink
+type MathematicaIO = ErrorT ASError (StateT MathState ML)
 
 instance AssignmentStore MathematicaIO where
-    assign  = mathematicaAssign (evalMathematicaString True) mathematicaTransS mathematicaTransVarE
-    assigns =
-        mathematicaAssigns (evalMathematicaString False []) mathematicaTransS mathematicaTransVarE
-    lookup = mathematicaLookup (evalMathematicaString True []) mathematicaTransS
-    eval = mathematicaEval (evalMathematicaString True []) mathematicaTransE
-    check = mathematicaCheck (evalMathematicaString True []) mathematicaTransE
     names = get >>= return . SMem . getBMap
-    evalRaw s = get >>= liftIO . flip (mathematicaDirect True) s
-
     getUndefinedConstants e = do
       adg <- gets depGraph
       let g = isNothing . depGraphLookup adg
@@ -118,9 +80,6 @@ instance VCGenerator MathematicaIO where
           s = show
               $ (text "Verification condition for" <+> pretty ea <> text ":")
                     $++$ printExpForVC e
-          --s = show $ printExpForVC e <> text ";"
-          -- s = (++ "\n\n;\n\n") $ showRaw $ text "VC for" <+> pretty ea <> text ":" $++$ pretty e
---          vcHdl = stdout
       vcHdl <- liftM (fromMaybe stdout) $ gets vericondOut
       liftIO $ hPutStrLn vcHdl s where
                
@@ -137,190 +96,13 @@ instance MessagePrinter MathematicaIO where
     printMessage = liftIO . putStrLn
 
 -- ----------------------------------------------------------------------
--- * Mathematica Transformation Instances
--- ----------------------------------------------------------------------
-
--- TODO: Review the vargen facility and the cache-stuff in Transformation
-
--- | Variable generator instance for internal variables on the Hets-side,
--- in contrast to the newvar generation in lookupOrInsert of the BMap, which
--- generates variables for the Mathematica-side. We use nevertheless the same counter.
-instance VarGen MathematicaIO where
-    genVar = do
-      s <- get
-      let i = newkey $ getBMap s
-      put $ s { getBMap = (getBMap s) { newkey = i + 1 } }
-      return $ "?" ++ show i
-
-
--- ----------------------------------------------------------------------
 -- * Mathematica syntax functions
 -- ----------------------------------------------------------------------
 
-printExp :: EXPRESSION -> String
-printExp e = show $ runReader (printExpression e) mathematicaOpInfoNameMap
---printExp = exportExp
---printExp = show . pretty
--- :: ExpressionPrinter m => EXPRESSION -> m Doc
-
-
-
-mathematicaOpInfoMap :: OpInfoMap
-mathematicaOpInfoMap = operatorInfoMap
-
-mathematicaOpInfoNameMap :: OpInfoNameMap
-mathematicaOpInfoNameMap = operatorInfoNameMap
-
-printAssignment :: String -> [String] -> EXPRESSION -> String
-printAssignment n [] e = concat [n, ":= ", printExp e, ":", n, ";"]
-printAssignment n l e = concat [ n, ":= proc", args, printExp e
-                               , " end proc:", n, args, ";"]
-    where args = concat [ "(", intercalate ", " l, ") " ]
-
-printAssignmentWithEval :: String -> [String] -> EXPRESSION -> String
-printAssignmentWithEval n [] e =
---    concat [n, ":= evalf(", printExp e, "):", n, " &+ 0", ";"]
---    concat [n, ":= evalf(", printExp e, "):", n, ";"]
-    concat [n, ":= evalf(", printExp e, "):g(", n, ")", ";"]
-printAssignmentWithEval n l e = concat [ n, ":= proc", args, printExp e
-                                       , " end proc:", n, args, ";"]
-    where args = concat [ "(", intercalate ", " l, ") " ]
-
-printEvaluation :: EXPRESSION -> String
-printEvaluation e = printExp e ++ ";"
-
-printEvaluationWithEval :: EXPRESSION -> String
-printEvaluationWithEval e = "evalf(" ++ printExp e ++ ");"
-
-printLookup :: String -> String
-printLookup n = n ++ ";"
-
-
-{-
-The evalf makes the decision much faster. As we verify the result formally
-this should not be problematic in a formal context!
-
-
-In the following context "is" gives up if we do not use "evalf":
-
-x2 := cos(10+cos(10)/sin(10)+cos(10+cos(10)/sin(10))/sin(10+cos(10)/sin(10))
-      + cos(10+cos(10)/sin(10)+cos(10+cos(10)/sin(10))/sin(10+cos(10)/sin(10)))
-      / sin(10+cos(10)/sin(10)+cos(10+cos(10)/sin(10))/sin(10+cos(10)/sin(10))));
-is(abs(x2)<1.0e-4);
--}
-printBooleanExpr :: EXPRESSION -> String
-printBooleanExpr e = concat [ "is(evalf(", printExp e, "));" ]
-
-getBooleanFromExpr :: EXPRESSION -> Either String Bool
-getBooleanFromExpr (Op (OpId OP_true) _ _ _) = Right True
-getBooleanFromExpr (Op (OpId OP_false) _ _ _) = Right False
-getBooleanFromExpr (Op (OpId OP_failure) _ _ _) = Left "Mathematica FAILURE"
-getBooleanFromExpr e = Left $ "Cannot translate expression to boolean: "
-                       ++ show e
-
-
-
--- The evalf is mandatory if we use the if-statement for encoding
-{-
-
--- | As mathematica does not evaluate boolean expressions we encode them in an
--- if-stmt and transform the numeric response back.
-printBooleanExpr :: EXPRESSION -> String
-printBooleanExpr e = concat [ "if evalf("
-                            , printExp e, ") then 1 else 0 fi;"
-                            ]
-
-getBooleanFromExpr :: EXPRESSION -> Bool
-getBooleanFromExpr (Int 1 _) = True
-getBooleanFromExpr (Int 0 _) = False
-getBooleanFromExpr e =
-    error $ "getBooleanFromExpr: can't translate expression to boolean: "
-              ++ show e
--}
 
 -- ----------------------------------------------------------------------
 -- * Generic Communication Interface
 -- ----------------------------------------------------------------------
-
-{- |
-   The generic interface abstracts over the concrete evaluation function
--}
-
-mathematicaAssign :: (MonadError ASError s, MonadIO s, SymbolicEvaluator s) =>
-               ([String] -> String -> s [EXPRESSION])
-            -> (ConstantName -> s String)
-            -> ([String] -> EXPRESSION -> s (EXPRESSION, [String]))
-            -> ConstantName -> AssDefinition -> s EXPRESSION
-mathematicaAssign ef trans transE n def = do
-  let e = getDefiniens def
-      args = getArguments def
-  (e', args') <- transE args e
-  n' <- trans n
-  -- liftIO $ putStrLn $ show e'
-  b <- getSymbolicMode
-  let f = if b then printAssignment else printAssignmentWithEval
-  el <- ef args $ f n' args' e'
---  el <- ef args $ printAssignment n' args' e'
-  case el of
-    [rhs] -> return rhs
-    l -> throwError $ ASError InterfaceError $
-         "mathematicaAssign: unparseable result for assignment of "
-         ++ (show $ pretty n) ++ "\n" ++ (show $ pretty l)
-
-mathematicaAssigns :: (AssignmentStore s) => (String -> s [EXPRESSION])
-          -> (ConstantName -> s String)
-          -> ([String] -> EXPRESSION -> s (EXPRESSION, [String]))
-          -> [(ConstantName, AssDefinition)] -> s ()
-mathematicaAssigns ef trans transE l =
-    let f (n, def) = do
-          let e = getDefiniens def
-              args = getArguments def
-          (e', args') <- transE args e
-          n' <- trans n
-          return $ printAssignment n' args' e'
-    in mapM f l >>= ef . unlines >> return ()
-
-mathematicaLookup :: (AssignmentStore s) => (String -> s [EXPRESSION])
-           -> (ConstantName -> s String)
-           -> ConstantName -> s (Maybe EXPRESSION)
-mathematicaLookup ef trans n = do
-  n' <- trans n
-  el <- ef $ printLookup n'
-  return $ listToMaybe el
--- we don't want to return nothing on id-lookup: "x; --> x"
---  if e == mkOp n [] then return Nothing else return $ Just e
-
-mathematicaEval :: (MonadError ASError s, SymbolicEvaluator s) =>
-             (String -> s [EXPRESSION])
-        -> (EXPRESSION -> s EXPRESSION)
-        -> EXPRESSION -> s EXPRESSION
-mathematicaEval ef trans e = do
-  e' <- trans e
-  b <- getSymbolicMode
-  let f = if b then printEvaluation else printEvaluationWithEval
-  el <- ef $ f e'
-  if null el
-   then throwError $ ASError InterfaceError $
-            "mathematicaEval: expression " ++ show e' ++ " couldn't be evaluated"
-   else return $ head el
-
-mathematicaCheck :: (MonadError ASError s, AssignmentStore s) =>
-              (String -> s [EXPRESSION])
-         -> (EXPRESSION -> s EXPRESSION)
-         -> EXPRESSION -> s Bool
-mathematicaCheck ef trans e = do
-  e' <- trans e
-  el <- ef $ printBooleanExpr e'
-  if null el
-   then throwError $ ASError CASError
-            $ "mathematicaCheck: expression " ++ show e' ++ " could not be evaluated"
-   else case getBooleanFromExpr $ head el of
-          Right b -> return b
-          Left s ->
-              throwError
-              $ ASError CASError $
-                concat [ "mathematicaCheck: CAS error for expression "
-                       , show e', "\n", s ]
 
 
 -- ----------------------------------------------------------------------
@@ -328,144 +110,116 @@ mathematicaCheck ef trans e = do
 -- ----------------------------------------------------------------------
 
 
-wrapCommand :: IOS PC.CommandState a -> IOS MathState a
-wrapCommand ios = do
-  r <- get
-  let map' x = setMI x r
-  stmap map' getMI ios
-
--- | A direct way to communicate with Mathematica
-mathematicaDirect :: Bool -> MathState -> String -> IO String
-mathematicaDirect b mit s = do
-  (res, _) <- runIOS (getMI mit) $ PC.call (getChannelTimeout mit) s
-  return $ if b then removeOutputComments res else res
-
-mathematicaTransE :: EXPRESSION -> MathematicaIO EXPRESSION
-mathematicaTransE e = do
-  r <- get
-  let bm = getBMap r
-      (bm', e') = translateExpr bm e
-  put r { getBMap = bm' }
-  return e'
-
-mathematicaTransVarE :: [String] -> EXPRESSION -> MathematicaIO (EXPRESSION, [String])
-mathematicaTransVarE vl e = do
-  r <- get
-  let bm = getBMap r
-      args = translateArgVars bm vl
-      (bm', e') = translateExprWithVars vl bm e
-  put r { getBMap = bm' }
-  return (e', args)
-
-mathematicaTransS :: ConstantName -> MathematicaIO String
-mathematicaTransS s = do
-  r <- get
-  let bm = getBMap r
-      (bm', s') = lookupOrInsert bm s
-  --     outs = [ "lookingUp " ++ show s ++ " in "
-  --            , show $ pretty bm, "{", show bm, "}" ]
-  -- liftIO $ putStrLn $ unlines outs
-
-  put r { getBMap = bm' }
-  return s'
-
-
--- | Evaluate the given String as mathematica expression and
--- parse the result to an expression list.
-evalMathematicaString :: Bool -- ^ Use parser
-                -> [String] -- ^ Use this argument list for variable trafo
-                -> String -> MathematicaIO [EXPRESSION]
-evalMathematicaString b args s = do
-  -- 0.09 seconds is a critical value for the accepted response time of Mathematica
-  mit <- get
-  res <- lift $ wrapCommand $ PC.call (getChannelTimeout mit) s
-  let bm = getBMap mit
-      trans = if null args then revtranslateExpr bm
-              else revtranslateExprWithVars args bm
-  -- when b $ liftIO $ putStrLn $ "evalMathematicaString:"
-  -- when b $ liftIO $ putStrLn $ show $ maybeToList $ parseExpression mathematicaOpInfoMap $ trimLeft
-  --          $ removeOutputComments res
-  -- when b $ liftIO $ putStrLn $ show $ map trans $ maybeToList $ parseExpression mathematicaOpInfoMap $ trimLeft
-  --          $ removeOutputComments res
-  return $ if b
-           then map trans $ maybeToList $ parseExpression mathematicaOpInfoMap
-                    $ trimLeft $ removeOutputComments res
-           else []
-
--- | init the mathematica communication
-mathematicaInit :: AssignmentDepGraph ()
-          -> Int -- ^ Verbosity level
-          -> PC.DTime -- ^ timeout for response
-          -> IO MathState
-mathematicaInit adg v to = do
-  rc <- lookupMathematicaShellCmd
-  libpath <- getEnvDef "HETS_MATHEMATICALIB"
-             $ error "mathematicaInit: Environment variable HETS_MATHEMATICALIB not set."
-  case rc of
-    Left mathematicacmd -> do
-            cs <- PC.start (mathematicacmd ++ " -q") v
-                  $ Just PC.defaultConfig { PC.startTimeout = 3 }
-            (_, cs') <- runIOS cs $ PC.call 0.5
-                        $ concat [ "interface(prettyprint=0); Digits := 10;"
-                                 , "libname := \"", libpath, "\", libname;" ]
-            return ASState { getBMap = initWithOpMap mathematicaOpInfoMap
-                           , getConnectInfo = (cs', to)
-                           , depGraph = adg
-                           , debugMode = False
-                           , symbolicMode = False
-                           , vericondOut = Nothing
-                           }
-    _ -> error "Could not find mathematica shell command!"
-
-
--- | Loads a mathematica module such as intpakX or intCompare
-mathematicaLoadModule :: MathState -> String -> IO String
-mathematicaLoadModule rit s =
-    fmap fst $ runIOS (getMI rit) (PC.call 0.5 $ "with(" ++ s ++ ");")
-
-
-mathematicaExit :: MathState -> IO (Maybe ExitCode)
-mathematicaExit mit = do
-  (ec, _) <- runIOS (getMI mit) $ PC.close $ Just "quit;"
-  return ec
-
-execWithMathematica :: MathState -> MathematicaIO a -> IO (MathState, a)
-execWithMathematica mit m = do
-  let err s = error $ "execWithMathematica: " ++ s
-  (res, mit') <- runIOS mit $ runErrorT m
-  case res of
-    Left s' -> err $ asErrorMsg s'
-    Right x -> return (mit', x)
-
-runWithMathematica :: AssignmentDepGraph () -> Int -- ^ Verbosity level
-          -> PC.DTime -- ^ timeout for response
-          -> [String] -> MathematicaIO a
-          -> IO (MathState, a)
-runWithMathematica adg i to l m = do
-  mit <- mathematicaInit adg i to
-  mapM_ (mathematicaLoadModule mit) l
-
-  -- wraps an interval around the number
-  let debugFun = "g := proc(v) z:=abs(Float(v,1-Digits)):[v-z, v+z] end;"
-  runIOS (getMI mit) $ PC.call 0.3 debugFun
-        
-  execWithMathematica mit m
-
 -- ----------------------------------------------------------------------
--- * The Mathematica system
+-- * The Mathematica system via MathLink
 -- ----------------------------------------------------------------------
 
--- | Left String is success, Right String is failure
-lookupMathematicaShellCmd :: IO (Either String String)
-lookupMathematicaShellCmd  = do
-  cmd <- getEnvDef "HETS_MATHEMATICA" "mathematica"
-  -- check that prog exists
-  noProg <- missingExecutableInPath cmd
-  let f = if noProg then Right else Left
-  return $ f cmd
 
--- | Removes lines starting with ">"
-removeOutputComments :: String -> String
-removeOutputComments =
-    filter (/= '\\') . concat . filter (not . isPrefixOf ">") . lines
- 
+-- ----------------------------------------------------------------------
+-- * MathLink stuff
+-- ----------------------------------------------------------------------
+
+
+
+readAndPrintOutput :: ML ()
+readAndPrintOutput = do
+  exptype <- mlGetNext
+  let pr | exptype == dfMLTKSYM = readAndPrintML "Symbol: " mlGetSymbol
+         | exptype == dfMLTKSTR = readAndPrintML "String: " mlGetString
+         | exptype == dfMLTKINT = readAndPrintML "Int: " mlGetInteger
+         | exptype == dfMLTKREAL = readAndPrintML "Real: " mlGetReal
+         | exptype == dfMLTKFUNC =
+             do
+               len <- mlGetArgCount
+               if len == 0 then mlProcError
+                else do
+                 let f i = readAndPrintOutput
+                           >> if i < len then userMessage ", " else return ()
+                 readAndPrintOutput
+                 userMessage "["
+                 forM_ [1..len] f
+                 userMessage "]"
+
+         | exptype == dfMLTKERROR = userMessageLn "readAndPrintOutput-error" >> mlProcError
+         | otherwise = userMessageLn ("readAndPrintOutput-error" ++ show exptype)
+                       >> mlProcError
+  pr
+
+
+
+userMessage :: String -> ML ()
+userMessage s = logMessage s >> liftIO (putStr s)
+
+userMessageLn :: String -> ML ()
+userMessageLn s = logMessageLn s >> liftIO (putStrLn s)
+
+
+class MLShow a where
+    mlshow :: a -> String
+
+instance MLShow String where
+    mlshow s = s
+
+instance MLShow CDouble where
+    mlshow = show
+
+instance MLShow CInt where
+    mlshow = show
+
+readAndPrintML :: MLShow a => String -> ML a -> ML ()
+readAndPrintML _ pr = pr >>= userMessage . mlshow
+
+
+-- * Test functionality
+
+sendPlus :: CInt -> CInt -> CInt -> ML ()
+sendPlus i j k = do
+  mlPutFunction "EvaluatePacket" 1
+  mlPutFunction "Plus" 3
+  mlPutInteger i
+  mlPutInteger j
+  mlPutInteger k
+  mlEndPacket
+  userMessageLn "Sent"
+
+
+sendFormula :: CInt -> CInt -> CInt -> ML ()
+sendFormula i j k = do
+  mlPutFunction "EvaluatePacket" 1
+
+  mlPutFunction "Plus" 2
+  mlPutFunction "Times" 2
+  mlPutInteger i
+  mlPutInteger j
+  mlPutFunction "Times" 3
+  mlPutInteger k
+  mlPutInteger k
+  mlPutSymbol "xsymb"
+
+  mlEndPacket
+
+  userMessageLn "Sent"
+
+
+-- ----------------------------------------------------------------------
+-- * Tests
+-- ----------------------------------------------------------------------
+
+test :: [String] -> IO ()
+test argv = do
+  let (k, i, j) = (read $ argv!!0, read $ argv!!1, read $ argv!!2)
+      mN = if length argv > 3 then Just $ argv!!3 else Nothing
+
+--  liftIO (putStrLn $ "Sending " ++ show (i, j, k))
+
+  -- Send Plus[i, j]
+  eRes <- runLink (Just "/tmp/mlffi.txt") mN $
+          forM_ [1 .. k] $ sendFormula i j >=>
+                const (receivePacket >> readAndPrintOutput >> userMessageLn "")
+  case eRes of
+    Left eid -> putStrLn $ "Error " ++ show eid
+    _ -> putStrLn $ "OK"
+
+
+  
+  
---
+++ Common/MathLink.hs
@@ -0,0 +1,287 @@
+{-# LANGUAGE TypeSynonymInstances #-}
+{- |
+Module      :  $Header$
+Description :  A Haskell MathLink interface
+Copyright   :  (c) Ewaryst Schulz, DFKI Bremen 2011
+License     :  GPLv2 or higher, see LICENSE.txt
+Maintainer  :  Ewaryst.Schulz@dfki.de
+Stability   :  experimental
+Portability :  non-portable (see language extensions)
+
+A Haskell MathLink interface based on the Foreign.MathLink.Bindings module
+-}
+
+
+module Common.MathLink where
+
+import Foreign.C -- get the C types
+import Foreign.Marshal -- get the array marshalling utils
+import Foreign.Storable
+import Foreign.Ptr (Ptr,nullPtr)
+
+import Foreign.MathLink.Bindings
+ 
+import Control.Monad.Reader
+import System.Timeout
+import System.IO
+
+
+-- * Constants for the MathLink interface
+
+dfMLTKAEND, dfMLTKALL_DECODERS, dfMLTKAPCTEND, dfMLTKARRAY, dfMLTKARRAY_DECODER
+ , dfMLTKCONT, dfMLTKDIM, dfMLTKELEN, dfMLTKEND, dfMLTKERR, dfMLTKERROR
+ , dfMLTKFUNC, dfMLTKINT, dfMLTKMODERNCHARS_DECODER, dfMLTKNULL
+ , dfMLTKNULLSEQUENCE_DECODER, dfMLTKOLDINT, dfMLTKOLDREAL, dfMLTKOLDSTR
+ , dfMLTKOLDSYM, dfMLTKPACKED_DECODER, dfMLTKPACKED, dfMLTKPCTEND, dfMLTKREAL
+ , dfMLTKSEND, dfMLTKSTR, dfMLTKSYM, dfRETURNPKT, dfRETURNTEXTPKT
+ , dfRETURNEXPRPKT :: CInt
+
+dfRETURNPKT = 3
+dfRETURNTEXTPKT = 4
+dfRETURNEXPRPKT = 16
+
+
+dfMLTKAEND=13
+dfMLTKALL_DECODERS=983040
+dfMLTKAPCTEND=10
+dfMLTKARRAY=65
+dfMLTKARRAY_DECODER=262144
+dfMLTKCONT=92
+dfMLTKDIM=68
+dfMLTKELEN=32
+dfMLTKEND=10
+dfMLTKERR=0
+dfMLTKERROR=0
+dfMLTKFUNC=70
+dfMLTKINT=43
+dfMLTKMODERNCHARS_DECODER=524288
+dfMLTKNULL=46
+dfMLTKNULLSEQUENCE_DECODER=0
+dfMLTKOLDINT=73
+dfMLTKOLDREAL=82
+dfMLTKOLDSTR=83
+dfMLTKOLDSYM=89
+dfMLTKPACKED_DECODER=131072
+dfMLTKPACKED=80
+dfMLTKPCTEND=93
+dfMLTKREAL=42
+dfMLTKSEND=44
+dfMLTKSTR=34
+dfMLTKSYM=35
+
+
+-- * MathLink monad as Reader IO monad
+
+data MLState =
+    MLState
+    { mlink :: MLINK
+    , logHdl :: Maybe Handle
+    }
+
+mkState :: MLINK -> MLState
+mkState mlp = MLState { mlink = mlp, logHdl = Nothing }
+
+mkStateWithLog :: MLINK -> Handle -> MLState
+mkStateWithLog mlp hdl = MLState { mlink = mlp, logHdl = Just hdl }
+
+type ML = ReaderT MLState IO
+
+askMLink :: ML MLINK
+askMLink = asks mlink
+
+logMessage :: String -> ML ()
+logMessage s = do
+  mHdl <- asks logHdl
+  case mHdl of
+    Just hdl -> liftIO $ hPutStr hdl s
+    Nothing -> liftIO $ putStr s
+
+logMessageLn :: String -> ML ()
+logMessageLn s = do
+  mHdl <- asks logHdl
+  case mHdl of
+    Just hdl -> liftIO $ hPutStrLn hdl s
+    Nothing -> liftIO $ putStrLn s
+
+liftMLIO :: (MLINK -> IO b) -> ML b
+liftMLIO f = askMLink >>= liftIO . f
+
+
+-- * MathLink connection handling
+
+connectLink :: MLINK -> IO Bool
+connectLink lp = do
+  let p i j = do
+        i' <- cMlReady lp
+        if toBool i' || j > 3000 then return j
+         else cMlFlush lp >> if i > 1000 then p 0 (j+1) else p (i+1) j
+
+  p (0::Int) (0::Int) >>= putStrLn . ("ready after " ++) . show
+
+  res <- cMlConnect lp
+  return $ toBool res
+
+runLink :: Maybe FilePath -> Maybe String -> ML a -> IO (Either Int a)
+runLink mFP mName mlprog = do
+--  env <- mlInitialize ""
+  env <- cMlInitialize nullPtr
+  if (env == nullPtr) then return $ Left 1
+   else do
+    putStrLn "Initialized"
+
+    let (name, mode) = case mName of
+                         Just n -> (n, "connect")
+                         _ -> ("math -mathlink", "launch")
+
+    let openargs = ["-linkname", name, "-linkmode", mode]
+
+    lp <- mlOpen 4 openargs
+    mB <- if lp == nullPtr then return Nothing
+--          else liftM Just $ connectLink lp
+          else timeout 3000000 $ connectLink lp
+    
+    case mB of
+      Nothing -> return $ Left 2
+      Just False -> return $ Left 3
+      _ ->
+          do
+            putStrLn "Opened"
+            x <- case mFP of
+                   Just fp ->
+                       withFile fp WriteMode $ runReaderT mlprog . mkStateWithLog lp
+                   Nothing ->
+                       runReaderT mlprog $ mkState lp
+            mlClose lp
+            mlDeinitialize env
+            return $ Right x
+  
+
+mlInitialize  :: String -> IO MLEnvironment
+mlInitialize = flip withCString cMlInitialize
+
+mlDeinitialize  :: MLEnvironment -> IO ()
+mlDeinitialize = cMlDeinitialize
+
+mlOpen  :: CInt -> [String] -> IO MLINK
+mlOpen i l = withStringArray l $ cMlOpen i
+
+
+
+-- * C to Haskell utilities
+
+withStringArray :: MonadIO m => [String] -> (Ptr CString -> IO b) -> m b
+withStringArray l f = liftIO $ mapM newCString l >>= flip withArray f
+
+
+mlGetA  :: (Storable a, Show a, Show b) => (Ptr a -> IO b) -> IO a
+mlGetA f = let g ptr = f ptr >> peek ptr in alloca g
+
+
+-- maybe better via foreign pointer, check later
+mlGetCString  :: Show b => (Ptr CString -> IO b) -> (CString -> IO ()) -> IO String
+mlGetCString f disownF =
+    let g ptr = do
+          cs <- f ptr >> peek ptr
+          s <- peekCString cs
+          disownF cs
+          return s
+    in alloca g
+
+
+-- * Haskell friendly MathLink interface built on top of the raw bindings
+
+mlClose  :: MLINK -> IO CInt
+mlClose = cMlClose
+
+mlFlush  :: ML CInt
+mlFlush = liftMLIO cMlFlush
+
+mlReady  :: ML CInt
+mlReady = liftMLIO cMlReady
+
+mlConnect  :: ML CInt
+mlConnect = liftMLIO cMlConnect
+
+mlEndPacket  :: ML CInt
+mlEndPacket = liftMLIO cMlEndPacket
+
+mlNextPacket  :: ML CInt
+mlNextPacket = liftMLIO cMlNextPacket
+
+mlNewPacket  :: ML CInt
+mlNewPacket = liftMLIO cMlNewPacket
+
+mlGetNext  :: ML CInt
+mlGetNext = liftMLIO cMlGetNext
+
+
+mlGetArgCount :: ML CInt
+mlGetArgCount = askMLink >>= liftIO . mlGetA . cMlGetArgCount
+
+
+
+-- cMlGetSymbol  :: MLINK -> Ptr CString -> IO CInt
+mlGetSymbol  :: ML String
+mlGetSymbol = do
+  ml <- askMLink
+  liftIO $ mlGetCString (cMlGetSymbol ml) $ cMlDisownSymbol ml
+
+-- cMlGetString  :: MLINK -> Ptr CString -> IO CInt
+mlGetString  :: ML String
+--mlGetString = askMLink >>= liftIO . mlGetCString . cMlGetString
+mlGetString = do
+  ml <- askMLink
+  liftIO $ mlGetCString (cMlGetString ml) $ cMlDisownString ml
+
+-- cMlGetReal  :: MLINK -> Ptr CDouble -> IO CInt
+mlGetReal  :: ML CDouble
+mlGetReal = askMLink >>= liftIO . mlGetA . cMlGetReal
+
+-- cMlGetInteger  :: MLINK -> Ptr CInt -> IO CInt
+mlGetInteger  :: ML CInt
+mlGetInteger = askMLink >>= liftIO . mlGetA . cMlGetInteger
+
+
+mlPutString  :: String -> ML CInt
+mlPutString s = liftMLIO f where
+    f ml = withCString s $ cMlPutString ml
+
+mlPutSymbol  :: String -> ML CInt
+mlPutSymbol s = liftMLIO f where
+    f ml = withCString s $ cMlPutSymbol ml
+
+mlPutFunction  :: String -> CInt -> ML CInt
+mlPutFunction s i = liftMLIO f where
+    f ml = withCString s $ flip (cMlPutFunction ml) i
+
+mlPutInteger  :: CInt -> ML CInt
+mlPutInteger = liftMLIO . flip cMlPutInteger
+
+ 
+
+mlError  :: ML CInt
+mlError = liftMLIO cMlError
+
+mlErrorMessage  :: ML String
+mlErrorMessage = liftMLIO (cMlErrorMessage >=> peekCString)
+
+
+-- * MathLink interface utils
+
+mlProcError :: ML ()
+mlProcError = do
+  eid <- mlError
+  if toBool eid then mlErrorMessage >>= logMessageLn . ("Error detected by MathLink: " ++)
+   else logMessageLn "Error detected by Interface"
+
+receivePacket :: ML ()
+receivePacket = do
+  -- skip any packets before the first ReturnPacket
+  waitUntilPacket (0::Int) [dfRETURNPKT, dfRETURNEXPRPKT, dfRETURNTEXTPKT]
+
+waitUntilPacket :: Num a => a -> [CInt] -> ML ()
+waitUntilPacket i l = do
+  np <- mlNextPacket
+  if elem np l then logMessageLn $ "GotReturn after " ++ show i ++ " iterations"
+   else (logMessageLn $ "wap: " ++ show np) >> mlNewPacket >> waitUntilPacket (i+1) l
+
