630d56a3bb2213a0b9651804c992753e42ef8b93
Christian Maeder <Christian.Maeder@dfki.de>
2011-02-17 09:36:12 +0000

    added module header git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14566 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Framework.hs
+++ Framework.hs
@@ -2,10 +2,10 @@
 Description : syntax and analysis for adding object logics to Hets
 from their specification in a logical framework
 
-The folder "Framework" contains the infrastructure needed for 
-extending Hets with new logics which are specified in 
+The folder "Framework" contains the infrastructure needed for
+extending Hets with new logics which are specified in
 some logical framework. Currently, this only works for LF, but
-reading Isabelle and Maude definitions of logics is also 
+reading Isabelle and Maude definitions of logics is also
 possible.
 
 The module "Framework.AS" gives the abstract syntax, while the
@@ -14,3 +14,5 @@ the module "Framework.Analysis" and it relies on the methods
 provided by the instances of the class 'Logic.Logic.LogicalFramework'.
 
 -}
+
+module Framework where
