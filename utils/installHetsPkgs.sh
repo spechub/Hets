651b65d88715480a474041557bee863ea8b10fba
Corneliu-Claudiu Prodescu <Corneliu-Claudiu.Prodescu@dfki.de>
2011-03-04 18:54:19 +0000

    added deps for hets packs git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14665 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/installHetsPkgs.sh
+++ utils/installHetsPkgs.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+#
+#
+# Note that before running this script you need:
+#  - cabal-install
+#  - zlib.h (zlib1g-dev or similar)
+#  - curses.h (libncurses-dev or similar)
+#  - cairo C lib (libcairo2-dev or similar)
+#  - glib-2.0 C lib (libglib2.0-dev or similar)
+#  - pango(>=1.0) C lib (libpango-dev or similar)
+#  - gtk-2.0 C lib (libgtk2.0-dev or similar)
+#  - glade-2.0 C lib (libglade2-dev or similar)
+#  - happy
+#
+
 
 if [ -n "$1" ]
 then
@@ -13,11 +27,12 @@ if which $BINARIES &> /dev/null; then
     echo Binaries already installed
 else
     echo Installing binaries...
-    cabal install alex gtk2hs-buildtools
+    cabal install alex gtk2hs-buildtools \
+                  -O --global --prefix=$pre
 fi
 
 #installing the libraries
 echo Installing libraries...
-cabal install aterm xml fgl HTTP haskell-src tar glade haskeline \
+cabal install aterm cairo xml fgl HTTP haskell-src tar glade haskeline \
               hexpat wai-extra-0.2.4.2 uni-uDrawGraph \
               -O --enable-documentation --global --prefix=$pre
