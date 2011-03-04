f161f9015db69629c035851e94380fec712598c2
Corneliu-Claudiu Prodescu <Corneliu-Claudiu.Prodescu@dfki.de>
2011-03-04 14:14:48 +0000

    updated installHetsPckgs git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14662 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/installHetsPkgs.sh
+++ utils/installHetsPkgs.sh
@@ -7,6 +7,17 @@ else
 pre=`ghc --print-libdir | sed -e 's+/lib/.*++g'`
 fi
 
-cabal install gtk2hs-buildtools aterm xml fgl HTTP haskell-src tar glade \
-              haskeline hexpat wai-extra-0.2.4.2 uni-uDrawGraph \
+#installing the binaries
+BINARIES="alex gtk2hsC2hs gtk2hsHookGenerator gtk2hsTypeGen"
+if which $BINARIES &> /dev/null; then
+    echo Binaries already installed
+else
+    echo Installing binaries...
+    cabal install alex gtk2hs-buildtools
+fi
+
+#installing the libraries
+echo Installing libraries...
+cabal install aterm xml fgl HTTP haskell-src tar glade haskeline \
+              hexpat wai-extra-0.2.4.2 uni-uDrawGraph \
               -O --enable-documentation --global --prefix=$pre
