03f9c14dea1bb3dafc1297c40952a790fde7b503
Corneliu-Claudiu Prodescu <Corneliu-Claudiu.Prodescu@dfki.de>
2011-03-04 13:35:24 +0000

    updated utils/installHetsPkgs.sh git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14660 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/installHetsPkgs.sh
+++ utils/installHetsPkgs.sh
@@ -7,6 +7,6 @@ else
 pre=`ghc --print-libdir | sed -e 's+/lib/.*++g'`
 fi
 
-cabal install aterm xml fgl HTTP haskell-src tar glade haskeline hexpat \
-              wai-extra-0.2.4.2 uni-uDrawGraph \
+cabal install gtk2hs-buildtools aterm xml fgl HTTP haskell-src tar glade \
+              haskeline hexpat wai-extra-0.2.4.2 uni-uDrawGraph \
               -O --enable-documentation --global --prefix=$pre
