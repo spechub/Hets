a97055b3846103c11f2f39d16713fb278dd15d70
Christian Maeder <Christian.Maeder@dfki.de>
2011-03-16 17:03:42 +0000

    avoid name clash git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14725 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CspCASL/SymbItems.hs
+++ CspCASL/SymbItems.hs
@@ -13,10 +13,10 @@ data SymbMapItems = SymbMapItems SymbKind [SymbMap] deriving (Show, Eq)
 data SymbKind = CaslKind SYMB_KIND | ProcessKind | ChannelKind
     deriving (Show, Eq, Ord)
 
-data Symb = Symb Id (Maybe SymbType)
+data Symb = Symb Id (Maybe CspType)
             deriving (Show, Eq)
 
 -- for channels with sorts we may re-use A_type that is ambiguous
-data SymbType = CaslType TYPE | ProcType ProcProfile deriving (Show, Eq)
+data CspType = CaslType TYPE | ProcType ProcProfile deriving (Show, Eq)
 
 data SymbMap = SymbMap Symb (Maybe Symb) deriving (Show, Eq)
