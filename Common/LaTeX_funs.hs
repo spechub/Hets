7fb8d0aede4d54d56e0b9d37c7a28dd7453ff0a1
Christian Maeder <Christian.Maeder@dfki.de>
2011-03-30 12:11:16 +0000

    cleaned up using scan git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14831 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Common/LaTeX_funs.hs
+++ Common/LaTeX_funs.hs
@@ -57,18 +57,15 @@ import Common.Lib.Pretty as Pretty
 import Common.Parsec
 import Text.ParserCombinators.Parsec as Parsec
 
--- |
--- a constant String for starting a LaTeX indentation with tab stop
+-- | a constant String for starting a LaTeX indentation with tab stop
 startTab :: String
 startTab = "\\@begT@"
 
--- |
--- a constant String for releasing a LaTeX indentation with tab stop
+-- | a constant String for releasing a LaTeX indentation with tab stop
 endTab :: String
 endTab = "\\@endT@"
 
--- |
--- a constant String to set a tab stop and enable it
+-- | a constant String to set a tab stop and enable it
 setTab :: String
 setTab = "\\="
 
@@ -84,14 +81,14 @@ setTabWSp = "\\@setTS@{"
 
 calc_line_length :: String -> Int
 calc_line_length s =
-    let (r_unit,r_number) =
-            (\(x,y) -> (reverse x,reverse y)) $ span isAlpha $ reverse s
+    let (r_unit, r_number) =
+            (\ (x, y) -> (reverse x, reverse y)) $ span isAlpha $ reverse s
         unit = case r_unit of
                "mm" -> 1
                "cm" -> 10
                "pt" -> 0.351
                "in" -> 25.4
-               u    -> error ( "unknown or unsupported LaTeX unit: " ++ u )
+               u -> error ( "unknown or unsupported LaTeX unit: " ++ u )
         len :: Double
         len = read $ map (\ c -> case c of
                                    ',' -> '.'
@@ -104,40 +101,40 @@ calc_line_length s =
 
 data Word_type =
     Keyword | StructId | Normal | Comment | Annotation | AnnotationBold | Axiom
-    deriving (Show,Eq)
+    deriving (Show, Eq)
 
 calc_word_width :: Word_type -> String -> Int
 calc_word_width wt s = Map.findWithDefault
   (sum_char_width_deb (showString "In map \"" . shows wt . showString "\" \'")
    wFM k_wFM s - correction) s wFM
     where (wFM, k_wFM) = case wt of
-                        Keyword        -> (keyword_map,key_keyword_map)
-                        StructId       -> (structid_map,key_structid_map)
-                        Comment        -> (comment_map,key_comment_map)
-                        Annotation     -> (annotation_map,key_annotation_map)
+                        Keyword -> (keyword_map, key_keyword_map)
+                        StructId -> (structid_map, key_structid_map)
+                        Comment -> (comment_map, key_comment_map)
+                        Annotation -> (annotation_map, key_annotation_map)
                         AnnotationBold -> (annotationbf_map,
                                            key_annotationbf_map)
-                        Axiom          -> (axiom_map,key_axiom_map)
-                        Normal         -> (normal_map,key_normal_map)
+                        Axiom -> (axiom_map, key_axiom_map)
+                        Normal -> (normal_map, key_normal_map)
           correction = case wt of
                        Axiom -> itCorrection s
-                       _     -> 0
+                       _ -> 0
 
 itCorrection :: String -> Int
 itCorrection [] = 0
 itCorrection s
     | length s < 2 || head s == '\\' = 0
-    | otherwise    = itCorrection' 0 s
+    | otherwise = itCorrection' 0 s
     where itCorrection' :: Int -> String -> Int
           itCorrection' _ [] = error "itCorrection' applied to empty List"
           itCorrection' r ys@[y1, y2]
               | not (isAlphaNum y1) = r
               | not (isAlphaNum y2) = r
-              | otherwise           = r + lookupCorrection ys
+              | otherwise = r + lookupCorrection ys
 
           itCorrection' r (y1 : ys@(y2 : _))
               | not (isAlphaNum y1) = itCorrection' r ys
-              | otherwise           =
+              | otherwise =
                   itCorrection'
                         (r + lookupCorrection [y1, y2])
                         ys
@@ -148,17 +145,17 @@ itCorrection s
 
 sum_char_width_deb :: (String -> String) -- only used for an hackie debug thing
                    -> Map.Map String Int
-                   -> Map.Map Char [String]  -> String -> Int
+                   -> Map.Map Char [String] -> String -> Int
 sum_char_width_deb _pref_fun cFM key_cFM s = sum_char_width' s 0
-    where sum_char_width' []  r = r
+    where sum_char_width' [] r = r
           sum_char_width' [c] r = r + case c of
               '}' -> 0
               '{' -> 0
               ' ' -> lookupWithDefault_cFM "~"
               _ -> lookupWithDefault_cFM [c]
-          sum_char_width' full@(c1:rest@(c2:cs)) r
+          sum_char_width' full@(c1 : rest@(c2 : cs)) r
               | isLigature [c1, c2] = case Map.lookup [c1, c2] cFM of
-                                        Just l  -> sum_char_width' cs (r+l)
+                                        Just l -> sum_char_width' cs (r + l)
                                         Nothing -> sum_char_width' rest nl
               | [c1, c2] == "\\ " =
                   sum_char_width' cs (r + lookupWithDefault_cFM "~")
@@ -180,7 +177,7 @@ sum_char_width_deb _pref_fun cFM key_cFM s = sum_char_width' s 0
 
 prefixIsKey :: String -> Map.Map Char [String] -> Maybe String
 prefixIsKey [] _ = Nothing
-prefixIsKey ls@(c:_) key_cFM = case filter (`isPrefixOf` ls)
+prefixIsKey ls@(c : _) key_cFM = case filter (`isPrefixOf` ls)
         $ Map.findWithDefault [] c key_cFM of
     [] -> Nothing
     s : _ -> Just s
@@ -193,17 +190,17 @@ isLigature s = case s of
 keyword_width, structid_width, axiom_width, annotationbf_width,
     comment_width, normal_width :: String -> Int
 annotationbf_width = calc_word_width AnnotationBold
-keyword_width      = calc_word_width Keyword
-structid_width     = calc_word_width StructId
-comment_width      = calc_word_width Comment
-normal_width       = calc_word_width Normal
-axiom_width        = sum . map (calc_word_width Axiom) . parseAxiomString
+keyword_width = calc_word_width Keyword
+structid_width = calc_word_width StructId
+comment_width = calc_word_width Comment
+normal_width = calc_word_width Normal
+axiom_width = sum . map (calc_word_width Axiom) . parseAxiomString
 
--- |
--- latex_macro creates a document ('Doc') containing String
--- that has a zero width.
--- So it can be used for LaTeX-macros not needing any space, i.e.
--- @\textit{@ or @}@
+{- |
+latex_macro creates a document ('Doc') containing String
+that has a zero width.
+So it can be used for LaTeX-macros not needing any space, i.e.
+@\textit{@ or @}@ -}
 latex_macro :: String -> Doc
 latex_macro = sp_text 0
 
@@ -212,11 +209,11 @@ casl_keyword_latex, casl_annotationbf_latex,
        casl_comment_latex, casl_structid_latex,
        casl_normal_latex :: String -> Doc
 casl_annotationbf_latex s = sp_text (annotationbf_width s) s
-casl_structid_latex s     = sp_text (structid_width s) s
-casl_comment_latex s      = sp_text (comment_width s) s
-casl_keyword_latex s      = sp_text (keyword_width s) s
-casl_normal_latex s       = sp_text (normal_width s) s
-casl_axiom_latex s        = sp_text (axiom_width s) s
+casl_structid_latex s = sp_text (structid_width s) s
+casl_comment_latex s = sp_text (comment_width s) s
+casl_keyword_latex s = sp_text (keyword_width s) s
+casl_normal_latex s = sp_text (normal_width s) s
+casl_axiom_latex s = sp_text (axiom_width s) s
 
 -- | sort, op, pred, type and its plurals
 hc_sty_casl_keyword :: String -> Doc
@@ -233,34 +230,32 @@ hc_sty_small_keyword kw =
     latex_macro "\\KW{" <> casl_annotationbf_latex (escapeSpecial kw)
                     <> latex_macro "}"
 
-hc_sty_axiom, hc_sty_structid, hc_sty_id,hc_sty_structid_indexed
+hc_sty_axiom, hc_sty_structid, hc_sty_id, hc_sty_structid_indexed
     :: String -> Doc
-hc_sty_structid sid = latex_macro "\\SId{"<>sid_doc<>latex_macro "}"
+hc_sty_structid sid = latex_macro "\\SId{" <> sid_doc <> latex_macro "}"
     where sid_doc = casl_structid_latex (escapeSpecial sid)
 hc_sty_structid_indexed sid =
-    latex_macro "\\SIdIndex{"<>sid_doc<>latex_macro "}"
+    latex_macro "\\SIdIndex{" <> sid_doc <> latex_macro "}"
     where sid_doc = casl_structid_latex (escapeSpecial sid)
-hc_sty_id i        = latex_macro "\\Id{"<>id_doc<>latex_macro "}"
+hc_sty_id i = latex_macro "\\Id{" <> id_doc <> latex_macro "}"
     where id_doc = casl_axiom_latex i
-hc_sty_axiom ax = latex_macro "\\Ax{"<>ax_doc<>latex_macro "}"
+hc_sty_axiom ax = latex_macro "\\Ax{" <> ax_doc <> latex_macro "}"
     where ax_doc = casl_axiom_latex ax
 
 -- | flush argument doc to the right
 flushright :: Doc -> Doc
 flushright = (latex_macro "\\`" <>)
 
--- |
--- a constant String for the start of annotations
+-- | a constant String for the start of annotations
 startAnno :: String
 startAnno = "{\\small{}"
 
--- |
--- a constant string ending an annotation
+-- | a constant string ending an annotation
 endAnno :: String
 endAnno = "%@%small@}"
 
 escapeSpecial :: String -> String
-escapeSpecial = concatMap $ \ c -> if  elem c "_%$&{}#" then '\\' : [c] else
+escapeSpecial = concatMap $ \ c -> if elem c "_%$&{}#" then '\\' : [c] else
   Map.findWithDefault [c] c escapeMap
 
 escapeLatex :: String -> String
@@ -303,58 +298,58 @@ parseAtom = do
 -- | a character map for special latex characters
 escapeMap :: Map.Map Char String
 escapeMap = Map.fromList
- [('\\',"\\Ax{\\setminus}"),
-  ('^',"\\Ax{\\hat{\\ }}"),
-  ('"',"''"),
-  ('~',"\\Ax{\\sim}"),
-  ('\160',"\\ "),
-  ('\162',"\\Id{\\textcent}"),
-  ('\164',"\\Id{\\textcurrency}"),
-  ('\165',"\\Id{\\textyen}"),
-  ('\166',"\\Id{\\textbrokenbar}"),
-  ('\170',"\\Id{\\textordfeminine}"),
-  ('\171',"\\Id{\\guillemotleft}"),
-  ('\172',"\\Ax{\\neg}"),
-  ('\173',"-"),
-  ('\174',"\\Id{\\textregistered}"),
-  ('\175',"\\Ax{\\bar{\\ }}"),
-  ('\176',"\\Id{\\textdegree}"),
-  ('\177',"\\Ax{\\pm}"),
-  ('\178',"\\Ax{^2}"),
-  ('\179',"\\Ax{^3}"),
-  ('\180',"\\Ax{\\acute{\\ }}"),
-  ('\181',"\\Ax{\\mu}"),
-  ('\185',"\\Ax{^1}"),
-  ('\186',"\\Id{\\textordmasculine}"),
-  ('\187',"\\Id{\\guillemotright}"),
-  ('\192',"\\Ax{\\grave{A}}"),
-  ('\193',"\\Ax{\\acute{A}}"),
-  ('\200',"\\Ax{\\grave{E}}"),
-  ('\201',"\\Ax{\\acute{E}}"),
-  ('\204',"\\Ax{\\grave{I}}"),
-  ('\205',"\\Ax{\\acute{I}}"),
-  ('\208',"\\Id{\\DH}"),
-  ('\210',"\\Ax{\\grave{O}}"),
-  ('\211',"\\Ax{\\acute{O}}"),
-  ('\215',"\\Ax{\\times}"),
-  ('\217',"\\Ax{\\grave{U}}"),
-  ('\218',"\\Ax{\\acute{U}}"),
-  ('\221',"\\Ax{\\acute{Y}}"),
-  ('\222',"\\Id{\\TH}"),
-  ('\224',"\\Ax{\\grave{a}}"),
-  ('\225',"\\Ax{\\acute{a}}"),
-  ('\232',"\\Ax{\\grave{e}}"),
-  ('\233',"\\Ax{\\acute{e}}"),
-  ('\236',"\\Ax{\\grave{\\Id{\\i}}}"),
-  ('\237',"\\Ax{\\acute{\\Id{\\i}}}"),
-  ('\240',"\\Id{\\dh}"),
-  ('\242',"\\Ax{\\grave{o}}"),
-  ('\243',"\\Ax{\\acute{o}}"),
-  ('\247',"\\Ax{\\div}"),
-  ('\249',"\\Ax{\\grave{u}}"),
-  ('\250',"\\Ax{\\acute{u}}"),
-  ('\253',"\\Ax{\\acute{y}}"),
-  ('\254',"\\Id{\\th}")]
+ [('\\', "\\Ax{\\setminus}"),
+  ('^', "\\Ax{\\hat{\\ }}"),
+  ('"', "''"),
+  ('~', "\\Ax{\\sim}"),
+  ('\160', "\\ "),
+  ('\162', "\\Id{\\textcent}"),
+  ('\164', "\\Id{\\textcurrency}"),
+  ('\165', "\\Id{\\textyen}"),
+  ('\166', "\\Id{\\textbrokenbar}"),
+  ('\170', "\\Id{\\textordfeminine}"),
+  ('\171', "\\Id{\\guillemotleft}"),
+  ('\172', "\\Ax{\\neg}"),
+  ('\173', "-"),
+  ('\174', "\\Id{\\textregistered}"),
+  ('\175', "\\Ax{\\bar{\\ }}"),
+  ('\176', "\\Id{\\textdegree}"),
+  ('\177', "\\Ax{\\pm}"),
+  ('\178', "\\Ax{^2}"),
+  ('\179', "\\Ax{^3}"),
+  ('\180', "\\Ax{\\acute{\\ }}"),
+  ('\181', "\\Ax{\\mu}"),
+  ('\185', "\\Ax{^1}"),
+  ('\186', "\\Id{\\textordmasculine}"),
+  ('\187', "\\Id{\\guillemotright}"),
+  ('\192', "\\Ax{\\grave{A}}"),
+  ('\193', "\\Ax{\\acute{A}}"),
+  ('\200', "\\Ax{\\grave{E}}"),
+  ('\201', "\\Ax{\\acute{E}}"),
+  ('\204', "\\Ax{\\grave{I}}"),
+  ('\205', "\\Ax{\\acute{I}}"),
+  ('\208', "\\Id{\\DH}"),
+  ('\210', "\\Ax{\\grave{O}}"),
+  ('\211', "\\Ax{\\acute{O}}"),
+  ('\215', "\\Ax{\\times}"),
+  ('\217', "\\Ax{\\grave{U}}"),
+  ('\218', "\\Ax{\\acute{U}}"),
+  ('\221', "\\Ax{\\acute{Y}}"),
+  ('\222', "\\Id{\\TH}"),
+  ('\224', "\\Ax{\\grave{a}}"),
+  ('\225', "\\Ax{\\acute{a}}"),
+  ('\232', "\\Ax{\\grave{e}}"),
+  ('\233', "\\Ax{\\acute{e}}"),
+  ('\236', "\\Ax{\\grave{\\Id{\\i}}}"),
+  ('\237', "\\Ax{\\acute{\\Id{\\i}}}"),
+  ('\240', "\\Id{\\dh}"),
+  ('\242', "\\Ax{\\grave{o}}"),
+  ('\243', "\\Ax{\\acute{o}}"),
+  ('\247', "\\Ax{\\div}"),
+  ('\249', "\\Ax{\\grave{u}}"),
+  ('\250', "\\Ax{\\acute{u}}"),
+  ('\253', "\\Ax{\\acute{y}}"),
+  ('\254', "\\Id{\\th}")]
 
 {- acute and grave characters don't work in a tabbing environment
    \textcent upto textbrokenbar requires \usepackage{textcomp}
