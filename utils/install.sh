b5d33f4c1367106479df8bcd7d01d549f1f6d2b7
Christian Maeder <Christian.Maeder@dfki.de>
2011-04-05 12:16:35 +0000

    compute date if argument is missing git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14849 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/install.sh
+++ utils/install.sh
@@ -2,10 +2,13 @@
 
 #change daily snapshot into a backup or release
 #the first argument should be a version number or a date
-#compute date by "date +%F"
 
 INSTALLDIR=/home/www/agbkb/forschung/formal_methods/CoFI/hets
-VERSION=$1
+if [ -z "$1" ] ; then
+  VERSION=`date +%F`
+else
+  VERSION=$1
+fi
 
 case `uname -s` in
   SunOS) TAR=gtar;;
