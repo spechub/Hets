7a1053a6e02ce96e63d39dc94f0b50f84634173e
Robert Savu <Robert.Savu@dfki.de>
2011-05-04 21:42:57 +0000

    the brep_to_xml convertor now is able to iterate through each sub-object of a geometrical object stored in the brp file git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@14999 cec4b9c1-7d33-0410-9eda-942365e851bb

--- FreeCAD/brep_conversion/src/brep_to_xml.cxx
+++ FreeCAD/brep_conversion/src/brep_to_xml.cxx
@@ -5,23 +5,82 @@
 
 using namespace std;
 
-int main (int argc, char* argv[]) {
-  char* filename = "input.brp";
-  if (argv[1] != NULL) {
-	strcpy(filename,argv[1]);
-  }
-  TopoDS_Shape Sh;
-  BRep_Builder builder;
+void print_shape_type (TopoDS_Shape sh) 
+{
+    TopAbs_ShapeEnum st;
+    st = sh.ShapeType();
+    switch (st)
+    {
+        case TopAbs_COMPOUND:
+            cout << "TopAbs_COMPOUND" << endl;
+            break;
+        case TopAbs_COMPSOLID:
+            cout << "TopAbs_COMPSOLID" << endl;
+            break;
+        case TopAbs_SOLID:
+            cout << "TopAbs_SOLID" << endl;
+            break;
+        case TopAbs_SHELL:
+            cout << "TopAbs_SHELL" << endl;
+            break;
+        case TopAbs_FACE:
+            cout << "TopAbs_FACE" << endl;
+            break;
+        case TopAbs_WIRE:
+            cout << "TopAbs_WIRE" << endl;
+            break;
+        case TopAbs_EDGE:
+            cout << "TopAbs_EDGE" << endl;
+            break;
+        case TopAbs_VERTEX:
+            cout << "TopAbs_VERTEX" << endl;
+            break;
+        case TopAbs_SHAPE:
+            cout << "TopAbs_SHAPE" << endl;
+            break;
+    }
+}
 
-  bool b = BRepTools::Read(Sh, (const Standard_CString)filename, builder);
+int main (int argc, char* argv[]) 
+{
+    char filename[50] = "./test/input.brp";
+    if (argv[1] != NULL) {
+	    strcpy(filename,argv[1]);
+    }
+    TopoDS_Shape Sh;
+    BRep_Builder builder;
 
-  if (b) {
-    cout<< "reading ok";
-  }
-  else {
-    cout<< "reading failure";
-  }
-  cout<< endl;
+    bool b = BRepTools::Read(Sh, (const Standard_CString)filename, builder);
 
-  return 0;
+    if (b) 
+    {
+        cout<< "reading ok" << endl;
+        
+        print_shape_type(Sh);
+        BRepTools_ShapeSet SS;
+        SS.Add(Sh);//A shapeSet cand help accessing subshapes of Sh.
+        SS.Dump(Sh, std::cout);//says how many subshapes are in SS.
+        cout << "############################################################"
+             << endl;
+        //SS.Dump(std::cout); //dumps the whole info.
+        SS.DumpExtent(std::cout);//says how many shapes of each type are in SS
+        cout << "############################################################"
+        << endl;
+        
+        int i;
+        TopoDS_Shape temp;
+        for (i = 1; i <= SS.NbShapes(); i++)
+        {
+            temp = SS.Shape(i);
+            print_shape_type(temp);
+        }
+                  
+    }
+    else 
+    {
+        cout<< "reading failure" << endl;
+    }  
+    
+    
+    return 0;
 }
--- FreeCAD/brep_conversion/src/brep_to_xml.hxx
+++ FreeCAD/brep_conversion/src/brep_to_xml.hxx
@@ -2,5 +2,7 @@
 #include <TopoDS_Shape.hxx>
 #include <TopAbs_ShapeEnum.hxx>
 #include <BRep_Builder.hxx>
-
-
+#include <Standard_PrimitiveTypes.hxx>
+#include <BRepTools_ShapeSet.hxx>
+#include <Standard_TypeDef.hxx>
+#include <Standard_Stream.hxx>
