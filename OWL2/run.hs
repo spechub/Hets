a6264798dafb838561b52df2d0fa876b0f6ff746
Christian Maeder <Christian.Maeder@dfki.de>
2011-06-15 14:48:47 +0000

    corrected test example git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@15145 cec4b9c1-7d33-0410-9eda-942365e851bb

--- OWL2/run.hs
+++ OWL2/run.hs
@@ -4,13 +4,14 @@ import OWL2.Parse
 import OWL2.Print ()
 
 import Common.DocUtils
+import Common.Parsec
 
 import Text.ParserCombinators.Parsec
 
 processFile :: String -> IO ()
 processFile file = do
   str <- readFile file
-  case runParser basicSpec () file str of
+  case runParser (basicSpec << eof) () file str of
     Right o -> putStrLn $ showDoc o "\n"
     Left err -> print err
 
