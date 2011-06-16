62e297f2981c4447636bf1aa04e9cb2821d8e5e1
Francisc Nicolae Bungiu <f.bungiu@jacobs-university.de>
2011-06-16 09:05:24 +0000

    Started parsing git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@15151 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ OWL2/OWLAnalysis.hs
@@ -0,0 +1,83 @@
+{- |
+Module      :  $Header$
+Copyright   :  Heng Jiang, Uni Bremen 2004-2007
+License     :  GPLv2 or higher, see LICENSE.txt
+
+Maintainer  :  Christian.Maeder@dfki.de
+Stability   :  provisional
+Portability :  non-portable (imports Logic.Logic)
+
+analyse OWL files by calling the external Java parser.
+-}
+
+module OWL2.OWLAnalysis (parseOWL) where
+
+import OWL2.AS
+import OWL2.Parse
+
+import Static.GTheory
+import Static.DevGraph
+import Static.ComputeTheory
+
+import Common.Id
+import Common.GlobalAnnotations
+import Common.ExtSign
+import Common.LibName
+import Common.Result
+import Common.ProverTools
+import Common.Utils
+import Common.AS_Annotation hiding (isAxiom, isDef)
+--import ATerm.ReadWrite
+--import ATerm.Unshared
+
+import Driver.Options
+
+import Logic.Comorphism
+import Logic.Grothendieck
+import Logic.Logic
+import Logic.Prover
+
+import System.Directory
+import System.Exit
+import System.FilePath
+import System.Process
+
+import Text.ParserCombinators.Parsec
+import Common.Parsec
+
+import qualified Data.Map as Map
+import qualified Data.List as List
+import Data.Graph.Inductive.Graph
+import qualified Data.Graph.Inductive.Query.DFS as DFS
+import qualified Data.Graph.Inductive.Query.BFS as BFS
+import Data.Maybe (fromJust)
+
+
+-- | call for owl parser (env. variable $HETS_OWL_TOOLS muss be defined)
+parseOWL :: FilePath              -- ^ local filepath or uri
+         -> IO OntologyMap        -- ^ map: uri -> OntologyFile
+parseOWL filename = do
+    let jar = "OWL2Parser.jar"
+    absfile <- if checkUri filename then return filename else
+      fmap ("file://" ++) $ canonicalizePath filename
+    (hasJar, toolPath) <- check4HetsOWLjar jar
+    if hasJar then do
+       tmpFile <- getTempFile "" $ basename filename ++ ".term"
+       (exitCode, _, _) <- readProcessWithExitCode "java"
+         ["-jar", toolPath </> jar, absfile, tmpFile] ""
+       case exitCode of
+         ExitSuccess -> parseProc tmpFile
+         _ -> error ("process stop! " ++ show exitCode)
+      else error $ jar ++ " not found"
+
+-- | parse the tmp-omn-file from java-owl-parser
+parseProc :: FilePath -> IO OntologyMap
+parseProc filename = do 
+    str <- readFile filename
+    case runParser (many1 basicSpec << eof) () filename str of
+      Right os -> return $ Map.fromList 
+        $ map (\ o -> (showQN $ uri $ ontology o, o)) os 
+      Left err -> do 
+        print err
+        return Map.empty
+	
