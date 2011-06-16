43afc12b21eb01c8b513f694502881c92bf938ba
Robert Savu <Robert.Savu@dfki.de>
2011-06-16 13:56:07 +0000

    added XML printer for FreeCAD documents git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@15159 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ FreeCAD/XMLPrinter.hs
@@ -0,0 +1,84 @@
+module XMLPrinter where
+
+import Text.XML.Light
+import FreeCAD.As
+
+makeAttr :: String -> String -> Attr
+makeAttr key value = Attr (unqual key) value
+
+doc2XML :: Document -> Element
+doc2XML list  = unode  "document" (map sendNamedObj list)
+
+sendNamedObj :: NamedObject -> Element
+sendNamedObj no = unode (name no) (getNOChildren no)
+
+getNOChildren :: NamedObject -> [Element]
+getNOChildren no = (makePlaceElem place):(makeObjElem obj):[] where
+    pobj = object no
+    obj = o pobj
+    place = p pobj
+
+makePlaceElem :: Placement -> Element
+makePlaceElem pl = unode "placement" attrList 
+    where
+      attrList = xAt:yAt:zAt:q0At:q1At:q2At:q3At:[]
+      mkp a b = Attr (unqual a) (show b)
+      mko a b = Attr (unqual a) (show b)
+      xAt = mkp "x" (x $ position pl)
+      yAt = mkp "y" (y $ position pl)
+      zAt = mkp "z" (z $ position pl)
+      q0At = mko "q0" (q0 $ orientation pl)
+      q1At = mko "q1" (q1 $ orientation pl)
+      q2At = mko "q2" (q2 $ orientation pl)
+      q3At = mko "q3" (q3 $ orientation pl)
+
+mkNumAtt :: Num a => String -> a -> Attr
+mkNumAtt key num = (Attr (unqual key) (show num))
+
+makeObjElem :: Object -> Element
+makeObjElem obj = case obj of
+                    BaseObject bo -> makeBOElem bo
+                    Cut eo1 eo2 -> mk2refs "Cut" eo1 eo2
+                    Common eo1 eo2 -> mk2refs "Common" eo1 eo2
+                    Fusion eo1 eo2 -> mk2refs "Fusion" eo1 eo2
+                    Section eo1 eo2 -> mk2refs "Section" eo1 eo2
+                    Extrusion eo1 val -> mk1refs "Extrusion" eo1 val
+    where
+      mkRefAtt key eo = (Attr (unqual key) (getEORef eo))
+      mk2refs consType ref1 ref2 = 
+          unode consType ((mkRefAtt "base" ref1):(mkRefAtt "tool" ref1):[])
+      mk1refs consType ref val = 
+          unode consType ((mkRefAtt "base" ref):(mkNumAtt "tool" val):[])
+
+getEORef :: ExtendedObject -> String
+getEORef eo = case eo of
+           Ref s -> s
+           Placed po -> error "cannot get reference"
+
+makeBOElem :: BaseObject -> Element
+makeBOElem obj = case obj of
+                   Box a1 a2 a3 -> 
+                       unode "box" ((mkNumAtt "height" a1):
+                          (mkNumAtt "width" a2):(mkNumAtt "length" a3):[])
+                   Cylinder a1 a2 a3 -> 
+                       unode "cylinder" ((mkNumAtt "angle" a1):
+                          (mkNumAtt "height" a2):(mkNumAtt "radius" a3):[])
+                   Sphere a1 a2 a3 a4 -> 
+                       unode "sphere" ((mkNumAtt "angle1" a1):
+                          (mkNumAtt "angle2" a2):(mkNumAtt "angle3" a3):
+                          (mkNumAtt "radius" a4):[])
+                   Cone a1 a2 a3 a4 ->
+                       unode "cone" ((mkNumAtt "angle" a1):
+                          (mkNumAtt "radius1" a2):(mkNumAtt "radius2" a3):
+                          (mkNumAtt "height" a4):[])
+                   Torus a1 a2 a3 a4 a5 ->
+                       unode "torus" ((mkNumAtt "angle1" a1):
+                          (mkNumAtt "angle2" a2):(mkNumAtt "angle3" a3):
+                          (mkNumAtt "radius1" a4):(mkNumAtt "radius2" a5):[])
+                   Line a1 -> unode "line" (mkNumAtt "length" a1)
+                   Circle a1 a2 a3 -> 
+                       unode "circle" ((mkNumAtt "startang" a1):
+                          (mkNumAtt "endang" a2):(mkNumAtt "radius" a3):[])
+                   Rectangle a1 a2 -> 
+                       unode "rectangle" ((mkNumAtt "height" a1):
+                          (mkNumAtt "length" a2):[])
