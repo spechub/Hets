ac7cded6520be0acb464c9cedd87925452b727ad
Christian Maeder <Christian.Maeder@dfki.de>
2011-06-28 08:43:12 +0000

    enhanced script git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@15343 cec4b9c1-7d33-0410-9eda-942365e851bb

--- OWL2/tests/run.sh
+++ OWL2/tests/run.sh
@@ -3,7 +3,24 @@
 HETS_OWL_TOOLS=`pwd`/..
 export HETS_OWL_TOOLS
 
+
 for i in *.rdf
 do
-  ../../hets -i ow2 $i
+  java -jar ../OWL2Parser.jar file://`pwd`/$i $i.omnm
 done
+
+for i in *.rdf
+do
+  ../../hets -v2 -i ow2 -o th $i
+done
+
+for i in *.het
+do
+  ../../hets -l OWL -v2 -o th $i
+done
+
+for i in *.th
+do
+  ../../hets -v2 $i
+done
+
