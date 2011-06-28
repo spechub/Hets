63971e2d3797ce5ac51ddd54f3a7b39305711d22
Felix Gabriel Mance <f.mance@jacobs-university.de>
2011-06-28 21:42:26 +0000

    deleted owl2 git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@15390 cec4b9c1-7d33-0410-9eda-942365e851bb

--- OWL2/tests/run.sh
+++ OWL2/tests/run.sh
@@ -3,23 +3,23 @@
 HETS_OWL_TOOLS=`pwd`/..
 export HETS_OWL_TOOLS
 
-for i in *.rdf
+for i in RDF/*.rdf
 do
   java -jar ../OWL2Parser.jar file://`pwd`/$i $i.omnm
 done
 
-for i in *.rdf
+for i in RDF/*.rdf
 do
   ../../hets -v2 -i ow2 -o th,pp.het,omn $i
 done
 
-for i in OWL2tests/*.ofn *.rdf
+for i in RDF/*.rdf OMN/*.omn
 do
   java -jar ../OWL2Parser.jar file://`pwd`/$i $i.omnm
   ../../hets -v2 -i ow2 -o th,pp.het,omn $i
 done
 
-for i in *.het
+for i in HET/*.het
 do
   ../../hets -l OWL -v2 -o th,pp.het,omn $i
 done
@@ -29,7 +29,7 @@ do
   ../../hets -l OWL -v2 $i
 done
 
-for i in *.omn
+for i in OMN/*.omn
 do
   echo $i
   java -jar ../OWL2Parser.jar file://`pwd`/$i $i.omn2
