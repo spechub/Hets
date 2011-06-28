8a9497c27679419c1665af2fe90cf404fb44d454
Christian Maeder <Christian.Maeder@dfki.de>
2011-06-28 09:39:17 +0000

    re-check created omn files git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@15348 cec4b9c1-7d33-0410-9eda-942365e851bb

--- OWL2/tests/run.sh
+++ OWL2/tests/run.sh
@@ -29,4 +29,10 @@ do
   ../../hets -l OWL -v2 $i
 done
 
-rm -f *.pp.het *.th *.omn OWL2tests/*.pp.het OWL2tests/*.th OWL2tests/*.omn
+for i in *.omn
+do
+  echo $i
+  java -jar ../OWL2Parser.jar file://`pwd`/$i $i.omn2
+done
+
+#rm -f *.pp.het *.th *.omn *.omn2
