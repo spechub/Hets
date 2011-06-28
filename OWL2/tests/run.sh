76512d3a8e03e5e5dea66f69cbd5c6503f0a388d
Felix Gabriel Mance <f.mance@jacobs-university.de>
2011-06-28 22:10:03 +0000

    run.sh git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@15395 cec4b9c1-7d33-0410-9eda-942365e851bb

--- OWL2/tests/run.sh
+++ OWL2/tests/run.sh
@@ -3,23 +3,23 @@
 HETS_OWL_TOOLS=`pwd`/..
 export HETS_OWL_TOOLS
 
-for i in RDF/*.rdf
+for i in *.rdf
 do
   java -jar ../OWL2Parser.jar file://`pwd`/$i $i.omnm
 done
 
-for i in RDF/*.rdf
+for i in *.rdf
 do
   ../../hets -v2 -i ow2 -o th,pp.het,omn $i
 done
 
-for i in RDF/*.rdf OMN/*.omn
+for i in *.rdf *.omn
 do
   java -jar ../OWL2Parser.jar file://`pwd`/$i $i.omnm
   ../../hets -v2 -i ow2 -o th,pp.het,omn $i
 done
 
-for i in HET/*.het
+for i in *.het
 do
   ../../hets -l OWL -v2 -o th,pp.het,omn $i
 done
@@ -29,7 +29,7 @@ do
   ../../hets -l OWL -v2 $i
 done
 
-for i in OMN/*.omn
+for i in *.omn
 do
   echo $i
   java -jar ../OWL2Parser.jar file://`pwd`/$i $i.omn2
