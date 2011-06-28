db5892a4565665fcdfa9e4fa24fb18889430ea64
Christian Maeder <Christian.Maeder@dfki.de>
2011-06-28 09:11:16 +0000

    fixed test script git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@15346 cec4b9c1-7d33-0410-9eda-942365e851bb

--- OWL2/tests/run.sh
+++ OWL2/tests/run.sh
@@ -3,7 +3,6 @@
 HETS_OWL_TOOLS=`pwd`/..
 export HETS_OWL_TOOLS
 
-
 for i in *.rdf
 do
   java -jar ../OWL2Parser.jar file://`pwd`/$i $i.omnm
@@ -11,16 +10,23 @@ done
 
 for i in *.rdf
 do
-  ../../hets -v2 -i ow2 -o th $i
+  ../../hets -v2 -i ow2 -o th,pp.het,omn $i
+done
+
+for i in OWL2tests/*.ofn *.rdf
+do
+  java -jar ../OWL2Parser.jar file://`pwd`/$i $i.omnm
+  ../../hets -v2 -i ow2 -o th,pp.het,omn $i
 done
 
 for i in *.het
 do
-  ../../hets -l OWL -v2 -o th $i
+  ../../hets -l OWL -v2 -o th,pp.het,omn $i
 done
 
-for i in *.th
+for i in *.th *.pp.het *.omn
 do
-  ../../hets -v2 $i
+  ../../hets -l OWL -v2 $i
 done
 
+rm -f *.pp.het *.th *.omn OWL2tests/*.pp.het OWL2tests/*.th OWL2tests/*.omn
