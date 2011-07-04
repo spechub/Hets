380618b72161ed46d5a21fd27487003302dbe6a7
Simon Ulbricht <tekknix@informatik.uni-bremen.de>
2011-07-04 23:03:28 +0000

    use Cursor to navigate within Xml structure git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@15447 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Static/ApplyXmlDiff.hs
+++ Static/ApplyXmlDiff.hs
@@ -13,11 +13,14 @@ change an Xml-DGraph according to XmlDiff-data.
 module Static.ApplyXmlDiff where
 
 import Common.XUpdate
-import Common.XPath
+import Common.XPath (Expr (..), Step (..), Axis (..), NodeTest (..), Path (..)
+  , showSteps)
 
 import Control.Monad
 
 import Text.XML.Light
+import Text.XML.Light.Cursor (Cursor (..))
+import qualified Text.XML.Light.Cursor as Cr (fromElement, findChild)
 
 applyXmlDiff :: Monad m => Element -> String -> m Element
 applyXmlDiff el diff = do
@@ -32,6 +35,36 @@ changeXml el (Change csel expr) = case csel of
         _ -> fail $ "no implementation for " ++ show c
     Remove -> undefined
     _ -> fail $ "no implementation for " ++ show csel
+
+cursorFromPathExpr :: Monad m => Expr -> Element -> m Cursor
+cursorFromPathExpr expr el = case expr of
+  PathExpr Nothing (Path False (stp : stps)) -> do
+    unless (checkStepElement "DGraph" stp)
+      $ fail "unexpected step"
+    cr <- findChildByName "input is not a DGraph element" (unqual "DGraph")
+            $ Cr.fromElement el
+    anaSteps cr stps
+  _ -> fail $ "no implementation for expression: " ++ show expr
+
+checkStepElement :: String -> Step -> Bool
+checkStepElement str stp = case stp of
+  Step Child (NameTest l) _ | l == str -> True
+  _ -> False
+
+findChildByName :: Monad m => String -> QName -> Cursor -> m Cursor
+findChildByName err qNm cr = case Cr.findChild ( \c -> case current c of
+            Elem e -> elName e == qNm
+            _ -> False ) cr of
+    Nothing -> fail err
+    Just cr' -> return cr'
+
+anaSteps :: Monad m => Cursor -> [Step] -> m Cursor
+anaSteps cr stps = case stps of
+  Step Child (NameTest l) _ : stps' -> do
+    cr' <- findChildByName "unexpected step(2)" (unqual l) cr
+    anaSteps cr' stps'
+  [] -> return cr
+  _ -> fail $ "no implementation for " ++ showSteps False stps
 
 removeXml :: (Element -> Bool) -> Element -> Element
 removeXml f e = let ct = elContent e in
