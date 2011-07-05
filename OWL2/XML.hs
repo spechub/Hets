c7b3ce5a2a1f81bfa99845d00c33f75252329f74
Felix Gabriel Mance <f.mance@jacobs-university.de>
2011-07-05 14:44:21 +0000

    Parsed literals and annotations git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@15452 cec4b9c1-7d33-0410-9eda-942365e851bb

--- OWL2/XML.hs
+++ OWL2/XML.hs
@@ -6,6 +6,8 @@ import Data.Maybe
 import OWL2.AS
 import OWL2.MS
 
+import Data.List
+
 entityList :: [String]
 entityList = ["Class", "Datatype", "NamedIndividual", 
     "ObjectProperty", "DataProperty", "AnnotationProperty"]
@@ -13,8 +15,8 @@ entityList = ["Class", "Datatype", "NamedIndividual",
 isEntity :: Text.XML.Light.QName -> Bool
 isEntity (QName {qName = qn}) = elem qn entityList
 
-isDecl :: Text.XML.Light.QName -> Bool
-isDecl (QName {qName = qn}) = qn == "Declaration"
+isSmth :: String -> Text.XML.Light.QName -> Bool
+isSmth s (QName {qName = qn}) = qn == s
 
 getEntityType :: String -> EntityType
 getEntityType ty = case ty of
@@ -39,8 +41,48 @@ getEntities :: Element -> [Entity]
 getEntities e = map toEntity $ filterElementsName isEntity e
 
 getDeclarations :: Element -> [Entity]
-getDeclarations e = concatMap getEntities $ catMaybes $ map (filterChildName isEntity) 
-            $ filterElementsName isDecl e
+getDeclarations e = concatMap getEntities $ catMaybes 
+      $ map (filterChildName isEntity) 
+            $ filterElementsName (isSmth "Declaration") e
+
+languageTag :: Text.XML.Light.QName
+languageTag = QName {qName = "lang", qURI = Nothing, qPrefix = Just "xml"}
+
+datatypeIRI :: Text.XML.Light.QName
+datatypeIRI = QName {qName = "datatypeIRI", qURI = Nothing, qPrefix = Nothing}
+
+isPlainLiteral :: String -> Bool
+isPlainLiteral s = "PlainLiteral" == drop (length s - 12) s
+
+getLiteral :: Element -> Literal
+getLiteral e = let lit = fromJust $ filterElementName (isSmth "Literal") e 
+                   lf = strContent e
+                   dt = fromJust $ findAttr datatypeIRI lit
+               in
+                  case findAttr languageTag lit of
+                    Just lang -> Literal lf (Untyped $ Just lang)
+                    Nothing -> if isPlainLiteral dt then
+                                  Literal lf (Untyped Nothing)
+                                else Literal lf (Typed $ mkQName dt)
+
+getValue :: Element -> AnnotationValue
+getValue e = let lit = filterElementName (isSmth "Literal") e
+                 val = strContent e
+             in case lit of
+                  Nothing -> AnnValue $ mkQName val
+                  Just _ -> AnnValLit $ getLiteral e
+
+getAnnotation :: Element -> Annotation
+getAnnotation (Element {elContent = c}) = 
+     let hd = onlyElems $ take (length c - 2) c
+         [ap, av] = onlyElems $ drop (length c - 2) c 
+     in
+         Annotation (getAnnotations hd) (getIRI ap) (getValue av)
+
+getAnnotations :: [Element] -> [Annotation]
+getAnnotations e = map getAnnotation $ concatMap (filterElementsName (isSmth "Annotation")) e
+
+
 
 
 
