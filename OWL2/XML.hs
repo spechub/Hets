365022b0933f402818ed3ec8544568ad9f41e9b6
Felix Gabriel Mance <f.mance@jacobs-university.de>
2011-07-06 13:43:18 +0000

    descriptions parsed git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@15466 cec4b9c1-7d33-0410-9eda-942365e851bb

--- OWL2/XML.hs
+++ OWL2/XML.hs
@@ -1,12 +1,10 @@
 module OWL2.XML where
 
-import Common.DocUtils
 import Text.XML.Light
 import Data.Maybe
 import OWL2.AS
 import OWL2.MS
-
-import Data.List
+import Common.Lexer
 
 entityList :: [String]
 entityList = ["Class", "Datatype", "NamedIndividual",
@@ -26,11 +24,18 @@ getEntityType ty = case ty of
     "ObjectProperty" -> ObjectProperty
     "DataProperty" -> DataProperty
     "AnnotationProperty" -> AnnotationProperty
+    _ -> error "not entity type"
 
 getIRI :: Element -> OWL2.AS.QName
 getIRI (Element {elAttribs = a}) =
         let Attr {attrVal = iri} = head a
         in mkQName iri
+
+getInt :: Element -> Int
+getInt (Element {elAttribs = a}) =
+        let Attr {attrVal = int} = head a
+        in value 10 int
+
 
 getName :: Element -> String
 getName (Element {elName = QName {qName = n}}) = n
@@ -42,16 +47,16 @@ getEntity :: Element -> Entity
 getEntity e = toEntity $ fromJust $ filterElementName isEntity e
 
 getDeclaration :: Element -> Frame
-getDeclaration e = 
+getDeclaration e =
    let ent = fromJust $ filterChildName isEntity e
        ans = getAllAnnos e
    in Frame (Right $ toEntity ent) [AnnFrameBit ans AnnotationFrameBit]
 
 getDeclarations :: Element -> [Frame]
-getDeclarations e = 
+getDeclarations e =
    let dcl = filterElementsName (isSmth "Declaration") e
    in map getDeclaration dcl
-     
+
 isPlainLiteral :: String -> Bool
 isPlainLiteral s = "PlainLiteral" == drop (length s - 12) s
 
@@ -93,10 +98,11 @@ getAllAnnos :: Element -> [Annotation]
 getAllAnnos e = map getAnnotation
             $ filterElementsName (isSmth "Annotation") e
 
-getObjProp :: Element -> ObjectPropertyExpression  --still need to cancel double inverses
+ -- still need to cancel double inverses
+getObjProp :: Element -> ObjectPropertyExpression
 getObjProp e = case filterElementName (isSmth "ObjectInverseOf") e of
                   Nothing -> ObjectProp $ getIRI e
-                  Just o -> ObjectInverseOf $ getObjProp $ head $ elChildren e
+                  Just _ -> ObjectInverseOf $ getObjProp $ head $ elChildren e
 
 getFacetValuePair :: Element -> (ConstrainingFacet, RestrictionValue)
 getFacetValuePair e = (getIRI e, getLiteral $ head $ elChildren e)
@@ -104,7 +110,7 @@ getFacetValuePair e = (getIRI e, getLiteral $ head $ elChildren e)
 getDataRange :: Element -> DataRange
 getDataRange e = case getName e of
     "Datatype" -> DataType (getIRI e) []
-    "DatatypeRestriction" -> 
+    "DatatypeRestriction" ->
         let dt = getIRI $ fromJust $ filterChildName (isSmth "Datatype") e
             fvp = map getFacetValuePair
                $ filterChildrenName (isSmth "FacetRestriction") e
@@ -117,13 +123,83 @@ getDataRange e = case getName e of
             $ map getDataRange $ elChildren e
     "DataUnionOf" -> DataJunction UnionOf
             $ map getDataRange $ elChildren e
+    _ -> error "XML parser: not data range"
 
 getClassExpression :: Element -> ClassExpression
 getClassExpression e = case getName e of
     "Class" -> Expression $ getIRI e
-    
-
-
+    "ObjectIntersectionOf" -> ObjectJunction IntersectionOf
+            $ map getClassExpression $ elChildren e
+    "ObjectUnionOf" -> ObjectJunction UnionOf
+            $ map getClassExpression $ elChildren e
+    "ObjectComplementOf" -> ObjectComplementOf
+            $ getClassExpression $ head $ elChildren e
+    "ObjectOneOf" -> ObjectOneOf
+            $ map getIRI $ elChildren e
+    "ObjectSomeValuesFrom" -> let ch = elChildren e in
+        ObjectValuesFrom SomeValuesFrom
+            (getObjProp $ head ch) (getClassExpression $ last ch)
+    "ObjectAllValuesFrom" -> let ch = elChildren e in
+        ObjectValuesFrom AllValuesFrom
+            (getObjProp $ head ch) (getClassExpression $ last ch)
+    "ObjectHasValue" -> let ch = elChildren e in
+        ObjectHasValue (getObjProp $ head ch) (getIRI $ last ch)
+    "ObjectHasSelf" -> ObjectHasSelf $ getObjProp $ head $ elChildren e
+    "ObjectMinCardinality" -> let ch = elChildren e in
+        if length ch == 2 then
+          ObjectCardinality $ Cardinality
+              MinCardinality (getInt e) (getObjProp $ head ch)
+                $ Just $ getClassExpression $ last ch
+         else ObjectCardinality $ Cardinality
+              MinCardinality (getInt e) (getObjProp $ head ch) Nothing
+    "ObjectMaxCardinality" -> let ch = elChildren e in
+        if length ch == 2 then
+          ObjectCardinality $ Cardinality
+              MaxCardinality (getInt e) (getObjProp $ head ch)
+                $ Just $ getClassExpression $ last ch
+         else ObjectCardinality $ Cardinality
+              MaxCardinality (getInt e) (getObjProp $ head ch) Nothing
+    "ObjectExactCardinality" -> let ch = elChildren e in
+        if length ch == 2 then
+          ObjectCardinality $ Cardinality
+              ExactCardinality (getInt e) (getObjProp $ head ch)
+                $ Just $ getClassExpression $ last ch
+         else ObjectCardinality $ Cardinality
+              ExactCardinality (getInt e) (getObjProp $ head ch) Nothing
+    "DataSomeValuesFrom" -> 
+        let ch = elChildren e 
+            dp = map getIRI $ init ch
+            dr = last ch
+        in DataValuesFrom SomeValuesFrom (head dp) (tail dp) (getDataRange dr)
+    "DataAllValuesFrom" -> 
+        let ch = elChildren e 
+            dp = map getIRI $ init ch
+            dr = last ch
+        in DataValuesFrom AllValuesFrom (head dp) (tail dp) (getDataRange dr)
+    "DataHasValue" -> let ch = elChildren e in
+        DataHasValue (getIRI $ head ch) (getLiteral $ last ch)
+    "DataMinCardinality" -> let ch = elChildren e in
+        if length ch == 2 then
+          DataCardinality $ Cardinality
+              MinCardinality (getInt e) (getIRI $ head ch)
+                $ Just $ getDataRange $ last ch
+         else DataCardinality $ Cardinality
+              MinCardinality (getInt e) (getIRI $ head ch) Nothing
+    "DataMaxCardinality" -> let ch = elChildren e in
+        if length ch == 2 then
+          DataCardinality $ Cardinality
+              MaxCardinality (getInt e) (getIRI $ head ch)
+                $ Just $ getDataRange $ last ch
+         else DataCardinality $ Cardinality
+              MaxCardinality (getInt e) (getIRI $ head ch) Nothing
+    "DataExactCardinality" -> let ch = elChildren e in
+        if length ch == 2 then
+          DataCardinality $ Cardinality
+              ExactCardinality (getInt e) (getIRI $ head ch)
+                $ Just $ getDataRange $ last ch
+         else DataCardinality $ Cardinality
+              ExactCardinality (getInt e) (getIRI $ head ch) Nothing
+    _ -> error "XML parser: not ClassExpression"
 
 
 
