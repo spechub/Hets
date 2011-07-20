c77c0efe19dc6556ac872828bfb4cfc5fbca5ac5
Felix Gabriel Mance <f.mance@jacobs-university.de>
2011-07-20 14:48:42 +0000

    scanned git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@15620 cec4b9c1-7d33-0410-9eda-942365e851bb

--- OWL2/AS.hs
+++ OWL2/AS.hs
@@ -95,164 +95,6 @@ type AnnotationProperty = IRI
 type NamedIndividual = IRI
 type Individual = IRI
 
--- * LITERALS
-
-data TypedOrUntyped = Typed Datatype | Untyped (Maybe LanguageTag)
-    deriving (Show, Eq, Ord)
-
-data Literal = Literal LexicalForm TypedOrUntyped
-    deriving (Show, Eq, Ord)
-
-cTypeS :: String
-cTypeS = "^^"
-
--- * PROPERTY EXPRESSIONS
-
-type InverseObjectProperty = ObjectPropertyExpression
-
-data ObjectPropertyExpression = ObjectProp ObjectProperty
-  | ObjectInverseOf InverseObjectProperty
-        deriving (Show, Eq, Ord)
-
-type DataPropertyExpression = DataProperty
-
--- | data type strings (some are not listed in the grammar)
-datatypeKeys :: [String]
-datatypeKeys =
-  [ booleanS
-  , dATAS
-  , decimalS
-  , floatS
-  , integerS
-  , negativeIntegerS
-  , nonNegativeIntegerS
-  , nonPositiveIntegerS
-  , positiveIntegerS
-  , stringS
-  , universalS
-  ]
-
-isDatatypeKey :: IRI -> Bool
-isDatatypeKey u =
-  elem (localPart u) datatypeKeys && elem (namePrefix u) ["", "xsd"]
-
--- * DATA RANGES
-
-data DatatypeFacet =
-    LENGTH
-  | MINLENGTH
-  | MAXLENGTH
-  | PATTERN
-  | MININCLUSIVE
-  | MINEXCLUSIVE
-  | MAXINCLUSIVE
-  | MAXEXCLUSIVE
-  | TOTALDIGITS
-  | FRACTIONDIGITS
-    deriving (Show, Eq, Ord)
-
-showFacet :: DatatypeFacet -> String
-showFacet df = case df of
-    LENGTH -> lengthS
-    MINLENGTH -> minLengthS
-    MAXLENGTH -> maxLengthS
-    PATTERN -> patternS
-    MININCLUSIVE -> lessEq
-    MINEXCLUSIVE -> lessS
-    MAXINCLUSIVE -> greaterEq
-    MAXEXCLUSIVE -> greaterS
-    TOTALDIGITS -> digitsS
-    FRACTIONDIGITS -> fractionS
-
-data DataRange
-        = DataType Datatype [(ConstrainingFacet, RestrictionValue)]
-        | DataJunction JunctionType [DataRange]
-          -- at least two elements in the list
-        | DataComplementOf DataRange
-        | DataOneOf [Literal]   -- at least one element in the list
-        deriving (Show, Eq, Ord)
-
-data JunctionType = UnionOf | IntersectionOf deriving (Show, Eq, Ord)
-
-type ConstrainingFacet = IRI
-type RestrictionValue = Literal
-
--- * CLASS EXPERSSIONS
-
-data QuantifierType = AllValuesFrom | SomeValuesFrom deriving (Show, Eq, Ord)
-
-showQuantifierType :: QuantifierType -> String
-showQuantifierType ty = case ty of
-    AllValuesFrom -> onlyS
-    SomeValuesFrom -> someS
-
-data CardinalityType = MinCardinality | MaxCardinality | ExactCardinality
-    deriving (Show, Eq, Ord)
-
-showCardinalityType :: CardinalityType -> String
-showCardinalityType ty = case ty of
-    MinCardinality -> minS
-    MaxCardinality -> maxS
-    ExactCardinality -> exactlyS
-
-data Cardinality a b = Cardinality CardinalityType Int a (Maybe b)
-    deriving (Show, Eq, Ord)
-
-data ClassExpression =
-    Expression Class
-  | ObjectJunction JunctionType [ClassExpression]  -- min. 2 ClassExpressions
-  | ObjectComplementOf ClassExpression
-  | ObjectOneOf [Individual]  -- min. 1 Individual
-  | ObjectValuesFrom QuantifierType ObjectPropertyExpression ClassExpression
-  | ObjectHasValue ObjectPropertyExpression Individual
-  | ObjectHasSelf ObjectPropertyExpression
-  | ObjectCardinality (Cardinality ObjectPropertyExpression ClassExpression)
-  | DataValuesFrom QuantifierType
-       DataPropertyExpression DataRange
-  | DataHasValue DataPropertyExpression Literal
-  | DataCardinality (Cardinality DataPropertyExpression DataRange)
-    deriving (Show, Eq, Ord)
-
--- * ANNOTATIONS
-
-data Annotation = Annotation [Annotation] AnnotationProperty AnnotationValue
-  deriving (Show, Eq, Ord)
-
-data AnnotationValue
-        = AnnValue IRI
-        | AnnValLit Literal
-          deriving (Show, Eq, Ord)
-
--- * ENTITIES
-
-data Entity = Entity EntityType IRI deriving (Show, Eq, Ord)
-
-instance GetRange Entity where
-  getRange (Entity _ iri) = iriPos iri
-
-data EntityType =
-    Datatype
-  | Class
-  | ObjectProperty
-  | DataProperty
-  | AnnotationProperty
-  | NamedIndividual
-    deriving (Enum, Bounded, Show, Read, Eq, Ord)
-
-showEntityType :: EntityType -> String
-showEntityType e = case e of
-    Datatype -> datatypeC
-    Class -> classC
-    ObjectProperty -> objectPropertyC
-    DataProperty -> dataPropertyC
-    AnnotationProperty -> annotationPropertyC
-    NamedIndividual -> individualC
-
-entityTypes :: [EntityType]
-entityTypes = [minBound .. maxBound]
-
--- * CASES
-
 type SourceIndividual = Individual
 type TargetIndividual = Individual
 type TargetValue = Literal
@@ -329,3 +171,159 @@ showSameOrDifferent sd = case sd of
     Different -> differentFromC
 
 data PositiveOrNegative = Positive | Negative deriving (Show, Eq, Ord)
+
+data QuantifierType = AllValuesFrom | SomeValuesFrom deriving (Show, Eq, Ord)
+
+showQuantifierType :: QuantifierType -> String
+showQuantifierType ty = case ty of
+    AllValuesFrom -> onlyS
+    SomeValuesFrom -> someS
+
+-- | data type strings (some are not listed in the grammar)
+datatypeKeys :: [String]
+datatypeKeys =
+  [ booleanS
+  , dATAS
+  , decimalS
+  , floatS
+  , integerS
+  , negativeIntegerS
+  , nonNegativeIntegerS
+  , nonPositiveIntegerS
+  , positiveIntegerS
+  , stringS
+  , universalS
+  ]
+
+isDatatypeKey :: IRI -> Bool
+isDatatypeKey u =
+  elem (localPart u) datatypeKeys && elem (namePrefix u) ["", "xsd"]
+
+data DatatypeFacet =
+    LENGTH
+  | MINLENGTH
+  | MAXLENGTH
+  | PATTERN
+  | MININCLUSIVE
+  | MINEXCLUSIVE
+  | MAXINCLUSIVE
+  | MAXEXCLUSIVE
+  | TOTALDIGITS
+  | FRACTIONDIGITS
+    deriving (Show, Eq, Ord)
+
+showFacet :: DatatypeFacet -> String
+showFacet df = case df of
+    LENGTH -> lengthS
+    MINLENGTH -> minLengthS
+    MAXLENGTH -> maxLengthS
+    PATTERN -> patternS
+    MININCLUSIVE -> lessEq
+    MINEXCLUSIVE -> lessS
+    MAXINCLUSIVE -> greaterEq
+    MAXEXCLUSIVE -> greaterS
+    TOTALDIGITS -> digitsS
+    FRACTIONDIGITS -> fractionS
+
+-- * LITERALS
+
+data TypedOrUntyped = Typed Datatype | Untyped (Maybe LanguageTag)
+    deriving (Show, Eq, Ord)
+
+data Literal = Literal LexicalForm TypedOrUntyped
+    deriving (Show, Eq, Ord)
+
+cTypeS :: String
+cTypeS = "^^"
+
+-- * PROPERTY EXPRESSIONS
+
+type InverseObjectProperty = ObjectPropertyExpression
+
+data ObjectPropertyExpression = ObjectProp ObjectProperty
+  | ObjectInverseOf InverseObjectProperty
+        deriving (Show, Eq, Ord)
+
+type DataPropertyExpression = DataProperty
+
+-- * DATA RANGES
+
+data DataRange
+        = DataType Datatype [(ConstrainingFacet, RestrictionValue)]
+        | DataJunction JunctionType [DataRange]
+          -- at least two elements in the list
+        | DataComplementOf DataRange
+        | DataOneOf [Literal]   -- at least one element in the list
+        deriving (Show, Eq, Ord)
+
+data JunctionType = UnionOf | IntersectionOf deriving (Show, Eq, Ord)
+
+type ConstrainingFacet = IRI
+type RestrictionValue = Literal
+
+-- * CLASS EXPERSSIONS
+
+data CardinalityType = MinCardinality | MaxCardinality | ExactCardinality
+    deriving (Show, Eq, Ord)
+
+showCardinalityType :: CardinalityType -> String
+showCardinalityType ty = case ty of
+    MinCardinality -> minS
+    MaxCardinality -> maxS
+    ExactCardinality -> exactlyS
+
+data Cardinality a b = Cardinality CardinalityType Int a (Maybe b)
+    deriving (Show, Eq, Ord)
+
+data ClassExpression =
+    Expression Class
+  | ObjectJunction JunctionType [ClassExpression]  -- min. 2 ClassExpressions
+  | ObjectComplementOf ClassExpression
+  | ObjectOneOf [Individual]  -- min. 1 Individual
+  | ObjectValuesFrom QuantifierType ObjectPropertyExpression ClassExpression
+  | ObjectHasValue ObjectPropertyExpression Individual
+  | ObjectHasSelf ObjectPropertyExpression
+  | ObjectCardinality (Cardinality ObjectPropertyExpression ClassExpression)
+  | DataValuesFrom QuantifierType
+       DataPropertyExpression DataRange
+  | DataHasValue DataPropertyExpression Literal
+  | DataCardinality (Cardinality DataPropertyExpression DataRange)
+    deriving (Show, Eq, Ord)
+
+-- * ANNOTATIONS
+
+data Annotation = Annotation [Annotation] AnnotationProperty AnnotationValue
+  deriving (Show, Eq, Ord)
+
+data AnnotationValue
+    = AnnValue IRI
+    | AnnValLit Literal
+          deriving (Show, Eq, Ord)
+
+-- * ENTITIES
+
+data Entity = Entity EntityType IRI deriving (Show, Eq, Ord)
+
+instance GetRange Entity where
+  getRange (Entity _ iri) = iriPos iri
+
+data EntityType =
+    Datatype
+  | Class
+  | ObjectProperty
+  | DataProperty
+  | AnnotationProperty
+  | NamedIndividual
+    deriving (Enum, Bounded, Show, Read, Eq, Ord)
+
+showEntityType :: EntityType -> String
+showEntityType e = case e of
+    Datatype -> datatypeC
+    Class -> classC
+    ObjectProperty -> objectPropertyC
+    DataProperty -> dataPropertyC
+    AnnotationProperty -> annotationPropertyC
+    NamedIndividual -> individualC
+
+entityTypes :: [EntityType]
+entityTypes = [minBound .. maxBound]
