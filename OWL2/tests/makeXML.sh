0c7f021530604c5c86674f8e8c3390387a50139f
Felix Gabriel Mance <f.mance@jacobs-university.de>
2011-07-20 17:13:32 +0000

    some more tests git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@15634 cec4b9c1-7d33-0410-9eda-942365e851bb

--- OWL2/tests/makeXML.sh
+++ OWL2/tests/makeXML.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 
-for i in *.rdf
+for i in *.rdf pizza.owl
 do
-    java -jar ../OWL2Parser.jar file://`pwd`/$i xml >> `pwd`/XML/$i.xml
+    java -jar ../OWL2Parser.jar file:`pwd`/$i xml >> `pwd`/XML/$i.xml
 done
 
 cd ../..
 
-make OWL2/runConv
+make OWL2/scripts/runConv
 
 for i in OWL2/tests/XML/*.xml
 do
-    OWL2/runConv $i >> $i.xml2
+    OWL2/scripts/runConv $i >> $i.xml
 done
+
+for i in OWL2/tests/XML/*.xml.xml
+do
+    OWL2/scripts/runConv $i >> $i.xml
+done
+
+cd OWL2/tests/XML
+
+for i in *.rdf.xml.xml
+do
+    diff $i $i.xml
+done
+
+
