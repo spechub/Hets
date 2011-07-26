4440f5c4ab1cb6dfd445da97f87a72d87d24c25a
Felix Gabriel Mance <f.mance@jacobs-university.de>
2011-07-26 21:46:24 +0000

    some more sublogic analysis added git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@15729 cec4b9c1-7d33-0410-9eda-942365e851bb

--- OWL2/Profiles.hs
+++ OWL2/Profiles.hs
@@ -52,11 +52,20 @@ topProfile = [True, True, True, True, True, True, True]
 el :: CoreProfiles
 el = [True, False, False]
 
+ql :: CoreProfiles
+ql = [False, True, False]
+
 rl :: CoreProfiles
 rl = [False, False, True]
 
 elrl :: CoreProfiles
-elrl = [True, False, True] 
+elrl = [True, False, True]
+
+elql :: CoreProfiles
+elql = [True, True, False]
+
+qlrl :: CoreProfiles
+qlrl = [False, True, True]
 
 subClass :: ClassExpression -> AllProfiles
 subClass cex = case cex of
@@ -65,39 +74,43 @@ subClass cex = case cex of
         IntersectionOf -> minimalCovering elrl $ map subClass cel
         UnionOf -> minimalCovering rl $ map subClass cel
     ObjectOneOf _ -> computeAll elrl
-    ObjectValuesFrom qt _ ce -> case qt of
-        SomeValuesFrom -> case ce of
-            Expression c -> if isThing c then topProfile
-                             else computeAll elrl
-            _ -> computeAll elrl
-        _ -> bottomProfile
+    ObjectValuesFrom SomeValuesFrom _ ce -> case ce of
+        Expression c -> if isThing c then topProfile
+                         else computeAll elrl
+        _ -> computeAll elrl
     ObjectHasValue _ _ -> computeAll elrl
     ObjectHasSelf _ -> computeAll el
     DataHasValue _ _ -> computeAll elrl
     DataValuesFrom SomeValuesFrom _ dr -> bottomProfile -- dataRange dr
     _ -> bottomProfile
-{-
-validSuperClassRL :: ClassExpression -> Bool
-validSuperClassRL cex = case cex of
-    Expression c -> (not . isThing) c
-    ObjectJunction IntersectionOf  cel -> all validSuperClassRL cel
-    ObjectComplementOf ce -> validSubClassRL ce
-    ObjectValuesFrom AllValuesFrom _ ce -> validSuperClassRL ce
-    ObjectHasValue _ _ -> True
-    ObjectCardinality (Cardinality MaxCardinality i _ mce) -> elem i [0, 1] &&
-        (case mce of
-            Nothing -> True
-            Just ce -> case ce of
-                Expression c -> isThing c
-                _ -> validSubClassRL ce)
-    DataValuesFrom AllValuesFrom _ dr -> validDataRangeRL dr
+
+superClass :: ClassExpression -> AllProfiles
+superClass cex = case cex of
+    Expression c -> computeAll [True, True, (not . isThing) c]
+    ObjectJunction IntersectionOf cel -> anaTable $ map superClass cel
+    ObjectComplementOf ce -> minimalCovering qlrl [subClass ce]
+    ObjectOneOf _ -> computeAll el
+    ObjectValuesFrom qt _ ce -> case qt of
+        SomeValuesFrom -> minimalCovering elql [subClass ce]  -- !! check this
+        AllValuesFrom -> minimalCovering rl [superClass ce]
+    ObjectHasValue _ _ -> computeAll elrl
+    ObjectCardinality (Cardinality MaxCardinality i _ mce) ->
+        let tf = elem i [0, 1]
+        in minimalCovering rl [[tf, tf, tf, tf, tf, tf, tf],
+            (case mce of
+                Nothing -> topProfile
+                Just ce -> case ce of
+                    Expression c -> topProfile
+                    _ -> subClass ce)]
+ {-   DataValuesFrom AllValuesFrom _ dr -> validDataRangeRL dr
     DataHasValue _ _ -> True
     DataCardinality (Cardinality MaxCardinality i _ mdr) -> elem i [0, 1] &&
         (case mdr of
             Nothing -> True
             Just dr -> validDataRangeRL dr)
     _ -> False
-
+-}
+{-
 validEquivClassRL :: ClassExpression -> Bool
 validEquivClassRL cex = case cex of
     Expression c -> (not . isThing) c
