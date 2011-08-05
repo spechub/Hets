c2bdfdf561d0cef7802547f13943cd5e56788ac7
Christian Maeder <Christian.Maeder@dfki.de>
2011-08-05 10:06:18 +0000

    more flexible instances for ghc-7.2 git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@15887 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Comorphisms/CommonLogic2CASL.hs
+++ Comorphisms/CommonLogic2CASL.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE MultiParamTypeClasses, TypeSynonymInstances #-}
+{-# LANGUAGE MultiParamTypeClasses, TypeSynonymInstances, FlexibleInstances #-}
 {- |
 Module      :  $Header$
 Description :  Comorphism from CommonLogic to CASL
--- Comorphisms/Maude2CASL.hs
+++ Comorphisms/Maude2CASL.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE MultiParamTypeClasses, TypeSynonymInstances #-}
+{-# LANGUAGE MultiParamTypeClasses, TypeSynonymInstances, FlexibleInstances #-}
 {- |
 Module      :  $Header$
 Description :  Coding of Maude with preorder semantics into CASL
