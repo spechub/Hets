74ac9f7f4f843873c44f03bf431a1dd46e06e02f
Christian Maeder <Christian.Maeder@dfki.de>
2011-08-25 11:29:42 +0000

    avoid mention of OWL1 git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@16024 cec4b9c1-7d33-0410-9eda-942365e851bb

--- OWL2/Taxonomy.hs
+++ OWL2/Taxonomy.hs
@@ -16,7 +16,7 @@ module OWL2.Taxonomy ( onto2Tax ) where
 import OWL2.MS
 import OWL2.Sign
 import OWL2.ManchesterPrint
-import OWL.ProvePellet
+import OWL2.ProvePellet
 
 import Common.AS_Annotation
 import Common.Result
