216af207343c6ee15605f8bff18447faa47968e5
Simon Ulbricht <tekknix@informatik.uni-bremen.de>
2011-09-05 14:21:47 +0000

    initial version git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@16089 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ Static/testApplyDiff.hs
@@ -0,0 +1,26 @@
+
+import Static.ApplyXmlDiff
+import System.Environment
+import Text.XML.Light (parseXMLDoc)
+
+main :: IO()
+main = do
+  args <- getArgs
+  case args of
+    (p1 : p2 : _) -> testDiff p1 p2
+    _ -> do
+      putStrLn "please enter xml location!"
+      p1 <- getLine
+      putStrLn "please enter diff location!"
+      p2 <- getLine
+      testDiff p1 p2
+
+testDiff :: FilePath -> FilePath -> IO()
+testDiff p1 p2 = do
+      xml <- readFile p1
+      diff <- readFile p2
+      case parseXMLDoc xml of
+        Just xml1 -> do
+          xml2 <- applyXmlDiff xml1 diff
+          writeFile (p1 ++ "-output") $ show xml2
+        _ -> fail "failed to parse xml-file"
