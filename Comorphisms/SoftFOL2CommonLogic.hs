84bc6a2ac0cdfcfbb2cfe467185590da9bc7f1da
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2011-09-07 10:46:41 +0000

    worked on SoftFOL2CL sentence translation git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@16101 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Comorphisms/SoftFOL2CommonLogic.hs
+++ Comorphisms/SoftFOL2CommonLogic.hs
@@ -88,7 +88,7 @@ mapMor mor = undefined {-
       pmp = PMor.propMap mor
   in  return $ ClMor.Morphism src tgt pmp
 -}
-  
+
 mapSentence :: FOLSign.Sign -> FOLSign.Sentence -> Result TEXT
 mapSentence _ f = return $ translate f
 
@@ -96,94 +96,142 @@ mapSign :: FOLSign.Sign -> ClSign.Sign
 mapSign sig = undefined
 --  ClSign.unite (ClSign.Sign (PSign.items sig) (PSign.items sig)) $ baseSig
 
+-- | translates FOL-theories to CL-theories keeping their names
 mapTheory :: (FOLSign.Sign, [AS_Anno.Named FOLSign.Sentence])
              -> Result (ClSign.Sign, [AS_Anno.Named TEXT])
-mapTheory (srcSign, srcFormulas) = undefined {-
+mapTheory (srcSign, srcFormulas) = 
   return (mapSign srcSign,
         map ((uncurry AS_Anno.makeNamed) . elimModSnd . senAndName) srcFormulas)
-  where senAndName :: AS_Anno.Named PBasic.FORMULA -> (String, PBasic.FORMULA)
+  where senAndName :: AS_Anno.Named FOLSign.Sentence -> (String, FOLSign.Sentence)
         senAndName f = (AS_Anno.senAttr f, AS_Anno.sentence f)
-        elimModSnd :: (String, PBasic.FORMULA) -> (String, TEXT)
+        elimModSnd :: (String, FOLSign.Sentence) -> (String, TEXT)
         elimModSnd (s, f) = (s, translate f)
--}
 
 translate :: FOLSign.Sentence -> TEXT
 translate f = Text [Sentence $ toSen f] nullRange
 
 toSen :: FOLSign.SPTerm -> SENTENCE
 toSen t = case t of
-  FOLSign.SPQuantTerm qsym vl f -> quantTerm qsym vl f
-  FOLSign.SPComplexTerm sym args ->
-    Atom_sent (Atom (symToTerm sym) (map sptermToTermSeq args)) nullRange
+  FOLSign.SPQuantTerm qsym vl f -> quantTrm qsym vl f
+  FOLSign.SPComplexTerm sym args -> case sym of
+    FOLSign.SPEqual -> case args of
+        [x,y] -> toEquation (x,y)
+        l@(_:_:_) -> Bool_sent (
+                        Conjunction $ map toEquation $ zip l $ tail l
+                    ) nullRange
+        x -> error $ "equation needs at least two arguments, but found: " ++ show x
+    FOLSign.SPTrue -> clTrue
+    FOLSign.SPFalse -> clFalse
+    FOLSign.SPOr -> Bool_sent (Disjunction $ map toSen args) nullRange
+    FOLSign.SPAnd -> Bool_sent (Conjunction $ map toSen args) nullRange
+    FOLSign.SPNot -> case args of
+        [x] -> Bool_sent (Negation $ toSen x) nullRange
+        x -> error $
+              "negation can only be applied to a single argument, but found: "
+              ++ show x
+    FOLSign.SPImplies -> case args of
+        [x,y] -> Bool_sent (Implication (toSen x) (toSen y)) nullRange
+        x -> error $
+              "implication can only be applied to two arguments, but found: "
+              ++ show x
+    FOLSign.SPImplied -> case args of -- --------------****** what is SPImplied?
+        [x,y] -> Bool_sent (Implication (toSen y) (toSen x)) nullRange
+        x -> error $
+              "implication can only be applied to two arguments, but found: "
+              ++ show x
+    FOLSign.SPEquiv ->  case args of
+        [x,y] -> Bool_sent (Biconditional (toSen x) (toSen y)) nullRange
+        x -> error $
+              "equivalence can only be applied to two arguments, but found: "
+              ++ show x
+    FOLSign.SPID -> predicate (Name_term idName) args
+    FOLSign.SPDiv -> predicate (Name_term divName) args
+    FOLSign.SPComp -> predicate (Name_term compName) args
+    FOLSign.SPSum -> predicate (Name_term sumName) args
+    FOLSign.SPConv -> predicate (Name_term convName) args
+    FOLSign.SPCustomSymbol _ -> predicate (symToTerm sym) args
 
-quantTerm :: FOLSign.SPQuantSym -> [FOLSign.SPTerm] -> FOLSign.SPTerm
-             -> SENTENCE
-quantTerm qsymm vl f =
+-- | converts a quantified FOL-term to a CL-Quant_sent
+quantTrm :: FOLSign.SPQuantSym -> [FOLSign.SPTerm] -> FOLSign.SPTerm -> SENTENCE
+quantTrm qsymm vl f =
   let trans_vl = map sptermToNameSeq vl
       trans_f = toSen f
       quantifier = case qsymm of
         FOLSign.SPForall -> Universal
         FOLSign.SPExists -> Existential
-        _ -> undefined -- is caught below
-  in case qsymm of 
-        FOLSign.SPCustomQuantSym cq -> 
-            Irregular_sent (
-              Comment_sent
-                (Comment "customQuantifier" nullRange)
-                (undefined)
-                nullRange
-            ) nullRange
-        _ -> Quant_sent (quantifier trans_vl trans_f) nullRange
+        _ -> error "custom quantifiers not allowed"
+  in Quant_sent (quantifier trans_vl trans_f) nullRange
 
-symToSen :: FOLSign.SPSymbol -> SENTENCE
-symToSen s = case s of
-  FOLSign.SPEqual -> undefined   -- =
-  FOLSign.SPTrue -> undefined
-  FOLSign.SPFalse -> undefined
-  FOLSign.SPOr -> undefined
-  FOLSign.SPAnd -> undefined
-  FOLSign.SPNot -> undefined
-  FOLSign.SPImplies -> undefined --  =>
-  FOLSign.SPImplied -> undefined -- <=
-  FOLSign.SPEquiv -> undefined   -- <=>
-  FOLSign.SPID -> undefined      -- ?
-  FOLSign.SPDiv -> undefined     -- ?
-  FOLSign.SPComp -> undefined    -- ?
-  FOLSign.SPSum -> undefined     -- ?
-  FOLSign.SPConv -> undefined    -- ?
-  FOLSign.SPCustomSymbol i -> undefined
+-- | converts SPEquation to an CL-Equation
+toEquation :: (FOLSign.SPTerm, FOLSign.SPTerm) -> SENTENCE
+toEquation (x,y) =
+  Atom_sent (Equation (sptermToTerm x) (sptermToTerm y)) nullRange
 
 symToTerm :: FOLSign.SPSymbol -> TERM
 symToTerm s = case s of
-  FOLSign.SPEqual -> undefined   -- =
-  FOLSign.SPTrue -> undefined
-  FOLSign.SPFalse -> undefined
-  FOLSign.SPOr -> undefined
-  FOLSign.SPAnd -> undefined
-  FOLSign.SPNot -> undefined
-  FOLSign.SPImplies -> undefined --  =>
-  FOLSign.SPImplied -> undefined -- <=
-  FOLSign.SPEquiv -> undefined   -- <=>
-  FOLSign.SPID -> undefined      -- ?
-  FOLSign.SPDiv -> undefined     -- ?
-  FOLSign.SPComp -> undefined    -- ?
-  FOLSign.SPSum -> undefined     -- ?
-  FOLSign.SPConv -> undefined    -- ?
   FOLSign.SPCustomSymbol i -> Name_term i
+  x -> error $ "symbol not allowed for a term: " ++ show x
 
+predicate :: TERM -> [FOLSign.SPTerm] -> SENTENCE
+predicate t args =
+  Atom_sent (Atom t (map sptermToTermSeq args)) nullRange
+
+-- converts an SPTerm to a TERM, i.e. for the arguments of an equation
+sptermToTerm :: FOLSign.SPTerm -> TERM
+sptermToTerm t = case t of
+  FOLSign.SPQuantTerm qsym vl f -> error "quantification not allowed for a term"
+  FOLSign.SPComplexTerm sym args -> case sym of
+      FOLSign.SPCustomSymbol i -> Name_term i
+      x -> error $ "symbol not allowed as a term: " ++ show x
+
+-- | converts an SPTerm to TERM_SEQ as an argument for a quantifier
 sptermToTermSeq :: FOLSign.SPTerm -> TERM_SEQ
-sptermToTermSeq t = undefined
+sptermToTermSeq t = case t of
+  FOLSign.SPComplexTerm sym [] -> Term_seq $ symToTerm sym
+  FOLSign.SPComplexTerm _ _ -> error "predicates not allowed in argument list"
+  FOLSign.SPQuantTerm _ _ _ ->
+      error "quantification not allowed in argument list"
 
+-- | converts an SPTerm to NAME_OR_SEQMARK as an argument for a predicate
 sptermToNameSeq :: FOLSign.SPTerm -> NAME_OR_SEQMARK
-sptermToNameSeq t = undefined
+sptermToNameSeq t = case t of
+  FOLSign.SPComplexTerm sym [] -> Name $ symToName sym
+  FOLSign.SPComplexTerm _ _ ->
+      error "predicates not allowed in argument list"
+  FOLSign.SPQuantTerm _ _ _ ->
+      error "quantification not allowed in argument list"
 
+-- | converts a custom symbol to a NAME, used in
+symToName :: FOLSign.SPSymbol -> NAME
+symToName s = case s of
+  FOLSign.SPCustomSymbol i -> i
+  x -> error $ "symbol not convertible to a name: " ++ show x
+
+-- representation for true in CL
 clTrue :: SENTENCE --forall x. x=x
 clTrue = Quant_sent (Universal [Name xName]
             $ Atom_sent (Equation (Name_term xName) (Name_term xName)) nullRange
           ) nullRange
 
+-- representation for false in CL
 clFalse :: SENTENCE
 clFalse = Bool_sent (Negation clTrue) nullRange
 
+-- simple names
 xName :: NAME
 xName = mkSimpleId "x"
+
+idName :: NAME
+idName = mkSimpleId "ID"
+
+divName :: NAME
+divName = mkSimpleId "DIV"
+
+compName :: NAME
+compName = mkSimpleId "COMP"
+
+sumName :: NAME
+sumName = mkSimpleId "SUM"
+
+convName :: NAME
+convName = mkSimpleId "CONV"
