0afc22ce685842b5a9ad8f4e8e9ed4759ce83f13
Christian Maeder <Christian.Maeder@dfki.de>
2011-09-27 12:01:26 +0000

    simplified pretty printing git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@16182 cec4b9c1-7d33-0410-9eda-942365e851bb

--- HolLight/Sign.hs
+++ HolLight/Sign.hs
@@ -1,8 +1,19 @@
+{- |
+Module      :  $Header$
+Description :  HolLight signature
+Copyright   :  (c) Jonathan von Schroeder, DFKI GmbH 2010
+License     :  GPLv2 or higher, see LICENSE.txt
+
+Maintainer  :  jonathan.von_schroeder@dfki.de
+Stability   :  experimental
+Portability :  portable
+
+-}
+
 module HolLight.Sign where
 
 import qualified Data.Set as Set
 import qualified Data.Map as Map
-import qualified Data.List as List
 import Common.DocUtils
 import Common.Doc
 import Common.Result
@@ -15,29 +26,23 @@ data Sign = Sign { types :: Map.Map String Int
   deriving (Eq, Ord, Show)
 
 pretty_types :: Map.Map String Int -> Doc
-pretty_types tps = hcat (List.intersperse (text ", ") (Map.foldWithKey (\k v a ->
-                              (hcat [text "arity_of(`:", text k,text "`)=",text (show v)]):a) [] tps))
+pretty_types = sepByCommas . map (\ (s, i) -> text s <> parens (pretty i))
+  . Map.toList
 
-pretty_type_list :: [HolType] -> Doc
-pretty_type_list tps = hcat (List.intersperse (text ", ")
-                              (map (\tp -> hcat
-  	                           [text "`:",pp_print_type tp,text "`"])
-  	                        tps))
+instance Pretty HolType where
+  pretty = pp_print_type
 
 instance Pretty Sign where
-  pretty s = let tps = types s in
-             let opsM = ops s
-             in hcat [text "Signature = { ", vcat $ (hcat [text "types = {",pretty_types tps,text "}"]):(
-               Map.foldWithKey (\k v a ->
-                 (hcat [text "type_of(",text k, text ") = {",
-                        pretty_type_list (Set.toList v),text "}"]):a) [] opsM
-             ), text " }"]
+  pretty s = keyword "types" <+> pretty_types (types s)
+    $++$ printMap id vcat (\ a b -> a <+> colon <+> b) (ops s)
 
 emptySig :: Sign
 emptySig = Sign{types = Map.empty, ops = Map.empty}
 
 isSubSig :: Sign -> Sign -> Bool
-isSubSig s1 s2 = ((types s1) `Map.isSubmapOf` (types s2)) && ((ops s1) `Map.isSubmapOf` (ops s2))
+isSubSig s1 s2 = types s1 `Map.isSubmapOf` types s2
+  && ops s1 `Map.isSubmapOf` ops s2
 
 sigUnion :: Sign -> Sign -> Result Sign
-sigUnion (Sign {types = t1,ops = o1}) (Sign {types = t2, ops = o2}) = return $ Sign {types=Map.union t1 t2,ops=Map.unionWith Set.union o1 o2}
+sigUnion (Sign {types = t1, ops = o1}) (Sign {types = t2, ops = o2}) =
+  return $ Sign {types=Map.union t1 t2, ops=Map.unionWith Set.union o1 o2}
