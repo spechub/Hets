1a10ac840725c1c1999b0cab11c27ebd305a9577
Christian Maeder <Christian.Maeder@dfki.de>
2011-10-17 09:02:34 +0000

    replaced OWL2 and ow2 with OWL and owl git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@16302 cec4b9c1-7d33-0410-9eda-942365e851bb

--- OWL2/tests/run.sh
+++ OWL2/tests/run.sh
@@ -10,23 +10,23 @@ done
 
 for i in *.rdf
 do
-  ../../hets -v2 -i ow2 -o th,pp.het,omn $i
+  ../../hets -v2 -i owl -o th,pp.het,omn $i
 done
 
 for i in *.rdf *.omn
 do
   java -jar ../OWL2Parser.jar file://`pwd`/$i $i.omnm
-  ../../hets -v2 -i ow2 -o th,pp.het,omn $i
+  ../../hets -v2 -i owl -o th,pp.het,omn $i
 done
 
 for i in *.het
 do
-  ../../hets -l OWL2 -v2 -o th,pp.het,omn $i
+  ../../hets -l OWL -v2 -o th,pp.het,omn $i
 done
 
 for i in *.th *.pp.het *.omn
 do
-  ../../hets -l OWL2 -v2 $i
+  ../../hets -l OWL -v2 $i
 done
 
 for i in *.omn
