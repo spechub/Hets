cc8254b76a129533491dcd45a08d20f6be5b949c
Simon Ulbricht <tekknix@informatik.uni-bremen.de>
2011-10-19 14:26:54 +0000

    implemented morphism update for links if necessary and thmlink updates. updateDG should run now, however is neither tested nor very fine. in any update case, the old element will be completely overwritten. git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@16338 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Static/ApplyChanges.hs
+++ Static/ApplyChanges.hs
@@ -53,6 +53,16 @@ dgXUpdate opts xs le ln dg = do
   rebuiltDgXml opts le xml
 --  updateDG opts xml chL dg le
 
+{- TODO (general): a lot of unused information is created. possibly check if
+update is required for an element BEFORE processing xgraph information. -}
+{- TODO (general): use ChangeGraph functions instead of DevGraph ones for
+insertion? (called through FromXml) -}
+{- TODO (general): determine the EXACT required change for each object instead
+of overwriting the existing one every time! -}
+
+{- | updates a dgraph partially in accordance with changelist data from a .diff
+application. an xgraph is created and used as a guideline for signature-
+hierachy and for retrieving required new data. -}
 updateDG :: HetcatsOpts -> Element -> ChangeList -> DGraph -> LibEnv
          -> ResultT IO (LibName, LibEnv)
 updateDG opts xml chL dg le = do
@@ -60,26 +70,26 @@ updateDG opts xml chL dg le = do
   xgr <- liftR $ xGraph xml
   let dg'' = if updateAnnotations chL then dg' { globalAnnos = globAnnos xgr }
         else dg'
-  (dgFin, le', chL'') <- iterateXgBody opts xgr le dg'' chL'
+  (dgFin, le', _) <- iterateXgBody opts xgr le dg'' chL'
   let ln = libName xgr
-  -- TODO update/insert Theorem Links
-  -- TODO adjust nextLinkId etc..
   -- TODO possibly test if any updates are left undone (corrupted dg/diff)
-  return (ln, Map.insert ln dgFin le')
+  return (ln, Map.insert ln dgFin { getNewEdgeId = nextLinkId xgr } le')
 
--- !! ALWAYS DELETE PROCESSED ENTRIES FROM CHANGELIST
+{- | move along xgraph structure and update or insert when required. updated
+elements are those initially marked as such in changelist as well as all
+elements that are subject to an ingoing signature modification. -}
 iterateXgBody :: HetcatsOpts -> XGraph -> LibEnv -> DGraph
               -> ChangeList -> ResultT IO (DGraph, LibEnv, ChangeList)
 iterateXgBody opts xgr lv dg chL = let lg = logicGraph in do
   rs1 <- foldM (mkNodeUpdate opts lg Nothing) (dg, lv, chL) $ startNodes xgr
-  foldM (foldM (mkXStepUpdate opts lg)) rs1 $ xg_body xgr
+  rs2 <- foldM (foldM (mkXStepUpdate opts lg)) rs1 $ xg_body xgr
+  mkThmLinkUpdates lg rs2 $ thmLinks xgr
 
-{- | apply updates to one branch of the xgraph. conducted updates are
-  - all updates or insertions found in ChangeList
-  - resulting theory changes of former updates (source of indoing deflinks -}
+{- | apply updates to one branch of the xgraph. -}
 mkXStepUpdate :: HetcatsOpts -> LogicGraph -> (DGraph, LibEnv, ChangeList)
               -> ([XLink], XNode) -> ResultT IO (DGraph, LibEnv, ChangeList)
 mkXStepUpdate opts lg (dg, lv, chL) (xlks, xnd) = let
+  -- TODO by adjusting this test, the partial update can get more precise
   needSigUpd = foldl (\ r xlk -> r || maybe False (\_ -> True)
     (Map.lookup (parseNodeName $ source xlk) $ changedInDg chL)) False xlks
   {- TODO use ChangeGraph.setSignature when possible to keep old node info. -} 
@@ -91,24 +101,37 @@ mkXStepUpdate opts lg (dg, lv, chL) (xlks, xnd) = let
         (dg, lv, chL') xnd
     foldM (mkLinkUpdate lg) rs1 mrs
 
-{- iterate one branches list of xlinks and make update/insertions to dg as
-required -}
+{- check one xlink and make update/insertion to dg if required -}
 mkLinkUpdate :: LogicGraph -> (DGraph, LibEnv, ChangeList)
              -> (Node, GMorphism, DGLinkType, XLink)
              -> ResultT IO (DGraph, LibEnv, ChangeList)
-mkLinkUpdate lg (dg, lv, chL) lkMr@(_, _, _, xlk) =
-  case Map.lookup (edgeId xlk) $ changeLinks chL of
-    -- no change required, move on
-    Nothing -> return (dg, lv, chL)
-    -- TODO do we need to delete link first if updating an existing one??
-    Just _ -> case lookupUniqueNodeByName (target xlk) dg of
+mkLinkUpdate lg (dg, lv, chL) lkMr@(_, _, _, xlk) = let
+  -- TODO use setMorphism if possible to keep old link data
+  mkLinkUpdateAux = case lookupUniqueNodeByName (target xlk) dg of
         Nothing -> fail $
           "required node [" ++ target xlk ++ "] was not found in DGraph!"
         Just (j, lbl) -> do
           dg' <- (insertLink lg j $ signOf $ dgn_theory lbl) dg lkMr
           return (dg', lv, chL { changeLinks = Map.delete (edgeId xlk)
             $ changeLinks chL })
-    
+  in case Map.lookup (edgeId xlk) $ changeLinks chL of
+    -- TODO do we need to delete link first if updating an existing one??
+    Just _ -> mkLinkUpdateAux
+    {- even if the link is not marked for update, a gmorphism change might be 
+    equired -}
+    Nothing -> let hasThUpd s = maybe False (\ _ -> True)
+                     $ Map.lookup (parseNodeName s) $ changedInDg chL in do
+      if hasThUpd (source xlk) || hasThUpd (target xlk) then mkLinkUpdateAux
+        else return (dg, lv, chL)
+
+-- | updates any necessary ThmLinks
+mkThmLinkUpdates :: LogicGraph -> (DGraph, LibEnv, ChangeList) -> [XLink]
+                 -> ResultT IO (DGraph, LibEnv, ChangeList)
+mkThmLinkUpdates lg (dg, lv, chL) xlks = do
+  mrs <- mapM (getTypeAndMorphism lg dg) xlks
+  -- TODO use setMorphism if possible to keep old link data
+  foldM (mkLinkUpdate lg) (dg, lv, chL) mrs
+  
 {- | update or insert a node in accordance with XGraph data ONLY if the element
 is marked for updating in changelist. -}
 mkNodeUpdate :: HetcatsOpts -> LogicGraph -> Maybe G_theory
