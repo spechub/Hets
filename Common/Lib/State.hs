b959b3ff2f129f3d88ad43c23bad0e78d05739d1
Christian Maeder <Christian.Maeder@dfki.de>
2011-10-31 12:11:39 +0000

    try without transformers for lucid and maverick git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@16400 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Common/Lib/State.hs
+++ Common/Lib/State.hs
@@ -15,6 +15,47 @@ Control.Monad.State, but now Control.Monad.Trans.State can be used instead.
 
 -}
 
-module Common.Lib.State (module Control.Monad.Trans.State) where
+module Common.Lib.State where
 
-import Control.Monad.Trans.State
+-- | Our fixed state monad
+newtype State s a = State { runState :: s -> (a, s) }
+
+state :: (s -> (a, s)) -> State s a
+state = State
+
+instance Functor (State s) where
+        fmap f m = State $ \ s -> let
+                (a, s') = runState m s
+                in (f a, s')
+
+instance Monad (State s) where
+        return a = State $ \ s -> (a, s)
+        m >>= k = State $ \ s -> let
+                (a, s') = runState m s
+                in runState (k a) s'
+
+-- put and get are non-overloaded here!
+
+get :: State s s
+get = State $ \ s -> (s, s)
+
+put :: s -> State s ()
+put s = State $ const ((), s)
+
+modify :: (s -> s) -> State s ()
+modify f = get >>= put . f
+
+gets :: (s -> a) -> State s a
+gets f = fmap f get
+
+evalState :: State s a -> s -> a
+evalState m = fst . runState m
+
+execState :: State s a -> s -> s
+execState m = snd . runState m
+
+mapState :: ((a, s) -> (b, s)) -> State s a -> State s b
+mapState f m = State $ f . runState m
+
+withState :: (s -> s) -> State s a -> State s a
+withState f m = State $ runState m . f
