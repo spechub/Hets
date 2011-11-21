aacb76860aaea310182aebe9598a97c7f447ffda
Jonathan von Schroeder <sternkinder@gmail.com>
2011-11-21 14:17:36 +0000

    hol light: fixed a problem with symlinks git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@16479 cec4b9c1-7d33-0410-9eda-942365e851bb

--- HolLight/OcamlTools/export.ml
+++ HolLight/OcamlTools/export.ml
@@ -2,7 +2,10 @@ exception EnvError of string;;
 let getenv env_var =
  try Sys.getenv env_var
  with Not_found -> raise (EnvError (Printf.sprintf "Couldn't find env variable %s" env_var));;
-let hol_dir = getenv "HETS_HOL_DIR";;
+let realpath p = let curr_cwd = Sys.getcwd (); in 
+                  Sys.chdir p; let real_p = Sys.getcwd ()
+                               in Sys.chdir curr_cwd; real_p;;
+let hol_dir = realpath (getenv "HETS_HOL_DIR");;
 let ocaml_source_dir = getenv "HETS_OCAML_LIB_DIR";;
 let ocaml_tools_dir = getenv "HETS_HOLLIGHT_TOOLS";;
 let use_file' s =
