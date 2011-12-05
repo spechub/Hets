dbb8328db437a85827e4bfdc46e0c41b0703db2f
Francisc Nicolae Bungiu <f.bungiu@jacobs-university.de>
2011-12-05 15:28:44 +0000

    symbols for hets added git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@16510 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ RDF/Symbols.hs
@@ -0,0 +1,28 @@
+{- |
+Module      :  $Header$
+Copyright   :  (c) Francisc-Nicolae Bungiu
+License     :  GPLv2 or higher, see LICENSE.txt
+
+Maintainer  :  f.bungiu@jacobs-university.de
+Stability   :  provisional
+Portability :  portable
+
+Symbol items for Hets
+-}
+
+module RDF.Symbols where
+
+import RDF.AS
+import OWL2.AS
+
+-- * SYMBOL ITEMS FOR HETS
+
+data SymbItems = SymbItems (Maybe RDFEntityType) [IRI]
+    deriving (Show, Eq)
+
+data SymbMapItems = SymbMapItems (Maybe RDFEntityType) [(IRI, Maybe IRI)]
+    deriving (Show, Eq)
+
+-- | raw symbols
+data RawSymb = ASymbol EntityRDF | AnUri IRI
+    deriving (Show, Eq, Ord)
