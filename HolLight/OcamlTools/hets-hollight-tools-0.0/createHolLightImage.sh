847035a704180c826b91c0145d5dcd5a07432833
Christian Maeder <Christian.Maeder@dfki.de>
2011-12-05 13:18:29 +0000

    pbuilder fails with: dmtcp: cannot open output channel for error logging git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@16507 cec4b9c1-7d33-0410-9eda-942365e851bb

--- HolLight/OcamlTools/hets-hollight-tools-0.0/createHolLightImage.sh
+++ HolLight/OcamlTools/hets-hollight-tools-0.0/createHolLightImage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-export HETS_HOLLIGHT_TOOLS=/usr/lib/hets/hets-hollight-tools
+export HETS_HOLLIGHT_TOOLS=$PWD
 export HETS_HOL_DIR=/usr/share/hol-light
 export HETS_OCAML_LIB_DIR=/usr/lib/ocaml/compiler-libs
 
@@ -14,5 +14,5 @@ dmtcp_command --quiet --bcheckpoint
 dmtcp_command --quiet --quit) &
 dmtcp_checkpoint --quiet ocaml -w a -init $HETS_HOLLIGHT_TOOLS/export.ml
 
-mv ckpt_ocamlrun_* hol_light.dmtcp
-rm dmtcp_restart_script*.sh
+mv -f ckpt_ocamlrun_* hol_light.dmtcp
+rm -f dmtcp_restart_script*.sh
