c18150047a08abe95e9495468ef8d2124d43dbf8
Jonathan von Schroeder <sternkinder@gmail.com>
2011-12-12 07:21:57 +0000

    modified checkpointing script to include distro and architecture in the image name git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@16553 cec4b9c1-7d33-0410-9eda-942365e851bb

--- HolLight/OcamlTools/imageTools/makeCheckpoint.sh
+++ HolLight/OcamlTools/imageTools/makeCheckpoint.sh
@@ -7,7 +7,7 @@ imageTools/dmtcp/bin/dmtcp_command --quiet --bcheckpoint
 echo "----------------------------- Moving Checkpoint"
 DISTR=`cat /etc/*-release | grep DISTRIB_RELEASE | cut -d= -f2`
 ARCH=`uname -m`
-mv -f ckpt_ocamlrun_* hol_light_$DISTR\_$ARCH.dmtcp
-rm -f dmtcp_restart_script*.sh
+mv ckpt_ocamlrun_* hol_light_$DISTR\_$ARCH.dmtcp
+rm dmtcp_restart_script*.sh
 echo "------------------------------ Killing Process & Quitting Coordinator"
 imageTools/dmtcp/bin/dmtcp_command --quiet --quit
