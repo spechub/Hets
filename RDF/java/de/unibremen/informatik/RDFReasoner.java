4b0c94a11262fd5b208008bb4e14cfd06351734f
Felix Gabriel Mance <f.mance@jacobs-university.de>
2011-12-28 20:49:02 +0000

    shortened git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@16582 cec4b9c1-7d33-0410-9eda-942365e851bb

--- RDF/java/de/unibremen/informatik/RDFReasoner.java
+++ RDF/java/de/unibremen/informatik/RDFReasoner.java
@@ -7,80 +7,62 @@ import java.io.File;
 import java.io.BufferedReader;
 import java.io.IOException;
 
-public class RDFReasoner extends Object {
-	public static void main (String args[]) {
+public class RDFReasoner {
+	public static void main (String args[]) throws Exception {
+		String current = null;
 
-	String current = null;
-	
-	if (args.length == 0) {
-		System.out.println("<rdf_filename> <query_filename>");
-		System.exit(1);
-	}
-	else 
-		if (args.length == 1) {
-			//System.out.println(current + "\n");
-	                String inputFile = null;
-	                Model model = ModelFactory.createDefaultModel();
-	                try {
-	                        inputFile = args[0];
-	                        InputStream in = new  FileInputStream(inputFile);
-	                        if (in == null) { 
-	                                System.out.println("File not found!");
-	                                System.exit(1);
-	                        } 
-	                        else {
-	                                current = new File(args[0]).getAbsolutePath();
-	                                current = "file://" + current;
-	                        }
-	                model.read(current);
-	                model.write(System.out);
-	                } catch(Exception e) {
-	                        System.out.println(e.getMessage());
-	                        System.exit(1);
-	                }
-		}
-		else {
-		//System.out.println(current + "\n");
-		String inputQuery = null;
-		String inputFile = null;
-		String queryString = "";
-		Model model = ModelFactory.createDefaultModel();
-		try {
-			inputFile = args[0];
-			inputQuery = args[1];
-			BufferedReader query_in = new BufferedReader(new FileReader(inputQuery));
-			String line;
-			while ((line = query_in.readLine())!=null)
-				queryString += line + "\n";
-
-			InputStream in = new  FileInputStream(inputFile);
-	  		if (in == null) {  
-				System.out.println("File not found!");
-				System.exit(1);
-			}  
-			else {
-				current = new File(args[0]).getAbsolutePath();
-				current = "file://" + current;
-			}
-		//System.out.println("\nQuery: " + queryString + "\n");
-		model.read(current);
-		Query query = QueryFactory.create(queryString);
-		
-		//Execute query and obtain results
-		QueryExecution qe = QueryExecutionFactory.create(query, model);
-		ResultSet results = qe.execSelect();
-		
-		// Output query results	
-		ResultSetFormatter.out(System.out, results, query);
-
-		// Important - free up resources used running the query
-		qe.close();
-
-		//model.write(System.out);
-	 	} catch(Exception e) {
-			System.out.println(e.getMessage());
+		if (args.length == 0) {
+			System.out.println("<rdf_filename> <query_filename>");
 			System.exit(1);
+		} else {
+			String inputFile = args[0];
+			InputStream in = new FileInputStream(inputFile);
+			Model model = ModelFactory.createDefaultModel();
+
+			if (in == null) { 
+				System.out.println("File" + inputFile + "not found");
+				System.exit(1);
+			} else
+				current = "file://" + new File(inputFile).getAbsolutePath();
+
+			if (args.length == 1) {
+				try {
+					model.read(current);
+					model.write(System.out);
+				} catch(Exception e) {
+					System.out.println(e.getMessage());
+					System.exit(1);
+				}
+			} else {
+				String inputQuery = null;
+				String queryString = "";
+				try {
+					inputQuery = args[1];
+					BufferedReader query_in = new BufferedReader(new FileReader(inputQuery));
+					String line;
+
+					while ((line = query_in.readLine()) != null)
+						queryString += line + "\n";
+
+					model.read(current);
+					Query query = QueryFactory.create(queryString);
+
+					//Execute query and obtain results
+					QueryExecution qe = QueryExecutionFactory.create(query, model);
+					ResultSet results = qe.execSelect();
+
+					// Output query results	
+					ResultSetFormatter.out(System.out, results, query);
+
+					// Important - free up resources used running the query
+					qe.close();
+
+					//model.write(System.out);
+				} catch(Exception e) {
+					System.out.println(e.getMessage());
+					System.exit(1);
+				}
+			}
 		}
-	}
 	}	
 }
