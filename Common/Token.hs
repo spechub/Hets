7fc5ecf094ed7ad8f6dd878e719ef95c0b2a5da0
Christian Maeder <Christian.Maeder@dfki.de>
2012-03-23 15:26:59 +0000

    try to fix #1015 git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@16743 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Common/Token.hs
+++ Common/Token.hs
@@ -267,7 +267,7 @@ varId ks = pToken (reserved (ks ++ casl_reserved_fwords) scanAnyWords)
 -- | non-skipping version for simple ids
 nonSkippingSimpleId :: GenParser Char st Token
 nonSkippingSimpleId =
-  parseToken $ reserved casl_structured_reserved_words scanAnyWords
+  parseToken $ reserved casl_reserved_words scanAnyWords
 
 -- | like 'varId'.  'Common.Id.SIMPLE_ID' for spec- and view names
 simpleId :: GenParser Char st Token
