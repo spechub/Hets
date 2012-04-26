da7a83e9d1b66c835f752b29e53b89f084743b2c
Christian Maeder <Christian.Maeder@dfki.de>
2012-04-26 08:11:03 +0000

    changed pretty printing with ghc-7.4.1 git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@16833 cec4b9c1-7d33-0410-9eda-942365e851bb

--- ToHaskell/test/BoolEx.hascasl.hs
+++ ToHaskell/test/BoolEx.hascasl.hs
@@ -30,13 +30,11 @@ neg :: Bool -> Bool
 vee :: (Bool, Bool) -> Bool
 wedge :: (Bool, Bool) -> Bool
 neg x
-    =
-        case x of
+    =   case x of
             False -> True
             True -> False
 wedge (x, y)
-    =
-        case (x, y) of
+    =   case (x, y) of
             (False, False) -> False
             (True, False) -> False
             (False, True) -> False
