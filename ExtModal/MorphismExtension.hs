21277f0823ebf73550029159e220c9e3cbeca22a
Christian Maeder <Christian.Maeder@dfki.de>
2012-06-08 16:32:13 +0000

    compute induced signature git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@16874 cec4b9c1-7d33-0410-9eda-942365e851bb

--- ExtModal/MorphismExtension.hs
+++ ExtModal/MorphismExtension.hs
@@ -16,86 +16,67 @@ import qualified Data.Map as Map
 import qualified Data.Set as Set
 
 import CASL.Morphism
+import CASL.Sign
 import CASL.MapSentence
 
 import Common.Doc
 import Common.DocUtils
 import Common.Id
+import Common.Utils
+import qualified Common.Lib.MapSet as MapSet
 
 import ExtModal.ExtModalSign
 import ExtModal.AS_ExtModal
 import ExtModal.Print_AS ()
 
 data MorphExtension = MorphExtension
-        { source :: EModalSign
-        , target :: EModalSign
-        , mod_map :: Map.Map Id Id
+        { mod_map :: Map.Map Id Id
         , nom_map :: Map.Map SIMPLE_ID SIMPLE_ID
         } deriving (Show, Eq, Ord)
 
 emptyMorphExtension :: MorphExtension
-emptyMorphExtension =
-  MorphExtension emptyEModalSign emptyEModalSign Map.empty Map.empty
+emptyMorphExtension = MorphExtension Map.empty Map.empty
 
 instance Pretty MorphExtension where
-        pretty me = specBraces (pretty $ source me) $+$
-           mapsto <+> specBraces (pretty $ target me)
-           $+$ pretty (mod_map me) $+$ pretty (nom_map me)
+        pretty me = pretty (mod_map me) $+$ pretty (nom_map me)
 
 instance MorphismExtension EModalSign MorphExtension where
-
-        ideMorphismExtension sgn =
-                let insert_next old_map s_id = Map.insert s_id s_id old_map in
-                MorphExtension sgn sgn
-                  (foldl insert_next Map.empty (Map.keys (modalities sgn)))
-                  (foldl insert_next Map.empty (Set.toList (nominals sgn)))
-
+        ideMorphismExtension _ = emptyMorphExtension
         composeMorphismExtension fme1 fme2 = let
           me1 = extended_map fme1
           me2 = extended_map fme2
-          in if me1 == me2
-                   then let me_compos second_map old_map (me_k, me_val) =
-                                if Map.member me_val second_map
-                                then Map.insert me_k (second_map Map.! me_val)
-                                     old_map
-                                else old_map
-                        in return $ MorphExtension (source me1) (target me2)
-                               (foldl (me_compos (mod_map me2))
-                                      Map.empty (Map.toList (mod_map me1)))
-                               (foldl (me_compos (nom_map me2))
-                                      Map.empty (Map.toList (nom_map me1)))
-                   else return emptyMorphExtension
-
-        inverseMorphismExtension fme = let me = extended_map fme in
-                let swap_arrows old_map (me_k, me_val) =
-                        Map.insert me_val me_k old_map
-                    occurs_alt [] once _ = once
-                    occurs_alt ((_, me_val1) : l) True me_val2 =
-                        me_val1 == me_val2 ||
-                            occurs_alt l True me_val2
-                    occurs_alt ((_, me_val1) : l) False me_val2 =
-                        if me_val1 == me_val2
-                        then occurs_alt l True me_val2
-                        else occurs_alt l False me_val2
-                in if Map.keys (modalities (target me))
-                       == Map.elems (mod_map me)
-                       && Set.toList (nominals (target me))
-                              == Map.elems (nom_map me)
-                       && Map.filter (occurs_alt (Map.toList (mod_map me))
-                                      False) (mod_map me) == Map.empty
-                        && Map.filter (occurs_alt (Map.toList (nom_map me))
-                                       False) (nom_map me) == Map.empty
-                      then return $ MorphExtension (target me) (source me)
-                               (foldl swap_arrows Map.empty
-                                (Map.toList (mod_map me)))
-                                (foldl swap_arrows Map.empty
-                                 (Map.toList (nom_map me)))
-                      else return emptyMorphExtension
-
+          src = msource fme1
+          mSrc = extendedInfo src
+          in return $ MorphExtension
+             (composeMap (modalities mSrc) (mod_map me1) $ mod_map me2)
+             $ composeMap (MapSet.setToMap $ nominals mSrc) (nom_map me1)
+             $ nom_map me2
+        -- ignore inverses
         isInclusionMorphismExtension me =
-                let target_ide = ideMorphismExtension (target me) in
-                Map.isSubmapOf (mod_map me) (mod_map target_ide)
-                        && Map.isSubmapOf (nom_map me) (nom_map target_ide)
+                Map.null (mod_map me) && Map.null (nom_map me)
+
+inducedEMsign :: InducedSign EM_FORMULA EModalSign MorphExtension EModalSign
+inducedEMsign sm om pm m sig =
+  let ms = extendedInfo sig
+      mods = mod_map m
+  in ms
+  { rigidOps = inducedOpMap sm om $ rigidOps ms
+  , rigidPreds = inducedPredMap sm pm $ rigidPreds ms
+  , modalities = Map.fromListWith (++)
+    $ map (\ (i, l) ->
+        (if Set.member i $ termMods ms then Map.findWithDefault i i sm
+            else Map.findWithDefault i i mods
+        , map (fmap $ mapSen mapEMform (embedMorphism m sig sig)
+                  { sort_map = sm
+                  , op_map = om
+                  , pred_map = pm }) l)) $ Map.toList $ modalities ms
+  , time_modalities = Set.map (\ i -> Map.findWithDefault i i mods)
+    $ time_modalities ms
+  , termMods = Set.map (\ i -> Map.findWithDefault i i sm)
+    $ termMods ms
+  , nominals = Set.map (\ i -> Map.findWithDefault i i $ nom_map m)
+    $ nominals ms
+  }
 
 mapEMmod :: Morphism EM_FORMULA EModalSign MorphExtension -> MODALITY
   -> MODALITY
@@ -112,8 +93,8 @@ mapEMmod morph tm = case tm of
 -- Modal formula mapping via signature morphism
 mapEMform :: MapSen EM_FORMULA EModalSign MorphExtension
 mapEMform morph frm = let rmapf = mapSen mapEMform morph in case frm of
-  BoxOrDiamond choice tm leq_geq number f pos ->
-    BoxOrDiamond choice (mapEMmod morph tm) leq_geq number (rmapf f) pos
+  BoxOrDiamond choice tm leq_geq num f pos ->
+    BoxOrDiamond choice (mapEMmod morph tm) leq_geq num (rmapf f) pos
   Hybrid choice nom f pos -> Hybrid choice
     (Map.findWithDefault nom nom $ nom_map $ extended_map morph)
     (rmapf f) pos
