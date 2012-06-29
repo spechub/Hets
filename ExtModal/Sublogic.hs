db7226fb6d8c51e928096b508ac274add50ba9c0
Mihaela Turcu <m.turcu@jacobs-university.de>
2012-06-29 12:48:47 +0000

    computed sublogics of ext modal git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@16994 cec4b9c1-7d33-0410-9eda-942365e851bb

--- ExtModal/Sublogic.hs
+++ ExtModal/Sublogic.hs
@@ -13,34 +13,98 @@ Sublogics for ExtModal Logic
 
 module ExtModal.Sublogic where
 
-data Frequency = None | One | Many
-        deriving (Show, Eq, Ord)
+import CASL.Fold
+import CASL.AS_Basic_CASL
+import ExtModal.AS_ExtModal
+import ExtModal.ExtModalSign
+import Common.AS_Annotation
+
+data Frequency = None 
+               | One 
+               | Many
+               deriving (Show, Eq, Ord)
+
 data Sublogic = Sublogic
-        { hasModalities :: Frequency
-        , hasTermMods :: Bool
-        , hasTransClos :: Bool
-        , hasNominals :: Bool
-        , hasTimeMods :: Frequency
-        , hasFixPoints :: Bool
- }
- deriving (Show, Eq, Ord)
+    { hasModalities :: Frequency
+    , hasTermMods :: Bool
+    , hasTransClos :: Bool
+    , hasNominals :: Bool
+    , hasTimeMods :: Frequency
+    , hasFixPoints :: Bool
+    }
+    deriving (Show, Eq, Ord)
 
 maxSublogic :: Sublogic
 maxSublogic = Sublogic
-  { hasModalities = Many
-  , hasTermMods = True
-  , hasTransClos = True
-  , hasNominals = True
-  , hasTimeMods = Many
-  , hasFixPoints = True
-  }
+    { hasModalities = Many
+    , hasTermMods = True
+    , hasTransClos = True
+    , hasNominals = True
+    , hasTimeMods = Many
+    , hasFixPoints = True
+    }
+
+minSublogic :: Sublogic
+minSublogic = Sublogic
+    { hasModalities = None
+    , hasTermMods = False
+    , hasTransClos = False
+    , hasNominals = False
+    , hasTimeMods = None
+    , hasFixPoints = False
+    }
 
 joinSublogic :: Sublogic -> Sublogic -> Sublogic
 joinSublogic a b = Sublogic
-  { hasModalities = hasModalities a `max` hasModalities b
-  , hasTermMods = hasTermMods a `max` hasTermMods b
-  , hasTransClos = hasTransClos a `max` hasTransClos b
-  , hasNominals = hasNominals a `max` hasNominals b
-  , hasTimeMods = hasTimeMods a `max` hasTimeMods b
-  , hasFixPoints = hasFixPoints a `max` hasFixPoints b
-        }
+    { hasModalities = hasModalities a `max` hasModalities b
+    , hasTermMods = hasTermMods a `max` hasTermMods b
+    , hasTransClos = hasTransClos a `max` hasTransClos b
+    , hasNominals = hasNominals a `max` hasNominals b
+    , hasTimeMods = hasTimeMods a `max` hasTimeMods b
+    , hasFixPoints = hasFixPoints a `max` hasFixPoints b
+    }
+
+joinSublogics :: [Sublogic] -> Sublogic
+joinSublogics = foldr joinSublogic minSublogic
+
+minSublogicOfForm :: FORMULA EM_FORMULA -> Sublogic --ExtModalFORMULA
+minSublogicOfForm = foldFormula (constRecord minSublogicOfEM 
+                    joinSublogics minSublogic) 
+
+
+minSublogicOfEM :: EM_FORMULA -> Sublogic
+minSublogicOfEM f = case f of 
+    BoxOrDiamond _ _ _ _ f _ -> minSublogicOfForm f
+    Hybrid _ _ f _ -> (minSublogicOfForm f) {hasNominals = True}
+    UntilSince _ f1 f2 _ -> joinSublogic (minSublogicOfForm f1)
+      (minSublogicOfForm f2)
+    PathQuantification _ f _ -> minSublogicOfForm f
+    NextY _ f _ -> minSublogicOfForm f
+    StateQuantification _ _ f _ -> minSublogicOfForm f
+    FixedPoint _ _ f _ -> minSublogicOfForm f
+    ModForm md -> minSublogicOfModDefn md
+
+
+minSublogicOfModDefn :: ModDefn -> Sublogic
+minSublogicOfModDefn (ModDefn time term il fl _) =
+    setModalities il
+    $ setTermMods term    
+    $ setTimeMods time il 
+    $ joinSublogics (map (minSublogicOfForm . item) fl)
+
+setModalities :: [a] -> Sublogic -> Sublogic
+setModalities il s = case il of
+    [_] -> s {hasModalities = max One (hasModalities s)}
+    _ -> s {hasModalities = Many}
+
+setTermMods :: Bool -> Sublogic -> Sublogic
+setTermMods b s = if b then s {hasTermMods = True} else s
+
+setTimeMods :: Bool -> [a] -> Sublogic -> Sublogic
+setTimeMods b il s = if b then case il of
+    [_] -> s {hasTimeMods = max One (hasTimeMods s)}
+    _ -> s {hasTimeMods = Many}
+    else s 
+    
+
+
