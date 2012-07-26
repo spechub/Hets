277c685b1429c51f92b006f38aca4b89e0f4f2fd
Christian Maeder <Christian.Maeder@dfki.de>
2012-07-26 14:06:32 +0000

    added argument and changed destination git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@17063 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/macports/makeMacImage.sh
+++ utils/macports/makeMacImage.sh
@@ -1,5 +1,10 @@
-#/bin/sh
-IMAGE=Hets-`date +%F`.dmg
+#/bin/sh -x
+if [ -z "$1" ] ; then
+  VERSION=`date +%F`
+else
+  VERSION=$1
+fi
+IMAGE=Hets-$VERSION.dmg
 /Users/Shared/maeder/Platypus-4.7/Platypus.app/Contents/Resources/platypus_clt \
  -P ../Hets/utils/macports/hets.platypus Hets.app
 cp hets Hets.app/Contents/Resources/
@@ -7,5 +12,5 @@ cp -r /Users/Shared/maeder/uDrawGraph-3.1 Hets.app/Contents/Resources/
 tar zxf /home/www.informatik.uni-bremen.de/cofi/Libraries/daily/lib.tgz \
  -C Hets.app/Contents/Resources
 hdiutil create -srcfolder Hets.app $IMAGE
-cp $IMAGE /home/www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets/intel-mac/mpkgs/
+cp $IMAGE /home/www.informatik.uni-bremen.de/agbkb/forschung/formal_methods/CoFI/hets/intel-mac/dmgs/
 
