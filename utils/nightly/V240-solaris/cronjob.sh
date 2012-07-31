59bf2cbc8c9db930c1057a66056544db3bb06054
Christian Maeder <Christian.Maeder@dfki.de>
2012-07-31 09:26:51 +0000

    current cronjobs git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@17079 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ utils/nightly/V240-solaris/cronjob.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -x
+
+PATH=/home/pub-bkb/ghc/ghc-6.10.4/bin:/home/maeder/haskell/solaris/bin:/home/pub-bkb/bin:/usr/local/bin:/usr/bin/:/usr/local/X11/bin:/usr/ccs/bin
+UDG_HOME=/home/pub-bkb/uDrawGraph-3.1
+HETS_LIB=/home/maeder/haskell/V240-solaris/haskell/Hets-lib
+LD_LIBRARY_PATH=/home/maeder/haskell/solaris/lib:/usr/local/lib
+
+export PATH
+export UDG_HOME
+export HETS_LIB
+export LD_LIBRARY_PATH
+
+cd /home/maeder/haskell/V240-solaris/haskell
+
+. ../cronjob.sh
+
+makeHets
+makeLibCheck faster
--- utils/nightly/solaris/cronjob.sh
+++ utils/nightly/solaris/cronjob.sh
@@ -1,16 +1,14 @@
 #!/bin/bash -x
 
-PATH=/home/maeder/haskell/solaris/bin:/home/pub-bkb/bin:/usr/local/bin:/usr/bin/:/usr/local/X11/bin:/usr/ccs/bin
+PATH=/local/home/maeder/ghc/bin:/local/home/maeder/gcc/bin:/home/pub-bkb/bin:/opt/csw/bin:/usr/bin/:/usr/ccs/bin
 UDG_HOME=/home/pub-bkb/uDrawGraph-3.1
-HETS_LIB=/home/maeder/haskell/V240-solaris/haskell/Hets-lib
-LD_LIBRARY_PATH=/home/maeder/haskell/solaris/lib:/usr/local/lib
+HETS_LIB=/local/home/maeder/haskell/Hets-lib
 
 export PATH
 export UDG_HOME
 export HETS_LIB
-export LD_LIBRARY_PATH
 
-cd /home/maeder/haskell/V240-solaris/haskell
+cd /local/home/maeder/haskell
 
 . ../cronjob.sh
 
