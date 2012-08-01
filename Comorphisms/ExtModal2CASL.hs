1070181294f9381ac4ac19eba1c3ecc40fc731a4
Mihaela Turcu <m.turcu@jacobs-university.de>
2012-08-01 12:23:00 +0000

    started comorphism from ext modal to casl git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@17088 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ Comorphisms/ExtModal2CASL.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE MultiParamTypeClasses, TypeSynonymInstances, FlexibleInstances #-}
+{- |
+Module      :  $Header$
+Copyright   :  (c) Mihaela Turcu, DFKI 2012
+License     :  GPLv2 or higher, see LICENSE.txt
+
+Maintainer  :  m.turcu@jacobs-university.de
+Stability   :  
+Portability :  
+-}
+
+module Comorphisms.ExtModal2CASL where
+
+import Logic.Logic
+import Logic.Comorphism
+
+import Common.ProofTree
+
+-- CASL
+import CASL.Logic_CASL
+import CASL.Sublogic as SL
+import CASL.Sign
+import CASL.AS_Basic_CASL
+import CASL.Morphism
+
+-- ExtModalCASL
+import ExtModal.Logic_ExtModal
+import ExtModal.AS_ExtModal
+--import ExtModal.ExtModalSign
+import ExtModal.Sublogic
+
+data ExtModal2CASL = ExtModal2CASL deriving (Show)
+instance Language ExtModal2CASL
+
+instance Comorphism ExtModal2CASL
+               ExtModal Sublogic EM_BASIC_SPEC ExtModalFORMULA SYMB_ITEMS 
+               SYMB_MAP_ITEMS ExtModalSign ExtModalMorph
+               Symbol RawSymbol ()
+               CASL CASL_Sublogics
+               CASLBasicSpec CASLFORMULA SYMB_ITEMS SYMB_MAP_ITEMS
+               CASLSign
+               CASLMor
+               Symbol RawSymbol ProofTree where
+    sourceLogic ExtModal2CASL = ExtModal
+    sourceSublogic ExtModal2CASL = maxSublogic
+    targetLogic ExtModal2CASL = CASL
+    mapSublogic ExtModal2CASL _ = Just SL.caslTop
+    map_theory ExtModal2CASL _ = fail "ExtModal2CASL.map_theory" 
+    {-(sig, sens) = case transSig sig of
+      mme -> return (mme, [])
+    map_morphism ExtModal2CASL = return . mapMor
+    map_sentence ExtModal2CASL sig = return . transSen sig
+    map_symbol ExtModal2CASL _ = Set.singleton . mapSym
+    -}
+    has_model_expansion ExtModal2CASL = True
+    is_weakly_amalgamable ExtModal2CASL = True
+
+{-
+transSig :: ExtModalSign -> CASLSign
+transSig sign =
+   let sorSet = sortSet sign
+       fws = freshWorldSort sorSet
+       flexOps' = MapSet.fromMap . Map.foldWithKey (addWorld_OP fws)
+                                    Map.empty $ MapSet.toMap flexibleOps
+       flexPreds' = MapSet.fromMap . addWorldRels True relsTermMod
+                    . addWorldRels False relsMod
+                    . Map.foldWithKey (addWorld_PRED fws)
+                                    Map.empty $ MapSet.toMap
+-}
+
