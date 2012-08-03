0df6e825862d5f0c98b52e8c7f7b9d00c0af9d34
Christian Maeder <Christian.Maeder@dfki.de>
2012-08-03 16:45:36 +0000

    remove unused import git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@17110 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CASL/CCC/FreeTypes.hs
+++ CASL/CCC/FreeTypes.hs
@@ -14,7 +14,6 @@ Consistency checking of free types
 module CASL.CCC.FreeTypes (checkFreeType) where
 
 import CASL.AS_Basic_CASL       -- FORMULA, OP_{NAME,SYMB}, TERM, SORT, VAR
-import CASL.MapSentence(mapSen)
 import CASL.Morphism
 import CASL.Sign(OpType(..), PredType(..), Sign(..), sortSet, sortOfTerm)
 import CASL.SimplifySen(simplifyCASLSen)
