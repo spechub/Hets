b79718d28f308182b99c9053573cb57dad5268f6
Simon Ulbricht <tekknix@informatik.uni-bremen.de>
2012-08-07 10:14:30 +0000

    pulled some functionalities out of GtkAutoProof for use in other modules git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@17121 cec4b9c1-7d33-0410-9eda-942365e851bb

---
+++ Common/AutoProofUtils.hs
@@ -0,0 +1,96 @@
+{- |
+Module      :  $Header$
+Description :  Utils for automatic proving procedure of multiple nodes
+Copyright   :  (c) Simon Ulbricht, Uni Bremen 2010
+License     :  GPLv2 or higher, see LICENSE.txt
+
+Maintainer  :  tekknix@informatik.uni-bremen.de
+Stability   :  provisional
+Portability :  portable
+
+This module provides sturctures and methods for the automatic proofs module.
+-}
+
+module Common.AutoProofUtils where
+
+import Common.GtkGoal
+
+import Data.Maybe
+import Data.Graph.Inductive.Graph (LNode)
+import Data.List
+import Data.Ord (comparing)
+
+import Logic.Grothendieck
+
+import Static.DevGraph
+import Static.GTheory
+
+-- | stores each node to be considered along with some further infomation
+data FNode = FNode { name     :: String
+                   , node     :: LNode DGNodeLab
+                   , sublogic :: G_sublogics
+                   , goals    :: [String]
+                     {- after proving, a new GTheory with the new Proofstatus
+                     is computed -}
+                   , results  :: G_theory }
+
+{- | mostly for the purpose of proper display, the resulting G_theory of each
+FNode can be converted into a list of Goals. -}
+toGtkGoals :: FNode -> [Goal]
+toGtkGoals fn = map toGtkGoal . filter ((`elem` goals fn) . fst) . getThGoals
+  $ results fn
+
+{- | as a Prefix for display purpose, the ratio of proven and total goals
+is shown -}
+goalsToPrefix :: [Goal] -> String
+goalsToPrefix gs = let p = length $ filter (\ g -> gStatus g == GProved) gs
+                   in "[" ++ show p ++ "/" ++ show (length gs) ++ "] "
+
+{- | Displays every goal of a Node with a prefix showing the status and the
+goal name. -}
+showStatus :: FNode -> String
+showStatus fn = intercalate "\n" . map (\ g -> statusToPrefix
+                 (gStatus g) ++ show (gName g)) $ toGtkGoals fn
+
+-- | Get a markup string containing name and color
+instance Show FNode where
+  show fn = let gs = toGtkGoals fn
+                gmin = gStatus $ minimum gs
+            in
+    "<span color=\"" ++ statusToColor gmin ++ "\">"
+     ++ goalsToPrefix gs ++ name fn ++ "</span>"
+
+instance Eq FNode where
+  (==) f1 f2 = compare f1 f2 == EQ
+
+{- | for comparison, the goal status of each node is considered. only with
+equal goal status, nodes are sorted by name. -}
+instance Ord FNode where
+  compare = let gmin f = (minimum $ toGtkGoals f, name f)
+            in comparing gmin
+
+{- | gets all Nodes from the DGraph as input and creates a list of FNodes only
+containing Nodes to be considered.
+The results status field is initialised with the nodes local theory -}
+initFNodes :: [LNode DGNodeLab] -> [FNode]
+initFNodes = foldr (\ n@(_, l) t -> case globalTheory l of
+                      Nothing -> t
+                      Just gt ->
+                        let gt' = dgn_theory l
+                            gs = map fst $ getThGoals gt'
+                        in if null gs then t else
+                        FNode (getDGNodeName l) n (sublogicOfTh gt) gs gt' : t
+              ) []
+
+-- | returns True if a node has not been proved jet
+unchecked :: FNode -> Bool
+unchecked fn = all (isNothing . snd) . getThGoals $ results fn
+
+-- | returns True if at least one goal has been timed out
+timedout :: FNode -> Bool
+timedout fn = any (\ a -> gStatus a == GTimeout) $ toGtkGoals fn
+
+-- | returns True only if every goal has been proved
+allProved :: FNode -> Bool
+allProved fn = all (\ a -> gStatus a == GProved) $ toGtkGoals fn
+
--- GUI/GtkAutomaticProofs.hs
+++ GUI/GtkAutomaticProofs.hs
@@ -38,8 +38,8 @@ import Logic.Prover
 import Comorphisms.LogicGraph (logicGraph)
 
 import Common.LibName (LibName)
+import Common.AutoProofUtils
 import Common.Result
-import Common.GtkGoal
 
 import Control.Concurrent (forkIO, killThread)
 import Control.Concurrent.MVar
@@ -47,11 +47,9 @@ import Control.Monad (foldM_, join, when)
 
 import Proofs.AbstractState
 
-import Data.Graph.Inductive.Graph (LNode)
 import qualified Data.Map as Map
 import Data.List
 import Data.Maybe
-import Data.Ord (comparing)
 
 -- | Data structure for saving the user-selected prover and comorphism
 data Finder = Finder { fName      :: String
@@ -61,76 +59,6 @@ data Finder = Finder { fName      :: String
 
 instance Eq Finder where
   f1 == f2 = fName f1 == fName f2 && comorphism f1 == comorphism f2
-
--- | stores each node to be considered along with some further infomation
-data FNode = FNode { name     :: String
-                   , node     :: LNode DGNodeLab
-                   , sublogic :: G_sublogics
-                   , goals    :: [String]
-                     {- after proving, a new GTheory with the new Proofstatus
-                     is computed -}
-                   , results  :: G_theory }
-
-{- | mostly for the purpose of proper display, the resulting G_theory of each
-FNode can be converted into a list of Goals. -}
-toGtkGoals :: FNode -> [Goal]
-toGtkGoals fn = map toGtkGoal . filter ((`elem` goals fn) . fst) . getThGoals
-  $ results fn
-
-{- | as a Prefix for display purpose, the ratio of proven and total goals
-is shown -}
-goalsToPrefix :: [Goal] -> String
-goalsToPrefix gs = let p = length $ filter (\ g -> gStatus g == GProved) gs
-                   in "[" ++ show p ++ "/" ++ show (length gs) ++ "] "
-
-{- | Displays every goal of a Node with a prefix showing the status and the
-goal name. -}
-showStatus :: FNode -> String
-showStatus fn = intercalate "\n" . map (\ g -> statusToPrefix
-                 (gStatus g) ++ show (gName g)) $ toGtkGoals fn
-
--- | Get a markup string containing name and color
-instance Show FNode where
-  show fn = let gs = toGtkGoals fn
-                gmin = gStatus $ minimum gs
-            in
-    "<span color=\"" ++ statusToColor gmin ++ "\">"
-     ++ goalsToPrefix gs ++ name fn ++ "</span>"
-
-instance Eq FNode where
-  (==) f1 f2 = compare f1 f2 == EQ
-
-{- | for comparison, the goal status of each node is considered. only with
-equal goal status, nodes are sorted by name. -}
-instance Ord FNode where
-  compare = let gmin f = (minimum $ toGtkGoals f, name f)
-            in comparing gmin
-
-{- | gets all Nodes from the DGraph as input and creates a list of FNodes only
-containing Nodes to be considered.
-The results status field is initialised with the nodes local theory -}
-initFNodes :: [LNode DGNodeLab] -> [FNode]
-initFNodes = foldr (\ n@(_, l) t -> case globalTheory l of
-                      Nothing -> t
-                      Just gt ->
-                        let gt' = dgn_theory l
-                            gs = map fst $ getThGoals gt'
-                        in if null gs then t else
-                        FNode (getDGNodeName l) n (sublogicOfTh gt) gs gt' : t
-              ) []
-
--- | returns True if a node has not been proved jet
-unchecked :: FNode -> Bool
-unchecked fn = all (isNothing . snd) . getThGoals $ results fn
-
--- | returns True if at least one goal has been timed out
-timedout :: FNode -> Bool
-timedout fn = any (\ a -> gStatus a == GTimeout) $ toGtkGoals fn
-
--- | returns True only if every goal has been proved
-allProved :: FNode -> Bool
-allProved fn = all (\ a -> gStatus a == GProved) $ toGtkGoals fn
-
 
 -- | Displays the consistency checker window
 showAutomaticProofs :: GInfo -> LibEnv -> IO (Result LibEnv)
