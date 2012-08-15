17470fbb474418032e6ebd5677ac63785ba30580
Soeren D. Schulze <soeren.d.schulze@gmx.de>
2012-08-15 18:08:25 +0000

    cleaned up KIF lexer and parser git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@17194 cec4b9c1-7d33-0410-9eda-942365e851bb

--- CommonLogic/Lexer_KIF.hs
+++ CommonLogic/Lexer_KIF.hs
@@ -1,76 +1,50 @@
+{- |
+Module      :  $Header$
+Description :  Parser of the Knowledge Interchange Format
+Copyright   :  (c) Karl Luc, DFKI Bremen 2010, Soeren Schulze 2012
+License     :  GPLv2 or higher
+
+Maintainer  :  s.schulze@uni-bremen.de
+Stability   :  experimental
+Portability :  portable
+-}
+
 module CommonLogic.Lexer_KIF where
 
-import CommonLogic.AS_CommonLogic
 import Common.Id as Id
 import qualified Common.Lexer as Lexer
 import Common.Parsec
-import Common.Keywords
 
 import Text.ParserCombinators.Parsec as Parsec
-import Data.Char (ord)
+import Data.Char (isSpace, isUpper, isLower, isDigit, isAscii)
 import Control.Monad (liftM)
 
 -- literally from Lexer_CLIF.hs -- abstract?
+skip :: CharParser st String
+skip = liftM concat $ many white
+
 pToken :: CharParser st String -> CharParser st Token
-pToken p = Lexer.pToken p << many white
+pToken p = Lexer.pToken p << skip
 
 oParenT :: CharParser st Token
-oParenT = Lexer.oParenT << many white
+oParenT = pToken $ string "("
 
 cParenT :: CharParser st Token
-cParenT = Lexer.cParenT << many white
+cParenT = pToken $ string ")"
 
 parens :: CharParser st a -> CharParser st a
 parens p = oParenT >> p << cParenT
 
-commentLineStart :: String
-commentLineStart = ";"
-
-newLinec :: String
-newLinec = "\n\r"
-
-whitec :: String
-whitec = newLinec ++ "\t\v\f "
-
-whiteSpace :: CharParser st String
-whiteSpace = many1 $ oneOf whitec
-
-andKey :: CharParser st Id.Token
-andKey = pToken (string andS) <?> "conjunction"
-
-notKey :: CharParser st Id.Token
-notKey = pToken (string notS) <?> "negation"
-
-orKey :: CharParser st Id.Token
-orKey = pToken (string orS) <?> "disjunction"
-
-ifKey :: CharParser st Id.Token
-ifKey = pToken (string implS) <?> "implication"
-
-iffKey :: CharParser st Id.Token
-iffKey = pToken (string equivS) <?> "equivalence"
-
-forallKey :: CharParser st Id.Token
-forallKey = pToken (string forallS) <?> "universal quantification"
-
-existsKey :: CharParser st Id.Token
-existsKey = pToken (string existsS) <?> "existential quantification"
-
-equalsKey :: CharParser st Id.Token
-equalsKey = pToken (string "=") <?> "equation"
-
-notEqualKey :: CharParser st Id.Token
-notEqualKey = pToken (string "/=") <?> "inequality"
+key :: String -> CharParser st Id.Token
+key s = pToken $ try $ string s << notFollowedBy (satisfy kifWordChar)
 
 word :: CharParser st String
-word = do c <- satisfy kifInitialChar
-          s <- many (satisfy kifWordChar)
-          return $ c:s
+word = satisfy kifInitialChar <:> many (satisfy kifWordChar)
 
 quotedChar :: CharParser st Char
 quotedChar = do
-  try (satisfy kifChar) <|> try (satisfy kifUnofficial)
-  <|> try (char '\\' >> char '\"')
+  satisfy kifChar <|> satisfy kifUnofficial
+  <|> (char '\\' >> char '\"')
 
 quotedString :: CharParser st String
 quotedString = do
@@ -80,10 +54,7 @@ quotedString = do
   return $ q1:s++[q2]
 
 variable :: CharParser st String
-variable = do
-  c <- oneOf "?@"
-  s <- word
-  return $ c:s
+variable = oneOf "?@" <:> word
 
 sign :: Num a => CharParser st a
 sign = liftM (maybe 1 (const (-1))) (optionMaybe $ char '-')
@@ -91,8 +62,8 @@ sign = liftM (maybe 1 (const (-1))) (optionMaybe $ char '-')
 number :: CharParser st String
 number = do sgn <- sign
             prePoint <- many1 (satisfy kifDigit)
-            postPoint <- optionMaybe $ try $ char '.' >> many1 (satisfy kifDigit)
-            ex <- optionMaybe $ try $ liftM fromIntegral expo
+            postPoint <- optionMaybe $ char '.' >> many1 (satisfy kifDigit)
+            ex <- optionMaybe $ liftM fromIntegral expo
             return $ show $ (sgn::Double) * case (postPoint, ex) of
               (Just pp, Just e)  -> (read $ prePoint ++ '.' : pp) * 10**e
               (Just pp, Nothing) -> (read $ prePoint ++ '.' : pp)
@@ -106,10 +77,10 @@ expo = do char 'e'
           return $ sgn*(read e)
 
 kifUpper :: Char -> Bool
-kifUpper ch = let c = ord ch in c >= 65 && c <= 90
+kifUpper ch = Data.Char.isUpper ch && Data.Char.isAscii ch
 
 kifLower :: Char -> Bool
-kifLower ch = let c = ord ch in c >= 97 && c <= 122
+kifLower ch = Data.Char.isLower ch && Data.Char.isAscii ch
 
 kifSpecial :: Char -> Bool
 kifSpecial ch = ch `elem` "!$%&*+-,./<=>?@_~"
@@ -124,20 +95,19 @@ kifWordChar ch = kifUpper ch || kifLower ch || kifSpecial ch || kifDigit ch
 
 kifChar :: Char -> Bool
 kifChar ch = kifUpper ch || kifLower ch || kifSpecial ch || kifDigit ch
-             || ch `elem` whitec
+             || isSpace ch
 
 kifInitialChar :: Char -> Bool
 kifInitialChar ch = kifUpper ch || kifLower ch
 
 kifDigit :: Char -> Bool
-kifDigit ch = ch `elem` "0123456789"
+kifDigit = isDigit
 
 commentLine :: CharParser st String
-commentLine =
-  string commentLineStart >> many (noneOf newLinec) <?> ""
+commentLine = char ';' <:> many (noneOf "\n")
 
 white :: CharParser st String
 white =
-    whiteSpace
+    many1 (satisfy isSpace)
   <|>
     commentLine
--- CommonLogic/Parse_KIF.hs
+++ CommonLogic/Parse_KIF.hs
@@ -1,10 +1,22 @@
+{- |
+Module      :  $Header$
+Description :  Parser of the Knowledge Interchange Format
+Copyright   :  (c) Karl Luc, DFKI Bremen 2010, Eugen Kuksa and Uni Bremen 2011, Soeren Schulze 2012
+License     :  GPLv2 or higher, see LICENSE.txt
+
+Maintainer  :  s.schulze@uni-bremen.de
+Stability   :  provisional
+Portability :  portable
+-}
+
 module CommonLogic.Parse_KIF where
 
 import qualified Common.AnnoState as AnnoState
 import qualified Common.AS_Annotation as Annotation
 import CommonLogic.AS_CommonLogic as AS
 import Common.Id as Id
-import Common.Parsec
+import Common.Keywords
+import Common.Token
 
 import CommonLogic.Lexer_KIF
 
@@ -13,107 +25,108 @@ import Control.Monad (liftM)
 
 quantsent :: CharParser st SENTENCE
 quantsent = do
-  quant <- (string "forall" <|> string "exists") <?> "quantifier"
-  many1 white
+  quant <- (key forallS <|> key existsS) <?> "quantifier"
   vars <- (parens $ many1 (pToken variable)) <?> "quantified variables"
   sent <- sentence
-  return $ Quant_sent ((case quant of
-                          "forall" -> Universal
-                          "exists" -> Existential
-                          _ -> error "Unknown quantifier")
+  return $ Quant_sent ((case tokStr quant of
+                          s | s == forallS -> Universal
+                          s | s == existsS -> Existential
+                          _ -> error "Unknown quantifier in \"quantsent\"")
                        (map Name vars) sent)
-    (Range (joinRanges [rangeSpan quant,
-                                             rangeSpan vars,
-                                             rangeSpan sent]))
+    (Range (joinRanges [rangeSpan quant, rangeSpan vars, rangeSpan sent]))
 
 logsent :: CharParser st SENTENCE
 logsent = do
-    c <- andKey
+    c <- key andS <?> "conjunction"
     s <- many sentence -- joinRanges with s = []?
     return $ Bool_sent (Conjunction s) $ Range $ joinRanges [rangeSpan c,
              rangeSpan s]
   <|> do
-    c <- orKey
+    c <- key orS <?> "disjunction"
     s <- many sentence
     return $ Bool_sent (Disjunction s) $ Range $ joinRanges [rangeSpan c,
                rangeSpan s]
   <|> do
-    c <- notKey
-    s <- sentence <?> "sentence after \"not\""
+    c <- key notS <?> "negation"
+    s <- sentence <?> "sentence after \"" ++ notS ++ "\""
     return $ Bool_sent (Negation s) $ Range $ joinRanges [rangeSpan c,
                rangeSpan s]
   <|> do
-    c <- try iffKey
-    s1 <- sentence <?> "sentence after \"<=>\""
-    s2 <- sentence <?> "second sentence after \"<=>\""
+    c <- key equivS <?> "equivalence"
+    s1 <- sentence <?> "sentence after \"" ++ equivS ++ "\""
+    s2 <- sentence <?> "second sentence after \"" ++ equivS ++ "\""
     return $ Bool_sent (Biconditional s1 s2) $ Range $ joinRanges [rangeSpan c,
                                                    rangeSpan s1, rangeSpan s1]
   <|> do
-    c <- ifKey
-    s1 <- sentence <?> "sentence after \"=>\""
-    s2 <- sentence <?> "second sentence after \"=>\""
+    c <- key implS <?> "implication"
+    s1 <- sentence <?> "sentence after \"" ++ implS ++ "\""
+    s2 <- sentence <?> "second sentence after \"" ++ implS ++ "\""
     return $ Bool_sent (Implication s1 s2) $ Range $ joinRanges [rangeSpan c,
                                                    rangeSpan s1, rangeSpan s1]
 
-atom :: CharParser st ATOM
-atom = try (do
-    oParenT
-    equalsKey
-    t1 <- term <?> "term after \"=\""
-    t2 <- term <?> "second term after \"=\""
-    cParenT
-    return $ Equation t1 t2)
-  <|> do
-    t <- pToken (reserved ["end"] word) <?> "word"
-    return $ Atom (Name_term t) []
+equalAtom :: CharParser st ATOM
+equalAtom = do
+  key equalS
+  t1 <- term <?> "term after \"" ++ equalS ++ "\""
+  t2 <- term <?> "second term after \"" ++ equalS ++ "\""
+  return $ Equation t1 t2
 
-atomsent :: CharParser st SENTENCE
-atomsent = do
-  a <- atom
-  return $ Atom_sent a $ Range $ rangeSpan a
+plainAtom :: CharParser st ATOM
+plainAtom = do
+  t <- pToken word <?> "word"
+  return $ Atom (Name_term t) []
+
+atomsent :: CharParser st ATOM -> CharParser st SENTENCE
+atomsent = liftM (\a -> Atom_sent a $ Range $ rangeSpan a)
+
+plainsent :: CharParser st SENTENCE
+plainsent = atomsent plainAtom
+
+parensent :: CharParser st SENTENCE
+parensent = parens $ quantsent <|> logsent <|> relsent <|> eqsent <|> neqsent
 
 funterm :: CharParser st TERM
-funterm = parens (do relword <- pToken word <?> "funword"
-                     t <- many1 term
-                     return $ Funct_term (Name_term relword) (map Term_seq t)
-                       (Range $ joinRanges [rangeSpan relword, rangeSpan t]))
+funterm = do relword <- pToken (word <|> variable) <?> "funword"
+             t <- many1 term
+             return $ Funct_term (Name_term relword) (map Term_seq t)
+               (Range $ joinRanges [rangeSpan relword, rangeSpan t])
 
 relsent :: CharParser st SENTENCE
 relsent = do
   Funct_term p args rn <- funterm
   return $ Atom_sent (Atom p args) rn
 
+eqsent :: CharParser st SENTENCE
+eqsent = atomsent equalAtom
+
 neqsent :: CharParser st SENTENCE
 neqsent = do
-  oParenT
-  notEqualKey
+  key "/="
   t1 <- term <?> "term after \"/=\""
   t2 <- term <?> "second term after \"/=\""
-  cParenT
   let eq = (Equation t1 t2)
   let rn = Range $ rangeSpan eq
   return $ Bool_sent (Negation (Atom_sent eq rn)) rn
 
 term :: CharParser st TERM
-term = try (liftM Name_term (pToken variable))
-   <|> try (liftM Name_term (pToken word))
-   <|> try (liftM Name_term (pToken quotedString))
-   <|> try funterm
-   <|> try (liftM Name_term (pToken number))
---   <|> try sentence
+term = liftM Name_term (pToken variable)
+   <|> liftM Name_term (pToken word)
+   <|> liftM Name_term (pToken quotedString)
+   <|> liftM Name_term (pToken number)
+   <|> parens funterm
+--   <|> sentence
 
 sentence :: CharParser st SENTENCE
-sentence =
-  try (parens quantsent)
-   <|> try (parens logsent)
-   <|> try relsent
-   <|> try atomsent
-   <|> neqsent
+sentence = parensent <|> plainsent
+
+topLevelSentence :: CharParser st SENTENCE
+topLevelSentence = notFollowedBy (foldr (<|>) pzero (map key terminatingKeywords))
+                   >> sentence
 
 basicSpec :: AnnoState.AParser st BASIC_SPEC
 basicSpec = do
   many white
-  sentences <- many sentence
+  sentences <- many topLevelSentence
   let phrases = map Sentence sentences
   let text = Text phrases $ Range $ joinRanges $ map rangeSpan phrases
   let text_meta = Text_meta text Nothing Nothing []
