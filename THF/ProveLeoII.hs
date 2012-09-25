06e0297730e16674a727ef4920a7805561ba24b6
Jonathan von Schroeder <sternkinder@gmail.com>
2012-09-25 19:58:58 +0000

    increased time limit for leo so it doesn't die early on examples git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@17431 cec4b9c1-7d33-0410-9eda-942365e851bb

--- THF/ProveLeoII.hs
+++ THF/ProveLeoII.hs
@@ -20,11 +20,16 @@ import Data.Maybe (fromMaybe)
 
 pfun :: ProverFuncs
 pfun = ProverFuncs {
- cfgTimeout = \ cfg -> maybe 601 (+1) (timeLimit cfg),
- proverCommand = \ _ tmpFile cfg ->
-  let extraOptions = maybe "-po 1" (("-po 1 -t " ++) . show) (timeLimit cfg)
+ cfgTimeout = \ cfg -> maybe 601 (+20) (timeLimit cfg),
+ proverCommand = \ tout tmpFile _ ->
+  let extraOptions = ("-po 0 -t " ++) . show $ tout
   in return ("leo",words extraOptions ++ [tmpFile]),
- getMessage = \ res' _ perr -> res' ++ perr,
+ getMessage = \ res' _ perr -> let msg = res' ++ perr
+                               in if not $ null msg then msg
+                                  else unlines
+                                   ["Leo seem to have terminated early.",
+                                    "Probably increasing the timelimit",
+                                    "will help." ],
  getTimeUsed = \ line ->
   case words (fromMaybe "" $ stripPrefix "# Total time" line) of
    _ : (tim : _) -> Just $ round $ (read tim :: Float) * 1000
