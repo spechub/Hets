b93289f84597f04e3923deb5a1399b813c1a8152
Jonathan von Schroeder <sternkinder@gmail.com>
2012-09-25 19:59:32 +0000

    provers are for THF0 sublogic only git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@17436 cec4b9c1-7d33-0410-9eda-942365e851bb

--- THF/SZSProver.hs
+++ THF/SZSProver.hs
@@ -59,7 +59,7 @@ type ProverType = Prover SignTHF SentenceTHF MorphismTHF THFSl ProofTree
 
 createSZSProver :: String -> String -> ProverFuncs ->
                    ProverType
-createSZSProver name hlp d = mkAutomaticProver name THF
+createSZSProver name hlp d = mkAutomaticProver name THF0
  (proverGUI hlp name d)
  (proverCMDLautomaticBatch hlp name d)
 
