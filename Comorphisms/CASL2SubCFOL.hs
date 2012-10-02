d98249a1adad0ccd2143bc8e2f283edc1397a0ff
Christian Maeder <Christian.Maeder@dfki.de>
2012-10-02 13:45:39 +0000

    only consider proper subsorts git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@17467 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Comorphisms/CASL2SubCFOL.hs
+++ Comorphisms/CASL2SubCFOL.hs
@@ -134,7 +134,7 @@ totalizeSymbType t = case t of
 sortsWithBottom :: FormulaTreatment -> Sign f e -> Set.Set SORT -> Set.Set SORT
 sortsWithBottom m sig formBotSrts =
     let bsrts = treatFormula m Set.empty formBotSrts
-          (Rel.keysSet $ sortRel sig) (sortSet sig)
+          (Rel.keysSet . Rel.rmNullSets $ sortRel sig) (sortSet sig)
         ops = MapSet.elems $ opMap sig
         -- all supersorts inherit the same bottom element
         allSortsWithBottom s =
