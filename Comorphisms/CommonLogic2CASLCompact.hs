f38f36a47dc2fe33ec81812c2df768e60c0af912
Christian Maeder <Christian.Maeder@dfki.de>
2012-10-11 17:25:33 +0000

    shortened sublogic git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@17531 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Comorphisms/CommonLogic2CASLCompact.hs
+++ Comorphisms/CommonLogic2CASLCompact.hs
@@ -130,10 +130,8 @@ removeFromTI n ti = ti { vars = Set.delete n $ vars ti
 
 
 mapSub :: ClSl.CommonLogicSL -> CSL.CASL_Sublogics
-mapSub _ = CSL.caslTop
-        { CSL.cons_features = CSL.emptyMapConsFeature
-        , CSL.sub_features = CSL.NoSub
-        , CSL.has_part = False }
+mapSub _ = CSL.cFol
+        { CSL.cons_features = CSL.emptyMapConsFeature }
 
 mapMor :: ClMor.Morphism -> Result CMor.CASLMor
 mapMor mor = Result [] $ Just (CMor.embedMorphism ()
