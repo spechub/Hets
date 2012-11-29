1b2fbfb81387322c4058abb7919276665e2e119a
Jonathan von Schroeder <sternkinder@gmail.com>
2012-11-29 22:50:25 +0000

    changed to correct sublogic git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@17653 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Comorphisms/THFP_P_ST2THFP_P.hs
+++ Comorphisms/THFP_P_ST2THFP_P.hs
@@ -45,9 +45,9 @@ instance Comorphism THFP_P_ST2THFP_P
                 BasicSpecTHF THFFormula () ()
                 SignTHF MorphismTHF SymbolTHF () ProofTree where
     sourceLogic THFP_P_ST2THFP_P = THF
-    sourceSublogic THFP_P_ST2THFP_P = SL.tHF0_P_ST
+    sourceSublogic THFP_P_ST2THFP_P = SL.tHFP_P_ST
     targetLogic THFP_P_ST2THFP_P = THF
-    mapSublogic THFP_P_ST2THFP_P _ = Just SL.tHF0_P
+    mapSublogic THFP_P_ST2THFP_P _ = Just SL.tHFP_P
     map_theory THFP_P_ST2THFP_P = trans_theory
     has_model_expansion THFP_P_ST2THFP_P = True
 
