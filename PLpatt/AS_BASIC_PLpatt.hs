e3dbe588a77811c5660ed8e643ba659a35c62e68
Aivaras Jakubauskas <a.jakubauskas@jacobs-university.de>
2013-01-28 12:38:09 +0000

    code cleaned git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@17737 cec4b9c1-7d33-0410-9eda-942365e851bb

--- PLpatt/AS_BASIC_PLpatt.hs
+++ PLpatt/AS_BASIC_PLpatt.hs
@@ -1,14 +1,10 @@
 module PLpatt.AS_BASIC_PLpatt where
 
---import Common.Id as Id
-
-
 type Id = String
 type Var = Int
-data Form = Or Form Form | And Form Form | Not Form | Prop_bool Id
+data Form = Prop_bool Id
 data Prop = Prop Id
 data Axiom = Axiom Id Form
 data Decl = Prop_decl Prop | Axiom_decl Axiom
 type Basic_spec = [Decl]
-data Symbol = Symb{sname :: Id}
-
+data Symbol = Symbol{ sname :: Id}
