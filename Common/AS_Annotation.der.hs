e38c33b7d735aef772073840f856bfd9a42df317
Christian Maeder <Christian.Maeder@dfki.de>
2013-02-19 16:04:34 +0000

    rewrote partPrefixes git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@17757 cec4b9c1-7d33-0410-9eda-942365e851bb

--- Common/AS_Annotation.der.hs
+++ Common/AS_Annotation.der.hs
@@ -16,8 +16,8 @@ Datastructures for annotations of (Het)CASL.
 module Common.AS_Annotation where
 import Common.Id
 import Common.IRI (IRI)
+
 import Data.Maybe
-import Data.List
 import qualified Data.Map as Map
 
 -- DrIFT command
@@ -144,14 +144,12 @@ isComment c = case c of
 isAnnote :: Annotation -> Bool
 isAnnote = not . isComment
 
--- | separate prefix annotations and put them into a map 
+-- | separate prefix annotations and put them into a map
 partPrefixes :: [Annotation] -> (Map.Map String IRI, [Annotation])
-partPrefixes as = (Map.fromList $ concatMap (\(Prefix_anno p _) -> p)
-                   prefixes, other_annos)
-  where (prefixes, other_annos) =
-          partition (\a -> case a of Prefix_anno _ _ -> True
-                                     _ -> False) as
-  
+partPrefixes = foldr (\ a (m, l) -> case a of
+    Prefix_anno p _ -> (Map.union m $ Map.fromList p, l)
+    _ -> (m, a : l)) (Map.empty, [])
+
 {- | an item wrapped in preceding (left 'l_annos')
 and following (right 'r_annos') annotations.
 'opt_pos' should carry the position of an optional semicolon
