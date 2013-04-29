12c7a4176df87ce4aa54f4a50e97dff05cd65a7a
Aivaras Jakubauskas <a.jakubauskas@jacobs-university.de>
2013-04-29 10:30:31 +0000

    added auxiliary functions for MMT output handling git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@17921 cec4b9c1-7d33-0410-9eda-942365e851bb

--- MMT/Hets2mmt.hs
+++ MMT/Hets2mmt.hs
@@ -1,17 +1,59 @@
 {- |
-   a.jakubauskas@jacobs-university.de
-	A wrapper/interface for MMT
+    a.jakubauskas@jacobs-university.de
+    A wrapper/interface for MMT
 -}
-
 module MMT.Hets2mmt
     where
 
 import System.Process
-import System.IO
+import GHC.IO.Handle
+import Common.Result
+import Common.Id
+import Static.DevGraph
+import Common.LibName
 
-jar = "/hets-mmt-standalone.jar"
+jar :: String
+jar = "hets-mmt-standalone.jar"
+
+staloneclass :: String
+staloneclass = "com.simontuffs.onejar.Boot"
+
+{-
+callMMT :: String -> IO ExitCode
 callMMT fileName = rawSystem "java" ["-jar", jar, fileName]
+-}
 
-main fileName = do
-    x <- callMMT fileName
-    return Nothing
+calljar :: FilePath -> IO (String, Maybe String)
+calljar fileName = do
+  (_, Just hout, maybeErr, _) <- createProcess (
+              proc "java" ["-cp",
+                           jar,
+                           staloneclass,
+                           "-newLogic",
+                           fileName])
+              { std_out = CreatePipe }
+  cont <- hGetContents hout
+  case maybeErr of
+    (Just hErr) -> do
+            err <- hGetContents hErr
+            putStr err
+            return (cont, Just err)
+    Nothing -> return (cont, Nothing)
+
+callMMT :: FilePath -> IO [Diagnosis]
+callMMT fp = do
+    (out, maybeErr) <- calljar fp
+    case maybeErr of
+      (Just err) -> return [Diag Warning out nullRange,
+                            Diag Error err nullRange]
+      Nothing -> return [Diag Warning out nullRange]
+
+mmtRes :: FilePath -> IO (Result (LibName, LibEnv))
+mmtRes fname = do
+          putStr $ "calling MMT on " ++ fname
+          dgs <- callMMT fname
+          putStr $ show dgs
+          return (emptyRes dgs)
+
+emptyRes :: [Diagnosis] -> Result (LibName, LibEnv)
+emptyRes = (`Result` Just (emptyLibName "MMT", emptyLibEnv))
