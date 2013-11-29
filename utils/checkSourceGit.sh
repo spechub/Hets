ef4aebdc5dabc9863baefdf0a83bf9528529bb70
Jonathan von Schroeder <sternkinder@gmail.com>
2013-11-29 02:56:53 +0000

    ignore template files git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@18341 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/checkSourceGit.sh
+++ utils/checkSourceGit.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-FILES=`git ls-tree --name-only HEAD -r | grep -v Template.append.hs | grep '\.hs'`
+FILES=`git ls-tree --name-only HEAD -r | \
+        grep -v GUI/Glade/Template.append.hs | \
+        grep -v Haskell/PreludeString.append.hs | \
+        grep -v Haskell/ProgramaticaPrelude.hs \
+      | grep -v ToHaskell/test | grep -v test/ | grep -v Haskell/test \
+      | grep '\.hs$'`
 #echo $FILES | xargs -n 1 $HOME/.cabal/bin/scan -i > /dev/null
 #echo $FILES | xargs $HOME/.cabal/bin/scan
 echo $FILES | xargs $HOME/.cabal/bin/hlint -i "Use camelCase" -i "Use infix" \
