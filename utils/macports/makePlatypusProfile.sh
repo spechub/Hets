5c78047dd7a7d948367f3bed14412fbe711eb72c
Christian Maeder <Christian.Maeder@dfki.de>
2014-01-31 16:37:04 +0000

    bumped version git-svn-id: https://svn-agbkb.informatik.uni-bremen.de/Hets/trunk@18453 cec4b9c1-7d33-0410-9eda-942365e851bb

--- utils/macports/makePlatypusProfile.sh
+++ utils/macports/makePlatypusProfile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 /Users/Shared/maeder/Platypus-4.7/Platypus.app/Contents/Resources/platypus_clt \
- -O -a Hets -o "Text Window" -p /bin/bash -V 0.98 -u "C. Maeder" -I de.dfki.cps.hets \
+ -O -a Hets -o "Text Window" -p /bin/bash -V 0.99 -u "C. Maeder" -I de.dfki.cps.hets \
  -D -X "casl|het|owl|clif" -C "-v|-g|-A" -x -l hets.platypus
