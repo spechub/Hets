4a5becf8385c7875c8d6838ea12976942d302744
Christian Maeder <cmaeder@users.noreply.github.com>
2014-06-16 15:12:11 +0200

    use magic file under HETS_MAGIC

--- Common/FileType.hs
+++ Common/FileType.hs
@@ -18,12 +18,40 @@ import System.IO
 
 import Common.Utils
 
+import Data.List
+import Data.Maybe
+
 showFileType :: FilePath -> IO ()
 showFileType fn = do
-  (ex, out, err) <- executeProcess "file" ["-b", "--mime-type", fn] ""
-  case (ex, lines out) of
-    (ExitSuccess, [l]) | null err -> putStrLn l
-    _ -> do
-      hPutStrLn stderr $ "hets unexpected file type output for: " ++ fn
+  res <- getMagicFileType (Just "--mime-type") fn
+  case res of
+    Right s -> putStrLn s
+    Left err -> do
       hPutStrLn stderr err
-      exitWith ex
+      exitWith $ ExitFailure 2
+
+getMagicFileType :: Maybe String -> FilePath -> IO (Either String String)
+getMagicFileType pm fn = do
+  magic <- getEnvDef "HETS_MAGIC" ""
+  res <- if null magic then return $ Left "" else
+      getFileType Nothing Nothing magic
+  case res of
+    Right s | isInfixOf "magic" s ->
+        getFileType (Just magic) pm fn
+    _ -> do
+      putStrLn "set HETS_MAGIC to a proper magic file"
+      getFileType Nothing pm fn
+
+getFileType :: Maybe FilePath -> Maybe String -> FilePath
+  -> IO (Either String String)
+getFileType mmf mp fn = do
+  (ex, out, err) <- executeProcess "file"
+    (maybe [] (\ mf -> ["-m", mf]) mmf
+    ++ maybeToList mp ++ ["-b", fn]) ""
+  let unexp = fn ++ ": unexpected file type "
+  return $ case (ex, lines out) of
+    (ExitSuccess, ls) -> if null err then case ls of
+        [l] -> Right l
+        _ -> Left $ unexp ++ "output:\n" ++ out
+      else Left $ unexp ++ "error:\n" ++ err
+    _ -> Left $ unexp ++ "exit code: " ++ show ex
