dc69f6718cb3efa1200a90dcd14709dd94bd6bd4
Jonathan von Schroeder <sternkinder@gmail.com>
2014-07-25 00:54:15 +0200

    also test malformed examples

--- RDF/tests/runtests.sh
+++ RDF/tests/runtests.sh
@@ -28,3 +28,17 @@ do
         sort -k1,4 ./${BASENAME}.out > ./${BASENAME}.out.sorted
 	diff -q ./out/${BASENAME}.out.sorted ./${BASENAME}.out.sorted
 done
+
+for f in $BAD
+do
+	BASENAME=`basename $f .ttl`
+        echo "logic RDF" > ./out/${BASENAME}.ttl
+        echo "spec test =" >> ./out/${BASENAME}.ttl
+        echo "@base <http://www.w3.org/2001/sw/DataAccess/df1/tests/$BASENAME.ttl> ." >> ./out/${BASENAME}.ttl
+        cat $f >> ./out/${BASENAME}.ttl
+	$HETS ./out/${BASENAME}.ttl -o th > ./out/${BASENAME}.hetsout
+	cat ./out/${BASENAME}.hetsout | grep "*** Error:" -q
+	if [ $? -eq 1 ]; then
+	    echo "Example $BASENAME should have failed!"
+	fi
+done
