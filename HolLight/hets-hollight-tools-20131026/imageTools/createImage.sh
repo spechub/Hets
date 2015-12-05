3f99c558dd0cc6e03e4d0e89eb98a70e6e2e4d10
J.v.S <sternkinder@gmail.com>
2015-12-04 19:53:17 -0800

    make sure all processes are cleaned up (launchpad build will fail otherwise)

--- HolLight/hets-hollight-tools-20131026/imageTools/createImage.sh
+++
@@ -1,3 +0,0 @@
-dmtcp_coordinator --daemon --quiet
-sleep infinity | dmtcp_checkpoint --quiet ocaml -w a -I +compiler-libs -init exportTools/export.ml
-echo "------------------------------ Done running OCAML"
--- HolLight/hets-hollight-tools-20131026/imageTools/makeCheckpoint.sh
+++ HolLight/hets-hollight-tools-20131026/imageTools/makeCheckpoint.sh
@@ -1,3 +1,6 @@
+dmtcp_coordinator --daemon --quiet
+(sleep infinity | HETS_HOL_DIR="/usr/share/hol-light/" HETS_HOLLIGHT_TOOLS="${PWD}/exportTools/" dmtcp_checkpoint --quiet ocaml -w a -I +compiler-libs -init ${PWD}/exportTools/export.ml) &
+PROG_PID=$!
 while [ ! -e /tmp/exportml_done ]
  do
    sleep 0.1
@@ -9,3 +12,8 @@ mv ckpt_ocamlrun_* hol_light.dmtcp
 rm dmtcp_restart_script*.sh
 echo "------------------------------ Killing Process & Quitting Coordinator"
 dmtcp_command --quiet --quit
+for child in $(ps -o pid --no-headers --ppid ${PROG_PID})
+do
+	kill $child || true
+done
+kill ${PROG_PID} || true
