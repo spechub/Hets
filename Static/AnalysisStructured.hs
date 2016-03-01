5bddb065c6402f16e0d2992faf2f19503c8eb099
mcodescu <mscodescu@gmail.com>
2016-03-01 12:58:50 +0100

    rejecting sentences goes through

--- Static/AnalysisStructured.hs
+++ Static/AnalysisStructured.hs
@@ -76,7 +76,7 @@ import Proofs.ComputeColimit (insertColimitInGraph)
 
 import Common.Lib.Graph
 
--- import Debug.Trace
+import Debug.Trace
 import Static.ComputeTheory
 
 -- overrides CUIRIE expansion for Download_items
@@ -393,6 +393,13 @@ anaSpecAux conser addSyms lg libEnv ln dg nsig name opts eo sp rg = case sp of
         _ -> hint p2 ("nothing renamed by:\n" ++ showDoc restr "")
             $ getRange restr
       return (Reduction (replaceAnnoted sp1' asp) restr, fs, dgf)
+  Filtering asp filtering -> do
+       let sp1 = item asp
+           rname = extName "Filtering" name
+       (sp', nsig', dg') <- anaSpec addSyms lg libEnv ln dg nsig rname opts eo sp1 rg
+       (nf, dgF) <- anaFiltering lg libEnv dg' nsig' filtering 
+       return (Filtering (replaceAnnoted sp' asp) filtering, nf, dgF)
+       -- error "analysis of filterings not yet implemented"
   Minimization asp (Mini kw cm cv poss) -> do
       (nasp, nsig', dg') <- anaFreeOrCofreeSpec addSyms lg libEnv opts ln dg nsig
         name Minimize eo asp poss
@@ -733,6 +740,27 @@ anaIntersect addSyms lg libEnv ln dg nsig name opts eo asps rg = case asps of
                    return $ insLink dgl incl globalThm DGLinkIntersect node n
              dg3 <- foldM insE dg2 nsigs'
              return (newAsps, nsigs', ns, dg3)
+
+anaFiltering :: LogicGraph -> LibEnv -> DGraph -> NodeSig -> FILTERING 
+   -> Result (NodeSig, DGraph) 
+anaFiltering lg libEnv dg nsig filtering = case filtering of
+  FilterSymbolList selectOrReject syms@(G_symb_items_list lidS sItems) _ -> 
+   if not selectOrReject then do
+     let strs = map (symb_items_name lidS) sItems
+         dgThm = computeDGraphTheories libEnv dg
+         th = 
+            case (globalTheory . labDG dgThm . getNode) nsig of 
+                  Nothing -> error "error computing theory"
+                  Just t -> t
+     case th of 
+      G_theory l1 ser1 sig1 ind1 sens1 ind1' -> do
+         let gth' = G_theory l1 ser1 sig1 ind1 (foldl (\m x -> Map.delete x m) sens1 strs) ind1'
+         let (ns@(NodeSig node gsigma), dg') = insGTheory dg emptyNodeName DGEmpty gth'
+         gmor <- ginclusion lg gsigma $ getSig nsig
+         let dg2 = insLink dg' gmor globalThm SeeSource node $ getNode nsig
+         return (ns, dg2)
+    else error "1"
+  FilterBasicSpec selectOrReject bSpec _ -> error "basic spec"
 
 
 -- analysis of renamings
