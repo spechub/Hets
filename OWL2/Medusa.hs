27de5bf664bde811f402d8e43db0721d3c58362c
Till Mossakowski <till@communtu.de>
2016-03-02 09:31:15 +0100

    extract individuals and their classes from OWL ontology

--- OWL2/Medusa.hs
+++ OWL2/Medusa.hs
@@ -14,12 +14,13 @@ see https://github.com/ConceptualBlending/monster_render_system
 
 module OWL2.Medusa where
 
+import OWL2.AS
 import OWL2.Sign
 import OWL2.MS
 
 import Common.AS_Annotation
 import Common.Id
-import Common.IRI (IRI)
+import Common.IRI as IRI
 import Common.DocUtils
 import Common.Result
 import qualified Common.Lib.Rel as Rel
@@ -27,10 +28,33 @@ import qualified Common.Lib.Rel as Rel
 import Data.Maybe
 import qualified Data.Set as Set
 
-data Medusa = Medusa
+
+data Medusa = Medusa { indivs :: (Set.Set (QName,QName)) }
 
 -- | given an OWL ontology (iri and theory), compute the medusa data
-medusa :: IRI -> (Sign, [Named Axiom])
+medusa :: IRI.IRI -> (Sign, [Named Axiom])
                        -> Result Medusa
-medusa iri (sig, nsens) = return Medusa
+medusa iri (sig, nsens) = do
+  let inds = individuals sig
+      getC = getClass (map sentence nsens)
+  return $ Medusa $ Set.map (\ i -> (i,getC i)) inds
 
+-- | get the class of an individual
+getClass :: [Axiom] -> QName -> QName
+getClass axs n = case mapMaybe (getClassAux n) axs of
+   (c:_) -> c
+   [] -> QN { localPart = "unknown" }
+
+getClassAux :: QName -> Axiom -> Maybe QName
+getClassAux ind ax =
+  case axiomTopic ax of
+    SimpleEntity e | cutIRI e == ind ->
+      case axiomBit ax of
+         ListFrameBit (Just Types) (ExpressionBit classes) -> firstClass classes
+         _ -> Nothing
+    _ -> Nothing
+
+-- | retrieve the first class of list, somewhat arbitrary
+firstClass :: AnnotatedList ClassExpression -> Maybe QName
+firstClass ((_,Expression c):_) = Just c
+firstClass _ = Nothing
--- OWL2/MedusaToJson.hs
+++ OWL2/MedusaToJson.hs
@@ -13,9 +13,10 @@ JSON output for Medusa
 
 module OWL2.MedusaToJson (medusaToJson, medusaToJsonString) where
 
-
+import OWL2.AS
 import OWL2.Medusa
 import Common.Json
+import qualified Data.Set as Set
 
 -- render Medusa data as JSON string
 medusaToJsonString :: Medusa -> String
@@ -24,7 +25,12 @@ medusaToJsonString = ppJson . medusaToJson
 -- render Medusa data as JSON
 medusaToJson :: Medusa -> Json
 medusaToJson m = JObject [("Definitions",defs),("Relations",rels)]
-  where defs = JArray []
+  where inds = Set.toList $ indivs m
+        defs = JArray $ map indToJson inds
         rels = JArray []
 
-
+-- convert an OWL2 individual with its type to JSON
+indToJson :: (QName,QName) -> Json
+indToJson (i,t) = 
+  JObject [("Identifier",JString $ localPart i),
+           ("Type",JString $ localPart t)]
