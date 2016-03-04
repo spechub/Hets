6da7cba97b6cfe06626e61a66931a7f4b4d9ca80
Till Mossakowski <till@communtu.de>
2016-03-04 06:43:32 +0100

    merged

--- OWL2/Medusa.hs
+++ OWL2/Medusa.hs
@@ -73,14 +73,8 @@ getClassAux ind ax =
 --  and return i1 type(p1) i2 type(p2)
 getMeetsFacts :: [Axiom] -> Set.Set (QName, QName) -> QName ->
               Set.Set (QName, QName, QName, QName)
-<<<<<<< HEAD
-getMeetsFacts axs tInds n = case mapMaybe (getMeetsFactsAux axs tInds n) axs of
-   [] -> Set.empty
-   x -> Set.fromList x
-=======
 getMeetsFacts axs tInds n = 
   Set.fromList $ mapMaybe (getMeetsFactsAux axs tInds n) axs
->>>>>>> origin/medusa
 
 getMeetsFactsAux :: [Axiom] -> Set.Set (QName, QName) -> QName -> Axiom -> Maybe (QName, QName, QName, QName)
 getMeetsFactsAux axs tInds point1 ax =
@@ -100,17 +94,8 @@ getMeetsFactsAux axs tInds point1 ax =
 getFiatBoundaryFacts :: [Axiom] -> Set.Set (QName, QName) -> QName -> QName ->
                      Maybe (QName, QName, QName, QName)
 getFiatBoundaryFacts axs tInds point1 point2 =
-<<<<<<< HEAD
-   let i1 = case mapMaybe (getFiatBoundaryFactsAux point1) axs of
-              (c:_) -> Just c
-              [] -> Nothing
-       i2 = case mapMaybe (getFiatBoundaryFactsAux point2) axs of
-              (c:_) -> Just c
-              [] -> Nothing
-=======
    let i1 = checkMapMaybe (getFiatBoundaryFactsAux point1) axs 
        i2 = checkMapMaybe (getFiatBoundaryFactsAux point2) axs
->>>>>>> origin/medusa
        typeOf ind =
          case Set.toList $ Set.filter (\(x, _) -> x == ind) tInds of
            [(_, t)] -> t
