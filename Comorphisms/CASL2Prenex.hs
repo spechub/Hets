a81a3e6bff3e7b764d78c2864a1324b15e4135fa
mcodescu <mscodescu@gmail.com>
2016-05-18 08:35:55 +0200

    keep names of sentences, formatting

--- Comorphisms/CASL2Prenex.hs
+++ Comorphisms/CASL2Prenex.hs
@@ -9,9 +9,6 @@ Maintainer  :  codescu@iws.cs.uni-magdeburg.de
 Stability   :  provisional
 Portability :  non-portable (imports Logic.Comorphism)
 
-as described in 
-http://resources.mpi-inf.mpg.de/departments/rg1/teaching/autrea-ss10/script/lecture10.pdf
-
 -}
 
 module Comorphisms.CASL2Prenex where
@@ -38,7 +35,7 @@ data CASL2Prenex = CASL2Prenex deriving Show
 
 instance Language CASL2Prenex where
     language_name CASL2Prenex = "CASL2Prenex"
-       
+
 instance Comorphism CASL2Prenex
                CASL CASL_Sublogics
                CASLBasicSpec CASLFORMULA SYMB_ITEMS SYMB_MAP_ITEMS
@@ -61,7 +58,8 @@ instance Comorphism CASL2Prenex
     has_model_expansion CASL2Prenex = True -- check
     is_weakly_amalgamable CASL2Prenex = True --check
 
--- remove equivalences and implications, turn arbitrary junctions into binary ones
+-- remove equivalences and implications,
+-- turn arbitrary junctions into binary ones
 preprocessSen :: CASLFORMULA -> CASLFORMULA
 preprocessSen sen = case sen of
  Relation sen1 Equivalence sen2 _ -> let -- remove equivalences
@@ -79,9 +77,9 @@ preprocessSen sen = case sen of
    sen2' = preprocessSen sen2
   in Junction Dis [Negation sen2' nullRange, sen1'] nullRange
  Junction j sens _ -> let -- only binary junctions
-   sens' = map preprocessSen sens 
+   sens' = map preprocessSen sens
    bindLeft [] = Junction j [] nullRange
-   bindLeft [x] = Junction j [x] nullRange 
+   bindLeft [x] = Junction j [x] nullRange
    bindLeft [x,y] = Junction j [x,y] nullRange
    bindLeft (x:y:s') = Junction j [x, bindLeft (y:s')] nullRange
   in bindLeft sens'
@@ -97,48 +95,56 @@ preprocessSen sen = case sen of
 
 computePNF :: Int -> CASLFORMULA -> (Int, CASLFORMULA)
 computePNF n sen = case sen of
- Negation nsen _ -> let 
+ Negation nsen _ -> let
    (n', nsen') = computePNF n nsen
    (n'', nsen'') = case nsen' of
     Quantification q vdecls qsen _ -> -- deMorgan
-       (n', Quantification (dualQuant q) vdecls (Negation qsen nullRange) nullRange)
+       (n', Quantification (dualQuant q) vdecls
+              (Negation qsen nullRange) nullRange)
     _ -> (n', Negation nsen' nullRange)
-  in (n'', nsen'') 
+  in (n'', nsen'')
  Junction j [sen1, sen2] _ -> let
    (n', sen1') = computePNF n sen1
    (n'', sen2') = computePNF n' sen2
-   (n3, sen') = case (sen1', sen2') of 
+   (n3, sen') = case (sen1', sen2') of
      (Quantification q vdecls qsen _, aSen) -> let
          vars = flatVAR_DECLs vdecls
          (n''', vars') = getFreshVars vars n''
          vdecls' = map (\(v,s) -> mkVarDecl v s) $ map fst vars'
-         qsen' = foldl (\f (v,s,t)-> substitute v s t f) qsen $  
-                        map (\((x,y),(z,t)) -> (z, t, Qual_var x y nullRange)) vars'
+         qsen' = foldl (\f (v,s,t)-> substitute v s t f) qsen $
+                        map (\((x,y),(z,t)) -> (z, t, Qual_var x y nullRange))
+                        vars'
          (n4, qsen'') = computePNF n''' (Junction j [qsen', aSen] nullRange)
         in (n4, Quantification q vdecls' qsen'' nullRange)
      (aSen, Quantification q vdecls qsen _) -> let
          vars = flatVAR_DECLs vdecls
          (n''', vars') = getFreshVars vars n''
          vdecls' = map (\(v,s) -> mkVarDecl v s) $ map fst vars'
-         qsen' = foldl (\f (v,s,t)-> substitute v s t f) qsen $  
-                        map (\((x,y),(z,t)) -> (z, t, Qual_var x y nullRange)) vars'
+         qsen' = foldl (\f (v,s,t)-> substitute v s t f) qsen $
+                        map (\((x,y),(z,t)) -> (z, t, Qual_var x y nullRange))
+                        vars'
          (n4, qsen'') = computePNF n''' (Junction j [aSen, qsen'] nullRange)
         in (n4, Quantification q vdecls' qsen'' nullRange)
      _ -> (n'', Junction j [sen1', sen2'] nullRange)
   in (n3, sen')
- Quantification q vars qsen _ -> let 
+ Quantification q vars qsen _ -> let
    (n', qsen') = computePNF n qsen
-  in (n', Quantification q vars qsen' nullRange) 
+  in (n', Quantification q vars qsen' nullRange)
  _ -> (n, sen)
 
 getFreshVars :: [(VAR,SORT)] -> Int -> (Int, [((VAR,SORT),(VAR,SORT))])
-getFreshVars oldVars n = let 
-  (n', newVars) = foldl (\(x, vs) (_, s) -> (x+1, (genNumVar "x" n, s):vs)) (n, []) $ oldVars
+getFreshVars oldVars n = let
+  (n', newVars) = foldl
+                   (\(x, vs) (_, s) -> (x+1, (genNumVar "x" n, s):vs))
+                   (n, []) $ oldVars
  in (n', zip newVars $ reverse oldVars)
-  
-mapTheory :: (CASLSign, [Named CASLFORMULA]) -> Result (CASLSign, [Named CASLFORMULA])
-mapTheory (sig, nsens) = do
- let sens = foldl (\sens0 s -> let (_, s') =  computePNF 0 $ preprocessSen s
-                               in s':sens0) [] $ map sentence nsens
- return (sig, map (makeNamed "") sens)
 
+mapTheory :: (CASLSign, [Named CASLFORMULA]) ->
+          Result (CASLSign, [Named CASLFORMULA])
+mapTheory (sig, nsens) = do
+ let nsens' = map (\nsen -> nsen{sentence =
+      let
+       s = preprocessSen $ sentence nsen
+       (_, s') = computePNF 0 s
+      in s'}) nsens
+ return (sig, nsens')
