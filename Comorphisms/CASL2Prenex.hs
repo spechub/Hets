e2fdd28e29f151215a081dd95fcc443a4c8ccf23
mcodescu <mscodescu@gmail.com>
2016-06-07 14:25:49 +0200

    corrected CASL2Prenex

--- Comorphisms/CASL2Prenex.hs
+++ Comorphisms/CASL2Prenex.hs
@@ -48,103 +48,137 @@ instance Comorphism CASL2Prenex
                CASLMor
                Symbol RawSymbol ProofTree where
     sourceLogic CASL2Prenex = CASL
-    sourceSublogic CASL2Prenex = SL.caslTop --TODO
+    sourceSublogic CASL2Prenex = SL.caslTop
     targetLogic CASL2Prenex = CASL
     mapSublogic CASL2Prenex sl = Just sl -- TODO
     map_theory CASL2Prenex = mapTheory
-    map_morphism CASL2Prenex = error "nyi"
-    map_sentence CASL2Prenex _ _ = error "nyi"
+    map_morphism CASL2Prenex = return
+    map_sentence CASL2Prenex sig = return . (mapSentence sig)
     map_symbol CASL2Prenex _ = Set.singleton . id
-    has_model_expansion CASL2Prenex = True -- check
-    is_weakly_amalgamable CASL2Prenex = True --check
+    has_model_expansion CASL2Prenex = True
+    is_weakly_amalgamable CASL2Prenex = True
 
--- remove equivalences and implications,
--- turn arbitrary junctions into binary ones
+-- remove equivalences
 preprocessSen :: CASLFORMULA -> CASLFORMULA
 preprocessSen sen = case sen of
- Relation sen1 Equivalence sen2 _ -> let -- remove equivalences
-   sen1' = preprocessSen sen1
-   sen2' = preprocessSen sen2
-  in Junction Con [Junction Dis [Negation sen1' nullRange, sen2'] nullRange,
-                   Junction Dis [Negation sen2' nullRange, sen1'] nullRange
-                  ] nullRange
- Relation sen1 Implication sen2 _ -> let -- remove implications
-   sen1' = preprocessSen sen1
-   sen2' = preprocessSen sen2
-  in Junction Dis [Negation sen1' nullRange, sen2'] nullRange
- Relation sen1 RevImpl sen2 _ -> let -- remove reverse implications
-   sen1' = preprocessSen sen1
-   sen2' = preprocessSen sen2
-  in Junction Dis [Negation sen2' nullRange, sen1'] nullRange
- Junction j sens _ -> let -- only binary junctions
-   sens' = map preprocessSen sens
-   bindLeft [] = Junction j [] nullRange
-   bindLeft [x] = Junction j [x] nullRange
-   bindLeft [x,y] = Junction j [x,y] nullRange
-   bindLeft (x:y:s') = Junction j [x, bindLeft (y:s')] nullRange
-  in bindLeft sens'
- Quantification q vdecls qsen _ -> let -- recursion
-   qsen' = preprocessSen qsen
-  in Quantification q vdecls qsen' nullRange
+ Quantification q vdecls sen' r ->
+   Quantification q vdecls (preprocessSen sen') r
+ Junction j sens r -> 
+   Junction j (map preprocessSen sens) r
+ Relation sen1 Equivalence sen2 _ -> let
+    sen1' = Relation sen1 Implication sen2 nullRange
+    sen2' = Relation sen2 Implication sen1 nullRange
+   in Junction Con [sen1', sen2'] nullRange
+ Relation sen1 rel sen2 r -> 
+   Relation (preprocessSen sen1) rel 
+            (preprocessSen sen2) r
+ Negation sen' r -> 
+   Negation (preprocessSen sen') r
  _ -> sen
 
--- function 2: computePNF
--- see page 292 in LPL.pdf
--- but instead of checking that a variable occurs free in a formula
--- I do a alpha renaming such that this property is ensured
+-- make variables distinct: 
+-- if a variable v of sort s already appears in a formula
+-- substitute it with a fresh variable
+mkDistVars :: CASLFORMULA -> CASLFORMULA
+mkDistVars sen = 
+ let (_, _, sen') = mkDistVarsAux 0 [] sen
+ in sen'
 
-computePNF :: Int -> CASLFORMULA -> (Int, CASLFORMULA)
-computePNF n sen = case sen of
- Negation nsen _ -> let
-   (n', nsen') = computePNF n nsen
-   (n'', nsen'') = case nsen' of
-    Quantification q vdecls qsen _ -> -- deMorgan
-       (n', Quantification (dualQuant q) vdecls
-              (Negation qsen nullRange) nullRange)
-    _ -> (n', Negation nsen' nullRange)
-  in (n'', nsen'')
- Junction j [sen1, sen2] _ -> let
-   (n', sen1') = computePNF n sen1
-   (n'', sen2') = computePNF n' sen2
-   (n3, sen') = case (sen1', sen2') of
-     (Quantification q vdecls qsen _, aSen) -> let
-         vars = flatVAR_DECLs vdecls
-         (n''', vars') = getFreshVars vars n''
-         vdecls' = map (\(v,s) -> mkVarDecl v s) $ map fst vars'
-         qsen' = foldl (\f (v,s,t)-> substitute v s t f) qsen $
-                        map (\((x,y),(z,t)) -> (z, t, Qual_var x y nullRange))
-                        vars'
-         (n4, qsen'') = computePNF n''' (Junction j [qsen', aSen] nullRange)
-        in (n4, Quantification q vdecls' qsen'' nullRange)
-     (aSen, Quantification q vdecls qsen _) -> let
-         vars = flatVAR_DECLs vdecls
-         (n''', vars') = getFreshVars vars n''
-         vdecls' = map (\(v,s) -> mkVarDecl v s) $ map fst vars'
-         qsen' = foldl (\f (v,s,t)-> substitute v s t f) qsen $
-                        map (\((x,y),(z,t)) -> (z, t, Qual_var x y nullRange))
-                        vars'
-         (n4, qsen'') = computePNF n''' (Junction j [aSen, qsen'] nullRange)
-        in (n4, Quantification q vdecls' qsen'' nullRange)
-     _ -> (n'', Junction j [sen1', sen2'] nullRange)
-  in (n3, sen')
- Quantification q vars qsen _ -> let
-   (n', qsen') = computePNF n qsen
-  in (n', Quantification q vars qsen' nullRange)
- _ -> (n, sen)
+mkDistVarsAux :: Int -- first available suffix for vars
+              -> [(VAR, SORT)] -- vars encountered so far
+              -> CASLFORMULA
+              -> (Int, [(VAR, SORT)], CASLFORMULA)
+mkDistVarsAux n vlist sen = case sen of
+ Quantification q vdecls form range -> let
+   -- first make variables distinct for form
+   dvars = flatVAR_DECLs vdecls
+   (n', _, form') = mkDistVarsAux n (dvars ++ vlist) form
+   -- here we are not interested in the variables generated for the quantified form, so we can forget them
+   replVarDecl vds oN nN s = reverse $ 
+                              foldl (\usedD vd@(Var_decl names sort r) -> 
+                                       if sort == s then -- found it, replace oN with nN in names
+                                         let vd' = Var_decl (map (\x -> if x == oN then nN else x) names) sort r  
+                                         in vd':usedD
+                                       else vd:usedD 
+                                 ) [] vds
+   checkAndReplace (n0, knownV, quantF, quants) (x, s) =
+      if (x,s) `elem` knownV 
+       then
+         let  
+           x' = genNumVar "x" n0
+           quants' = replVarDecl quants x x' s
+           quantF' = substitute x s (Qual_var x' s nullRange) quantF
+         in (n0 + 1, (x', s):knownV, quantF', quants')
+       else (n0    , (x , s):knownV, quantF , quants )  
+   (n'', vlist', form'', vdecls') = foldl checkAndReplace (n', vlist, form', vdecls) dvars
+  in (n'', vlist', Quantification q vdecls' form'' range)
+ Junction j sens r -> let
+   (n', vlist', sens') = foldl (\(x, vs, osens) s -> 
+                           let
+                            (x', vs', s') = mkDistVarsAux x vs s
+                           in (x', vs', s':osens)) (n, vlist, []) sens
+  in (n', vlist', Junction j (reverse sens') r)
+ Relation sen1 rel sen2 r -> let
+   (n1, vlist1, sen1') = mkDistVarsAux n  vlist  sen1
+   (n2, vlist2, sen2') = mkDistVarsAux n1 vlist1 sen2
+  in (n2, vlist2, Relation sen1' rel sen2' r)
+ Negation sen' r -> let 
+   (n', vlist', sen'') = mkDistVarsAux n vlist sen' 
+  in (n', vlist', Negation sen'' r)
+ _ -> (n, vlist, sen)
+       
+computePNF :: CASLFORMULA -> CASLFORMULA
+computePNF sen = case sen of 
+ Negation nsen _ -> let 
+   nsen' = computePNF nsen
+   in case nsen' of 
+       Quantification q vdecls qsen _ -> 
+         Quantification (dualQuant q) vdecls 
+                        (computePNF $ Negation qsen nullRange) nullRange
+       _ -> Negation nsen' nullRange
+ Relation sen1 Implication sen2 _ -> let 
+   sen1' = computePNF sen1
+   sen2' = computePNF sen2
+   in case sen1' of 
+      Quantification q vdecls qsen _ -> 
+        Quantification (dualQuant q) vdecls (computePNF $ Relation qsen Implication sen2 nullRange) nullRange
+      _ -> case sen2' of 
+        Quantification q vdecls qsen _ -> 
+          Quantification q vdecls 
+            (computePNF $ Relation sen1' Implication qsen nullRange) nullRange 
+          -- recursive call to catch multiple quantifiers for sen2'
+        _ -> -- no quantifications
+          Relation sen1' Implication sen2' nullRange
+ Relation sen1 RevImpl sen2 _ -> 
+  computePNF $ Relation sen2 Implication sen1 nullRange
+ Quantification q vdecls qsen _ -> 
+  Quantification q vdecls (computePNF qsen) nullRange
+ Junction j sens _ -> let
+   sens' = map computePNF sens
+   processQuants asen = case asen of
+     Quantification q vdecls sen' _ -> Quantification q vdecls (processQuants sen') nullRange
+     Junction j' jsens _ -> let
+       nonQuantifiedSen (Quantification _ _ _ _) = False
+       nonQuantifiedSen _ = True
+       (nonQuantJSens, remJSens) = span nonQuantifiedSen jsens
+       in if nonQuantJSens == jsens then asen
+           else case remJSens of 
+                  (Quantification q vdecls qsen _):remJSens' -> 
+                    Quantification q vdecls (processQuants $ Junction j' (nonQuantJSens ++ (qsen: remJSens')) nullRange) nullRange
+                  _ -> error "computePNF" -- should not be possible
+     _ -> asen
+  in processQuants $ Junction j sens' nullRange
+ _ -> sen 
 
-getFreshVars :: [(VAR,SORT)] -> Int -> (Int, [((VAR,SORT),(VAR,SORT))])
-getFreshVars oldVars n = let
-  (n', newVars) = foldl
-                   (\(x, vs) (_, s) -> (x+1, (genNumVar "x" n, s):vs))
-                   (n, []) $ oldVars
- in (n', zip newVars $ reverse oldVars)
+mapSentence :: CASLSign -> CASLFORMULA -> CASLFORMULA
+mapSentence _ =
+ computePNF . mkDistVars . preprocessSen
 
 mapTheory :: (CASLSign, [Named CASLFORMULA]) ->
           Result (CASLSign, [Named CASLFORMULA])
 mapTheory (sig, nsens) = do
- let nsens' = map (\nsen -> nsen{sentence =
-      let
-       s = preprocessSen $ sentence nsen
-       (_, s') = computePNF 0 s
-      in s'}) nsens
+ let nsens' = map (\nsen -> nsen{
+                             sentence = mapSentence sig $ sentence nsen
+                             }) 
+              nsens
  return (sig, nsens')
