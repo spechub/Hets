f6a8b4d876e629841e286db08ebccb7d81f883fe
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-01-03 19:40:52 +0100

    Add TPTP Morphism.

---
+++ TPTP/Morphism.hs
@@ -0,0 +1,63 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{- |
+Module      :  ./TPTP/Morphism.hs
+Description :  Symbol-related functions for TPTP.
+Copyright   :  (c) Eugen Kuksa University of Magdeburg 2017
+License     :  GPLv2 or higher, see LICENSE.txt
+
+Maintainer  :  Eugen Kuksa <kuksa@iks.cs.ovgu.de>
+Stability   :  provisional
+Portability :  non-portable (imports Logic)
+
+Symbol-related functions for TPTP.
+-}
+
+module TPTP.Morphism ( symbolsOfSign
+                     , symbolToId
+                     ) where
+
+import TPTP.AS
+import TPTP.Sign as Sign
+
+import Common.Id
+import qualified Data.Map as Map
+
+import Data.Maybe
+import qualified Data.Set as Set
+
+mkSymbol :: Token -> SymbolType -> Symbol
+mkSymbol i t = Symbol { symbolId = i , symbolType = t }
+
+gatherTHFSymbols :: SymbolType -> THFTypeDeclarationMap -> Set.Set Symbol
+gatherTHFSymbols symbolType = Set.map fromJust . Set.filter isJust .
+  Set.map (\ x -> mkSymbolFromTHFType x symbolType) . Map.keysSet
+  where
+    mkSymbolFromTHFType :: THFTypeable -> SymbolType -> Maybe Symbol
+    mkSymbolFromTHFType x t = case x of
+      THFTypeConstant c -> Just $ Symbol { symbolId = c, symbolType = t }
+      THFTypeFormula _ -> Nothing
+
+mkSymbolFromTFFType :: Untyped_atom -> SymbolType -> Symbol
+mkSymbolFromTFFType x t = case x of
+  UA_constant c -> Symbol { symbolId = c, symbolType = t }
+  UA_system c -> Symbol { symbolId = c, symbolType = t }
+
+mkToken :: Show a => a -> Token
+mkToken = mkSimpleId . show
+
+symbolsOfSign :: Sign -> Set.Set Symbol
+symbolsOfSign sign = Set.unions [
+    Set.map (\ x -> mkSymbol x Sign.Constant) $ constantSet sign
+  , Set.map (\ x -> mkSymbol (mkToken x) Sign.Number) $ numberSet sign
+  , Set.map (\ x -> mkSymbol x Sign.Proposition) $ propositionSet sign
+  , gatherTHFSymbols Sign.TypeConstant $ thfTypeConstantMap sign
+  , Set.map (\ x -> mkSymbolFromTFFType x Sign.TypeConstant) $ Map.keysSet $ tffTypeConstantMap sign
+  , gatherTHFSymbols Sign.Predicate $ thfPredicateMap sign
+  , Set.map (\ x -> mkSymbolFromTFFType x Sign.Predicate) $ Map.keysSet $ tffPredicateMap sign
+  , Set.map (\ x -> mkSymbol x Sign.Predicate) $ Map.keysSet $ fofPredicateMap sign
+  , gatherTHFSymbols Sign.Function $ thfTypeFunctorMap sign
+  , Set.map (\ x -> mkSymbolFromTFFType x Sign.Function) $ Map.keysSet $ tffTypeFunctorMap sign
+  ]
+
+symbolToId :: Symbol -> Id
+symbolToId = simpleIdToId . symbolId
