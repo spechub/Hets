be6246297c079e8424cefcaf6716c9bf26108095
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-08-15 15:39:34 +0200

    Implement all_sublogics.

--- TPTP/Logic_TPTP.hs
+++ TPTP/Logic_TPTP.hs
@@ -79,6 +79,7 @@ instance StaticAnalysis TPTP BASIC_SPEC Sentence () () Sign Morphism Symbol ()
 instance Logic TPTP Sublogic BASIC_SPEC Sentence () () Sign Morphism Symbol () ProofTree
   where
     stability _ = Testing
+    all_sublogics TPTP = [CNF, FOF, TFF, THF]
     provers TPTP = [cvc4, darwin, eprover, geo3, isabelle, leo2, satallax,
                     spass, vampire]
 
