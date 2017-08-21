2a4b83903b3ea905199a467ebfd764fd142a9ca8
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-08-21 18:18:03 +0200

    Use http-client instead of wget.

--- Common/Http.hs
+++ Common/Http.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 {- |
 Module      :  ./Common/Http.hs
 Description :  wrapper for simple http
@@ -12,15 +13,36 @@ Portability :  non-portable (uses package HTTP)
 
 module Common.Http where
 
-import Common.Utils
-
-import System.Exit
+import Control.Exception (try)
+import qualified Data.ByteString.Lazy.Char8 as Char8
+import Network.Connection (TLSSettings(..))
+import Network.HTTP.Client
+import Network.HTTP.Client.TLS
 
 loadFromUri :: String -> IO (Either String String)
-loadFromUri str = do
-  (code, out, err) <- executeProcess "wget"
-     ["--header=Accept: */*; q=0.1, text/plain",
-      "--no-check-certificate", "-O", "-", str] ""
-  return $ case code of
-    ExitSuccess -> Right out
-    _ -> Left err
+loadFromUri uri = do
+  manager <- newManager noVerifyTlsManagerSettings
+  initialRequest <- parseRequest uri
+  let request = initialRequest
+        { requestHeaders = [ ("Accept", "*/*; q=0.1, text/plain") ]}
+  eResponse <- try $ httpLbs request manager
+  case eResponse of
+    Left err ->
+      case err :: HttpException of
+        HttpExceptionRequest _ exceptionContent ->
+          return $ Left
+            ("Failed to load " ++ show uri ++ ": " ++ show exceptionContent)
+        InvalidUrlException invalidUrl reason ->
+          return $ Left ("Failed to load " ++ show invalidUrl ++ ": " ++ reason)
+    Right response ->
+      return $ Right $ Char8.unpack $ responseBody response
+
+noVerifyTlsManagerSettings :: ManagerSettings
+noVerifyTlsManagerSettings = mkManagerSettings noVerifyTlsSettings Nothing
+
+noVerifyTlsSettings :: TLSSettings
+noVerifyTlsSettings =
+  TLSSettingsSimple { settingDisableCertificateValidation = True
+                    , settingDisableSession = True
+                    , settingUseServerName = False
+                    }
