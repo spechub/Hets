9059f40541eedf08d6053f95287bf7b42627cf36
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-08-24 13:24:05 +0200

    Revert outsourcing the function `parens`.

--- CommonLogic/Lexer_CLIF.hs
+++ CommonLogic/Lexer_CLIF.hs
@@ -18,7 +18,6 @@ module CommonLogic.Lexer_CLIF where
 import CommonLogic.AS_CommonLogic
 import Common.Id as Id
 import qualified Common.Lexer as Lexer
-import Common.Lexer (parens)
 import Common.Parsec
 import Common.Keywords
 
@@ -51,6 +50,10 @@ enclosedname = do
    c2 <- char '\"' <?> "\""
    many white
    return $ c1 : s ++ [c2]
+
+-- | parser for parens
+parens :: CharParser st a -> CharParser st a
+parens p = oParenT >> p << cParenT
 
 -- Parser Keywords
 andKey :: CharParser st Id.Token
