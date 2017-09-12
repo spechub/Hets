9b5dd04d28feffb04c8d0559b9cc6227051f6021
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-09-12 10:37:14 +0200

    Add config option for DB file.

--- Persistence/DBConfig.hs
+++ Persistence/DBConfig.hs
@@ -43,24 +43,36 @@ emptyDBConfig = DBConfig { adapter = Nothing
                          , pool = Nothing
                          }
 
-databaseConfig :: HetcatsOpts -> FilePath -> IO DBConfig
-databaseConfig opts dbFile = case databaseConfigFile opts of
-  "" -> return $ emptyDBConfig { adapter = Just "sqlite"
-                               , database = dbFile
-                               }
-  filepath -> do
-    fileExist <- doesFileExist filepath
-    if fileExist
-    then do
-      content <- BS.readFile filepath
-      case databaseSubConfig opts of
-        "" -> parseDBConfig content
-        subconfig | subconfig `elem` ["production", "development", "test"] ->
-          parseExtDBConfig subconfig content
-        _ -> fail "Persistence.DBConfig: Bad database-subconfig specified."
-    else
-      fail "Persistence.DBConfig: Database configuration file does not exist."
+databaseConfig :: HetcatsOpts -> IO DBConfig
+databaseConfig opts =
+  let dbFile = databaseOutputFile opts
+      dbConfigFile = databaseConfigFile opts
+  in case (null dbFile, null dbConfigFile) of
+       (True, True) -> fail ("No database configuration supplied. "
+                             ++ "Please specify either --database-config "
+                             ++ "or --database-file.")
+       (_, False) -> configFromYaml dbConfigFile
+       (False, _) -> return $ sqliteConfig dbFile
   where
+    sqliteConfig :: FilePath -> DBConfig
+    sqliteConfig filepath = emptyDBConfig { adapter = Just "sqlite"
+                                          , database = filepath
+                                          }
+
+    configFromYaml :: FilePath -> IO DBConfig
+    configFromYaml dbConfigFile = do
+      fileExist <- doesFileExist dbConfigFile
+      if fileExist
+      then do
+        content <- BS.readFile dbConfigFile
+        case databaseSubConfig opts of
+          "" -> parseDBConfig content
+          subconfig | subconfig `elem` ["production", "development", "test"] ->
+            parseExtDBConfig subconfig content
+          _ -> fail "Persistence.DBConfig: Bad database-subconfig specified."
+      else
+        fail "Persistence.DBConfig: Database configuration file does not exist."
+
     parseDBConfig :: BS.ByteString -> IO DBConfig
     parseDBConfig content =
       let parsedContent = Yaml.decode content :: Maybe DBConfig
