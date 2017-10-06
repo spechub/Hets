f5c9926f1490dbef623b64b2689113aeb9faa16a
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-10-06 09:40:37 +0200

    Add advisoryLocked function.

--- Persistence/Utils.hs
+++ Persistence/Utils.hs
@@ -1,8 +1,18 @@
-module Persistence.Utils where
+module Persistence.Utils ( parameterize
+                         , advisoryLocked
+                         ) where
+
+import Persistence.DBConfig
 
 import Common.Utils (replace)
+import Driver.Options
+import Persistence.Database
 
+import Control.Monad.IO.Class (MonadIO (..))
 import Data.Char
+import qualified Data.Text as Text
+import Database.Persist hiding (replace)
+import Database.Persist.Sql hiding (replace)
 
 parameterize :: String -> String
 parameterize =
@@ -32,3 +42,24 @@ parameterize =
 
     dropDashes :: [Char] -> [Char]
     dropDashes = reverse . dropWhile (== '-') . reverse . dropWhile (== '-')
+
+advisoryLocked :: MonadIO m
+               => HetcatsOpts -> String -> DBMonad m a -> DBMonad m a
+advisoryLocked opts key f =
+  case adapter $ databaseConfig opts of
+    Just "postgresql" -> do
+      advisoryLock key
+      f
+    _ -> f
+
+advisoryLock :: MonadIO m => String -> DBMonad m [Single (Maybe Text.Text)]
+advisoryLock key = do
+  let query = Text.pack (
+        "SELECT pg_advisory_xact_lock("
+        ++ advisoryLockKeyConvert
+        ++ ");")
+  rawSql query [PersistText $ Text.pack key]
+
+advisoryLockKeyConvert :: String
+advisoryLockKeyConvert =
+  "(SELECT ('x' || lpad(md5(?),16,'0'))::bit(64)::bigint)"
