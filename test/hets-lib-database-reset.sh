aa2af1b9cc87b16111fdd9f7cd300adb998a66b1
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-10-26 16:53:23 +0200

    Add SQLite to Travis.

--- test/hets-lib-database-reset.sh
+++ test/hets-lib-database-reset.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-if [ "$JOB" = "PostgreSQL" ]
-then
-  psql -U postgres -c 'drop database hets_test;' > /dev/null
-  psql -U postgres -c 'create database hets_test;' > /dev/null
-fi
-if [ "$JOB" = "MySQL" ]
-then
-  mysql -u root -e 'drop database hets_test;'
-  mysql -u root -e 'create database hets_test;'
-fi
-hets --quiet --output-types=db --database-config=$DATABASE_CONFIG --database-subconfig=test --logic-graph 2> /dev/null
+case "$JOB" in
+  "PostgreSQL")
+    psql -U postgres -c 'drop database hets_test;' > /dev/null
+    psql -U postgres -c 'create database hets_test;' > /dev/null
+    ;;
+  "MySQL")
+    mysql -u root -e 'drop database if exists hets_test;'
+    mysql -u root -e 'create database hets_test;'
+    ;;
+  "SQLite")
+    rm -rf $DATABASE_FILE
+    ;;
+esac
+
+case "$JOB" in
+  "PostgreSQL"|"MySQL")
+    hets --quiet --output-types=db --database-config=$DATABASE_CONFIG --database-subconfig=test --logic-graph 2> /dev/null
+    ;;
+  "SQLite")
+    hets --quiet --output-types=db --database-file=$DATABASE_FILE --logic-graph 2> /dev/null
+    ;;
+esac
