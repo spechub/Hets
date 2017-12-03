c3e1064ac97dff032126f79620d19ba50af79f05
Till Mossakowski <till@iws.cs.ovgu.de>
2017-12-03 11:13:09 +0100

    small examples for 5 more logics

--- PGIP/Server/Examples.hs
+++ PGIP/Server/Examples.hs
@@ -52,3 +52,85 @@ clif = "(cl-text strict_partial_order.clif\n\
        \  (forall (x y z) (if (and (< x y) (< y z)) (< x z))) // transitive\n\
        \  (forall (x y) (if (< x y) (not (< y x)))) // asymmetric\n\
        \)"
+
+propositional :: String
+propositional = "logic Propositional\n\
+                \spec Raining =\n\
+                \  props raining, wet_street\n\
+                \  . raining => wet_street\n\
+                \                    %(if raining, the street is wet)%\n\
+                \end\n\
+                \spec Scenario1 = Raining then\n\
+                \  . not wet_street    %(the street is not wet)%\n\
+                \  . not raining       %(it is not raining)%  %implied\n\
+                \end\n\
+                \spec Scenario2 = Raining then\n\
+                \  . wet_street      %(the street is wet)%\n\
+                \  %% the following does not hold...\n\
+                \  . raining         %(hence it must rain...)%  %implied\n\
+                \end"
+
+rdf :: String
+rdf = "@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .\n\
+      \@prefix dc: <http://purl.org/dc/elements/1.1/> .\n\
+      \@prefix ex: <http://example.org/stuff/1.0/> .\n\
+      \\n\
+      \<http://www.w3.org/TR/rdf-syntax-grammar>\n\
+      \  dc:title \"RDF/XML Syntax Specification (Revised)\" ;\n\
+      \  ex:editor [\n\
+      \    ex:fullname \"Dave Beckett\";\n\
+      \    ex:homePage <http://purl.org/net/dajobe/>\n\
+      \  ] ."
+
+tptp :: String
+tptp = "fof(irreflexive, axiom,\n\
+       \ ! [X]: (~ less(X, X))).\n\
+       \fof(transitive, axiom,\n\
+       \ ! [X, Y, Z]: ((less(X, Y) & less(Y, Z)) => less(X, Z))).\n\
+       \fof(asYmmetric, conjecture,\n\
+       \ ! [X, Y]: (less(X, Y) => ~ less(Y, X)))."
+
+hascasl :: String
+hascasl = "logic HasCASL\n\
+          \spec Functions =\n\
+          \  ops __o__ : forall a, b, c:Type . \n\
+          \               (b ->? c) * (a ->? b) -> (a ->? c);\n\
+          \      id    : forall a:Type . a ->? a;\n\
+          \  vars a, b, c : Type;\n\
+          \         g         : b ->? c;\n\
+          \         f         : a ->? b;\n\
+          \         x         : a\n\
+          \   . g o f = \\ x . g (f x)               %(o_def)%\n\
+          \   . id = \\ x . x                        %(id_def)%\n\
+          \ then %implies\n\
+          \  vars a, b, c , d : Type;\n\
+          \         f'         : a ->? a;\n\
+          \         h          : c ->? d;\n\
+          \         g          : b ->? c;\n\
+          \         f          : a ->? b\n\
+          \   . h o (g o f) = (h o g) o f           %(o_assoc)%\n\
+          \   . f' o id = f'                        %(id_neut)%\n\
+          \   . f' o f' = id => forall x, y:a .\n\
+          \         f' x = f' y => x = y            %(inj)%\n\
+          \   . f' o f' = id => forall x:a .\n\
+          \         exists y:a . f' y = x           %(surj)%\n\
+          \end"
+
+modal :: String
+modal = "logic Modal\n\
+        \spec M =\n\
+        \     modality empty { [](p /\\ q) => []p }\n\
+        \end\n\
+        \spec K =\n\
+        \     modality empty { [] true; [](p => q) => ([]p => []q) }\n\
+        \end\n\
+        \spec T = K then\n\
+        \     modality empty { []p => p }\n\
+        \end\n\
+        \spec S4 = T then\n\
+        \     modality empty { []A => [][]A }\n\
+        \end\n\
+        \spec S5 = T then\n\
+        \     modality empty {<>A => []<>A}\n\
+        \end"
+
