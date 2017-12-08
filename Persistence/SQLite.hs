7c36416ce369495efc8453433ce41b25802fa425
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-12-08 22:34:27 +0100

    Fix SQLite module.

--- Persistence/SQLite.hs
+++ Persistence/SQLite.hs
@@ -6,12 +6,22 @@ module Persistence.SQLite (connection) where
 
 import Persistence.DBConfig
 
-import qualified Data.ByteString.Char8 as BS
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Control
+import Control.Monad.Logger
 import Data.Maybe
-import Data.Text (Text, pack)
+import Data.Pool (Pool)
+import Data.Text (pack)
 import Database.Persist.Sqlite
 
 
+connection :: ( BaseBackend backend ~ SqlBackend
+              , IsPersistBackend backend
+              , MonadIO m
+              , MonadBaseControl IO m
+              , MonadLogger m
+              )
+           => DBConfig -> Int -> (Pool backend -> m a) -> m a
 connection dbConfig defaultPoolSize =
   withSqlitePool (pack $ database dbConfig) $
     fromMaybe defaultPoolSize $ pool dbConfig
