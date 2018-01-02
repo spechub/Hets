c9498c2d3e2ea902388dc52cd935468fd6be9dbc
Eugen Kuksa <kuksa.eugen@gmail.com>
2018-01-02 13:41:00 +0100

    Don't use PostgreSQL for hets-desktop.

--- Persistence/DatabaseConnection.hs
+++ Persistence/DatabaseConnection.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE GADTs                      #-}
 
@@ -5,11 +6,13 @@ module Persistence.DatabaseConnection (getConnection) where
 
 import Persistence.DBConfig
 
+import qualified Persistence.SQLite as SQLite
 -- MySQL support is deactivated because it requires users to install MySQL even
 -- if they want to use SQLite or PostgreSQL.
 -- import qualified Persistence.MySQL as MySQL
+#ifndef UNI_PACKAGE
 import qualified Persistence.PostgreSQL as PSQL
-import qualified Persistence.SQLite as SQLite
+#endif
 
 import Control.Monad.IO.Class
 import Control.Monad.Trans.Control
@@ -34,10 +37,17 @@ getConnection dbConfig = case adapter dbConfig of
   Just "mysql2" -> fail mySQLErrorMessage
   -- Just "mysql" -> return $ MySQL.connection dbConfig defaultPoolSize
   -- Just "mysql" -> return $ MySQL.connection dbConfig defaultPoolSize
+#ifdef UNI_PACKAGE
+  Just "postgresql" -> fail postgreSQLErrorMessage
+#else
   Just "postgresql" -> return $ PSQL.connection dbConfig defaultPoolSize
+#endif
   Just "sqlite" -> return $ SQLite.connection dbConfig defaultPoolSize
   Just "sqlite3" -> return $ SQLite.connection dbConfig defaultPoolSize
   _ -> fail ("Persistence.Database: No database adapter specified "
                ++ "or adapter unsupported.")
   where
     mySQLErrorMessage = "MySQL support is deactivated. If you need it, please let us know by filing an issue at https://github.com/spechub/Hets or write an email to hets-devel@informatik.uni-bremen.de"
+#ifdef UNI_PACKAGE
+    postgreSQLErrorMessage = "PostgreSQL support is deactivated. If you need it, please use the package hets-server instead of hets-desktop."
+#endif
