c5f7fd61fd80aea08e829992a6db9d20f2ce3a45
Jens Elkner <jel+git@iks.cs.uni-magdeburg.de>
2018-01-16 11:22:31 +0100

    simplify version checking in makefiles

--- var.mk
+++ var.mk
@@ -1,7 +1,18 @@
 # to be included by Makefile
 
+SHELL := $(shell [ -x /bin/ksh93 ] && echo '/bin/ksh93' || echo '/bin/bash' )
 OSNAME := $(shell uname -s)
 OSVERS := $(shell uname -v 2>/dev/null)
+
+# Strip off the longest prefix ending with '-' incl. of arg $1, split remaining
+# string by '.' and calculate 1.000.000 * ${major} + 1.000 * ${minor} + ${tiny)
+# what makes comparing version numbers much easier. If any part in the version
+# string is not a number, it gets replaced by '0'.
+# This macro requires features found in shells like ksh93 or bash.
+version = $(shell X="$(1)"; X="$${X\#\#*-}"; A=( $${X//./ } 0 0 0 ); \
+	A=( $$( printf "%d %d %d" "$${A[0]}" "$${A[1]}" "$${A[2]}" )); \
+	echo $$(( $${A[0]} * 1000000 + $${A[1]} * 1000 + $${A[2]} )) \
+)
 
 # If stack exists, use it. Otherwise skip it and use the system GHC.
 STACK ?= $(shell command -v stack 2> /dev/null)
@@ -12,13 +23,10 @@ STACK_DEPENDENCIES_FLAGS :=
 ifneq ($(STACK),)
     STACK_EXEC := $(STACK) exec --
     # Upgrade Haskell-Stack if the version requirement of 1.4.0 is not met
-    STACK_VERSION := $(shell stack --numeric-version)
-    STACK_VERSION_SPLIT := $(subst ., ,$(STACK_VERSION))
+    STACK_VERSION := $(call version, $(shell stack --numeric-version))
     STACK_TARGET := stack
-
-    ifneq "$(shell [ $(firstword $(STACK_VERSION_SPLIT)) -ge '1' ] && [ $(word 2,$(STACK_VERSION_SPLIT)) -ge '4' ] && echo '1')" '1'
-        STACK_UPGRADE_TARGET := stack_upgrade
-    endif
+    STACK_UPGRADE_TARGET := \
+		$(shell [ $(STACK_VERSION) -lt 1004000 ] && echo 'stack_upgrade' )
 
     ifeq "$(OSNAME)" "Darwin"
         STACK_DEPENDENCIES_FLAGS := --flag gtk:have-quartz-gtk
@@ -40,8 +48,8 @@ PROGRAMATICA_SRC_FILE ?= \
 # If both are unset or set to an empty string, programatica support is skipped.
 
 # We assume ghc 7+
-GHCVERSION := $(shell $(STACK_EXEC) ghc --numeric-version)
-GHCRTSOPTS := $(shell [ $(firstword $(subst ., ,$(GHCVERSION))) -ge 7 ] && echo '-rtsopts')
+GHCVERSION := $(call version, $(shell $(STACK_EXEC) ghc --numeric-version))
+GHCRTSOPTS := $(shell [ $(GHCVERSION) -ge 7000000 ] && echo '-rtsopts')
 
 ifneq ($(findstring SunOS, $(OSNAME)),)
   TAR = gtar
@@ -58,98 +66,58 @@ else
 endif
 
 HC = $(STACK_EXEC) ghc -optl-s -XTemplateHaskell -threaded $(GHCRTSOPTS)
+# Compile on all CPU cores in parallel if GHC >= 8.0 is used.
+HC += $(shell [ $(GHCVERSION) -ge 8000000 ] && echo '-j')
 
-HCPKG := $(STACK_EXEC) ghc-pkg
+HCPKG := $(STACK_EXEC) ghc-pkg $(GHC_PKG_FLAGS)
 
-HAXMLVERSION = $(shell $(HCPKG) latest HaXml)
-ifneq ($(findstring HaXml-1.2, $(HAXMLVERSION)),)
-HAXML_PACKAGE = -DHAXML
-endif
-ifneq ($(findstring HaXml-1.20, $(HAXMLVERSION)),)
-HAXML_PACKAGE_COMPAT = -DHAXML_COMPAT
-endif
+HAXMLVERSION := $(call version, $(shell $(HCPKG) latest HaXml))
+HAXML_PACKAGE := $(shell [ $(HAXMLVERSION) -ge 1020000 ] && echo '-DHAXML')
+HAXML_PACKAGE_COMPAT := \
+	$(shell [ $(HAXMLVERSION) -lt 1021000 ] && echo '-DHAXML_COMPAT')
 
-TARVERSION = $(shell $(HCPKG) latest tar)
-ifneq ($(findstring 0., $(TARVERSION)),)
-TAR_PACKAGE = -DTAR_PACKAGE
-endif
+TARVERSION := $(call version, $(shell $(HCPKG) latest tar))
+TAR_PACKAGE := $(shell [ $(TARVERSION) -gt 0 ] && echo '-DTAR_PACKAGE')
 
-UNIXVERSION = $(shell $(HCPKG) latest unix)
-ifneq ($(findstring 2., $(UNIXVERSION)),)
-UNIX_PACKAGE = -DUNIX
-endif
+UNIXVERSION := $(call version, $(shell $(HCPKG) latest unix))
+UNIX_PACKAGE := $(shell [ $(UNIXVERSION) -ge 2000000 ] && echo '-DUNIX')
 
-GLADEVERSION = $(shell $(HCPKG) latest glade)
-ifneq ($(findstring 0.12, $(GLADEVERSION)),)
-  GLADE_PACKAGE = -DGTKGLADE $(SUNRUNPATH)
-  ifneq ($(FIXED_GLADE),1)
-    GLADE_PACKAGE += -DGTK12
-  endif
-endif
-ifneq ($(findstring 0.13, $(GLADEVERSION)),)
-GLADE_PACKAGE = -DGTKGLADE $(SUNRUNPATH)
-endif
+GLADEVERSION := $(call version, $(shell $(HCPKG) latest glade))
+GLADE_PACKAGE := \
+	$(shell [ $(GLADEVERSION) -ge 12000 ] && echo '-DGTKGLADE $(SUNRUNPATH)')
+GLADE_PACKAGE += $(shell [ $(GLADEVERSION) -lt 13000 ] && \
+	[ $(FIXED_GLADE) = '0' ] && echo '-DGTK12')
 
-HASKELINEVERSION = $(shell $(HCPKG) latest haskeline)
-ifneq ($(findstring 0.6, $(HASKELINEVERSION)),)
-HASKELINE_PACKAGE = -DHASKELINE
-endif
-ifneq ($(findstring 0.7, $(HASKELINEVERSION)),)
-HASKELINE_PACKAGE = -DHASKELINE
-endif
+HASKELINEVERSION := $(call version, $(shell $(HCPKG) latest haskeline))
+HASKELINE_PACKAGE := \
+	$(shell [ $(HASKELINEVERSION) -ge 6000 ] && echo '-DHASKELINE')
 
-HEXPATVERSION = $(shell $(HCPKG) latest hexpat)
-ifneq ($(findstring 0., $(HEXPATVERSION)),)
-HEXPAT_PACKAGE = -DHEXPAT
-endif
+HEXPATVERSION := $(call version, $(shell $(HCPKG) latest hexpat))
+HEXPAT_PACKAGE := $(shell [ $(HEXPATVERSION) -gt 0 ] && echo '-DHEXPAT')
 
-HTTPVERSION = $(shell $(HCPKG) latest HTTP)
-ifneq ($(findstring 4000., $(HTTPVERSION)),)
-else
-HTTP_PACKAGE = -DNOHTTP
-endif
+HTTPVERSION = $(call version, $(shell $(HCPKG) latest HTTP))
+HTTP_PACKAGE = $(shell [ $(HTTPVERSION) -lt 4000000000 ] && echo '-DNOHTTP')
 
-UNIVERSION = $(shell $(HCPKG) latest uni-uDrawGraph)
-ifneq ($(findstring 2., $(UNIVERSION)),)
-UNI_PACKAGE = -DUNI_PACKAGE
-endif
+UNIVERSION := $(call version, $(shell $(HCPKG) latest uni-uDrawGraph))
+UNI_PACKAGE := $(shell [ $(UNIVERSION) -ge 2000000 ] && echo '-DUNI_PACKAGE')
 
 PFE_SETUP_FILE := programatica/tools/Setup.hs
 # If programatica src, i.e. Setup.hs et. al. is there ...
 PFE_SETUP := $(shell ls -1 $(PFE_SETUP_FILE) 2>/dev/null )
-ifneq ($(PFE_SETUP),)
 # check for haskell programatica module ...
-PROGRAMATICAVERSION = $(shell $(HCPKG) latest programatica)
-ifneq ($(findstring 1.0, $(PROGRAMATICAVERSION)),)
-# and enable programatica support
-PFE_FLAGS := -package programatica -DPROGRAMATICA
-else
-PFE_FLAGS :=
-endif
-endif
+PROGRAMATICAVERSION := $(call version, \
+	$(shell [ -n "$(PFE_SETUP)" ] && $(HCPKG) latest programatica))
+PFE_FLAGS := $(shell [ $(PROGRAMATICAVERSION) -ge 1000000 ] && \
+	echo '-package programatica -DPROGRAMATICA')
 
-WAIEXTVERSION = $(shell $(HCPKG) latest wai-extra)
-WARPVERSION = $(shell $(HCPKG) latest warp)
-HTTPTYPESVERSION = $(shell $(HCPKG) latest http-types)
-ifneq ($(findstring -1., $(WARPVERSION)),)
-  ifneq ($(findstring -1., $(WAIEXTVERSION)),)
-    ifneq ($(findstring .8, $(HTTPTYPESVERSION)),)
-      SERVER_FLAG = -DSERVER -DWARP1
-    else
-      SERVER_FLAG = -DSERVER -DWARP1 -DHTTPTYPES
-    endif
-  endif
-endif
-ifneq ($(findstring -2., $(WARPVERSION)),)
-  ifneq ($(findstring -2., $(WAIEXTVERSION)),)
-  SERVER_FLAG = -DSERVER
-  endif
-endif
-ifneq ($(findstring -3., $(WARPVERSION)),)
-  ifneq ($(findstring -3., $(WAIEXTVERSION)),)
-  SERVER_FLAG = -DSERVER -DWARP3
-  endif
-endif
+WARPVERSION := $(call version, $(shell $(HCPKG) latest warp))
+HTTPTYPESVERSION := $(call version, $(shell $(HCPKG) latest http-types))
+# warp ensures by itself, that it is linked against the proper wai package. So
+# no need to check by ourselves.
+SERVER_FLAG := $(shell [ $(WARPVERSION) -ge 1000000 ] && echo '-DSERVER')
+SERVER_FLAG += $(shell [ $(WARPVERSION) -ge 3000000 ] && echo '-DWARP3')
+SERVER_FLAG += $(shell [ $(WARPVERSION) -lt 2000000 ] && \
+	echo '-DWARP1 ' && [ $(HTTPTYPESVERSION) -ge 9000 ] && echo '-DHTTPTYPES')
 
 ifneq ($(strip $(UNI_PACKAGE)),)
   ifeq ($(strip $(HTTP_PACKAGE)),)
@@ -167,5 +135,3 @@ HC_OPTS_WITHOUTGLADE = $(PARSEC_FLAG) \
 # and the $(GLADE_PACKAGE) below
 
 HC_OPTS = $(HC_OPTS_WITHOUTGLADE) $(GLADE_PACKAGE)
-# Compile on all CPU cores in parallel if GHC >= 8.0 is used.
-HC_OPTS += $(shell [ $(firstword $(subst ., ,$(GHCVERSION))) -ge 8 ] && echo '-j')
