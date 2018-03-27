a9a3a5a71fbe8624ee64c5a293834c82beeba1ec
mscodescu <mscodescu@gmail.com>
2018-03-28 00:33:23 +0200

    added more files

---
+++ RigidCASL/Print_AS.hs
@@ -0,0 +1,59 @@
+
+{- |
+Module      :  ./RigidCASL/Print_AS.hs
+Copyright   :  (c) M. Codescu, IMAR, 2018
+License     :  GPLv2 or higher, see LICENSE.txt
+
+Maintainer  :  mscodescu@gmail.com
+Stability   :  provisional
+Portability :  portable
+
+printing rigid declarations
+-}
+
+module RigidCASL.Print_AS where
+
+import CASL.ToDoc
+
+import Common.Doc
+import Common.Lexer
+import Common.DocUtils
+import Common.Keywords
+import qualified Common.Lib.MapSet as MapSet
+
+import RigidCASL.AS_Rigid
+import RigidCASL.Sign
+import qualified Data.Set as Set
+
+instance Pretty R_SIG_ITEM where
+    pretty (Rigid_sorts ls _) =
+        cat [keyword (rigidS ++ sortS ++ pluralS ls)]
+    pretty (Rigid_op_items ls _) =
+        cat [keyword (rigidS ++ opS ++ pluralS ls),
+             space <> semiAnnos pretty ls]
+    pretty (Rigid_pred_items ls _) =
+        cat [keyword (rigidS ++ predS ++ pluralS ls),
+             space <> semiAnnos pretty ls]
+
+instance Pretty RigidExt where
+    pretty = printRigidExt
+
+--printRigidSorts :: Doc
+printRigidSorts xs = case xs of 
+   [] -> empty 
+   _ -> 
+     keyword rigidS <+> 
+     keyword (sortS ++ case xs of
+     [_] -> ""
+     _ -> "s") <+> 
+     ppWithCommas xs 
+
+printRigidExt :: RigidExt -> Doc
+printRigidExt s = let 
+  xs = Set.toList $ rigidSorts s
+ in printRigidSorts xs
+    $+$
+    printSetMap (keyword rigidS <+> keyword opS) empty
+                (MapSet.toMap $ rigidOps s)
+    $+$ printSetMap (keyword rigidS <+> keyword predS) space
+        (MapSet.toMap $ rigidPreds s)
