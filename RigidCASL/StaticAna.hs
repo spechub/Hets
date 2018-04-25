53009fcd0c878581022ddab55459e1499d7660fb
mscodescu <mscodescu@gmail.com>
2018-04-25 12:24:33 +0200

    hint for symbols redeclared as rigid

--- RigidCASL/StaticAna.hs
+++ RigidCASL/StaticAna.hs
@@ -28,9 +28,9 @@ import Common.Id
 import Common.Result
 import Common.ExtSign
 import qualified Common.Lib.MapSet as MapSet
+import Common.Lib.State
 
 import qualified Data.Set as Set
-import qualified Data.Map as Map
 
 -- TODO: for now, no checking that a rigid symbol is already in use etc.
 
@@ -48,13 +48,14 @@ anaRBasicItem :: Ana () () R_SIG_ITEM () RigidExt
 anaRBasicItem = const return
 
 anaRSigItem :: Ana R_SIG_ITEM () R_SIG_ITEM () RigidExt
-anaRSigItem mix ritem = 
+anaRSigItem mix ritem = do
+  sig <- get
   case ritem of 
     Rigid_sorts al ps ->
         do ul <- mapM (ana_SORT_ITEM typeAnaF mix NonEmptySorts) al
            mapM_ (\aoi -> case item aoi of 
                             Sort_decl sdecls _ -> 
-                              mapM_ (updateExtInfo . addRigidSort) sdecls
+                              mapM_ (updateExtInfo . addRigidSort sig) sdecls
                             _ -> error "no subsorts or isos should be rigid")
                  ul
            return $ Rigid_sorts ul ps 
@@ -62,43 +63,53 @@ anaRSigItem mix ritem =
         do ul <- mapM (ana_OP_ITEM typeAnaF mix) al
            mapM_ (\ aoi -> case item aoi of
                    Op_decl ops ty _ _ ->
-                       mapM_ (updateExtInfo . addRigidOp (toOpType ty)) ops
+                       mapM_ (updateExtInfo . addRigidOp sig (toOpType ty)) ops
                    Op_defn i par _ _ -> maybe (return ())
-                       (\ ty -> updateExtInfo $ addRigidOp (toOpType ty) i)
+                       (\ ty -> updateExtInfo $ addRigidOp sig (toOpType ty) i)
                        $ headToType par) ul
            return $ Rigid_op_items ul ps
     Rigid_pred_items al ps ->
         do ul <- mapM (ana_PRED_ITEM typeAnaF mix) al
            mapM_ (\ aoi -> case item aoi of
                    Pred_decl ops ty _ ->
-                       mapM_ (updateExtInfo . addRigidPred (toPredType ty)) ops
+                       mapM_ (updateExtInfo . addRigidPred sig (toPredType ty)) ops
                    Pred_defn i (Pred_head args _) _ _ ->
-                       updateExtInfo $ addRigidPred
+                       updateExtInfo $ addRigidPred sig
                                 (PredType $ sortsOfArgs args) i ) ul
            return $ Rigid_pred_items ul ps 
 
-addRigidSort :: Id -> RigidExt -> Result RigidExt
-addRigidSort i ext = 
- let rsorts = rigidSorts ext 
+addRigidSort :: Sign () RigidExt -> Id -> RigidExt -> Result RigidExt
+addRigidSort sig i ext = 
+ let rsorts = rigidSorts ext
+     ssorts = sortSet sig 
+     extI = ext {rigidSorts = Set.insert i $ rigidSorts ext} 
  in 
-  if Set.member i rsorts then Result [mkDiag Hint "repeated rigid sort" i] $ Just ext
-   else return 
-         ext {rigidSorts = Set.insert i $ rigidSorts ext} 
+  if Set.member i ssorts then Result [mkDiag Hint "redeclaring sort as rigid" i] $ Just extI
+   else 
+     if Set.member i rsorts then Result [mkDiag Hint "repeated rigid sort" i] $ Just ext
+      else return extI
 
-addRigidOp :: OpType -> Id -> RigidExt -> Result RigidExt
-addRigidOp ty i ext = let 
-  rops = rigidOps ext 
+addRigidOp :: Sign () RigidExt -> OpType -> Id -> RigidExt -> Result RigidExt
+addRigidOp sig ty i ext = let 
+  rops = rigidOps ext
+  ops = opMap sig
+  extI = ext {rigidOps = MapSet.insert i ty $ rigidOps ext}
  in 
-   if MapSet.member i ty rops then Result [mkDiag Hint "repeated rigid op" i] $ Just ext
-    else return ext {rigidOps = MapSet.insert i ty $ rigidOps ext}
+   if MapSet.member i ty ops then Result [mkDiag Hint "redeclaring op as rigid" i] $ Just extI 
+    else
+     if MapSet.member i ty rops then Result [mkDiag Hint "repeated rigid op" i] $ Just ext
+      else return extI
           
 
-addRigidPred :: PredType -> Id -> RigidExt -> Result RigidExt
-addRigidPred ty i ext = let 
-  rpreds = rigidPreds ext 
+addRigidPred :: Sign () RigidExt -> PredType -> Id -> RigidExt -> Result RigidExt
+addRigidPred sig ty i ext = let 
+  rpreds = rigidPreds ext
+  preds = predMap sig
+  extI = ext {rigidPreds = MapSet.insert i ty $ rigidPreds ext}
  in 
-   if MapSet.member i ty rpreds then Result [mkDiag Hint "repeated rigid pred" i] $ Just ext
-    else return ext {rigidPreds = MapSet.insert i ty $ rigidPreds ext}
+   if MapSet.member i ty preds then Result [mkDiag Hint "redeclaring pred as rigid" i] $ Just extI
+   else if MapSet.member i ty rpreds then Result [mkDiag Hint "repeated rigid pred" i] $ Just ext
+         else return extI
 
 anaMix :: Mix () R_SIG_ITEM () RigidExt
 anaMix = emptyMix
