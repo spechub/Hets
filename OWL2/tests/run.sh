226e7651d53272c6f82d49c5e9bb15adf4823bfe
Till Mossakowski <till@iws.cs.ovgu.de>
2019-05-19 17:52:34 +0200

    obey review

--- OWL2/tests/run.sh
+++ OWL2/tests/run.sh
@@ -10,21 +10,21 @@ done
 
 for i in *.rdf
 do
-  ../../hets -v2 -i owl -o th,pp.het,omn $i
+  ../../hets -v2 -i owl -o th,pp.dol,omn $i
 done
 
 for i in *.rdf *.omn
 do
   java -jar ../OWL2Parser.jar file://`pwd`/$i $i.omnm
-  ../../hets -v2 -i owl -o th,pp.het,omn $i
+  ../../hets -v2 -i owl -o th,pp.dol,omn $i
 done
 
 for i in *.het
 do
-  ../../hets -l OWL -v2 -o th,pp.het,omn $i
+  ../../hets -l OWL -v2 -o th,pp.dol,omn $i
 done
 
-for i in *.th *.pp.het *.omn
+for i in *.th *.pp.dol *.omn
 do
   ../../hets -l OWL -v2 $i
 done
@@ -35,4 +35,4 @@ do
   java -jar ../OWL2Parser.jar file://`pwd`/$i $i.omn2
 done
 
-#rm -f *.pp.het *.th *.omn *.omn2
+#rm -f *.pp.dol *.th *.omn *.omn2
