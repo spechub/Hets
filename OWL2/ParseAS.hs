fd62a7a72d7ebdbf558c963ff2c244bf9c4863d6
Björn Gehrke <5106696+b-gehrke@users.noreply.github.com>
2021-05-05 16:47:41 +0200

    Removed test code

--- OWL2/ParseAS.hs
+++ OWL2/ParseAS.hs
@@ -2,12 +2,6 @@
 
 module OWL2.ParserAS where
 
-
--- **TESTING PURPOSES ONLY**
-import System.Directory
-import Data.List
-
-
 import OWL2.AS
 
 import Common.AnnoParser (newlineOrEof)
@@ -758,39 +752,3 @@ parseOntologyDocument =
     OntologyDocument <$>
     manySkip parsePrefixDeclaration <*>
     (skipsp parseOntology)
-
--- ** TESTING PURPOSES ONLY **
-
-
-runAllTestsInDir :: FilePath -> IO ()
-runAllTestsInDir d = do
-    files <- getDirectoryContents d
-    sequence (runTest <$> filter (isSuffixOf ".ofn") (sort files))
-    return ()
-    where 
-        runTest f = do
-            content <- readFile (d ++ "/" ++ f)
-            let res = parse parseOntologyDocument f content
-            let n = ": " ++ d ++ "/" ++ f
-            putStrLn $ either (\err -> "❌ Failed" ++ n ++ ": " ++ show err) (const $ "✅ Success" ++ n) res
-
-pta :: IO ()
-pta = forget $ sequence (runAllTestsInDir <$> dirs)
-    where dirs = [
-            "./OWL2/tests",
-            "./OWL2/tests/1",
-            "./OWL2/tests/2",
-            "./OWL2/tests/3",
-            "./OWL2/tests/4",
-            "./OWL2/tests/5",
-            "./OWL2/tests/6",
-            "./OWL2/tests/7",
-            "./OWL2/tests/8",
-            "./OWL2/tests/9",
-            "./tmp"]
-
-pt :: IO ()
-pt = do
-    content <- readFile "./test.ofn"
-    parseTest parseOntologyDocument content
-    return ()
