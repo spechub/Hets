200c8a427dd72b416919004012df00838b89eedb
Björn Gehrke <5106696+b-gehrke@users.noreply.github.com>
2021-08-16 15:09:36 +0200

    Commented out conversion to ship and xml to be implemented later

--- OWL2/ProverState.hs
+++ OWL2/ProverState.hs
@@ -45,10 +45,11 @@ insertOWLAxiom :: ProverState -- ^ prover state containing initial logical part
 insertOWLAxiom pps s = pps { initialState = initialState pps ++ [s] }
 
 showOWLProblemS :: ProverState -> String -- ^ formatted output
-showOWLProblemS pst =
-    let namedSens = initialState pst
-        sign = ontologySign pst
-    in mkODoc sign (filter isAxiom namedSens)
+showOWLProblemS pst = []
+    -- TODO: commented out in 1993
+    -- let namedSens = initialState pst
+    --     sign = ontologySign pst
+    -- in mkODoc sign (filter isAxiom namedSens)
 
 {- |
   Pretty printing OWL goal for pellet or fact++
