8e46553e0d976d17a597c686668ba2acba78ee15
Jens Elkner <jel+git@iks.cs.uni-magdeburg.de>
2021-12-11 15:38:40 +0100

    test/hets-lib-check.sh: allow to check all at once

--- test/hets-lib-check.sh
+++ test/hets-lib-check.sh
@@ -1,9 +1,23 @@
 #!/bin/bash
 
-test_files=$(cat $(dirname $0)/hets-lib-testfiles)
+FLIST="$(dirname $0)/hets-lib-testfiles"
+declare -i E=0 FAIL_EARLY=1
+[[ -n $1 ]] && FAIL_EARLY=0
 
-set -ex
-for i in $test_files
-do
-  hets --quiet -a none $HETS_LIB/$i
-done
+if [[ ! -e ${FLIST} ]]; then
+	printf "${FLIST} not found. Exiting.\n"
+	exit 99
+fi
+[[ -x ./hets ]] && HETS='./hets' || HETS=`which hets`
+if [[ -z ${HETS} ]]; then
+	printf "hets not found. Exiting.\n"
+	exit 98
+fi
+
+printf "Testing named files in ${FLIST} from ${HETS_LIB:-/} ...\n"
+while read F ; do
+	./hets --quiet -a none ${HETS_LIB}/$F || (( E++ ))
+	(( FAIL_EARLY && E )) && exit 1
+done<${FLIST}
+(( $E )) && printf "Failed with $E errors.\n" || printf "Done.\n"
+exit $E
