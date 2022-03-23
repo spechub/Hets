d6dc207aa4bcc2249a4db0e57f66ff500b89cda1
Björn Gehrke <5106696+b-gehrke@users.noreply.github.com>
2022-03-23 10:09:54 +0100

    Corrected forbidden keywords check for OWL2 symbols

--- OWL2/Parse.hs
+++ OWL2/Parse.hs
@@ -165,7 +165,7 @@ fullIri = angles iriParser
 uriP :: CharParser st IRI
 uriP =
   skips $ try $ checkWithUsing showIRI uriQ $ \ q -> let p = prefixName q in
-  if null p then notElem (show $ iriPath q) owlKeywords
+  if null p then notElem (iFragment q) owlKeywords
    else notElem p $ map (takeWhile (/= ':'))
         $ colonKeywords
         ++ [ show d ++ e | d <- equivOrDisjointL, e <- [classesC, propertiesC]]
