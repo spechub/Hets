9ef0a00fc76e628c717f422e4ee21dbc67db9b4e
Björn Gehrke <5106696+b-gehrke@users.noreply.github.com>
2022-06-06 15:08:52 +0200

    Added parser for NeSy patterns

---
+++ NeSyPatterns/Parse.hs
@@ -0,0 +1,68 @@
+module NeSyPatterns.Parse (basicSpec, symb, symbItems, symbMapItems) where
+
+import Common.Keywords
+import Common.AnnoState
+import Common.Id
+import Common.Lexer
+import Common.Parsec
+
+import NeSyPatterns.AS
+
+import Data.Maybe (isJust, catMaybes)
+
+import Text.ParserCombinators.Parsec
+
+symb :: AParser st SYMB
+symb = Symb_id <$> name
+
+symbItems :: AParser st SYMB_ITEMS
+symbItems = do
+    items <- fst <$> symb `separatedBy` anComma
+    let range = concatMapRange getRange items
+    return $ Symb_items items range
+
+symbOrMap :: AParser st SYMB_OR_MAP
+symbOrMap = do
+    s1 <- symb
+    s2M <- optionMaybe (asKey mapsTo >> symb)
+    case s2M of
+        Nothing -> return $ Symb s1
+        Just s2 -> return $ Symb_map s1 s2 (concatMapRange getRange [s1, s2])
+
+symbMapItems :: AParser st SYMB_MAP_ITEMS
+symbMapItems = do
+    items <- fst <$> symbOrMap `separatedBy` anComma
+    let range = concatMapRange getRange items
+    return $ Symb_map_items items range
+
+
+name :: AParser st Token
+name = wrapAnnos $ pToken (alphaNum <:> many (alphaNum <|> char ':'))
+
+ontologyTerm :: AParser st Token
+ontologyTerm = name
+
+nodeId :: AParser st Token
+nodeId = brackets name
+
+node :: AParser st Node
+node = do
+    otM <- optionMaybe ontologyTerm
+    idM <- if isJust otM then optionMaybe nodeId else Just <$> nodeId
+    let range = catRange . catMaybes $ [otM, idM]
+    return $ Node otM idM range
+
+
+basicItem :: AParser st BASIC_ITEM
+basicItem = Path <$> fst <$> separatedBy node (asKey "->") << anSemi
+
+basicSpec :: AParser st BASIC_SPEC
+basicSpec = Basic_spec <$> annosParser basicItem
+
+
+
+-- -- Function for easier testing
+-- test :: AParser () a -> String -> a
+-- test p s = case runParser p (emptyAnnos ()) "NeSyPatterns.Parse.test" s of
+--     Left e -> error $ "***Error:"  ++ show e
+--     Right a -> a
