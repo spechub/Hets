8500e5dbcaa0a2b48af938a0d94aa9c5b31c4cac
Björn Gehrke <5106696+b-gehrke@users.noreply.github.com>
2022-11-23 15:27:17 +0100

    Added more commands for api

--- Hets.hs
+++ Hets.hs
@@ -1,2 +1,31 @@
-module Hets where
+module Hets (
+    -- Hets.Commands
+    automatic
+   , globalSubsume
+   , globalDecomposition
+   , localInference
+   , localDecomposition
+   , compositionProveEdges
+   , conservativity
+   , automaticHideTheoremShift
+   , theoremHideShift
+   , computeColimit
+   , normalForm
+   , triangleCons
+   , freeness
+   , libFlatImports
+   , libFlatDUnions
+   , libFlatRenamings
+   , libFlatHiding
+   , libFlatHeterogen
+   , qualifyLibEnv
+   , loadLibrary
+   , getGraphForLibrary
+   , getNodesFromDevelopmentGraph
+   , getLNodesFromDevelopmentGraph
+   , usableProvers
+   , autoProveNode
+   , proveNode 
+) where
 
+import Hets.Commands
