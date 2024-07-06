dd1d3fca0a472d04862429e39ee9b9ba261a02e9
JoKurth <71062996+JoKurth@users.noreply.github.com>
2024-07-06 19:19:40 +0200

    implemented testing for sublogics in propositional

---
+++ Propositional/test/runcheck.sh
@@ -0,0 +1,13 @@
+#!/bin/ksh93
+
+#first parameter is executable
+#second parameter resets ouput files
+
+SD=$( cd ${ dirname $0; }; printf "$PWD" )
+BD=${SD%/*/*}
+
+. ${BD}/Common/test/checkFunctions.sh
+
+./TestSublogic || addErr
+
+(( ! ERR ))
